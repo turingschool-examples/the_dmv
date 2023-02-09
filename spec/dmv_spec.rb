7877821ccbdde4e774ede3f1a9a2b035006ba2e6
Amy Spears <amspears007@gmail.com>
2023-02-09 16:59:12 -0700

    Debugged Dmv class

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Dmv do
   end
 
   describe '#add facilities' do
-    xit 'can add available facilities' do
+    it 'can add available facilities' do
       expect(@dmv.facilities).to eq([])
       @dmv.add_facility(@facility_1)
       expect(@dmv.facilities).to eq([@facility_1])
@@ -24,7 +24,7 @@ RSpec.describe Dmv do
   end
 
   describe '#facilities_offering_service' do
-    xit 'can return list of facilities offering a specified Service' do
+    it 'can return list of facilities offering a specified Service' do
       @facility_1.add_service('New Drivers License')
       @facility_1.add_service('Renew Drivers License')
       @facility_2.add_service('New Drivers License')
