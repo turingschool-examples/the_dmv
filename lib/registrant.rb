6bbe56ccc636fb153451a22e936b6c02c9b2e977
Reid Miller <reid.s.miller@gmail.com>
2023-02-09 14:41:42 -0700

    feat: Add permit method to Registrant class

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -2,9 +2,13 @@ class Registrant
   attr_reader :name, :age
   attr_accessor :permit
   
-  def initialize(name, age, permit = false)
+  def initialize (name, age, permit = false)
     @name = name
     @age = age
     @permit = permit
+  end
+
+  def permit?
+    permit
   end
 end
