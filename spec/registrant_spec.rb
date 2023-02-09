f9f449e27c1754021eff24b876010025eb9de1e4
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-09 18:16:14 -0500

    added new class and spec files and added the start of the spec file

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,15 @@
+require './lib/registrant'
+
+RSpec.describe Registrant do
+  
+
+  describe '#initialize' do
+    it 'exitsts' do
+      expect().to be_an_instance_of(Registrant)
+    end
+
+    it 'has attributes' do
+      expect().to eq()
+    end
+  end
+end
