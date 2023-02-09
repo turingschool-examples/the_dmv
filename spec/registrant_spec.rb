6365da6a78d679594a895c649d6bac1c13a83951
Reid Miller <reid.s.miller@gmail.com>
2023-02-09 13:57:06 -0700

    test: Add permit? test to registrant_spec

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -12,5 +12,10 @@ RSpec.describe Registrant do
       expect(@registrant_1.age).to eq(18)
       expect(@registrant_2.name).to eq('Penny')
     end
+
+    it 'has no permit by default' do
+      expect(@registrant_1.permit?).to eq(true)
+      expect(@registrant_2.permit?).to eq(false)
+    end
   end
 end
