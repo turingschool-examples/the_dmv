3b0e562d3ff01331a489f9544414362a3974a73c
Reid Miller <reid.s.miller@gmail.com>
2023-02-09 14:44:50 -0700

    feat: Add licence_data to Registration class

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,11 +1,12 @@
 class Registrant
   attr_reader :name, :age
-  attr_accessor :permit
+  attr_accessor :permit, :license_data
   
   def initialize (name, age, permit = false)
     @name = name
     @age = age
     @permit = permit
+    @license_data = {:written=>false, :license=>false, :renewed=>false}
   end
 
   def permit?
