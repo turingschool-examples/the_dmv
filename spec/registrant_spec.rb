57e2f21fa23eb1cbad0a9734e8f1e7e5cee2a933
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-09 18:23:52 -0500

    add test: for the #initialize of registrant

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,15 +1,24 @@
 require './lib/registrant'
 
 RSpec.describe Registrant do
-  
+  let(:registrant_1) { Registrant.new('Bruce', 18, true ) }
+  let(:registrant_2) { Registrant.new('Penny', 15 ) }
 
   describe '#initialize' do
     it 'exitsts' do
-      expect().to be_an_instance_of(Registrant)
+      expect(registrant_1).to be_an_instance_of(Registrant)
+      expect(registrant_2).to be_an_instance_of(Registrant)
     end
 
     it 'has attributes' do
-      expect().to eq()
+      expect(registrant_1.name).to eq('Bruce')
+      expect(registrant_1.age).to eq(15)
+      expect(registrant_1.permit?).to be(true)
+      
+
+      expect(registrant_2.name).to eq('Penny')
+      expect(registrant_2.age).to eq(15)
+      expect(registrant_2.permit?).to be(false)
     end
   end
 end
