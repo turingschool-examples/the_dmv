d14e4f579ad738ff9b3938e2074f9b8d9b17866c
Reid Miller <reid.s.miller@gmail.com>
2023-02-09 16:24:18 -0700

    test: Add vehicle registration test to dmv_spec

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Dmv do
     @facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
     @facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
     @facility_3 = Facility.new({name: 'Bend DMV Office', address: '63030 O B Riley Rd Bend OR 97701', phone: '541-388-6322' })
+
+    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
   end
 
   describe '#initialize' do
@@ -38,6 +42,15 @@ RSpec.describe Dmv do
       @dmv.add_facility(@facility_3)
 
       expect(@dmv.facilities_offering_service('Road Test')).to eq([@facility_2, @facility_3])
+    end
+  end
+
+  describe '#Iteration 2' do
+    it 'can add vehicle registration service and initialize cars' do
+      @facility_1.add_service('Vehicle Registration')
+
+      expect(@facility_1.services).to eq(['Vehicle Registration'])
+      expect(@cruz.registration_date).to be nil
     end
   end
 end
