d4de571ae09f4fc9c2d273803f2be271ef097e6c
Katie Lonsdale <katie952@gmail.com>
2023-02-09 19:10:26 -0700

    fix: registrant variable names

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,30 +1,31 @@
 require 'spec_helper'
+require './lib/registrant'
 
-RSpec.describe do
-  before :each do
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
+RSpec.describe Registrant do
+  before(:each) do
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_2 = Registrant.new('Penny', 15 )
   end
 
   describe '#initialize' do
     it 'exists' do
-      expect(registrant_1).to be_an_instance_of(Registrant)
+      expect(@registrant_1).to be_an_instance_of(Registrant)
     end
 
     it 'has a name' do
-      expect(registrant_1.name).to eq('Bruce')
+      expect(@registrant_1.name).to eq('Bruce')
     end
 
     it 'has an age' do
-      expect(registrant_1.age).to eq 18
+      expect(@registrant_1.age).to eq 18
     end
 
     it 'does not have a permit by default' do
-      expect(registrant_2.permit).to be false
+      expect(@registrant_2.permit).to be false
     end
 
     it 'has default license data' do
-      expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
   end
 
