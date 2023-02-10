c541cb0e6fc7fdb7b3ea6b621da7e905892d6ea0
Grace Joh <graceehjoh@gmail.com>
2023-02-10 11:24:16 -0600

    test: add tests for Facility #administer_written_test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -71,3 +71,39 @@ RSpec.describe Facility do
     end
   end
 end
+
+RSpec.describe Facility do
+  before(:each) do
+    @registrant_1 = Registrant.new('Bruce', 18, true)
+    @registrant_2 = Registrant.new('Penny', 16)
+    @registrant_3 = Registrant.new('Tucker', 15)
+    @facility_1 = Facility.new({ name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+    @facility_2 = Facility.new({ name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+  end
+
+    it 'can administer written test if registrant has permit and facility has service' do
+      expect(@registrant_1.permit?).to eq(true)
+
+      @facility_1.add_service('Written Test')
+
+      expect(@facility_1.administer_written_test(@registrant_1)). to eq(true)
+      expect(@registrant_1.license_data).to eq({ written: true, license: false, renewed: false })
+    end
+
+    it 'can not administer written test if facility does not have service' do
+      expect(@facility_1.services).to eq([])
+
+      @facility_1.administer_written_test(@registrant_2)
+
+      expect(@registrant_2.license_data).to eq({ written: false, license: false, renewed: false })
+    end
+
+    it 'can not administer written test if registrant does not have permit' do
+      expect(@registrant_2.license_data).to eq({ written: false, license: false, renewed: false })
+
+      @facility_1.add_service('Written Test')
+      @facility_1.administer_written_test(@registrant_2)
+
+      expect(@registrant_2.license_data).to eq({ written: false, license: false, renewed: false })
+    end
+end
