5eedde02d3a62d74d7183de0a3c8343ceb5c14fe
Reid Miller <reid.s.miller@gmail.com>
2023-02-09 17:56:42 -0700

    test: Add will not administer road test without service to registrant_spec

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -64,5 +64,14 @@ RSpec.describe Registrant do
       expect(@facility_1.administer_written_test(@registrant_3)).to be false
       expect(@registrant_3.license_data[:written]).to be false
     end
+
+    it 'will not administer road test without service' do
+      @facility_1.add_service('Written Test')
+      expect(@facility_1.administer_road_test(registrant_1)).to be false
+
+      @facility_1.add_service('Road Test')
+      expect(@facility_1.administer_road_test(registrant_1)).to be true
+      expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
+    end
   end
 end
