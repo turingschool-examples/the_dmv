858c7467e7827b14e0f02987f2d4b31d6484b379
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-02-10 08:35:24 -0700

    add tests for written_test method

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require './lib/facility'
 require './lib/vehicle'
+require './lib/registrant'
 
 RSpec.describe Facility do
   before(:each) do
@@ -26,9 +27,8 @@ RSpec.describe Facility do
     end
   end
 
-end
 
-RSpec.describe "Facility Functionality" do
+  describe "Facility Functionality" do
 
   before(:each) do
     @facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
@@ -76,8 +76,49 @@ RSpec.describe "Facility Functionality" do
     expect(@facility_2.collected_fees).to eq(0)
   end
 
-  
 
+  describe "Drivers license" do
+    before(:each) do
+      @registrant_1 = Registrant.new('Bruce', 18, true)
+      @registrant_2 = Registrant.new('Penny', 16)
+      @Registrant.new('Tucker', 15)
+      @facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      @facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+    end
 
+    it "can administer written test (registrant_1)" do
+      expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(registrant_1.permit?).to eq(true)
+      expect(facility_1.administer_written_test(registrant_1)).to eq(false)
+      expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+
+      facility_1.add_service("Written Test")
+
+      expect(facility_1.administer_written_test(registrant_1)).to eq(true)
+      expect(registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+    end
+
+    it "written test (registrant_2)" do
+      expect(registrant_2.age).to eq(16)
+      expect(registrant_2.permit?).to eq(false)
+      expect(facility_1.administer_written_test(registrant_2)).to eq(false)
+
+      registrant_2.earn_permit
+      
+      expect(facility_1.administer_written_test(registrant_2)).to eq(true)
+      expect(registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+    end
+
+    it "written test fails due to age (registrant_3)" do
+      expect(registrant_3.age).to eq(15)
+      expect(registrant_3.permit?).to eq(false)
+      expect(facility_1.administer_written_test(registrant_3)).to eq(false)
+
+      registrant_3.earn_permit
+
+      expect(facility_1.administer_written_test(registrant_3)).to eq(false)
+      expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+  end
 
 end
