277e33d926ee40dba436d834fe94119caa090881
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-10 13:41:03 -0700

    Create test for create vehicles method.

--- spec/external_data_sets_spec.rb
+++ spec/external_data_sets_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe 'Vehicle Factory' do
       expect(factory).to be_an_instance_of(VehicleFactory)
     end
   end
+
+  describe 'accesses the API' do
+    it 'can create vehicles' do
+      factory = VehicleFactory.new
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+      expect(factory.create_vehicles(wa_ev_registrations)).to eq(factory.created_vehicles)
+    end
+  end
 end
