bf489acf69563bbdd1c5c115a4bdeea91ca9b209
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-09 19:58:08 -0700

    Create test for earn permit method.

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -31,5 +31,10 @@ RSpec.describe Registrant do
       expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
       expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
+
+    it 'can earn permit' do
+      registrant_2.earn_permit
+      expect(registrant_2.permit?).to be(true)
+    end
   end
 end
