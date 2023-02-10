d14a09085a49906dbac25ecc887ba5f40378ce6d
Nick Sacco <nicksacco10@gmail.com>
2023-02-10 13:03:02 -0800

    Feat: complete permit default to false

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -3,13 +3,15 @@ class Registrant
               :age,
               :permit
 
-  def initialize(name, age, permit)
+  def initialize(name, age, permit = false)
     @name = name
     @age = age
     @permit = permit
   end
 
-  
+  def permit?
+    @permit
+  end
 
 
 
