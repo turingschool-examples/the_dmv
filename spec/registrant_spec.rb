d8b4472e783b41ef6037e060650b538fe35bf884
Thomas Hawley <thawley22@turing.edu>
2023-02-09 20:13:58 -0700

    test: Add test for registrants attributes

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,9 +1,27 @@
 require './lib/registrant'
 
 RSpec.describe Registrant do
-  it 'exists' do
-    bruce = Registrant.new('Bruce', 18, true)
+  before(:each) do
+    @registrant_1 = Registrant.new('Bruce', 18, true)
+  end
 
-    expect(bruce).to be_instance_of(Registrant)
+  it 'exists' do
+    expect(@registrant_1).to be_instance_of(Registrant)
+  end
+
+  it 'has name' do
+    expect(@registrant_1.name).to eq("Bruce")
+  end
+
+  it 'has age' do
+    expect(@registrant_1.age).to eq(18)
+  end
+
+  it 'has permit' do
+    expect(@registrant_1.permit?).to be true
+  end
+
+  it 'has license_data' do
+    expect(@registrant_1.license_data).to eq({written: false, license: false, renewed: false})
   end
 end
