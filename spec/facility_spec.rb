39df38fc3c3b4b0fa6bc4c9a154e00cc8c0feaa9
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-09 22:18:32 -0700

    Create tests for road test method.

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -78,7 +78,8 @@ RSpec.describe Facility do
   end
   
   describe 'has drivers licensing service' do
-    it 'administers written test' do
+    it 'administers tests and renews license' do
+      # Written test #
       expect(@facility_1.administer_written_test(@registrant_1)).to be(false)
       expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
       @facility_1.add_service('Written Test')
@@ -92,6 +93,17 @@ RSpec.describe Facility do
       @registrant_3.earn_permit
       expect(@facility_1.administer_written_test(@registrant_3)).to be(false)
       expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    # Road test #
+      expect(@facility_1.administer_road_test(@registrant_3)).to be(false)
+      @registrant_3.earn_permit
+      expect(@facility_1.administer_road_test(@registrant_3)).to be(false)
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@facility_1.administer_road_test(@registrant_1)).to be(false)
+      @facility_1.add_service('Road Test')
+      expect(@facility_1.administer_road_test(@registrant_1)).to be(true)
+      expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
+      expect(@facility_1.administer_road_test(@registrant_2)).to be(true)
+      expect(@registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
     end
   end
 end
