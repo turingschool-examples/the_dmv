a1ffca967346408529125ff6c51bac86cbbf279c
Reid Miller <reid.s.miller@gmail.com>
2023-02-09 18:01:13 -0700

    test: Add will not administer road test without passing written test to registrant_spec

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -73,5 +73,13 @@ RSpec.describe Registrant do
       expect(@facility_1.administer_road_test(registrant_1)).to be true
       expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
     end
+
+    it 'will not administer road test without passing written test' do
+      @facility_1.add_service('Written Test')
+      @facility_1.add_service('Road Test')
+
+      expect(@facility_1.administer_road_test(registrant_3)).to be false
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
   end
 end
