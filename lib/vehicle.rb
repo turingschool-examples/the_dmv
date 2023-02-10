1315fdc498e0ede307ffb5959253d85482f0d2ab
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-09 21:09:27 -0700

    Modify format.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -6,7 +6,7 @@ class Vehicle
               :make,
               :model,
               :engine
-  attr_accessor :registration_date
+  attr_accessor :registration_date, :plate_type
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
