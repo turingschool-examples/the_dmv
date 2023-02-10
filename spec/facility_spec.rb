c3e8beccc8ff9acb71a160dfa2a5c6efe47decd8
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-09 21:30:39 -0700

    Test: added testing for collected fees, registration date and registration

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -7,7 +7,9 @@ RSpec.describe Facility do
     @facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
   end
+  
   describe '#initialize' do
     it 'can initialize' do
       expect(@facility).to be_an_instance_of(Facility)
@@ -29,4 +31,36 @@ RSpec.describe Facility do
       expect(@facility_1.services).to eq(['Vehicle Registration'])
     end
   end
+
+  describe '#vehicle registration' do
+    it 'facilities start with no registered vehicles' do
+      expect(@facility_1.registered_vehicles).to eq[]
+    end
+    it ''
+  end
+
+  describe '#collected fees' do
+    it 'facilities start with no collected fees and charge appropriately' do
+      expect(@facility_1.collected_fees).to eq(0)
+      @facility_1.register_vehicle(cruz)
+      expect(@facility_1.collected_fees).to eq(100)
+      @facility_1.register_vehicle(camaro)
+      expect(@facility_1.collected_fees).to eq(125)
+      @facility_1.register_vehicle(bolt)
+      expect(@facility_1.collected_fees).to eq(325)
+    end
+
+    describe '#registration date' do
+      it 'vehicles start with no registration date' do
+        expect(@cruz.registration_date).to eq(nil)
+      end
+      it 'vehicles get registration date once registered' do 
+        @facility_1.register_vehicle(cruz)
+        expect(cruz.registration_date).to be_an_instance_of(Date.today.month)
+        @facility_1.register_vehicle(camaro)
+        expect(camaro.registration_date).to be_an_instance_of(Date.today.month)
+        @facility_1.register_vehicle(bolt)
+        expect(bolt.registration_date).to be_an_instance_of(Date.today.month)
+      end
+    end
 end
