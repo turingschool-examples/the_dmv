56e0f7687b1ec074c485821ab78ae76d28556434
Grace Joh <graceehjoh@gmail.com>
2023-02-10 15:47:15 -0600

    test: add test for VehicleFactory #create_vehicle

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe VehicleFactory do
       expect(@factory.vehicles).to eq([])
     end
   end
+
+  describe '#create_vehicles' do
+    it 'can recreate an array of vehicle objects' do
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+      @factory.create_vehicles(wa_ev_registrations)
+
+      expect(@factory.vehicles).to be_a(Array)
+      expect(@factory.vehicles[0]).to be_a(Vehicle)
+      expect(@factory.vehicles.length).to eq(1000)
+    end
+  end
 end
