bd8cd2060dd6b3ecffa71e5b0b5a0d2c67dca0d9
Grace Joh <graceehjoh@gmail.com>
2023-02-10 15:48:47 -0600

    feat: add VehicleFactory #create_vehicles

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -4,4 +4,21 @@ class VehicleFactory
   def initialize
     @vehicles = []
   end
+
+  def create_vehicles(data)
+    car_data = []
+    data.each do |data_hash|
+      vehicle_details = {}
+      vehicle_details[:vin] = data_hash[:vin_1_10]
+      vehicle_details[:year] = data_hash[:model_year]
+      vehicle_details[:make] = data_hash[:make]
+      vehicle_details[:model] = data_hash[:model]
+      vehicle_details[:engine] = :ev
+      car_data << vehicle_details
+    end
+    car_data.each do |vehicle_details|
+      @vehicles << Vehicle.new(vehicle_details)
+    end
+    @vehicles
+  end
 end
