ee4a618c5fe348ea14e8ac04362858ada2d91c8a
Reid Miller <reid.s.miller@gmail.com>
2023-02-09 18:09:27 -0700

    test: Add will not renew license without servie to registrant_spec

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -81,5 +81,16 @@ RSpec.describe Registrant do
       expect(@facility_1.administer_road_test(registrant_3)).to be false
       expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
+
+    it 'wont renew without service' do
+      @facility_1.add_service('Written Test')
+      @facility_1.add_service('Road Test')
+
+      expect(@facility_1.renew_drivers_license(registrant_1)).to be false
+
+      @facility_1.add_service('Renew License')
+      expect(@facility_1.renew_drivers_license(registrant_1)).to be true
+      expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
+    end
   end
 end
