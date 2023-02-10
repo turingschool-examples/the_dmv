c9cdfc9e7405c7d8deb01040e564561cb05c2f7a
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-09 20:01:35 -0700

    Create Registrant class.

---
+++ lib/registrant.rb
@@ -0,0 +1,11 @@
+
+class Registrant
+  attr_reader :name, :age
+  attr_accessor :license_data, :permit
+  def initialize(name, age, permit = false)
+    @name = name
+    @age = age
+    @license_data = {written: false, license: false, renewed: false}
+    @permit = permit
+  end
+end
