f7645c754216c8714002d6cfeef0436dd61961ba
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-10 13:30:40 -0700

    test: Add initialize tests for registrant class

---
+++ lib/registrant.rb
@@ -0,0 +1,29 @@
+class Registrant
+  
+  attr_reader :name, :age, :permit
+  def initialize(name, age, permit = false)
+    @name = name
+    @age = age
+    @permit = permit
+    @license_data = {}
+  end
+
+  def permit?
+    @permit
+  end
+  
+  def license_data
+    @license_data
+    {
+      written: false,
+      license: false,
+      renewed: false
+    }
+  end
+
+  def earn_permit
+    @permit = true
+   
+  
+  end
+end
