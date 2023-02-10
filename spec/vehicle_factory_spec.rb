c860afb977f9c242c563aa4d49585c5765bbc889
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-02-10 13:20:51 -0700

    add markdown file with washington data to study and add tests to ensure that data is readable

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -2,14 +2,33 @@ require './lib/vehicle/'
 require './lib/vehicle_factory'
 require './lib/dmv_data_service'
 
-RSPec.describe VehicleFactory do
-  it "parses data into an array of vehicles" do
-    factory = VehicleFactory.new
-    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
-    wa_vehicles = factory.create_vehicles(wa_ev_registrations)
+RSpec.describe VehicleFactory do
 
-    for i in 0..wa_vehicles.length - 1
-      expect wa_vehicles[0].to be_a Vehicle
+  before(:each) do
+    @factory = VehicleFactory.new
+    @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+    # factory.create_vehicles(wa_ev_registrations)
+  end
+  
+  it "is an array" do
+    expect(@wa_ev_registrations).to be_a(Array)
+  end
+
+  it "is an array of hashes" do
+    for i in 0..@wa_ev_registrations.count - 1
+      expect(@wa_ev_registrations[i]).to be_a(Hash)
     end
   end
+
+ 
+
 end
+
+  # it "parses data into an array of vehicles" do
+
+  #   for i in 0..wa_vehicles.length - 1
+  #     expect(wa_vehicles[i]).to be_a(Vehicle)
+  #   end
+
+#     p wa_vehicles.length
+  # end
