8ede91c87efe39b1e13b4a20e5a429ee6bdfe685
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-09 18:19:16 -0700

    Write test for earn permit

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -16,7 +16,6 @@ RSpec.describe Registrant do
   it 'exists' do
     registrant_1 = Registrant.new('Bruce', 18, true )
     registrant_2 = Registrant.new('Penny', 15 )
-    require 'pry'; binding.pry
     expect(registrant_1.name).to eq("Bruce")
     expect(registrant_1.age).to eq(18)
     expect(registrant_1.permit?).to eq(true)
@@ -25,8 +24,19 @@ RSpec.describe Registrant do
     expect(registrant_2.age).to eq(15)
     expect(registrant_2.permit?).to eq(false)
     expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
-
+    
   end
+  
+  it 'earn permit' do
+    registrant_1 = Registrant.new('Bruce', 18, true )
+    registrant_2 = Registrant.new('Penny', 15 )
+    require 'pry'; binding.pry
+    registrant_2.earn_permit
+    expect(registrant_2.permit?).to eq(true)
+  end
+    
+
+
 
 
 
