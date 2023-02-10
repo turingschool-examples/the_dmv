9faab645fcabc49859c302ac600a1ddf6e277a65
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-09 18:13:07 -0700

    Create Registrant class

---
+++ lib/registrant.rb
@@ -0,0 +1,11 @@
+class Registrant
+attr_reader :name, :age
+  def initialize(name, age, permit=false)
+    @name = name
+    @age = age
+    @permit = permit
+  end
+  def permit?
+    @permit
+  end
+end
