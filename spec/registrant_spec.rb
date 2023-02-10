bf4d8ac8e1462e4302818c5472c14d60f7cf53ed
Nick Sacco <nicksacco10@gmail.com>
2023-02-10 12:28:58 -0800

    Fix registrant test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -4,11 +4,11 @@ require './lib/registrant'
 RSpec.describe Registrant do
   it 'exists' do
     registrant_1 = Registrant.new('Bruce', 18, true )
+
     
-
     expect(registrant_1).to be_a Registrant
-    expect(registant_1.name).to eq("Bruce")
-
+    expect(registrant_1.name).to eq("Bruce")
+  
   end
 
 
