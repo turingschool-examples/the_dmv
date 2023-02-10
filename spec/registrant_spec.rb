52c5e4a8e236bbd39d317f8403b802c30827d7c2
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-10 09:28:12 -0600

    Test: add tests for #initialize in Registrant class

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+RSpec.describe Registrant do
+  before(:each) do
+    @person_1 = {
+      name: 'Bruce',
+      age: 18
+      permit: true
+    }
+
+    @person_2 = {
+      name: 'Penny',
+      age: 15
+    }
+
+    @registrant_1 = Registrant.new(@person_1[:name], @person_1[:age], @person_1[:permit])
+    @registrant_2 = Registrant.new(@person_2[:name], @person_2[:age])
+  end
+
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@registrant_1).to be_a(Registrant)
+    end
+
+    it 'can initialize without a permit argument' do
+      expect(@registrant_2).to be_a(Registrant)
+    end
+  end
+end
