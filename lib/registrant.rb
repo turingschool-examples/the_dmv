e7cb8956d167a617b2aedef0ca0b7d6bc340b252
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-10 14:30:20 -0700

    Initial commit

---
+++ lib/registrant.rb
@@ -0,0 +1,12 @@
+class Registrant
+  def initialize(name, age, permit = false, license_data)
+    @name = name 
+    @age = age
+    @permit = permit
+    @license_data = license_data
+  end
+
+
+
+  
+end
