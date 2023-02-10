7aa16d985c3ba9bbcfd4cfbc86526390dbad94ab
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-10 12:33:41 -0700

    test: add plate type test to facility_spec

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe Facility do
       expect(facility_1.registered_vehicles).to eq([cruz])
     end
 
-    it 'has a vehcile registration date' do
+    it 'has a vehicle registration date' do
       facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
       facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
 
@@ -49,6 +49,24 @@ RSpec.describe Facility do
       facility_1.add_service('Vehicle Registration')
       facility_1.register_vehicle(cruz)
       expect(cruz.registration_date).to be_an_instance_of(Date)
+    end
+
+    it 'can assign plate type' do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+
+      facility_1.add_service('Vehicle Registration')
+      facility_1.register_vehicle(cruz)
+      facility_1.register_vehicle(bolt)
+      facility_1.register_vehicle(camaro)
+
+      expect(cruz.plate_type).to eq(:regular)
+      expect(bolt.plate_type).to eq(:ev)
+      expect(camaro.plate_type).to eq(:antique)
     end
   end
 
