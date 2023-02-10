1cd26c58e7a75705058502453c8e0b1802255c62
Thomas Hawley <thawley22@turing.edu>
2023-02-09 20:14:54 -0700

    feat: Add methods for returning registrants attributs

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,8 +1,16 @@
 class Registrant
+  attr_reader :name, 
+              :age,
+              :license_data
 
-  def initialize(name, age, permit)
+  def initialize(name, age, permit = false)
     @name = name
     @age = age
     @permit = permit
+    @license_data = {written: false, license: false, renewed: false}
+  end
+
+  def permit?
+    @permit
   end
 end
