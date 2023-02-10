f7448bfea72a9b030a5f48c42b418f4e4c77f11c
Reid Miller <reid.s.miller@gmail.com>
2023-02-09 17:38:23 -0700

    test: Add test for administering test once service added to registrant_spec

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe Registrant do
   describe '#Iteration 1' do
     before(:each) do
       @registrant_1 = Registrant.new('Bruce', 18, true)
-      @registrant_2 = Registrant.new('Penny', 15)
+      @registrant_2 = Registrant.new('Penny', 16)
+      @registrant_3 = Registrant.new('Tucker' 15)
+
+      @facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      @facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
     end
 
     it 'has a name and age' do
@@ -26,6 +30,16 @@ RSpec.describe Registrant do
     it 'can earn a permit' do
       @registrant_2.earn_permit
       expect(@registrant_2.permit?).to eq(true)
+    end
+  end
+
+  describe 'Iteration 2: Getting a Drivers License' do
+    it 'can administer a test to registrant_1 after service added' do
+      expect(@facility_1.administer_written_test(@registrant_1)).to be false
+
+      @facility_1.add_service('Written Test')
+
+      expect(@facility_1.administer_written_test(@registrant_1)).to be true
     end
   end
 end
