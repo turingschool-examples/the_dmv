e394881cfd2c96eebba6c196fce091c70b6b7907
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-10 16:48:15 -0700

    Add plate type and collected fees to register vehicle method

--- lib/facility.rb
+++ lib/facility.rb
@@ -3,8 +3,9 @@ class Facility
               :address,
               :phone,
               :services,
-              :registered_vehicles,
-              :collected_fees
+              :registered_vehicles
+              
+  attr_accessor :collected_fees
 
   def initialize(hash)
     @name = hash[:name]
@@ -20,7 +21,19 @@ class Facility
   end
 
   def register_vehicle(car)
+    car.registration_date = Date.today
+    if car.antique?
+      car.plate_type = :antique
+      @collected_fees += 25
+    elsif car.electric_vehicle?
+      car.plate_type = :ev
+      @collected_fees += 200
+    else
+      car.plate_type = :regular
+      @collected_fees += 100
+    end
     @registered_vehicles << car
   end
+
 
 end
