bdd1daa054426d005070538632af1f8393423ef1
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-09 19:56:19 -0700

    Define class Registrant - first pass

---
+++ lib/registrant.rb
@@ -0,0 +1,9 @@
+class Registrant
+  attr_reader :name, :age, :license_data
+  
+  def initialize(name, age, license_data)
+    @name = name
+    @age = age
+    @license_data = @license_data
+  end
+end
