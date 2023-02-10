43102e3244e34754651a66b647defea5c2bdce92
Katie Lonsdale <katie952@gmail.com>
2023-02-09 19:58:19 -0700

    feat: add registered vehicles and collected fees attributes

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,18 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name,
+              :address,
+              :phone,
+              :services,
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(attributes)
     @name = attributes[:name]
     @address = attributes[:address]
     @phone = attributes[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
