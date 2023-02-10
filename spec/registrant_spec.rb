8c6e5ebe86fc0c2dcc6eab46532323376fa307de
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-09 20:03:31 -0700

    Modify test.

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -13,28 +13,28 @@ RSpec.describe Registrant do
     end
 
     it 'has name' do
-      expect(registrant_1.name).to eq("Bruce")
-      expect(registrant_2.name).to eq("Penny")
+      expect(@registrant_1.name).to eq("Bruce")
+      expect(@registrant_2.name).to eq("Penny")
     end
 
     it 'has age' do
-      expect(registrant_1.age).to eq(18)
-      expect(registrant_2.age).to eq(15)
+      expect(@registrant_1.age).to eq(18)
+      expect(@registrant_2.age).to eq(15)
     end
 
     it 'has permit status' do
-      expect(registrant_1.permit?).to be(true)
-      expect(registrant_2.permit?).to be(false)
+      expect(@registrant_1.permit?).to be(true)
+      expect(@registrant_2.permit?).to be(false)
     end
 
     it 'has license data' do
-      expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
-      expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
 
     it 'can earn permit' do
-      registrant_2.earn_permit
-      expect(registrant_2.permit?).to be(true)
+      @registrant_2.earn_permit
+      expect(@registrant_2.permit?).to be(true)
     end
   end
 end
