43952914f98d72a0b97f7399c2d36b0c716ffe28
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-10 15:34:07 -0700

    Test: small updates for facility_2 testing

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -58,6 +58,7 @@ RSpec.describe Facility do
       expect(@facility_1.collected_fees).to eq(125)
       @facility_1.register_vehicle(@bolt)
       expect(@facility_1.collected_fees).to eq(325)
+      expect(@facility_2.collected_fees).to eq(0)
     end
   end
 
