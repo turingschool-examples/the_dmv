1e9d2be7b7c18be616ddee5350f31f815bf35511
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-10 08:24:44 -0700

    Add tests for #registered_vehilces

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,5 +1,8 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services
 
   def initialize(facility_details)
     @name = facility_details[:name]
