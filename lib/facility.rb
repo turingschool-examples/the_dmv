915158ae3d76359fef38d1e26440f22da6cd8ab5
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-09 20:41:19 -0500

    feat: all tests pass for #register_vehicle

--- lib/facility.rb
+++ lib/facility.rb
@@ -20,18 +20,20 @@ class Facility
   end
 
   def register_vehicle(vehicle)
+    if services.include?('Vehicle Registration')
       @registered_vehicles << vehicle
       vehicle.registration_date = Date.today
 
-   if vehicle.year <= 1998
-       @collected_fees += 25
-       vehicle.plate_type = :antique
-    elsif vehicle.engine == :ev
-        @collected_fees += 200
-        vehicle.plate_type = :ev
-    else 
-      @collected_fees += 100
-        vehicle.plate_type = :regular
+      if vehicle.year <= 1998
+          @collected_fees += 25
+          vehicle.plate_type = :antique
+        elsif vehicle.engine == :ev
+            @collected_fees += 200
+            vehicle.plate_type = :ev
+        else 
+          @collected_fees += 100
+            vehicle.plate_type = :regular
+      end
     end
   end
 end
