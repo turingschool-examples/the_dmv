3743aefda54311ae374077132ea4a14c70681152
Grace Joh <graceehjoh@gmail.com>
2023-02-10 00:07:00 -0600

    test: add tests for #register_vehicle and update #initialize

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -11,16 +11,50 @@ RSpec.describe Facility do
       expect(@facility.address).to eq('2242 Santiam Hwy SE Albany OR 97321')
       expect(@facility.phone).to eq('541-967-2014')
       expect(@facility.services).to eq([])
+      expect(@facility.registered_vehicles).to eq([])
+      expect(@facility.collected_fees).to eq(0)
     end
   end
 
-  describe '#add service' do
+  describe '#add_service' do
     it 'can add available services' do
-      expect(@facility.services).to eq([])
       @facility.add_service('New Drivers License')
       @facility.add_service('Renew Drivers License')
       @facility.add_service('Vehicle Registration')
+
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+    end
+  end
+end
+
+RSpec.describe Facility do
+  before(:each) do
+    @facility_1 = Facility.new({ name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+    @facility_2 = Facility.new({ name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+    @cruz = Vehicle.new({ vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice })
+    @bolt = Vehicle.new({ vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev })
+    @camaro = Vehicle.new({ vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice })
+  end
+
+  describe '#register_vehicle' do
+    it 'can register vehicles' do
+      @facility_1.add_service('Vehicle Registration')
+
+      expect(@facility_1.register_vehicle(@cruz)).to eq(@cruz)
+      expect(@cruz.registration_date).to be_a Date
+      expect(@cruz.plate_type).to eq(:regular)
+      expect(@facility_1.registered_vehicles).to eq([@cruz])
+      expect(@facility_1.collected_fees).to eq(100)
+    end
+  end
+
+  describe '#make_plate' do
+    it '' do
+    end
+  end
+
+  describe '#collect_fee' do
+    it '' do
     end
   end
 end
