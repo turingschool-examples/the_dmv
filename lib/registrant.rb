d831d5aad60ccd1ff15d25b580d2cfface543cec
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-10 11:28:38 -0600

    Feat: add #license_data to Registrant class

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,10 +1,17 @@
 class Registrant
-  attr_reader :name, :age
+  attr_reader :name,
+              :age,
+              :license_data
 
   def initialize(name, age, has_permit = false)
     @name = name
     @age = age
     @has_permit = has_permit
+    @license_data = {
+      written: false,
+      license: false,
+      renewed: false
+    }
   end
 
   def permit?
