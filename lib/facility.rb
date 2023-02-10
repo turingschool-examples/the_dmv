b68c267dcdf97aff0870df1ad2031e9dcce6e443
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-09 21:48:39 -0500

    fix: parameter name in Facility initialize method

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,10 +1,10 @@
 class Facility
   attr_reader :name, :address, :phone, :services
 
-  def initialize(details)
-    @name = details[:name]
-    @address = details[:address]
-    @phone = details[:phone]
+  def initialize(facility_details)
+    @name = facility_details[:name]
+    @address = facility_details[:address]
+    @phone = facility_details[:phone]
     @services = []
   end
 
