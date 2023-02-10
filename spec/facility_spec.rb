5c154883a88c0a957df573fd45acbad74aad646d
Sean Cowans <zedesia@yahoo.com>
2023-02-09 17:16:07 -0700

    Add working facility test file

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -23,4 +23,60 @@ RSpec.describe Facility do
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
   end
+
+  describe 'Has registration date, registered vehicles, and collected fees' do
+    it 'Has registration date, registered vehicles, and collected fees' do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+      facility_1.add_service('Vehicle Registration')
+
+      expect(cruz.registration_date).to eq(nil)
+      expect(facility_1.registered_vehicles).to eq([])
+      expect(facility_1.collected_fees).to eq(0)
+    end
+  end
+
+  describe 'Can register vehicles' do
+    it 'Can register vehicles with correct list, date, plate type, and fees' do
+    facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+    facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+    cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+    camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+    facility_1.add_service('Vehicle Registration')
+
+    expect(facility_1.register_vehicle(cruz)).to eq(facility_1.registered_vehicles)
+    expect(facility_1.register_vehicle(camaro)).to eq(facility_1.registered_vehicles)
+    expect(facility_1.register_vehicle(bolt)).to eq(facility_1.registered_vehicles)
+    expect(cruz.registration_date).to be_a(Date)
+    expect(camaro.registration_date).to be_a(Date)
+    expect(bolt.registration_date).to be_a(Date)
+    expect(cruz.plate_type).to eq(:regular)
+    expect(camaro.plate_type).to eq(:antique)
+    expect(bolt.plate_type).to eq(:ev)
+    expect(facility_1.registered_vehicles).to eq([cruz, camaro, bolt])
+    expect(facility_1.collected_fees).to eq(325)
+    end
+  end
+
+  describe 'Cannot double register' do
+    it 'will not let vehicle register at 2 facilities' do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+      facility_1.add_service('Vehicle Registration')
+      facility_1.register_vehicle(cruz)
+      facility_1.register_vehicle(camaro)
+      facility_1.register_vehicle(bolt)
+    
+      expect(facility_2.register_vehicle(bolt)).to eq(nil)
+      expect(facility_2.registered_vehicles).to eq([])
+      expect(facility_2.collected_fees).to eq(0)
+    end
+  end
 end
