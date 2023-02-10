3977b70b5bfe465cc0110c5253eb5f114a3c0810
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-10 13:01:03 -0700

    Add tests to only allow facilities that can do a service to do the service

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -60,5 +60,12 @@ RSpec.describe Facility do
       expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro, @bolt])
       expect(@facility_1.collected_fees).to eq(325)
     end
+    it 'only allows facilities with the service to do complete' do
+      expect(@facility_2.registered_vehicles).to eq([])
+      expect(@facility_2.services).to eq([])
+      expect(@facility_2.register_vehicle(@bolt)).to eq(nil)
+      expect(@facility_2.registered_vehicles).to eq([])
+      expect(@facility_2.collected_fees).to eq(0)
+    end
   end
 end
