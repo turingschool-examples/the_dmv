f075b34cb5b31adfb16e5ebe3b5ce5beaf55d302
Katie Lonsdale <katie952@gmail.com>
2023-02-09 20:16:57 -0700

    test: add more register_vehicle tests

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Facility do
   before(:each) do
     @facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
     @facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
-    cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
-    bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
-    camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
   end
 
   describe '#initialize' do
@@ -33,9 +33,12 @@ RSpec.describe Facility do
 
   describe '#register_vehicle' do
     it 'adds vehicle to registered vehicles' do
-      @facility_1.register_vehicle(cruz)
+      @facility_1.register_vehicle(@cruz)
 
-      expect(@facility_1.registered_vehicles).to eq([cruz])
+      expect(@facility_1.registered_vehicles).to eq([@cruz])
+      expect(@cruz.registration_date).to eq('2/9/2023')
+      expect(@cruz.plate_type).to eq(:regular)
+      expect(@collected_fees).to eq 100
     end
 
   end
