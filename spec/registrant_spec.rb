c1be8e90bc6ba4302d75f6f2e0cc7e53d14e05de
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-09 18:47:54 -0700

    Add #earn_permit

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe Registrant do
       expect(@registrant_1.name).to eq('Bruce')
       expect(@registrant_1.age).to eq(18)
       expect(@registrant_1.permit).to eq(true)
+
       expect(@registrant_2.name).to eq('Penny')
       expect(@registrant_2.age).to eq(15)
       expect(@registrant_2.permit).to eq(false)
@@ -25,6 +26,13 @@ RSpec.describe Registrant do
     it 'can check permit status' do
       expect(@registrant_1.permit?).to eq(true)
       expect(@registrant_2.permit?).to eq(false)
+    end
+  end
+  describe '#earn_permit' do
+    it 'can earn their permit' do
+      registrant_2.earn_permit
+
+      expect(registrant_2.permit?).to eq(true)
     end
   end
 end
