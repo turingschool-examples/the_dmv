ddf9f54e441e67bd731cb2cca6c09260692c50a6
Isaac Thill <ithill22@turing.edu>
2023-02-09 19:39:43 -0700

    Fixed: added method for registration_date

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -14,6 +14,8 @@ class Vehicle
     @make = vehicle_details[:make]
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
+    @registration_date = nil
+
   end
 
   def antique?
