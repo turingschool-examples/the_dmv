d4bae607e508fa5c693e0eeb5486130511e8c6e8
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-10 15:23:13 -0700

    write a test for register vehicle

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe Facility do
     camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
     expect(facility_1.add_service('Vehicle Registration')).to eq(["Vehicle Registration"])
   end
-  
+
   it 'checks registered vehicles and collected fees' do
     facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
     facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
@@ -47,7 +47,6 @@ RSpec.describe Facility do
     expect(cruz.registration_date).to eq(nil)
     expect(facility_1.registered_vehicles).to eq([])
     expect(facility_1.collected_fees).to eq(0)
-    
   end
   
   it 'registers a vehicle' do
@@ -56,11 +55,12 @@ RSpec.describe Facility do
     cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
     camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+    facility_1.add_service('Vehicle Registration')
     require 'pry'; binding.pry
-    expect(facility_1.add_service('Vehicle Registration')).to eq(["Vehicle Registration"])
-    
-
+    expect(facility_1.register_vehicle(cruz)).to eq([cruz]) 
   end
+
+
 
 
 end
