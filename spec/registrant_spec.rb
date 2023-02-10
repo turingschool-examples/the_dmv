dc2929fc79f67316776f88040d0562d499d6c271
Amy Spears <amspears007@gmail.com>
2023-02-09 19:50:09 -0700

    Add more tests to Registrant class

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -18,10 +18,24 @@ RSpec.describe Registrant do
     expect(registrant_1.permit?).to eq(true)
   end
 
-  it 'can create a hash of license data' do
+  it 'can call a hash of license data' do
+    registrant_1 = Registrant.new('Bruce', 18, true )
+    registrant_2 = Registrant.new('Penny', 15 )
+    # require 'pry'; binding.pry
+
+    expected = {:written=>false, :license=>false, :renewed=>false}
+
+    expect(registrant_1.license_data).to eq(expected)
+  end
+
+  it 'registrant_2 has an age, name, and license data' do
     registrant_1 = Registrant.new('Bruce', 18, true )
     registrant_2 = Registrant.new('Penny', 15 )
 
-    
+    expect(registrant_2.name).to eq("Penny")
+    expect(registrant_2.age).to eq(15)
+
+    expected = {:written=>false, :license=>false, :renewed=>false}
+    expect(registrant_2.license_data).to eq(expected)
   end
 end
