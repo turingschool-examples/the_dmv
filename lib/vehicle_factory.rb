62502cf3cd6ffce03f37f077eced5441517447f4
Sean Cowans <zedesia@yahoo.com>
2023-02-09 22:23:44 -0700

    Complete vehcile factory for iteration 2

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,10 +1,21 @@
 class VehicleFactory
-  attr_reader :vehicle_data
+  attr_reader :manufactured_vehicles, :manufactured_vehicle_count, :vehicle_data
   def initialize
     @vehicle_data = []
+    @manufactured_vehicles = []
+    @manufactured_vehicle_count = 0
   end
 
   def manufacture_vehicles(vehicles)
     @vehicle_data = vehicles
+    vehicles.each do |vehicle|
+      @manufactured_vehicle_count += 1
+      number = @manufactured_vehicle_count
+      vehicle_number = Vehicle.new(vehicle)
+      vehicle_number.vin(vehicle.fetch(:vin_1_10))
+      vehicle_number.engine(:ev)
+      vehicle_number.year(vehicle.fetch(:model_year))
+      @manufactured_vehicles << vehicle_number
+    end
   end
 end
