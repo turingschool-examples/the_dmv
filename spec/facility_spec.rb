0a68e9a6e0fccd854aaa1c450c7897132295bbe2
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-10 17:21:04 -0500

    test: add structure for drivers license feature tests

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -87,4 +87,26 @@ RSpec.describe Facility do
       expect(@camaro.plate_type).to eq(:antique)
     end
   end
+
+  describe 'Drivers License' do
+    before(:each) do
+      @registrant_1 = Registrant.new('Bruce', 18, true )
+      @registrant_2 = Registrant.new('Penny', 16 )
+      @registrant_3 = Registrant.new('Tucker', 15 )
+      @facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      @facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+    end
+
+    describe '#administer_written_test' do
+
+    end
+
+    describe '#administer_road_test' do
+
+    end
+
+    describe '#renew_drivers_license' do
+
+    end
+  end
 end
