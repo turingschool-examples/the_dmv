004f90d00e49ab0116af38146903e090f0a231fc
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-10 14:51:11 -0700

    feat: Add method/instance variable for registered vehicles

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,12 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles
 
   def initialize(facility_info)
     @name = facility_info[:name]
     @address = facility_info[:address]
     @phone = facility_info[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
