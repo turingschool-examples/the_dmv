5163aee0b073f9f3f1d156e7f2dd91d92643c189
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-10 15:16:36 -0700

    make registration date an instance variable

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -14,7 +14,7 @@ class Vehicle
     @make = vehicle_details[:make]
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
-    registration_date = nil
+    @registration_date = nil
   end
 
   def antique?
