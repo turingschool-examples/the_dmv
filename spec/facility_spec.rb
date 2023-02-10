0dbc13f156d15470606345f35ee5b9326670a935
Katie Lonsdale <katie952@gmail.com>
2023-02-09 20:01:34 -0700

    test: register_vehicle test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -30,4 +30,13 @@ RSpec.describe Facility do
       expect(@facility_1.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
   end
+
+  describe '#register_vehicle' do
+    it 'adds vehicle to registered vehicles' do
+      @facility_1.register_vehicle(cruz)
+
+      expect(@facility_1.registered_vehicles).to eq([cruz])
+    end
+
+  end
 end
