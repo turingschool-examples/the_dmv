deb731f5dafcfa90edb826f6a16cacde65a346bb
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-09 20:44:51 -0700

    actually refactor tests

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Registrant do
 
     expect(registrant_1.name).to eq('Bruce')
     expect(registrant_1.age).to eq(18)
-    expect(registrant_1.permit).to eq(false)
+    expect(registrant_1.permit).to eq(true)
   end
 
   it 'permits are false if not specified' do
@@ -25,10 +25,9 @@ RSpec.describe Registrant do
   expect(registrant_2.earn_permit.permit?).to eq(true)
   end
 
-  xit 'defaults licencees to false if not specified' do
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
+  xit 'stores license data in a hash' do
+    registrant_2 = Registrant.new('Penny', 15)
     
-    expect(registrant_2.licence_data).to eq(false)
+    expect(registrant_2.licence_data).to eq{:written=>false, :license=>false, :renewed=>false}
   end
 end
