1a8e9079a67498f3fbb5df4f10c96e1953e513aa
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-10 13:45:46 -0700

    Create method for create vehicles.

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,5 +1,18 @@
+require 'spec_helper'
+
 class VehicleFactory
+  attr_reader :created_vehicles, :create_vehicles
   def initialize
-    
+    @created_vehicles = []
   end
+
+  def create_vehicles(registrations)
+    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+    wa_ev_registrations.each do |car|
+      @created_vehicles << (vehicle = Vehicle.new(car))
+    end
+    @created_vehicles
+   
+  end
+
 end
