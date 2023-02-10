271f888ab088d65120cd8522333a4434ff5ee03b
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-10 17:33:11 -0500

    add tests: to test if license_data will return all false and all true after using last three methods

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -159,4 +159,34 @@ RSpec.describe Facility do
       end
     end
   end
+
+  describe 'testing to check the past 3 methods' do
+    it 'license_data returns all false' do
+      facility_1.administer_written_test(registrant_1)
+      facility_1.administer_road_test(registrant_1)
+      facility_1.renew_drivers_license(registrant_1)
+
+      expect(registrant_1.license_data).to eq({
+        written: false, 
+        license: false, 
+        renewed: false 
+      })
+    end
+
+    it 'license_data returns all false' do
+      facility_1.add_service('Written Test')
+      facility_1.add_service('Road Test')
+      facility_1.add_service('Renew License')
+
+      facility_1.administer_written_test(registrant_1)
+      facility_1.administer_road_test(registrant_1)
+      facility_1.renew_drivers_license(registrant_1)
+
+      expect(registrant_1.license_data).to eq({
+        written: true, 
+        license: true, 
+        renewed: true 
+      })
+    end
+  end
 end
