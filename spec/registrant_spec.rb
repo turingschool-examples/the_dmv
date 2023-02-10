586a9f53e5eda834240318d5ed9bd073a9264730
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-09 20:06:09 -0500

    make a hash for license_data

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe do Registrant
     expect(registrant_1).to be_an_instance_of(Registrant)
     expect(registrant_1.age).to eq(18)
     expect(registrant_1.name).to eq('Bruce')
-    expect(registrant_1.permit).to eq(true)
+    expect(registrant_1.permit?).to eq(true)
+    expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
   end
 end
