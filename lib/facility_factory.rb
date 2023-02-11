dc23042f3f35de5204072d60bbc8b0a8bfca9c73
Katie Lonsdale <katie952@gmail.com>
2023-02-11 15:39:11 -0700

    feat: add create_facilities and extract_address methods

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,5 +1,15 @@
 class FacilityFactory
   def initialize
-    
+  end
+
+  def create_facilities(source)
+    source.map do |facility|
+      Facility.new({name: facility[:title], address: extract_address(facility), 
+      phone: facility[:phone_number]})
+    end
+  end
+
+  def extract_address(facility)
+    JSON.parse(facility.dig(:location_1, :human_address)).values.join(' ')
   end
 end
