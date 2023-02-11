de7ca77eaabf7f2b33e4761a88fa4301ab3320ce
Katie Lonsdale <katie952@gmail.com>
2023-02-11 15:49:23 -0700

    test: add create multiple facilities test

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe FacilityFactory do
   before(:each) do
     @factory = FacilityFactory.new
     @or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
+    @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+    @missouri_facilities = DmvDataService.new.mo_dmv_office_locations
   end
 
   describe '#initialize' do
@@ -17,6 +19,13 @@ RSpec.describe FacilityFactory do
       expect(@factory.create_facilities(@or_dmv_office_locations)).to be_an_instance_of(Array)
       expect(@factory.create_facilities(@or_dmv_office_locations)[0]).to be_an_instance_of(Facility)
       # running function twice...take out?
+    end
+    it 'can create multiple facilities' do
+      expect(@factory.create_facilities(@new_york_facilities)).to be_an_instance_of(Array)
+      expect(@factory.create_facilities(@new_york_facilities)[0]).to be_an_instance_of(Facility)
+
+      # expect(@factory.create_facilities(@missouri_facilities)).to be_an_instance_of(Array)
+      # expect(@factory.create_facilities(@missouri_facilities)[0]).to be_an_instance_of(Facility)
     end
   end
 
