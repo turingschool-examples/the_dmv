3de0c67dc59018790546e5325cc7edfe3d7a7aea
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-11 02:45:32 -0600

    Refactor: change constant names

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 RSpec.describe Facility do
-  LOCATION_1 = {
+  FACILITY_1 = {
     name: 'Albany DMV Office',
     address: '2242 Santiam Hwy SE Albany OR 97321',
     phone: '541-967-2014'
   }.freeze
-  LOCATION_2 = {
+  FACILITY_2 = {
     name: 'Ashland DMV Office',
     address: '600 Tolman Creek Rd Ashland OR 97520',
     phone: '541-776-6092'
