eb3f25bd122c8048bfa5e176577ce56179c44a66
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-10 17:42:42 -0700

    Add permit? method

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,11 +1,17 @@
 class Registrant
-  attr_reader :name
+  attr_reader :name, :age, :permit
   def initialize(name, age, permit = false)
     @name = name 
     @age = age
     @permit = permit
     # @license_data = license_data
   end
+
+  def permit?
+    @permit
+  end
+
+  
 
 
 
