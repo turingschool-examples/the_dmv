3bdc6cb2ef39d87bd046224379f3faf5b07eb5ab
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-11 00:02:59 -0600

    Refactor: before(:each) block and #initialize tests for Facility class

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -2,19 +2,52 @@ require 'spec_helper'
 
 RSpec.describe Facility do
   before(:each) do
-    @facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014'})
-    @facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092'})
+    @location_1 = {
+      name: 'Albany DMV Office',
+      address: '2242 Santiam Hwy SE Albany OR 97321',
+      phone: '541-967-2014'
+    }
+    @location_2 = {
+      name: 'Ashland DMV Office',
+      address: '600 Tolman Creek Rd Ashland OR 97520',
+      phone: '541-776-6092'
+    }
+    @car_1 = {
+      vin: '123456789abcdefgh',
+      year: 2012,
+      make: 'Chevrolet',
+      model: 'Cruz',
+      engine: :ice
+    }
+    @car_2 = {
+      vin: '987654321abcdefgh',
+      year: 2019,
+      make: 'Chevrolet',
+      model: 'Bolt',
+      engine: :ev
+    }
+    @car_3 = {
+      vin: '1a2b3c4d5e6f',
+      year: 1969,
+      make: 'Chevrolet',
+      model: 'Camaro',
+      engine: :ice
+    }
 
-    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
-    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
-    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+    @facility_1 = Facility.new(@location_1)
+    @facility_2 = Facility.new(@location_2)
+
+    @cruz = Vehicle.new(@car_1)
+    @bolt = Vehicle.new(@car_2)
+    @camaro = Vehicle.new(@car_3)
   end
+
   describe '#initialize' do
     it 'can initialize' do
       expect(@facility_1).to be_an_instance_of(Facility)
-      expect(@facility_1.name).to eq('Albany DMV Office')
-      expect(@facility_1.address).to eq('2242 Santiam Hwy SE Albany OR 97321')
-      expect(@facility_1.phone).to eq('541-967-2014')
+      expect(@facility_1.name).to eq(@location_1[:name])
+      expect(@facility_1.address).to eq(@location_1[:address])
+      expect(@facility_1.phone).to eq(@location_1[:phone])
       expect(@facility_1.services).to eq([])
       expect(@facility_1.registered_vehicles).to eq([])
       expect(@facility_1.collected_fees).to eq(0)
@@ -22,9 +55,9 @@ RSpec.describe Facility do
 
     it 'can initialize again' do
       expect(@facility_2).to be_an_instance_of(Facility)
-      expect(@facility_2.name).to eq('Ashland DMV Office')
-      expect(@facility_2.address).to eq('600 Tolman Creek Rd Ashland OR 97520')
-      expect(@facility_2.phone).to eq('541-776-6092')
+      expect(@facility_2.name).to eq(@location_2[:name])
+      expect(@facility_2.address).to eq(@location_2[:address])
+      expect(@facility_2.phone).to eq(@location_2[:phone])
       expect(@facility_2.services).to eq([])
       expect(@facility_2.registered_vehicles).to eq([])
       expect(@facility_2.collected_fees).to eq(0)
