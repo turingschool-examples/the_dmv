d33d97c75eea7a5bdc9adf578eeb91923774518c
margdaly <margaret.daly@gmail.com>
2023-02-11 10:10:38 -0700

    Add expect to must pass written test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -133,9 +133,6 @@ RSpec.describe Facility do
       facility_1.add_service('Written Test')
 
       expect(facility_1.services).to eq(['Written Test'])
-      expect(registrant_1.age).to eq(18)
-      expect(registrant_2.age).to eq(16)
-      expect(registrant_3.age).to eq(15)
       expect(registrant_1.permit?).to eq(true)
       expect(registrant_2.permit?).to eq(false)
       expect(registrant_3.permit?).to eq(false)
@@ -155,12 +152,23 @@ RSpec.describe Facility do
   end
 
   describe '#administer road test' do
-    xit 'can only be at a facility with that service' do
-      facility_1.add_service('Road Test')
-    end
-
     xit 'only administered if passed written test' do
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 16 )
+      registrant_3 = Registrant.new('Tucker', 15 )
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      
+      facility_1.add_service('Written Test')
+      facility_1.add_service('Road Test')
+      registrant_2.earn_permit
+      registrant_3.earn_permit
+      facility_1.administer_written_test(registrant_1)
+      facility_1.administer_written_test(registrant_2)
+      facility_1.administer_written_test(registrant_3)
 
+      expect(facility_1.administer_road_test(registrant_1)).to eq(true)
+      expect(facility_1.administer_road_test(registrant_2)).to eq(true)
+      expect
     end
 
     xit 'registrant earns a license after road test' do
