1038afdd8a2e24b70095bee02caef792578fb9fd
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-10 19:12:26 -0700

    Add tests and working code for license renewal

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -143,7 +143,14 @@ RSpec.describe Facility do
       @facility_1.administer_written_test(@registrant_1)
       @facility_1.administer_road_test(@registrant_1)
       expect(@facility_1.renew_drivers_license(@registrant_1)).to eq(true)
-      expect(@ registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
+      expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
+      expect(@facility_1.renew_drivers_license(@registrant_3)).to eq(false)
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      @registrant_2.earn_permit
+      @facility_1.administer_written_test(@registrant_2)
+      @facility_1.administer_road_test(@registrant_2)
+      expect(@facility_1.renew_drivers_license(@registrant_2)).to eq(true)
+      expect(@registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
     end
   end
 end
