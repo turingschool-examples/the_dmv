f4c49f5299f610105701d988fab1dc9301d64577
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-11 14:19:36 -0600

    Fix: remove references to default object in tests for the Registrant class

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -78,8 +78,12 @@ RSpec.describe Registrant do
     end
 
     it 'sets its default values correctly' do
-      expect(@registrant_1.license_data).to eq(Registrant::DEFAULTS[:license_data])
-      expect(@registrant_2.license_data).to eq(Registrant::DEFAULTS[:license_data])
+      expect(@registrant_1.license_data[:written]).to be(false)
+      expect(@registrant_1.license_data[:license]).to be(false)
+      expect(@registrant_1.license_data[:renewed]).to be(false)
+      expect(@registrant_2.license_data[:written]).to be(false)
+      expect(@registrant_2.license_data[:license]).to be(false)
+      expect(@registrant_2.license_data[:renewed]).to be(false)
     end
 
     describe '#earn_permit' do
