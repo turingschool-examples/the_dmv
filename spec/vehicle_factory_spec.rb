9852e7600a749cdd4dafc891aa9d37ed60477423
Isaac Thill <ithill22@turing.edu>
2023-02-10 22:10:40 -0700

    test: add create vehicle test

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -10,6 +10,18 @@ RSpec.describe VehicleFactory do
       @factory = VehicleFactory.new
 
       expect(@factory).to be_an_instance_of(VehicleFactory)
+      expect(@vehicles).to eq([])
+    end
+  end
+
+  describe '#create_vehicles' do
+    it 'can create vehicles' do
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+
+      @factory.create_vehicles(wa_ev_registrations)
+
+      expect(factory.vehicles).to be_a(Array)
+      expect(factory.vehicles[0]).to be_an_instance_of(Vehicle)
     end
   end
 
