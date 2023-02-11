5eca52c200592a63c4fef5f83d234ef8961343be
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-02-10 17:03:35 -0700

    add vehicle_factory class with create_vehicles method to iterate through data and extract information from hashes

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,5 +1,14 @@
 class VehicleFactory
+  attr_reader :state_vehicles
 
-  
+
+  def create_vehicles(state_data)
+    state_vehicles = []
+    for i in 0..state_data.count - 1
+      current_hash = state_data[i]
+      vehicle_hash = {vin: current_hash[:vin_1_10]}
+      @state_vehicles << Vehicle.new(vehicle_hash)
+    end
+  end
 
 end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -3,11 +3,10 @@ require './lib/vehicle_factory'
 require './lib/dmv_data_service'
 
 RSpec.describe VehicleFactory do
-
   before(:each) do
     @factory = VehicleFactory.new
     @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
-    # factory.create_vehicles(wa_ev_registrations)
+    @factory.create_vehicles(@wa_ev_registrations)
   end
   
   it "is an array" do
@@ -20,9 +19,16 @@ RSpec.describe VehicleFactory do
     end
   end
 
- 
 
 end
+ 
+  # it "all hashes have the same keys" do  ***ALL HASHES DO NOT HAVE IDENTICAL KEYS ****
+  #   current_hash = @wa_ev_registrations[0]
+  #   for i in 0..@wa_ev_registrations.count - 1
+  #     expect(current_hash.keys).to eq(@wa_ev_registrations[i+1].keys)
+  #     current_hash = @wa_ev_registrations[i+1]
+  #   end
+  # end
 
   # it "parses data into an array of vehicles" do
 
