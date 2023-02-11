11ff4a29b5696acc562850a18ee827b9278515e7
Angel Byun <abyun22@turing.edu>
2023-02-11 11:03:37 -0700

    Modify attr_accessor

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,12 +1,14 @@
 require 'date'
 
 class Vehicle
-  attr_reader :vin,
-              :year,
-              :make,
-              :model,
-              :engine,
-              :registration_date
+  attr_reader   :vin,
+                :year,
+                :make,
+                :model,
+                :engine
+
+  attr_accessor :registration_date,
+                :plate_type
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -15,6 +17,7 @@ class Vehicle
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
     @registration_date = nil
+    @plate_type = vehicle_details[:plate_type]
   end
 
   def antique?
