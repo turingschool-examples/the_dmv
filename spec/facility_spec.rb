8b8d9e6e9a897fcb41c3398619f12faf375d20b8
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-10 23:24:58 -0700

    refactor: fix syntax

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -62,9 +62,7 @@ RSpec.describe Facility do
         expect(facility_2.services).to eq([])
         expect(facility_2.register_vehicle(bolt)).to eq(nil)
         expect(facility_2.registered_vehicles).to eq([])
-        expect(facility_2).collected_fees.to eq(0)
-
-        
+        expect(facility_2.collected_fees).to eq(0)
       end 
     end
 
