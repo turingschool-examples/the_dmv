c4c6ce79c54c0d50f407fcde52f9fde1d099b4c3
Reid Miller <reid.s.miller@gmail.com>
2023-02-10 23:19:31 -0700

    feat: Add ny_create_facilities method to FacilityFactory class

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -4,7 +4,7 @@ class FacilityFactory
     @facilities = []
   end
 
-  def or_create_facilites(data)
+  def or_create_facilities(data)
     data.each do |facility|
       facility_details = {
         name: facility[:title],
@@ -20,5 +20,19 @@ class FacilityFactory
   def or_find_address(data)
     address_h = eval(data[:location_1][:human_address])
     address_s = address_h.values.join(' ')
+  end
+
+  def ny_create_facilities(data)
+    require 'pry'; binding.pry
+    data.each do |facility|
+      facility_details = {
+        name: facility[:office_namee],
+        address: ny_find_address(facility),
+        phone: facility[:public_phone_number]
+      }
+      facility_sorted = Facility.new(facility_details)
+      self.facilities << facility_sorted
+    end
+    @facilities
   end
 end
