7f1e645782b47a008569a662f53303f33bf5cbdf
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-11 01:00:21 -0600

    Refactor: before(:each) block and #initialize tests for Vehicle class

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -2,19 +2,62 @@ require 'spec_helper'
 
 RSpec.describe Vehicle do
   before(:each) do
-    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
-    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
-    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+    @car_1 = {
+      vin: '123456789abcdefgh',
+      year: 2012,
+      make: 'Chevrolet',
+      model: 'Cruz',
+      engine: :ice
+    }
+    @car_2 = {
+      vin: '987654321abcdefgh',
+      year: 2019,
+      make: 'Chevrolet',
+      model: 'Bolt',
+      engine: :ev
+    }
+    @car_3 = {
+      vin: '1a2b3c4d5e6f',
+      year: 1969,
+      make: 'Chevrolet',
+      model: 'Camaro',
+      engine: :ice
+    }
+
+    @cruz = Vehicle.new(@car_1)
+    @bolt = Vehicle.new(@car_2)
+    @camaro = Vehicle.new(@car_3)
   end
+
   describe '#initialize' do
     it 'can initialize' do
       expect(@cruz).to be_an_instance_of(Vehicle)
-      expect(@cruz.vin).to eq('123456789abcdefgh')
-      expect(@cruz.year).to eq(2012)
-      expect(@cruz.make).to eq('Chevrolet')
-      expect(@cruz.model).to eq('Cruz')
-      expect(@cruz.engine).to eq(:ice)
+      expect(@cruz.vin).to eq(@car_1[:vin])
+      expect(@cruz.year).to eq(@car_1[:year])
+      expect(@cruz.make).to eq(@car_1[:make])
+      expect(@cruz.model).to eq(@car_1[:model])
+      expect(@cruz.engine).to eq(@car_1[:engine])
       expect(@cruz.registration_date).to eq(nil)
+    end
+
+    it 'can initialize a different car' do
+      expect(@bolt).to be_an_instance_of(Vehicle)
+      expect(@bolt.vin).to eq(@car_2[:vin])
+      expect(@bolt.year).to eq(@car_2[:year])
+      expect(@bolt.make).to eq(@car_2[:make])
+      expect(@bolt.model).to eq(@car_2[:model])
+      expect(@bolt.engine).to eq(@car_2[:engine])
+      expect(@bolt.registration_date).to eq(nil)
+    end
+
+    it 'can initialize a different car again' do
+      expect(@camaro).to be_an_instance_of(Vehicle)
+      expect(@camaro.vin).to eq(@car_3[:vin])
+      expect(@camaro.year).to eq(@car_3[:year])
+      expect(@camaro.make).to eq(@car_3[:make])
+      expect(@camaro.model).to eq(@car_3[:model])
+      expect(@camaro.engine).to eq(@car_3[:engine])
+      expect(@camaro.registration_date).to eq(nil)
     end
   end
 
