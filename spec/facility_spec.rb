d3d1e28728f87d80ada04b27d41b4d87c7462f2e
margdaly <margaret.daly@gmail.com>
2023-02-10 19:06:15 -0700

    Add more expects to register a vehicle if it has that service test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -29,6 +29,8 @@ RSpec.describe Facility do
       facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
       facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
       cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
 
       facility_1.add_service('Vehicle Registration')
 
@@ -38,11 +40,16 @@ RSpec.describe Facility do
       expect(facility_2.registered_vehicles).to eq([])
       expect(facility_1.register_vehicle(cruz)).to eq([cruz])
       expect(facility_2.register_vehicle(cruz)).to eq([])
+      expect(facility_1.register_vehicle(camaro)).to eq([cruz, camaro])
+      expect(facility_2.register_vehicle(camaro)).to eq([])
+      expect(facility_1.register_vehicle(bolt)).to eq([cruz, camaro, bolt])
+      expect(facility_2.register_vehicle(bolt)).to eq([])
+      expect(facility_1.registered_vehicles).to eq([cruz, camaro, bolt])
+      expect(facility_2.reg).to eq([])
     end
 
     it 'assigns plate_type to vehicle' do
       facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
-      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
       cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
       bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
       camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
@@ -52,7 +59,6 @@ RSpec.describe Facility do
       facility_1.register_vehicle(camaro)
       facility_1.register_vehicle(bolt)
 
-      expect(facility_1.registered_vehicles).to eq([cruz, camaro, bolt])
       expect(cruz.plate_type).to eq(:regular)
       expect(camaro.plate_type).to eq(:antique)
       expect(bolt.plate_type).to eq(:ev)
