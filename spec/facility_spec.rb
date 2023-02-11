46fe4fa726f82cd7fbae3f421bbe8555203eb68a
margdaly <margaret.daly@gmail.com>
2023-02-10 18:53:39 -0700

    test: Add collect registration fees test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -75,14 +75,24 @@ RSpec.describe Facility do
       expect(bolt.registration_date).to eq(Date.today.year)
     end
 
-    xit 'can collect registration fees' do
+    it 'can collect registration fees' do
       facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
       cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
       bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
       camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
 
       facility_1.add_service('Vehicle Registration')
       facility_1.register_vehicle(cruz)
+
+      expect(facility_1.collected_fees).to eq(100)
+      expect(facility_2.collected_fees).to eq(0)
+
+      facility_1.register_vehicle(camaro)
+      facility_1.register_vehicle(bolt)
+
+      expect(facility_1.collected_fees).to eq(325)
+      expect(facility_2.collected_fees).to eq(0)
     end
   end
 end
