1a308edee5b6320b201466cfd4b6ad320b51aacb
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-11 02:57:03 -0600

    Refactor: use constants for test setup data for Dmv class

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -1,11 +1,27 @@
 require 'spec_helper'
 
 RSpec.describe Dmv do
+  FACILITY_1 = {
+    name: 'Albany DMV Office',
+    address: '2242 Santiam Hwy SE Albany OR 97321',
+    phone: '541-967-2014'
+  }.freeze
+  FACILITY_2 = {
+    name: 'Ashland DMV Office',
+    address: '600 Tolman Creek Rd Ashland OR 97520',
+    phone: '541-776-6092'
+  }.freeze
+  FACILITY_3 = {
+    name: 'Bend DMV Office',
+    address: '63030 O B Riley Rd Bend OR 97701',
+    phone: '541-388-6322'
+  }.freeze
+
   before(:each) do
     @dmv = Dmv.new
-    @facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
-    @facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
-    @facility_3 = Facility.new({name: 'Bend DMV Office', address: '63030 O B Riley Rd Bend OR 97701', phone: '541-388-6322' })
+    @facility_1 = Facility.new(FACILITY_1)
+    @facility_2 = Facility.new(FACILITY_2)
+    @facility_3 = Facility.new(FACILITY_3)
   end
 
   describe '#initialize' do
