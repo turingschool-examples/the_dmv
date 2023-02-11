bc4174edf7be9aef7d5427342c9e0161f6ec783a
Katie Lonsdale <katie952@gmail.com>
2023-02-11 15:38:52 -0700

    test: add create_facilities test

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -5,9 +5,18 @@ RSpec.describe FacilityFactory do
     @factory = FacilityFactory.new
     @or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
   end
+
   describe '#initialize' do
     it 'exists' do
       expect(@factory).to be_an_instance_of(FacilityFactory)
+    end
+  end
+
+  describe '#create_facilities' do
+    it 'creates facilities from given source' do
+      expect(@factory.create_facilities(@or_dmv_office_locations)).to be_an_instance_of(Array)
+      expect(@factory.create_facilities(@or_dmv_office_locations)[0]).to be_an_instance_of(Facility)
+      # running function twice...take out?
     end
   end
 
