db0fab77a278b7956488713de231d84b65d8cc4d
Isaac Thill <ithill22@turing.edu>
2023-02-11 13:03:43 -0700

    test: add test for add mo faciliites

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -36,4 +36,15 @@ RSpec.describe FacilityFactory do
     end
   end
 
+  describe '#create_mo_facilities' do
+    it 'can create and store facilities' do
+      mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
+
+      @facility_factory.create_mo_facilities(mo_dmv_office_locations)
+
+      expect(@facility_factory.facilities).to be_a(Array)
+      expect(@facility_factory.facilities[0]).to be_an_instance_of(Facility)
+    end
+  end
+
 end
