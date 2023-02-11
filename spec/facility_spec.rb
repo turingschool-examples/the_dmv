146a75521fdbdfa2f10a57e9d10ea6532fad0d06
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-10 17:53:18 -0700

    Add tests for road test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -110,8 +110,12 @@ RSpec.describe Facility do
       expect(@facility_1.services).to eq(['Written Test'])
       expect(@facility_1.administer_road_test(@registrant_3)).to eq(false)
       @registrant_3.earn_permit
-      expect(@facility_1.administer_road_test(registrant_3)).to eq(false)
-
+      expect(@facility_1.administer_road_test(@registrant_3)).to eq(false)
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(administer_road_test(@registrant_1)).to eq(false)
+      @facility_1.add_service('Road Test')
+      expect(@facility_1.services).to eq(['Written Test', 'Road Test'])
+      
     end
   end
 end
