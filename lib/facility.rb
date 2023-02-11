5d3ea1f51f98723facd65635b8b099e27eddcfaf
Katie Lonsdale <katie952@gmail.com>
2023-02-10 19:10:37 -0700

    feat: make sure registrant has license

--- lib/facility.rb
+++ lib/facility.rb
@@ -76,10 +76,11 @@ class Facility
   end
 
   def renew_drivers_license(registrant)
-    return 'Service not available here.' unless @services.
+    return 'Service not available at this location.' unless @services.
     include?('Renew License')
-    registrant.license_data[:renewed] = true if registrant.license_data
-    [:written] && registrant.license_data[:license]
+
+    (registrant.license_data[:written] && registrant.license_data[:license]) ? registrant.
+    license_data[:renewed] = true : 'Must have license.'
   end
 
 end
