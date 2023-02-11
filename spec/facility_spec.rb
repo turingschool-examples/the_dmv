789268f36ddb87f20aca6fffab35716071077674
Julian Beldotti <julian.beldotti@gmail.com>
2023-02-11 00:35:27 -0500

    Add more facility test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -25,4 +25,46 @@ RSpec.describe Facility do
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
   end
+  
+  describe '#offer services' do
+    it 'exists with more services' do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+    
+      expect(facility_1).to be_a(Facility)
+      expect(facility_2).to be_a(Facility)
+    end
+
+    it 'can register a vehicle' do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+
+      expect(cruz).to be_a(Vehicle)
+      expect(bolt).to be_a(Vehicle)
+      expect(camaro).to be_a(Vehicle)
+    end
+
+    it 'adds a new service "vehicle registration"' do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+
+      expect(facility_1.add_service('Vehicle Registration')).to eq(["Vehicle Registration"])
+    end
+
+    it 'can collect fees' do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      
+      expect(facility_1.collected_fees).to eq(0)
+      expect(facility_2.collected_fees).to eq(0)
+    end
+  end
+
+  describe "#register a vehicle" do
+    it "shows unregistered vehicle details" do
+    
+    end
+  end
 end
