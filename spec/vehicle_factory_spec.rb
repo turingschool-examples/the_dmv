ca45ccf86a9ec5c20f345cba8b62452df1e3ba38
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-11 15:47:26 -0700

    test: create first test for vehicle facotry spec

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,1 +1,13 @@
 require 'spec_helper'
+
+
+RSpec.describe VehicleFactory do
+  describe '#initialize' do
+    it 'can initialize' do
+      factory = VehicleFactory.new
+
+      expect(factory).to be_an_instance_of(VehicleFactory)
+    end
+  end
+
+end
