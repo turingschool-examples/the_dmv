d94cd06d639803a4cb7eff7cafb0482735fd512e
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-10 22:54:16 -0500

    refactor this branch

--- lib/facility.rb
+++ lib/facility.rb
@@ -53,12 +53,4 @@ class Facility
       false
     end
   end
-
-  def renew_drivers_license(registrant)
-    if services.include?('Renew License') && registrant.permit == true && registrant.age >= 16
-      registrant.license_data[:renewed] = true
-    else
-      false
-    end
-  end
 end
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -145,48 +145,4 @@ RSpec.describe Facility do
 
       expect(registrant_1.license_data[:license]).to be(true)
     end
-
-    describe '#renew_drivers_license' do
-      it 'cannot renew drivers license if the facility does not offer it' do
-        expect(facility_1.renew_drivers_license(registrant_1)).to be(false)
-      end
-
-      it 'can renew driver license if the facility offers it' do
-        facility_1.add_service('Renew License')
-        facility_1.renew_drivers_license(registrant_1)
-
-        expect(registrant_1.license_data[:renewed]).to be(true)
-      end
-    end
-  end
-
-  describe 'testing to check the past 3 methods' do
-    it 'license_data returns all false' do
-      facility_1.administer_written_test(registrant_1)
-      facility_1.administer_road_test(registrant_1)
-      facility_1.renew_drivers_license(registrant_1)
-
-      expect(registrant_1.license_data).to eq({
-        written: false, 
-        license: false, 
-        renewed: false 
-      })
-    end
-
-    it 'license_data returns all false' do
-      facility_1.add_service('Written Test')
-      facility_1.add_service('Road Test')
-      facility_1.add_service('Renew License')
-
-      facility_1.administer_written_test(registrant_1)
-      facility_1.administer_road_test(registrant_1)
-      facility_1.renew_drivers_license(registrant_1)
-
-      expect(registrant_1.license_data).to eq({
-        written: true, 
-        license: true, 
-        renewed: true 
-      })
-    end
-  end
 end
