58f8f0657e1f034a799b63eabb4af03c158da58f
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-11 11:39:56 -0700

    Initial commit

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,15 +1,26 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services, 
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(facility_hash)
     @name = facility_hash[:name]
     @address = facility_hash[:address]
     @phone = facility_hash[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
   
 
   def add_service(service)
     @services << service
   end
+
+  # def registration_date
+  #   @registration_date
+  # end
 end
