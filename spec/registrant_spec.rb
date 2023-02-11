b07d39b3b65ca15c5399b0f880d3bbc075caa929
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-10 18:00:39 -0700

    Add license_data test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -36,7 +36,15 @@ RSpec.describe Registrant do
     expect(registrant_2.permit?).to be false
   end
 
-  
+  it 'registrants have liscene data' do
+    registrant_1 = Registrant.new('Bruce', 18, true )
+    registrant_2 = Registrant.new('Penny', 15 )
+    # require 'pry'; binding.pry
+    expect(registrant_1.license_data).to eq({:license=>false, :renewed=>false, :written=>false})
+    expect(registrant_2.license_data).to eq({:license=>false, :renewed=>false, :written=>false})
+  end
+
+
 
 
 
