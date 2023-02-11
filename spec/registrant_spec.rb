d3d5c3221af39e8993ed9bb018735323abbf5e9b
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-10 17:35:59 -0700

    Add name test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -5,8 +5,19 @@ RSpec.describe Registrant do
    
   it 'exist' do
     registrant_1 = Registrant.new('Bruce', 18, true )
+    registrant_2 = Registrant.new('Penny', 15 )
 
+    require 'pry'; binding.pry
     expect(registrant_1).to be_a Registrant
+    expect(registrant_2).to be_a Registrant
+  end
+
+  it 'registrant has a name' do 
+    registrant_1 = Registrant.new('Bruce', 18, true )
+    registrant_2 = Registrant.new('Penny', 15 )
+    
+    expect(registrant_1.name).to eq("Bruce")
+    expect(registrant_2.name).to eq("Penny")
   end
 
 
