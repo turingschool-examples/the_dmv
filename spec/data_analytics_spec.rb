b6e5857d29045012898a2e30cbe023fcc36470d3
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-11 15:26:57 -0700

    Create tests for EV registration specifications

---
+++ spec/data_analytics_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.describe 'Analyize Data' do
+  describe 'EV Registration' do
+    it 'finds the most popular make and model' do
+      # CODE HERE
+    end
+
+    it 'counts number of registered vehicles by model year' do
+      # CODE HERE
+    end
+
+    it 'finds county that has most registered vehicles' do
+      # CODE HERE
+    end
+  end
+end
