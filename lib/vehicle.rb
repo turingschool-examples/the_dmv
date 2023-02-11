106828adeb5cb14edcc9bb43dd72bf2d5cae4f3f
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-10 19:14:04 -0700

    Will fix bugged tests

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,14 +5,15 @@ class Vehicle
               :year,
               :make,
               :model,
-              :engine
-
+              :engine,
+              :registration_date
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
     @year = vehicle_details[:year]
     @make = vehicle_details[:make]
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
+    @registration_date = nil
   end
 
   def antique?
