aec042914c141fa39b86c53bdc5a9fb6e248a98a
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-11 15:56:27 -0700

    Add final changes to vehicle_factory

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -6,5 +6,18 @@ class VehicleFactory
   end
   
   def create_vehicles(wa_ev_registrations)
-   
+    wa_ev_registrations.each do |vehicle|
+      vehicle_info = {
+        vin: vehicle[:vin_1_10],
+        year: vehicle[:model_year],
+        make: vehicle[:make],
+        model: vehicle[:model],
+        engine: :ev,
+        registration_date: nil,
+        plate_type: nil
+      }
+      @vehicles << vehicle = Vehicle.new(vehicle_info)
+      end
+      @vehicles
+    end
 end
