08f759f8d0852db187db12b5b4880fde50092b4e
margdaly <margaret.daly@gmail.com>
2023-02-10 18:46:12 -0700

    test: Add assigns plate_type test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -25,16 +25,51 @@ RSpec.describe Facility do
   end
 
   describe '#vehicle registration' do
-    it 'has vehicle registration service' do
+    it 'can register a vehicle if it has that service' do
       facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
       facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
 
       facility_1.add_service('Vehicle Registration')
 
-      expect(facility_1).to be_an_instance_of(Facility)
-      expect(facility_2).to be_an_instance_of(Facility)
       expect(facility_1.services).to eq(['Vehicle Registration'])
       expect(facility_2.services).to eq([])
+      expect(facility_1.registered_vehicles).to eq([])
+      expect(facility_2.registered_vehicles).to eq([])
+      expect(facility_1.register_vehicle(cruz)).to eq([cruz])
+      expect(facility_2.register_vehicle(cruz)).to eq([])
+    end
+
+    it 'assigns plate_type to vehicle' do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+
+      facility_1.add_service('Vehicle Registration')
+      facility_1.register_vehicle(cruz)
+      facility_1.register_vehicle(camaro)
+      facility_1.register_vehicle(bolt)
+
+      expect(facility_1.registered_vehicles).to eq([cruz, camaro, bolt])
+      expect(cruz.plate_type).to eq(:regular)
+      expect(camaro.plate_type).to eq(:antique)
+      expect(bolt.plate_type).to eq(:ev)
+    end
+
+    xit 'records when the vehicle was registered' do
+
+    end
+
+    xit 'can collect registration fees' do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+
+      facility_1.add_service('Vehicle Registration')
+      facility_1.register_vehicle(cruz)
     end
   end
 end
