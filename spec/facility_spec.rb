2880f4fd8ba7e3293cbaf7621b101b5e374e74f8
margdaly <margaret.daly@gmail.com>
2023-02-10 18:05:37 -0700

    test: Add registration service test to Facility

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -23,4 +23,18 @@ RSpec.describe Facility do
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
   end
+
+  describe '#vehicle registration' do
+    it 'has vehicle registration service' do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+
+      facility_1.add_service('Vehicle Registration')
+
+      expect(facility_1).to be_an_instance_of(Facility)
+      expect(facility_2).to be_an_instance_of(Facility)
+      expect(facility_1.services).to eq(['Vehicle Registration'])
+      expect(facility_2.services).to eq([])
+    end
+  end
 end
