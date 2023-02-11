de8f56e3429954b63bda7a8cd84a9b517ee02536
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-11 11:39:38 -0700

    initial commit

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -1,3 +1,4 @@
+require 'rspec'
 require 'spec_helper'
 
 RSpec.describe Facility do
@@ -13,6 +14,7 @@ RSpec.describe Facility do
       expect(@facility.services).to eq([])
     end
   end
+  # require 'pry'; binding.pry
 
   describe '#add service' do
     it 'can add available services' do
@@ -22,5 +24,18 @@ RSpec.describe Facility do
       @facility.add_service('Vehicle Registration')
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
+  end
+
+  describe '#register a vehicle' do
+    it 'vehicles start with a nil regisrtation date' do
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+
+      expect(cruz.registration_date).to be nil 
+    end
+    
+    it 'DMVs start with an empty array of registered vehicles' do
+      expect(@facility.registered_vehicles).to eq([])
+    end
+      
   end
 end
