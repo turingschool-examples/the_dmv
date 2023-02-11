4f5f429ac73bf642ef1193ba83ca9e904f8a3c1f
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-10 17:42:44 -0700

    Add and modify methods.

--- lib/facility.rb
+++ lib/facility.rb
@@ -16,29 +16,17 @@ class Facility
   end
 
   def register_vehicle(vehicle)
-    service_check = services.any? {|service| service == 'Vehicle Registration'}
-    if service_check == true
+    if @services.include?("Vehicle Registration")
       @registered_vehicles << vehicle
       vehicle.registration_date = Date.today
-      if (2023 - vehicle.year) >= 25
-        vehicle.plate_type = :antique
-        @collected_fees += 25
-      elsif vehicle.engine == :ev
-        vehicle.plate_type = :ev
-        @collected_fees += 200
-      else
-        vehicle.plate_type = :regular
-        @collected_fees += 100
-      end
+      determine_plates_and_fees(vehicle)
       @registered_vehicles
     else 
-      return nil
     end
   end
 
   def administer_written_test(registrant)
-    service_check = services.any? {|service| service == 'Written Test'}
-    if service_check == true
+    if @services.include?("Written Test")
       if (registrant.permit == true) && (registrant.age >= 16)
         registrant.license_data[:written] = true
         return true
@@ -50,8 +38,7 @@ class Facility
   end
 
   def administer_road_test(registrant)
-    service_check = services.any? {|service| service == 'Road Test'}
-    if service_check == true
+    if @services.include?("Road Test")
       if registrant.license_data[:written] == true
         registrant.license_data[:license] = true
         return true
@@ -63,8 +50,7 @@ class Facility
   end
 
   def renew_drivers_license(registrant)
-    service_check = services.any? {|service| service == 'Renew License'}
-    if service_check == true
+    if @services.include?("Renew License")
       if registrant.license_data[:license] == true 
         registrant.license_data[:renewed] = true
         return true
@@ -73,5 +59,19 @@ class Facility
       end
     end
     false
+  end
+
+  ## Helper Method
+  def determine_plates_and_fees(vehicle)
+    if vehicle.antique?
+      vehicle.plate_type = :antique
+      @collected_fees += 25
+    elsif vehicle.electric_vehicle?
+      vehicle.plate_type = :ev
+      @collected_fees += 200
+    else
+      vehicle.plate_type = :regular
+      @collected_fees += 100
+    end
   end
 end
