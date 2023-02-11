fe606978a1d7f35c172fe711e44cc53741881479
margdaly <margaret.daly@gmail.com>
2023-02-11 10:56:33 -0700

    Add expect to renew license test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -194,9 +194,31 @@ RSpec.describe Facility do
     end
   end
 
-  describe '#renew drivers license' do
+  describe '#renew license' do
     xit 'can be renewed if you have a license' do
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 16 )
+      registrant_3 = Registrant.new('Tucker', 15 )
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
 
+      facility_1.add_service('Written Test')
+      facility_1.add_service('Road Test')
+      facility_1.add_service('Renew License')
+      registrant_2.earn_permit
+      registrant_3.earn_permit
+      facility_1.administer_written_test(registrant_1)
+      facility_1.administer_written_test(registrant_2)
+      facility_1.administer_written_test(registrant_3)
+      facility_1.administer_road_test(registrant_1)
+      facility_1.administer_road_test(registrant_2)
+      facility_1.administer_road_test(registrant_3)
+
+      expect(facility_1.renew_drivers_license(registrant_1)).to eq(true)
+      expect(facility_1.renew_drivers_license(registrant_2)).to eq(true)
+      expect(facility_1.renew_drivers_license(registrant_3)).to eq(false)
+      expect(registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
+      expect(registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
+      expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
   end
 end
