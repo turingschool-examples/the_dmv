9b369938d832b9952edfcfa306796d06ae036f7c
margdaly <margaret.daly@gmail.com>
2023-02-10 21:18:11 -0700

    test: Add only administer if at least 16 and has permit test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -126,29 +126,50 @@ RSpec.describe Facility do
 
     it 'only administered if at least 16 and has a permit' do
       registrant_1 = Registrant.new('Bruce', 18, true )
-
-
+      registrant_2 = Registrant.new('Penny', 16 )
+      registrant_3 = Registrant.new('Tucker', 15 )
       facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
-      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      
+      facility_1.add_service('Written Test')
+
+      expect(facility_1.services).to eq(['Written Test'])
+      expect(registrant_1.age).to eq(18)
+      expect(registrant_2.age).to eq(16)
+      expect(registrant_3.age).to eq(15)
+      expect(registrant_1.permit?).to eq(true)
+      expect(registrant_2.permit?).to eq(false)
+      expect(registrant_3.permit?).to eq(false)
+      expect(facility_1.administer_written_test(registrant_1)).to eq(true)
+      expect(facility_1.administer_written_test(registrant_2)).to eq(false)
+      expect(facility_1.administer_written_test(registrant_3)).to eq(false)
+      
+      registrant_2.earn_permit
+      registrant_3.earn_permit
+
+      expect(facility_1.administer_written_test(registrant_2)).to eq(true)
+      expect(facility_1.administer_written_test(registrant_3)).to eq(false)
+      expect(registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+      expect(registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+      expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
   end
 
   describe '#administer road test' do
-    it 'can only be at a facility with that service' do
+    xit 'can only be at a facility with that service' do
+      facility_1.add_service('Road Test')
+    end
+
+    xit 'only administered if passed written test' do
 
     end
 
-    it 'only administered if passed written test' do
-
-    end
-
-    it 'registrant earns a license after road test' do
+    xit 'registrant earns a license after road test' do
 
     end
   end
 
   describe '#renew drivers license' do
-    it 'can be renewed if you have a license' do
+    xit 'can be renewed if you have a license' do
 
     end
   end
