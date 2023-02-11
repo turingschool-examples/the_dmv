f308541efdc17d7e0d7bd00530b05fd58afc4f8a
margdaly <margaret.daly@gmail.com>
2023-02-11 11:09:46 -0700

    test: Complete renew_license test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -195,7 +195,7 @@ RSpec.describe Facility do
   end
 
   describe '#renew license' do
-    xit 'can be renewed if you have a license' do
+    it 'can be renewed if you have a license' do
       registrant_1 = Registrant.new('Bruce', 18, true )
       registrant_2 = Registrant.new('Penny', 16 )
       registrant_3 = Registrant.new('Tucker', 15 )
