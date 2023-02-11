5bb377db7554154c3df41cdef04f3f4ff51b0926
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-11 14:49:47 -0700

    Test: include testing for administer_written_test  method

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe Facility do
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_2 = Registrant.new('Penny', 16 )
+    @registrant_3 = Registrant.new('Tucker', 15 )
   end
   
   describe '#initialize' do
@@ -89,6 +92,43 @@ RSpec.describe Facility do
       expect(@camaro.plate_type).to eq(:antique)
       @facility_1.register_vehicle(@bolt)
       expect(@bolt.plate_type).to eq(:ev)
+    end
+  end
+
+  describe '#written test' do
+    it 'registrants start with all keys as false' do
+      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+    it 'registrants permit status can be checked' do
+      expect(@registrant_1.permit?).to eq(true)
+      expect(@registrant_2.permit?).to eq(false)
+    end
+    it 'facility needs to register service before administering written test' do
+      expect(@facility_1.administer_written_test(registrant_1)).to eq(false)
+      # facility_1.add_service('Written Test')
+      expect(@facility_1.add_service('Written Test')).to eq(['Written Test'])
+      expect(@facility_1.administer_written_test(registrant_1)).to eq(true)
+    end
+    it 'registrant can take written test and updated written status' do
+      facility_1.add_service('Written Test')
+      expect(@facility_1.administer_written_test(registrant_1)).to eq(true)
+      expect(@registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+    end
+    it 'registrant must be 16 & have permit to do written test' do
+      facility_1.add_service('Written Test')
+      expect(@registrant_2.age).to eq(16)
+      expect(@registrant_2.permit?).to eq(false)
+      expect(@facility_1.administer_written_test(registrant_2)).to eq(false)
+      registrant_2.earn_permit
+      expect(@facility_1.administer_written_test(registrant_2)).to eq(true)
+      expect(@registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+      expect(@registrant_3.age).to eq(15)
+      expect(@registrant_3.age).to eq(false)
+      expect(@facility_1.administer_written_test(registrant_3)).to eq(false)
+      registrant_3.earn_permit
+      expect(@facility_1.administer_written_test(registrant_3)).to eq(false)
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
   end
 end
