fc325a260a30b68f61a8e88d5fdd975e66349d91
Nick Sacco <nicksacco10@gmail.com>
2023-02-12 11:08:39 -0800

    Test: Initialize vehicle_factory

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,1 +1,14 @@
 require 'spec_helper'
+
+RSpec.describe VehicleFactory do
+  before(:each) do
+    @factory = VehicleFactory.new
+  end
+
+  describe 'Initialize' do
+    it 'exists' do
+
+      expect(@factory).to be_a(VehicleFactory)
+    end
+  end
+end
