8311782cf42b2593e39673d6a36495c884c01ce4
Katie Lonsdale <katie952@gmail.com>
2023-02-12 10:30:20 -0700

    test: test for source

--- spec/analytics_spec.rb
+++ spec/analytics_spec.rb
@@ -2,13 +2,17 @@ require 'spec_helper'
 
 RSpec.describe do
   before(:each) do
-    dds = DmvDataService.new.wa_ev_registrations
-    analytics = Analytics.new
+    @dds = DmvDataService.new.wa_ev_registrations
+    @analytics = Analytics.new(@dds)
   end
 
   describe '#initialize' do
     it 'exists' do
-      expect(analytics).to be_an_instance_of(Analytics)
+      expect(@analytics).to be_an_instance_of(Analytics)
+    end
+
+    it 'has a source' do
+      expect(@analytics.source).to eq(@dds.wa_ev_registrations)
     end
   end
 
