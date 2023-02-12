60e2a31a05233ad21b96e6f515671d8f793cc9d7
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-12 00:32:35 -0700

    Create tests for Facilities specifications.

--- spec/data_analytics_spec.rb
+++ spec/data_analytics_spec.rb
@@ -16,12 +16,44 @@ RSpec.describe 'Analyize Data' do
   end
 
   describe 'Facilities' do 
-    xit 'finds daily hours for each facility' do
-      # CODE HERE
+    before(:each) do
+    @dmv_office_location = DmvDataService.new.or_dmv_office_locations
+    @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+    @missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+    @facility = FacilityConstructor.new  
+  end
+
+    it 'finds daily hours for NY facilities' do
+      new_york =  @facility.create_facility(@new_york_facilities)
+      
+      expect(new_york.first.daily_open_hours).to eq([
+        {:monday=>{:open=>"8:30 AM", :close=>"4:15 PM"},
+        :tuesady=>{:open=>"8:30 AM", :close=>"4:15 PM"},
+        :wednesday=>{:open=>"8:30 AM", :close=>"4:15 PM"},
+        :thursday=>{:open=>"8:30 AM", :close=>"5:45 PM"},
+        :friday=>{:open=>"8:30 AM", :close=>"4:15 PM"}}])
     end
 
-    xit 'which holidays are closed for each specific state' do
-      # CODE HERE
+    it 'finds daily hours for OR facilities' do
+      oregon_facilities = @facility.create_facility(@dmv_office_location)
+
+      expect(oregon_facilities.first).to be_a(Facility)
+      expect(oregon_facilities.first.daily_open_hours).to be(nil)
+    end
+
+    it 'finds daily hours for MO facilities' do
+      missouri =  @facility.create_facility(@missouri_facilities)
+      mo = missouri.first.daily_open_hours
+
+      expect(mo).to eq("Monday - Friday - 8:30 to 5:00, Last 4 week days - 8:00 to 5:00")
+    end
+      
+    it 'which holidays are closed for each specific state' do
+      missouri =  @facility.create_facility(@missouri_facilities)
+      mo = missouri.first.closed_for_the_holidays
+
+      expect(mo).to eq("Independence Day (07/04/22), Labor Day (09/05/22), Columbus Day (10/10/22), Veterans Day (11/11/22), Thanksgiving (11/24/22), Christmas Day Observed (12/26/22), New Year's Day Observed (01/02/23), Martin Luther King Day (01/16/23), Lincoln’s Birthday (02/13/23), President’s Day (02/20/23), Truman’s Birthday (05/08/23), Memorial Day (05/29/23), Independence Day (07/04/23), Labor Day (09/04/23), Columbus Day (10/09/23), Veterans Day (11/10/23), Thanksgiving (11/23/23), Christmas Day Observed (12/25/23)")
+
     end
   end
 
@@ -35,6 +67,5 @@ RSpec.describe 'Analyize Data' do
       expect(created_vehicles.first).to be_a(Vehicle)
       expect(created_vehicles.length).to be(250)
     end
-   
   end
 end
