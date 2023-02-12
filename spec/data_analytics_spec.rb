997c7df6a846251754610126feab1fbdb02e02ee
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-11 21:13:37 -0700

    Creaate and modify tests.

--- spec/data_analytics_spec.rb
+++ spec/data_analytics_spec.rb
@@ -2,32 +2,39 @@ require 'spec_helper'
 
 RSpec.describe 'Analyize Data' do
   describe 'EV Registration' do
-    it 'finds the most popular make and model' do
+    xit 'finds the most popular make and model' do
       # CODE HERE
     end
 
-    it 'counts number of registered vehicles by model year' do
+    xit 'counts number of registered vehicles by model year' do
       # CODE HERE
     end
 
-    it 'finds county that has most registered vehicles' do
+    xit 'finds county that has most registered vehicles' do
       # CODE HERE
     end
   end
 
   describe 'Facilities' do 
-    it 'finds daily hours for each facility' do
+    xit 'finds daily hours for each facility' do
       # CODE HERE
     end
 
-    it 'which holidays are closed for each specific state' do
+    xit 'which holidays are closed for each specific state' do
       # CODE HERE
     end
   end
 
   describe 'Vehicles' do
     it 'creates vehicle objects from NY State Registration data' do
-      #CODE HERE
+      factory = VehicleFactory.new
+      ny_state_registrations = DmvDataService.new.ny_state_registrations
+      created_vehicles = factory.create_vehicles(ny_state_registrations)
+      
+      expect(created_vehicles).to be_a(Array)
+      expect(created_vehicles.first).to be_a(Vehicle)
+      expect(created_vehicles.length).to be(250)
     end
+   
   end
 end
