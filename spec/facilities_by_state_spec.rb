b9309ff81071911f6c68d0c92cd2003afabd2de1
Thomas Hawley <thawley22@turing.edu>
2023-02-11 18:52:41 -0700

    test: Create tests for each state data

--- spec/facilities_by_state_spec.rb
+++ spec/facilities_by_state_spec.rb
@@ -12,13 +12,28 @@ RSpec.describe FacilitiesByState do
     dmv = DmvDataService.new.or_dmv_office_locations
 
     facilities.create_facilities(dmv)
-
+    
     expect(facilities.all_facilities[0]).to be_instance_of(Facility)
     expect(facilities.all_facilities.length).to eq(59)
   end
-
-  it 'can create facilities from a different state' do 
+  
+  it 'can create facilities from NY' do 
     facilities = FacilitiesByState.new
     dmv = DmvDataService.new.ny_dmv_office_locations
+    
+    facilities.create_facilities(dmv)
+    require 'pry'; binding.pry
+    expect(facilities.all_facilities[0]).to be_instance_of(Facility)
+    expect(facilities.all_facilities.length).to eq(169)
+  end
+  
+  it 'can create facilities from MO' do
+    facilities = FacilitiesByState.new
+    dmv = DmvDataService.new.mo_dmv_office_locations
+    
+    facilities.create_facilities(dmv)
+    
+    expect(facilities.all_facilities[0]).to be_instance_of(Facility)
+    expect(facilities.all_facilities.length).to eq(178)
   end
 end
