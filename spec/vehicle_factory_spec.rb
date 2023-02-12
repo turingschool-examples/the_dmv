455ddab3d4ca743d56451fcd573d9a60b110b1b7
margdaly <margaret.daly@gmail.com>
2023-02-12 13:23:42 -0700

    test: Add describe #create_vehicles test

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -9,12 +9,16 @@ RSpec.describe VehicleFactory do
     end
   end
 
-  describe 'ev_registration' do
-    it 'has data' do
+  describe '#create_vehicles' do
+    it 'has creates cars from wa_registration info' do
       factory = VehicleFactory.new
       wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+      factory.create_vehicles(wa_ev_registrations)
 
-      expect(factory.create_vehicles(wa_ev_registrations)).to eq([wa_ev_registrations]) 
+      expect(wa_ev_registrations).to be_an_instance_of(Array)
+      expect(wa_ev_registrations.size).to eq(1000) 
+      expect(factory.factory_vehicles).to be_an_instance_of(Array)
+      expect(factory.factory_vehicles.size).to eq(1000)
     end
   end
 end
