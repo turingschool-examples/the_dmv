c49b774c7a58fdac24bb2061e466f6c638bf8bd9
Katie Lonsdale <katie952@gmail.com>
2023-02-12 11:56:36 -0700

    feat: add most popular model helper method

--- lib/analytics.rb
+++ lib/analytics.rb
@@ -6,17 +6,19 @@ class Analytics
   end
 
   def most_popular_make_model
-    "#{@source.most_popular_make} #{@source.most_popular_model}"
-
+    "#{most_popular_make} #{most_popular_model}"
   end
 
   def most_popular_make
-    # create hash with make as keys and occurences as values
-    # iterate over array and increment count by one each time make(key) occurs
-    # sort hash by number of ocurrences
-    # return key with highest value and assign to make
     make_and_count = Hash.new(0)
     @source.each{|car| make_and_count[car[:make]] += 1}
     make_and_count.sort_by{|make, count| count}.last[0]
+  end
+
+  def most_popular_model
+    model_and_count = Hash.new(0)
+    @source.select{|car| car[:make] == most_popular_make}.
+    each{|car| model_and_count[car[:model]] += 1}
+    model_and_count.sort_by{|model, count| count}.last[0]
   end
 end
