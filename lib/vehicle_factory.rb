98539becd77f61613ceb6a45b922684bc9ec9e70
Young Koh <youngheon6@gmail.com>
2023-02-12 11:19:51 -0700

    Add vehicle_factory

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,9 +1,13 @@
-require './lib/vehicle'
-require './lib/dmv_data_service'
-
 class VehicleFactory
   attr_reader :vehicles
-  def initialize
+
+  def create_vehicles(dmv_hash)
     @vehicles = []
+    dmv_hash.each do |vehicle|
+      new_vehicle = Vehicle.new(vehicle)
+      new_vehicle.engine = :ev
+      @vehicles << new_vehicle
+      @vehicles
+    end
   end
 end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
-Rspec.describe VehicleFactory do
+RSpec.describe VehicleFactory do
   describe '#initialize' do
     it 'can initialize' do
       factory = VehicleFactory.new
       expect(factory).to be_an_instance_of(VehicleFactory)
+    end
+  end
+
+  describe '#create_vehicles' do
+    it "creates an array of vehicle objects with the given hash" do
+      factory = VehicleFactory.new
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+      factory.create_vehicles(wa_ev_registrations)
+      factory.vehicles
     end
   end
 end
