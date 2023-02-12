eabe8104983a92726dfce9a7bfe968cefe458c9c
Nick Sacco <nicksacco10@gmail.com>
2023-02-12 09:38:10 -0800

    Feat: Add renew license method

--- lib/facility.rb
+++ lib/facility.rb
@@ -52,6 +52,13 @@ class Facility
     end
   end
 
+  def renew_drivers_license(registrant)
+    if @services.include?('Renew License') && registrant.license_data[:license] == true
+      registrant.license_data[:renewed] = true
+    else
+      false
+    end
+  end
 
 
 end
