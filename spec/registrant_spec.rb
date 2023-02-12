3f5ffb1b1c49e32360c49dffa5fbb7303b192227
Angel Byun <abyun22@turing.edu>
2023-02-11 18:11:41 -0700

    Modify registrant spec file

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -3,31 +3,35 @@ require 'spec_helper'
 require './lib/registrant'
 
 RSpec.describe Registrant do
-    it 'exists' do
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
-
-    expect(registrant_1.name).to eq('Bruce')
-    expect(registrant_1.age).to eq(18)
-    expect(registrant_1.permit?).to eq(true)
-    expect(registrant_2.name).to eq('Penny')
-    expect(registrant_2.age).to eq(15)
-    expect(registrant_2.permit?).to eq(false)
+    before(:each) do
+        @registrant_1 = Registrant.new('Bruce', 18, true )
+        @registrant_2 = Registrant.new('Penny', 16 )
+        @registrant_3 = Registrant.new('Tucker', 15 )    
     end
 
-    it '#license_data' do
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
-
-    expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
-    expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    describe '#initialize' do
+        it 'can initialize' do
+         expect(@registrant_1.name).to eq('Bruce')
+            expect(@registrant_1.age).to eq(18)
+            expect(@registrant_1.permit?).to eq(true)
+            expect(@registrant_2.name).to eq('Penny')
+            expect(@registrant_2.age).to eq(16)
+            expect(@registrant_2.permit?).to eq(false)
+        end
     end
 
-    it '#earn_permit' do
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
+    describe '#license data' do
+        it 'can add license data' do
 
-    expect(registrant_2.earn_permit).to eq(true)
+        expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+        expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+        end
+    end
+    
+    describe '#ear permit' do
+        it 'can earn a permit' do
+        expect(@registrant_2.earn_permit).to eq(true)
+        end
     end
 
 end
