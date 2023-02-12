046187a29dbb7929442e8f4e437f1869346eff1b
Nick Sacco <nicksacco10@gmail.com>
2023-02-12 14:53:53 -0800

    Feat: create_vehicles hash

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -3,6 +3,16 @@ class VehicleFactory
   end
 
   def create_vehicles(vehicle_details)
-    
+    new_vehicles = []
+    vehicle_details.each do |car|
+      vehicle_details_hash = Hash.new
+      vehicle_details_hash[:engine] = :ev
+      vehicle_details_hash[:make] = car[:make]
+      vehicle_details_hash[:model] = car[:model]
+      vehicle_details_hash[:vin] = car[:vin_1_10]
+      vehicle_details_hash[:year] = car[:model_year]
+      new_vehicles << Vehicle.new(vehicle_details_hash)
+    end
+    new_vehicles
   end
 end
