65be5f5abb9742cf0c0eea97637c5e648b08289d
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-12 14:29:38 -0600

    Test: add tests for #create_facilities in FacilityFactory class and Oregon state data

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -1,15 +1,57 @@
 require 'spec_helper'
 
 RSpec.describe FacilityFactory do
-  or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
-  or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
-  or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
+  oregon_facility_data = DmvDataService.new.or_dmv_office_locations
+  new_york_facility_data = DmvDataService.new.ny_dmv_office_locations
+  missouri_facility_data = DmvDataService.new.mo_dmv_office_locations
 
   factory = FacilityFactory.new
+
+  oregon_facilities = factory.create_facilities(oregon_facility_data, :OR)
+  new_york_facilities = factory.create_facilities(new_york_facility_data, :NY)
+  missouri_facilities factory.create_facilities(missouri_facility_data, :MO)
 
   describe '#initialize' do
     it 'can initialize' do
       expect(factory).to be_a(FacilityFactory)
+    end
+  end
+
+  describe '#create_facilities' do
+    it 'returns an array' do
+      expect(oregon_facilities).to be_a(Array)
+      expect(new_york_facilities).to be_a(Array)
+      expect(missouri_facilities).to be_a(Array)
+    end
+
+    it 'returns an array of Facility objects' do
+      expect(oregon_facilties.all? { |facility| facility.is_a?(Facility) }).to be(true)
+      expect(new_york_facilties.all? { |facility| facility.is_a?(Facility) }).to be(true)
+      expect(missouri_facilties.all? { |facility| facility.is_a?(Facility) }).to be(true)
+    end
+
+    it 'assigns the @name attribute on every facility to a string value' do
+      expect(oregon_facilities.all? { |facility| facility.name.is_a?(String) }).to be(true)
+    end
+
+    it 'assigns the @address attribute on every facility to a string value' do
+      expect(oregon_facilities.all? { |facility| facility.address.is_a?(String) }).to be(true)
+    end
+
+    it 'assigns the @phone attribute on every facility to a string value' do
+      expect(oregon_facilities.all? { |facility| facility.phone.is_a?(String) }).to be(true)
+    end
+
+    it 'assigns the correct string value to the @name attribute on every Oregon facility' do
+      expect(oregon_facilities.first.name).to eq('JTDKN3DP8D')
+    end
+
+    it 'assigns the correct string value to the @address attribute on every Oregon facility' do
+      expect(oregon_facilities.first.address).to eq('2013')
+    end
+
+    it 'assigns the correct string value to the @phone attribute on every Oregon facility' do
+      expect(oregon_facilities.first.phone).to eq('TOYOTA')
     end
   end
 end
