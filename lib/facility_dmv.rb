ce35dbb4e14675d32d345313b6d20e7b90615c47
Amy Spears <amspears007@gmail.com>
2023-02-12 14:46:40 -0700

    Add create_facility method to FacilityDmv class

--- lib/facility_dmv.rb
+++ lib/facility_dmv.rb
@@ -1,6 +1,17 @@
 class FacilityDmv
-
+  attr_reader :facilities
   def initialize
-    
+    @facilities = []
+  end
+
+  def create_facility(facility_data)
+    facility_data.each do |facility|
+      facility_hash = {
+        title: facility[:title],
+        zip_code: facility[:zip_code],
+        website: facility[:website],
+        phone_number: facility[:phone_number]
+        }
+    end
   end
 end
--- spec/facility_dmv_spec.rb
+++ spec/facility_dmv_spec.rb
@@ -2,12 +2,20 @@ require 'spec_helper'
 
 RSpec.describe FacilityDmv do
   it 'exists from API' do
-    facility = FacilityDmv.new
+    facility_dmv = FacilityDmv.new
     oregon_facilities = DmvDataService.new.or_dmv_office_locations
 
-    expect(facility).to be_an_instance_of(FacilityDmv)
+    expect(facility_dmv).to be_an_instance_of(FacilityDmv)
     expect(oregon_facilities).to be_an_instance_of(Array)
     expect(oregon_facilities.size).to eq(59)
+  end
 
+  it 'creates new facilityDmv instances' do
+    facility_dmv = FacilityDmv.new
+    oregon_facilities = DmvDataService.new.or_dmv_office_locations
+
+    expect(facility_dmv.facilities).to eq([])
+    # require 'pry'; binding.pry
+    expect(facility_dmv.create_facility(oregon_facilities)).to be_an_instance_of(Array)
   end
 end
