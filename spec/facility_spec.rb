b78f8a75527056b857a6bff914c1915377010199
Nick Sacco <nicksacco10@gmail.com>
2023-02-12 09:19:55 -0800

    Test: Add registrant2 to road test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -143,6 +143,25 @@ RSpec.describe Facility do
       @facility_1.administer_written_test(registrant_1)
 
       expect(@facility_1.administer_road_test(registrant_3)).to eq(false)
+      
+      registrant_3.earn_permit
+      
+      expect(@facility_1.administer_road_test(registrant_3)).to eq(false)
+      expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@facility_1.administer_road_test(registrant_1)).to eq(false)
+
+      @facility_1.add_service('Road Test')
+
+      expect(@facility_1.services).to eq(["Written Test", "Road Test"])
+      expect(@facility_1.administer_road_test(registrant_1)).to eq(true)
+      expect(registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
+
+      @facility_1.administer_written_test(registrant_2)
+      registrant_2.earn_permit
+      
+      expect(@facility_1.administer_written_test(registrant_2)).to eq(true)
+      expect(@facility_1.administer_road_test(registrant_2)).to eq(true)
+      expect(registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
     end
   end
 end
