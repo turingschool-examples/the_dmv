e7e4e2b0dffb66a9ddfa46a51d3f85f2f26ad5c6
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-02-11 20:50:58 -0700

    add office_builder_spec file and create test to ensure that data is being pulled from remote source

---
+++ spec/office_builder_spec.rb
@@ -0,0 +1,13 @@
+require './spec/spec_helper'
+# require './lib/office_builder'
+
+RSpec.describe "OfficeBuilder" do
+  before(:each) do
+    # @state_offices = OfficeBuilder.new
+    @or_offices = DmvDataService.new.or_dmv_office_locations
+    # @state_offices.create_offices(@or_offices)
+  end
+  it "pulls data from remote source" do
+    expect(@or_offices).to be_a(Array)
+  end
+end
