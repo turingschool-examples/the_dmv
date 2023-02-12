b08f784fa0c9bcd6aa8fafda58dec99b68708d33
Crow Rising <miscbyrd@gmail.com>
2023-02-12 16:38:10 -0700

    Complete Iteration 3

--- lib/remote_offices.rb
+++ lib/remote_offices.rb
@@ -1,50 +1,49 @@
 class RemoteOffice
 
-  attr_reader :other_office_locations
+  attr_reader :other_offices,
+              :remote_office_locations
   def initialize
     @other_offices = []
   end
 
-  def or_office_locations(details)
+  def checks_location(location_data)
+    remote_office_locations(location_data)
+  end
+
+  def remote_office_locations(location_data)
     or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
-      or_dmv_office_locations.each do |office|
-        details = {
-          name: office[:title],
-          address: office[:human_address],
-          phone: office[:phone_number],
-          }
-        @other_offices << (offices = Facility.new(details))
-    end
-    @other_offices
-  end
-
-  def ny_office_locations(details)
     ny_office_locations = DmvDataService.new.ny_dmv_office_locations
-      ny_office_locations.each do |office|
-        details = {
-          name: office[:office_name],
-          address: "#{office[:street_address_line_1]}, #{office[:street_address_line_2]}, #{office[:city]}, #{office[:state]}, #{office[:zip_code]}",
-          phone: office[:public_phone_number]
-        }
-        @other_offices << (offices = Facility.new(details))
-    end
-    @other_offices  
-  end
-
-  def mo_office_locations(details)
     mo_office_locations = DmvDataService.new.mo_dmv_office_locations
-      mo_office_locations.each do |office|
-        details = {
-          name: office[:name],
-          address: "#{office[:address1]}, #{office[:city]}, #{office[:state]}, #{office[:zip_code]}",
-          phone: office[:phone]
-        }
-        @other_offices << (offices = Facility.new(details))
+      if location_data == or_dmv_office_locations
+        or_dmv_office_locations.each do |office|
+           location_data = {
+            name: office[:title],
+            address: office[:human_address],
+            phone: office[:phone_number],
+            }
+            @other_offices << (offices = Facility.new(location_data))
+        end
+        @other_offices
+      elsif location_data == ny_office_locations
+        ny_office_locations.each do |office|
+          location_data = {
+            name: office[:office_name],
+            address: "#{office[:street_address_line_1]}, #{office[:street_address_line_2]}, #{office[:city]}, #{office[:state]}, #{office[:zip_code]}",
+            phone: office[:public_phone_number]
+          }
+          @other_offices << (offices = Facility.new(location_data))
+        end
+        @other_offices
+      else location_data == mo_office_locations
+        mo_office_locations.each do |office|
+          location_data = {
+            name: office[:name],
+            address: "#{office[:address1]}, #{office[:city]}, #{office[:state]}, #{office[:zip_code]}",
+            phone: office[:phone]
+          }
+          @other_offices << (offices = Facility.new(location_data))
+        end
+          @other_offices  
+      end
     end
-    @other_offices  
   end
-end
-
-# Sonow I want all of these data bases to be able to run together
-# how?
-#
--- spec/remote_offices_spec.rb
+++ spec/remote_offices_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe RemoteOffice do
     it 'has oregon office info' do
       offices = RemoteOffice.new
       or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
-      office = offices.or_office_locations(or_dmv_office_locations)
+      office = offices.checks_location(or_dmv_office_locations)
 
       expect(office).to be_a(Array)
       expect(office.first).to be_a(Facility)
@@ -25,7 +25,7 @@ RSpec.describe RemoteOffice do
     it 'has ny office info' do
       offices = RemoteOffice.new
       ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
-      office = offices.ny_office_locations(ny_dmv_office_locations)
+      office = offices.checks_location(ny_dmv_office_locations)
 
       expect(office).to be_a(Array)
       expect(office.first).to be_a(Facility)
@@ -33,11 +33,11 @@ RSpec.describe RemoteOffice do
     end
   end
 
-  describe 'ny_offices' do
-    it 'has ny office info' do
+  describe 'mo_offices' do
+    it 'has mo office info' do
       offices = RemoteOffice.new
       mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
-      office = offices.mo_office_locations(mo_dmv_office_locations)
+      office = offices.checks_location(mo_dmv_office_locations)
 
       expect(office).to be_a(Array)
       expect(office.first).to be_a(Facility)
