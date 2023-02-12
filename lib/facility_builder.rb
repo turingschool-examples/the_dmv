ae984dc5542a6116c52ed633be74d7eefc86e3e7
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-02-11 21:32:13 -0700

    allow facility builder to create an array with information from or state website

--- lib/facility_builder.rb
+++ lib/facility_builder.rb
@@ -1,6 +1,16 @@
 class FacilityBuilder
 
-  def create_facilities do
-    @state_facilities = []
+attr_reader :state_facility_list
+
+  def create_facilities(facility_data)
+    @state_facility_list = []
+    for i in 0..facility_data.count - 1
+      current_hash = facility_data[i]
+      facility_hash = {name: current_hash[:title]}
+      @state_facility_list << facility_hash
+    end
+    @state_facility_list
   end
 end
+#                  address: facility_data[]
+#                  phone: facility_data[]}
--- spec/facility_builder_spec.rb
+++ spec/facility_builder_spec.rb
@@ -1,19 +1,23 @@
 require './spec/spec_helper'
-# require './lib/office_builder'
+require './lib/facility_builder'
 
 RSpec.describe "FacilityBuilder" do
   before(:each) do
-    # @state_facilities = FacilityBuilder.new
+    @state_facilities = FacilityBuilder.new
     @or_facilities = DmvDataService.new.or_dmv_office_locations
-    # @state_facilities.create_facilities(@or_facilities)
+    @or_dmv_locations = @state_facilities.create_facilities(@or_facilities)
   end
 
   it "pulls data from remote source" do
     expect(@or_facilities).to be_a(Array)
     p @or_facilities.count
-    p @or_facilities[0].keys
+    p @or_facilities[0][:title]
     p @or_facilities[0].values
   end
 
-  # it
+  it "exists" do
+    expect(@state_facilities).to be_a(FacilityBuilder)
+    expect(@or_dmv_locations).to be_a(Array)
+    expect(@or_dmv_locations.count).to eq(59)
+  end
 end
