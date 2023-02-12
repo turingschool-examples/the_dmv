31110dc9e2cb7c789293b7063a9fe1607b8ce79a
Nick Sacco <nicksacco10@gmail.com>
2023-02-11 20:41:05 -0800

    Test: Add more tests to written test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -30,6 +30,7 @@ RSpec.describe Facility do
       @facility_1.add_service('New Drivers License')
       @facility_1.add_service('Renew Drivers License')
       @facility_1.add_service('Vehicle Registration')
+      #@facility_1.add_service('Written Test')
       expect(@facility_1.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
       expect(@facility_2.services).to eq([])
     end
@@ -100,10 +101,19 @@ RSpec.describe Facility do
       registrant_2 = Registrant.new('Penny', 16 )
       registrant_3 = Registrant.new('Tucker', 15 )
       registrant_1.license_data
-      
+  
       expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
       expect(registrant_1.permit?).to eq(true)
       expect(@facility_1.administer_written_test(registrant_1)).to eq(false)
+      expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+
+      @facility_1.add_service('Written Test')
+      @facility_1.administer_written_test(registrant_1)
+
+      expect(@facility_1.administer_written_test(registrant_1)).to eq(true)
+      expect(registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+
+
     end
   end
 
