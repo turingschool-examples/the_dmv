9e500abee04c73644826bb3765764d99ef3dc8f5
Grace Joh <graceehjoh@gmail.com>
2023-02-12 16:04:27 -0600

    test: add tests for #build_facilities

--- spec/facility_builder_spec.rb
+++ spec/facility_builder_spec.rb
@@ -13,13 +13,32 @@ RSpec.describe FacilityBuilder do
   end
 
   describe '#build_facilities' do
-    it 'can create an array of facility objects' do
+    it 'can create an array of facility objects for Oregon' do
       or_facilities = DmvDataService.new.or_dmv_office_locations
       @builder.build_facilities(or_facilities)
 
       expect(@builder.facilities).to be_a(Array)
       expect(@builder.facilities.first).to be_a(Facility)
       expect(@builder.facilities.size).to eq(59)
+    end
+
+    it 'can create an array of facility objects for New York' do
+      ny_facilities = DmvDataService.new.ny_dmv_office_locations
+      
+      @builder.build_facilities(ny_facilities)
+
+      expect(@builder.facilities).to be_a(Array)
+      expect(@builder.facilities.first).to be_a(Facility)
+      expect(@builder.facilities.size).to eq(169)
+    end
+
+    it 'can create an array of facility objects for Missouri' do
+      mo_facilities = DmvDataService.new.mo_dmv_office_locations
+      @builder.build_facilities(mo_facilities)
+
+      expect(@builder.facilities).to be_a(Array)
+      expect(@builder.facilities.first).to be_a(Facility)
+      expect(@builder.facilities.size).to eq(178)
     end
   end
 end
