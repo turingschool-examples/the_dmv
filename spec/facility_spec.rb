7748716dbf8335e42c2b529ebdf45b7ef0fd54f9
Nickolas Marquez <nickomarquez7@gmail.com>
2023-02-11 17:30:54 -0700

    Included @facility_2 as a new instance

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 RSpec.describe Facility do
   before(:each) do
-    
     @facility = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+    @facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+
   end
   describe '#initialize' do
     it 'can initialize' do
