f2d8a90108b94054659ae5ba1b9f773383bfd3f2
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-02-11 22:48:33 -0700

    added and tested helper method to clean address data from oregon dmv list

--- lib/facility_builder.rb
+++ lib/facility_builder.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class FacilityBuilder
 
 attr_reader :state_facility_list
@@ -6,11 +8,18 @@ attr_reader :state_facility_list
     @state_facility_list = []
     for i in 0..facility_data.count - 1
       current_hash = facility_data[i]
-      facility_hash = {name: current_hash[:title]
-                       address: facility_data[]
-                       phone: facility_data[]}
+      facility_hash = {name: current_hash[:title],
+                       address: or_address_cleaner(current_hash[:location_1][:human_address]),
+                       phone: current_hash[:phone_number]}
       @state_facility_list << facility_hash
     end
     @state_facility_list
   end
+
+  def or_address_cleaner(ugly_address)
+    JSON.parse(ugly_address)["address"]
+  end
 end
+
+# "{\"address\": \"2242 Santiam Hwy SE\", \"city\": \"Albany\", \"state\": \"OR\", \"zip\": \"97321\"}"
+# or_address_cleaner(current_hash[:location_1][:human_address]),
--- spec/facility_builder_spec.rb
+++ spec/facility_builder_spec.rb
@@ -1,29 +1,42 @@
 require './spec/spec_helper'
 require './lib/facility_builder'
 
-RSpec.describe "FacilityBuilder" do
-  before(:each) do
-    @state_facilities = FacilityBuilder.new
-    @or_facilities = DmvDataService.new.or_dmv_office_locations
-    @or_dmv_locations = @state_facilities.create_facilities(@or_facilities)
-  end
+RSpec.describe FacilityBuilder do
+  describe "Oregon" do
+    before(:each) do
+      @or_state_facilities = FacilityBuilder.new
+      @or_facilities = DmvDataService.new.or_dmv_office_locations
+      @or_state_facilities.create_facilities(@or_facilities)
+    end
 
-  it "pulls data from remote source" do
-    expect(@or_facilities).to be_a(Array)
-    p @or_facilities.count
-    p @or_facilities[0][:title]
-    p @or_facilities[0].values
-  end
+    it "pulls data from remote source" do
+      expect(@or_facilities).to be_a(Array)
+      p @or_facilities.count
+      p @or_facilities[0].keys
+      p @or_facilities[0][:title]
+      p @or_facilities[0].values
+    end
 
-  it "exists" do
-    expect(@state_facilities).to be_a(FacilityBuilder)
-    expect(@or_dmv_locations).to be_a(Array)
-    expect(@or_dmv_locations.count).to eq(59)
-  end
+    it "exists" do
+      expect(@or_state_facilities).to be_a(FacilityBuilder)
+      expect(@or_state_facilities.state_facility_list).to be_a(Array)
+      expect(@or_state_facilities.state_facility_list.count).to eq(59)
+    end
 
-  it "has the expected keys" do
-    for i in 0..@or_dmv_locations - 1
-      expect(@or_dmv_locations[i].keys).to eq([:name, :address, :phone])
+    it "has the expected keys" do
+      for i in 0..@or_state_facilities.state_facility_list.count - 1
+        expect(@or_state_facilities.state_facility_list[i].keys).to eq([:name, :address, :phone])
+      end
+    end
+
+    it "has the expected values" do
+      p @or_state_facilities.state_facility_list[0][:address]
+      # expect(@or_state_facilities.state_facility_list[0].values). to eq(["Albany DMV Office", "2242 Santiam Hwy SE", "541-967-2014"])
+    end
+
+    it "has a working or_address_cleaner method" do
+      expect(@or_state_facilities.or_address_cleaner("{\"address\": \"2242 Santiam Hwy SE\", \"city\": \"Albany\", \"state\": \"OR\", \"zip\": \"97321\"}")).to eq("2242 Santiam Hwy SE")
     end
   end
+
 end
