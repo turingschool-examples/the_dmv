b5cf7eb722d944d644da032b01ac7303ea223318
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-12 15:08:45 -0600

    Feat: add #create_facilities and #create_oregon_facilities

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,3 +1,22 @@
-class FacilityFactory
+require './lib/facility'
+require 'json'
 
+class FacilityFactory
+  def create_facilities(facilities_data, state)
+    case state
+      when :OR then create_oregon_facilities(facilities_data)
+    end
+  end
+
+  def create_oregon_facilities(facilities_data)
+    facilities_data.map do |facility|
+      address = JSON.parse(facility[:location_1][:human_address], symbolize_names: true)
+
+      Facility.new({
+        name: facility[:title],
+        address: "#{address[:address]} #{address[:city]} #{address[:state]} #{address[:zip]}",
+        phone: facility[:phone_number]
+      })
+    end
+  end
 end
