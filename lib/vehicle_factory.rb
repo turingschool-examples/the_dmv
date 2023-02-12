adad50d1de7f80def3acf4d58190ce2fd7442840
margdaly <margaret.daly@gmail.com>
2023-02-12 13:22:36 -0700

    feat: Add initialize and create_vehicles to VehicleFactory class

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,9 +1,19 @@
 class VehicleFactory
-  def initialize
-    @create_vehicles = []
-  end
+  attr_reader :factory_vehicles
 
-  def create_vehicles(ev_registration)
-    @create_vehicles << ev_registration
+  def initialize
+    @factory_vehicles = []
+  end
+  
+  def create_vehicles(info_registrations)
+    info_registrations.map do |hash| 
+      new_car = {
+        vin: hash[:vin_1_10],
+        year: hash[:model_year],
+        engine: :ev
+      }
+      @factory_vehicles << Vehicle.new(new_car)
+    end
   end
 end
+   
