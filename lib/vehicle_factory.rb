954acdc62a7e5a63dd0b8b70dc2eb463662c4aa3
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-11 19:08:00 -0700

    feat: create create vehicle method

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -2,6 +2,33 @@ require 'dmv_data_service'
 require 'vehicle'
 
 class VehicleFactory
+  attr_accessor :rendered_data_array, :made_vehicles
+
   def initialize
+    @rendered_data_array = []
+    @made_vehicles = []
   end
+
+  def render_data(list)
+    list.each do |spec|
+      rendered_data = {
+        vin: (spec[:vin_1_10]),
+        year: (spec[:model_year]),
+        make: (spec[:make]),
+        model: (spec[:model]),
+        engine: :ev
+      }
+      @rendered_data_array << rendered_data
+    end
+  end
+
+
+  def create_vehicles(list)
+    self.render_data(list)
+    @rendered_data_array.each do |vehicle_details1|
+      @made_vehicles << Vehicle.new(vehicle_details1)
+      
+    end
+  end
+    
 end
