ae57fb6a75583dfbd6e4b5e93c8f2f6efb1de9e8
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-12 17:29:42 -0500

    test: add test for initialize method

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe FacilityFactory do
     @factory = FacilityFactory.new
     @or_dmv = DmvDataService.new.or_dmv_office_locations
   end
+
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@factory).to be_a FacilityFactory
+    end
+  end
 end
