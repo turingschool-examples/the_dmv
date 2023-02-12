e55a955491950207828fa2e3df79f43aa7ea75ad
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-12 04:03:14 -0600

    Feat: add vehicle_factory to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,6 @@ require './lib/facility'
 require './lib/vehicle'
 require './lib/dmv_data_service'
 require './lib/registrant'
+require './lib/vehicle_factory'
 require './lib/test_data'
 include TestData
