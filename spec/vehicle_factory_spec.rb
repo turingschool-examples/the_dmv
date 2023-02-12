052eefa29821e54304fbc4e3f86b2642f84bed4c
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-11 20:07:13 -0700

    test: Add test for accessing api from multiple data sets

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -3,13 +3,26 @@ require 'spec_helper'
 RSpec.describe VehicleFactory do
   it 'accesses data from api' do
     factory = VehicleFactory.new
-    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+    wa_ev_registrations = :wa_ev_registrations
 
     expect(factory).to be_an_instance_of(VehicleFactory)
     
     factory.create_vehicles(wa_ev_registrations)
 
-    expect(factory.create_vehicles(wa_ev_registrations)).to eq(DmvDataService.new.wa_ev_registrations
-    )
+    expect(factory.create_vehicles(wa_ev_registrations)).to eq(DmvDataService.new.send(:wa_ev_registrations))
+  end
+
+  it 'access data from another dmv #oregon' do
+    factory = VehicleFactory.new
+    or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
+    
+    expect(factory).to be_an_instance_of(VehicleFactory)
+        
+    factory.create_vehicles(or_dmv_office_locations)
+    
+    expect(factory.create_vehicles(or_dmv_office_locations)).to eq(DmvDataService.new.send)
+      
+    
+    
   end
 end
