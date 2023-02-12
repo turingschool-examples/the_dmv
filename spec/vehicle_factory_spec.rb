37f2e8f928ec88356d8f655b94e995b69f6b141d
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-12 04:03:40 -0600

    Test: add tests for #create_vehicles

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe VehicleFactory do
       expect(@factory).to be_a(VehicleFactory)
     end
   end
+
+  describe '#create_vehicles' do
+    it 'returns an array' do
+      expect(@factory.create_vehicles).to be_a(Array)
+    end
+
+    it 'returns an array of Vehicle objects' do
+      expect(@factory.create_vehicles.all { |vehicle| vehicle.is_a?(Vehicle) }).to be(true)
+    end
+  end
 end
