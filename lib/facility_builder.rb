bc65fb06064c886cbaec9192e8082a7ad2a12689
Grace Joh <graceehjoh@gmail.com>
2023-02-11 22:14:06 -0600

    feat: add FacilityBuilder #build_facilities for Oregon data

--- lib/facility_builder.rb
+++ lib/facility_builder.rb
@@ -1,7 +1,24 @@
+require 'json'
+
 class FacilityBuilder
   attr_reader :facilities
 
   def initialize
     @facilities = []
+  end
+
+  def build_facilities(data)
+    state_data = []
+    data.each do |data_hash|
+      facility_details = {}
+      facility_details[:name] = data_hash[:title]
+      facility_details[:address] = JSON.parse(data_hash[:location_1][:human_address]).values.join(' ')
+      facility_details[:phone] = data_hash[:phone_number]
+      state_data << facility_details
+    end
+    state_data.each do |facility_details|
+      @facilities << Facility.new(facility_details)
+    end
+    @facilities
   end
 end
