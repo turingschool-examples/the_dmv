c5f98288ca86a6cf67f345e9fb5815980c93476d
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-13 16:40:06 -0700

    test if facility can administer a written test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -77,6 +77,14 @@ RSpec.describe Facility do
       expect(@facility_2.registered_vehicles).to eq([])
     end
   end
+
+  describe '#registrant' do
+    it 'can administer a written test' do
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      @facility.add_service('Written Test')
+      @facility.administer_written_test(registrant_1).to eq(true)
+    end
+  end
 end
 
 
