5d425f0330a5f6da0a12ee7fea9e405f39f62a1d
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-12 20:43:46 -0500

    refactor: create standardize_data helper methods for each state

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,6 +1,6 @@
 class FacilityFactory
 
-  @@valid_states = [:OR, :MO, :NY]
+  @@valid_states = [:MO, :NY, :OR]
 
   def initialize
   end
@@ -8,40 +8,39 @@ class FacilityFactory
   def create_facilities(facility_data, state)
     return "Sorry, this state is not yet supported." if !@@valid_states.include?(state)
 
-    if state == :OR
-      facility_details_array =
-      facility_data.map do |facility|
-          facility_details = {
-            name: facility[:title],
-            phone: facility[:phone_number],
-            address: or_format_address(facility)
-          }
-        end
+    if state == :MO
+      facility_details_array = mo_standardize_data(facility_data)
     elsif state == :NY
-      facility_details_array =
-      facility_data.map do |facility|
-        facility_details = {
-          name: ny_format_name(facility),
-          phone: ny_format_phone(facility[:public_phone_number]),
-          address: ny_format_address(facility)
-        }
-      end
-    elsif state == :MO
-      facility_details_array =
-      facility_data.map do |facility|
-        facility_details = {
-          name: mo_format_name(facility),
-          phone: mo_format_phone(facility),
-          address: mo_format_address(facility)
-        }
-      end
+      facility_details_array = ny_standardize_data(facility_data)
+    elsif state == :OR
+      facility_details_array = or_standardize_data(facility_data)
     end
 
     facility_details_array.map { |facility_details| Facility.new(facility_details) }
   end
 
-  def or_format_address(data)
-    data[:location_1][:human_address].delete('{}\"').split(', ').map { |address| address.split(': ')[1] }.join(" ")
+  def mo_standardize_data(data)
+    data.map do |facility|
+      {name: mo_format_name(facility),
+       phone: mo_format_phone(facility),
+       address: mo_format_address(facility)}
+    end
+  end
+
+  def ny_standardize_data(data)
+    data.map do |facility|
+      {name: ny_format_name(facility),
+       phone: ny_format_phone(facility),
+       address: ny_format_address(facility)}
+    end
+  end
+
+  def or_standardize_data(data)
+    data.map do |facility|
+      {name: facility[:title],
+       phone: facility[:phone_number],
+       address: or_format_address(facility)}
+    end
   end
 
   def mo_format_name(data)
@@ -66,7 +65,11 @@ class FacilityFactory
   end
 
   def ny_format_phone(data)
-    return "No phone." if data.nil?
-    data.insert(3, '-').insert(7, '-')
+    return "No phone." if data[:public_phone_number].nil?
+    data[:public_phone_number].insert(3, '-').insert(7, '-')
+  end
+
+  def or_format_address(data)
+    data[:location_1][:human_address].delete('{}\"').split(', ').map { |address| address.split(': ')[1] }.join(" ")
   end
 end
