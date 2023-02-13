36ffc333b9e7471ea10907d304a324656a2a0c6b
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-13 15:59:07 -0700

    Test: add testing for facility_factory class and begin creating method for create_facilities_oregon

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,3 +1,11 @@
 class FacilityFactory
 
+  def create_facilities_oregon(facility_database)
+
+    vehicle_instances = []
+    vehicle_database.each do |vehicle|
+      vehicle_instances.push(Vehicle.new({vin: vehicle[:vin_1_10], year: vehicle[:model_year], make: vehicle[:make], model:  vehicle[:model], engine: :ev}))
+    end
+    vehicle_instances
+  end
 end
