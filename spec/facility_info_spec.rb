869f8d6fb217c511e9faaab8c01f28c837f6e9ea
Katie Lonsdale <katie952@gmail.com>
2023-02-13 15:58:12 -0700

    test: add daily hours test

--- spec/facility_info_spec.rb
+++ spec/facility_info_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe FacilityInfo do 
   before(:each) do
-    @dds = DmvDataService.new.or_dmv_office_locations
+    @dds = DmvDataService.new.ny_dmv_office_locations
     @facility_info = FacilityInfo.new(@dds)
   end
 
@@ -13,6 +13,13 @@ RSpec.describe FacilityInfo do
 
     it 'has a source' do
       expect(@facility_info.source).to eq(@dds)
+    end
+  end
+
+  describe '#daily_hours' do
+    it 'lists hours facility is open' do
+      expect(@facility_info.daily_hours).to be_an_instance_of Hash
+      expect(@facility_info.daily_hours.keys).to eq(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'])
     end
   end
 
