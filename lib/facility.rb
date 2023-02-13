0189c52ac19cc4f8b76b8d7f7fde2988cf2e7ff9
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-13 01:43:29 -0500

    feat: add written/road test administration + license renewal to facilities

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,63 @@
+require 'date'
+
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services,
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(facility)
     @name = facility[:name]
     @address = facility[:address]
     @phone = facility[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    if @services.include?('Vehicle Registration')
+      if vehicle.antique?
+        @collected_fees += 25
+        vehicle.plate_type = :antique
+      elsif vehicle.electric_vehicle?
+        @collected_fees += 200
+        vehicle.plate_type = :ev
+      else
+        @collected_fees += 100
+        vehicle.plate_type = :regular
+      end
+    else
+      return nil
+    end
+    vehicle.registration_date = Date.today
+    @registered_vehicles << vehicle
+  end
+
+  def administer_written_test(registrant)
+    if @services.include?('Written Test') && registrant.age >= 16 && registrant.permit
+      registrant.license_data[:written] = true
+    end
+    registrant.license_data[:written]
+  end
+
+  def administer_road_test(registrant)
+    if @services.include?('Road Test') && registrant.license_data[:written]
+      registrant.license_data[:license] = true
+    end
+    registrant.license_data[:license]
+  end
+
+  def renew_drivers_license(registrant)
+    if @services.include?('Renew License') && registrant.license_data[:written] && registrant.license_data[:license]
+      registrant.license_data[:renewed] = true
+    end
+    registrant.license_data[:renewed]
   end
 end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,8 +5,9 @@ class Vehicle
               :year,
               :make,
               :model,
-              :engine,
-              :registration_date
+              :engine
+  attr_accessor :plate_type,
+                :registration_date
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -14,6 +15,7 @@ class Vehicle
     @make = vehicle_details[:make]
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
+    @plate_type = nil
     @registration_date = nil
   end
 
