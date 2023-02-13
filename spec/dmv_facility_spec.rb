7e2870b1f75c7876101b21bd07c4e16dff6529eb
margdaly <margaret.daly@gmail.com>
2023-02-12 21:21:40 -0700

    test: Add create ny_facility objects tests

--- spec/dmv_facility_spec.rb
+++ spec/dmv_facility_spec.rb
@@ -10,14 +10,31 @@ RSpec.describe DmvFacility do
   end
 
   describe '#create dmv facility' do
-    it 'can creat dmv facility objects' do
+    it 'can create or dmv facility objects' do
       dmv_facility = DmvFacility.new
       or_facilities = DmvDataService.new.or_dmv_office_locations
 
       expect(or_facilities).to be_an_instance_of(Array)
       expect(or_facilities.size).to eq(59)
       expect(or_facilities.first).to be_an_instance_of(Hash)
-      expect(dmv_facility.create_facilities(or_facilities)).to be_an_instance_of(Array)
+      expect(dmv_facility.create_or_facilities(or_facilities)).to be_an_instance_of(Array)
+      expect(dmv_facility.locations.size).to eq(59)
+      expect(dmv_facility.locations.first).to be_an_instance_of(Facility)
+      expect(dmv_facility.locations.last).to be_an_instance_of(Facility)
+    end
+
+    it 'can create ny dmv facility objects' do
+      dmv_facility = DmvFacility.new
+      ny_facilities = DmvDataService.new.ny_dmv_office_locations
+
+      expect(dmv_facility.create_ny_facilities(ny_facilities)).to be_an_instance_of(Array)
+      expect(dmv_facility.locations.size).to eq(169)
+      expect(dmv_facility.locations.first.address).to be_an_instance_of(String)
+      expect(dmv_facility.locations.first.address.include?("NY")).to eq(true)
+    end
+
+    xit 'can create mo dmv facility objects' do
+      missouri_facilities = DmvDataService.new.mo_dmv_office_locations
     end
   end
 end
