0740ab38968cc2b0ebc30070b4b44bc50354cbac
margdaly <margaret.daly@gmail.com>
2023-02-12 21:20:49 -0700

    feat: Add create_ny_facilities method for dmv_facility class

--- lib/dmv_facility.rb
+++ lib/dmv_facility.rb
@@ -5,14 +5,23 @@ class DmvFacility
     @locations = []
   end
 
-  def create_facilities(office_locations)
+  def create_or_facilities(office_locations)
     office_locations.map do |location|
-      # chuck_norris = Hash[:punch, 99, :kick, 98, :stops_bullets_with_hands, true]
-      # new_location = Hash[:name, location[:title], :address, location[:location_1][:human_address], :phone, location[:phone_number]]
       new_location = {
         name: location[:title],
         address: location[:location_1][:human_address],
-        phone: location[:phone_number],
+        phone: location[:phone_number]
+      }
+      @locations << Facility.new(new_location)
+    end
+  end
+
+  def create_ny_facilities(office_locations)
+    office_locations.map do |location|
+      new_location = {
+        name: location[:office_name],
+        address: location.fetch_values(:street_address_line_1, :city, :state, :zip_code).join(' '),
+        phone: location[:public_phone_number]
       }
       @locations << Facility.new(new_location)
     end
