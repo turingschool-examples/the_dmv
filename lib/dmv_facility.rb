fc2ef19d2282f6f2bdecb645e58bf563214e6546
margdaly <margaret.daly@gmail.com>
2023-02-13 16:36:39 -0700

    feat: Add create_facilities method and create ny, mo, and or location arrays

--- lib/dmv_facility.rb
+++ lib/dmv_facility.rb
@@ -1,8 +1,26 @@
 class DmvFacility
-  attr_reader :locations
+  attr_reader :locations,
+              :ny_locations,
+              :mo_locations,
+              :or_locations
 
   def initialize
     @locations = []
+    @ny_locations = []
+    @mo_locations = []
+    @or_locations = []
+  end
+
+  def create_facilities(all_office_locations)
+    office_locations.map do |location| 
+      if location.has_key?(:state) && location[:state] == "NY"
+        create_ny_facilities(office_locations)
+      elsif location.has_key?(:state) && location[:state] == "MO"
+        create_mo_facilities(office_locations)
+      else
+        create_or_facilities(office_locations)
+      end
+    end
   end
 
   def create_or_facilities(office_locations)
@@ -12,7 +30,10 @@ class DmvFacility
         address: location[:location_1][:human_address],
         phone: location[:phone_number]
       }
-      @locations << Facility.new(new_location)
+        @or_locations << Facility.new(new_location)
+      end
+    @or_locations.map do |location|
+      @locations << location 
     end
   end
 
@@ -23,14 +44,20 @@ class DmvFacility
         address: location.fetch_values(:street_address_line_1, :city, :state, :zip_code).join(' '),
         phone: location[:public_phone_number]
       }
-      @locations << Facility.new(new_location)
+      @ny_locations << Facility.new(new_location)
+      end
+    @ny_locations.map do |location|
+      @locations << location 
     end
   end
 
   def create_mo_facilities(office_locations)
     office_locations.map do |location|
       new_location = {address: location.fetch_values(:address1, :city, :state, :zipcode).join(' ')}
-      @locations << Facility.new(new_location)
+      @mo_locations << Facility.new(new_location)
+    end
+    @mo_locations.map do |location|
+      @locations << location 
     end
   end
 end
