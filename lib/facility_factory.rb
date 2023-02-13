1d48117debdd0d83c72d025215e13f81fdd5ae92
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-13 04:09:19 -0600

    Refactor: move .upcase method call in #create_oregon_facilities for consistency/readability

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -13,11 +13,11 @@ class FacilityFactory
   def create_oregon_facilities(facilities_data)
     facilities_data.map do |facility|
       address = JSON.parse(facility[:location_1][:human_address], symbolize_names: true)
-      formatted_address = "#{address[:address]} #{address[:city]} #{address[:state]} #{address[:zip]}".upcase
+      formatted_address = "#{address[:address]} #{address[:city]} #{address[:state]} #{address[:zip]}"
 
       Facility.new({
         name: facility[:title].upcase,
-        address: formatted_address,
+        address: formatted_address.upcase,
         phone: facility[:phone_number].delete('-')
       })
     end
