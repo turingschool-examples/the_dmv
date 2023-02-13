6cb9e30be3ebdffc875c836657a384e6b7c847de
Reid Miller <reid.s.miller@gmail.com>
2023-02-13 14:52:39 -0700

    fix: Remove duplicate entries in create_vehicles

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -5,17 +5,18 @@ class VehicleFactory
   end
 
   def create_vehicles(data)
+    vehicles_with_dup = []
     data.each do |car|
       vehicle_details = {
         make: car[:make],
         model: car[:model],
-        vin: car[:vin_1_10],
+        vin: car[:vin_1_10] += car[:dol_vehicle_id],
         year: car[:model_year].to_i,
         engine: :ev
       }
-      vehicle_sorted = Vehicle.new(vehicle_details)
-      @vehicles << vehicle_sorted
+      vehicles_with_dup << Vehicle.new(vehicle_details)
     end
+    @vehicles = vehicles_with_dup.uniq {|vehicle| vehicle.vin}
     @vehicles
   end
 end
