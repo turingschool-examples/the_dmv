f8cc23938611ab35368bf1d2b7f6c370d157cf28
Nick Sacco <nicksacco10@gmail.com>
2023-02-12 18:57:28 -0800

    Test: Initialize test

--- spec/dmv_locations_spec.rb
+++ spec/dmv_locations_spec.rb
@@ -1,1 +1,13 @@
 require 'spec_helper'
+
+RSpec.describe DMVLocations do
+  before(:each) do
+    @dmv = DMVLocation.new
+  end
+
+  describe 'Initialize' do
+    it 'exists' do
+      expect(@dmv).to be_a(DMVLocation)
+    end
+  end
+end
