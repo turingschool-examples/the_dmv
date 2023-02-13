034ad9bfd224d8a69371883fe7275fe748a092bc
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-13 16:23:06 -0500

    feat: facility_generator generates facilities from Oregon dataset

--- lib/facility_generator.rb
+++ lib/facility_generator.rb
@@ -1,1 +1,16 @@
+require './lib/facility'
 
+class FacilityGenerator
+  def create_facility(facility_info)
+    facility_info.map do |info|
+      Facility.new(
+        address: info[:location_1],
+        collected_fees: info[:collected_fees],
+        name: info[:title],
+        phone: info[:phone_number],
+        register_vehicles: info[:register_vehicles],
+        services: info[:services]
+      ) 
+    end
+  end
+end
--- spec/facility_generator_spec.rb
+++ spec/facility_generator_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe FacilityGenerator do
   
   let(:factory) {FacilityGenerator.new}
   let(:or_dmv_office_locations) { DmvDataService.new.or_dmv_office_locations }
+  let(:ny_dmv_office_locations) { DmvDataService.new.ny_dmv_office_locations }
   
   describe '#create_facility' do
     it 'returns an array of facilities' do
