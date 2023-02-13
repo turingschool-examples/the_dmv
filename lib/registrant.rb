2a5d27a92c8dd9573918a3211ab7c9b617ea90e0
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-12 17:23:03 -0700

    fix tests

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -6,10 +6,22 @@ attr_reader :name, :age, :license_data
     @permit = permit
     @license_data = {:written=>false, :license=>false, :renewed=>false}
   end
+
   def permit?
     @permit
   end
+
   def earn_permit
     @permit = true
   end
+
+  # def administer_written_test(registrant)
+  #   return false unless @services.include?("Written Test")
+      
+  #     if registrant.age >= 16 && registrant.permit? == true
+  #       @license_data = {:written=>true, :license=>false, :renewed=>false}
+  #     end
+    
+  # end
+
 end
