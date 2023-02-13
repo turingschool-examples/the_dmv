e5698676b638f5fccabe06187125958fb10b8279
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-13 04:54:44 -0600

    Refactor: tests for #create_wa_ev_vehicles extracted out

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 RSpec.describe VehicleFactory do
   wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+  ny_registrations = DmvDataService.new.ny_registrations
   factory = VehicleFactory.new
   wa_ev_vehicles = factory.create_vehicles(wa_ev_registrations)
+  ny_vehicles = factory.create_vehicles(ny_registrations)
 
   describe '#initialize' do
     it 'can initialize' do
@@ -14,32 +16,55 @@ RSpec.describe VehicleFactory do
   describe '#create_vehicles' do
     it 'returns an array' do
       expect(wa_ev_vehicles).to be_a(Array)
+      expect(ny_vehicles).to be_a(Array)
     end
 
     it 'returns an array of Vehicle objects' do
-      expect(wa_ev_vehicles.all? { |vehicle| vehicle.is_a?(Vehicle) }).to be(true)
+      expect(wa_ev_vehicles.all? { |vehicle| vehicle.is_a?(Vehicle) })
+        .to be(true)
+      expect(ny_vehicles.all? { |vehicle| vehicle.is_a?(Vehicle) })
+        .to be(true)
     end
 
     it 'assigns the @vin attribute on every vehicle to a string value' do
-      expect(wa_ev_vehicles.all? { |vehicle| vehicle.vin.is_a?(String) }).to be(true)
+      expect(wa_ev_vehicles.all? { |vehicle| vehicle.vin.is_a?(String) })
+        .to be(true)
+      expect(ny_vehicles.all? { |vehicle| vehicle.vin.is_a?(String) })
+        .to be(true)
     end
 
     it 'assigns the @year attribute on every vehicle to a string value' do
-      expect(wa_ev_vehicles.all? { |vehicle| vehicle.year.is_a?(String) }).to be(true)
+      expect(wa_ev_vehicles.all? { |vehicle| vehicle.year.is_a?(String) })
+        .to be(true)
+      expect(ny_vehicles.all? { |vehicle| vehicle.year.is_a?(String) })
+        .to be(true)
     end
 
     it 'assigns the @make attribute on every vehicle to a string value' do
-      expect(wa_ev_vehicles.all? { |vehicle| vehicle.make.is_a?(String) }).to be(true)
+      expect(wa_ev_vehicles.all? { |vehicle| vehicle.make.is_a?(String) })
+        .to be(true)
+      expect(ny_vehicles.all? { |vehicle| vehicle.make.is_a?(String) })
+        .to be(true)
     end
 
-    it 'assigns the @model attribute on every vehicle to a string value' do
-      expect(wa_ev_vehicles.all? { |vehicle| vehicle.model.is_a?(String) }).to be(true)
+    it 'assigns the @model attribute on every vehicle to a string value or nil' do
+      expect(wa_ev_vehicles.all? do |vehicle|
+        vehicle.model.is_a?(String) || vehicle.model == nil
+      end).to be(true)
+      expect(ny_vehicles.all? do |vehicle|
+        vehicle.model.is_a?(String) || vehicle.model == nil
+      end).to be(true)
     end
 
     it 'assigns the @engine attribute on every vehicle to a symbol value' do
-      expect(wa_ev_vehicles.all? { |vehicle| vehicle.engine.is_a?(Symbol) }).to be(true)
+      expect(wa_ev_vehicles.all? { |vehicle| vehicle.engine.is_a?(Symbol) })
+        .to be(true)
+      expect(ny_vehicles.all? { |vehicle| vehicle.engine.is_a?(Symbol) })
+        .to be(true)
     end
+  end
 
+  describe '#create_wa_ev_vehicles' do
     it 'assigns the correct string value to the @vin attribute on every WA state electric vehicle' do
       expect(wa_ev_vehicles.first.vin).to eq(WA_EV_VEHICLE[:vin])
     end
@@ -58,6 +83,10 @@ RSpec.describe VehicleFactory do
 
     it 'assigns the correct symbol value to the @engine attribute on every WA state electric vehicle' do
       expect(wa_ev_vehicles.first.engine).to eq(WA_EV_VEHICLE[:engine])
+    end
+
+    describe '#create_ny_vehicles' do
+
     end
   end
 end
