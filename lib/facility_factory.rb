a8d9ffc239dc31c6b6e9476af6224eb070e39f8a
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-12 23:35:34 -0500

    refactor: add JSON parse to format helper method

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,6 +1,8 @@
+require 'json'
+
 class FacilityFactory
 
-  @@valid_states = [:MO, :NY, :OR]
+  @@valid_states = %i[MO NY OR]
 
   def initialize
   end
@@ -8,16 +10,17 @@ class FacilityFactory
   def create_facilities(facility_data, state)
     return "Sorry, this state is not yet supported." if !@@valid_states.include?(state)
 
-    case state
-    when :MO
-      facility_details_array = mo_standardize_data(facility_data)
-    when :NY
-      facility_details_array = ny_standardize_data(facility_data)
-    when :OR
-      facility_details_array = or_standardize_data(facility_data)
-    end
+    details_array =
+      case state
+      when :MO
+        mo_standardize_data(facility_data)
+      when :NY
+        ny_standardize_data(facility_data)
+      when :OR
+        or_standardize_data(facility_data)
+      end
 
-    facility_details_array.map { |facility_details| Facility.new(facility_details) }
+    details_array.map { |facility| Facility.new(facility) }
   end
 
   def mo_standardize_data(data)
@@ -71,6 +74,6 @@ class FacilityFactory
   end
 
   def or_format_address(data)
-    data[:location_1][:human_address].delete('{}\"').split(', ').map { |address| address.split(': ')[1] }.join(" ")
+    JSON.parse(data[:location_1][:human_address]).values.join(" ")
   end
 end
