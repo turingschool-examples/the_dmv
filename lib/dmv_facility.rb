f4a5207a3cc132e64d40564f21e31e62301e0e05
margdaly <margaret.daly@gmail.com>
2023-02-12 17:12:10 -0700

    feat: Add create facilities method for DmvFacility class

--- lib/dmv_facility.rb
+++ lib/dmv_facility.rb
@@ -1,7 +1,20 @@
 class DmvFacility
   attr_reader :locations
-  
+
   def initialize
     @locations = []
+  end
+
+  def create_facilities(office_locations)
+    office_locations.map do |location|
+      # chuck_norris = Hash[:punch, 99, :kick, 98, :stops_bullets_with_hands, true]
+      # new_location = Hash[:name, location[:title], :address, location[:location_1][:human_address], :phone, location[:phone_number]]
+      new_location = {
+        name: location[:title],
+        address: location[:location_1][:human_address],
+        phone: location[:phone_number],
+      }
+      @locations << Facility.new(new_location)
+    end
   end
 end
