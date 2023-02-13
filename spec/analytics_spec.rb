91f85d981377bbfad1bf3b239b35b3ed309566b0
Katie Lonsdale <katie952@gmail.com>
2023-02-13 08:25:58 -0700

    test: add county with most vehicles test

--- spec/analytics_spec.rb
+++ spec/analytics_spec.rb
@@ -27,21 +27,32 @@ RSpec.describe do
   describe '#most_popular_make' do
     it 'returns most popular make' do
       expect(@analytics.most_popular_make).to be_an_instance_of(String)
-      expect(@analytics.source.map{|car| car[:make]}).to include(@analytics.most_popular_make)
+      expect(@analytics.source.map{|car| car[:make]}).
+      to include(@analytics.most_popular_make)
     end
   end
 
   describe '#most_popular_model' do
     it 'returns most popular model' do
       expect(@analytics.most_popular_model).to be_an_instance_of(String)
-      expect(@analytics.source.map{|car| car[:model]}).to include(@analytics.most_popular_model)
+      expect(@analytics.source.map{|car| car[:model]}).
+      to include(@analytics.most_popular_model)
     end
   end
 
   describe '#vehicles_per_model_year' do
     it 'returns number of vehicles with given model year' do
-      expect(@analytics.vehicles_per_model_year('2019')).to be_an_instance_of(Integer)
+      expect(@analytics.vehicles_per_model_year('2019')).
+      to be_an_instance_of(Integer)
       expect(@analytics.vehicles_per_model_year('1600')).to be 0 
+    end
+  end
+
+  describe '#county_with_most_vehicles' do
+    it 'returns county with most registered vehicles' do
+      expect(@analytics.county_with_most_vehicles).to be_an_instance_of(String)
+      expect(@analytics.source.map{|car| car[:county]}).
+      to include(@analytics.county_with_most_vehicles)
     end
   end
 end
