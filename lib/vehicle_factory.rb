79ef7caf539492f25eb547aafe88cff93166226a
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-13 09:06:19 -0700

    Feat: implement vehicle_factory testing and completed method

---
+++ lib/vehicle_factory.rb
@@ -0,0 +1,19 @@
+require 'date'
+
+class VehicleFactory
+
+  # attr_reader :vehicle_data
+
+  # def initialize
+  # end
+
+  def create_vehicles(vehicle_database)
+    # if vehicle_database == []
+    #   return nil
+    vehicle_instances = []
+    vehicle_database.each do |vehicle|
+      vehicle_instances.push(Vehicle.new({vin: vehicle[:vin_1_10], year: vehicle[:model_year], make: vehicle[:make], model:  vehicle[:model], engine: :ev}))
+    end
+    vehicle_instances
+  end
+end
