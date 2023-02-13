49b5b2397e5d383aa4d4e9052e722b140e378708
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-13 14:50:18 -0700

    refactor register_vehicle method

--- lib/facility.rb
+++ lib/facility.rb
@@ -18,23 +18,27 @@ class Facility
   end
 
   def register_vehicle(vehicle)
-    vehicle.registration_date = Date.today
-    if vehicle.antique? == true
-      @collected_fees += 25
-    elsif vehicle.electric_vehicle? == true
-       @collected_fees += 200
-    else @collected_fees += 100
+    if vehicle.registration_date != nil
+      nil
+    else 
+      vehicle.registration_date = Date.today
+      if vehicle.antique? == true
+        @collected_fees += 25
+      elsif vehicle.electric_vehicle? == true
+        @collected_fees += 200
+      else @collected_fees += 100
+      end
+      @registered_vehicles << vehicle
     end
-    @registered_vehicles << vehicle
   end
 
-  # def plate_type(vehicle)
-  #   if vehicle.antique? == true 
-  #     @plate_type = :antique
-  #   elsif vehicle.electric_vehicle == true
-  #     @plate_type = :ev
-  #   else  
-  #     @plate_type = :regular
-  #   end
-  # end
+  def plate_type(vehicle)
+    if vehicle.antique? == true 
+      vehicle.plate_type = :antique
+    elsif vehicle.electric_vehicle? == true
+      vehicle.plate_type = :ev
+    else  
+      vehicle.plate_type = :regular
+    end
+  end
 end
