8d622ea1fc5fa5b7117d6cb19146ca5fce4a0512
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-13 13:23:59 -0500

    feat: vehicle_factory can #create_vehicles and give assign attributes

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
-Rspec.describe VehicleFactory do
+RSpec.describe VehicleFactory do
   
-  let(:factory) {VehicleFactory.new}}
+  let(:factory) {VehicleFactory.new}
   let(:wa_ev_registrations) { DmvDataService.new.wa_ev_registrations }
   
   describe '#create_vehicles' do
     it 'returns an array of vehicles' do
       vehicles = factory.create_vehicles(wa_ev_registrations)
+      require 'pry'; binding.pry
       expect(vehicles).to all(be_a(Vehicle))
     end
   end
