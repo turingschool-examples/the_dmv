fa34ab65552429dde7733a144f114707edd43b01
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-12 17:31:44 -0700

    refactor @registered_vehicles test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -26,7 +26,8 @@ RSpec.describe Facility do
 
   describe '#registered_vehicles' do
     it 'can register vehicles' do 
-      expect(@facility.registered_vehicles).to eq([])
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      expect(facility_1.registered_vehicles).to eq([])
     end
   end
 end
