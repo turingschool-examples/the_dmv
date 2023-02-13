5a7cae02c698068bdaa3cabd80758151660e4990
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-13 15:58:28 -0500

    add create facility functionality

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,4 +1,11 @@
 class FacilityFactory
+  attr_reader :new_facilities
   def initialize
+    @new_facilities = []
+  end
+
+  def create_facility(hash)
+    facility = Facility.new(hash)
+    @new_facilities << facility
   end
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -4,17 +4,22 @@ require 'spec_helper'
 RSpec.describe FacilityFactory do
   describe '#initialize' do
     it 'exists' do
-      facility_factory_1 = FacilityFactory.new
+      facility_factory = FacilityFactory.new
 
-      expect(facility_factory_1).to be_a(FacilityFactory)
+      expect(facility_factory).to be_a(FacilityFactory)
+      expect(facility_factory.new_facilities).to eq([])
     end
   end
 
-  describe '#create_facilities'
-    it 'can create a facility'
-      facility_factory_1 = FacilityFactory.new
+  describe '#create_facility' do
+    it 'can create a facility' do
+      facility_factory = FacilityFactory.new
       facility1 = {:name=>'Test', :address=>'123 Spring St', :phone=>'(555)123-0987'}
-      facility_factory.create_facilities(facility1)
-
-      expect(dmv.facilities.size).to eq(1)
+      facility2 = {:name=>'Test2', :address=>'456 Spruce Ave', :phone=>'(555-465-2049)'}
+      expect(facility_factory.create_facility(facility1)[0]).to be_a(Facility)
+      expect(facility_factory.new_facilities.size).to eq(1)
+      expect(facility_factory.create_facility(facility2)[0]).to be_a(Facility)
+      expect(facility_factory.new_facilities.size).to eq(2)
+    end
+  end
 end
