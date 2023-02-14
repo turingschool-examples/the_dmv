6708afb310f0b14cd41515ab3ea17c896682d650
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-14 08:48:58 -0700

    Modified test organization.

--- spec/external_data_sets_spec.rb
+++ spec/external_data_sets_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 RSpec.describe 'Vehicle Factory' do
   describe 'exists' do
-    it '#is an instance of' do
+    it 'is an instance of' do
       factory = VehicleFactory.new
       expect(factory).to be_an_instance_of(VehicleFactory)
     end
   end
 
-  describe 'accesses the API' do
+  describe '#create_vehicles' do
     it 'can create vehicles' do
       factory = VehicleFactory.new
       wa_ev_registrations = DmvDataService.new.wa_ev_registrations
@@ -17,7 +17,6 @@ RSpec.describe 'Vehicle Factory' do
       expect(created_vehicles).to be_a(Array)
       expect(created_vehicles.first).to be_a(Vehicle)
       expect(created_vehicles.length).to be(1000)
-
     end
   end
 end
