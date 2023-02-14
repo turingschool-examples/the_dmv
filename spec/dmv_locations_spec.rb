e4b15d554cbf069fc4e15b82f67d56801ea3f2ef
Nick Sacco <nicksacco10@gmail.com>
2023-02-14 13:14:44 -0800

    Test: Iteration 3 1st test

--- spec/dmv_locations_spec.rb
+++ spec/dmv_locations_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'json'
 
 RSpec.describe DMVLocation do
   before(:each) do
@@ -11,10 +12,15 @@ RSpec.describe DMVLocation do
     end
   end
 
-  describe 'List Locations' do
-    it 'can list DMV locations' do
-      or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
-      @dmv.dmv_locations.(or_dmv_office_locations)
+  describe 'Create new Locations' do
+    it 'can create DMV locations' do
+      
+      or_dmv = DmvDataService.new.or_dmv_office_locations
+      @dmv.dmv_locations(or_dmv)
+     
+      expect(@dmv.locations[1].name).to eq('Ashland DMV Office')
+      expect(@dmv.locations[1].address).to eq("600 Tolman Creek Rd")
+      expect(@dmv.locations[1].phone).to eq("541-776-6092")
     end
   end
 end
