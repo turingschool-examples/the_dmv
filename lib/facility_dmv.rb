564029218b9d6b3304b0bb515fd00d6a167f779b
Amy Spears <amspears007@gmail.com>
2023-02-13 17:59:37 -0700

    Add address converter method to parse address from hash in FaciltyDmv class

--- lib/facility_dmv.rb
+++ lib/facility_dmv.rb
@@ -14,11 +14,20 @@ class FacilityDmv
         type: facility[:type],
         phone: facility[:phone_number],
         agency: facility[:agency],
-        address: facility[:location_1][:human_address]
-        }
-# require 'pry'; binding.pry
+        address: address_converter(facility[:location_1][:human_address])
+        
+        # add_hash = facility[:location_1][:human_address]
+        # string_hash = JSON.parse(add_hash)
+        # string_hash.values.join(' ')
+      }
+       
         @facilities << Facility.new(facility_hash)
-        require 'pry'; binding.pry
+        # require 'pry'; binding.pry
     end
+  end
+  def address_converter(api_info)
+        add_hash = (api_info)
+        string_hash = JSON.parse(add_hash)
+        string_hash.values.join(' ')
   end
 end
