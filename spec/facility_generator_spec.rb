8fdf87a1a6c88c505fbc1b1cfb094559f060bced
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-14 13:36:08 -0500

    add test: for #create_facility_mo

--- spec/facility_generator_spec.rb
+++ spec/facility_generator_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe FacilityGenerator do
   let(:factory) {FacilityGenerator.new}
   let(:or_dmv_office_locations) { DmvDataService.new.or_dmv_office_locations }
   let(:ny_dmv_office_locations) { DmvDataService.new.ny_dmv_office_locations }
-  
+  let(:missouri_facilities) { DmvDataService.new.mo_dmv_office_locations }
   describe '#create_facility' do
     it 'returns an array of facilities' do
       or_facilities = factory.create_facility_or(or_dmv_office_locations)
@@ -13,10 +13,15 @@ RSpec.describe FacilityGenerator do
       expect(or_facilities).to all(be_a(Facility))
     end
     
-    it 'returns an array of facilities but with a different dataset' do
+    it 'returns an array of facilities but with a NY dataset' do
       ny_facilities = factory.create_facility_ny(ny_dmv_office_locations)
 
       expect(ny_facilities).to all(be_a(Facility))
+    end
+
+    it 'returns an array of facilities but with a  dataset' do
+      mo_facilities = factory.create_facility_mo(mo_dmv_office_locations)
+      expect(mo_facilities).to all(be_a(Facility))
     end
   end
 end
