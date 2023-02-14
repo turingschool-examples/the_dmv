aefd84fd2784b1b691fd73c49bd97ffe6634ffb1
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-02-14 13:03:02 -0700

    complete most_common_vehicle method in analysis_bot class

--- lib/analysis_bot.rb
+++ lib/analysis_bot.rb
@@ -1,14 +1,15 @@
 class AnalysisBot
-
-  
-  
   def most_common_vehicle(data_set)
     makes_array = data_set.map{|hash| hash[:make]}
     makes_number_hash = Hash.new(0)
     makes_array.each {|make| makes_number_hash[make.to_sym] += 1}
-    most_pop_make = makes_number_hash.sort_by{|k_v_pair| k_v_pair[-1]}.last[0].to_s
+    most_pop_make = makes_number_hash.sort_by{|k_v_pair| k_v_pair.last}.last[0].to_s
     
-    models_array = data_set.select{|hash| hash[:make] == most_pop_make}
-    require 'pry'; binding.pry
+    most_pop_make_array = data_set.select{|hash| hash[:make] == most_pop_make}
+    models_array = most_pop_make_array.map{|car_hash| car_hash[:model]}
+    models_number_hash = Hash.new(0)
+    models_array.each {|car| models_number_hash[car.to_sym] += 1}
+    most_pop_model = models_number_hash.sort_by{|k_v_pair| k_v_pair.last}.last[0]
+    return "#{most_pop_make} #{most_pop_model}"
   end
 end
--- spec/analysis_bot_spec.rb
+++ spec/analysis_bot_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe AnalysisBot do
   jeremy = AnalysisBot.new
 
   it "can produce the most common make and model" do
-    
+    # I tested this one almost entirely with pry. I couldn't come
+    # up with a way to test each step of the process without just
+    # making every single variable public, which just didn't seem
+    # like the best way to go. So I had a pry line at the end of the
+    # method which allowed me to test every new line.
+
+    expect(jeremy.most_common_vehicle(washington_vehicles)).to eq("TESLA Model 3")
   end
 end
