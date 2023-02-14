17f095fbd3d98743dd3a4b2a51812616c968d431
Katie Lonsdale <katie952@gmail.com>
2023-02-13 17:43:07 -0700

    feat: add holidays closed method

--- lib/facility_info.rb
+++ lib/facility_info.rb
@@ -15,4 +15,12 @@ class FacilityInfo
       @source.map {|facility| facility[:daysopen]}
     end
   end
+
+  def holidays_closed
+    if @source.size == 178
+      @source.map {|facility| facility[:holidaysclosed]}
+    else
+      'Information not available.'
+    end
+  end
 end
