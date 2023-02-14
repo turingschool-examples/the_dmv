5987fb5163ee5109434ef225498419234992cbd3
Nick Sacco <nicksacco10@gmail.com>
2023-02-14 14:14:11 -0800

    Added ny tests and method

--- lib/dmv_locations.rb
+++ lib/dmv_locations.rb
@@ -2,13 +2,12 @@ require 'json'
 
 class DMVLocation
   attr_reader :locations
-  
+
   def initialize
     @locations = []
   end
 
-
-  def dmv_locations(office_locations)
+  def or_locations(office_locations)
     office_locations.each do |location|
       location_info = location[:location_1][:human_address]
       address = JSON.parse(location_info, symbolize_names: true)
@@ -18,5 +17,16 @@ class DMVLocation
       facility.phone = location[:phone_number]
       @locations << facility
     end
+  end
+
+  def ny_locations(office_locations)
+    office_locations.each do |location|
+      location[:name] = location[:office_name]
+      location[:phone] = location[:public_phone_number]
+      location[:address] = location[:street_address_line_1]
+      facility = Facility.new(location)
+      @locations << facility
+    end
+    
   end
 end
--- spec/dmv_locations_spec.rb
+++ spec/dmv_locations_spec.rb
@@ -12,15 +12,27 @@ RSpec.describe DMVLocation do
     end
   end
 
-  describe 'Create new Locations' do
-    it 'can create DMV locations' do
+  describe 'Oregon Locations' do
+    it 'can create Oregon DMV locations' do
       
       or_dmv = DmvDataService.new.or_dmv_office_locations
-      @dmv.dmv_locations(or_dmv)
+      @dmv.or_locations(or_dmv)
      
       expect(@dmv.locations[1].name).to eq('Ashland DMV Office')
       expect(@dmv.locations[1].address).to eq("600 Tolman Creek Rd")
       expect(@dmv.locations[1].phone).to eq("541-776-6092")
+    end
+  end
+
+  describe 'New York Locations' do
+    it 'can create NY DMV locations' do
+
+      ny_dmv = DmvDataService.new.ny_dmv_office_locations
+      @dmv.ny_locations(ny_dmv)
+
+      expect(@dmv.locations[0].name).to eq("JAMESTOWN")
+      expect(@dmv.locations[0].phone).to eq("7166618220")
+      expect(@dmv.locations[0].address).to eq("512 WEST 3RD STREET")
     end
   end
 end
