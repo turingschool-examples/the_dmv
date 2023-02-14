d01ed0d581abbe92eb35eb2ae20999d54ec75f40
Nick Sacco <nicksacco10@gmail.com>
2023-02-14 13:18:39 -0800

    Feat: Section 1 Iteration 3 complete

--- lib/dmv_locations.rb
+++ lib/dmv_locations.rb
@@ -1,17 +1,22 @@
+require 'json'
+
 class DMVLocation
+  attr_reader :locations
+  
   def initialize
+    @locations = []
   end
 
-  def dmv_locations(office_locations)
-    locations = []
-    office_locations.each do |office|
-      office_locations_hash = Hash.new
-      office_locations_hash[:name] = office[:title]
-      office_locations_hash[:address] = office[:location_1]
-      office_locations_hash[:phone] = office[:phone_number]
 
-      locations << DMVLocation.new(office_locations_hash)
+  def dmv_locations(office_locations)
+    office_locations.each do |location|
+      location_info = location[:location_1][:human_address]
+      address = JSON.parse(location_info, symbolize_names: true)
+      facility = Facility.new(location)
+      facility.name = location[:title]
+      facility.address = address[:address]
+      facility.phone = location[:phone_number]
+      @locations << facility
     end
-    locations
   end
 end
