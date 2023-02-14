f8076b1e0e1a09ae46582624485fdc42e4710563
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-14 13:50:52 -0700

    test begins with an empty array

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -8,6 +8,20 @@ RSpec.describe VehicleFactory do
       factory = VehicleFactory.new
       expect(factory).to be_a(VehicleFactory)
     end
+
+    it 'begins with an empty array' do
+      factory = VehicleFactory.new
+      expect(factory.vehicle_registrations).to eq[]
+    end
+
+  end
     
+  describe '#factory is operative' do
+    xit 'creates vehicles' do
+      factory = VehicleFactory.new
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+      
+      factory.create_vehicles(wa_ev_registrations).to 
+    end
   end
 end
