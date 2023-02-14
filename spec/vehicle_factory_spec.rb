a303e52fa2d054db9068381de4b51340fd26c22d
Thomas Hawley <thawley22@turing.edu>
2023-02-13 21:15:12 -0700

    test: Add additional tests for robustness and for most popular make and model

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -15,4 +15,27 @@ RSpec.describe VehicleFactory do
     expect(factory.vehicle_storage[0]).to be_instance_of(Vehicle)
     expect(factory.vehicle_storage.length).to eq(1000)
   end
+
+  it 'has the correct attributes' do
+    factory = VehicleFactory.new
+    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+    factory.create_vehicles(wa_ev_registrations)
+
+    expect(factory.vehicle_storage[0].vin).to eq("JTDKN3DP8D")
+    expect(factory.vehicle_storage[0].year).to eq("2013")
+    expect(factory.vehicle_storage[0].make).to eq("TOYOTA")
+    expect(factory.vehicle_storage[0].model).to eq("Prius Plug-in")
+  end
+
+  it 'can find the most popular make of vehicle' do
+    factory = VehicleFactory.new
+    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+    factory.create_vehicles(wa_ev_registrations)
+
+    expect(factory.find_most_popular_make).to eq('TESLA')
+    expect(factory.find_most_popular_model).to eq('Model 3')
+    expect(factory.most_popular_make_model).to eq('TESLA, Model 3')
+  end
+
+  it 'can count the number of vehicles for a model year'
 end
