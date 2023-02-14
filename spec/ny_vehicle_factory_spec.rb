dacf9175e648afc3ed74f18dc4d5cf7f2a3c6d9a
Katie Lonsdale <katie952@gmail.com>
2023-02-14 15:45:54 -0700

    test: add test for source in initialize

--- spec/ny_vehicle_factory_spec.rb
+++ spec/ny_vehicle_factory_spec.rb
@@ -2,27 +2,30 @@ require 'spec_helper'
 
 RSpec.describe NyVehicleFactory do
   before(:each) do
-    @factory = NyVehicleFactory.new
-    @ny_registrations = DmvDataService.new.ny_registrations
+    @source = DmvDataService.new.ny_registrations
+    @factory = NyVehicleFactory.new(@source)
   end
   describe '#initialize' do
     it 'exists' do
       expect(@factory).to be_an_instance_of(NyVehicleFactory)
     end
+
+    it 'has a source' do
+      expect(@factory.source).to be_an_instance_of(Array)
+    end
   end
 
   describe '#create_vehicles' do
     it 'creates vehicles from given source' do
-      expect(@factory.create_vehicles(@ny_registrations)).to be_an_instance_of(Array)
-      expect(@factory.create_vehicles(@ny_registrations)[0]).to be_an_instance_of(Vehicle)
+      expect(@factory.create_vehicles(@source)).to be_an_instance_of(Array)
+      expect(@factory.create_vehicles(@source)[0]).to be_an_instance_of(Vehicle)
     end
   end
 
   describe '#filter_cars' do
-    it 'creates and array of only cars' do
-        expect(@factory.filter_cars(@ny_registrations)).to be_an_instance_of(Array)
-        expect(@factory.filter_cars(@ny_registrations)[0][:record_type]).to eq ('VEH')
+    it 'creates an array of only cars' do
+        expect(@factory.filter_cars(@source)).to be_an_instance_of(Array)
+        expect(@factory.filter_cars(@source)[0][:record_type]).to eq ('VEH')
     end
   end
-
 end
