9f49ec0e07221f849d1a53268274ed4c3eb35d53
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-14 16:45:41 -0700

    define create_vehicles method first go

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,3 +1,16 @@
 class VehicleFactory
-  
+  attr_accessor :vehicles
+
+  def initialize
+    @vehicles = []
+  end
+
+  def create_vehicles(registrations)
+    registrations.each do |registration|
+    vehicle = Vehicle.new(registration)
+      # require 'pry'; binding.pry
+      @vin = registration[:vin_1_10]
+    @vehicles << [vehicle]
+    end
+  end
 end
