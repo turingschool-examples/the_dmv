47681a8b53ec7f148be0da63cec6299a5207d0bf
Katie Lonsdale <katie952@gmail.com>
2023-02-14 14:35:31 -0700

    final edits

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -4,7 +4,8 @@ class VehicleFactory
 
   def create_vehicles(source)
     source.map do |car|
-      Vehicle.new({vin: car[:vin_1_10], year: car[:model_year], make: car[:make], model: car[:model], engine: :ev})
+      Vehicle.new({vin: car[:vin_1_10], year: car[:model_year], 
+      make: car[:make], model: car[:model], engine: :ev})
     end
   end
 end
