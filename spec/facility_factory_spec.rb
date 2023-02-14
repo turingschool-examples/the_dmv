06c2563d3df78fef208d1361c82f901bacce3173
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-14 16:10:07 -0700

    create tests for ny facilities

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe FacilityFactory do
   let(:facility) {FacilityFactory.new}
   let(:or_dmv_office_locations) {DmvDataService.new.or_dmv_office_locations}
+  # let(:ny_dmv_office_locations) {DmvDataService.new.ny_dmv_office_locations}
+  # require 'pry'; binding.pry
 
   it 'exists' do
     expect(facility).to be_a(FacilityFactory)
@@ -15,7 +17,21 @@ RSpec.describe FacilityFactory do
       end
     end
 
-    it 'matches the first facility in the data base array' do
+    it 'can create facilties from new york' do
+      new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+      facility.create_facilities_ny(new_york_facilities).each do |facility|
+        expect(facility).to be_a Facility
+      end
+    end
+
+    it 'can create facilities from missouri' do
+      missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+      facility.create_facilities_mo(missouri_facilities).each do |facility|
+        expect(facility).to be_a Facility
+      end
+    end
+
+    it 'matches the first facility in the OR data base array' do
       facility_1 = Facility.new({
         :name => "Albany DMV Office",
         :address => '2242 Santiam Hwy SE Albany OR 97321', 
@@ -33,12 +49,25 @@ RSpec.describe FacilityFactory do
       expect(facility.create_facilities_or(or_dmv_office_locations)[0].collected_fees).to eq(facility_1.collected_fees)
     end
 
+    it 'matches the first facility in the NY data base array' do
+      new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+      facility_2 = Facility.new({
+        :name => "JAMESTOWN",
+        :address => '512 WEST 3RD STREET', 
+        :phone => "7166618220",
+        :services => [], 
+        :registered_vehicles => [],
+        :collected_fees => 0
+      })
 
 
+      expect(facility.create_facilities_ny(new_york_facilities)[0].name).to eq(facility_2.name)
+      expect(facility.create_facilities_ny(new_york_facilities)[0].address).to eq(facility_2.address)
+      expect(facility.create_facilities_ny(new_york_facilities)[0].phone).to eq(facility_2.phone)
+      expect(facility.create_facilities_ny(new_york_facilities)[0].services).to eq(facility_2.services)
+      expect(facility.create_facilities_ny(new_york_facilities)[0].registered_vehicles).to eq(facility_2.registered_vehicles)
+      expect(facility.create_facilities_ny(new_york_facilities)[0].collected_fees).to eq(facility_2.collected_fees)
+    end
   end
-
-
-
-
-
 end
+
