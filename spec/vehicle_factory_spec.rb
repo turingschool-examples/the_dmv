30820e5659b892d3ce5cbb5557eb836f1e2406db
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-14 15:34:34 -0700

    test vehicles have vin numbers

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -9,19 +9,26 @@ RSpec.describe VehicleFactory do
       expect(factory).to be_a(VehicleFactory)
     end
 
-    it 'begins with an empty array' do
-      factory = VehicleFactory.new
-      expect(factory.vehicle_registrations).to eq([])
-    end
+    # it 'begins with an empty array' do
+    #   factory = VehicleFactory.new
+    #   expect(factory.vehicle_registrations).to eq([])
+    # end
 
   end
     
   describe '#factory is operative' do
-    xit 'creates vehicles' do
+    it 'creates vehicles' do
       factory = VehicleFactory.new
       wa_ev_registrations = DmvDataService.new.wa_ev_registrations
       
-      factory.create_vehicles(wa_ev_registrations).to 
+      expect(factory.create_vehicles(wa_ev_registrations)).to be_an_instance_of(Array)
+    end
+
+    it 'vehicles have vin numbers' do
+      factory = VehicleFactory.new
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+      vehicle = factory.create_vehicles(wa_ev_registrations)
+      expect(vehicle.vin).to eq([:vin_1_10])
     end
   end
 end
