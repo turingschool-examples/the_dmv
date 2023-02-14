51652066690d50720ba8bde295605775d159c9ff
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-14 14:05:31 -0700

    write tests for new york and missouri

--- spec/dmv_locations_spec.rb
+++ spec/dmv_locations_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe do
 
   it 'exists' do
     dmv_facility = DmvLocations.new
+
     expect(dmv_facility).to be_an_instance_of(DmvLocations)
     expect(dmv_facility.dmv_locations).to eq([])
   end
@@ -18,7 +19,6 @@ RSpec.describe do
     or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
     
     expect(dmv_facility.create_dmv_locations(or_dmv_office_locations)).to be_an_instance_of(Array)
-    
   end
   
   it 'creates locations from OR' do
@@ -31,6 +31,29 @@ RSpec.describe do
     expect(dmv_facility.dmv_locations[1]).to be_an_instance_of(Facility)
     expect(dmv_facility.dmv_locations[2]).to be_an_instance_of(Facility)
   end
+
+  it 'creates locations from NY' do
+    dmv_facility = DmvLocations.new
+    new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+    
+    expect(dmv_facility.create_dmv_locations_2(new_york_facilities)).to be_an_instance_of(Array)
+    expect(dmv_facility.dmv_locations.length).to be(169)
+    expect(dmv_facility.dmv_locations[0]).to be_an_instance_of(Facility)
+    expect(dmv_facility.dmv_locations[1]).to be_an_instance_of(Facility)
+    expect(dmv_facility.dmv_locations[2]).to be_an_instance_of(Facility)
+    expect(dmv_facility.dmv_locations[168]).to be_an_instance_of(Facility)
+  end
   
+  it 'creates locations from Missouri' do
+    dmv_facility = DmvLocations.new
+    missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+    # require 'pry'; binding.pry
+    expect(dmv_facility.create_dmv_locations_2(missouri_facilities)).to be_an_instance_of(Array)
+    expect(dmv_facility.dmv_locations.length).to be(178)
+    expect(dmv_facility.dmv_locations[0]).to be_an_instance_of(Facility)
+    expect(dmv_facility.dmv_locations[1]).to be_an_instance_of(Facility)
+    expect(dmv_facility.dmv_locations[2]).to be_an_instance_of(Facility)
+    expect(dmv_facility.dmv_locations[177]).to be_an_instance_of(Facility)
+  end
   
 end
