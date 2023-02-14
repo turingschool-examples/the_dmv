bb93f2692acf452c99e16dba8a5e7a83c1b81496
Logan Wilson <loganwilson777@gmail.com>
2023-02-13 17:31:46 -0700

    added attributes to vehicle class

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,7 +5,9 @@ class Vehicle
               :year,
               :make,
               :model,
-              :engine
+              :engine,
+              :plate_type,
+              :registration_date
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -13,6 +15,8 @@ class Vehicle
     @make = vehicle_details[:make]
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
+    @registration_date = nil
+    @plate_type = nil
   end
 
   def antique?
