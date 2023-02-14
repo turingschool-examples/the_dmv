592216885e4fda2c6f92a7a6d9acc1971fb007bf
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-13 21:29:00 -0700

    Refactor class

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,21 +1,19 @@
 class Registrant
   attr_reader :name, :age, :permit
+  attr_accessor :license_data
   def initialize(name, age, permit = false)
     @name = name 
     @age = age
     @permit = permit
+    @license_data = {written: false, license: false, renewed: false}
   end
 
   def permit?
     @permit
   end
 
-  def license_data
-    ld_hash = {written: false, license: false, renewed: false} 
-  end
-
   def earn_permit
-    return @permit = true if @age >= 15
+    @permit = true if @age >= 15
   end
 
 
