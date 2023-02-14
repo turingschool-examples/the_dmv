3ec5192816171f0a4977a7c5d545ad53f0184fe8
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-13 20:31:30 -0700

    change attributes, undo changes

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,31 +1,21 @@
 class Registrant
   attr_reader :name, :age
-  attr_accessor :license_data, :permit, :written_test, :road_test
-
+  attr_accessor :license_data, :permit
   
   def initialize(name, age, permit = false)
     @name = name
     @age = age
     @permit = permit
     @license_data = {:written => false, :license => false, :renewed => false}
-    @written_test = false
-    @road_test = false
   end
 
   def permit? 
     @permit
   end
 
-  # def license_data(written, license, renewed)
-  #   #This is somehow a hash? HOW.
-  # end
-
   def earn_permit
     if @permit == false
     @permit = true
     end
   end
-
-  # def license_data 
-  # end 
 end
