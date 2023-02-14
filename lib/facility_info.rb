c91c4a0602d81adf70a9ea168246724323423fee
Katie Lonsdale <katie952@gmail.com>
2023-02-13 17:30:27 -0700

    feat: refactor daily hours feature

--- lib/facility_info.rb
+++ lib/facility_info.rb
@@ -6,9 +6,13 @@ class FacilityInfo
   end
 
   def daily_hours
-    @source.map do |facility|
-      facility.select{|key, value| key.to_s.include?('day')}
+    if @source.size == 169
+      @source.map do |facility|
+        facility.select{|key, value| key.to_s.include?('day')}
+      end
+      # format days and times?
+    elsif @source.size == 178
+      @source.map {|facility| facility[:daysopen]}
     end
-    # format days and times?
   end
 end
