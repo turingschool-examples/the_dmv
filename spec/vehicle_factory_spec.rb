80892fc26b8345a2516d7dfb29408be7096dc156
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-13 22:01:00 -0700

    test: Add assertions for the facility objects from dmv data

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -17,44 +17,40 @@ RSpec.describe VehicleFactory do
     expect(cars[0].year).to eq("2013")
   end
 
-  xit 'access data from another dmv #oregon' do
+  it 'access data from another dmv #oregon' do
     factory = VehicleFactory.new
     or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
 
     expect(factory).to be_an_instance_of(VehicleFactory)
         
     locations = factory.create_facilities(or_dmv_office_locations)
-
-
-
-
     
-    # expect(factory.create_vehicles(or_dmv_office_locations)).to eq(or_dmv_office_locations)
-    # expect(or_dmv_office_locations[0][:zip_code]).to eq("97321")
+    expect(locations[0].address).to eq("Albany DMV Office")
+    expect(locations[0].phone).to eq("97321")
+    expect(locations[0].name).to eq("Transportation, Department of ")
   end
 
-  xit 'access data from another dmv #new york' do
+  it 'access data from another dmv #new york' do
     factory = VehicleFactory.new
     new_york_facilities = DmvDataService.new.ny_dmv_office_locations
 
     expect(factory).to be_an_instance_of(VehicleFactory)
     
     locations = factory.create_facilities(new_york_facilities)
-
-    require 'pry'; binding.pry
-
-    factory.create_facility (new_york_facilities)
-    expect(factory.create_vehicles(new_york_facilities)).to eq(new_york_facilities)
-    expect(new_york_facilities[0][:city]).to eq("JAMESTOWN")
+   
+    expect(locations[0].phone).to eq("14701")
   end
 
-  xit 'access data from another dmv #missouri' do
+  it 'access data from another dmv #missouri' do
     factory = VehicleFactory.new
     missouri_facilities = DmvDataService.new.mo_dmv_office_locations
 
     expect(factory).to be_an_instance_of(VehicleFactory)
-    factory.create_vehicles(missouri_facilities)
-    expect(factory.create_vehicles(missouri_facilities)).to eq(missouri_facilities)
-    expect(missouri_facilities[0][:state]).to eq("MO")
+    
+    locations = factory.create_vehicles(missouri_facilities)
+
+    expect(locations[0].phone).to eq("63670")
+    # expect(factory.create_vehicles(missouri_facilities)).to eq(missouri_facilities)
+    # expect(missouri_facilities[0][:state]).to eq("MO")
   end
 end
