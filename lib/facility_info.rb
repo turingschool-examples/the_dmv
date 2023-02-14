6c678017adef747675a325057f02fc9ed397663c
Katie Lonsdale <katie952@gmail.com>
2023-02-13 17:12:36 -0700

    feat: add daily hours method

--- lib/facility_info.rb
+++ lib/facility_info.rb
@@ -4,4 +4,11 @@ class FacilityInfo
   def initialize(source)
     @source = source
   end
+
+  def daily_hours
+    @source.map do |facility|
+      facility.select{|key, value| key.to_s.include?('day')}
+    end
+    # format days and times?
+  end
 end
