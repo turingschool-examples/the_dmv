85b1fcbdf5b0d2304ed849c0849cc0b565e8fede
Katie Lonsdale <katie952@gmail.com>
2023-02-13 17:29:56 -0700

    test: refactor daily hours tests

--- spec/facility_info_spec.rb
+++ spec/facility_info_spec.rb
@@ -23,10 +23,21 @@ RSpec.describe FacilityInfo do
   describe '#daily_hours' do
     it 'lists hours NY facility is open' do
       expect(@facility_info.daily_hours[0]).to be_an_instance_of Hash
+      expect(@facility_info.daily_hours[0]).to eq({:monday_beginning_hours=>"8:30 AM",
+        :monday_ending_hours=>"4:15 PM",
+        :tuesday_beginning_hours=>"8:30 AM",
+        :tuesday_ending_hours=>"4:15 PM",
+        :wednesday_beginning_hours=>"8:30 AM",
+        :wednesday_ending_hours=>"4:15 PM",
+        :thursday_beginning_hours=>"8:30 AM",
+        :thursday_ending_hours=>"5:45 PM",
+        :friday_beginning_hours=>"8:30 AM",
+        :friday_ending_hours=>"4:15 PM"})
     end
 
     it 'returns days open key for MO facilities' do
-      expect(@facility_info_2.daily_hours).to be_an_instance_of String
+      expect(@facility_info_2.daily_hours[0]).to be_an_instance_of String
+      expect(@facility_info_2.daily_hours[0]).to eq('Monday - Friday - 8:30 to 5:00, Last 4 week days - 8:00 to 5:00')
     end
   end
 end
