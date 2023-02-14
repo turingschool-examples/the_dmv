9b884994228af8798382fcad0e7b4f8c383343d6
Katie Lonsdale <katie952@gmail.com>
2023-02-14 15:20:39 -0700

    test: add filter cars test

--- spec/ny_vehicle_factory_spec.rb
+++ spec/ny_vehicle_factory_spec.rb
@@ -16,10 +16,13 @@ RSpec.describe NyVehicleFactory do
       expect(@factory.create_vehicles(@ny_registrations)).to be_an_instance_of(Array)
       expect(@factory.create_vehicles(@ny_registrations)[0]).to be_an_instance_of(Vehicle)
     end
+  end
 
-    # it 'only uses cars' do
-    #   @factory.create_vehicles(@ny_registrations)).
-    # end
+  describe '#filter_cars' do
+    it 'creates and array of only cars' do
+        expect(@factory.filter_cars(@ny_registrations)).to be_an_instance_of(Array)
+        expect(@factory.filter_cars(@ny_registrations)[0][:record_type]).to eq ('VEH')
+    end
   end
 
 end
