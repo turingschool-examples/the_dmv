b617470aa28abd33ca5f5e6ed1858ab48b518f16
Logan Wilson <loganwilson777@gmail.com>
2023-02-13 17:29:47 -0700

    added mulitple methods

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,34 @@
+
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services, 
+              :registered_vehicles,
+              :collected_fees
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(facility_hash)
+    @name = facility_hash[:name]
+    @address = facility_hash[:address]
+    @phone = facility_hash[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
+    return [service]
   end
+
+  def register_vehicle(model)
+    time = Time.now.to_s
+    date = []
+    date << time.split.first
+    registration_date = date
+    registered_vehicles << model
+    return registered_vehicles
+  end
+
+  
 end
