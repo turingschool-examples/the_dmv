afd55e1e18f62a1cc6d97227a1b3a726f3e19397
Logan Cole <emtlogancole@gmail.com>
2023-02-14 16:58:09 -0600

    refactor: facility and vehicle factory

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -8,15 +8,15 @@ class VehicleFactory
   end
   def create_vehicles(source)
     source.each do |vehicle_hash|
-    hash = {}
-    hash[:vin] = vehicle_hash[:vin_1_10]
-    hash[:year] = vehicle_hash[:model_year]
-    hash[:make] = vehicle_hash[:make]
-    hash[:model] = vehicle_hash[:model]
-    hash[:engine] = :ev
-    hash[:plate_type] = :ev
-    # hash[:vin] = vehicle_hash[:vin_1_10]
-    @vehicles << Vehicle.new(hash)
+    saved_vehicles = {}
+    saved_vehicles[:vin] = vehicle_hash[:vin_1_10]
+    saved_vehicles[:year] = vehicle_hash[:model_year]
+    saved_vehicles[:make] = vehicle_hash[:make]
+    saved_vehicles[:model] = vehicle_hash[:model]
+    saved_vehicles[:engine] = :ev
+    saved_vehicles[:plate_type] = :ev
+    # saved_vehicles[:vin] = vehicle_hash[:vin_1_10]
+    @vehicles << Vehicle.new(saved_vehicles)
     end
   end
 end
