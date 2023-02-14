1a88166f86389baf9d428698901a13e676712b35
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-02-14 16:37:11 -0700

    add ny_vehicle data into dmv_data_service and corresponding spec file

--- spec/dmv_data_service_spec.rb
+++ spec/dmv_data_service_spec.rb
@@ -25,6 +25,12 @@ RSpec.describe DmvDataService do
     end
   end
 
+  describe '#ny_vehicle_registrations' do
+    it 'can load new york vehicle registration data' do
+      expect(@dds.ny_vehicle_registrations.size).to eq(1000)
+    end
+  end
+
   describe '#or_dmv_office_locations' do
     it 'can load oregon dmv office locations' do
       expect(@dds.or_dmv_office_locations.size).to eq(59)
