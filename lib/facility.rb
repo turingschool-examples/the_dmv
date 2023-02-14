ec22770eebba929be3e8a41b9b2e21156fadd930
Julian Beldotti <julian.beldotti@gmail.com>
2023-02-13 19:15:47 -0500

    Works on iteration 2

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,9 +1,9 @@
 require 'date'
 
 class Facility
-  attr_reader :name, 
-              :address, 
-              :phone, 
+  attr_reader :name,
+              :address,
+              :phone,
               :services,
               :registered_vehicles,
               :collected_fees
@@ -22,11 +22,25 @@ class Facility
   end
 
   # def add_date
-  #   @add_date  = add_date  
+  #   @add_date  = add_date
   #   @registration_date == Date.today
   # end
 
   def register_vehicle(car)
-    @registered_vehicles.push(car)
+    @registered_vehicles.push(car) 
+    # if @services.include?(['Vehicle Registration'])
+    # end
   end
+
+  def collect_fees
+    if @plate_type == :regular
+      @collected_fees += 100
+    elsif @plate_type == :ev
+      @collected_fees += 200
+    elsif @plate_type == :antique
+      @collected_fees += 25
+    end
+    # collect_fees
+  end
+
 end
