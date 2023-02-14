ee426fb1d17842d9139d4aefe0d96f980f5597d2
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-13 19:29:57 -0700

    Feat: add functional create_facilities_new_york method

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -11,5 +11,13 @@ class FacilityFactory
     facility_instances
   end
 
-  
+  def create_facilities_new_york(facility_database)
+
+    facility_instances = []
+    facility_database.each do |facility|
+      address = facility[:street_address_line_1], facility[:street_address_line_2], facility[:city], facility[:state], facility[:zip_code]
+      facility_instances.push(Facility.new({name: facility[:office_name], address: address.compact.join(' ') , phone: facility[:public_phone_number]}))
+    end
+    facility_instances
+  end
 end
