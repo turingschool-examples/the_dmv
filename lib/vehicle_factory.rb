ffd8cc88b3694a21357c746b6c68ab9fc4f6b79b
Thomas Hawley <thawley22@turing.edu>
2023-02-13 21:15:55 -0700

    feat: Add method to find the most popular make and model

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -16,4 +16,25 @@ class VehicleFactory
       @vehicle_storage << Vehicle.new(car_hash)
     end
   end
+
+  def most_popular_make_model
+    "#{find_most_popular_make}, #{find_most_popular_model}"
+  end
+  
+  def find_most_popular_make
+    most_popular_make = @vehicle_storage.group_by do |vehicle|
+      vehicle.make
+    end
+    most_popular_make.max_by {|make, vehicles| vehicles.length}.first
+  end
+
+  def find_most_popular_model
+    popular_make = @vehicle_storage.select do |vehicle|
+      vehicle.make == find_most_popular_make
+    end
+    most_popular_model = popular_make.group_by do |vehicle|
+      vehicle.model
+    end
+    most_popular_model.max_by {|model, vehicles| vehicles.length}.first
+  end
 end
