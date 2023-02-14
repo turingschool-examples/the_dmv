49973ceb44e38e6ff4e01d9d18c2807b62082c59
Katie Lonsdale <katie952@gmail.com>
2023-02-14 15:24:09 -0700

    feat: add filter cars helper method

--- lib/ny_vehicle_factory.rb
+++ lib/ny_vehicle_factory.rb
@@ -3,10 +3,16 @@ class NyVehicleFactory
   def initialize; end
 
   def create_vehicles(source)
-    # cars = source.select{|vehicle| vehicle[:record_type] == 'VEH'}
+    
     source.map do |car|
       Vehicle.new({vin: car[:vin], year: car[:model_year], make: car[:make], model: 'unspecified', engine: car[:fuel_type]})
       # fuel types won't match engine
+    end
+  end
+
+  def filter_cars(source)
+    source.select do |car|
+      car[:record_type] == 'VEH'
     end
   end
 end
