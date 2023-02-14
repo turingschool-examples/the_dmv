9d538a8dfa37f417de2434fdea2f9531d69a495a
Katie Lonsdale <katie952@gmail.com>
2023-02-14 15:27:02 -0700

    refactor: create vehicles method

--- lib/ny_vehicle_factory.rb
+++ lib/ny_vehicle_factory.rb
@@ -3,8 +3,8 @@ class NyVehicleFactory
   def initialize; end
 
   def create_vehicles(source)
-    
-    source.map do |car|
+    cars = filter_cars(source)
+    cars.map do |car|
       Vehicle.new({vin: car[:vin], year: car[:model_year], make: car[:make], model: 'unspecified', engine: car[:fuel_type]})
       # fuel types won't match engine
     end
