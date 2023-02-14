1ba6064fbee8143bc0d315d4b439ac7b5372fcd0
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-13 23:34:53 -0500

    feat: add ny registration handling to VehicleFactory

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -15,16 +15,19 @@ class VehicleFactory
         }
         Vehicle.new(vehicle_details)
       end
-    else
-      vehicles.map do |vehicle|
-        vehicle_details = {
-          vin: vehicle[:vin_1_10],
-          year: vehicle[:model_year],
-          make: vehicle[:make],
-          model: vehicle[:model],
-          engine: :ice
-        }
-        Vehicle.new(vehicle_details)
+
+    elsif vehicles[0].keys.include?(:record_type)
+      vehicles.filter{ 
+        |vehicle| vehicle[:record_type] == "VEH"
+        }.map do |vehicle|
+          vehicle_details = {
+            vin: vehicle[:vin],
+            year: vehicle[:model_year],
+            make: vehicle[:make],
+            model: vehicle[:body_type],
+            engine: vehicle[:fuel_type]
+          }
+          Vehicle.new(vehicle_details)
       end
     end
   end
