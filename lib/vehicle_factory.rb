4242ff825a89f35f2168dd18090ac63a5ee110c4
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-13 20:53:38 -0700

    feat: Add vehicle object to create_vehicles method

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,10 +1,36 @@
 class VehicleFactory
-  
+  attr_reader :vin,
+              :year,
+              :make,
+              :model,
+              :engine
   def initialize
   end
 
   def create_vehicles(dmv_data)
-    dmv_data
-    #  dmv_data = DmvDataService.new.send(dmv)
+    cars = []
+    dmv_data.each do |car|
+      cars << Vehicle.new(
+        engine: :ev,
+        make: car[:make],
+        model: car[:model],
+        vin: car[:vin_1_10],
+        year: car[:model_year]
+      )
+    end
+    cars
+  end   
+
+  def create_facilities(dmv_data)
+    locations = []
+    dmv_data.each do |location|
+      locations << Facility.new(
+        title: location[:title],
+        zip_code: location[:zip_code],
+        website: location[:website],
+        type: location[:type]
+      )
+    end
+    locations
   end
 end
