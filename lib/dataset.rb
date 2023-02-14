85ee4a930dad94c4543008be97d0e13d90798fb3
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-13 19:44:28 -0500

    feat: add most_popular_make_model method

--- lib/dataset.rb
+++ lib/dataset.rb
@@ -4,4 +4,19 @@ class Dataset
   def initialize(data)
     @data = data
   end
+
+  def most_popular_make_model
+    makes = @data.map {|vehicle| vehicle.make }
+    make_count = Hash.new(0)
+    makes.each {|make| make_count[make] += 1}
+    make = make_count.sort_by{|make, number| number}.last[0]
+
+    models = @data.find_all {|vehicle| vehicle.make == make }.map {|vehicle| vehicle.model }
+    model_count = Hash.new(0)
+    models.each {|model| model_count[model] += 1}
+    model = model_count.sort_by{|model, number| number}.last[0]
+
+    result = [make, model]
+    require 'pry'; binding.pry
+  end
 end
