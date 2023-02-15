8f99c3cf8254da329bba5aab47b0065e9cf98d49
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-14 17:43:02 -0700

    refactor tests

---
+++ spec/vehicle_factory_spec.rb
@@ -0,0 +1,43 @@
+require './lib/vehicle'
+require './lib/vehicle_factory'
+require './lib/dmv_data_service'
+
+RSpec.describe VehicleFactory do
+  describe '#initialize' do 
+    it 'exists' do
+      factory = VehicleFactory.new
+      expect(factory).to be_a(VehicleFactory)
+    end
+
+  end
+    
+  describe '#factory is operative' do
+    it 'creates vehicles' do
+      factory = VehicleFactory.new
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+      
+      expect(factory.create_vehicles(wa_ev_registrations)).to be_an_instance_of(Array)
+    end
+
+    it 'vehicles have vin numbers' do
+      factory = VehicleFactory.new
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+      factory.create_vehicles(wa_ev_registrations)
+      expect(factory.vehicles[0].vin).to eq(wa_ev_registrations[0][:vin_1_10])
+    end
+
+    it 'vehicles have engines' do
+      factory = VehicleFactory.new
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+      factory.create_vehicles(wa_ev_registrations)
+      expect(factory.vehicles[0].engine).to eq(:ev)
+    end
+
+    it 'vehicles have years' do 
+      factory = VehicleFactory.new
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+      factory.create_vehicles(wa_ev_registrations)
+      expect(factory.vehicles[0].year).to eq(wa_ev_registrations[0][:model_year])
+    end
+  end
+end
