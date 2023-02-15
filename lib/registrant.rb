fcd8aabf95567164e8a17fc2db1fb4ee7e547ccd
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-15 12:25:03 -0700

    Add reference notes for myself

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -5,7 +5,7 @@ class Registrant
     @name = name 
     @age = age
     @permit = permit
-    @license_data = {written: false, license: false, renewed: false}
+    @license_data = {written: false, license: false, renewed: false} ## setting license_data to the default hash values
   end
 
   def permit?
@@ -13,7 +13,7 @@ class Registrant
   end
 
   def earn_permit
-    @permit = true if @age >= 15
+    @permit = true if @age >= 15 ## give registrant permit if they are 15 or older
   end
 
 
--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,15 +1,15 @@
 class VehicleFactory
   
   def create_vehicles(data)
-    data.map do |vehicle|
+    data.map do |vehicle| ## map the data being passed through in order to reformat into data structure I want
       formatted_data = {
-        vin: vehicle[:vin_1_10],
-        year: vehicle[:model_year],
+        vin: vehicle[:vin_1_10], ## making the data key the new value and matching it to the original key
+        year: vehicle[:model_year], ## "                                                                "
         engine: vehicle[:electric_vehicle_type] ? :ev : :ice, ## ternary operator evaluates left of ? and decides which side of : to use
-        make: vehicle[:make],
-        model: vehicle[:model]
+        make: vehicle[:make], ## Same as line 6
+        model: vehicle[:model] ## Same as line 6
       }
-      Vehicle.new(formatted_data)
+      Vehicle.new(formatted_data)  ## Returning a new instance of the Vehilce class with the reformated data
     end
 
   end
