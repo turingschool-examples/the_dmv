218bb2ce707ba308181bf815210fd886e8c9833c
Logan Wilson <loganwilson777@gmail.com>
2023-02-14 17:07:40 -0700

    added tests for create facility NY & MO  methods

--- spec/facility_compiler_spec.rb
+++ spec/facility_compiler_spec.rb
@@ -21,6 +21,26 @@ RSpec.describe FacilityCompiler do
     expect(created_facilities).to be_a(Array)
     expect(created_facilities[0]).to be_a(Facility)
     expect(created_facilities.size).to eq(59)
-    # require 'pry'; binding.pry
+  end
+
+  it 'can create facilities with New York data set' do
+    factories = FacilityCompiler.new
+    new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+    created_facilities = factories.create_facilities_ny(new_york_facilities)
+    
+    expect(created_facilities).to be_a(Array)
+    expect(created_facilities[0]).to be_a(Facility)
+    expect(created_facilities.size).to eq(169)
+  end
+  
+  it 'can create facilities with New York data set' do
+    factories = FacilityCompiler.new
+    missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+    created_facilities = factories.create_facilities_mo(missouri_facilities)
+    
+    expect(created_facilities).to be_a(Array)
+    expect(created_facilities[0]).to be_a(Facility)
+    expect(created_facilities.size).to eq(178)
+    require 'pry'; binding.pry
   end
 end
