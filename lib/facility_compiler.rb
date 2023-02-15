f412caa81868f491e4312af2b06b4791a2ef189e
Logan Wilson <loganwilson777@gmail.com>
2023-02-14 17:06:23 -0700

    added create facility methods for two more data types

--- lib/facility_compiler.rb
+++ lib/facility_compiler.rb
@@ -18,4 +18,34 @@ class FacilityCompiler
     end
     @office_locations
   end
+  
+  def create_facilities_ny(facilities)
+    facilities.each do |facility|
+      facility_information = {
+        name: facility[:office_name],
+        address: facility[:street_address_line_1],
+        phone: facility[:public_phone_number],
+        services: [],
+        registered_vehicles: [],
+        collected_fees: 0
+      }
+      @office_locations << facility = Facility.new(facility_information)
+    end
+    @office_locations
+  end
+
+  def create_facilities_mo(facilities)
+    facilities.each do |facility|
+      facility_information = {
+        name: facility[:name],
+        address: facility[:address1],
+        phone: facility[:phone],
+        services: [],
+        registered_vehicles: [],
+        collected_fees: 0
+      }
+      @office_locations << facility = Facility.new(facility_information)
+    end
+    @office_locations
+  end
 end
