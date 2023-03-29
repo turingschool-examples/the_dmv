0f563079d734fd037d0ef8d2617ebca1222d14d7
K.D. Hubbard <hubbard.kd@gmail.com>
2023-03-29 17:03:16 -0600

    fix before each loop

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 RSpec.describe Registrant do 
   before(:each) do 
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_2 = Registrant.new('Penny', 15 )
   end
-  describe 'can #initialize' do
+  describe '#initialize' do
     it 'can initialize' do 
-     expect(registrant_1).to be_a Registrant
+     expect(@registrant_1).to be_a Registrant
     end
   end
 
