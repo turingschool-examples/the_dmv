53121fe1b92f11164256d55a9f00efd173d76efc
K.D. Hubbard <hubbard.kd@gmail.com>
2023-03-29 16:51:58 -0600

    fix formatting

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -5,24 +5,25 @@ RSpec.describe Registrant do
     registrant_1 = Registrant.new('Bruce', 18, true )
     registrant_2 = Registrant.new('Penny', 15 )
   end
-  
-  it "exists" do 
-    expect(registrant_1).to be_a Registrant
+  describe 'can initialize'do
+    it 'exists' do 
+     expect(registrant_1).to be_a Registrant
+    end
   end
 
-  it "has a name attribute/method" do
+  it 'has a name attribute/method' do
     registrant_1 = Registrant.new('Bruce', 18, true )
 
     expect(registrant_1.name).to eq('Bruce')
   end
 
-  it "has an age attribute/method" do 
+  it 'has an age attribute/method' do 
     registrant_1 = Registrant.new('Bruce', 18, true )
 
     expect(registrant_1.age).to eq(18)
   end
 
-  it "has a permit attribute/method that defaults to false" do 
+  it 'has a permit attribute/method that defaults to false' do 
     registrant_1 = Registrant.new('Bruce', 18, true )
     registrant_2 = Registrant.new('Penny', 15 )
 
@@ -30,7 +31,7 @@ RSpec.describe Registrant do
     expect(registrant_2.permit).to be false
   end
 
-  it "has a permit? method" do 
+  it 'has a permit? method' do 
     registrant_1 = Registrant.new('Bruce', 18, true )
     registrant_2 = Registrant.new('Penny', 15 )
 
@@ -38,7 +39,7 @@ RSpec.describe Registrant do
     expect(registrant_2.permit?).to be false
   end
 
-  it "has a license data attribute with false default values" do 
+  it 'has a license data attribute with false default values' do 
     registrant_1 = Registrant.new('Bruce', 18, true )
     registrant_2 = Registrant.new('Penny', 15 )
   end
