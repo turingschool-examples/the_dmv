37557dc43512e70e31535617f60a9e23aad5d341
K.D. Hubbard <hubbard.kd@gmail.com>
2023-03-29 17:08:01 -0600

    fix formatting in tests

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -7,36 +7,18 @@ RSpec.describe Registrant do
   end
   describe '#initialize' do
     it 'can initialize' do 
-     expect(@registrant_1).to be_a Registrant
+      expect(@registrant_1).to be_a Registrant
+      expect(@registrant_1.name).to eq('Bruce')
+      expect(@registrant_1.age).to eq(18)
+      expect(@registrant_1.permit).to be true
+      expect(@registrant_2.permit).to be false
     end
   end
-
-  it 'has a name attribute/method' do
-    registrant_1 = Registrant.new('Bruce', 18, true )
-
-    expect(registrant_1.name).to eq('Bruce')
-  end
-
-  it 'has an age attribute/method' do 
-    registrant_1 = Registrant.new('Bruce', 18, true )
-
-    expect(registrant_1.age).to eq(18)
-  end
-
-  it 'has a permit attribute/method that defaults to false' do 
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
-
-    expect(registrant_1.permit).to be true
-    expect(registrant_2.permit).to be false
-  end
-
-  it 'has a permit? method' do 
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
-
-    expect(registrant_1.permit?).to be true
-    expect(registrant_2.permit?).to be false
+  describe 'permit?' do
+    it 'can determine if a registrant has a permit' do 
+      expect(@registrant_1.permit?).to be true
+      expect(@registrant_2.permit?).to be false
+    end
   end
 
   it 'has a license data attribute with false default values' do 
