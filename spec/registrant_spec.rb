0147b668a5fbc6a05f97b4861687216dd1d15a92
K.D. Hubbard <hubbard.kd@gmail.com>
2023-03-29 16:31:23 -0600

    add name method test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Registrant do
     registrant_1 = Registrant.new('Bruce', 18, true )
     expect(registrant_1).to be_a Registrant
   end
+
+  it "has a name attribute/method" do
+    registrant_1 = Registrant.new('Bruce', 18, true )
+
+    expect(registrant_1.name).to eq('Bruce')
+  end
 end
