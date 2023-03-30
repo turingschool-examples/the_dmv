a3c88e6e9e9c7e158ef41661d5be03606365d3c4
Lauren Golden <llgolden@gmail.com>
2023-03-30 14:13:49 -0600

    feat: Add Registrant class

---
+++ lib/registrant.rb
@@ -0,0 +1,11 @@
+class Registrant
+  attr_reader :name, :age, :permit
+
+  def initialize(name, age, permit)
+    @name = name 
+    @age = age
+    @permit = facilities
+    @license_data = license_data
+  end
+
+end
---
+++ spec/registrant_spec.rb
@@ -0,0 +1,15 @@
+require "rspec"
+require "./lib/registrant"
+
+describe Registrant do
+  it "can add a registrant" do
+    registrant_1 = Registrant.new("Bruce", 18, true )
+    registrant_2 = Registrant.new("Penny", 15 )
+
+
+
+  end
+
+
+
+end #final
