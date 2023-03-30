d09bdd5dfdcc7eadbfe0621e04260d598cf2b0d7
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-03-30 14:31:52 -0600

    feat: Add license_data and earn_permit methods

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,5 +1,25 @@
 class Registrant
+  attr_reader :name, :age, :permit
+  
   def initialize(name, age, permit = false)
-    
+    @name = name
+    @age = age
+    @permit = permit
+  end
+
+  def permit?   
+    @permit
+  end
+
+  def license_data
+    {
+      :written => false,
+      :license => false,
+      :renewed => false,
+    }
+  end
+
+  def earn_permit
+   @permit = true
   end
 end
