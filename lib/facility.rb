96ba506b3909be90ccfabb46e8ce54183fe6dfe3
K.D. Hubbard <hubbard.kd@gmail.com>
2023-03-30 17:42:16 -0600

    add feat collected fees method

--- lib/facility.rb
+++ lib/facility.rb
@@ -3,7 +3,8 @@ class Facility
               :address, 
               :phone, 
               :services,
-              :registered_vehicles
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(facility_attributes)
     @name = facility_attributes[:name]
@@ -11,6 +12,7 @@ class Facility
     @phone = facility_attributes[:phone]
     @services = []
     @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe Facility do
     @facility = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
     @facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
     @facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
   end
   describe '#initialize' do
     it 'can initialize' do
@@ -16,7 +19,7 @@ RSpec.describe Facility do
     end
   end
 
-  describe '#add service' do
+  describe '#add_service' do
     it 'can add available services' do
       expect(@facility.services).to eq([])
       @facility.add_service('New Drivers License')
@@ -29,6 +32,12 @@ RSpec.describe Facility do
   describe '#registered_vehicles' do 
     it 'returns an empty array by default' do 
       expect(@facility_1.registered_vehicles).to eq([])
+    end
+  end
+
+  describe '#collected_fees' do 
+    it 'returns an integer of total collected fees, default 0' do 
+      expect(@facility_1.collected_fees).to eq(0)
     end
   end
 end
