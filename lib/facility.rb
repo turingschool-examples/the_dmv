5d327b4bb6ed8815312513630111c7b6626247c0
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-03-30 15:12:29 -0600

    feat: Add registered_vehicles and collected_fees methods

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,18 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services,
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(data)
     @name = data[:name]
     @address = data[:address]
     @phone = data[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
