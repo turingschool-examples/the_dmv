f5dabf6ed80f3119c28e1ea605188f9716afa029
Steven Stackhouse <stackmm@gmail.com>
2023-03-31 16:55:21 -0600

    add create_vehicle tests

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe VehicleFactory do
       expect(@factory).to be_an_instance_of(VehicleFactory)
     end
   end
+
+  describe '#create_vehicles' do
+    it 'can create vehicles' do
+      vehicles = @factory.create_vehicles(@wav_ev_registrations)
+      expect(vehicles).to be_an_instance_of(Array)
+      expect(vehicles.all? { |vehicle| vehicle.is_a?(Vehicle) }).to be(true)
+      expect(vehicles.all? { |vehicle| vehicle.vin.is_a?(String) }).to be(true)
+      expect(vehicles.all? { |vehicle| vehicle.year.is_a?(String) }).to be(true)
+      expect(vehicles.all? { |vehicle| vehicle.make.is_a?(String) }).to be(true)
+      expect(vehicles.all? { |vehicle| vehicle.model.is_a?(String) }).to be(true)
+      expect(vehicles.all? { |vehicle| vehicle.engine.is_a?(Symbol) }).to be(true)
+    end
+  end
 end
