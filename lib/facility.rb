cbf55281af2e694390ef716ff7485c29438cc184
Ashley Plasket <ashley.plasket@gmail.com>
2023-03-31 10:44:19 -0400

    add collected_fees instance variable

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,5 +1,10 @@
 class Facility
-  attr_reader :name, :address, :phone, :services, :registered_vehicles
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services, 
+              :registered_vehicles, 
+              :collected_fees
 
   def initialize(facility_details)
     @name = facility_details[:name]
@@ -7,9 +12,12 @@ class Facility
     @phone = facility_details[:phone]
     @services = []
     @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
   end
+
+  
 end
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -36,5 +36,16 @@ RSpec.describe Facility do
       expect(cruz.registration_date).to eq(nil)
       expect(facility_1.registered_vehicles).to eq([])
     end
+
+    it 'can collects fees for registered vehicles' do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+      facility_1.add_service('Vehicle Registration')
+
+      expect(facility_1.collected_fees).to eq(0)
+    end
   end
 end
