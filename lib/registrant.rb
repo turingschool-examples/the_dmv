8b521153106472c21c7d9ba264e522ce05adcd1e
Jorja Fleming <aset284@gmail.com>
2023-03-30 19:06:45 -0600

    Added update feature

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,3 +1,15 @@
 class Registrant
+  attr_reader :name, :age, :license_data
+
+  def initialize(name, age, permit = false, license_data = false)
+    @name = name
+    @age = age
+    @permit = permit
+    @license_data = license_data
+  end
+
+  def permit?
+    @permit
+  end
   
 end
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -2,9 +2,28 @@ require 'spec_helper'
 
 RSpec.describe Registrant do
   it 'exists' do
-    registrant = Registrant.new
+    registrant = Registrant.new("x",3)
     expect(registrant).to be_an_instance_of(Registrant)
   end
 
-  
+  it "has atttibutes" do
+    registrant = Registrant.new('Bruce', 18, true )
+    expect(registrant.name).to eq("Bruce")
+    expect(registrant.age).to eq(18)
+    expect(registrant.permit?).to eq(true)
+  end
+
+  it "can be updated" do
+    registrant_1 = Registrant.new('Bruce', 18, true )
+    registrant_2 = Registrant.new('Penny', 15 )
+    expect(registrant_1.name). to eq("Bruce")
+    expect(registrant_1.age). to eq(18)
+    expect(registrant_1.permit?). to eq(true)
+    expect(registrant_1.license_data). to eq(false)
+
+    expect(registrant_2.name). to eq("Penny")
+    expect(registrant_2.age). to eq(15)
+    expect(registrant_2.permit?). to eq(false)
+    expect(registrant_2.license_data). to eq(false)
+  end
 end
