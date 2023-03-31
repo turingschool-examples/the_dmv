12dc38a6fbcd1fbd1f836c4d7bed42380da8b807
Steven Stackhouse <stackmm@gmail.com>
2023-03-31 16:03:15 -0600

    add initialize for vehicle_factory

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe VehicleFactory do
-  
+  before(:each) do
+    @factory = VehicleFactory.new
+    @wav_ev_registrations = DmvDataService.new.wa_ev_registrations
+  end
+
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@factory).to be_an_instance_of(VehicleFactory)
+    end
+  end
 end
