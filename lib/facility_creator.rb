b33bce2c56a25b1226053b588673f5f81a0de6b3
Michael Callahan <mcalla242@gmail.com>
2023-03-31 16:57:58 -0600

    Add testing specs and facility creator for NY DMV locations

--- lib/facility_creator.rb
+++ lib/facility_creator.rb
@@ -5,14 +5,26 @@ class FacilityCreator
   end
 
   def create_facilities(json)
-    json.each do |facility|
-      hash = facility[:"location_1"]
-      long_address = hash[:human_address]
-      hash_address = JSON.load( long_address.gsub(/(?<=\")=>/, ":") )
-      address = "#{hash_address["address"]} #{hash_address["city"]} #{hash_address["state"]} #{hash_address["zip"]}"
+    if json == DmvDataService.new.or_dmv_office_locations
+      json.each do |facility|
+        hash = facility[:"location_1"]
+        long_address = hash[:human_address]
+        hash_address = JSON.load( long_address.gsub(/(?<=\")=>/, ":") )
+        address = "#{hash_address["address"]} #{hash_address["city"]} #{hash_address["state"]} #{hash_address["zip"]}"
 
-      new_facility = Facility.new({name: facility[:"title"], address: address, phone: facility[:"phone_number"]})
-      @facilities << new_facility
+        new_facility = Facility.new({name: facility[:"title"], address: address, phone: facility[:"phone_number"]})
+        @facilities << new_facility
+      end
+    elsif json == DmvDataService.new.ny_dmv_office_locations
+      json.each do |facility|
+        address = "#{facility[:"street_address_line_1"]} #{facility[:"city"]} #{facility[:"state"]} #{facility[:"zip_code"]}"
+        new_facility = Facility.new({name: facility[:"office_name"], address: address, phone: facility[:"public_phone_number"]})
+        @facilities << new_facility
+      end
+    elsif json == DmvDataService.new.mo_dmv_office_locations
+      json.each do |facility|
+        
+      end
     end
   end
 end
--- spec/facility_creator_spec.rb
+++ spec/facility_creator_spec.rb
@@ -2,12 +2,30 @@ require 'spec_helper'
 
 RSpec.describe FacilityCreator do
   describe "#import facilities" do
-    it "imports facilities from DMV API" do
+    it "imports facilities for Oregon from DMV API" do
       creator = FacilityCreator.new
       or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
 
       creator.create_facilities(or_dmv_office_locations)
-require 'pry'; binding.pry
+      expect(creator.facilities).to be_an_instance_of(Array)
+      expect(creator.facilities.first).to be_an_instance_of(Facility)
+    end
+
+    it "imports facilities for New York from DMV API" do
+      creator = FacilityCreator.new
+      ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
+
+      creator.create_facilities(ny_dmv_office_locations)
+      expect(creator.facilities).to be_an_instance_of(Array)
+      expect(creator.facilities.first).to be_an_instance_of(Facility)
+    end
+
+    it "imports facilities for Missouri from DMV API" do
+      creator = FacilityCreator.new
+      mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
+
+      creator.create_facilities(mo_dmv_office_locations)
+      require 'pry'; binding.pry
       expect(creator.facilities).to be_an_instance_of(Array)
       expect(creator.facilities.first).to be_an_instance_of(Facility)
     end
