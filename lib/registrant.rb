48ee21a4fae56747056b0ae3364f238f654e90d0
Steven Stackhouse <stackmm@gmail.com>
2023-03-30 19:23:45 -0600

    add initialize method and tests

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,9 +1,14 @@
-require 'date'
-
 class Registrant
+  attr_reader :name, :age, :permit
 
-  def initialize()
-    
+  def initialize(name, age, permit = false)
+    @name = name
+    @age = age
+    @permit = permit
+  end
+  
+  def permit?
+    @permit
   end
 
 end
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 
 RSpec.describe Registrant do
   before(:each) do
-    
+    @registrant_1 = Registrant.new('Bruce', 18, true)
+    @registrant_2 = Registrant.new('Penny', 15)
   end
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@registrant_1).to be_an_instance_of(Registrant)
+      expect(@registrant_2).to be_an_instance_of(Registrant)
+      require 'pry'; binding.pry
+    end
+  end
+
 end
