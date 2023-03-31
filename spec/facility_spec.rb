d154da9ca67ac02133a056d218a41dd018dbc4ff
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-03-31 14:28:46 -0600

    mistake/confusion: move test back to correct specs

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -99,7 +99,98 @@ RSpec.describe Facility do
       expect(facility_2.collected_fees).to eq(0)
     end
   end
+
+  describe "Iteration 2, DL written test" do
+    it "administers written test if facility has service" do
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 16 )
+      registrant_3 = Registrant.new('Tucker', 15 )
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+
+      expect(facility_1.administer_written_test(registrant_1)).to eq(false)
+      expect(registrant_1.license_data).to eq({:written => false, :license => false, :renewed => false})
+      facility_1.add_service('Written Test')
+      
+      expect(facility_1.services).to eq(["Written Test"])
+      
+      expect(facility_1.administer_written_test(registrant_1)).to eq(true)
+      expect(registrant_1.license_data).to eq({:written => true, :license => false, :renewed => false})
+    end
+
+    it "checks other registrants data, administers written test, rechecks license data" do
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 16 )
+      registrant_3 = Registrant.new('Tucker', 15 )
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      
+      facility_1.add_service('Written Test')
+      
+      expect(registrant_2.age).to eq(16)
+      expect(registrant_2.permit?).to eq(false)
+      expect(facility_1.administer_written_test(registrant_2)).to eq(false)
+      
+      registrant_2.earn_permit
+      
+      expect(registrant_2.permit?).to eq(true)
+      expect(facility_1.administer_written_test(registrant_2)).to eq(true)
+      expect(registrant_2.license_data).to eq({:written => true, :license => false, :renewed => false})
+
+      expect(registrant_3.age).to eq(15)
+      expect(registrant_3.permit?).to eq(false)
+      expect(facility_1.administer_written_test(registrant_3)).to eq(false)
+    
+      registrant_3.earn_permit
+
+      expect(facility_1.administer_written_test(registrant_3)).to eq(false)
+      expect(registrant_3.license_data).to eq({:written => false, :license => false, :renewed => false})
+    end
+  end
+  describe "Iteration 2, DL road_test" do
+    it "sees if facilities offer road_test service, administers road_test to registrants, checks registrant data" do
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 16 )
+      registrant_3 = Registrant.new('Tucker', 15 )
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      
+      expect(facility_1.administer_road_test(registrant_3)).to eq(false)
+    end
+  end
 end
+
+
+# # Road Test
+
+# pry(main)> facility_1.administer_road_test(registrant_3)
+# #=> false
+
+# pry(main)> registrant_3.earn_permit
+
+# pry(main)> facility_1.administer_road_test(registrant_3)
+# #=> false
+
+# pry(main)> registrant_3.license_data
+# #=> {:written=>false, :license=>false, :renewed=>false}
+
+# pry(main)> facility_1.administer_road_test(registrant_1)
+# #=> false
+
+# pry(main)> facility_1.add_service('Road Test')
+# #=> ["Written Test", "Road Test"]
+
+# pry(main)> facility_1.administer_road_test(registrant_1)
+# #=> true
+
+# pry(main)> registrant_1.license_data
+# #=> {:written=>true, :license=>true, :renewed=>false}
+
+# pry(main)> facility_1.administer_road_test(registrant_2)
+# #=> true
+
+# pry(main)> registrant_2.license_data
+# #=> {:written=>true, :license=>true, :renewed=>false}
 
 
 
