abd55cc850ee80aebdd52c623bffa033bd5ee15c
Alex Kiselich <alexander.kiselich@gmail.com>
2023-03-31 13:26:38 -0600

    Creat a test to see if the class registrant exisits

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+RSpec.describe "Registrant" do
+  it "exists" do
+    registrant_1 = Registrant.new('Bruce', 18, true )
+
+    expect(registrant_1).to be_a Registrant
+  end
+
+end
