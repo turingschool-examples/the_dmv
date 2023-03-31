8d728dc2bc5b3d36b9ac51f17e98eaa056318337
Alex Kiselich <alexander.kiselich@gmail.com>
2023-03-31 14:34:20 -0600

    Add a test for earn_permit method

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -21,7 +21,24 @@ describe '#permit?' do
     expect(registrant_2.age).to eq(15)
     expect(registrant_2.permit?).to eq(false)
   end
+end
 
-  describe '#earn_permit' do
-  it 'can earn a permit'
+describe '#earn_permit' do
+  it 'can earn a permit' do
+    registrant_1 = Registrant.new('Bruce', 18, true )
+    registrant_2 = Registrant.new('Penny', 15 )
+
+    expect(registrant_1.name).to eq("Bruce")
+    expect(registrant_1.age).to eq(18)
+    expect(registrant_1.permit?).to eq(true)
+    expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    expect(registrant_2.name).to eq("Penny")
+    expect(registrant_2.age).to eq(15)
+    expect(registrant_2.permit?).to eq(false)
+    expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+
+    registrant_2.earn_permit
+
+    expect(registrant_2.permit?).to eq(true)
+  end
 end
