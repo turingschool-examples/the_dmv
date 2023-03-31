401aa9bcd8e9ee411e2e590e1b07fd2871b31401
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-03-31 11:32:03 -0600

    Update Facility class for iteration 2

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Facility do
   end
 
   describe '#initialize' do
+
     it 'can initialize' do
       expect(@facility).to be_an_instance_of(Facility)
       expect(@facility.name).to eq('Albany DMV Office')
@@ -13,9 +14,11 @@ RSpec.describe Facility do
       expect(@facility.phone).to eq('541-967-2014')
       expect(@facility.services).to eq([])
     end
+
   end
 
   describe '#add service' do
+
     it 'can add available services' do
       expect(@facility.services).to eq([])
       @facility.add_service('New Drivers License')
@@ -23,5 +26,8 @@ RSpec.describe Facility do
       @facility.add_service('Vehicle Registration')
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
+
   end
+
+  
 end
