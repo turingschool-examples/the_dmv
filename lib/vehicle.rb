f2ad74b01f3acebf6cca96b6eceafab1c1be2cea
Nick Tassinari <ntass7800@gmail.com>
2023-03-31 15:23:56 -0600

    add plate_type method

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -6,7 +6,8 @@ class Vehicle
               :make,
               :model,
               :engine,
-              :registration_date
+              :registration_date 
+  attr_accessor :registration_date 
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -24,4 +25,19 @@ class Vehicle
   def electric_vehicle?
     @engine == :ev
   end
+
+  def plate_type 
+    if 
+      @engine == :antique 
+      :antique 
+    elsif 
+     @engine == :ev 
+      :ev 
+    else
+        
+      :regular 
+    end
+    
+  end
+
 end
