1e3bcc9b9d69730c65494d8bfec6d312277cd9b2
Justin Pasko <jpasko1112@gmail.com>
2023-03-30 23:23:39 -0400

    Add registrant.spec file.

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,8 @@
+require './lib/registrant'
+
+  RSpec.describe Registrant do
+    before(:each) do
+      @registrant_1 = Registrant.new('Bruce', 18, true )
+      @registrant_2 = Registrant.new('Penny', 15 )
+    end
+  end
