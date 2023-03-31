d99888488aa3c9720360373690f15c21a58591a9
Garrett Gregor <garrett.gregor@gmail.com>
2023-03-30 20:37:17 -0600

    Add the ability for a registrant to earn a permit

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -30,4 +30,31 @@ RSpec.describe Registrant do
     expect(registrant_2.permit?).to be false
     expect(registrant_2.license_data).to eq(r2_data)
   end
+  
+  it "can earn a permit" do
+    registrant_1 = Registrant.new('Bruce', 18, true )
+    registrant_2 = Registrant.new('Penny', 15 )
+    r1_data = {
+      :written  =>  false,
+      :license  =>  false, 
+      :renewed  =>  false
+    }
+
+    r2_data = {
+      :written  =>  false,
+      :license  =>  false,
+      :renewed  =>  false
+    }
+
+    expect(registrant_1.name).to eq("Bruce")
+    expect(registrant_1.age).to eq(18)
+    expect(registrant_1.permit?).to be true
+    expect(registrant_1.license_data).to eq(r1_data)
+    expect(registrant_1.license_data[:written]).to be false
+    expect(registrant_2.permit?).to be false
+    expect(registrant_2.license_data).to eq(r2_data)
+    
+    registrant_2.earn_permit
+    expect(registrant_2.permit?).to be true
+  end
 end
