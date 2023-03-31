560ebd2fda8151ae5683dc71eeb485ae315a6f5d
Myles Nottingham <MylesNottingham@gmail.com>
2023-03-30 22:27:00 -0600

    Test: Add tests for administer_written_test.

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -19,6 +19,9 @@ RSpec.describe Facility do
     @cruz = Vehicle.new({ vin: "123456789abcdefgh", year: 2012, make: "Chevrolet", model: "Cruz", engine: :ice })
     @bolt = Vehicle.new({ vin: "987654321abcdefgh", year: 2019, make: "Chevrolet", model: "Bolt", engine: :ev })
     @camaro = Vehicle.new({ vin: "1a2b3c4d5e6f", year: 1969, make: "Chevrolet", model: "Camaro", engine: :ice })
+    @registrant_1 = Registrant.new("Bruce", 18, true)
+    @registrant_2 = Registrant.new("Penny", 16)
+    @registrant_3 = Registrant.new("Tucker", 15)
   end
 
   describe "#initialize" do
@@ -85,6 +88,49 @@ RSpec.describe Facility do
 
       expect(@facility_2.registered_vehicles).to eq([])
       expect(@facility_2.collected_fees).to eq(0)
+    end
+  end
+
+  describe "#administer_written_test" do
+    it "will administer written tests only if service is offered" do
+      expect(@registrant_1.license_data).to eq({ written: false, license: false, renewed: false })
+      expect(@registrant_1.permit?).to eq(true)
+
+      expect(@facility_1.administer_written_test(@registrant_1)).to eq(false)
+
+      expect(@registrant_1.license_data).to eq({ written: false, license: false, renewed: false })
+
+      expect(@facility_1.add_service("Written Test")).to eq(["Written Test"])
+
+      expect(@facility_1.administer_written_test(@registrant_1)).to eq(true)
+
+      expect(@registrant_1.license_data).to eq({ written: true, license: false, renewed: false })
+    end
+
+    it "will not administer written tests if registrant does not have permit" do
+      expect(@registrant_2.age).to eq(16)
+      expect(@registrant_2.permit?).to eq(false)
+
+      expect(@facility_1.administer_written_test(@registrant_2)).to eq(false)
+
+      @registrant_2.earn_permit
+
+      expect(@facility_1.administer_written_test(@registrant_2)).to eq(true)
+
+      expect(@registrant_2.license_data).to eq({ written: true, license: false, renewed: false })
+    end
+
+    it "will not administer written tests unless registrant is at least 16 years of age" do
+      expect(@registrant_3.age).to eq(15)
+      expect(@registrant_3.permit?).to eq(false)
+
+      expect(@facility_1.administer_written_test(@registrant_3)).to eq(false)
+
+      @registrant_3.earn_permit
+
+      expect(@facility_1.administer_written_test(@registrant_3)).to eq(false)
+
+      expect(@registrant_3.license_data).to eq({ written: false, license: false, renewed: false })
     end
   end
 end
