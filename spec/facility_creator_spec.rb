f9610680c4d07d1ed5085786cc3942bb0cfa9ebe
Michael Callahan <mcalla242@gmail.com>
2023-03-31 15:41:01 -0600

    Add facility_creator testing specs

---
+++ spec/facility_creator_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+RSpec.describe FacilityCreator do
+  describe "#import facilities" do
+    it "imports facilities from DMV API" do
+      creator = FacilityCreator.new
+      or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
+
+      creator.create_facilities(or_dmv_office_locations)
+
+      expect(creator.facilities).to be_an_instance_of(Array)
+      expect(creator.facilities.first).to be_an_instance_of(Facility)
+    end
+  end
+end
