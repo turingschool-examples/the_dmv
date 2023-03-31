84036703f7ef068bc931410419a27cdbd27501aa
Andy Weissman <andyweissman6@gmail.com>
2023-03-30 18:28:24 -0600

    makes test run.
    I changed the 'find' enumerator to 'find_all' to ensure all facilities show up in array instead of first occurence. I also deleted the 's' in 'add_services.'

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,15 @@
 class Facility
   attr_reader :name, :address, :phone, :services
 
-  def initialize(name, address, phone)
+  def initialize(name:, address:, phone:)
+    # require 'pry'; binding.pry
     @name = name
     @address = address
     @phone = phone
     @services = []
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
   end
 end
