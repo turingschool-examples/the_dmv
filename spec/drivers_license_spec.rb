82adc9c037fccc7155682e68c4e650edd24d5e45
Michael Callahan <mcalla242@gmail.com>
2023-03-31 11:51:21 -0600

    Fix typo in testing specs

--- spec/drivers_license_spec.rb
+++ spec/drivers_license_spec.rb
@@ -83,6 +83,7 @@ RSpec.describe Facility do
   describe "#renew license" do
     it "can renew licenses" do
       @facility_1.add_service("Written Test")
+      @facility_1.add_service("Road Test")
       @facility_1.administer_written_test(@registrant_1)
       @registrant_2.earn_permit
       @facility_1.administer_written_test(@registrant_2)
@@ -90,7 +91,7 @@ RSpec.describe Facility do
       @facility_1.administer_road_test(@registrant_2)
 
       expect(@facility_1.renew_drivers_license(@registrant_1)).to eq(false)
-      expect(@facilitt_1.add_service("Renew License")).to eq(["Written Test", "Road Test", "Renew License"])
+      expect(@facility_1.add_service("Renew License")).to eq(["Written Test", "Road Test", "Renew License"])
       expect(@facility_1.renew_drivers_license(@registrant_1)).to eq(true)
       expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
       expect(@facility_1.renew_drivers_license(@registrant_3)).to eq(false)
