9e4d3019c84d90bdef4c394a0b1db8f0db54e45a
Nick Tassinari <ntass7800@gmail.com>
2023-03-31 08:54:19 -0600

    for some reason these were deleted locally

---
+++ lib/registrant.rb
@@ -0,0 +1,30 @@
+class Registrant 
+  attr_reader :license_data, :name, :age, :permit, :earn_permit
+def initialize(name, age, permit = false)
+  @name = name 
+  @age = age 
+  @permit = permit
+ 
+end
+
+def permit? 
+  if permit == true 
+    true 
+  else 
+    false 
+  end
+end
+
+def earn_permit
+  @permit = true 
+end
+
+def license_data
+  {
+    written: false,
+    license: false,
+    renewed: false
+
+  }
+end
+end
