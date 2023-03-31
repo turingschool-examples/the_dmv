7f75e893fef9732bf5ef6e7edc80701af608a3bb
Garrett Gregor <garrett.gregor@gmail.com>
2023-03-31 16:00:56 -0600

    Save progress through registering vehicles

--- lib/facility.rb
+++ lib/facility.rb
@@ -3,15 +3,30 @@ class Facility
               :address,
               :phone,
               :services
+  attr_accessor :registered_vehicles,
+                :collected_fees
 
   def initialize(facility_details)
+    @collected_fees = 0
     @name     = facility_details[:name]
     @address  = facility_details[:address]
     @phone    = facility_details[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    vehicle.registration_date = Date.today
+    if vehicle.antique?
+      vehicle.plate_type = :antique
+      collected_fees += 100
+    else
+      vehicle.plate_type = :regular
+    end
+    @registered_vehicles << vehicle
   end
 end
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -1,4 +1,6 @@
 require 'spec_helper'
+require "./lib/facility"
+require "./lib/vehicle"
 
 RSpec.describe Facility do
   before(:each) do
@@ -21,6 +23,28 @@ RSpec.describe Facility do
       @facility.add_service('Renew Drivers License')
       @facility.add_service('Vehicle Registration')
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+    end
+  end
+
+  describe "Iteration 2" do
+    it "can register vehicles" do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+      
+      facility_1.add_service("Vehicle Registration")
+      expect(facility_1.services).to eq(["Vehicle Registration"])
+      expect(facility_1.registered_vehicles).to eq([])
+      expect(facility_1.collected_fees).to eq(0)
+      expect(cruz.registration_date).to eq(nil)
+      
+      facility_1.register_vehicle(cruz)
+      require 'pry'; binding.pry
+      expect(facility_1.registered_vehicles).to eq([cruz])
+      expect(cruz.registration_date).to eq(Date.today)
+
     end
   end
 end
