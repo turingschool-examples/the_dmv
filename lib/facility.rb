ace28cc80c288aa7d0e7d8f8ac682bdbb1cb7b26
Ashley Plasket <ashley.plasket@gmail.com>
2023-03-30 21:15:06 -0400

    add registered vehicles as instance variable

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,12 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles
 
   def initialize(facility_details)
     @name = facility_details[:name]
     @address = facility_details[:address]
     @phone = facility_details[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe Facility do
     end
   end
 
-  describe 'offer more services'
+  describe 'offer more services' do
     it 'has default empty array of registered vehicles' do
       facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
       facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
