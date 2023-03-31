9fc813b745a9841a0e409c228f9e90249cabfc3d
Myles Nottingham <MylesNottingham@gmail.com>
2023-03-31 13:40:29 -0600

    Test: Add tests for administer_road_test.

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -108,6 +108,9 @@ RSpec.describe Facility do
     end
 
     it "will not administer written tests if registrant does not have permit" do
+      @facility_1.add_service("Written Test")
+      @facility_1.administer_written_test(@registrant_1)
+
       expect(@registrant_2.age).to eq(16)
       expect(@registrant_2.permit?).to eq(false)
 
@@ -121,6 +124,11 @@ RSpec.describe Facility do
     end
 
     it "will not administer written tests unless registrant is at least 16 years of age" do
+      @facility_1.add_service("Written Test")
+      @facility_1.administer_written_test(@registrant_1)
+      @registrant_2.earn_permit
+      @facility_1.administer_written_test(@registrant_2)
+      
       expect(@registrant_3.age).to eq(15)
       expect(@registrant_3.permit?).to eq(false)
 
@@ -131,6 +139,50 @@ RSpec.describe Facility do
       expect(@facility_1.administer_written_test(@registrant_3)).to eq(false)
 
       expect(@registrant_3.license_data).to eq({ written: false, license: false, renewed: false })
+    end
+  end
+
+  describe "#administer_road_test" do
+    before(:each) do
+      @facility_1.add_service("Written Test")
+      @facility_1.administer_written_test(@registrant_1)
+      @registrant_2.earn_permit
+    end
+
+    it "will administer road tests only if service is offered" do
+      @facility_1.administer_written_test(@registrant_2)
+      @registrant_3.earn_permit
+
+      expect(@facility_1.administer_road_test(@registrant_3)).to eq(false)
+      @registrant_3.earn_permit
+      expect(@facility_1.administer_road_test(@registrant_3)).to eq(false)
+      expect(@registrant_3.license_data).to eq({ written: false, license: false, renewed: false })
+
+      expect(@facility_1.administer_road_test(@registrant_1)).to eq(false)
+
+      expect(@facility_1.add_service("Road Test")).to eq(["Written Test", "Road Test"])
+
+      expect(@facility_1.administer_road_test(@registrant_1)).to eq(true)
+      expect(@registrant_1.license_data).to eq({ written: true, license: true, renewed: false })
+
+      expect(@facility_1.administer_road_test(@registrant_2)).to eq(true)
+      expect(@registrant_2.license_data).to eq({ written: true, license: true, renewed: false })
+    end
+
+    it "will not administer road tests if registrant has not passed written" do
+      @facility_1.add_service("Road Test")
+
+      expect(@facility_1.administer_road_test(@registrant_2)).to eq(false)
+
+      expect(@registrant_2.license_data).to eq({ written: false, license: false, renewed: false })
+
+      @facility_1.administer_written_test(@registrant_2)
+
+      expect(@registrant_2.license_data).to eq({ written: true, license: false, renewed: false })
+
+      expect(@facility_1.administer_road_test(@registrant_2)).to eq(true)
+
+      expect(@registrant_2.license_data).to eq({ written: true, license: true, renewed: false })
     end
   end
 end
