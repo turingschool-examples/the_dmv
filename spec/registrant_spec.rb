05d077b85ef2fb136ae948b24db2310a6ee9f03b
Sarah Garlock <garlock.sarah10@gmail.com>
2023-03-30 21:02:46 -0600

    Add age

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -16,5 +16,9 @@ RSpec.describe Registrant do
     expect(@registrant_2.name).to eq('Penny')
   end
 
+  it 'has an age' do
+    expect(@registrant_1.age).to eq 18
+    expect(@registrant_2.age).to eq 15
+  end
 
 end
