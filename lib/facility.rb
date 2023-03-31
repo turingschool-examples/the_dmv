54845519611ef5b20b45d5a81786abda87d3d383
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-03-30 21:45:07 -0500

    Fix attributes to initialize

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,7 +1,7 @@
 class Facility
   attr_reader :name, :address, :phone, :services
 
-  def initialize(name, address, phone)
+  def initialize(name:, address:, phone:)
     @name = name
     @address = address
     @phone = phone
