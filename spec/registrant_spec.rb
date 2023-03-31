b03dc2abbc807c13230f72a40c7d703a75561d31
Justin Pasko <jpasko1112@gmail.com>
2023-03-30 23:29:49 -0400

    Fix registrant_spec.rb to require spec_helper.rb.

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,8 +1,12 @@
-require './lib/registrant'
+require 'spec_helper'
 
   RSpec.describe Registrant do
     before(:each) do
       @registrant_1 = Registrant.new('Bruce', 18, true )
       @registrant_2 = Registrant.new('Penny', 15 )
+    end
+
+    it "exists" do
+
     end
   end
