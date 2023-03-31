e752778bfa0b760926e9f468a906632d73ca59be
Steven Stackhouse <stackmm@gmail.com>
2023-03-31 16:23:05 -0600

    add create_vehicles method

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,5 +1,15 @@
+require './lib/vehicle'
+
 class VehicleFactory
-  def initialize()
-    
+  def create_vehicles(vehicle_data)
+    vehicle_data.map do |data|
+      Vehicle.new({
+        vin: data[:vin_1_10],
+        year: data[:model_year],
+        make: data[:make],
+        model: data[:model],
+        engine: :ev
+      })
+    end
   end
 end
