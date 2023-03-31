8d2390b2629102eacd4eaf476c1ad37e8919c944
Jimmy Jurado <jurado140@gmail.com>
2023-03-31 16:23:45 -0400

    Add the register vehicle method and shuffle into reg vehicle array.

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,23 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles
 
-  def initialize(info)
-    @name = info[:name]
-    @address = info[:address]
-    @phone = info[:phone]
+  def initialize(data)
+    @name = data[:name]
+    @address = data[:address]
+    @phone = data[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
     @services << service
+    @service
+  end
+
+  def register_vehicle(vehicle)
+    return nil if !@services.include?('Vehicle Registration')
+
+    @registered_vehicles << vehicle
+    require 'pry'; binding.pry
   end
 end
