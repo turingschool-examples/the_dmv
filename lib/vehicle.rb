0bd6ab1a11773d519aa962b25cf314fd1e3b8d9b
Jesse Thomas <jgthomas3@gmail.com>
2023-03-31 20:28:11 -0600

    Add attr_accessor, add registration_date method, add plate_type method vehicle.rb

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,8 +5,9 @@ class Vehicle
               :year,
               :make,
               :model,
-              :engine,
-              :registration_date
+              :engine
+
+  attr_accessor :registration_date
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -25,4 +26,25 @@ class Vehicle
   def electric_vehicle?
     @engine == :ev
   end
+
+  # def registration_date
+  #   time = Time.new
+  #   @registration_date = time.strftime("%d/%m/%Y")
+  #   #this sets the intial test on 53 to return a date, not sure how to work around the attr_accessor yet
+  # end
+
+  def plate_type
+    date = Time.new.strftime("%Y")
+    return @plate_type = :antique if (date.to_i - @year) >= 25
+    return @plate_type = :ev if @engine == :ev
+    return @plate_type = :regular
+    require 'pry'; binding.pry
+    #@year - 25? = :antique (25+) $25
+    #:ev = $200
+    #everything else = $100
+  end
+
+
+
+
 end
