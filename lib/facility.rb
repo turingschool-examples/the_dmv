9a9ff8c43fd863e388be795bc7231e2c1206aa69
Javen Wilson <javenb022@gmail.com>
2023-04-01 19:02:09 -0400

    add register vehicle method

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,48 @@
-class Facility
-  attr_reader :name, :address, :phone, :services
+require 'date'
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
-    @services = []
+class Facility
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services,
+              :collected_fees,
+              :registered_vehicles
+
+  def initialize(facility_details)
+    @name                = facility_details[:name]
+    @address             = facility_details[:address]
+    @phone               = facility_details[:phone]
+    @services            = []
+    @collected_fees      = 0
+    @registered_vehicles = []
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
   end
+
+  def register_vehicle(vehicle)
+    if vehicle.year >= 25 && vehicle.engine == :ice
+      # require 'pry'; binding.pry
+      @collected_fees += 25
+      @plate_type = :antique 
+      @registered_vehicles << vehicle
+      @registration_date = DateTime.now
+      return vehicle
+    elsif vehicle[:year] < 25 && vehicle[:engine] == :ev
+      @collected_fees += 200
+      @plate_type = :ev
+      @registered_vehicles << vehicle
+      @registration_date = DateTime.now
+      return vehicle
+    elsif vehicle[:year] < 25 && vehicle[:engine] == :ice
+      @collected_fees += 100
+      @plate_type = :regular
+      @registered_vehicles << vehicle
+      @registration_date = DateTime.now
+      return vehicle
+    end
+  end
+
+
 end
