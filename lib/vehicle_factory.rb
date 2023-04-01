46777f0b00df3aa7bff24e5d0eca54ef56826e85
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-01 17:38:33 -0600

    feat: Add most_registered_county and least_popular methods

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -21,7 +21,15 @@ class VehicleFactory
    dmv.group_by { |car| car[:make] }.max_by { |make, brand| brand.count }.first
   end
 
+  def least_popular(dmv)
+    dmv.group_by { |car| car[:make] }.min_by { |make, brand| brand.count }.first
+  end
+
   def count_model_year(dmv)
-   model_year = dmv.group_by { |car| car[:model_year] }.transform_values { |car| car.count }
+   dmv.group_by { |car| car[:model_year] }.transform_values { |car| car.count }
+  end
+
+  def most_registered_county(dmv)
+    dmv.group_by { |car| car[:county] }.max_by { |name, car| car.count }.first
   end
 end
