b76bf5d62cd18a280550aad194910181e74102aa
Steven Stackhouse <stackmm@gmail.com>
2023-04-01 16:27:03 -0600

    add create_facilities method

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,5 +1,14 @@
 require './lib/facility'
 
 class FacilityFactory
-  
+  def create_facilities(data)
+    data.map do |facility|
+      Facility.new({
+        name: facility[:title],
+        address: facility[:human_address],
+        phone: facility[:phone_number]
+      })
+    end
+  end
+
 end
