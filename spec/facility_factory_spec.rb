97ed7d409b9ff1939ad69c87676650575f8d6d0b
K.D. Hubbard <hubbard.kd@gmail.com>
2023-04-01 17:13:25 -0600

    add create facilities testing

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -11,6 +11,12 @@ RSpec.describe FacilityFactory do
       expect(@fac_factory).to be_a(FacilityFactory)
       expect(@fac_factory.facilities).to eq([])
     end
-    
+  end
+
+  describe '#create_facilities' do 
+    it 'can create hashes of facility data' do 
+      expect(@fac_factory.create_facilities(@or_dmv_office_locations)).to be_a(Array)
+      expect(@fac_factory.facilities.length).not_to eq(0)
+    end
   end
 end
