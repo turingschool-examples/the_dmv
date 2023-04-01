de456e7749fc812433a117a291151c8ea319ef45
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-03-31 21:56:54 -0600

    test: Add tests for newly created facility objects from oregon dataset

---
+++ spec/facility_factory_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+RSpec.describe FacilityFactory do
+  describe "Iteration 3" do
+    it "exists" do
+      facility = FacilityFactory.new
+      
+      expect(facility).to be_a(FacilityFactory)
+    end
+
+    it "creates new facilities from oregon data set" do
+      facility = FacilityFactory.new
+      oregon = DmvDataService.new.or_dmv_office_locations
+      
+      require 'pry'; binding.pry
+      facilities = facility.create_facility(oregon)
+      
+      expect(facilities[0].name).to eq("Albany DMV Office")
+      expect(facilities[0].phone).to eq("541-967-2014")
+      expect(facilities[2].phone).to eq("503-325-3951")
+      expect(facilities[4].name).to eq("Beaverton DMV Office")
+      expect(facilities[0].address).to eq(nil)
+    end
+
+    it "creates new facilities from new york data set" do
+      facility = FacilityFactory.new
+      new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+
+
+      ny_facilities = facility.create_facility
+
+    end
+  end
+end
