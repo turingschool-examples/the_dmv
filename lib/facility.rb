c767fa296ad5deed8211b569ef0d7e38d605b663
Jesse Sorman <jessesorman@gmail.com>
2023-04-01 13:32:07 -0400

    refactor register vehicle method

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,5 +1,5 @@
 class Facility
-  attr_reader :name, :address, :phone, :services, :date_of_registration, :vehicle_details, :registration_date
+  attr_reader :name, :address, :phone, :services, :registration_date, :vehicle_details
   def initialize(facility_info)
     @name = facility_info[:name]
     @address = facility_info[:address]
@@ -23,11 +23,22 @@ class Facility
 
   def register_vehicle(vehicle)
     if services.include?('Vehicle Registration')
-      registered_vehicles << vehicle
+      if vehicle.plate_type == :antique
+        @collected_fees += 25
+        registered_vehicles << vehicle
+        vehicle.registration_date = Date.today.year
+      elsif vehicle.plate_type == :ev
+        @collected_fees += 200
+        registered_vehicles << vehicle
+        vehicle.registration_date = Date.today.year
+      elsif vehicle.plate_type == :regular
+        @collected_fees += 100
+        registered_vehicles << vehicle
+        vehicle.registration_date = Date.today.year
+      end
     else
       "registering not available"
     end
-    # @vehicle.registration_date << Date.today.year
   end
 
 
