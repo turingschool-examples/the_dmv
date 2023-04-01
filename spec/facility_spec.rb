119f8a5ce999f982dadb1420dce9d017bf28545e
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-01 09:12:08 -0600

    Allows facility to administer test after registrant has earned permit

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -261,6 +261,23 @@ RSpec.describe Facility do
         expect(registrant_2.permit?).to be false
         expect(facility_1.administer_written_test(registrant_2)).to be false
       end
+      
+      it "can administer written test after regisrant has earned permit" do
+        registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 16 )
+        registrant_3 = Registrant.new('Tucker', 15 )
+        facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+        facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+        facility_1.add_service("Written Test")
+        facility_1.administer_written_test(registrant_1)
+        
+        expect(registrant_2.age).to eq(16)
+        expect(registrant_2.permit?).to be false
+        expect(facility_1.administer_written_test(registrant_2)).to be false
+        
+        registrant_2.earn_permit
+        expect(facility_1.administer_written_test(registrant_2)).to be true
+      end
     end
   end
 end
