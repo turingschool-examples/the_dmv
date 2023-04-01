0e72191446b80fdb47dd57c9ba46b6975be0f895
Jesse Thomas <jgthomas3@gmail.com>
2023-03-31 20:28:44 -0600

    Adjust and rearrange test methods facility_spec.rb

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 RSpec.describe Facility do
-  before(:each) do
-    @facility = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
-  end
+  # before(:each) do
+  #   @facility = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+  # end
   # describe "Established information" do
 
   #   describe '#initialize' do
@@ -43,76 +43,131 @@ RSpec.describe Facility do
     # one action per test. This is more convention issues than anything. Come back and adjust them if I have time. Decide on one way or the other.
 
     it "returns the services added" do
+
       expect(@facility_1.add_service('Vehicle Registration')).to eq(["Vehicle Registration"])
+
     end
 
-    it "vehicle registration date defaults to nil" do
+    it "vehicle registration_date defaults to nil" do
+
       expect(@cruz.registration_date).to eq(nil)
+
     end
 
     it "registered_vehicles attribute defaults to empty" do
+
       expect(@facility_1.registered_vehicles).to eq([])
+
     end
 
     it "collected_fees attribute defaults to 0" do
+
       expect(@facility_1.collected_fees).to eq(0)
+
     end
     
     it "returns the register_vehicle method" do
 
       expect(@facility_1.register_vehicle(@cruz)).to eq([@cruz])
-      #=> [#<Vehicle:0x0000000135a48b08...>]
+
+      expect(@facility_1.register_vehicle(@camaro)).to eq([@cruz, @camaro])
+
+      expect(@facility_1.register_vehicle(@bolt)).to eq([@cruz, @camaro, @bolt])
+
     end
 
     it "will set the vehicle registration date" do
-      
-      expect(@cruz.registration_date).to eq(Date.today)
-      #=> #<Date: 2023-01-12 ((2459957j,0s,0n),+0s,2299161j)>
+
+      date = Time.new.strftime("%d/%m/%Y")
+      @facility_1.register_vehicle(@cruz)
+      @facility_1.register_vehicle(@camaro)
+      @facility_1.register_vehicle(@bolt)
+
+      expect(@cruz.registration_date).to eq(date)
+      expect(@camaro.registration_date).to eq(date)
+      expect(@bolt.registration_date).to eq(date)
+
     end
 
-    xit "can have a :regular plate type" do
+    it "can have a :regular plate type" do
+
       expect(@cruz.plate_type).to eq(:regular)
-      #=> :regular
+
     end
 
-    xit "adds vehicles to @registered_vehicles" do
+    it "can have an :ev plate type" do
 
+      expect(@bolt.plate_type).to eq(:ev)
+
+    end
+
+    it "can have an :antique plate type" do
+
+      expect(@camaro.plate_type).to eq(:antique)
+      
+    end
+
+    xit "adds plate_type to vehicle when registered" do
+
+      #this isn't asserting anything or at least my shit isn't returnig the right way
+
+      @facility_1.register_vehicle(@cruz)
+
+      expect(@facility_1.registered_vehicles[0].plate_type).to eq(:regular)
+
+    end
+
+    xit "adds vehicles to @registered_vehicles attribute" do
+
+      @facility_1.register_vehicle(@cruz)
       
       expect(@facility_1.registered_vehicles).to eq([@cruz])
-      #=> [#<Vehicle:0x0000000135a48b08...>]
+
+      @facility_1.register_vehicle(@camaro)
+      
+      expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro])
+
+      # @facility_1.register_vehicle(@bolt)
+
+      # expect(@facility_1.registered_vehicles).to eq([@cruz, @camara, @bolt])
+
+
     end
       
     xit "can collect fees for type of car" do
 
+      @facility_1.register_vehicle(@cruz)
+
       expect(@facility_1.collected_fees).to eq(100)
-      #=> 100
+      
     end
+
+
 
     xit "can register a antique type" do
 
-      expect(@facility_1.register_vehicle(@camaro)).to eq([@cruz, @camaro])
       #=> [#<Vehicle:0x0000000135a48b08...>, #<Vehicle:0x0000000135adb610...>]
       
-      expect(@camaro.registration_date).to eq(Date.today)
+      # expect(@camaro.registration_date).to eq(Date.today)
       #=> #<Date: 2023-01-12 ((2459957j,0s,0n),+0s,2299161j)>
       
-      expect(@camaro.plate_type).to eq(:antique)
-      #=> :antique
-      expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro])
+      # expect(@camaro.plate_type).to eq(:antique)
+      # #=> :antique
+      # expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro])
       expect(@facility_1.collected_fees).to eq(25)
     end
       
     xit "can register a third vehicle" do 
 
 
-      expect(@facility_1.register_vehicle(@bolt)).to eq([@cruz, @camaro, @bolt])
+      # expect(@facility_1.register_vehicle(@bolt)).to eq([@cruz, @camaro, @bolt])
       #=> [#<Vehicle:0x0000000135a48b08...>, #<Vehicle:0x0000000135adb610...>, #<Vehicle:0x0000000125832180...>]
       
-      expect(@bolt.registration_date).to eq(Date.today)
+      # expect(@bolt.registration_date).to eq(Date.today)
       #=> #<Date: 2023-01-12 ((2459957j,0s,0n),+0s,2299161j)>
       
-      expect(@bolt.plate_type).to eq(:ev)
-      #=> :ev
+      # expect(@bolt.plate_type).to eq(:ev)
+      # #=> :ev
       
       expect(@facility_1.registered_vehicles).to eq([@cruz, @camara, @bolt])
       #=> [#<Vehicle:0x0000000135a48b08...>, #<Vehicle:0x0000000135adb610...>, #<Vehicle:0x0000000125832180...>]
