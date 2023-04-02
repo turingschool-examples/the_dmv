e1068f6fb7f7e68ede79d0a7316a7f679b3000c7
Sarah Garlock <garlock.sarah10@gmail.com>
2023-04-01 18:49:37 -0600

    Fix registrant class

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+
+RSpec.describe Registrant do
+  before(:each) do
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_2 = Registrant.new('Penny', 15 )
+  end
+
+  it 'exists'do
+    expect(@registrant_1).to be_an_instance_of Registrant
+    expect(@registrant_2).to be_an_instance_of Registrant
+  end
+
+
+end
