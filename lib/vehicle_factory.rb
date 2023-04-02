979855de00f9ff4d67292b0782f9727b20520862
Michael Callahan <mcalla242@gmail.com>
2023-04-02 08:42:48 -0600

    Increase readability for vehicle_factory parameter

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -6,7 +6,13 @@ class VehicleFactory
 
   def create_vehicles(json)
     json.each do |vehicle|
-      new_vehicle = Vehicle.new({vin: vehicle[:"vin_1_10"], year: vehicle[:"model_year"].to_i, make: vehicle[:"make"], model: vehicle[:"model"], engine: :ev})
+      new_vehicle = Vehicle.new({
+        vin: vehicle[:"vin_1_10"], 
+        year: vehicle[:"model_year"].to_i, 
+        make: vehicle[:"make"], 
+        model: vehicle[:"model"], 
+        engine: :ev
+      })
       @vehicles << new_vehicle
     end
   end
