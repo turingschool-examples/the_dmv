36595bea3193214a5ae4e726245349019fbfb16e
Sarah Garlock <garlock.sarah10@gmail.com>
2023-04-01 19:18:23 -0600

    Start on registering a vehicle

--- lib/facility.rb
+++ lib/facility.rb
@@ -2,16 +2,28 @@ class Facility
   attr_reader :name, 
               :address, 
               :phone, 
-              :services
+              :services,
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(attributes)
     @name = attributes[:name]
     @address = attributes[:address]
     @phone = attributes[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
   end
+
+  def register_vehicle(vehicle)
+
+  end
+
+  # def collected_fees
+
+  # end
 end
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+    @facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+    @facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+
   end
   describe '#initialize' do
     it 'can initialize' do
@@ -11,6 +17,8 @@ RSpec.describe Facility do
       expect(@facility.address).to eq('2242 Santiam Hwy SE Albany OR 97321')
       expect(@facility.phone).to eq('541-967-2014')
       expect(@facility.services).to eq([])
+      expect(@facility.registered_vehicles).to eq([])
+      expect(@facility.collected_fees).to eq 0
     end
   end
 
@@ -21,6 +29,34 @@ RSpec.describe Facility do
       @facility.add_service('Renew Drivers License')
       @facility.add_service('Vehicle Registration')
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+    end
+  end
+
+  describe '#register vehicle' do
+    xit 'may offer vehicle registration' do
+      expect(@facility_1.add_service('Vehicle Registration')).to eq(["Vehicle Registration"])
+      expect(@cruz.registration_date).to eq(nil)
+      expect(@facility_1.registered_vehicles).to eq([])
+      expect(@facility_1.collected_fees).to eq(0)
+
+      @facility_1.add_service('Vehicle Registration')
+      @facility_1.register_vehicle(@cruz)
+
+      expect(@facility_1.registered_vehicles).to eq([@cruz])
+      expect(@cruz.registration_date).to eq(Date.today)
+      expect(@cruz.plate_type).to eq(:regular)
+      expect(@facility_1.registered_vehicles).to eq([cruz])
+      expect(@facility_1.collected_fees).to eq(100)
+
+      # expect(@facility_1.register_vehicle(camaro)).to eq([cruz, camaro])
+      # expect(@camaro.registration_date).to eq(Date.today)
+      # expect(@camaro.plate_type).to eq(:antique)
+      # expect(@facility_1.register_vehicle(bolt)).to eq([cruz, camaro, bolt])
+      # expect(@bolt.registration_date).to eq(Date.today)
+      # expect(@bolt.plate_type).to eq(:ev)
+      # expect(@facility_1.registered_vehicles).to eq([cruz, camaro, bolt])      
+      # expect(@facility_1.collected_fees).to eq(325)  
+
     end
   end
 end
