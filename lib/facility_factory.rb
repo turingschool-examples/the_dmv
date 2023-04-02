226b7399e1df5713ad2e15c4ce6a1468627e06c4
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-02 16:58:04 -0400

    feat- define create facility method

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,12 +1,21 @@
-class FacilityFactory
+require 'json'
 
-  def create_facilities(facilities)
-    until facilities.empty?
-      facilities.map do |facility|
-        require 'pry'; binding.pry
+class FacilityFactory
+  def initialize
+    @oregon_created_facilities = []
+  end
+
+  def create_facilities(data)
+    until data.empty?
+      data.map do |facility|
         new_facility = Facility.new(facility)
-        new_facility.name = facility[:title]
+         new_facility.name = facility[:title]
+         new_facility.address = JSON.parse(facility[:location_1][:human_address]).values.join(" ")
+         new_facility.phone = facility[:phone_number]
+        @oregon_created_facilities << new_facility
+        require 'pry'; binding.pry
       end
     end
+    @oregon_created_facilities
   end
 end
