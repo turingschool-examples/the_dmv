ca2be1b48323a981faf4b7294d0e237f259ee08b
Austin Woods <austinwoods99@gmail.com>
2023-04-02 12:26:37 -0400

    Add create facilities for two api data points

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -4,6 +4,7 @@ class FacilityFactory
   end
 
   def create_facilities(facilities)
+    if facilities[0].keys.include?(:location_1)
       facilities.map do |facility|
         location = JSON.parse(facility[:location_1][:human_address].gsub('\"', ''))
         facility_details = {
@@ -20,5 +21,17 @@ class FacilityFactory
         }
         Facility.new(facility_details)
       end
+   
+    elsif facilities[0].keys.include?(:office_name)
+      facilities.map do |facility|
+        facility_details = {
+          name: facility[:office_name],
+          address: "#{facility[:street_address_line_1]} #{facility[:city]} #{facility[:state]} #{facility[:zip_code]}",
+          phone: facility[:public_phone_number]
+        }
+        Facility.new(facility_details)
+      end
+      
+    end
   end
 end
