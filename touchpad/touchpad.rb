e6df5d47c3d7c99406d40ed13be977ab3f87f014
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-04-02 12:34:25 -0600

    add scrapwork

---
+++ touchpad/touchpad.rb
@@ -0,0 +1,64 @@
+#### COPY 1 BEGIN ---
+# class Facility
+#   attr_reader :name, 
+#               :address, 
+#               :phone, 
+#               :services, 
+#               :registered_vehicles,
+#               :collected_fees
+
+#   def initialize(facility)
+#     @name = facility[:name]
+#     @address = facility[:address]
+#     @phone = facility[:phone]
+#     @services = []
+#     @registered_vehicles = []
+#     @collected_fees = 0
+#   end
+
+#   def add_service(service)
+#     @services << service
+#     [service]
+#   end
+
+#   # def antique?
+#   #   if vehicle.registration_date - vehicle.year >= 25
+#   #     true
+#   #   else
+#   #     false
+#   #   end
+#   # end
+
+#   # def plate_type
+#   #   if vehicle.electric_vehicle?
+#   #     :ev 
+#   #   elsif vehicle.antique?
+#   #     :antique
+#   #   else
+#   #     :regular
+#   #   end
+#   # end
+
+
+
+#   def register_vehicle(vehicle)
+#     @registered_vehicles << vehicle
+#     vehicle.registration_date = Date.today
+#     # if vehicle.electric_vehicle?
+#     #   @collected_fees = 200
+#     # end
+
+#### COPY 1 END!!!
+
+
+
+    [vehicle]
+  end
+
+  
+  
+
+
+
+end
+
