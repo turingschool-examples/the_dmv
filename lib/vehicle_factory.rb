98504d6b85da8e755ac1f22a6eb4fa1acbfc542c
Jesse Thomas <jgthomas3@gmail.com>
2023-04-01 22:35:41 -0600

    Not sure what this is doing, says the file is empty but I gotta try and save it because there is work in it vehicle_factory.rb

---
+++ lib/vehicle_factory.rb
@@ -0,0 +1,17 @@
+class VehicleFactory
+  attr_reader :database, :new_vehicles
+
+  def initialize
+    @database = DmvDataService.new.wa_ev_registrations
+    @new_vehicles = []
+  end
+end
+
+def create_vehicles(database)
+new_car = Vehicle.new 
+require 'pry'; binding.pry
+end
+
+# I need to call on the database key/value pairs 
+# to build vehicles from that information. 
+# I need to instantiate an empty array? 
