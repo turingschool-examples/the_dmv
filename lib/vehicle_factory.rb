174beb92327b15dd6aceb43597e4fc41852f0311
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-01 22:26:40 -0600

    Save progress on end of iteration 2 with pseudocode for data retrieval

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,5 +1,22 @@
 class VehicleFactory
+  attr_reader :vehicles
   def initialize
-  
+    @vehicles = []
+  end
+
+  def create_vehicles(vehicles)
+  #  vehicles.map do |vehicle|
+  #   vehicle.keys.uniq
+  #  end
+  #  vehicles << veh
+  ## Lightbulb
+  # each do
+  #   first, change certain keys to correct keys matching vehicle class
+  #   v1 = Vehicle.new(wa_ev_registrations[0]), pass in default value of engine :ev
+  #   add engine type to be ev, bonus if you can make it so that it does so based on whether the data passed in includes ev
+  # end
+
+      # Vehicle.new
+      # vehicle[:make] = vehicle_details[:make]
   end
 end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe VehicleFactory do
 
     expect(factory).to be_an_instance_of(VehicleFactory)
   end
+  
+  it "can create vehicles" do
+    factory = VehicleFactory.new
+    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+    require 'pry'; binding.pry
+    factory.create_vehicles(wa_ev_registrations)
+    require 'pry'; binding.pry
+    expect(factory).to be_an_instance_of(VehicleFactory)
+  end
+
+
 end
