e5d3754052e1c7b210306c5265342ec83432f586
Matthew Lim <matlim200@gmail.com>
2023-04-01 21:08:26 -0600

    add remainder of test for the written test feature

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -51,7 +51,17 @@ RSpec.describe Registrant do
     expect(facility_1.administer_written_test(registrant_1)).to eq (true)
     expect(registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
     expect(registrant_2.age).to eq(16)
-    
+    expect(registrant_2.permit?).to eq(false)
+    expect(facility_1.administer_written_test(registrant_2)).to eq(false)
+    registrant_2.earn_permit
+    expect(facility_1.administer_written_test(registrant_2)).to eq(true)
+    expect(registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+    expect(registrant_3.age).to eq(15)
+    expect(registrant_3.permit?).to eq(false)
+    expect(facility_1.administer_written_test(registrant_3)).to eq(false)
+    registrant_3.earn_permit
+    expect(facility_1.administer_written_test(registrant_3)).to eq(false)
+    expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
   end
 
 end
