9e4130f5cbbbf549055676acac47432affa60f28
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-01 20:53:13 -0600

    Verify facility can renew license for multiple people

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -414,6 +414,33 @@ RSpec.describe Facility do
         }
         expect(registrant_3.license_data).to eq(r3_data)
       end
+
+      it "can renew test for multiple people" do
+        registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 16 )
+        registrant_3 = Registrant.new('Tucker', 15 )
+        facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+        facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+        facility_1.add_service("Written Test")
+        facility_1.administer_written_test(registrant_1)
+        registrant_2.earn_permit
+        facility_1.administer_written_test(registrant_2)
+        registrant_3.earn_permit
+        facility_1.administer_written_test(registrant_3)
+        facility_1.add_service("Road Test")
+        facility_1.administer_road_test(registrant_1)
+        facility_1.administer_road_test(registrant_2)
+        facility_1.add_service("Renew License")
+        facility_1.renew_drivers_license(registrant_1)
+        facility_1.renew_drivers_license(registrant_3)
+        facility_1.renew_drivers_license(registrant_2)
+        r2_data = {
+          :written  =>  true,
+          :license  =>  true,
+          :renewed  =>  true
+        }
+        expect(registrant_2.license_data).to eq(r2_data)
+      end
     end
   end
 end
