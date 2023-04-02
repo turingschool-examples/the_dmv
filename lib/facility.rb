9781bf4a0e1e0a00ba9d9055f3f9842a11240bb1
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-04-02 12:57:10 -0600

    add code to reassign value of #@collected_fees

--- lib/facility.rb
+++ lib/facility.rb
@@ -20,10 +20,18 @@ class Facility
     [service]
   end
 
+
   def register_vehicle(vehicle)
     @registered_vehicles << vehicle
     vehicle.registration_date = Date.today
-    [vehicle]
+    if vehicle.plate_type == :ev
+      @collected_fees += 200
+    elsif vehicle.plate_type == :antique
+      @collected_fees += 25
+    else
+      @collected_fees += 100
+    end
+    @registered_vehicles
   end
 
   
