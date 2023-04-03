ffb59ea3fe77731db0626fdec6813b46a6e8def2
Andy Weissman <andyweissman6@gmail.com>
2023-04-03 15:56:28 -0600

    add tests for written and road tests

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -91,28 +91,83 @@ RSpec.describe Facility do
 
       expect(facility_2.services).to eq([])
       expect(facility_2.register_vehicle(@bolt)).to eq(nil)
-      # require 'pry'; binding.pry
       expect(facility_2.registered_vehicles).to eq([])
       expect(facility_2.collected_fees).to eq(0)
-
-
-
-
     end
   end
-  # describe '#getting a drivers license' do
-  #   it "tests for administering a written test @ facility_1" do
-  #     facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
-  #     facility_1.add_services('Written Test')
 
-  #     expect
+  describe '#taking Written Test' do
+    it "tests for administering a written test for registrant_1 @ facility_1" do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 16 )
+      registrant_3 = Registrant.new('Tucker', 15 )
 
-  #   end
+    
+      expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(registrant_1.permit?).to eq(true)
+      expect(facility_1.administer_written_test(registrant_1)).to eq(false)
+      
+      facility_1.add_services('Written Test')
 
+      expect(facility_1.administer_written_test(registrant_1)).to eq(true)
+      expect(registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+    end
 
-   
+    it "tests for administering a written test for registrant_2 @ facility_1" do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 16 )
+      registrant_3 = Registrant.new('Tucker', 15 )
+      facility_1.add_services('Written Test')
 
+      expect(facility_1.administer_written_test(registrant_2)).to eq(false)
+      registrant_2.earn_permit
+      expect(facility_1.administer_written_test(registrant_2)).to eq(true)
+      expect(registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+    end
+    
+    it "tests for administering a written test for registrant_3 @ facility_1" do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 16 )
+      registrant_3 = Registrant.new('Tucker', 15 )
+      facility_1.add_services('Written Test')
+    
+      expect(facility_1.administer_written_test(registrant_3)).to eq(false)
+      registrant_3.earn_permit
+      expect(facility_1.administer_written_test(registrant_3)).to eq(false)
+      expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+  end
 
+  describe '#taking Road Test' do
+# A road test can only be administered to registrants who have passed the written test
+# For simplicity’s sake, Registrants who qualify for the road test automatically earn a license
+    it "tests for administering a road test for @ facility_1" do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 16 )
+      registrant_3 = Registrant.new('Tucker', 15 )
+      facility_1.add_services('Written Test')
 
-  # end
+      expect(facility_1.administer_road_test(registrant_3)).to eq(false)
+      expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(facility_1.administer_road_test(registrant_1)).to eq(false)
+      expect(facility_1.add_services('Road Test')).to eq(["Written Test", "Road Test"])
+
+      expect(facility_1.administer_road_test(registrant_1)).to eq(true)
+      expect(registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
+
+      expect( facility_1.administer_road_test(registrant_2)).to eq(true)
+      expect(registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
+      
+      ### go back and make these tests per individual
+    end
+  end
+ 
 end
