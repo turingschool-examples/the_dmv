dd6954273199426df90d2caed983f7d489dccb4e
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-04-03 15:34:08 -0600

    create './spec/vehicle_factory.rb'

---
+++ spec/vehicle_factory_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+RSpec.describe VehicleFactory do
+
+  it 'exists' do 
+    factory = VehicleFactory.new
+
+    expect(factory).to be_a(VehicleFactory)
+  end
+
+  it 'creates new vehicles for washington' do 
+    factory = VehicleFactory.new
+    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+    vehicle = factory.create_vehicles(wa_ev_registrations)
+   
+    expect(factory.create_vehicles(wa_ev_registrations)).to eq(vehicle)
+    expect(vehicle[1].year).to eq("2014")
+  end
+
+end
