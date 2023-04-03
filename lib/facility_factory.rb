f2833339d44b0ec03a3bd23483e291bca4f253bc
Justin Pasko <jpasko1112@gmail.com>
2023-04-03 16:56:21 -0400

    Add methods for OR and NY facilities.

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -2,13 +2,28 @@ class FacilityFactory
 
   def initialize; end
 
-  def create_facility(facilities)
+  def create_facility_or(facilities)
     facilities.map do |facility|
+      eval_address = eval(facility[:location_1][:human_address])
       Facility.new({
-                name: "Albany DMV Office",
-                address: ,
-                phone: "541-967-2014"
-      })
+                name: facility[:title],
+                address: eval_address.fetch_values(:address, :city, :state, :zip).join(" "),
+                phone: facility[:phone_number]
+               })
+    end
+  end
+
+  def create_facility_ny(facilities)
+    facilities.map do |facility|
+      require 'pry'; binding.pry
+      eval_address = eval(facility[:location_1][:human_address])
+      Facility.new({
+                name: facility[:office_name],
+                address: eval_address.fetch_values(:address, :city, :state, :zip).join(" "),
+                phone: facility[:phone_number]
+               })
     end
   end
 end
+
+# ny_address = eval(facility["street_address_line_1":])
