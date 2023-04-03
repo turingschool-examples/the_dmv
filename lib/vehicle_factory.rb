deabfcb1decad4cb2769c91875ec73830f9a8345
Jesse Thomas <jgthomas3@gmail.com>
2023-04-02 20:04:03 -0600

    Refactor create_vehicles method, delete initialize, clean up comments vehicle_factory.rb

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,42 +1,14 @@
 class VehicleFactory
-  attr_reader :database, 
-              :new_vehicles, 
-              :city
 
-  def initialize
-    # @city = city
-    # @database = DmvDataService.new.wa_ev_registrations
-    @new_vehicles = []
+  def create_vehicles(database)
+    database.map do |data|
+      vehicle = Vehicle.new({
+        vin: data[:vin_1_10], 
+        year: data[:model_year], 
+        make: data[:make], 
+        model: data[:model], 
+        engine: :ev
+      })
+    end
   end
-  
-  def create_vehicles(database = nil)
-    # require 'pry'; binding.pry
-    vehicle_details = {
-      vin: nil, 
-      year: nil , 
-      make: nil, 
-      model: nil, 
-      engine: :ev
-    }
-    empty_new_car = Vehicle.new(vehicle_details)
-    return @new_vehicles << empty_new_car unless database.class == Array
-    vehicle_details[:make] = database[0][:make]
-    vehicle_details[:model] = database[0][:model]
-    vehicle_details[:vin] = database[0][:vin_1_10]
-    vehicle_details[:year] = database[0][:model_year]
-    full_new_car = Vehicle.new(vehicle_details)
-    @new_vehicles << full_new_car
-    # require 'pry'; binding.pry
-  end
-  
- 
-  # I need to call on the database key/value pairs 
-  # to build vehicles from that information. 
-  # I need to instantiate an empty array? 
-  
-  
-  #could initialize it with the city as a parameter
-  # and then have the database be assgined to a method
-  # and the method could include? the city name and match 
-  # the database to that?
 end
