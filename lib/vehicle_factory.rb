8a2407402cbb109f78d1c69275f94a88b4ff0f0d
Justin Pasko <jpasko1112@gmail.com>
2023-04-02 21:38:58 -0400

    Add create_vehicles method.

---
+++ lib/vehicle_factory.rb
@@ -0,0 +1,14 @@
+class VehicleFactory
+
+  def create_vehicles(database)
+      database.map do |vehicle|
+        Vehicle.new({
+                    vin: vehicle[:vin_1_10],
+                    year: vehicle[:model_year].to_i,
+                    make: vehicle[:make],
+                    model: vehicle[:model],
+                    engine: :ev
+                 })
+    end
+  end
+end
