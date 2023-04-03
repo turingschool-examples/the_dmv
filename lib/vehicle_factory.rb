f928fa0d8f217b46c2c5414de3b2df72d59872f9
Justin Pasko <jpasko1112@gmail.com>
2023-04-03 10:25:17 -0400

    Add initialize test.

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,14 +1,16 @@
 class VehicleFactory
 
+  def initialize; end
+
   def create_vehicles(database)
-      database.map do |vehicle|
-        Vehicle.new({
-                    vin: vehicle[:vin_1_10],
-                    year: vehicle[:model_year].to_i,
-                    make: vehicle[:make],
-                    model: vehicle[:model],
-                    engine: :ev
-                 })
+    database.map do |vehicle|
+      Vehicle.new({
+                  vin: vehicle[:vin_1_10],
+                  year: vehicle[:model_year].to_i,
+                  make: vehicle[:make],
+                  model: vehicle[:model],
+                  engine: :ev
+                })
     end
   end
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 RSpec.describe FacilityFactory do
+  before(:each) do
+    @facility = FacilityFactory.new
+    @or_facilities = DmvDataService.new.or_dmv_office_locations
+    @ny_facilities = DmvDataService.new.ny_dmv_office_locations
+    @mo_facilities = DmvDataService.new.mo_dmv_office_locations
+  end
 
+  describe "initialize" do
+    it "can itialize" do
+      expect(@facility).to be_an_instance_of(FacilityFactory)
+    end
+  end
 end
