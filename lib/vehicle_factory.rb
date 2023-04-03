188dfdf24ad2337b5e85d28fc5884c9b4e1749e0
Alex Kiselich <alexander.kiselich@gmail.com>
2023-04-03 17:28:36 -0600

    Add a Vehicle Factory class

---
+++ lib/vehicle_factory.rb
@@ -0,0 +1,23 @@
+class VehicleFactory
+  attr_reader :vehicles
+  def initialize
+    @vehicles = []
+  end
+
+  def create_vehicles(state_registrations)
+    state_registrations.map do |vehicle|
+      vehicle_details = {
+        vin: vehicle[:vin_1_10],
+        year: vehicle[:model_year],
+        make: vehicle[:make],
+        model: vehicle[:model],
+        engine: :ev,
+        registration_date: nil
+      }
+      
+      @vehicles << vehicle = Vehicle.new(vehicle_details)
+    end
+  end
+
+
+end
