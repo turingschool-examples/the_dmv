463b426313168b83df72a2b2ea533707a51fb413
Nick Tassinari <ntass7800@gmail.com>
2023-04-03 14:21:25 -0600

    add new york tests

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -36,6 +36,28 @@ RSpec.describe FacilityFactory do
     expect(@factory.determine_phone_num(@oregon_facilities.first)).to eq("541-967-2014")
   end
 
+  it 'can create facility from New York database' do 
+    @factory = FacilityFactory.new
+    @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+
+    expect(@factory.facilities).to eq([])
+    @factory.create_facilities(@new_york_facilities)
+    expect(@factory.facilities.first).to be_a(Facility)
+    expect(@factory.facilities.first.name).to eq("SELDEN")
+    expect(@factory.facilities.first.address).to eq("407 SELDEN RD")
+    
+    #the first entry in the database has no number
+    #so I'm running the second location for this test
+    expect(@factory.facilities[1].phone).to eq("718-526-8546")
+
+  end
+
+  it 'can create facility from Missouri database' do 
+    @factory = FacilityFactory.new
+    @missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+
+  end
+
 
 
 
