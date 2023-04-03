8f04a9a3fbcde7f26dff6fff83ccffecf509b963
Nick Tassinari <ntass7800@gmail.com>
2023-04-03 14:42:18 -0600

    add tests for more locations

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -1,13 +1,6 @@
 require "spec_helper"
 
 RSpec.describe FacilityFactory do
-  
-
-    # @factory = FacilityFactory.new
-    # @oregon_facilities = DmvDataService.new.or_dmv_office_locations
-    # @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
-    # @missouri_facilities = DmvDataService.new.mo_dmv_office_locations
-
    
   it 'exists' do 
     @factory = FacilityFactory.new
@@ -56,11 +49,52 @@ RSpec.describe FacilityFactory do
     @factory = FacilityFactory.new
     @missouri_facilities = DmvDataService.new.mo_dmv_office_locations
 
+    expect(@factory.facilities).to eq([])
+    @factory.create_facilities(@missouri_facilities)
+    expect(@factory.facilities.first).to be_a(Facility)
+    expect(@factory.facilities.first.name).to eq("OAKVILLE")
+    expect(@factory.facilities.first.address).to eq("3164 TELEGRAPH ROAD")
+    expect(@factory.facilities.first.phone).to eq("314-887-1050")
   end
 
+  #I'll now make sure everything is running properly by testing 
+  #other locations in each database 
 
+  it 'can test facility 3 in Oregon' do 
+    @factory = FacilityFactory.new
+    @oregon_facilities = DmvDataService.new.or_dmv_office_locations
 
+    expect(@factory.facilities).to eq([])
+    @factory.create_facilities(@oregon_facilities)
+    expect(@factory.facilities[2]).to be_a(Facility)
+    expect(@factory.facilities[2].name).to eq("Astoria DMV Office")
+    expect(@factory.facilities[2].address).to eq("705 W Marine Dr")
+    expect(@factory.facilities[2].phone).to eq("503-325-3951")
+  end 
 
+  it 'can test facility 3 in New York' do 
+    @factory = FacilityFactory.new
+    @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+
+    expect(@factory.facilities).to eq([])
+    @factory.create_facilities(@new_york_facilities)
+    expect(@factory.facilities[2]).to be_a(Facility)
+    expect(@factory.facilities[2].name).to eq("SARANAC LAKE")
+    expect(@factory.facilities[2].address).to eq("39 MAIN STREET")
+    expect(@factory.facilities[2].phone).to eq("518-891-1748")
+  end 
+
+  it 'can test facility 3 in Missouri' do 
+    @factory = FacilityFactory.new
+    @missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+
+    expect(@factory.facilities).to eq([])
+    @factory.create_facilities(@missouri_facilities)
+    expect(@factory.facilities[2]).to be_a(Facility)
+    expect(@factory.facilities[2].name).to eq("LICKING (closed)")
+    expect(@factory.facilities[2].address).to eq("117A NORTH MAIN")
+    expect(@factory.facilities[2].phone).to eq(nil)
+  end
 
 
 
