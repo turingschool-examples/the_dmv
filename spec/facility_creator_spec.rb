7e80ebb8fe0bb20a282d7e183d8e134c0ee78a96
Michael Callahan <mcalla242@gmail.com>
2023-04-03 14:59:26 -0600

    Refactor testing specs for facility creator

--- spec/facility_creator_spec.rb
+++ spec/facility_creator_spec.rb
@@ -1,34 +1,42 @@
 require 'spec_helper'
 
 RSpec.describe FacilityCreator do
-  describe "#import facilities" do
-    it "imports facilities for Oregon from DMV API" do
-      creator = FacilityCreator.new
-      or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
+  before(:each) do
+    @creator = FacilityCreator.new
+    @or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
+    @ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
+    @mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
+  end
 
-      creator.create_facilities(or_dmv_office_locations)
-      expect(creator.facilities).to be_an_instance_of(Array)
-      expect(creator.facilities.first).to be_an_instance_of(Facility)
+  describe "#initializes" do
+    it "initializes" do
+      expect(@creator).to be_an_instance_of(FacilityCreator)
+    end
+  end
+
+  describe "#create_facilities" do
+    it "creates facilities for Oregon from DMV API" do
+      @creator.create_facilities(@or_dmv_office_locations)
+      expect(@creator.facilities).to be_an_instance_of(Array)
+      expect(@creator.facilities.first).to be_an_instance_of(Facility)
     end
 
-    it "imports facilities for New York from DMV API" do
-      creator = FacilityCreator.new
-      ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
-
-      creator.create_facilities(ny_dmv_office_locations)
-      expect(creator.facilities).to be_an_instance_of(Array)
-      expect(creator.facilities.first).to be_an_instance_of(Facility)
+    it "creates facilities for New York from DMV API" do
+      @creator.create_facilities(@ny_dmv_office_locations)
+      expect(@creator.facilities).to be_an_instance_of(Array)
+      expect(@creator.facilities.first).to be_an_instance_of(Facility)
     end
 
-    it "imports facilities for Missouri from DMV API" do
-      creator = FacilityCreator.new
-      mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
-
-      creator.create_facilities(mo_dmv_office_locations)
-      expect(creator.facilities).to be_an_instance_of(Array)
-      expect(creator.facilities.first).to be_an_instance_of(Facility)
+    it "creates facilities for Missouri from DMV API" do
+      @creator.create_facilities(@mo_dmv_office_locations)
+      expect(@creator.facilities).to be_an_instance_of(Array)
+      expect(@creator.facilities.first).to be_an_instance_of(Facility)
     end
+  end
+end
 
+RSpec.describe FacilityCreator do
+  describe "stores different data depending on the state" do
     it "stores operating hours data" do
       or_creator = FacilityCreator.new
       ny_creator = FacilityCreator.new
