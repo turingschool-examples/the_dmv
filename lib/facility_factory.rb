18e5b9a0ac38095e2c58e3dc63923f7ede812177
Justin Pasko <jpasko1112@gmail.com>
2023-04-03 11:01:25 -0400

    Add creat_facility method.

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,4 +1,14 @@
 class FacilityFactory
 
   def initialize; end
+
+  def create_facility(facilities)
+    facilities.map do |facility|
+      Facility.new({
+                name: "Albany DMV Office",
+                address: ,
+                phone: "541-967-2014"
+      })
+    end
+  end
 end
