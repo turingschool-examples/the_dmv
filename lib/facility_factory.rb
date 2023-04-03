6299d99f59ffec91e571ecd43424e568edc5c619
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-03 17:03:28 -0400

    test: format ny address and name

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -4,7 +4,7 @@ class FacilityFactory
   def create_facilities(data, state)
     if state == :OR
      oregon_processing(data)
-    elsif data == :NY
+    elsif state == :NY
      new_york_processing(data)
     end
   end
@@ -17,5 +17,24 @@ class FacilityFactory
        phone: facility[:phone_number]
                   })
     end
+  end
+
+  def new_york_processing(data)
+    data.map do |facility|
+      require 'pry'; binding.pry
+      Facility.new({ 
+        name: NY_format_name(facility),
+        address: NY_format_address(facility)
+                  })
+    end
+  end
+
+  def NY_format_name(facility)
+    facility[:office_name].capitalize + " DMV Office"
+  end
+
+  def NY_format_address(facility)
+    # facility[:street_address_line_1].capitalize + " " + facility[:city].capitalize + " " + facility[:state] + " " + facility[:zip_code]
+    "#{facility[:street_address_line_1].capitalize} #{facility[:city].capitalize} #{facility[:state]} #{facility[:zip_code]}"
   end
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe FacilityFactory do
   before(:each) do
     @factory = FacilityFactory.new
-    # @oregon_facilities = DmvDataService.new.or_dmv_office_locations
+    @oregon_facilities = DmvDataService.new.or_dmv_office_locations
     @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
   end
 
@@ -13,13 +13,15 @@ RSpec.describe FacilityFactory do
     end
   end
 
+  before(:each) do
+    @oregon = @factory.create_facilities(@oregon_facilities, :OR)
+    @new_york = @factory.create_facilities(@new_york_facilities, :NY)
+  end
+ 
   describe 'creates facilities' do
     it 'can create facility objects' do
-      # @oregon = @factory.create_facilities(@oregon_facilities, :OR)
-      @new_york = @factory.create_facilities(@new_york_facilities, :NY)
-      
-      # expect(@oregon).to be_an(Array)
-      # expect(@oregon.first).to be_a(Facility)
+      expect(@oregon).to be_an(Array)
+      expect(@oregon.first).to be_a(Facility)
       expect(@new_york).to be_an(Array)
       expect(@new_york.first).to be_a(Facility)
     end
