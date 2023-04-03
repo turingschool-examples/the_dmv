0b711e811e25ce2cdeae6680e0f053a64c5bc50d
Lauren Golden <llgolden@gmail.com>
2023-04-03 16:56:02 -0600

    refactor: updates after JB feedback

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -8,14 +8,25 @@ describe Registrant do
     expect(registrant_1).to be_an_instance_of(Registrant)
     expect(registrant_1.name).to eq("Bruce")
     expect(registrant_1.age).to eq(18)
+  end 
 
-    it "can check "
+  it "can check permit status" do
+    registrant_1 = Registrant.new("Bruce", 18, true )
+    registrant_2 = Registrant.new("Penny", 15 )
+
     expect(registrant_1.permit?).to eq(true)
     expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     expect(registrant_2.name).to eq("Penny")
     expect(registrant_2.age).to eq(15)
     expect(registrant_2.permit?).to eq(false)
     expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+  end
+
+  it "can change permit status" do
+    registrant_1 = Registrant.new("Bruce", 18, true )
+    registrant_2 = Registrant.new("Penny", 15 )
+
+    expect(registrant_2.permit?).to eq(false)
     registrant_2.earn_permit
     expect(registrant_2.permit?).to eq(true)
   end 
