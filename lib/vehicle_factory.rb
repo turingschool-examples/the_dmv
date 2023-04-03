bd58884f0542aad49c139eed0cb4bc5b6c7ada82
Blaine Glasgow <glasgowb@outlook.com>
2023-04-02 19:20:21 -0700

    Make create vehicle method in vehicle factory

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,4 +1,14 @@
 class VehicleFactory
-  
+
+  attr_reader :vehicles
+    def initialize
+      @vehicles_list = []
+    end
+
+    def create_vehicles(vehicles)
+      vehicles.each do |vehicle|
+        @vehicles_list << Vehicle.new(vehicle)
+      end
+    end
 
 end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -10,32 +10,19 @@ RSpec.describe VehicleFactory do
 
   it 'Can register Vehicles' do
     factory = VehicleFactory.new
-  # wa_ev_registrations = DmvDataService.new.wa_ev_registrations
-  #  [{:electric_vehicle_type=>"Plug-in Hybrid Electric Vehicle (PHEV)",
-  #    :vin_1_10=>"JTDKN3DP8D",
-  #    :dol_vehicle_id=>"229686908",
-  #    :model_year=>"2013",
-  #    :make=>"TOYOTA",
-  #    :model=>"Prius Plug-in",
-  #    ...},
-  #    ...,
-  #    {:electric_vehicle_type=>"Plug-in Hybrid Electric Vehicle (PHEV)",
-  #     :vin_1_10=>"1G1RD6E47D",
-  #     :dol_vehicle_id=>"289314742",
-  #     :model_year=>"2013",
-  #     :make=>"CHEVROLET",
-  #     :model=>"Volt",
-  #     ...}]
+    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+
+    factory.create_vehicles(wa_ev_registrations)
+      #=> [#<Vehicle:0x000000012d3812f0 @engine=:ev, @make="TOYOTA", @model="Prius Plug-in", @plate_type=nil, @registration_date=nil, @vin="JTDKN3DP8D", @year="2013">,
+      #<Vehicle:0x000000012d3812a0 @engine=:ev, @make="TOYOTA", @model="Prius Prime", @plate_type=nil, @registration_date=nil, @vin="JTDKARFP9J", @year="2018">,
+      #<Vehicle:0x000000012d381200 @engine=:ev, @make="NISSAN", @model="Leaf", @plate_type=nil, @registration_date=nil, @vin="1N4AZ1CP0J", @year="2018">,
+      #<Vehicle:0x000000012d381188 @engine=:ev, @make="NISSAN", @model="Leaf", @plate_type=nil, @registration_date=nil, @vin="1N4AZ1CP0J", @year="2018">,
+      #<Vehicle:0x000000012d381138 @engine=:ev, @make="NISSAN", @model="Leaf", @plate_type=nil, @registration_date=nil, @vin="1N4AZ1CP0J", @year="2018">,
+      # ...]
+  end
 end
 
 
 
 
-# factory.create_vehicles(wa_ev_registrations)
-  #=> [#<Vehicle:0x000000012d3812f0 @engine=:ev, @make="TOYOTA", @model="Prius Plug-in", @plate_type=nil, @registration_date=nil, @vin="JTDKN3DP8D", @year="2013">,
-  #<Vehicle:0x000000012d3812a0 @engine=:ev, @make="TOYOTA", @model="Prius Prime", @plate_type=nil, @registration_date=nil, @vin="JTDKARFP9J", @year="2018">,
-  #<Vehicle:0x000000012d381200 @engine=:ev, @make="NISSAN", @model="Leaf", @plate_type=nil, @registration_date=nil, @vin="1N4AZ1CP0J", @year="2018">,
-  #<Vehicle:0x000000012d381188 @engine=:ev, @make="NISSAN", @model="Leaf", @plate_type=nil, @registration_date=nil, @vin="1N4AZ1CP0J", @year="2018">,
-  #<Vehicle:0x000000012d381138 @engine=:ev, @make="NISSAN", @model="Leaf", @plate_type=nil, @registration_date=nil, @vin="1N4AZ1CP0J", @year="2018">,
-  # ...]
 
