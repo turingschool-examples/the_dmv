b15b9dbce7a4769fbbec5eda02dd83c2b441dc09
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-03 18:04:55 -0400

    add tests and helper methods for dry code

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -12,16 +12,27 @@ class FacilityFactory
   def oregon_processing(data)
     data.map do |facility|
       Facility.new({
-       name: facility[:title],
-       address: JSON.parse(facility[:location_1][:human_address]).values.join(" "),
-       phone: facility[:phone_number]
+       name: OR_format_name(facility),
+       address: OR_format_address(facility),
+       phone: OR_format_phone_number(facility)
                   })
     end
   end
 
+  def OR_format_name(facility)
+    facility[:title]
+  end
+
+  def OR_format_address(facility)
+    JSON.parse(facility[:location_1][:human_address]).values.join(" ")
+  end
+
+  def OR_format_phone_number(facility)
+    facility[:phone_number]
+  end
+
   def new_york_processing(data)
     data.map do |facility|
-      require 'pry'; binding.pry
       Facility.new({ 
         name: NY_format_name(facility),
         address: NY_format_address(facility), 
@@ -39,10 +50,7 @@ class FacilityFactory
   end
 
   def NY_format_phone(facility)
-    if facility[:public_phone_number].nil?
-      return "no phone number available" 
-    else
-      facility[:public_phone_number].insert(3,"-").insert(-5, "-")
-    end
+    return nil if facility[:public_phone_number].nil?
+    facility[:public_phone_number].insert(3,"-").insert(-5, "-")
   end
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -26,4 +26,32 @@ RSpec.describe FacilityFactory do
       expect(@new_york.first).to be_a(Facility)
     end
   end
+
+  describe 'formats Oregon facility info' do
+    it 'formats :OR facility name' do
+      expect(@oregon.first.name).to eq("Albany DMV Office")
+    end
+
+    it 'formats :OR facility phone number' do
+      expect(@oregon.first.phone).to eq("541-967-2014")
+    end
+
+    it 'formats :OR facility address' do
+      expect(@oregon.first.address).to eq("2242 Santiam Hwy SE Albany OR 97321")
+    end
+  end
+
+  describe 'formats New York facility info' do
+    it 'formats :NY facility name' do
+      expect(@new_york.first.name).to eq("Selden DMV Office")
+    end
+
+    it 'formats :NY facility phone number' do
+      expect(@new_york.first.phone).to eq(nil)
+    end
+
+    it 'formats :NY facility address' do
+      expect(@new_york.first.address).to eq("407 SELDEN RD Selden NY 11784")
+    end
+  end
 end
