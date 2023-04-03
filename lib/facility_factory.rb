16ecc030ff5dcc86a1ed21880c60a4bc892a109e
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-03 16:19:22 -0400

    refactor create facilities argument to pass in successfully

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,7 +1,15 @@
 require 'json'
 
 class FacilityFactory
-   def create_facilities(data)
+  def create_facilities(data, state)
+    if state == :OR
+     oregon_processing(data)
+    elsif data == :NY
+     new_york_processing(data)
+    end
+  end
+
+  def oregon_processing(data)
     data.map do |facility|
       Facility.new({
        name: facility[:title],
