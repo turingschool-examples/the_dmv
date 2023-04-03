6c12e4aaa81ee76dda256ce2b4148382ce27e52d
Andy Weissman <andyweissman6@gmail.com>
2023-04-03 14:55:27 -0600

    add tests to make facility_2 not be able to register

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
+require './lib/registrant'
 require './lib/vehicle'
+require './lib/facility'
+
+
 RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
@@ -34,10 +38,12 @@ RSpec.describe Facility do
       
       expect(facility_1.services).to eq(['Vehicle Registration'])
     end
-    
-  describe '#register vehicles' do
+  end
+
+  describe '#vehicle registration' do
     it "registers vehicle @ facility_1" do
       facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_1.add_services('Vehicle Registration')
       
       facility_1.register_vehicle(@cruz)
       expect(@cruz.registered?).to eq(true)
@@ -49,14 +55,64 @@ RSpec.describe Facility do
       expect(@camaro.registration_date).to eq(Date.today)
       expect(@camaro.plate_type).to eq(:antique)
 
-      facility_1.register_vehicle(@cruz)
+      facility_1.register_vehicle(@bolt)
       expect(@bolt.registered?).to eq(true)
       expect(@bolt.registration_date).to eq(Date.today)
       expect(@bolt.plate_type).to eq(:ev)
     end
+
+    it "tests we can add services to facility_1" do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      
+      expect(facility_1.add_services('Vehicle Registration')).to eq(['Vehicle Registration'])
+    end
+
+    it "tests for total collected fees after registering @ facility_1" do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_1.add_services('Vehicle Registration')
+     
+      facility_1.register_vehicle(@cruz)
+      expect(facility_1.collected_fees).to eq(100)
+
+      facility_1.register_vehicle(@camaro)
+      expect(facility_1.collected_fees).to eq(125)
+
+      facility_1.register_vehicle(@bolt)
+      expect(facility_1.collected_fees).to eq(325)
+    end
+
+    it "tests that facility_2 cannot register vehicles" do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      facility_1.add_services('Vehicle Registration')
+      facility_1.register_vehicle(@cruz)
+      facility_1.register_vehicle(@camaro)
+      facility_1.register_vehicle(@bolt)
+
+      expect(facility_2.services).to eq([])
+      expect(facility_2.register_vehicle(@bolt)).to eq(nil)
+      # require 'pry'; binding.pry
+      expect(facility_2.registered_vehicles).to eq([])
+      expect(facility_2.collected_fees).to eq(0)
+
+
+
+
+    end
+  end
+  # describe '#getting a drivers license' do
+  #   it "tests for administering a written test @ facility_1" do
+  #     facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+  #     facility_1.add_services('Written Test')
+
+  #     expect
+
+  #   end
+
+
    
 
 
 
-  end
+  # end
 end
