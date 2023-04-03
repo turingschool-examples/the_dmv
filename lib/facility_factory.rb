11f3b91d676e47676ab3a4a8d52b6f62a1ab2a90
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-03 15:10:36 -0400

    final commit of OR facility data factory

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -4,7 +4,6 @@ class FacilityFactory
    def create_facilities(data)
     data.map do |facility|
       Facility.new({
-      require 'pry'; binding.pry
        name: facility[:title],
        address: JSON.parse(facility[:location_1][:human_address]).values.join(" "),
        phone: facility[:phone_number]
