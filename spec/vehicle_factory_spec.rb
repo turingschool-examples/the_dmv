eea15e4fed8bbfd3593bea0d55f1c397d34abc5e
Michael Callahan <mcalla242@gmail.com>
2023-04-03 06:55:09 -0600

    Make changes to break down original testing specs

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,15 +1,26 @@
 require 'spec_helper'
 
 RSpec.describe VehicleFactory do
-  describe "#initialize" do
-    it "initializes" do
+  before(:each) do
       factory = VehicleFactory.new
       wa_ev_registrations = DmvDataService.new.wa_ev_registrations
-
-      factory.create_vehicles(wa_ev_registrations)
-      
+  end
+  describe "#initialize" do
+    it "initializes" do
       expect(factory.vehicles).to be_an_instance_of(Array)
+    end
+  end
+  
+  describe "#create_vehicles" do
+    it "creates_vehicles" do
+      expect(factory.create_vehicles(wa_ev_registrations)).to be_an_instance_of(Array)
       expect(factory.vehicles.first).to be_an_instance_of(Vehicle)
+    end
+  end
+
+  describe "#most_popular_makes_model" do
+    it "can determine the most registered vehicle" do
+      
     end
   end
 end
