0f6b77f408ace0510caaf51de85b03fbe47b23a5
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-03 15:32:14 -0600

    Remove notes and pseudocode from vehicle factory to conform to convention

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -3,21 +3,6 @@ class VehicleFactory
     
   end
 
-  # def create_vehicles(vehicles)
-  #   arr = vehicles.each do |vehicle|
-  #     vehicle[:year] = vehicle.delete(:model_year)  
-  #     vehicle[:vin] = vehicle.delete(:vin_1_10)  
-  #   end
-  #   vehicles.replace(arr)
-  #   arr1 = []
-  #   manufactured = vehicles.each do |created|
-  #     created = Vehicle.new(vehicles)
-  #     created.engine = :ev
-  #     arr1 << created
-  #   end
-  #   return arr1
-  # end
-
   def create_vehicles(registrations)
     created_vehicles = registrations.map do |car|
       Vehicle.new(
@@ -31,25 +16,5 @@ class VehicleFactory
       )
     end
     created_vehicles
-    ## Second Iteration, but was modifying the original arrray with delete
-    # created_vehicles = registrations.map do |registration|
-    #   registration[:year] = registration.delete(:model_year)  
-    #   registration[:vin] = registration.delete(:vin_1_10)
-    #   registration[:engine] = :ev
-    #   Vehicle.new(registration)
-    # end
-    # created_vehicles
-    ## Earlier Iteration
-    # registrations.map do |registration|
-    #   registration[:year] = registration.delete(:model_year)  
-    #   registration[:vin] = registration.delete(:vin_1_10)
-    #   registration[:engine] = :ev
-    # end
-    # vehicles = []
-    # registrations.each do |registration|
-    #   car = Vehicle.new(registration)
-    #   vehicles << car
-    # end
-    # return vehicles
   end
 end
