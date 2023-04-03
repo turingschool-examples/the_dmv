16f421a64e7710fed86baca24a504d417d345526
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-03 17:47:38 -0400

    create NY formatted name, address, phone number helper methods

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -24,17 +24,25 @@ class FacilityFactory
       require 'pry'; binding.pry
       Facility.new({ 
         name: NY_format_name(facility),
-        address: NY_format_address(facility)
-                  })
+        address: NY_format_address(facility), 
+        phone: NY_format_phone(facility)
+      })
     end
   end
-
+  
   def NY_format_name(facility)
     facility[:office_name].capitalize + " DMV Office"
   end
-
+  
   def NY_format_address(facility)
-    # facility[:street_address_line_1].capitalize + " " + facility[:city].capitalize + " " + facility[:state] + " " + facility[:zip_code]
-    "#{facility[:street_address_line_1].capitalize} #{facility[:city].capitalize} #{facility[:state]} #{facility[:zip_code]}"
+    "#{facility[:street_address_line_1]} #{facility[:city].capitalize} #{facility[:state]} #{facility[:zip_code]}"
+  end
+
+  def NY_format_phone(facility)
+    if facility[:public_phone_number].nil?
+      return "no phone number available" 
+    else
+      facility[:public_phone_number].insert(3,"-").insert(-5, "-")
+    end
   end
 end
