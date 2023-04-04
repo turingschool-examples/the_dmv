3382740d07f87bea2043ee7bdd7d83614b1b1511
Matthew Lim <matlim200@gmail.com>
2023-04-04 16:58:10 -0600

    add facility feature and test build out

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,3 +1,15 @@
 class FacilityFactory
- 
+ def or_facility_creator(or_dmv_data)
+  or_dmv_info = []
+  or_dmv_data.each do |data|
+    or_dmv_info << Facility.new({
+      name: data[:title], 
+      address: data[:human_address], 
+      phone: data[:phone_number]})
+    require 'pry'; binding.pry
+  end
+
+
+ end
 end
+
