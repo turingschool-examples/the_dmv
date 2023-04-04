18df25e9106910ebb5049cc98cd51ce32cd9467d
Blaine Glasgow <glasgowb@outlook.com>
2023-04-04 16:09:58 -0700

    Test for permit in registrant 1

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe Registrant do
     expect(registrant_1).to be_an_instance_of(Registrant)
   end
 
+  it "registrant 1 has permit" do
+    registrant_1 = Registrant.new("Bruce", 18, true )
+    expect(registrant_1.permit?).to eq(true)
+  end
+
   it "Can call instance variables" do
     registrant_1 = Registrant.new("Bruce", 18, true)
     registrant_2 = Registrant.new("Penny", 15)
