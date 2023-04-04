72c2d3c3d15d4cdda22be47b7cda397b37a5cb62
Alex Kiselich <alexander.kiselich@gmail.com>
2023-04-04 13:12:09 -0600

    Update lib and spec files

--- lib/dmv_factory.rb
+++ lib/dmv_factory.rb
@@ -1,7 +1,7 @@
 class DMVFacility
   attr_reader :list_of_DMVs 
   def initialize
-    @list_of_DMVs = []
+    @list_of_Facilities = []
   end
 
   def create_dmv_facilities(dmv_data)
@@ -14,7 +14,7 @@ class DMVFacility
         registered_vehicles: [],
         collected_fees: 0
       }
-      @list_of_DMVs << facility = Facility.new(facility_details)
+      @list_of_Facilities << facility = Facility.new(facility_details)
 
     end
   end
--- spec/dmv_factory_spec.rb
+++ spec/dmv_factory_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe DMVFacility do
     expect(dmv_factory).to be_a DMVFacility
   end
 
-  it 'can have DMVs' do
+  it 'can create facilities from APIs' do
     dmv_factory = DMVFacility.new
 
     dmv_data = DmvDataService.new.or_dmv_office_locations
     dmv_factory.create_dmv_facilities(dmv_data)
 
-    expect(dmv_factory.list_of_DMVs[0]).to be_a Facility
+    expect(dmv_factory.list_of_Facilities[0]).to be_a Facility
 
   end
 
