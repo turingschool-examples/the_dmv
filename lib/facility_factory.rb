95a99c2bb3bbd00f3d1220ec3f0489c106cb1648
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-04 16:07:07 -0600

    Refactore facility factory to parse data from all three states, test for new york, and leave previous iterations pseudocode

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,10 +1,74 @@
 class FacilityFactory
-  def initialize
-
+  def create_facilities(state_raw_data)
+    created_facilities = state_raw_data.map do |facility| 
+      facility_details = format_facility(facility)
+        Facility.new(
+          {
+            name:         facility_details[:name],
+            address:      facility_details[:address],
+            phone:        facility_details[:phone]
+          }
+        )
+      end
+    created_facilities
   end
 
-  def create_facilities(facilities_data)
-    ### Future State
+  def format_facility(data)
+    data = {
+        name:         format_name(data),
+        address:      format_address(data),
+        phone:        format_phone(data)
+      }
+  end
+
+  def format_name(data)
+    if data[:state] == "NY"
+      data[:office_name]
+    elsif data[:state] == "MO"
+      data[:name].rstrip 
+    elsif data[:wesbite].include?("ODOT")  
+      data[:title]
+    end
+  end
+
+  def format_address(data)
+    if data[:state] == "NY"
+      "#{data[:street_address_line_1]}, #{data[:city]}, #{data[:state]} #{data[:zip_code]}"
+    elsif data[:state] == "MO"
+      "#{data[:address1]}, #{data[:city]}, #{data[:state]} #{data[:zipcode]}"
+    elsif data[:wesbite].include?("ODOT")  
+      address = JSON.parse(data[:location_][:human_address], {symbolize_names: true})
+      "#{data[:address1]}, #{data[:city]}, #{data[:state]} #{data[:zipcode]}"
+    end
+  end
+
+  def format_phone(data)
+    if data[:state] == "NY"
+      data[:public_phone_number]
+    elsif data[:state] == "MO"
+      data[:phone]
+    elsif data[:wesbite].include?("ODOT")  
+      data[:phone_number]
+    end
+  end
+end
+
+# def facility_location(facilities_raw_data)
+#   facilities_raw_data.find do |facility_raw_data|
+#     facility_raw_data[:state] == "NY"
+#     :NY
+#   end
+#   facilities_raw_data.find do |facility_raw_data|
+#     facility_raw_data[:state] == "OR"
+#     :OR
+#   end
+#   facilities_raw_data.find do |facility_raw_data|
+#     facility_raw_data[:state] == "MO"
+#     :MO
+#   end
+# end
+### PRevious versions of creat_facility
+ ### Future State
     # if facilities_data.any? do |facility_data|
     #   facility_data.has_key?(:address1)
     #   created_facilities = facilities_data.map do |facility_data|
@@ -30,36 +94,35 @@ class FacilityFactory
     # end
     # created_facilities
     
-    created_facilities = facilities_data.map do |facility_data|
-      ## Not working for MO
-      # Facility.new(
-      #   {
-      #     name:         "#{facility_data[:office_name]} DMV Office" || "#{facility_data[:name]} DMV Office",
-      #     # address:      "#{facility_data[:street_address_line_1]} #{facility_data[:city]} #{facility_data[:state]}" || "#{facility_data[:address1]} #{facility_data[:city]} #{facility_data[:state]}",
-      #     address:      facility_data[:address1] || facility_data[:street_address_line_1],
-      #     # address:      facility_data[:address1] || facility_data[:street_address_line_1] && "#{facility_data[:city]} #{facility_data[:state]}",
-      #     # address:      facility_data[:address1] || facility_data[:street_address_line_1] + "#{facility_data[:city]}" + ", " + "#{facility_data[:state]}",
-      #     phone:        facility_data[:public_phone_number] || facility_data[:phone]
-      #   }
-      # )
-      ## Working
-      Facility.new(
-        {
-          name:         facility_data[:office_name] || facility_data[:name],
-          address:      facility_data[:city],
-          phone:        facility_data[:public_phone_number] || facility_data[:phone]
-        }
-      )
-    end
-    created_facilities
+  #   created_facilities = facilities_data.map do |facility_data|
+  #     ## Not working for MO
+  #     # Facility.new(
+  #     #   {
+  #     #     name:         "#{facility_data[:office_name]} DMV Office" || "#{facility_data[:name]} DMV Office",
+  #     #     # address:      "#{facility_data[:street_address_line_1]} #{facility_data[:city]} #{facility_data[:state]}" || "#{facility_data[:address1]} #{facility_data[:city]} #{facility_data[:state]}",
+  #     #     addrqess:      facility_data[:address1] || facility_data[:street_address_line_1],
+  #     #     # address:      facility_data[:address1] || facility_data[:street_address_line_1] && "#{facility_data[:city]} #{facility_data[:state]}",
+  #     #     # address:      facility_data[:address1] || facility_data[:street_address_line_1] + "#{facility_data[:city]}" + ", " + "#{facility_data[:state]}",
+  #     #     phone:        facility_data[:public_phone_number] || facility_data[:phone]
+  #     #   }
+  #     # )
+  #     ## Working
+  #     Facility.new(
+  #       {
+  #         name:         facility_data[:office_name] || facility_data[:name],
+  #         address:      facility_data[:city],
+  #         phone:        facility_data[:public_phone_number] || facility_data[:phone]
+  #       }
+  #     )
+  #   end
+  #   created_facilities
     
-    #   Facility.new(
-    #     {
-    #       name:         "#{facility_data[:office_name] DMV Office}" or "#{facility_data[:name]}",
-    #       address:      "#{facility_data[:street_address_line_1]}, #{facility_data[:city]}, #{facility_data[:state]}" or "#{facility_data[:address1]}, #{facility_data[:city]}, #{facility_data[:state]}",
-    #       phone:        "#{facility_data[:public_phone_number]}" or "#{facility_data[:phone]]}"
-    #     }
-    #   )
-    # end
-  end
-end
+  #   #   Facility.new(
+  #   #     {
+  #   #       name:         "#{facility_data[:office_name] DMV Office}" or "#{facility_data[:name]}",
+  #   #       address:      "#{facility_data[:street_address_line_1]}, #{facility_data[:city]}, #{facility_data[:state]}" or "#{facility_data[:address1]}, #{facility_data[:city]}, #{facility_data[:state]}",
+  #   #       phone:        "#{facility_data[:public_phone_number]}" or "#{facility_data[:phone]]}"
+  #   #     }
+  #   #   )
+  #   # end
+  # end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -8,23 +8,44 @@ RSpec.describe FacilityFactory do
   describe "Iteration 3" do
     it "exist" do
       facility_maker = FacilityFactory.new
-
+      
       expect(facility_maker).to be_an_instance_of(FacilityFactory)
     end
-
+    
     context "different factories" do
       it "can make a facility out of NY facilities" do
-        ny_facility_maker = FacilityFactory.new
-        new_york_facilities = DmvDataService.new.ny_dmv_office_locations
-        ny_facilities = ny_facility_maker.create_facilities(new_york_facilities)
-        require 'pry'; binding.pry
-        expect(ny_facility_maker.create_facilities(new_york_facilities)).to be_an(Array)
+        facility_maker = FacilityFactory.new
+        new_york_facilities_raw_data = DmvDataService.new.ny_dmv_office_locations
+        ny_facilities = facility_maker.create_facilities(new_york_facilities_raw_data)
+        
+        expect(ny_facilities).to be_an(Array)
+        expect(ny_facilities[0].name).to eq("SELDEN")
+        expect(ny_facilities[0].phone).to be nil
+        expect(ny_facilities[0].address).to eq("407 SELDEN RD, SELDEN, NY 11784")
+        expect(ny_facilities[0].registered_vehicles).to eq([])
+        expect(ny_facilities[0].services).to eq([])
+        expect(ny_facilities[0].collected_fees).to eq(0)
+        
+        expect(ny_facilities[2].name).to eq("SARANAC LAKE")
+        expect(ny_facilities[2].phone).to eq("5188911748")
+        expect(ny_facilities[2].address).to eq("39 MAIN STREET, SARANAC LAKE, NY 12983")
+        expect(ny_facilities[2].registered_vehicles).to eq([])
+        expect(ny_facilities[2].services).to eq([])
+        expect(ny_facilities[2].collected_fees).to eq(0)
       end
       
       it "can make a facility out of MO facilities" do
+        ny_facility_maker = FacilityFactory.new
+        new_york_facilities_raw_data = DmvDataService.new.ny_dmv_office_locations
+        ny_facilities = ny_facility_maker.create_facilities(new_york_facilities_raw_data)
         mo_facility_maker = FacilityFactory.new
         missouri_facilities = DmvDataService.new.mo_dmv_office_locations
         mo_facilities = mo_facility_maker.create_facilities(missouri_facilities)
+        or_facility_maker = FacilityFactory.new
+        oregon_facilities = DmvDataService.new.or_dmv_office_locations
+        or_facilities = or_facility_maker.create_facilities(oregon_facilities)
+
+        return_value = mo_facility_maker.facility_location(missouri_facilities)
         
         require 'pry'; binding.pry
         expect(mo_facility_maker.create_facilities(missouri_facilities)).to be_an(Array)
