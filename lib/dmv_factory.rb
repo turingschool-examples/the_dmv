69a1980040f32652a3ef25e272fba71e18b68f6b
Alex Kiselich <alexander.kiselich@gmail.com>
2023-04-04 14:40:30 -0600

    Created a ny dmv factories method

--- lib/dmv_factory.rb
+++ lib/dmv_factory.rb
@@ -1,7 +1,8 @@
 class DMVFacility
-  attr_reader :list_of_DMVs 
+  attr_reader :list_of_Facilities, :list_of_NY_Facilities 
   def initialize
     @list_of_Facilities = []
+    @list_of_NY_Facilities = []
   end
 
   def create_dmv_facilities(dmv_data)
@@ -18,5 +19,22 @@ class DMVFacility
 
     end
   end
+
+
+
+    def ny_dmv_office_locations(data)
+      data.to_a.map do |facility|
+        binding.pry
+        facility_details = {
+          name: facility[:office_name],
+          address: "#{facility[:street_address_line_1]}" + ", #{facility[:city]}" + ", #{facility[:state]}",
+          phone: facility[:public_phone_number],
+          services: [],
+          registered_vehicles: [],
+          collected_fees: 0
+        }
+        @list_of_NY_Facilities << facility = Facility.new(facility_details)
+        end
+    end 
 
 end
