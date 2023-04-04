bda1a0e4c51266e5d814f3a37e3620f415898d84
Lauren Golden <llgolden@gmail.com>
2023-04-03 21:23:32 -0600

    refactor: facility factor state methods

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -15,58 +15,21 @@ class FacilityFactory
       original_datum[:address] = original_datum[:location_1][:human_address] 
       original_datum[:phone] = original_datum[:phone_number]
     end
-# helper method
   end
-
-  # def oregon_
-
-
+  
   def ny_transform(original_dataset)
     original_dataset.each do |original_datum|
-      facility_details = {
-        name: original_datum[:office_name],
-        address: original_datum[:street_address_line_1],
-        phone: original_datum[:public_phone_number]
-      }
-    # create_facility(facility_details)
-
-
-      # original_datum[:name] = original_datum[:office_name]
-      # original_datum[:address] = original_datum[:street_address_line_1]
-      # original_datum[:phone] = original_datum[:public_phone_number]
-
+      original_datum[:name] = original_datum[:office_name]
+      original_datum[:address] = original_datum[:street_address_line_1]
+      original_datum[:phone] = original_datum[:public_phone_number]
     end
   end
 
   def mo_transform(original_dataset)
-    original_dataset.find_all do |original_datum|
+    original_dataset.each do |original_datum|
       original_datum[:name] = original_datum[:name]
       original_datum[:address] = original_datum[:address1]
       original_datum[:phone] = original_datum[:phone]
     end
   end
-  
-
-
-    # human_address = or_dmv_office_locations[1][:location_1][:human_address]
-    # human_address.delete("{").delete("}")
-    # human_address.join("")
-    # human_address.delete("address").delete("city").delete("state").delete("zip")
-    # human_address 
-  
-
-  # def rename_vehicles(vehicle_list)
-  #   vehicle_list.find_all do |vehicle_data|
-  #     vehicle_data[:vin] = vehicle_data.delete :dol_vehicle_id
-  #     vehicle_data[:year] = vehicle_data.delete :model_year
-  #     vehicle_data[:engine] = :ev
-  #   end
-  # end
-
-
-#   or_dmv_office_locations[1][:location_1][:human_address]
-#   {name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' }
-
-
-  
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -31,8 +31,6 @@ RSpec.describe FacilityFactory do
     or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations 
     or_data = facility_factory.oregon_transform(or_dmv_office_locations)
     or_created_facilities = facility_factory.create_facility(or_data)
-    # require 'pry'; binding.pry
-    # address not formatted correctly - formatted as a hash with :latitude, :longitude
     expect(or_created_facilities[0]).to be_an_instance_of(Facility)
     expect(or_created_facilities).to be_an(Array)
     expect(or_created_facilities.count).to eq(59)
@@ -41,16 +39,12 @@ RSpec.describe FacilityFactory do
   it "creates facilities in New York from an external list of facilities" do
     facility_factory = FacilityFactory.new
     ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
-    
     ny_data = facility_factory.ny_transform(ny_dmv_office_locations)
     ny_created_facilities = facility_factory.create_facility(ny_data)
     
     expect(ny_created_facilities[0]).to be_an_instance_of(Facility)
     expect(ny_created_facilities).to be_an(Array)
-    require 'pry'; binding.pry
     expect(ny_created_facilities.count).to eq(170) 
-    # returns 133 - maybe takes out instances without a phone number?
-  # require 'pry'; binding.pry
   end
 
   it "creates facilities in Missouri from an external list of facilities" do
@@ -61,7 +55,6 @@ RSpec.describe FacilityFactory do
       
     expect(mo_created_facilities[0]).to be_an_instance_of(Facility)
     expect(mo_created_facilities).to be_an(Array)
-    # expect(mo_created_facilities.count).to eq(177)
-    # returns 175 
+    expect(mo_created_facilities.count).to eq(177)
   end
 end
