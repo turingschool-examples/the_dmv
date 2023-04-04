a0148f243e5eea42bed45fe1bbcb4c6ce053afdb
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-03 19:01:50 -0600

    refactor: delete local variable for array, change to .map

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -4,15 +4,13 @@ class FacilityFactory
   end
 
   def create_facility(dmv)
-    new_facilities = []
-    dmv.each do |facility|
-      new_facilities << Facility.new(
-        name: facility[:title] || facility[:office_name] || facility[:name] || nil,
+    dmv.map do |facility|
+      Facility.new(
+        {name: facility[:title] || facility[:office_name] || facility[:name] || nil,
         address: facility[:address1]  || facility[:street_address_line_1] || facility[:location_1][:human_address] || nil,
         phone: facility[:phone_number] || facility[:public_phone_number] || facility[:phone] || nil,   
         zipcode: facility[:zip_code] || facility[:zipcode]
-      )
+        })
     end
-    new_facilities 
   end
 end
