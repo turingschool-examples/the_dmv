50fd4ce2c4718eed1b822d21fc55bbd66d836342
Alex Kiselich <alexander.kiselich@gmail.com>
2023-04-03 22:38:29 -0600

    Add 2 tests for DMV Facility class

---
+++ spec/dmv_factory_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+RSpec.describe DMVFacility do
+  it 'exists' do
+    
+    dmv_factory = DMVFacility.new
+
+    expect(dmv_factory).to be_a DMVFacility
+  end
+
+  it 'can have DMVs' do
+    dmv_factory = DMVFacility.new
+
+    dmv_data = DmvDataService.new.or_dmv_office_locations
+    dmv_factory.create_dmv_facilities(dmv_data)
+
+    expect(dmv_factory.list_of_DMVs[0]).to be_a Facility
+
+  end
+
+end
