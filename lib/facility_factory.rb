cd042cc091bf1c600b805fbc56d043026b95745a
Andy Weissman <andyweissman6@gmail.com>
2023-04-04 14:25:32 -0600

    add create facility method

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,3 +1,23 @@
 class FacilityFactory
-  
+
+  # def create_facility(dmv_database)
+  #   dmv_database.map do |data|
+  #     facility = Facility.new({
+
+  #             vin: data[:vin_1_10],
+  #            year: data[:model_year],
+  #            make: data[:make],
+  #           model: data[:model],
+  #          engine: :ev
+  #                           })
+      
+  #   end
+  # end
+
+
 end
+
+#facility_1 = Facility.new({
+  # name: 'Albany DMV Office', 
+  # address: '2242 Santiam Hwy SE Albany OR 97321', 
+  # phone: '541-967-2014' })
