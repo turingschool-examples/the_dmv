8eff45170c83ab32b3508d234f5073d9d7ec101d
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-03 19:05:16 -0600

    refactor: delete local array and change to .map

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -4,17 +4,15 @@ class VehicleFactory
   end
 
   def create_vehicles(dmv)
-    new_cars = []
-    dmv.each do |car|
-      new_cars << Vehicle.new(
-      engine: :ev,
-      make:  car[:make],
-      model: car[:model],
-      vin: car[:vin_1_10],
-      year: car[:model_year]
-      )
+    dmv.map do |car|
+      Vehicle.new({
+        engine: :ev,
+        make:  car[:make],
+        model: car[:model],
+        vin: car[:vin_1_10],
+        year: car[:model_year]
+        })
     end
-    new_cars
   end
 
   def most_popular(dmv)
