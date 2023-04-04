2b7f906582a85f2b35352bdcc084f934e830a17d
Justin Pasko <jpasko1112@gmail.com>
2023-04-03 20:02:12 -0400

    Add a method to creat MO facility.

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -9,18 +9,27 @@ class FacilityFactory
                 name: facility[:title],
                 address: eval_address.fetch_values(:address, :city, :state, :zip).join(" "),
                 phone: facility[:phone_number]
-               })
+              })
     end
   end
 
   def create_facility_ny(ny_facilities)
-    ny_facilities.map do |facility|
-require 'pry'; binding.pry 
+    ny_facilities.map do |facility| 
       Facility.new({
                 name: facility[:office_name],
                 address: ny_address = facility.fetch_values(:street_address_line_1, :city, :state, :zip_code).join(" "),
                 phone: facility[:phone_number]
-               })
+              })
+    end
+  end
+
+  def create_facility_mo(mo_facilities)
+    mo_facilities.map do |facility|
+      Facility.new({
+                name: [:name],
+                address: mo_address = facility.fetch_values(:address1, :city, :state, :zipcode).join(""),
+                phone: [:phone]
+              })
     end
   end
 end
