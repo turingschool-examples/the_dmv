b0793ccf54837511879fd018993c10c091c53008
Andy Weissman <andyweissman6@gmail.com>
2023-04-04 16:50:12 -0600

    edit address

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -5,7 +5,7 @@ class FacilityFactory
       facility = Facility.new({
 
             name: facility[:title] || facility[:office_name] || facility[:name] || nil,
-         address: facility[0][:location_1][:human_address] || facility[:street_address_line_1] || facility[0][:address1] || nil,
+         address: facility[:location_1][:human_address] || facility[:street_address_line_1] || facility[0][:address1] || nil,
            phone: facility[:phone_number] || facility[:public_phone_number] || nil
                               })
     end
