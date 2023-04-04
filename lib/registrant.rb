fcfda5bc132f939f0b367758edd1da3c19ca721c
Bradley Milton <bmilton2412@gmail.com>
2023-04-04 00:44:51 -0700

    removed permit from attr_reader, added license_data, added earn_permit method

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,19 +1,23 @@
 class Registrant
   attr_reader :name,
               :age,
-              :permit,
-              :license_data
+              :license_data,
+              :earn_permit
 
-  def initialize(registrant, permit = false)
-    @name         = registrant[:name]
-    @age          = registrant[:age]
-    @permit       = permit
-    #@license_data = registrant[:license_data]
+  def initialize(name, age, permit  = false)
+    @name = name
+    @age = age
+    @permit = permit
+    @license_data = {:written=>false, 
+                     :license=>false, 
+                     :renewed=>false}
   end
 
   def permit?
+    @permit == true 
+  end
+
+  def earn_permit
     @permit = true
   end
 end
-
-
