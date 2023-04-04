1252b4e9beae8ddf89213dbf22a962b20dcafaff
Lauren Golden <llgolden@gmail.com>
2023-04-03 22:24:50 -0600

    refactor: facility methods

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -64,6 +64,22 @@ RSpec.describe Facility do
       expect(facility_2.registered_vehicles).to eq([])
       expect(facility_2.collected_fees).to eq(0)
     end
+
+    it 'can collect fees and assign plates' do
+      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+      facility_1.add_service('Vehicle Registration')
+      facility_1.register_vehicle(cruz)
+      facility_1.register_vehicle(camaro)
+      facility_1.register_vehicle(bolt)
+
+      expect(facility_1.fees_and_plates(cruz)).to eq(:regular)
+      expect(facility_1.fees_and_plates(camaro)).to eq(:antique)
+      expect(facility_1.fees_and_plates(bolt)).to eq(:ev)
+    end
   end
 
   describe 'get a drivers license' do
