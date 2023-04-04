f39a0910fcce1b3f35db96a3dbf9e319e26d538a
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-04-04 17:40:19 -0500

    Add and fix register_vehicle and collect_fees methods

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,10 +1,10 @@
 class Facility
-  attr_reader :name, 
-              :address, 
-              :phone, 
-              :services, 
-              :registered_vehicles, 
-              :collected_fees
+  attr_accessor :name, 
+                :address, 
+                :phone, 
+                :services, 
+                :registered_vehicles, 
+                :collected_fees
 
   def initialize(facility_details)
     @name = facility_details[:name]
@@ -18,6 +18,23 @@ class Facility
   def add_service(service)
     @services << service
   end
+  
+  def register_vehicle(vehicle)
+    if @services.include?('Vehicle Registration')
+      collect_fees(vehicle)
+      vehicle.registration_date = Date.today
+      vehicle.set_plate_type
+      @registered_vehicles << vehicle
+    end
+  end
 
-  # def 
+  def collect_fees(vehicle)
+    if vehicle.antique?
+      @collected_fees += 25
+    elsif vehicle.electric_vehicle?
+      @collected_fees += 200
+    else
+      @collected_fees += 100
+    end
+  end
 end
