1eec4640a6527d301ccc11baebad79977517595a
Alex Kiselich <alexander.kiselich@gmail.com>
2023-04-04 16:08:05 -0600

    Add a create dmv facilities method using either OR, NY or MO API data. Also add a test to check all dmvs created go into DMV list.

--- lib/dmv_factory.rb
+++ lib/dmv_factory.rb
@@ -1,12 +1,34 @@
 class DMVFacility
-  attr_reader :list_of_Facilities, 
+  attr_reader :list_of_OR_Facilities, 
               :list_of_NY_Facilities, 
-              :list_of_MO_Facilities 
+              :list_of_MO_Facilities,
+              :list_of_DMVs 
   def initialize
-    @list_of_Facilities = []
+    @list_of_OR_Facilities = []
     @list_of_NY_Facilities = []
     @list_of_MO_Facilities = []
+    @list_of_DMVs = []
   end
+
+
+
+  def create_dmv_facilities(data)
+    data.map do |facility|
+      if facility.has_key?(:title)
+        create_or_dmv_facilities(data)
+        @list_of_DMVs << @list_of_OR_Facilities
+      elsif facility.has_key?(:office_name)
+        create_ny_dmv_office_locations(data)
+        @list_of_DMVs << @list_of_NY_Facilities
+      else
+        create_mo_dmv_office_locations(data)
+        @list_of_DMVs << @list_of_MO_Facilities
+      end
+    end
+      
+  end
+        
+   #helper methods 
 
   def create_or_dmv_facilities(dmv_data)
     dmv_data.map do |facility|
@@ -18,7 +40,7 @@ class DMVFacility
         registered_vehicles: [],
         collected_fees: 0
       }
-      @list_of_Facilities << facility = Facility.new(facility_details)
+      @list_of_OR_Facilities << facility = Facility.new(facility_details)
 
     end
   end
--- spec/dmv_factory_spec.rb
+++ spec/dmv_factory_spec.rb
@@ -11,10 +11,11 @@ RSpec.describe DMVFacility do
   it 'can create OR facilities from APIs' do
     dmv_factory = DMVFacility.new
 
-    dmv_data = DmvDataService.new.or_dmv_office_locations
-    dmv_factory.create_dmv_facilities(dmv_data)
+    or_dmv_data = DmvDataService.new.or_dmv_office_locations
 
-    expect(dmv_factory.list_of_Facilities[0]).to be_a Facility
+    dmv_factory.create_or_dmv_facilities(or_dmv_data)
+
+    expect(dmv_factory.list_of_OR_Facilities[0]).to be_a Facility
 
   end
 
@@ -38,4 +39,17 @@ RSpec.describe DMVFacility do
     expect(dmv_factory.list_of_MO_Facilities[3]).to be_a Facility
   end
 
+  it 'can creat DMVs with any API data' do
+    dmv_factory = DMVFacility.new
+
+    or_dmv_data = DmvDataService.new.or_dmv_office_locations
+    ny_dmv_data = DmvDataService.new.ny_dmv_office_locations
+    mo_dmv_data = DmvDataService.new.mo_dmv_office_locations
+
+    dmv_factory.create_dmv_facilities(or_dmv_data)
+    dmv_factory.create_dmv_facilities(ny_dmv_data)
+    dmv_factory.create_dmv_facilities(mo_dmv_data)
+
+    expect(dmv_factory.list_of_DMVs.count).to eq(406)
+  end
 end
