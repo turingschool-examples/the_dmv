161fa75533f967140f3cf22b2e9719998a41268d
Andy Weissman <andyweissman6@gmail.com>
2023-04-04 12:34:22 -0600

    add method to create vehicle and get data from database

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -3,11 +3,12 @@ class VehicleFactory
   def create_vehicles(database)
     database.map do |data|
       vehicle = Vehicle.new({
-        vin: data[:vin_1_10],
-        year: data[:model_year],
-        make: data[:make],
-        model: data[:model],
-        engine: :ev
+        
+              vin: data[:vin_1_10],
+             year: data[:model_year],
+             make: data[:make],
+            model: data[:model],
+           engine: :ev
                             })
       
     end
