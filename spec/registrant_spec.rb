4b085c6a24d1a67099d64825b54619659f5478f5
Myles Nottingham <MylesNottingham@gmail.com>
2023-04-04 15:45:49 -0600

    Test: Simplify tests for initialize.

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -7,23 +7,17 @@ RSpec.describe Registrant do
   end
 
   describe "#initialize" do
-    it "can initialize" do
+    it "can initialize a registrant" do
       expect(@registrant_1).to be_an_instance_of(Registrant)
-      expect(@registrant_1).to be_an_instance_of(Registrant)
-    end
-
-    it "has a name" do
       expect(@registrant_1.name).to eq("Bruce")
-      expect(@registrant_2.name).to eq("Penny")
-    end
-
-    it "has an age" do
       expect(@registrant_1.age).to eq(18)
-      expect(@registrant_2.age).to eq(15)
+      expect(@registrant_1.license_data).to eq({ written: false, license: false, renewed: false })
     end
 
-    it "has license data" do
-      expect(@registrant_1.license_data).to eq({ written: false, license: false, renewed: false })
+    it "can initialize another registrant" do
+      expect(@registrant_2).to be_an_instance_of(Registrant)
+      expect(@registrant_2.name).to eq("Penny")
+      expect(@registrant_2.age).to eq(15)
       expect(@registrant_2.license_data).to eq({ written: false, license: false, renewed: false })
     end
   end
