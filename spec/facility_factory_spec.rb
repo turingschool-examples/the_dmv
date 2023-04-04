e6c84c641d3b44a5136a47f727c1fd0ebc4a90d0
K.D. Hubbard <hubbard.kd@gmail.com>
2023-04-03 19:28:28 -0600

    add refactor tests

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe FacilityFactory do
     @or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
     @ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
     @mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
+    @first_fac = @fac_factory.facilities[0]
   end
 
   describe '#initialize' do 
@@ -15,76 +16,68 @@ RSpec.describe FacilityFactory do
     end
   end
 
-  describe '#create_facilities' do 
+  describe '#add_oregon_source' do
+    it 'it normalizes incoming oregon data' do
+      @fac_factory.add_oregon_source(@or_dmv_office_locations)
+     
+      expect(@first_fac).to be_a(Hash)
+      expect(@first_fac[:name]).not_to be nil
+      expect(@first_fac[:address]).not_to be nil
+      expect(@first_fac[:phone]).not_to be nil
+    end
+  end
+
+  describe '#add_ny_source' do 
+    it 'normalizes incoming NY data' do 
+      @fac_factory.add_oregon_source(@ny_dmv_office_locations)
+     
+      expect(@first_fac).to be_a(Hash)
+      expect(@first_fac[:name]).not_to be nil
+      expect(@first_fac[:address]).not_to be nil
+      expect(@first_fac[:phone]).not_to be nil
+    end
+
+  describe '#add_mo_source' do 
     it 'can create hashes of facility data' do 
-      expect(@fac_factory.create_facilities(@or_dmv_office_locations)).to be_a(Array)
+      expect(@fac_factory.create_facilities(@mo_dmv_office_locations)).to be_a(Array)
+      expect(@fac_factory.facilities.length).not_to eq(0)
+    end
+  end
+
+  describe '#create_facilities' do 
+    it 'can create facility objects with attributes' do 
+      @fac_factory.add_oregon_source(@or_dmv_office_locations)
+      @fac_factory.create_facilities2
+     
+      expect(@first_fac).to be_a(Facility)
+      expect(@first_fac.name).not_to be nil
+      expect(@first_fac.address).not_to be nil
+      expect(@first_fac.phone).not_to be nil
+      expect(@first_fac.services).to eq([])
+      expect(@first_fac.registered_vehicles).to eq([])
+      expect(@first_fac.collected_fees).to eq(0)
+    end
+
+    it 'adds new facilities to facilities attribute' do 
+      @fac_factory.add_oregon_source(@or_dmv_office_locations)
+      @fac_factory.create_facilities2
+
+      expect(@fac_factory.facilities).to be_a(Array)
       expect(@fac_factory.facilities.length).not_to eq(0)
     end
 
-    it 'can create facility objects with attributes' do
+    xit 'runs with any of the three above data sources' do 
       @fac_factory.add_oregon_source(@or_dmv_office_locations)
+      @fac_factory.add_oregon_source(@ny_dmv_office_locations)
+      @fac_factory.add_oregon_source(@mo_dmv_office_locations)
       @fac_factory.create_facilities2
-      first_fac = @fac_factory.facilities[0]
 
-      expect(first_fac).to be_a(Facility)
-      expect(first_fac.name).not_to be nil
-      expect(first_fac.address).not_to be nil
-      expect(first_fac.phone).not_to be nil
-      expect(first_fac.services).to eq([])
-      expect(first_fac.registered_vehicles).to eq([])
-      expect(first_fac.collected_fees).to eq(0)
+      expect(@first_fac).to be_a(Facility)
+      expect(@first_fac.name).not_to be nil
+      expect(@first_fac.address).not_to be nil
+      expect(@first_fac.phone).not_to be nil
+      expect(@first_fac.services).to eq([])
+      expect(@first_fac.registered_vehicles).to eq([])
+      expect(@first_fac.collected_fees).to eq(0)
     end
-
-    it 'returns an array of created facilities' do 
-      expect(@fac_factory.create_facilities(@or_dmv_office_locations)).to eq(@fac_factory.facilities)
-    end
-
-    describe 'it can work with a NY data source' do 
-      it 'can create hashes of facility data' do 
-        expect(@fac_factory.create_facilities(@ny_dmv_office_locations)).to be_a(Array)
-        expect(@fac_factory.facilities.length).not_to eq(0)
-      end
-  
-      it 'can create facility objects with attributes' do
-        @fac_factory.create_facilities(@ny_dmv_office_locations)
-        first_fac = @fac_factory.facilities[0]
-
-        expect(first_fac).to be_a(Facility)
-        expect(first_fac.name).not_to be nil
-        expect(first_fac.address).not_to be nil
-        expect(first_fac.phone).not_to be nil
-        expect(first_fac.services).to eq([])
-        expect(first_fac.registered_vehicles).to eq([])
-        expect(first_fac.collected_fees).to eq(0)
-      end
-
-      it 'returns an array of created facilities' do 
-        expect(@fac_factory.create_facilities(@ny_dmv_office_locations)).to eq(@fac_factory.facilities)
-      end
-    end
-
-    describe 'it can work with a MO data source' do 
-      it 'can create hashes of facility data' do 
-        expect(@fac_factory.create_facilities(@mo_dmv_office_locations)).to be_a(Array)
-        expect(@fac_factory.facilities.length).not_to eq(0)
-      end
-  
-      it 'can create facility objects with attributes' do
-        @fac_factory.create_facilities(@mo_dmv_office_locations)
-        first_fac = @fac_factory.facilities[0]
-
-        expect(first_fac).to be_a(Facility)
-        expect(first_fac.name).not_to be nil
-        expect(first_fac.address).not_to be nil
-        expect(first_fac.phone).not_to be nil
-        expect(first_fac.services).to eq([])
-        expect(first_fac.registered_vehicles).to eq([])
-        expect(first_fac.collected_fees).to eq(0)
-      end
-
-      it 'returns an array of created facilities' do 
-        expect(@fac_factory.create_facilities(@mo_dmv_office_locations)).to be_a(Array)
-      end
-    end
-  end
 end
