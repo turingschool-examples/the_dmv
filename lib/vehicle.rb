daa3ca36a37d416e2d9e3e29172d36fb0abfa5ad
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-03 19:02:06 -0600

    fix spacing

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,8 +5,10 @@ class Vehicle
               :year,
               :make,
               :model,
-              :engine
-  attr_accessor :registration_date, :plate_type
+              :engine  
+  
+  attr_accessor :registration_date, 
+                :plate_type
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
