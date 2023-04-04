9fc7bd26e4c39a0a8269a0db7c3dc1b4c2280202
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-04 17:05:47 -0400

    refactor by creating before(each) do block to tighten assertion tests

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,29 +1,26 @@
 require 'spec_helper'
 
 RSpec.describe Registrant do
+  before(:each) do
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_2 = Registrant.new('Penny', 15 )
+  end
+
   describe "#inialize" do
     it 'exists' do
-      registrant_1 = Registrant.new('Bruce', 18, true )
-      registrant_2 = Registrant.new('Penny', 15 )
-
-      expect(registrant_1).to be_a(Registrant)
-      expect(registrant_2).to be_a(Registrant)
+      expect(@registrant_1).to be_a(Registrant)
+      expect(@registrant_2).to be_a(Registrant)
     end
 
     it 'has readable attributes' do
-      registrant_1 = Registrant.new('Bruce', 18, true )
-      registrant_2 = Registrant.new('Penny', 15 )
-
-      expect(registrant_1.name).to eq("Bruce")
-      expect(registrant_1.age).to eq(18)
-      expect(registrant_1.permit?).to eq(true)
-      expect(registrant_1.license_data).to eq({:written=>false, 
+      expect(@registrant_1.name).to eq("Bruce")
+      expect(@registrant_1.age).to eq(18)
+      expect(@registrant_1.license_data).to eq({:written=>false, 
                                                :license=>false, 
                                                :renewed=>false})
-      expect(registrant_2.name).to eq("Penny")
-      expect(registrant_2.age).to eq(15)
-      expect(registrant_2.permit?).to eq(false)
-      expect(registrant_2.license_data).to eq({:written=>false, 
+      expect(@registrant_2.name).to eq("Penny")
+      expect(@registrant_2.age).to eq(15)
+      expect(@registrant_2.license_data).to eq({:written=>false, 
                                                :license=>false, 
                                                :renewed=>false})   
     end
@@ -31,11 +28,17 @@ RSpec.describe Registrant do
 
   describe '#earn permit' do
     it 'can earn permit' do
-      registrant_1 = Registrant.new('Bruce', 18, true )
-      registrant_2 = Registrant.new('Penny', 15 )
-      registrant_2.earn_permit
+      @registrant_2.earn_permit
       
-      expect(registrant_2.permit?).to eq(true)
+      expect(@registrant_2.permit).to eq(true)
+    end
+
+    it 'checks permit status' do
+      expect(@registrant_1.permit?).to eq(true)
+      expect(@registrant_2.permit?).to eq(false)
+      
+      @registrant_2.earn_permit
+      expect(@registrant_2.permit?).to eq(true)
     end
   end
 end
