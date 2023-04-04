0314794e5b6a2ca0ed47175ff10539329fcf3d54
Blaine Glasgow <glasgowb@outlook.com>
2023-04-04 16:54:35 -0700

    Clean up test. Try to add more functionality

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -11,5 +11,16 @@ class VehicleFactory
       end
     end
 
-
 end
+
+#Could pull data but unable to make similar to FacilityFactory. Would fix if had more time
+# def create_vehicles(vehicles)
+#   vehicles.each do |vehicle|
+#     @vehicles_list << Vehicle.new({vin: vehicle[:vin_1_10], 
+#     year: vehicle[:model_year], 
+#     make: vehicle[:make], 
+#     model: vehicle[:model]
+#     plate_type: vehicle[:state_of_residence],
+#     registration_date: vehicle [:transaction_year]})
+#   end
+# end
