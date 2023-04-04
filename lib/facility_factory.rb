9b5c5f9bfa55018fc48fd962cee1726d294a635a
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-03 19:48:04 -0600

    feat: add oepn and closing hour to facility object in create_facility method

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -9,7 +9,9 @@ class FacilityFactory
         {name: facility[:title] || facility[:office_name] || facility[:name] || nil,
         address: facility[:address1]  || facility[:street_address_line_1] || facility[:location_1][:human_address] || nil,
         phone: facility[:phone_number] || facility[:public_phone_number] || facility[:phone] || nil,   
-        zipcode: facility[:zip_code] || facility[:zipcode]
+        zipcode: facility[:zip_code] || facility[:zipcode],
+        open_hour: facility[:monday_beginning_hours] || nil,
+        closing_hour: facility[:friday_ending_hours] || nil
         })
     end
   end
