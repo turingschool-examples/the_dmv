26b0c7e59f15095b37fc1e0427429c5a38b31a76
K.D. Hubbard <hubbard.kd@gmail.com>
2023-04-03 19:37:54 -0600

    fix tests wip

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe FacilityFactory do
     @or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
     @ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
     @mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
-    @first_fac = @fac_factory.facilities[0]
   end
 
   describe '#initialize' do 
@@ -17,30 +16,38 @@ RSpec.describe FacilityFactory do
   end
 
   describe '#add_oregon_source' do
-    it 'it normalizes incoming oregon data' do
+    it 'it normalizes incoming OR data' do
       @fac_factory.add_oregon_source(@or_dmv_office_locations)
-     
-      expect(@first_fac).to be_a(Hash)
-      expect(@first_fac[:name]).not_to be nil
-      expect(@first_fac[:address]).not_to be nil
-      expect(@first_fac[:phone]).not_to be nil
+      first_fac = @fac_factory.facilities[0]
+
+      expect(first_fac).to be_a(Hash)
+      expect(first_fac[:name]).not_to be nil
+      expect(first_fac[:address]).not_to be nil
+      expect(first_fac[:phone]).not_to be nil
     end
   end
 
   describe '#add_ny_source' do 
-    it 'normalizes incoming NY data' do 
+    xit 'normalizes incoming NY data' do 
       @fac_factory.add_oregon_source(@ny_dmv_office_locations)
-     
-      expect(@first_fac).to be_a(Hash)
-      expect(@first_fac[:name]).not_to be nil
-      expect(@first_fac[:address]).not_to be nil
-      expect(@first_fac[:phone]).not_to be nil
+      first_fac = @fac_factory.facilities[0]
+
+      expect(first_fac).to be_a(Hash)
+      expect(first_fac[:name]).not_to be nil
+      expect(first_fac[:address]).not_to be nil
+      expect(first_fac[:phone]).not_to be nil
     end
+  end
 
   describe '#add_mo_source' do 
-    it 'can create hashes of facility data' do 
-      expect(@fac_factory.create_facilities(@mo_dmv_office_locations)).to be_a(Array)
-      expect(@fac_factory.facilities.length).not_to eq(0)
+    xit 'normalizes incoming MO data' do 
+      @fac_factory.add_oregon_source(@mo_dmv_office_locations)
+      first_fac = @fac_factory.facilities[0]
+      
+      expect(first_fac).to be_a(Hash)
+      expect(first_fac[:name]).not_to be nil
+      expect(first_fac[:address]).not_to be nil
+      expect(first_fac[:phone]).not_to be nil
     end
   end
 
@@ -48,14 +55,15 @@ RSpec.describe FacilityFactory do
     it 'can create facility objects with attributes' do 
       @fac_factory.add_oregon_source(@or_dmv_office_locations)
       @fac_factory.create_facilities2
-     
-      expect(@first_fac).to be_a(Facility)
-      expect(@first_fac.name).not_to be nil
-      expect(@first_fac.address).not_to be nil
-      expect(@first_fac.phone).not_to be nil
-      expect(@first_fac.services).to eq([])
-      expect(@first_fac.registered_vehicles).to eq([])
-      expect(@first_fac.collected_fees).to eq(0)
+      first_fac = @fac_factory.facilities[0]
+
+      expect(first_fac).to be_a(Facility)
+      expect(first_fac.name).not_to be nil
+      expect(first_fac.address).not_to be nil
+      expect(first_fac.phone).not_to be nil
+      expect(first_fac.services).to eq([])
+      expect(first_fac.registered_vehicles).to eq([])
+      expect(first_fac.collected_fees).to eq(0)
     end
 
     it 'adds new facilities to facilities attribute' do 
@@ -71,13 +79,15 @@ RSpec.describe FacilityFactory do
       @fac_factory.add_oregon_source(@ny_dmv_office_locations)
       @fac_factory.add_oregon_source(@mo_dmv_office_locations)
       @fac_factory.create_facilities2
+      first_fac = @fac_factory.facilities[0]
 
-      expect(@first_fac).to be_a(Facility)
-      expect(@first_fac.name).not_to be nil
-      expect(@first_fac.address).not_to be nil
-      expect(@first_fac.phone).not_to be nil
-      expect(@first_fac.services).to eq([])
-      expect(@first_fac.registered_vehicles).to eq([])
-      expect(@first_fac.collected_fees).to eq(0)
+      expect(first_fac).to be_a(Facility)
+      expect(first_fac.name).not_to be nil
+      expect(first_fac.address).not_to be nil
+      expect(first_fac.phone).not_to be nil
+      expect(first_fac.services).to eq([])
+      expect(first_fac.registered_vehicles).to eq([])
+      expect(first_fac.collected_fees).to eq(0)
     end
+  end
 end
