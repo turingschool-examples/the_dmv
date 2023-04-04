7680a21fc340cccb07ad42d10e3fc17115526bc3
Andy Weissman <andyweissman6@gmail.com>
2023-04-03 20:06:13 -0600

    create method for creating vehicles and assigning their traits via database

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,8 +1,15 @@
 class VehicleFactory
   
   def create_vehicles(database)
-    create
-
+    database.map do |data|
+      vehicle = Vehicle.new({
+        vin: data[:vin_1_10],
+        year: data[:model_year],
+        make: data[:make],
+        model: data[:model],
+        engine: :ev
+                            })
+      
+    end
   end
-  
 end
