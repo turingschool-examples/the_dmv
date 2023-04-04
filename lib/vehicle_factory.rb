b3a73b12ee1413bc2407dc96d6f40dfacfd5672b
Jesse Sorman <jessesorman@gmail.com>
2023-04-03 20:04:11 -0400

    refactor vehicle factory

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -5,6 +5,9 @@ class VehicleFactory
   end
 
   def create_vehicles(vehicles)
-    @vehicles << vehicles
+    vehicles.map do |vehicle|
+      car = Vehicle.new(vin: vehicle[:vin_1_10], year: vehicle[:model_year], make: vehicle[:make], model: vehicle[:model], engine: :ev)
+      @vehicles << car
+    end
   end
 end
