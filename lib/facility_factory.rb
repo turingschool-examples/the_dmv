af5bee43de8107692126e6701eae76b6913fd1b3
Andy Weissman <andyweissman6@gmail.com>
2023-04-04 16:22:14 -0600

    clean up method. add more specific data to each attribute

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,17 +1,15 @@
 class FacilityFactory
 
-  # def create_facility(dmv_database)
-  #   dmv_database.map do |facility|
-  #     facility = Facility.new({
+  def create_facilities(dmv_database)
+    dmv_database.map do |facility|
+      facility = Facility.new({
 
-    #     name: facility[:title] || facility[:office_name] || facility[:name]
-    #     address: facility[0][:location_1][:human_address] || facility[:street_address_line_1] || facility[0][:address1]
-    #     phone: facility[:phone_number] || facility[:public_phone_number]
-      
-    #  })
-      
-  #   end
-  # end
+            name: facility[:title] || facility[:office_name] || facility[:name] || nil,
+         address: facility[0][:location_1][:human_address] || facility[:street_address_line_1] || facility[0][:address1] || nil,
+           phone: facility[:phone_number] || facility[:public_phone_number] || nil
+                              })
+    end
+  end
 
 
 end
