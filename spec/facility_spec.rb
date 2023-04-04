ba112c031c2021695cfbfdec7fb2c63e69bd61f3
Andy Weissman <andyweissman6@gmail.com>
2023-04-03 19:06:53 -0600

    add extra expect statements for register

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe Facility do
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
     
-    it "can add available services to each facility" do
+    it "can add available services to specific facility" do
       facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
       facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
       facility_1.add_services('Vehicle Registration')
@@ -40,8 +40,8 @@ RSpec.describe Facility do
     end
   end
 
-  describe '#vehicle registration' do
-    it "registers vehicle @ facility_1" do
+  describe '#registers vehicle' do
+    it "registers vehicle at specific facility" do
       facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
       facility_1.add_services('Vehicle Registration')
       
@@ -61,13 +61,7 @@ RSpec.describe Facility do
       expect(@bolt.plate_type).to eq(:ev)
     end
 
-    it "tests we can add services to facility_1" do
-      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
-      
-      expect(facility_1.add_services('Vehicle Registration')).to eq(['Vehicle Registration'])
-    end
-
-    it "tests for total collected fees after registering @ facility_1" do
+    it "will total collected fees after registering" do
       facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
       facility_1.add_services('Vehicle Registration')
      
@@ -81,7 +75,7 @@ RSpec.describe Facility do
       expect(facility_1.collected_fees).to eq(325)
     end
 
-    it "tests that facility_2 cannot register vehicles" do
+    it "will not allow facility_2 to register vehicles" do
       facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
       facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
       facility_1.add_services('Vehicle Registration')
@@ -96,47 +90,31 @@ RSpec.describe Facility do
     end
   end
 
-  describe '#getting a Drivers License: WRITTEN TEST' do
-    it "tests for administering a written test for registrant_1 @ facility_1" do
+  describe '#written test' do
+    it "will administer a written test" do
       facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
       facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
       registrant_1 = Registrant.new('Bruce', 18, true )
       registrant_2 = Registrant.new('Penny', 16 )
       registrant_3 = Registrant.new('Tucker', 15 )
-
-    
+      
       expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      
       expect(registrant_1.permit?).to eq(true)
       expect(facility_1.administer_written_test(registrant_1)).to eq(false)
-      
-      facility_1.add_services('Written Test')
-
+      expect(facility_1.add_services('Written Test')).to eq(['Written Test'])
       expect(facility_1.administer_written_test(registrant_1)).to eq(true)
       expect(registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
-    end
-
-    it "tests for administering a Written Test for registrant_2 @ facility_1" do
-      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
-      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
-      registrant_1 = Registrant.new('Bruce', 18, true )
-      registrant_2 = Registrant.new('Penny', 16 )
-      registrant_3 = Registrant.new('Tucker', 15 )
-      facility_1.add_services('Written Test')
-
+    
+      expect(registrant_2.permit?).to eq(false)
       expect(facility_1.administer_written_test(registrant_2)).to eq(false)
       registrant_2.earn_permit
       expect(facility_1.administer_written_test(registrant_2)).to eq(true)
       expect(registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
-    end
-    
-    it "tests for administering a written test for registrant_3 @ facility_1" do
-      facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
-      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
-      registrant_1 = Registrant.new('Bruce', 18, true )
-      registrant_2 = Registrant.new('Penny', 16 )
-      registrant_3 = Registrant.new('Tucker', 15 )
-      facility_1.add_services('Written Test')
-    
+
+      expect(registrant_3.permit?).to eq(false)
       expect(facility_1.administer_written_test(registrant_3)).to eq(false)
       registrant_3.earn_permit
       expect(facility_1.administer_written_test(registrant_3)).to eq(false)
@@ -144,10 +122,8 @@ RSpec.describe Facility do
     end
   end
 
-  describe '#getting a Drivers License: ROAD TEST' do
-# A road test can only be administered to registrants who have passed the written test
-# For simplicity’s sake, Registrants who qualify for the road test automatically earn a license
-    it "tests for administering a road test for @ facility_1" do
+  describe '#road test' do
+    it "will administer a road test" do
       facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
       facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
       registrant_1 = Registrant.new('Bruce', 18, true )
@@ -165,14 +141,11 @@ RSpec.describe Facility do
 
       expect( facility_1.administer_road_test(registrant_2)).to eq(true)
       expect(registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
-      
-      ### go back and make these tests per individual
     end
   end
 
-  describe '#getting a Drivers License: RENEWAL' do
-  #registrant has already passed the road test and earned a license
-    it "tests to renew license  " do
+  describe '#license renewal' do
+    it "will renew license " do
       facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
       facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
       registrant_1 = Registrant.new('Bruce', 18, true )
