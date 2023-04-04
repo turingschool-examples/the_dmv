cc247065c55456ca891808d1c39d7c7e24cb7b32
Andy Weissman <andyweissman6@gmail.com>
2023-04-03 18:36:48 -0600

    create factory test and links api

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,3 +1,28 @@
 require './lib/vehicle'
 require './lib/vehicle_factory'
 require './lib/dmv_data_service'
+
+RSpec.describe Vehicle do
+  before(:each) do
+    @factory = VehicleFactory.new
+    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+    require 'pry'; binding.pry
+  end
+
+  describe "Vehicle Factory" do
+    it "exists" do 
+      expect(@factory).to be_a(VehicleFactory)
+    end
+  end
+
+  describe "creates vehicles" do
+    it "gets model from database and creates vehicle" do
+      expect(factory.create_vehicles(wa_ev_registrations)
+    end
+  end
+
+
+
+
+
+end
