8885027842e37b0ecccf28161f969bb22ee78a3b
William Zale <williamzale@gmail.com>
2023-05-17 14:07:40 -0400

    fix: Change initialize to accept hash, change add_services method to add_service

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,14 @@
 class Facility
   attr_reader :name, :address, :phone, :services
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(facility_data)
+    @name = facility_data[:name]
+    @address = facility_data[:address]
+    @phone = facility_data[:phone]
     @services = []
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
   end
 end
