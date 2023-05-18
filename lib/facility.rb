e1564e800e1a97fe4ab33cfb2569f9a21ebac9bd
Allen Russell <garyrusselljr@gmail.com>
2023-05-18 17:41:09 -0600

    fix: modify Facility class for debugging

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,21 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :facility_details, :services
 
-  def initialize(name)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(facility_details)
+    @facility_details = facility_details
     @services = []
+  end
+
+  def name
+    @facility_details.fetch(:name)
+  end
+
+  def address
+    @facility_details.fetch(:address)
+  end
+
+  def phone
+    @facility_details.fetch(:phone)
   end
 
   def add_service(service)
