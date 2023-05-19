5308e5ef44f7093bbb6c8424a06f6b93aeb9e8f4
Ian Lyell <ian.lyell@yahoo.com>
2023-05-18 22:48:37 -0500

    Pass registered_vehicals test in facility_spec

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,12 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicals
 
   def initialize(facility_hash)
     @name = facility_hash[:name]
     @address = facility_hash[:address]
     @phone = facility_hash[:phone]
     @services = []
+    @registered_vehicals = []
   end
 
   def add_service(service)
