d13a8b7019019a548d328576c58e6e6e862ef319
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-19 07:05:29 -0600

    call instances as instance variables in the spec

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe Vehicle do
+RSpec.describe Registrant do
   before(:each) do
     @registrant_1 = Registrant.new('Bruce', 18, true )
     @registrant_2 = Registrant.new('Penny', 15 )
@@ -8,26 +8,26 @@ RSpec.describe Vehicle do
 
   describe "Initialize" do  
     it "exists" do 
-      expect(registrant_1).to be_an_instance_of Registrant
-      expect(registrant_2).to be_an_instance_of Registrant
+      expect(@registrant_1).to be_an_instance_of Registrant
+      expect(@registrant_2).to be_an_instance_of Registrant
     end 
 
     it "can pass name and age as arguments" do 
-      expect(registrant_1.name).to eq("Bruce")
-      expect(registrant_1.age).to eq("18")
-      expect(registrant_2.name).to eq("Penny")
-      expect(registrant_2.age).to eq("15")
+      expect(@registrant_1.name).to eq("Bruce")
+      expect(@registrant_1.age).to eq(18)
+      expect(@registrant_2.name).to eq("Penny")
+      expect(@registrant_2.age).to eq(15)
     end 
     
     it "permit has a default value of false that can be altered as a pass through agruement" do
-      expect(registrant_2.permit).to be false
-      expect(registrant_1.permit).to be true 
+      expect(@registrant_2.permit).to be false
+      expect(@registrant_1.permit).to be true 
     end 
 
     it "stores license_data as a hash with all three keys pointing to false by default" do 
-      expect(registrant_1.license_data[:written]).to be false 
-      expect(registrant_1.license_data[:license]).to be false 
-      expect(registrant_1.license_data[:renewed]).to be false 
+      expect(@registrant_1.license_data[:written]).to be false 
+      expect(@registrant_1.license_data[:drivers_license]).to be false 
+      expect(@registrant_1.license_data[:renewed]).to be false 
     end 
   end 
 end 
