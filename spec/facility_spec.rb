96b18053aae8adb6ceae8687bf7ace801b024cf6
Allen Russell <garyrusselljr@gmail.com>
2023-05-19 16:30:03 -0600

    test: add test for administer written tests

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe Facility do
     @cruz = Vehicle.new({vin: "123456789abcdefgh", year: 2012, make: "Chevrolet", model: "Cruz", engine: :ice} )
     @bolt = Vehicle.new({vin: "987654321abcdefgh", year: 2019, make: "Chevrolet", model: "Bolt", engine: :ev} )
     @camaro = Vehicle.new({vin: "1a2b3c4d5e6f", year: 1969, make: "Chevrolet", model: "Camaro", engine: :ice} )
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_2 = Registrant.new('Penny', 16 )
+    @registrant_3 = Registrant.new('Tucker', 15 )
   end
   
   describe "#initialize" do
@@ -69,6 +72,34 @@ RSpec.describe Facility do
       @facility_2.register_vehicle(@bolt)
       expect(@facility_2.registered_vehicles).to eq []
       expect(@facility_2.collected_fees).to eq 0
+    end
+  end
+
+  describe "#adminster_written_test" do
+    it "can administer tests to registrants under certain conditions" do
+      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@registrant_1.permit?).to be true
+      expect(@facility_1.adminster_written_test(@registrant_1)).to be false
+
+      @facility_1.add_service("Written Test")
+      expect(@facility_1.adminster_written_test(@registrant_1)).to be true
+      expect(@registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+
+      expect(@registrant_2.age).to eq 16
+      expect(@registrant_2.permit?).to be false
+      expect(@facility_1.adminster_written_test(@registrant_2)).to be false
+      
+      @registrant_2.earn_permit
+      expect(@facility_1.adminster_written_test(@registrant_2)).to be true
+      expect(@registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+     
+      expect(@registrant_3.age).to eq 15
+      expect(@registrant_3.permit?).to be false
+      expect(@facility_1.adminster_written_test(@registrant_3)).to be false
+
+      @registrant_3.earn_permit
+      expect(@facility_1.adminster_written_test(@registrant_3)).to be false
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
   end
 end
