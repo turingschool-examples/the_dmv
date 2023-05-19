696cea87c02dce8441d0477620417128b490e265
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-19 12:39:00 -0800

    rspec earn permit method test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -3,14 +3,24 @@ require 'spec_helper'
 RSpec.describe "Registrant" do
 
   it 'can initiliza' do
-  registrant_1 = Registrant.new('Bruce', 18, true )
-  
-  expect(registrant_1.name).to eq('Bruce')
-  expect(registrant_1.age).to eq(18)
-  expect(registrant_1.permit?).to be true
-  expect(registrant_1.license_data).to eq({ written: false, license: false, renewed: false })
+    registrant_1 = Registrant.new('Bruce', 18, true )
+    registrant_2 = Registrant.new('Penny', 15)
+    
+    expect(registrant_1.name).to eq('Bruce')
+    expect(registrant_1.age).to eq(18)
+    expect(registrant_1.permit?).to be true
+    expect(registrant_1.license_data).to eq({ written: false, license: false, renewed: false })
+    expect(registrant_2.permit?).to be false
   end
-  
+
+  it 'can give permit to registrant' do
+    registrant_2 = Registrant.new('Penny', 15)
+
+    expect(registrant_2.permit?).to be false
+    expect(registrant_2.license_data).to eq({ written: false, license: false, renewed: false })
+    registrant_2.earn_permit
+    expect(registrant_2.permit?).to be true
+  end
 end
 
 #=> #<Registrant:0x000000015c10bed8 @age=18, @license_data={:written=>false, :license=>false, :renewed=>false}, @name="Bruce", @permit=true>
