67d2d02fceca3ded5c0c881575544e74b776764a
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-19 15:00:29 -0800

    add plate type attribute and accessor

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -7,7 +7,7 @@ class Vehicle
               :model,
               :engine
   
-  attr_accessor :registration_date
+  attr_accessor :registration_date, :plate_type
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -16,6 +16,7 @@ class Vehicle
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
     @registration_date = nil
+    @plate_type = nil
   end
 
   def antique?
