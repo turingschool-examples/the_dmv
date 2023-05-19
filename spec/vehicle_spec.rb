246c33910b109ee8234ebe79ea04c0996b4abfcb
Allen Russell <garyrusselljr@gmail.com>
2023-05-19 15:44:20 -0600

    test: add tests time_stamp and assign_plate

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 RSpec.describe Vehicle do
   before(:each) do
+    @facility_1 = Facility.new({name: "Albany DMV Office", address: "2242 Santiam Hwy SE Albany OR 97321", phone: "541-967-2014" })
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
@@ -34,9 +35,15 @@ RSpec.describe Vehicle do
     end
   end
 
-  describe "#registration_date" do
-    it "reports a registration date" do
-      expect(@cruz.registration_date).to eq nil
+  describe "#assign_plate" do
+    it "can toggle plate type date" do
+      expect(@cruz.plate_type).respond_to?(@cruz.assign_plate(:ev))
+    end
+  end
+
+  describe "#time_stamp" do
+    it "can toggle registration date" do
+      expect(@cruz.time_stamp).to be_within(0.01).of(Time.now)
     end
   end
 end
