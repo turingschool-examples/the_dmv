4f30b577b4afbd265081e38aaa7606ec36f6b820
Dylan Timmons <dylanstimmons@icloud.com>
2023-05-19 14:56:25 -0600

    Add #register_vehicle

--- lib/facility.rb
+++ lib/facility.rb
@@ -18,4 +18,17 @@ class Facility
   def add_service(service)
     @services << service
   end
+
+  def register_vehicle(vehicle)
+    return nil if !services.include?('Vehicle Registration')
+    @registered_vehicles << vehicle
+    if vehicle.plate_type == :antique
+      @collected_fees += 25
+    elsif vehicle.plate_type == :ev
+      @collected_fees += 200
+    else
+      @collected_fees += 100
+    end
+    @collected_fees
+  end
 end
