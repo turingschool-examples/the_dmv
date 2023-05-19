3dfb9991867870b489e977f8d8fd5ec5eabcc9a7
Connor Richmond <richmond.connor@gmail.com>
2023-05-19 17:15:21 -0400

    Add test for registration date and list

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe Facility do
     end
   end
 end
-
+#-----------------------------------------------------------------
 facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
 facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
 cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
@@ -39,6 +39,11 @@ describe Facility do
     expect(cruz.registration_date).to eq (nil)
 
     expect(facility_1.registered_vehicles).to eq([])
+    expect(facility_1.register_vehicle(cruz)).to eq([cruz])
+  end
+
+  it 'creates registration data' do
+    expect(cruz.registration_date).to eq(Date.today)
   end
 
 
