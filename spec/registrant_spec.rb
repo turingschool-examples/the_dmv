cae2541f375bdfa2986fed760945e7d25323d16a
Dylan Timmons <dylanstimmons@icloud.com>
2023-05-18 18:53:59 -0600

    Add test questions for permit? method

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe Registrant do
         registrant_2 = Registrant.new('Penny', 15 )
 
         expect(registrant_1.name).to eq("Bruce")
+        expect(registrant_1.age).to eq(18)
+        expect(registrant_1.permit?).to eq(true)
     end
 
 end
