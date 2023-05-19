78354f5d4cad0c1cf5003ebd5429023de4634589
Mike Wood <mikewwood@icloud.com>
2023-05-18 20:09:11 -0400

    test: added initialize test for Registrant class.

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+RSpec.describe Registrant do
+  before(:each) do
+    @registrant_1 = Registrant.new('Bruce', 18, true)
+  end
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@registrant_1).to be_a(Registrant)
+      expect(@registrant_1.name).to eq('Bruce')
+      expect(@registrant_1.age).to eq(18)
+      expect(@registrant_1.permit).to eq(true)
+    end
+  end
+end
