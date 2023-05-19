3a44a8f024590cb383c75a2efa3ecaeda1713153
Ethan Black <ethanrossblack@gmail.com>
2023-05-19 15:24:34 -0600

    feat: add attributes to attr_reader

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,22 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services, 
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(name, address, phone)
     @name = name
     @address = address
     @phone = phone
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_services(service)
     @services << service
   end
+
 end
