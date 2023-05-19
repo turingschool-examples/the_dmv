23ba9e5db760b7d006fc1f7cce2b9675a9eb8a98
Artemy Gibson <artemygibson@gmail.com>
2023-05-18 19:03:24 -0700

    Fix syntax on license test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Registrant do
         @registrant_1 = Registrant.new('Bruce', 18, true )
         @registrant_2 = Registrant.new('Penny', 15 )
     end
+
     it 'exists' do
         expect(@registrant_1).to be_a Registrant
     end
@@ -25,8 +26,8 @@ RSpec.describe Registrant do
     end
 
     it 'has license data' do
-        expect(@registrant_1.license_data).to eq {:written=>false, :license=>false, :renewed=>false}
-        expect(@registrant_2.license_data).to eq {:written=>false, :license=>false, :renewed=>false}
+        expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+        expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
 
     it 'can earn permit' do
