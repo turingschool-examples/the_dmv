0bcc998fc962fe95715c321d3f11520656a66795
Ian Lyell <ian.lyell@yahoo.com>
2023-05-18 21:54:31 -0500

    Add and pass registrant attributes test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -9,4 +9,17 @@ RSpec.describe Registrant do
         expect(registrant_1).to be_a(Registrant)
         expect(registrant_2).to be_a(Registrant)
     end
+
+
+    it 'Can hold a attribures: name, age, and permite. The latter defaults to false' do
+        registrant_1 = Registrant.new('Bruce', 18, true)
+        registrant_2 = Registrant.new('Penny', 15)
+
+        expect(registrant_1.name).to eq("Bruce")
+        expect(registrant_2.name).to eq("Penny")  
+        expect(registrant_1.age).to eq(18)
+        expect(registrant_2.age).to eq(15)
+        expect(registrant_1.permit?).to eq(true)
+        expect(registrant_2.permit?).to eq(false)
+    end
 end
