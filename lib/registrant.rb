27b4271fa431dc78c63aa7de4787f858c9eec082
Dani Wilson <danirae.powell@gmail.com>
2023-05-19 15:18:47 -0600

    Add permit and license data methods

---
+++ lib/registrant.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Registrant
+
+    attr_reader :name,
+                :age,
+                :permit
+                :license_data
+
+    def initialize(name, age, permit = false)
+        @name = name
+        @age = age
+        @permit = permit
+        @license_data = {
+            :written => false,
+            :license => false,
+            :renewed => false
+        }
+    end
+
+    def permit?
+    @permit
+    end
+
+    def license_data
+        @license_data
+    end
+
+end
