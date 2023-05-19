0f8600f82ce873d9f0676406b73b1cb284332259
Mike Wood <mikewwood@icloud.com>
2023-05-18 21:46:37 -0400

    test: add registered_vehicles and adding two facilities.

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 RSpec.describe Facility do
   before(:each) do
-    @facility = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+    @facility = Facility.new({ name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+    @facility_1 = Facility.new({ name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+    @facility_2 = Facility.new({ name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
   end
   describe '#initialize' do
     it 'can initialize' do
@@ -21,6 +23,18 @@ RSpec.describe Facility do
       @facility.add_service('Renew Drivers License')
       @facility.add_service('Vehicle Registration')
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+    end
+  end
+
+  describe '#add additional facilities' do
+    it 'returns empty array for registered_vehicles' do
+      expect(@facility_1.registered_vehicles).to eq([])
+      expect(@facility_2.registered_vehicles).to eq([])
+    end
+
+    it 'adds a service to facility_1' do
+      @facility_1.add_service('Vehicle Registration')
+      expect(@facility_1.services).to include('Vehicle Registration')
     end
   end
 end
