0472883e5e73c21caff2fecf2d135541370bf005
Connor Richmond <richmond.connor@gmail.com>
2023-05-18 21:22:05 -0400

    Debug initialize and facilities_offering_service

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -1,4 +1,6 @@
 class Dmv
+  #call facilities
+  attr_reader :facilities
 
   def initialize
     @facilities = []
@@ -7,9 +9,9 @@ class Dmv
   def add_facility(facility)
     @facilities << facility
   end
-
+  #use .select to select all elements of condition
   def facilities_offering_service(service)
-    @facilities.find do |facility|
+    @facilities.select do |facility|
       facility.services.include?(service)
     end
   end
