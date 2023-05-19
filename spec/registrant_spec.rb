5878ad401d830529eece5c91a7f409cd41da3709
Parker Boeing <parker.boeing@gmail.com>
2023-05-18 19:53:03 -0600

    Test: add test for registrant class

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -15,5 +15,23 @@ RSpec.describe Registrant do
       expect(registrant_1.age).to eq(18)
       expect(registrant_2.permit).to eq(false)
     end
+
+    it 'has a permit? method that checks if the registrant has a permit' do
+      registrant_1 = Registrant.new("Bruce", 18, true)
+      registrant_2 = Registrant.new("Penny", 15)
+      expect(registrant_1.permit?).to eq(true)
+      expect(registrant_2.permit?).to eq(false)
+    end
+
+    it 'has an empty hash with license data in it' do
+      registrant_1 = Registrant.new("Bruce", 18, true)
+      registrant_2 = Registrant.new("Penny", 15)
+      expected = {
+        written: false,
+        license: false,
+        renewed: false
+      }
+      expect(registrant_1.license_data).to eq(expected)
+    end
   end
 end
