d52d7cee6d4034c86b8cbeb35850cc715d9ca59c
Mike Wood <mikewwood@icloud.com>
2023-05-19 17:57:31 -0400

    Feat: adds calculate_collected_fees method.

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,3 +1,5 @@
+require 'vehicle'
+
 class Facility
   attr_reader :name,
               :address,
@@ -23,5 +25,20 @@ class Facility
     vehicle.registration_date = Date.today
     vehicle.determine_plate_type
     @registered_vehicles << vehicle
+    calculate_collected_fees(vehicle)
+  end
+
+  # Vehicles 25 years old or older are considered antique and cost $25 to register
+  # Electric Vehicles (EV) cost $200 to register
+  # All other vehicles cost $100 to register
+  # A vehicle’s plate_type should be set to :regular, :antique, or :ev upon successful registration.
+  def calculate_collected_fees(vehicle)
+    @collected_fees = if vehicle.plate_type == :ev
+                        200
+                      elsif vehicle.plate_type == :antique
+                        25
+                      else
+                        100
+                      end
   end
 end
