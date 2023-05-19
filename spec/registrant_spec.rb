dfd5eb730261664d2958e8caddc068ca999dd64c
Justin Pasko <jpasko1112@gmail.com>
2023-05-19 17:59:13 -0400

    Fix a spelling issue.

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe Registrant do
     it 'allows registrants to earn permit' do
       expect(@registrant_2.permit?).to eq(false)
       @registrant_2.earn_permit
-      expect(@registrant_2.permit)
+      expect(@registrant_2.permit).to eq(true)
     end
   end
 end
