bcf02570de3090bcd6d3398717a4b30c97e03ec8
Dylan Timmons <dylanstimmons@icloud.com>
2023-05-18 19:37:55 -0600

    Add test for license data

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe Registrant do
         expect(registrant_1.name).to eq("Bruce")
         expect(registrant_1.age).to eq(18)
         expect(registrant_1.permit?).to eq(true)
+        expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
 
 end
