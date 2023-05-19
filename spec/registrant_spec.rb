df8f176fcaf9dd3522447db64a61276e08ffe3b9
Dylan Timmons <dylanstimmons@icloud.com>
2023-05-18 18:45:29 -0600

    Add test to check if the name of the Registrant returns

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Registrant do
         expect(Registrant.new).to be_a_instance_of(Registrant)
     end
 
+    it 'returns the data for a Registrant' do
+        registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 15 )
+
+        expect(registrant_1.name).to eq("Bruce")
+    end
+
 end
