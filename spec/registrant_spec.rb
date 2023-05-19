ebad68bbf16cce9f7c96646ab2bea685058eb231
Dani Wilson <danirae.powell@gmail.com>
2023-05-19 15:18:31 -0600

    Add exists, has attributes, and license data tests

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,34 @@
+require 'pry'
+require './lib/registrant'
+
+RSpec.describe Registrant do
+
+    it 'exists' do
+        registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 15 )
+        registrant_2.permit?
+        expect(registrant_1).to be_an_instance_of(Registrant)
+        expect(registrant_2).to be_an_instance_of(Registrant)
+    end
+
+    it 'has attributes' do
+        registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 15 )
+
+        expect(registrant_1.name).to eq("Bruce")
+        expect(registrant_2.name).to eq("Penny")
+        expect(registrant_1.age).to eq(18)
+        expect(registrant_2.age).to eq(15)
+        expect(registrant_1.permit?).to eq(true)
+        expect(registrant_2.permit?).to eq(false)
+    end
+
+    it 'has license data' do
+        registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 15 )
+
+        expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+        expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+
+end
