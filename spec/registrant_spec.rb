836c31758dc59cb67be92dd72ec113797d5b99fb
Mike Wood <mikewwood@icloud.com>
2023-05-18 20:12:54 -0400

    test: updated initialize test to take second registrant.

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 RSpec.describe Registrant do
   before(:each) do
     @registrant_1 = Registrant.new('Bruce', 18, true)
+    @registrant_2 = Registrant.new('Penny', 15)
   end
   describe '#initialize' do
     it 'can initialize' do
