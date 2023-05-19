77c1e869833050b962f4d42d16cacb4d947c44cb
William Zale <williamzale@gmail.com>
2023-05-19 16:32:03 -0400

    feat: Add registered_vehicles and collected_fees instance variables, as well as register_vehicle method

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,40 @@
+require 'date'
+
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services,
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(facility_data)
     @name = facility_data[:name]
     @address = facility_data[:address]
     @phone = facility_data[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
   end
+
+  def register_vehicle(vehicle)
+    if services.include?("Vehicle Registration")
+      vehicle.issue_plate
+      if vehicle.antique?
+        @collected_fees += 25
+      elsif vehicle.engine == :ev
+        @collected_fees += 200
+      else
+        @collected_fees += 100
+      end
+      @registered_vehicles << vehicle
+    else
+      nil
+    end
+  end
+
 end
