8afb8c0bfca264e722382047c7b9d0e99a325682
Dylan Timmons <dylanstimmons@icloud.com>
2023-05-19 17:28:40 -0600

    Add tests to test if facility_2 had any input data from facility_1 tests

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -65,6 +65,7 @@ RSpec.describe Facility do
 
     it 'registers 3 seperate vehicles with correct attributes' do
       facility_1 = Facility.new({ name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
 
       cruz = Vehicle.new({ vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice })
       bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
@@ -88,6 +89,12 @@ RSpec.describe Facility do
       expect(bolt.plate_type).to eq(:ev)
       expect(facility_1.registered_vehicles).to eq([cruz, camaro, bolt])
       expect(facility_1.collected_fees).to eq(325)
+
+      expect(facility_2.registered_vehicles).to eq([])
+      expect(facility_2.services).to eq([])
+      expect(facility_2.register_vehicle(bolt)).to eq(nil)
+      expect(facility_2.registered_vehicles).to eq([])
+      expect(facility_2.collected_fees).to eq(0)
     end
 
 
