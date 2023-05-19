b7e363f03e4fe79763219642f738e420bfe262d9
Wil Fady <wrfady@gmail.com>
2023-05-18 23:02:00 -0400

    Refactor initialize method

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,10 +1,13 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services
 
-  def initialize(name: @name, address: @address, phone: @phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(facility_details)
+    @name = facility_details[:name]
+    @address = facility_details[:address]
+    @phone = facility_details[:phone]
     @services = []
   end
 
