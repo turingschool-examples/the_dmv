371b319ee42e85b799c30453ca278faad79dcb5b
Connor Richmond <richmond.connor@gmail.com>
2023-05-19 16:56:16 -0400

    Add register_vehicle method

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,26 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name,
+              :address,
+              :phone,
+              :services,
+              :registered_vehicles
+
+
   #change to look for keyword instead of positional 
   def initialize(name:, address:, phone:)
     @name = name
     @address = address
     @phone = phone
     @services = []
+    @registered_vehicles = []
+    @collected_fees
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+   @registered_vehicles << vehicle
   end
 end
