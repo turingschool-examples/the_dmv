d5a3783bb508316a66fc1442c4618d05c6365838
Connor Richmond <richmond.connor@gmail.com>
2023-05-19 14:41:38 -0400

    Create registrant class and test

---
+++ lib/registrant.rb
@@ -0,0 +1,14 @@
+class Registrant
+
+  attr_reader :name,
+              :age,
+              :permit,
+              :license_data
+
+  def initialize(name, age, permit, license_data)
+    @name = name
+    @age = age
+    @permit = false
+    @license_data = license_data
+  end
+end
