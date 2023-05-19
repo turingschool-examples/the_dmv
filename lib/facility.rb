5ec823f84d9c129cf249fa02aaf82099f294ac89
Justin Pasko <jpasko1112@gmail.com>
2023-05-19 17:51:56 -0400

    Update tests in spec file for better readability.

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,38 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services,
+              :registration_date,
+              :collected_fees,
+              :registered_vehicles
 
   def initialize(name:, address:, phone:)
     @name = name
     @address = address
     @phone = phone
     @services = []
+    @collected_fees = 0
+    @registered_vehicles = []
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    @registered_vehicles << vehicle
+    vehicle.registered_date
+    vehicle.assign_plate
+    add_collected_fees(vehicle)
+  end
+
+  def add_collected_fees(vehicle)
+    if vehicle.antique?
+      @collected_fees += 25
+    elsif vehicle.electric_vehicle?
+      @collected_fees += 200
+    else @collected_fees += 100
+    end
   end
 end
