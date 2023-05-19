0c015d983a921be066c56ab781586bfc6198973e
Connor Richmond <richmond.connor@gmail.com>
2023-05-19 17:35:06 -0400

    Add assignment for plate type

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -6,7 +6,8 @@ class Vehicle
               :make,
               :model,
               :engine,
-              :registration_date
+              :registration_date,
+              :plate_type
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -15,6 +16,7 @@ class Vehicle
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
     @registration_date = nil
+    @plate_type = nil
   end
 
   def antique?
@@ -25,7 +27,19 @@ class Vehicle
     @engine == :ev
   end
 
+
+  #self refers to current object
   def register
     @registration_date = Date.today
+    if self.electric_vehicle?
+      @plate_type = :ev
+    elsif self.antique?
+      @plate_type = :antique
+    else
+      @plate_type = :regular
+    end
   end
+
+
+  
 end
