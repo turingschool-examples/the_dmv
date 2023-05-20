8a0149a23f62c7a8e38950adb2e92f664b95ecf5
Artemy Gibson <artemygibson@gmail.com>
2023-05-19 20:35:56 -0700

    Make FacilityConstructor work with MO

--- lib/facility_constructor.rb
+++ lib/facility_constructor.rb
@@ -12,7 +12,7 @@ class FacilityConstructor
         formatted_locations = []
         locations.each do |location|
             location_data = {}
-            location_data[:name] = (location[:title] || location[:office_name])
+            location_data[:name] = (location[:title] || location[:office_name] || location[:name])
             if location[:location_1]
                 address_string = location[:location_1][:human_address]
                 address_hash = JSON.parse address_string
@@ -22,8 +22,11 @@ class FacilityConstructor
                 address_array.compact!
                 address_string = address_array.join(" ")
                 location_data[:address] = address_string
+            elsif location[:state] == "MO"
+                address_array = [location[:address1], location[:city], location[:state], location[:zipcode]]
+                location_data[:address] = address_array.join(" ")
             end
-            location_data[:phone] = (location[:phone_number] || location[:public_phone_number])
+            location_data[:phone] = (location[:phone_number] || location[:public_phone_number] || location[:phone])
             formatted_locations << location_data
         end
         formatted_locations
