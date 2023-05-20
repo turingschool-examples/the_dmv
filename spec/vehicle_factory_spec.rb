d2d3f4dd1db02761e2a033814cba695374e97b46
Allen Russell <garyrusselljr@gmail.com>
2023-05-20 12:52:18 -0600

    test: add expectations to create_vehicles

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -9,7 +9,16 @@ RSpec.describe VehicleFactory do
   describe "#initialize" do
     it "can initialize" do
       expect(@factory).to be_an_instance_of(VehicleFactory)
-      expect(@wa_ev_registrations). to be_an_instance_of(DmvDataService)
+      expect(@wa_ev_registrations).to be_an_instance_of(Array)
+    end
+  end
+
+  describe "#create_vehicles" do
+    it "lists vehicles from state's DMV" do
+      expect(@factory.create_vehicles(@wa_ev_registrations)).to be_an_instance_of(Array)
+      expect(@factory.create_vehicles(@wa_ev_registrations).first.vin).to eq("7SAYGDEF6N")
+      expect(@factory.create_vehicles(@wa_ev_registrations).last.vin).to eq("7SAYGDEE5N")
+      expect(@factory.create_vehicles(@wa_ev_registrations).count).to eq(4000)
     end
   end
 end
