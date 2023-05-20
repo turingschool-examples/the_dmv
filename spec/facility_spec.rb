c98834568bcef2d42230dcce5dc5d47e38c39cdc
Dani Wilson <danirae.powell@gmail.com>
2023-05-20 14:57:48 -0600

    Refactor facility 2 test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -85,11 +85,11 @@ RSpec.describe Facility do
   describe '#must have services listed to complete that service' do
     it 'should not register a vehicle at this location' do
 
-      expect(facility_2.registered_vehicles).to eq([])
-      expect(facility_2.services).to eq([])
-      expect(facility_2.register_vehicle(bolt)).to eq(nil)
-      expect(facility_2.registered_vehicles).to eq([])
-      expect(facility_2.collected_fees).to eq(0)
+      expect(@facility_2.registered_vehicles).to eq([])
+      expect(@facility_2.services).to eq([])
+      expect(@facility_2.register_vehicle(@bolt)).to eq(nil)
+      expect(@facility_2.registered_vehicles).to eq([])
+      expect(@facility_2.collected_fees).to eq(0)
     end
   end
 
