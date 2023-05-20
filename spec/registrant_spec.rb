4348912417281e38069d40a7ced061ab0f7c678d
Justin Pasko <jpasko1112@gmail.com>
2023-05-19 21:03:33 -0400

    Change indentation.

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Registrant do
   before(:each) do
     @registrant_1 = Registrant.new('Bruce', 18, true )
     @registrant_2 = Registrant.new('Penny', 15 )
-    
   end
 
   describe '#initialize' do
