18f4f3b95afb72001942af270e8d4aead32950e3
andi lovetto <andilovetto@gmail.com>
2023-05-19 20:33:25 -0600

    test: Remove collected fees method, changing to attribute

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,4 +1,5 @@
 class Facility
+  attr_accessor :collected_fees
   attr_reader :name, :address, :phone, :services, :registered_vehicles
 
   def initialize(facility_hash)
@@ -7,24 +8,25 @@ class Facility
     @phone = facility_hash[:phone]
     @services = []
     @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
   end
 
-  def collected_fees
-    0
-  end
-
   def register_vehicle(vehicle)
     if vehicle.antique? 
       vehicle.plate_type = :antique
+      @collected_fees += 25
     elsif vehicle.electric_vehicle? 
       vehicle.plate_type = :ev
+      @collected_fees += 200
     else 
       vehicle.plate_type = :regular
+      @collected_fees += 100
     end
+    
     vehicle.registration_date = Date.today.year
     @registered_vehicles.push(vehicle)
   end
