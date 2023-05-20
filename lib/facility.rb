2ec9e5d9a1043d6eb33fe1b589be0506f81fc073
Anna Wiley <awiley33@gmail.com>
2023-05-20 12:46:52 -0500

    feat: Update register method for facility

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,5 +1,12 @@
+
+
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name,
+              :address,
+              :phone,
+              :services,
+              :registered_vehicles,
+              :collected_fees
 
   # facility_details = { :name, :address, :phone}
 
@@ -8,9 +15,17 @@ class Facility
     @address = facility_details [:address]
     @phone = facility_details [:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    vehicle.is_registered = true
+    @registered_vehicles << vehicle
+    vehicle.register
   end
 end
