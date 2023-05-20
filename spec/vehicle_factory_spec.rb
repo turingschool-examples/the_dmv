ea14c6a8c514b76260d982c759c361c46763779c
Chris Asercion <chris.asercion@gmail.com>
2023-05-20 17:56:55 -0600

    creates initialize method for vehicle factory

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -2,3 +2,11 @@ require './lib/vehicle'
 require './lib/vehicle_factory'
 require './lib/dmv_data_service'
 
+RSpec.describe VehicleFactory do
+  describe '#initialize' do
+    it 'exist' do
+      factory = VehicleFactory.new
+      expect(factory).to be_an_instance_of(VehicleFactory)
+    end
+  end
+end
