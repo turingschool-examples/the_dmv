8f78f7c67fe145dcfa12965c08371598f7294567
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-19 19:18:31 -0600

    Test: Robust registration, plate type and fee collection

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -42,7 +42,36 @@ RSpec.describe Facility do
     end
   end
 
+  describe '#DMV functions' do 
+    it 'checks if facility can register properly and collect fees' do 
   
+      expect(@facility_1.registered_vehicles).to eq([])
+      expect(@facility_1.collected_fees).to eq(0)
+  
+      @facility_1.add_service('Vehicle Registration')
+      @facility_1.register_vehicle(@cruz)
+  
+      expect(@facility_1.registered_vehicles).to eq([@cruz])
+      expect(@cruz.plate_type).to eq(:regular)
+  
+      @facility_1.register_vehicle(@camaro)
+      expect(@camaro.registration_date).to eq(Date.today)
+      expect(@camaro.plate_type).to eq(:antique)
+  
+      @facility_1.register_vehicle(@bolt)
+      expect(@bolt.registration_date).to eq(Date.today)
+      expect(@bolt.plate_type).to eq(:ev)
+      expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro, @bolt])
+      expect(@facility_1.collected_fees).to eq(325)
+  
+      expect(@facility_2.registered_vehicles).to eq([])
+  
+      @facility_2.register_vehicle(@bolt)
+   
+      expect(@facility_2.register_vehicle(@bolt)).to eq(false)
+      expect(@facility_2.registered_vehicles).to eq([])
+    end
+  end
 
   
 end
