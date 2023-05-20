1f7a0d7ddf4aaf11b3c7d45eaa6deee99861ebdd
Ethan Black <ethanrossblack@gmail.com>
2023-05-20 11:47:48 -0600

    test: add test for renew_license for Registrant

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -83,4 +83,14 @@ describe "Registrant" do
       expect(@registrant_1.license_data[:license]).to be true
     end
   end
+
+  describe "#renew_license" do
+    it "can renew a license" do
+      expect(@registrant_1.license_data[:renewed]).to be false
+      
+      @registrant_1.renew_license
+      
+      expect(@registrant_1.license_data[:renewed]).to be true
+    end
+  end
 end
