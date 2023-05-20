e7086cf2a27d93421d77ddee93710261cebb5dc4
Dani Wilson <danirae.powell@gmail.com>
2023-05-20 12:33:27 -0600

    Refactor register vehicle method

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,36 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services, 
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(data_hash)
     @name = data_hash[:name]
     @address = data_hash[:address]
     @phone = data_hash[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
   end
+
+  def collected_fees
+  @collected_fees
+  end
+
+  def register_vehicle(vehicle)
+      if @services.include? 'Vehicle Registration'
+      @registered_vehicles << vehicle
+      return @registered_vehicles
+    end
+  end
+
+
+
+
 end
