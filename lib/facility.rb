c45a503acacb3f54cc606f3483ff6a6457264900
Parker Boeing <parker.boeing@gmail.com>
2023-05-20 17:06:15 -0600

    Feat: add register vehicle method to facility class

--- lib/facility.rb
+++ lib/facility.rb
@@ -16,7 +16,22 @@ class Facility
   end
 
   def register_vehicle(vehicle)
-    
+    if @services.include?("Vehicle Registration")
+      vehicle.registration_date = Date.today.year
+      @registered_vehicles << vehicle
+      if vehicle.antique?
+        vehicle.plate_type = :antique
+        @collected_fees += 25
+      elsif vehicle.electric_vehicle?
+        vehicle.plate_type = :ev
+        @collected_fees += 200
+      else
+        vehicle.plate_type = :regular
+        @collected_fees += 100
+      end
+    else
+      p "Facility does not currently offer this service"
+    end
   end
 
   def add_service(service)
