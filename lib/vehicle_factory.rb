3cd20d28a5a94e9b9b4fac5ca910e4756f3844c0
Ethan Black <ethanrossblack@gmail.com>
2023-05-20 13:24:22 -0600

    feat: add Vehicle factory class and #create_vehicles

---
+++ lib/vehicle_factory.rb
@@ -0,0 +1,19 @@
+class VehicleFactory
+
+  def initialize
+    
+  end
+
+  def create_vehicles(vehicle_data)
+    vehicle_data.map do |vehicle|
+      Vehicle.new({
+        vin: vehicle[:vin_1_10],
+        year: vehicle[:model_year],
+        make: vehicle[:make],
+        model: vehicle[:model],
+        engine: :ev
+      })
+    end
+  end
+
+end
