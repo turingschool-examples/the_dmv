91042503ffc93605a1a42751a28335393e396476
Davis Weimer <davisweimer@gmail.com>
2023-05-20 14:38:07 -0600

    spec: Create passing tests for administer_written_test method

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -117,6 +117,8 @@ RSpec.describe Facility do
       expect(@facility_2).to be_an_instance_of(Facility)
     end
 
+    # WRITTEN TEST
+
     it 'cannot administer written test without service' do
       expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
       expect(@registrant_1.permit?).to eq true
@@ -129,8 +131,37 @@ RSpec.describe Facility do
       expect(@facility_1.services).to eq(['Written Test'])
 
       @facility_1.administer_written_test(@registrant_1)
-      require 'pry'; binding.pry
       expect(@registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
     end
+
+    it 'can administer the Written Test to another registrant' do
+      @facility_1.add_service('Written Test')
+      expect(@registrant_2.age).to eq(16)
+      expect(@registrant_2.permit?).to eq false
+
+      @facility_1.administer_written_test(@registrant_2)
+      expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      
+      @registrant_2.earn_permit
+      
+      @facility_1.administer_written_test(@registrant_2)
+      expect(@registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+    end
+
+    it 'cannot administer Written Test to someone under 15 even with permit' do
+      @facility_1.add_service('Written Test')
+      expect(@registrant_3.age).to eq(15)
+      expect(@registrant_3.permit?).to eq false
+
+      @facility_1.administer_written_test(@registrant_3)
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      
+      @registrant_3.earn_permit
+      
+      @facility_1.administer_written_test(@registrant_3)
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+
+    
   end
 end
