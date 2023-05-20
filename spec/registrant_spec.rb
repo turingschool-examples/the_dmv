df32fde4f0ecb99ed2e2aef1942044ce7ce4e5db
Dani Wilson <danirae.powell@gmail.com>
2023-05-20 15:45:41 -0600

    Add test for criteria not being met

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -46,4 +46,31 @@ RSpec.describe Registrant do
         expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
         expect(registrant_1.permit?).to eq(true)
     end
+
+    it 'can administer a written test' do
+        registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 16 )
+        registrant_3 = Registrant.new('Tucker', 15 )
+        facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+        facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+        
+        facility_1.add_service('Written Test')
+
+        expect(facility_1.administer_written_test(registrant_1)).to eq(true)
+        expect(registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+    end
+
+    it 'will not administer written test if criteria are not met' do
+        registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 16 )
+        registrant_3 = Registrant.new('Tucker', 15 )
+        facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+        facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+        
+        facility_1.add_service('Written Test')
+
+        expect(registrant_2.age).to eq(16)
+        expect(registrant_2.permit?).to eq(false)
+        expect(facility_1.administer_written_test(registrant_2)).to eq(false)
+    end
 end
