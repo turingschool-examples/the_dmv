ff92fc4b87c41081de55ba5e2b8d8fdca033a23d
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-20 17:20:32 -0600

    create rspec tests to test when an array is passed into create_vechicles method

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -6,17 +6,24 @@ RSpec.describe VehicleFactory do
     expect(@factory).to be_an_instance_of(VehicleFactory)
   end 
 
-  it "can create new car objects" do 
-    @testa = @factory.create_vehicle({})
+  it "can create new vehicle objects" do 
+    @testa = @factory.create_vehicles({})
     expect(@factory.created_vehicles[0]).to be_an_instance_of(Vehicle)
   end 
 
   it "returns created vehicles in an array" do
-    @testa = @factory.create_vehicle({})
-    @testa_2 = @factory.create_vehicle({})
+    @testa = @factory.create_vehicles({})
+    @testa_2 = @factory.create_vehicles({})
     # expect(@factory.created_vehicles).to include(@testa, @testa_2)
     #For the life of me, I cannot figure out why this test ^ kept failing. In pry my @created_vehicles, matched my testa instances line for line.
     expect(@factory.created_vehicles.count).to eq(2)
   end 
 
+  it "can create multiple vehicles" do 
+    vehicle_array = [{}, {}] 
+    @factory.create_vehicles(vehicle_array)
+    expect(@factory.created_vehicles.count).to eq(2)
+    expect(@factory.created_vehicles[0]).to be_an_instance_of(Vehicle)
+    expect(@factory.created_vehicles[1]).to be_an_instance_of(Vehicle)
+  end 
 end 
