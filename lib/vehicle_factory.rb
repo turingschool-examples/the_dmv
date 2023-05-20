34b59c811d9a04dc9a32f6173c5228c0e019d399
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-20 16:00:29 -0600

    Feat: create_vehicles method

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -2,8 +2,23 @@ require './lib/facility'
 require './lib/dmv_data_service'
 
 class VehicleFactory 
+  attr_reader :new_vehicles
+  
   def initialize 
-    
+    @new_vehicles = []
   end
 
+  def create_vehicles(external_data) 
+    external_data.each do |data|
+     new_vehicles = {}
+     new_vehicles[:vin] = data[:vin_1_10]
+     new_vehicles[:year] = data[:model_year]
+     new_vehicles[:make] = data[:make]
+     new_vehicles[:model] = data[:model]
+     new_vehicles[:engine] = :ev
+     new_vehicles[:plate_type] = :ev
+      @new_vehicles << Vehicle.new(new_vehicles)
+      end
+      @new_vehicles
+    end
 end
