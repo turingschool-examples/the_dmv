3c38142d128f72e5b6b46f39220d3ad5c69723ab
Dani Wilson <danirae.powell@gmail.com>
2023-05-20 15:28:15 -0600

    Add license and permit data test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -35,4 +35,15 @@ RSpec.describe Registrant do
 
         expect(@registrant_2.permit?).to eq(true)
     end
+
+    it 'can return license and permit data' do
+        registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 16 )
+        registrant_3 = Registrant.new('Tucker', 15 )
+        facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+        facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+
+        expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+        expect(registrant_1.permit?).to eq(true)
+    end
 end
