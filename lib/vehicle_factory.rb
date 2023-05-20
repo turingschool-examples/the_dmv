f845e238ca5826dc83bdfb41e75f8be8bcfd5cd8
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-20 17:41:30 -0600

    give factory capacity to make multiple vehicles and assign engine type

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -5,9 +5,17 @@ class VehicleFactory
     @created_vehicles = []
   end
 
-  def create_vehicle(vehicle)
-    new_car = Vehicle.new(vehicle)
-    @created_vehicles << new_car
-    @created_vehicles
+  def create_vehicles(vehicles)
+    if vehicles.is_a?(Array)
+      vehicles.each do |vehicle|
+        new_car = Vehicle.new(vehicle)
+        new_car.engine = :ev 
+        @created_vehicles << new_car
+      end
+    else
+      new_car = Vehicle.new(vehicles)
+      @created_vehicles << new_car
+    end
   end
-end
+
+end 
