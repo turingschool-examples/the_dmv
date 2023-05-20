47af6126286640c9e2bcf3dab17f89524267d918
Kaina Cockett <kainacockett@gmail.com>
2023-05-20 09:52:33 -0600

    test: VehicleFactory #initialize

---
+++ spec/vehicle_factory_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+RSpec.describe VehicleFactory do
+    before(:each) do
+        #
+    end
+
+    describe '#initialize' do
+        it 'should create a new vehicle factory' do
+            excpect(@factory = VehicleFactory.new).to be_an_instance_of(VehicleFactory)
+        end
+    end
+end
