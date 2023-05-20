a2c981c2dc90f4de83c4f1b40569280877c9c223
Kaina Cockett <kainacockett@gmail.com>
2023-05-20 10:33:08 -0600

    feature: #create_vehicles for VahicleFactory

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -4,4 +4,18 @@ class VehicleFactory
     def initialize
         #
     end
+
+    def create_vehicles(vehicle_list)
+        output = []
+        vehicle_list.each do |vehicle|
+            output << Vehicle.new({
+                vin: vehicle[:vin_1_10], 
+                year: vehicle[:model_year], 
+                make: vehicle[:make], 
+                model: vehicle[:model], 
+                engine: :ev
+                })
+        end
+        output
+    end
 end
