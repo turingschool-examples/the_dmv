ff266b44a41bf60dec249d619ae0a0108d15c6fd
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-20 11:35:55 -0800

    feat: register vehicle method

--- lib/facility.rb
+++ lib/facility.rb
@@ -11,4 +11,21 @@ class Facility
   def add_service(service)
     @services << service
   end
+
+  def register_vehicle(vehicle)
+    if @services.include?('Vehicle Registration') && vehicle.registration_date.nil?
+      vehicle.registration_date = Date.today
+      if vehicle.antique?
+        vehicle.plate_type = :antique
+        @collected_fees += 25
+      elsif vehicle.electric_vehicle?
+        vehicle.plate_type = :electric
+        @collected_fees += 200
+      else
+        vehicle.plate_type = :regular
+        @collected_fees += 100
+      end
+      @registered_vehicles << vehicle
+    end
+  end
 end
