4349d675326b2fe7bdc50b52a9744b96e5e2976d
Mike Wood <mikewwood@icloud.com>
2023-05-20 17:42:09 -0400

    Test: added setup and test for Registrants in facility_spec file.

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -9,6 +9,9 @@ RSpec.describe Facility do
     @bolt = Vehicle.new({ vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev })
     @camaro = Vehicle.new({ vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice })
   end
+
+  # Starts 'Vehicle Registration' section of Iteration 2
+
   describe '#initialize' do
     it 'can initialize' do
       expect(@facility).to be_an_instance_of(Facility)
@@ -145,6 +148,21 @@ RSpec.describe Facility do
       expect(@facility_2.registered_vehicles).to eq([])
       expect(@facility_2.services).to eq([])
       expect(@facility_2.register_vehicle(@bolt)).to eq(nil)
+    end
+  end
+
+  # Start 'Getting a Driver's License' section of Iteration 2'
+
+  describe 'new registrants' do
+    before(:each) do
+      @registrant_1 = Registrant.new('Bruce', 18, true)
+      @registrant_2 = Registrant.new('Penny', 16)
+      @registrant_3 = Registrant.new('Tucker', 15)
+    end
+    it 'registrants license data is correct' do
+      expect(@registrant_1.license_data).to eq({ written: false, license: false, renewed: false })
+      expect(@registrant_2.license_data).to eq({ written: false, license: false, renewed: false })
+      expect(@registrant_3.license_data).to eq({ written: false, license: false, renewed: false })
     end
   end
 end
