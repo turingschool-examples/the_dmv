1592853c46cfc36f1fe64d2fb2c25dadf1ab2c43
Dani Wilson <danirae.powell@gmail.com>
2023-05-20 17:12:48 -0600

    Add unsuccessful road test test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -89,4 +89,17 @@ RSpec.describe Registrant do
         expect(facility_1.administer_written_test(registrant_3)).to eq(false)
         expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
+
+    it 'will not administer road test if criteria are not met' do
+        facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })        
+        registrant_3 = Registrant.new('Tucker', 15 )
+
+        expect(facility_1.administer_road_test(registrant_3)).to eq(false)
+
+        registrant_3.earn_permit
+
+        expect(facility_1.administer_road_test(registrant_3)).to eq(false)
+        expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+        expect(facility_1.administer_road_test(registrant_1)).to eq(false)
+    end
 end
