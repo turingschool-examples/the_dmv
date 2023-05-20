033c5882f3047599cd8045b03fdac402930b3959
Justin Pasko <jpasko1112@gmail.com>
2023-05-20 19:19:19 -0400

    feat: Add initialize and create_vehicle tests.

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe VehicleFacotry do
+RSpec.describe VehicleFactory do
   before(:each) do
     @factory = VehicleFactory.new
     @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
@@ -8,13 +8,15 @@ RSpec.describe VehicleFacotry do
 
   describe '#initialize' do
     it 'can initialize' do
-      expect(@factory).to be_a(VehicleFacotry)
+      expect(@factory).to be_a(VehicleFactory)
     end
   end
 
-  describe '#creat_vehicles(API)' do
+  describe '#creat_vehicles(vehicles)' do
     it 'can create vehicles from give database' do
-    @factory.create_vehicles(@wa_ev_registrations)
-    expect(@factory.create_vehicles(@wa_ev_registrations)).to be_an(Array)
+      @factory.create_vehicles(@wa_ev_registrations)
+      expect(@factory.create_vehicles(@wa_ev_registrations)).to be_an(Array)
+      expect(@factory.create_vehicles(@wa_ev_registrations)[0]).to be_a(Vehicle)
+    end
   end
 end
