f24d4673e4f6960a087cfcca6636b88e3f4ab9f1
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-19 19:33:44 -0600

    Test: before(:each) block added for test set up

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,35 +1,35 @@
 require './lib/registrant'
+require './lib/facility'
 
 RSpec.describe Registrant do 
-  it "exists w/ attributes" do 
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
-
-    expect(registrant_1).to be_a(Registrant)
-    expect(registrant_2).to be_a(Registrant)
-    expect(registrant_1.name).to eq("Bruce")
-    expect(registrant_1.age).to eq(18)
-    expect(registrant_1.permit?).to eq(true)
-    expect(registrant_2.name).to eq("Penny")
-    expect(registrant_2.age).to eq(15)
-    expect(registrant_2.permit?).to eq(false)
+  before(:each) do 
+      @registrant_1 = Registrant.new('Bruce', 18, true )
+      @registrant_2 = Registrant.new('Penny', 16 )
+      @registrant_3 = Registrant.new('Tucker', 15 )
+      @facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+      @facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
   end
-
-  it "provides license data" do 
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
+  it "exists w/ attributes" do 
+    expect(@registrant_1).to be_a(Registrant)
+    expect(@registrant_2).to be_a(Registrant)
+    expect(@registrant_1.name).to eq("Bruce")
+    expect(@registrant_1.age).to eq(18)
+    expect(@registrant_1.permit?).to eq(true)
+    expect(@registrant_2.name).to eq("Penny")
+    expect(@registrant_2.age).to eq(16)
+    expect(@registrant_2.permit?).to eq(false)
+  end
   
-    expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
-    expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+  it "provides license data" do 
+    expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
   end
 
   it "can earn permit" do 
-    registrant_2 = Registrant.new('Penny', 15 )
+    expect(@registrant_2.permit?).to eq(false)
 
-    expect(registrant_2.permit?).to eq(false)
+    @registrant_2.earn_permit
 
-    registrant_2.earn_permit
-
-    expect(registrant_2.permit?).to eq(true)
+    expect(@registrant_2.permit?).to eq(true)
   end
 end
