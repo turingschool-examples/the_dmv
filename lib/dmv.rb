d0ec1d35a8b7dfa3af99d4602f55ba99bcbfd0e9
Allen Russell <garyrusselljr@gmail.com>
2023-05-20 17:52:50 -0600

    feat: add create_facilities method

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -14,4 +14,17 @@ class Dmv
       facility.services.include?(service)
     end
   end
+
+  def create_facilities(dmv_office)
+    dmv_office.select do |office|
+      name = office[:title]
+      address_to_parse = JSON.parse(office[:location_1][:human_address])
+      address = address_to_parse.values.join(" ")
+      phone = office[:phone_number]
+
+      new_facility = Facility.new(name: name, address: address, phone: phone)
+      @facilities << new_facility
+    end
+    @facilities
+  end
 end
