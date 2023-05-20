b046cabdcdac50f5595e1dcd87d7496606f614f2
Gabriel Torres <thomastgtorres21@gmail.com>
2023-05-20 14:27:43 -0500

    Add register_vehicle method

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,23 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services, 
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(data)
     @name = data[:name]
     @address = data[:address]
     @phone = data[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
+  end
+
+  def register_vehicle(data)
+    @registered_vehicles.push(data)
   end
 
   def add_service(service)
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
+
 RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+    @facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
+    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
   end
   describe '#initialize' do
     it 'can initialize' do
@@ -13,7 +18,25 @@ RSpec.describe Facility do
       expect(@facility.services).to eq([])
     end
   end
-
+  describe '#registered vehicles' do
+    it 'can keep data on register vehicles' do 
+      expect(@facility.registered_vehicles).to eq([])
+      expect(@facility_2.registered_vehicles).to eq([])
+    end 
+  end
+  describe '#register vehicles' do
+    it 'can register vehicles' do
+      @facility.register_vehicle(@cruz)
+      expect(@facility.registered_vehicles).to eq([@cruz])
+    end
+  end
+  describe 
+  describe '#collected fees' do
+    it 'keeps track of collected fees' do
+      expect(@facility.collected_fees).to eq(0)
+      expect(@facility_2.collected_fees).to eq(0)
+    end
+  end
   describe '#add service' do
     it 'can add available services' do
       expect(@facility.services).to eq([])
