64cb2314c7e454a657a46ffb4c335b782122e4e5
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-20 11:49:58 -0800

    add register vehicle attributes

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,18 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services,
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(name:, address:, phone:)
     @name = name
     @address = address
     @phone = phone
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
