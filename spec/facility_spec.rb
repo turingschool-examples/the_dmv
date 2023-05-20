4965fcab771e5b09e307a79c51a8dfcd82fd2f50
andi lovetto <andilovetto@gmail.com>
2023-05-20 15:57:02 -0600

    test: Write test for administering written test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe Facility do
   let(:cruz) { Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} ) }
   let(:bolt) { Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} ) }
   let(:camaro) { Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} ) }
+  let(:registrant_1) { Registrant.new('Bruce', 18, true) }
+  let(:registrant_2) { Registrant.new('Penny', 16) }
+  let(:registrant_3) { Registrant.new('Tucker', 15) }
 
   before(:each) do
     @facility = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
@@ -54,5 +57,13 @@ RSpec.describe Facility do
     expect(facility_2.register_vehicle(bolt)).to eq(nil)
     expect(facility_2.registered_vehicles).to eq([])
     expect(facility_2.collected_fees).to eq(0)
+  end
+
+  it 'can administer written and road tests' do
+    expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    expect(registrant_1.permit?).to eq(true)
+    expect(facility_1.administer_written_test(registrant_1)).to eq(false)
+    expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+
   end
 end
