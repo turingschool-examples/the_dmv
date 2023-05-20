ebd08ddb83c282a6a89d13246c7f000d8a9cd362
Artemy Gibson <artemygibson@gmail.com>
2023-05-19 18:46:04 -0700

    Pass NY data formatting test

--- lib/facility_constructor.rb
+++ lib/facility_constructor.rb
@@ -12,11 +12,18 @@ class FacilityConstructor
         formatted_locations = []
         locations.each do |location|
             location_data = {}
-            location_data[:name] = location[:title]
-            address_string = location[:location_1][:human_address]
-            address_hash = JSON.parse address_string
-            location_data[:address] = address_hash.values.join(" ")
-            location_data[:phone] = location[:phone_number]
+            location_data[:name] = (location[:title] || location[:office_name])
+            if location[:location_1]
+                address_string = location[:location_1][:human_address]
+                address_hash = JSON.parse address_string
+                location_data[:address] = address_hash.values.join(" ")
+            elsif location[:state] == "NY"
+                address_array = [location[:street_address_line_1], location[:street_address_line_2], location[:city], location[:state], location[:zip_code]]
+                address_array.compact!
+                address_string = address_array.join(" ")
+                location_data[:address] = address_string
+            end
+            location_data[:phone] = (location[:phone_number] || location[:public_phone_number])
             formatted_locations << location_data
         end
         formatted_locations
