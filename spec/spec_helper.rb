87fdf6a678466d3499a8332f93e8d7ce6ca5ce4b
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-20 14:14:31 -0600

    create the inital tests for the VehicleFactory class and add an instance of it to the spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,5 +26,7 @@ RSpec.configure do |config|
     @facility_3 = Facility.new({name: 'Bend DMV Office', address: '63030 O B Riley Rd Bend OR 97701', phone: '541-388-6322'})
 
     @dmv = Dmv.new
+
+    @factory = VehicleFactory.new 
   end
 end
