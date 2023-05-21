f6e1a9b624cf600572afffc8ff0e8e70a01e51fc
Allen Russell <garyrusselljr@gmail.com>
2023-05-21 17:06:39 -0600

    feat: add helper methods to parse data

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -1,5 +1,5 @@
 class Dmv
-  attr_reader :facilities
+  attr_reader :facilities 
 
   def initialize
     @facilities = []
@@ -15,16 +15,41 @@ class Dmv
     end
   end
 
-  def create_facilities(dmv_office)
-      dmv_office.select do |office|
-        name = office[:title]
-        address_to_parse = JSON.parse(office[:location_1][:human_address])
-        address = address_to_parse.values.join(" ")
-        phone = office[:phone_number]
+  def or_parse(oregon)
+    oregon.select do |office|
+      name = office[:title]
+      address_to_parse = JSON.parse(office[:location_1][:human_address])
+      address = address_to_parse.values.join(" ")
+      phone = office[:phone_number]
 
-        new_facility = Facility.new(name: name, address: address, phone: phone)
-        @facilities << new_facility
-      end
-      @facilities
+      new_facility = Facility.new(name: name, address: address, phone: phone)
+      @facilities << new_facility
+    end   
+  end
+
+  def ny_parse(new_york)
+    new_york.select do |office|
+      name = office[:office_name]
+      address = office[:street_address_line_1]
+      phone = office[:public_phone_number]
+
+      new_facility = Facility.new(name: name, address: address, phone: phone)
+      @facilities << new_facility
+    end
+  end
+
+  def mo_parse(missouri)
+    missouri.select do |office|
+      name = office[:name]
+      address = office[:address1]
+      phone = office[:phone]
+
+      new_facility = Facility.new(name: name, address: address, phone: phone)
+      @facilities << new_facility
+    end
+  end
+
+  def create_facilities(dmv_office)
+    @facilities
   end
 end
