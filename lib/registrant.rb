1d7a44784d0a4b503d8323119c626741e7fc3347
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-21 14:56:47 -0500

    add an earn permit method

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -2,7 +2,7 @@ class Registrant
 attr_reader :name, :age, :permit,
             :license_data
    
-def initialize(name, age, permit = false)
+    def initialize(name, age, permit = false)
         @name = name
         @age = age
         @permit = permit
