645aa8b2544981b9031833682088751e7703b368
Captainlearyo <clayoleary@gmail.com>
2023-05-20 20:08:10 -0700

    add renew_drivers_license works

--- lib/facility.rb
+++ lib/facility.rb
@@ -30,8 +30,26 @@ class Facility
   end
 
   def administer_written_test(reg)
-    if @services.include?('Written Test')
+    if @services.include?('Written Test') && reg.age >= 16 && reg.permit?
       reg.license_data[:written] = true
+      true
+    else
+      false
+    end
+  end
+
+  def administer_road_test(reg)
+    if @services.include?('Road Test') && reg.license_data[:written] == true
+      reg.license_data[:license] = true
+      true
+    else
+      false
+    end
+  end
+
+  def renew_drivers_license(reg)
+    if @services.include?('Renew License') && reg.license_data[:written] == true && reg.license_data[:license] == true
+      reg.license_data[:license] = true
       true
     else
       false
