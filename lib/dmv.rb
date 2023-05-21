70b640da36f7d72d1dbc2780aeb7ff25aa1beea7
Allen Russell <garyrusselljr@gmail.com>
2023-05-20 18:14:49 -0600

    fix: remove code that was buggy for New York

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -16,15 +16,15 @@ class Dmv
   end
 
   def create_facilities(dmv_office)
-    dmv_office.select do |office|
-      name = office[:title]
-      address_to_parse = JSON.parse(office[:location_1][:human_address])
-      address = address_to_parse.values.join(" ")
-      phone = office[:phone_number]
+      dmv_office.select do |office|
+        name = office[:title]
+        address_to_parse = JSON.parse(office[:location_1][:human_address])
+        address = address_to_parse.values.join(" ")
+        phone = office[:phone_number]
 
-      new_facility = Facility.new(name: name, address: address, phone: phone)
-      @facilities << new_facility
-    end
-    @facilities
+        new_facility = Facility.new(name: name, address: address, phone: phone)
+        @facilities << new_facility
+      end
+      @facilities
   end
 end
