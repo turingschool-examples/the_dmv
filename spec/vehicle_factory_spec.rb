46cd3129b8843f9b968ca03cc7e96846a29c95b0
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-21 13:22:25 -0600

    create new test for dealing with non idential key names and reformat the existing tests to be more effective and less redundant

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -6,28 +6,36 @@ RSpec.describe VehicleFactory do
     expect(@factory).to be_an_instance_of(VehicleFactory)
   end 
 
-  it "can create new vehicle objects" do 
-    @testa = @factory.create_vehicles({})
+  it "can create new vehicle instances" do 
+    @factory.create_vehicle_order({})
     expect(@factory.created_vehicles[0]).to be_an_instance_of(Vehicle)
   end 
 
-  it "returns created vehicles in an array" do
-    @testa = @factory.create_vehicles({})
-    @testa_2 = @factory.create_vehicles({})
-    # expect(@factory.created_vehicles).to include(@testa, @testa_2)
-    #For the life of me, I cannot figure out why this test ^ kept failing. In pry my @created_vehicles, matched my testa instances line for line.
+  it "created vehicle instances are stored in an array" do 
+    @factory.create_vehicle_order({})
+    @factory.create_vehicle_order({})
+    expect(@factory.created_vehicles).to be_an(Array)
     expect(@factory.created_vehicles.count).to eq(2)
   end 
 
   it "can create multiple vehicles at once" do 
     vehicle_array = [{}, {}] 
-    @factory.create_vehicles(vehicle_array)
+    @factory.create_vehicle_order(vehicle_array)
+    expect(@factory.created_vehicles[0]).to be_an_instance_of(Vehicle)
+    expect(@factory.created_vehicles[1]).to be_an_instance_of(Vehicle)
     expect(@factory.created_vehicles.count).to eq(2)
   end 
 
-  it "sets engine type to :ev" do 
+  it "sets engine type to :ev upon instance creation" do 
     vehicle_array = [{}, {}] 
-    @factory.create_vehicles(vehicle_array)
+    @factory.create_vehicle_order(vehicle_array)
     expect(@factory.created_vehicles[0].engine).to eq(:ev)
+    expect(@factory.created_vehicles[1].engine).to eq(:ev)
+  end 
+
+  it "can set vin and year values when the key is not an identical match" do 
+    @factory.create_vehicle_order({vin_10: 12, model_year: 20})
+    expect(@factory.created_vehicles[0].vin).to eq(12)
+    expect(@factory.created_vehicles[0].year).to eq(20)
   end 
 end 
