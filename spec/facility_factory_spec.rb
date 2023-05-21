9e791d8c8aafb6962525ebb1ff3d7632af8f4af5
Parker Boeing <parker.boeing@gmail.com>
2023-05-21 16:47:36 -0600

    Test: ensure define facilities method works properly with oregon data set

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -9,21 +9,28 @@ RSpec.describe FacilityFactory do
   end
   
   describe '#initialize' do
-    xit 'can initialize' do
+    it 'can initialize' do
       expect(@oregon).to be_a FacilityFactory
     end
   end
 
   describe '#define_facilities' do
-    xit 'can put the facilities of oregon into an array' do
+    it 'can put the facilities of oregon into an array' do
       expect(@oregon.operating_facilities).to eq []
       @oregon.define_facilities(@oregon_facilities)
       expect(@oregon.operating_facilities).to include(Facility)
    
     end
 
-    xit 'each instantiated facility is formatted properly' do
-      expect(@facility_1.registered_vehicles).to eq []
+    it 'each instantiated facility is formatted properly' do
+      @oregon.define_facilities(@oregon_facilities)
+      expect(@oregon.operating_facilities[0].address).to eq "2242 Santiam Hwy SE Albany OR 97321"
+      expect(@oregon.operating_facilities[0].phone).to eq "541-967-2014"
+      expect(@oregon.operating_facilities[0].name).to eq "Albany DMV Office"
+      expect(@oregon.operating_facilities[1].address).to eq "600 Tolman Creek Rd Ashland OR 97520"
+      expect(@oregon.operating_facilities[1].phone).to eq "541-776-6092"
+      expect(@oregon.operating_facilities[1].name).to eq "Ashland DMV Office"
+      expect(@oregon.operating_facilities.length).to eq @oregon_facilities.length  
     end
   end
 end
