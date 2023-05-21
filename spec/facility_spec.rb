cfde1c077a2d9b3f7af496dd13001b84d2694fcb
andi lovetto <andilovetto@gmail.com>
2023-05-20 18:01:10 -0600

    test: Finish testing for administration of road tests

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -86,5 +86,15 @@ RSpec.describe Facility do
     registrant_3.earn_permit
     expect(facility_1.administer_road_test(registrant_3)).to eq(false)
     expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    expect(facility_1.administer_road_test(registrant_1)).to eq(false)
+    facility_1.add_service('Road Test')
+    expect(facility_1.administer_road_test(registrant_1)).to eq(true)
+    expect(registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
+    expect(facility_1.administer_road_test(registrant_2)).to eq(true)
+    expect(registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
   end
+
+  # it 'renews licenses' do
+
+  # end
 end
