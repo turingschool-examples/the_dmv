1a247513053ceabfb9ff644dde5d8258c676a3e8
Mike Wood <mikewwood@icloud.com>
2023-05-20 21:12:08 -0400

    add renew license tests.

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -254,7 +254,7 @@ RSpec.describe Facility do
       @facility_1.administer_written_test(@registrant_2)
     end
 
-    it 'administer_road_test' do
+    it 'administer_road_test follows services and license data rules' do
       @facility_1.administer_road_test(@registrant_3)
       expect(@registrant_3.license_data).to eq({ written: false, license: false, renewed: false })
 
@@ -269,6 +269,35 @@ RSpec.describe Facility do
 
       @facility_1.administer_road_test(@registrant_2)
       expect(@registrant_1.license_data).to eq({ written: true, license: true, renewed: false })
+    end
+  end
+
+  describe 'renew_drivers_license' do
+    before(:each) do
+      @registrant_1 = Registrant.new('Bruce', 18, true)
+      @registrant_2 = Registrant.new('Penny', 16)
+      @registrant_3 = Registrant.new('Tucker', 15)
+      @facility_1.add_service('Written Test')
+      @facility_1.earn_permit(@registrant_2)
+      @facility_1.administer_written_test(@registrant_1)
+      @facility_1.administer_written_test(@registrant_2)
+      @facility_1.add_service('Road Test')
+      @facility_1.administer_road_test(@registrant_1)
+    end
+
+    it 'renew_drivers_license follows service rule' do
+      @facility_1.renew_drivers_license(@registrant_1)
+      expect(@registrant_1.license_data).to eq({ written: true, license: true, renewed: false })
+
+      @facility_1.add_service('Renew License')
+      @facility_1.renew_drivers_license(@registrant_1)
+      expect(@registrant_1.license_data).to eq({ written: true, license: true, renewed: true })
+    end
+
+    it 'renew_drivers_license cannot renew unless registrant has a license' do
+      @facility_1.add_service('Renew License')
+      @facility_1.renew_drivers_license(@registrant_2)
+      expect(@registrant_2.license_data).to eq({ written: true, license: false, renewed: false })
     end
   end
 end
