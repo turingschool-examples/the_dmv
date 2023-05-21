cbb222ee803656b6b9e618fc2b118772a2098ceb
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-21 08:10:38 -0600

    Feat: initialize FacilityBuilder class

--- spec/new_facilities_spec.rb
+++ spec/new_facilities_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe FacilityBuilder do
 
     expect(facility).to be_a(FacilityBuilder)
   end
+
+  xit 'loads oregon locations' do
+    facility = FacilityBuilder.new()
+    or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
+    facility.load_locations(or_dmv_office_locations)
+
+    expect(facility.locations.length).to eq(59)
+    expect(facility.locations[0][:name]).to eq("Albany DMV Office")
+    expect(facility.locations[1][:address]).to eq("600 Tolman Creek Rd Ashland OR 97520")
+    expect(facility.locations[2][:phone]).to eq("503-325-3951")
+  end
 end
