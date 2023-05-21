918f1e89c351cfdebdae96ba033162f159bbbffe
Parker Boeing <parker.boeing@gmail.com>
2023-05-21 00:41:03 -0600

    Test / WIP: building out rspec to test reformatting from oregon dmv database

---
+++ spec/facility_factory_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+RSpec.describe FacilityFactory do
+  before(:each) do
+  @oregon_facilities = DmvDataService.new.or_dmv_office_locations
+  @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+  @missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+  @oregon = FacilityFactory.new
+  end
+  
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@oregon).to be_a FacilityFactory
+    end
+  end
+
+  describe '#define_facilities' do
+    it 'can put the facilities of oregon into an array' do
+      expect(@oregon.operating_facilities).to eq []
+      @oregon.define_facilities(@oregon_facilities)
+      require 'pry'; binding.pry
+      expect(@oregon.operating_facilities).to include(Facility)
+   
+    end
+
+    it 'each instantiated facility is formatted properly' do
+      expect(@facility_1.registered_vehicles).to eq []
+    end
+  end
+end
