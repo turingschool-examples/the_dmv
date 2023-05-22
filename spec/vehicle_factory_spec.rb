496aac82e11ff6374adecb51d30de6ab0e1a5618
Connor Richmond <richmond.connor@gmail.com>
2023-05-22 16:52:43 -0400

    Add test for formatting from api

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
+
+factory = VehicleFactory.new
+wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+
 describe VehicleFactory do
 
-  factory = VehicleFactory.new
-  wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+  it 'formats data to pass to Vehicle class' do
+    vehicle = {'electric_vehicle_type':'Battery Electric Vehicle (BEV)','vin_1_10':'7SAYGDEF6N','model_year':'2022','make':'TESLA','model':'Model Y'}
+
+    vehicle_formatted = {vin: '7SAYGDEF6N', year: '2022', make: 'TESLA', model: 'Model Y', engine: :ev}
+    expect(factory.format(vehicle)).to eq([vehicle_formatted])
+  end
 
 end
