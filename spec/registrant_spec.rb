9470ad6b404f309743125ecb7031abb633ae6dc8
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-05-21 21:40:53 -0500

    Add and pass initialize test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,33 +1,33 @@
-# require './lib/registrant'
+require './lib/registrant'
 
-# RSpec.describe Registrant do
+RSpec.describe Registrant do
 
-#   before(:each) do
-#     @registrant_1 = Registrant.new('Bruce', 18, true )
-#     @registrant_2 = Registrant.new('Penny', 15 )
-#   end
-#     describe '#initialize' do
-#     it 'can initialize' do
+  before(:each) do
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_2 = Registrant.new('Penny', 15 )
+  end
+    describe '#initialize' do
+    it 'exists' do
 
-#       expect(@registrant_1).to be_a(Registrant)
-#       expect(@registrant_2).to be_a(Registrant)
-#     end
-#   end
-#     describe '#name' do
+      expect(@registrant_1).to be_a(Registrant)
+      expect(@registrant_2).to be_a(Registrant)
+    end
+  end
+#     describe '#attributes' do
 #     it 'has a name' do
 
 #       expect(@registrant_1.name).to eq('Bruce')
 #       expect(@registrant_2.name).to eq('Penny')
 #     end
 #   end
-#     describe '#age' do
+
 #     it 'has an age' do
 
 #       expect(@registrant_1.age).to eq(18)
 #       expect(@registrant_2.age).to eq(15)
 #     end
 #   end 
-#     describe '#permit' do
+
 #     it 'does not have a permit by default' do
       
 
@@ -35,4 +35,4 @@
 #       expect(@registrant_2.permit?).to eq(false)
 #     end
 #   end
-# end
+end
