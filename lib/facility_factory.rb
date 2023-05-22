8af16dab46c2cb7eb6d97870c4707c81ae10a821
Kaina Cockett <kainacockett@gmail.com>
2023-05-22 15:39:06 -0600

    test: #create_facilities

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -4,16 +4,6 @@ class FacilityFactory
     end
 
     def create_facilities(facility_list)
-        output = []
-        facility_list.each do |facility|
-            output << Facility.new({
-                vin: facility[:vin_1_10], 
-                year: facility[:model_year], 
-                make: facility[:make], 
-                model: facility[:model], 
-                engine: :ev
-                })
-        end
-        output
+        #
     end
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -2,14 +2,20 @@ require 'spec_helper'
 
 RSpec.describe FacilityFactory do
   before(:each) do
+    @factory = FacilityFactory.new
     @oregon_facilities = DmvDataService.new.or_dmv_office_locations
     @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
     @missouri_facilities = DmvDataService.new.mo_dmv_office_locations
   end
   describe '#initialize' do
         it 'can initialize' do
-            factory = FacilityFactory.new
-            expect(factory).to be_an_instance_of(FacilityFactory)
+            expect(@factory).to be_an_instance_of(FacilityFactory)
+        end
+    end
+
+    describe '#create_facilities' do
+        it 'allows creating facilities' do
+            require 'pry'; binding.pry
         end
     end
 end
