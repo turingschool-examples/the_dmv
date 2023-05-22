0d1a34405bfd012c576fa83e9a957a043ce2f200
Dani Wilson <danirae.powell@gmail.com>
2023-05-22 16:50:41 -0600

    Refactor create_facility method and add find_state method

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -15,17 +15,56 @@ class VehicleFactory
         end
     end
 
+    def find_state(location)
+        if location.include?(:location_1)
+            parse_location = JSON.parse(location[:location_1][:human_address])
+            return parse_location["state"]
+        else location.include?(:state)
+            return location[:state]
+        end
+    end
+
     def create_facility(locations)
         locations.each do |location|
+            state = find_state(location)
 
+            if state == "OR"
             x = location[:location_1][:human_address]
             y = JSON.parse(x)
             string_address = y["address"] + " " + y["city"] + " " + y["state"]+ " " + y["zip"]
-                
-                new_facility = Facility.new({name: location[:title], phone: location[:phone_number], address: string_address})
-                
+            facility_details = {name: location[:title], phone: location[:phone_number], address: string_address}
             
-            return new_facility 
+            new_facility = Facility.new(facility_details)
+            return new_facility
+
+            elsif state == "NY"
+                address = location[:street_address_line_1]
+                city = location[:city]
+                state = location[:state]
+                zip = location[:zip_code]
+                string_address = address + " " + city + " " + state + " " + zip
+                phone = nil
+                if location[:public_phone_number] != nil
+                    phone = location[:public_phone_number]
+                end
+                facility_details = {name: location[:office_name], phone: phone, address: string_address}
+            
+                new_facility = Facility.new(facility_details)
+                return new_facility
+
+            elsif state == "MO"
+                address = location[:address1]
+                city = location[:city]
+                state = location[:state]
+                zip = location[:zipcode]
+                string_address = address + " " + city + " " + state + " " + zip
+            
+                facility_details = {name: location[:name], phone: location[:phone], address: string_address}
+            
+                new_facility = Facility.new(facility_details)
+                return new_facility
+            end
         end
     end
+
 end
