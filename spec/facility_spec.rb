c1de98b41fb19da4e3eea59dadc6bdb7877a622a
Anna Wiley <awiley33@gmail.com>
2023-05-22 15:12:40 -0500

    test: Add spec tests for written test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -84,4 +84,35 @@ RSpec.describe Facility do
     expect(@facility_2.registered_vehicles).to eq([])
     expect(@facility_2.collected_fees).to eq(0)
   end
+
+  it "can administer a written test" do
+    @facility_1.add_service('Written Test')
+    @facility_1.administer_written_test(@registrant_1)
+
+    expect(@registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+  end
+
+  it "can't administer written test if registrant has no permit" do
+    @facility_1.add_service('Written Test')
+    expect(@registrant_2.age).to eq(16)
+    expect(@registrant_2.permit?).to be false
+    expect(@facility_1.administer_written_test(@registrant_2)).to be false
+
+    @registrant_2.earn_permit
+
+    expect(@facility_1.administer_written_test(@registrant_2)).to be true
+    expect(@registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+  end
+
+  it "can't administer written test if registrant is under 16" do
+    @facility_1.add_service('Written Test')
+    expect(@registrant_3.age).to eq(15)
+    expect(@registrant_3.permit?).to be false
+    expect(@facility_1.administer_written_test(@registrant_3)).to be false
+
+    @registrant_3.earn_permit
+
+    expect(@facility_1.administer_written_test(@registrant_3)).to be false
+    expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+  end
 end
