833d61072234c602f1742930d273de750e322b0f
pcbennett108 <pcbennett42@gmail.com>
2023-05-22 16:26:30 -0600

    Add zipcode_locator method and seperate or_facilities method

--- lib/facility_locations.rb
+++ lib/facility_locations.rb
@@ -4,8 +4,31 @@ require './lib/dmv_data_service'
 
 class FacilityLocations
   def create_facilities(facilities)
+    if facilities.first.include?(:zip_code)
+      zipcode = facilities.first[:zip_code]
+    else
+      zipcode = facilities.first[:zipcode]
+    end
+    location = zipcode_locator(zipcode)
+    if location == "OR"
+      or_facilities(facilities)
+    end
+  end
+
+  def zipcode_locator(zipcode)
+    zipcode = zipcode.to_i
+    if zipcode.between?(97001, 97920)
+      return "OR"
+    elsif zipcode.between?(63001, 65899)
+      return "MO"
+    elsif zipcode.between?(10001, 14975) || zipcode == 6390
+      return "NY"
+    end
+  end
+
+  def or_facilities(facilities)
     facilities.map do |facility|
-      formatted_address = format_address(facility[:location_1][:human_address])
+      formatted_address = format_or_address(facility[:location_1][:human_address])
       Facility.new({
         name: facility[:title],
         address: formatted_address,
@@ -14,7 +37,7 @@ class FacilityLocations
     end
   end
 
-  def format_address(raw_address)
+  def format_or_address(raw_address)
     parsed = JSON.parse(raw_address)
     parsed.values.join(" ")
   end
@@ -23,13 +46,16 @@ class FacilityLocations
 end
 
 
-#locations = FacilityLocations.new
-# or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
+locations = FacilityLocations.new
+or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
 
-  
+  #* Oregon 97***
+  #* NY 6390 OR 10001 - 14975
+  #* MO 6390 OR 63001 - 65899
 
   # raw = "{\"address\": \"2242 Santiam Hwy SE\", \"city\": \"Albany\", \"state\": \"OR\", \"zip\": \"97321\"}"
 
   # parsed = JSON.parse(raw_2)
   # p parsed
   # p parsed.values.join(" ")
+p or_dmv_office_locations.first.include?(:zip_code)
