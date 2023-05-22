7145aee26062a7c2b5938f4aef8e427836a1662e
Mike Wood <mikewwood@icloud.com>
2023-05-22 00:46:10 -0400

    Test: updates test to validate website, type, and agency for Oregon.

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -356,16 +356,18 @@ RSpec.describe Facility do
       expect(oregon_facilities[0].name).to eq(expected_data[:title])
       expected_address = JSON.parse(expected_data[:location_1][:human_address]).values.join(', ')
       expect(oregon_facilities[0].address).to eq(expected_address)
-      expect(oregon_facilities[0].address).to eq(expected_address)
       expect(oregon_facilities[0].phone).to eq(expected_data[:phone_number])
+      expect(oregon_facilities[0].website).to eq(expected_data[:website])
+      expect(oregon_facilities[0].type).to eq(expected_data[:type])
+      expect(oregon_facilities[0].agency).to eq(expected_data[:agency])
 
       expected_data = facility_data[1]
 
       expect(oregon_facilities[1].name).to eq(expected_data[:title])
       expected_address = JSON.parse(expected_data[:location_1][:human_address]).values.join(', ')
       expect(oregon_facilities[1].address).to eq(expected_address)
-      expect(oregon_facilities[1].address).to eq(expected_address)
       expect(oregon_facilities[1].phone).to eq(expected_data[:phone_number])
+      expect(oregon_facilities[1].website).to eq(expected_data[:website])
     end
   end
 end
