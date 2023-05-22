a41d5b56b3abd145c2ea7fa85e6fa54850edc15b
andi lovetto <andilovetto@gmail.com>
2023-05-22 16:19:03 -0600

    test: Refactor test

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe VehicleFactory do
   it 'creates vehicle objects' do
     factory = VehicleFactory.new
     wa_ev_registrations = DmvDataService.new.wa_ev_registrations
-    expect(factory.create_vehicles(wa_ev_registrations)).to be_an_instance_of(Array)
     wa_vehicles = factory.create_vehicles(wa_ev_registrations)
+    expect(wa_vehicles).to be_an_instance_of(Array)
     wa_vehicles.each do |vehicle|
       expect(vehicle).to be_an_instance_of(Vehicle)
     end
