4749711de4e9d57f4d64d770aa684cf8be850b49
Derek Chavez <frrenose17@yahoo.com>
2023-05-22 17:27:43 -0600

    Refactor test and methods for vehicle factory

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -4,8 +4,8 @@ require './lib/dmv_data_service'
 
 RSpec.describe VehicleFactory do
     before(:each) do
-        @factory = VehicleFactory.new
         @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+        @factory = VehicleFactory.new
     end
     
     describe '#it exists' do
@@ -16,11 +16,8 @@ RSpec.describe VehicleFactory do
 
     describe '#create vehicles' do
         it 'creates vehicles using the WA EV registrations' do
-            vehicle_1 = Vehicle.new(@engine=:ev, @make="TOYOTA", @model="Prius Plug-in", @plate_type=nil, @registration_date=nil, @vin="JTDKN3DP8D", @year="2013")
-            vehicle_2 = Vehicle.new(@engine=:ev, @make="TOYOTA", @model="Prius Prime", @plate_type=nil, @registration_date=nil, @vin="JTDKARFP9J", @year="2018")
-            vehicle_3 = Vehicle.new(@engine=:ev, @make="NISSAN", @model="Leaf", @plate_type=nil, @registration_date=nil, @vin="1N4AZ1CP0J", @year="2018")
-            new_vehicles = [vehicle_1, vehicle_2, vehicle_3]
-            expect(@factory.create_vehicles(wa_ev_registrations)).to eq(new_vehicles)
+            vehicles = @factory.create_vehicles(@wa_ev_registrations)
+            expect(vehicles[0]).to be_an_instance_of(Vehicle)
         end
     end
 end
