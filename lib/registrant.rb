a4414afbc76fa31fce73076d90b477b366e26b98
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-05-21 21:40:17 -0500

    Add class Registrant, attr_reader, and initialize method

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,15 +1,19 @@
-# class Registrant
-# attr_reader :name, :age, :permit, :license_data
-#   def initialize(name, age, permit = false)
-#     @name = name
-#     @age = age
-#     @permit = permit
-#     @license_data = {
-#       :written=>false,
-#       :license=>false,
-#       :renewed=>false
-#     }
-#   end
+class Registrant
+attr_reader :name,
+            :age, 
+            :permit, 
+            :license_data
+
+  def initialize(name, age, permit = false)
+    @name = name
+    @age = age
+    @permit = permit
+    @license_data = {
+      :written=>false,
+      :license=>false,
+      :renewed=>false
+    }
+  end
 
 #   def permit?
 #     @permit == true 
@@ -18,4 +22,4 @@
 #   def earn_permit
 #     @permit = true 
 #   end
-# end
+end
