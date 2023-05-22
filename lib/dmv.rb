0cbdea3fc2a5557bdc71df8d5896c4b6209ee077
Allen Russell <garyrusselljr@gmail.com>
2023-05-21 19:14:56 -0600

    fix: create_facilities method to activate hash objects for parsing

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -16,40 +16,50 @@ class Dmv
   end
 
   def oregon_parse(oregon)
-    oregon.select do |office|
+    oregon.map do |office|
       name = office[:title]
       address_to_parse = JSON.parse(office[:location_1][:human_address])
       address = address_to_parse.values.join(" ")
       phone = office[:phone_number]
 
-      new_facility = Facility.new(name: name, address: address, phone: phone)
-      @facilities << new_facility
+      {name: name, address: address, phone: phone}
     end   
   end
 
   def new_york_parse(new_york)
-    new_york.select do |office|
+    new_york.map do |office|
       name = office[:office_name]
       address = office[:street_address_line_1]
       phone = office[:public_phone_number]
 
-      new_facility = Facility.new(name: name, address: address, phone: phone)
-      @facilities << new_facility
+      {name: name, address: address, phone: phone}
     end
   end
 
   def missouri_parse(missouri)
-    missouri.select do |office|
+    missouri.map do |office|
       name = office[:name]
       address = office[:address1]
       phone = office[:phone]
 
-      new_facility = Facility.new(name: name, address: address, phone: phone)
-      @facilities << new_facility
+      {name: name, address: address, phone: phone}
     end
   end
 
   def create_facilities(dmv_office)
-    @facilities
+    if dmv_office.first.has_key?(:title)
+      locations = oregon_parse(dmv_office)
+    elsif dmv_office.first.has_key?(:office_name)
+      locations = new_york_parse(dmv_office)
+    elsif dmv_office.first.has_key?(:name)
+      locations = missouri_parse(dmv_office)
+    else
+      nil
+    end
+    
+    locations.map do |location|
+      facility = Facility.new(location)
+      @facilities << facility
+    end
   end
 end
