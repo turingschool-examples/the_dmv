3e97d1deac46f57fa41450d14b9f10d8e6748ef0
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-05-22 17:14:29 -0500

    Add methods register_vehicle and collect_fees

--- lib/facility.rb
+++ lib/facility.rb
@@ -13,10 +13,27 @@ class Facility
     @services = []
     @registered_vehicles = []
     @collected_fees = 0
-
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    @services.include?('Vehicle Registration')
+      collect_fees(vehicle)
+      vehicle.registration_date = Date.today
+      vehicle.set_plate_type
+      @registered_vehicles << vehicle
+  end
+
+  def collect_fees(vehicle)
+    if vehicle.antique?
+      @collected_fees += 25
+    elsif vehicle.electric_vehicle?
+      @collected_fees += 200
+    else
+      @collected_fees += 100
+    end
   end
 end
