f72095f0a6610d659d47db36a6e50658f9056c2d
Connor Richmond <richmond.connor@gmail.com>
2023-05-22 16:53:29 -0400

    Add test for creating vehicles from api

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -13,4 +13,18 @@ describe VehicleFactory do
     expect(factory.format(vehicle)).to eq([vehicle_formatted])
   end
 
+  it 'creates vehicles' do
+    vehicles = factory.create_vehicles(wa_ev_registrations)
+
+    expect(vehicles).to be_a(Array)
+    expect(vehicles.first).to be_a(Vehicle)
+    expect(vehicles.first.vin).to eq('7SAYGDEF6N')
+    expect(vehicles.first.year).to eq('2022')
+    expect(vehicles.first.make).to eq('TESLA')
+    expect(vehicles.first.model).to eq('Model Y')
+    expect(vehicles.first.engine).to eq(:ev)
+    expect(vehicles.first.plate_type).to eq(nil)
+    expect(vehicles.first.registration_date).to eq(nil)
+  end
+
 end
