6ab238074ab7f704d0252ebac0b22d0d13c44668
pcbennett108 <pcbennett42@gmail.com>
2023-05-22 17:34:49 -0600

    Add facility builder and formatter methods for NY data

--- lib/facility_locations.rb
+++ lib/facility_locations.rb
@@ -10,19 +10,21 @@ class FacilityLocations
       zipcode = facilities.first[:zipcode]
     end
     location = zipcode_locator(zipcode)
-    if location == "OR"
+    if location == "or"
       or_facilities(facilities)
+    elsif location == "ny"
+      ny_facilities(facilities)
     end
   end
 
   def zipcode_locator(zipcode)
     zipcode = zipcode.to_i
     if zipcode.between?(97001, 97920)
-      return "OR"
+      return "or"
     elsif zipcode.between?(63001, 65899)
-      return "MO"
+      return "mo"
     elsif zipcode.between?(10001, 14975) || zipcode == 6390
-      return "NY"
+      return "ny"
     end
   end
 
@@ -36,26 +38,53 @@ class FacilityLocations
       })
     end
   end
-
+  
   def format_or_address(raw_address)
     parsed = JSON.parse(raw_address)
     parsed.values.join(" ")
   end
+  
+  def ny_facilities(facilities)
+    facilities.map do |facility|
+      formatted_address = format_ny_address(facility)
+      Facility.new({
+        name: facility[:office_name],
+        address: formatted_address,
+        phone: "N/A",
+      })
+    end
+  end
+  
+  def format_ny_address(facility)
+    address = []
+    address.push(facility[:street_address_line_1])
+    if facility.include?(:street_address_line_2)
+      address.push(facility[:street_address_line_2])
+    end
+    address.push(facility[:city])
+    address.push(facility[:state])
+    address.push(facility[:zip_code])
+    address.join(" ")
+  end
+
+
+
 
 
 end
 
 
-locations = FacilityLocations.new
-or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
+# locations = FacilityLocations.new
+# ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
 
-  #* Oregon 97***
-  #* NY 6390 OR 10001 - 14975
-  #* MO 6390 OR 63001 - 65899
+#   #* Oregon 97***
+#   #* NY 6390 OR 10001 - 14975
+#   #* MO 6390 OR 63001 - 65899
 
-  # raw = "{\"address\": \"2242 Santiam Hwy SE\", \"city\": \"Albany\", \"state\": \"OR\", \"zip\": \"97321\"}"
+#   # raw = "{\"address\": \"2242 Santiam Hwy SE\", \"city\": \"Albany\", \"state\": \"OR\", \"zip\": \"97321\"}"
 
-  # parsed = JSON.parse(raw_2)
-  # p parsed
-  # p parsed.values.join(" ")
-p or_dmv_office_locations.first.include?(:zip_code)
+#   # parsed = JSON.parse(raw_2)
+#   # p parsed
+#   # p parsed.values.join(" ")
+# ny_location = ny_dmv_office_locations[4]
+# p locations.format_ny_address(ny_location)
