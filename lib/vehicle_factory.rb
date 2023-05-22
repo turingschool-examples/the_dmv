e2df1028385a78c0a8974493200a6b1805a51432
Derek Chavez <frrenose17@yahoo.com>
2023-05-22 15:19:56 -0600

    test: create vehicles test

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,4 +1,4 @@
 class VehicleFactory
-
     
+
 end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe VehicleFactory do
             expect(@factory).to be_an_instance_of(VehicleFactory)
         end
     end
+
+    describe '#create vehicles' do
+        it 'creates vehicles using the WA EV registrations' do
+            vehicle_1 = Vehicle.new(@engine=:ev, @make="TOYOTA", @model="Prius Plug-in", @plate_type=nil, @registration_date=nil, @vin="JTDKN3DP8D", @year="2013")
+            vehicle_2 = Vehicle.new(@engine=:ev, @make="TOYOTA", @model="Prius Prime", @plate_type=nil, @registration_date=nil, @vin="JTDKARFP9J", @year="2018")
+            vehicle_3 = Vehicle.new(@engine=:ev, @make="NISSAN", @model="Leaf", @plate_type=nil, @registration_date=nil, @vin="1N4AZ1CP0J", @year="2018")
+            new_vehicles = [vehicle_1, vehicle_2, vehicle_3]
+            expect(@factory.create_vehicles(wa_ev_registrations)).to eq(new_vehicles)
+        end
+    end
 end
