13df6f9e427a8f1acf3f1e7e25fe0723c9997442
Connor Richmond <richmond.connor@gmail.com>
2023-05-22 15:04:51 -0400

    Add facilities

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 registrant_1 = Registrant.new('Bruce', 18, true )
 registrant_2 = Registrant.new('Penny', 15 )
 registrant_3 = Registrant.new('Tucker', 15 )
+facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
 
 describe Registrant do
   it 'checks registrant_1' do
@@ -23,5 +25,12 @@ describe Registrant do
     registrant_2.earn_permit
     expect(registrant_2.permit?).to eq(true)
   end
+
+  it 'gives tests for license' do
+    expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    expect(registrant_1.permit?).to be true
+    expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+  end
+
 end
 
