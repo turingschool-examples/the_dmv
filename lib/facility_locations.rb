6e8a659466e13ac69d6445482162f65a2a460823
Connor Richmond <richmond.connor@gmail.com>
2023-05-22 18:58:07 -0400

    Add methods to format taken data so readable

--- lib/facility_locations.rb
+++ lib/facility_locations.rb
@@ -10,3 +10,16 @@ class FacilityLocation
     facilities
   end
 
+  def formatted_location(locations)
+    dmv_formatted = []
+    [locations].flatten.each do |location|
+      location_data = {}
+      location_data[:name] = (location[:title] || location[:office_name] || location[:name])
+      location_data[:address] = parsed_address(location)
+      location_data[:phone] = (location[:phone_number] || location[:public_phone_number] || location[:phone])
+      dmv_formatted << location_data
+    end
+    dmv_formatted
+  end
+
+end
