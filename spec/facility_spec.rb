b4f5e9d5cc63eb4ba835ab7d67708265ab8daa61
Connor Richmond <richmond.connor@gmail.com>
2023-05-22 15:43:14 -0400

    Refactor and test for written test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -30,6 +30,9 @@ facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Cree
 cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
 bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
 camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+registrant_1 = Registrant.new('Bruce', 18, true )
+registrant_2 = Registrant.new('Penny', 15 )
+registrant_3 = Registrant.new('Tucker', 15 )
 
 describe Facility do
 
@@ -70,6 +73,12 @@ describe Facility do
     expect(facility_2.collected_fees).to eq(0)
   end
 
-
+  it 'gives written test'  do
+    expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    expect(registrant_1.permit?).to be true
+    expect(facility_1.administer_written_test(registrant_1)).to be false
+    facility_1.add_service('Written Test')
+    expect(facility_1.administer_written_test(registrant_1)).to be true
+  end
 
 end
