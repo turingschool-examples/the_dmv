86f44d3c6f1f58b84d69e0db42fdad3f5d2bce21
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-21 20:12:32 -0600

    Tests and Feat: NY and MO

--- lib/new_facilities.rb
+++ lib/new_facilities.rb
@@ -8,8 +8,14 @@ class FacilityBuilder
   end
 
   def create_facilities(source)
-      oregon_locations(source)
-      @locations
+    if source == DmvDataService.new.or_dmv_office_locations
+    oregon_locations(source)
+  elsif source == DmvDataService.new.ny_dmv_office_locations
+    nys_locations(source)
+  elsif source == DmvDataService.new.mo_dmv_office_locations
+    mo_locations(source)
+    end
+    @locations
   end
 
   def oregon_locations(source)
@@ -21,10 +27,32 @@ class FacilityBuilder
         or_location[:address] = address
         or_location[:name] = office[:title]
         or_location[:phone] = office[:phone_number]
-        
         @locations << Facility.new(or_location)
+      
       end
     end
 
- 
+    def nys_locations(source)
+
+      source.each do |office|
+        ny_location = {}
+        ny_location[:address] = office[:street_address_line_1], office[:city], office[:state], office[:zip_code]
+        ny_location[:name] = office[:office_name]
+        ny_location[:phone] = office[:public_phone_number]
+        @locations << Facility.new(ny_location)
+
+      end
+    end
+
+    def mo_locations(source)
+
+      source.each do |office|
+        mo_location = {}
+        mo_location[:address] = office[:address1], office[:city], office[:state], office[:zipcode]
+        mo_location[:name] = office[:office_name]
+        mo_location[:phone] = office[:phone]
+        @locations << Facility.new(mo_location)
+  
+      end
+    end
 end
--- spec/new_facilities_spec.rb
+++ spec/new_facilities_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe FacilityBuilder do
     expect(facility).to be_a(FacilityBuilder)
   end
 
-  it 'recognizes data types from JSON' do 
+  it 'recognizes data types from Oregon JSON' do 
     or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
 
     expect(or_dmv_office_locations).to be_a(Array)
@@ -20,5 +20,37 @@ RSpec.describe FacilityBuilder do
 
     expect(or_facilities).to be_a(Array)
     expect(or_facilities.first).to be_a(Facility)
+  end
+
+  describe '#NYS locations tests' do 
+    it 'can work with NYS JSON file' do
+      ny_dmv_office_locations =  DmvDataService.new.ny_dmv_office_locations
+      
+      expect(ny_dmv_office_locations).to be_a(Array)
+      expect(ny_dmv_office_locations.first).to be_a(Hash)
+      expect(ny_dmv_office_locations.length).to eq(172)
+
+      facility = FacilityBuilder.new
+      ny_facilities = facility.create_facilities(ny_dmv_office_locations)
+
+      expect(ny_facilities).to be_a(Array)
+      expect(ny_facilities.first).to be_a(Facility)
+    end
+  end
+
+  describe '#MO locations tests' do 
+    it 'can work with MO JSON file' do
+      mo_dmv_office_locations =  DmvDataService.new.mo_dmv_office_locations
+      
+      expect(mo_dmv_office_locations).to be_a(Array)
+      expect(mo_dmv_office_locations.first).to be_a(Hash)
+      expect(mo_dmv_office_locations.length).to eq(178)
+
+      facility = FacilityBuilder.new
+      mo_facilities = facility.create_facilities(mo_dmv_office_locations)
+
+      expect(mo_facilities).to be_a(Array)
+      expect(mo_facilities.first).to be_a(Facility)
+    end
   end
 end
