7aa8da81ef0a4b17072f47da331ea99c58613553
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-05-22 17:29:39 -0500

    test: Add 'register a vehicle and collect fees' to pass

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -44,8 +44,36 @@ RSpec.describe Facility do
       expect(@cruz.registration_date).to eq(nil)
       expect(@facility_1.registered_vehicles).to eq([])
       expect(@facility_1.collected_fees).to eq(0)
-
     end
+  end
+
+  it 'can register a vehicle and collect fees' do
+
+
+    @facility_1.add_service('Vehicle Registration')
+    @facility_1.register_vehicle(@cruz)
+
+
+    expect(@cruz.registration_date).to eq(Date.today)
+    expect(@cruz.plate_type).to eq(:regular)
+    expect(@facility_1.registered_vehicles.first).to eq(@cruz)
+    expect(@facility_1.collected_fees).to eq(100)
+
+    @facility_1.register_vehicle(@camaro)
+
+    expect(@camaro.registration_date).to eq(Date.today)
+    expect(@camaro.plate_type).to eq(:antique)
+
+    @facility_1.register_vehicle(@bolt)
+
+    expect(@bolt.registration_date).to eq(Date.today)
+    expect(@bolt.plate_type).to eq(:ev)
+
+
+    expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro, @bolt])
+    expect(@facility_1.collected_fees).to eq(325)
+    expect(@facility_2.registered_vehicles).to eq([])
+    expect(@facility_2.services).to eq([])
   end
 end
 
