f02c914049064afca28c7592c950a0a728452aa1
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-21 19:34:51 -0600

    create tests for the dmv created  facility instances atributes for NY OR and that other state

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -10,30 +10,34 @@ RSpec.describe Dmv do
 
   describe '#add facilities' do
 
-  it "can create facility instances" do 
-    @dmv.add_facility(@new_facility{})
-    expect(@new_facility).to be_an_instance_of(Facility) 
-  end 
+    it "can create facility instances" do 
+      @dmv.add_facility({})
+      expect(@dmv.facilities[0]).to be_an_instance_of(Facility) 
+    end 
 
-    it 'can add available facilities' do
-      @dmv.add_facility(@facility_1)
-      expect(@dmv.facilities).to eq([@facility_1])
+    it 'created instances are stored in an array' do
+      @dmv.add_facility({})
+      @dmv.add_facility({})
+      expect(@dmv.facilities).to be_an(Array)
+      expect(@dmv.facilities.count).to eq(2)
     end
   end
 
   describe '#facilities_offering_service' do
     it "can add services to a facility" do 
-      @dmv.add_facility(@facility_1)
-      @facility_1.add_service('Road Test')
-      expect(@facility_1.services).to eq("Road Test")
-    end 
+      @dmv.add_facility({})
+      @dmv.facilities[0].add_service('Road Test')
+      expect(@dmv.facilities[0].services).to eq(['Road Test'])
+    end
 
     it 'can return list of facilities offering a specified Service' do
-      @dmv.add_facility(@facility_1)
-      @dmv.add_facility(@facility_2)
-      @facility_1.add_service('New Drivers License')
-      @facility_2.add_service('New Drivers License')
-      expect(@dmv.facilities_offering_service('New Drivers License')).to eq([@facility_1, @facility_2])
+      @dmv.add_facility({})
+      @dmv.add_facility({})
+      facility_0 = @dmv.facilities[0]
+      facility_1 = @dmv.facilities[1]
+      facility_0.add_service("Road Test")
+      facility_1.add_service("Road Test")
+      expect(@dmv.facilities_offering_service('Road Test')).to eq([facility_0, facility_1])
     end
 
 
@@ -41,26 +45,30 @@ RSpec.describe Dmv do
       or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
       new_york_facilities = DmvDataService.new.ny_dmv_office_locations
       @dmv.add_facility(or_dmv_office_locations)
-      @dmv.add_facility(ny_dmv_office_locations)
-      expect(@dmv.facilities[0].count).to eq(59)
-      expect(@dmv.facilities[1].count).to eq(172)
+      @dmv.add_facility(new_york_facilities)
+      expect(@dmv.facilities.count).to eq(231)
     end 
 
     it "can organize data into specified key value pairs" do 
       or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
       new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+      missouri_facilities = DmvDataService.new.mo_dmv_office_locations
       @dmv.add_facility(or_dmv_office_locations)
-      @dmv.add_facility(ny_dmv_office_locations)
-      
-      expect(@dmv.facilities[0][0].name).to eq ("Albany DMV Office")
-      expect(@dmv.facilities[1][0].name).to eq ("JAMAICA KIOSK")
+      @dmv.add_facility(new_york_facilities)
+      @dmv.add_facility(missouri_facilities)
 
-      expect(@dmv.facilities[0][0].address).to eq ("2242 Santiam Hwy SE")
-      expect(@dmv.facilities[1][0].address).to eq ("168-46 91ST AVE., 2ND FLR")
+      expect(@dmv.facilities[0].name).to eq ("Albany DMV Office")
+      expect(@dmv.facilities[59].name).to eq ("JAMAICA KIOSK")
+      expect(@dmv.facilities[231].name).to eq ("FERGUSON-OFFICE CLOSED UNTIL FURTHER NOTICE")
 
-      expect(@dmv.facilities[0][0].phone_number).to eq ("541-967-2014")
-      expect(@dmv.facilities[1][0].phone_number).to eq (nil)
-      expect(@dmv.facilities[1][1].phone_number).to eq ("5857531604")
+      expect(@dmv.facilities[0].address).to eq ("2242 Santiam Hwy SE")
+      expect(@dmv.facilities[59].address).to eq ("168-46 91ST AVE., 2ND FLR")
+      expect(@dmv.facilities[231].address).to eq ("10425 WEST FLORISSANT")
+
+      expect(@dmv.facilities[0].phone).to eq ("541-967-2014")
+      expect(@dmv.facilities[59].phone).to eq (nil)
+      expect(@dmv.facilities[60].phone).to eq ("5857531604")
+      expect(@dmv.facilities[231].phone).to eq ("(314) 733-5316")
     end  
   end
 end
