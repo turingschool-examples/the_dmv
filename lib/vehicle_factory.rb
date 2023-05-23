acff52aabbe12e3d916d9305e6b95ff22aec0370
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-23 16:23:34 -0500

    refactor code for create_vehicle method, and add code to create_facility method

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -3,7 +3,19 @@ class VehicleFactory
     def initialize 
     end
 
-    def create_vehicles(locations)
-        new_vehicles = Vehicle.new({vin: locations[:vin_1_10], year: locations[:model_year], make: locations[:make], model: locations[:model], engine: [:ev]})
+    def create_vehicles(registration_locations)
+        registration_locations.each do |vehicle|
+            vehicle_details = ({vin: vehicle[:vin_1_10] , year: vehicle[:model_year], make: vehicle[:make], model: vehicle[:model], engine: [:ev]})
+            new_vehicles = Vehicle.new(vehicle_details)
+            return new_vehicles
+        end
+    end
+
+    def create_facility(facility_locations)
+        facility_locations.each do |facility|
+            facility_details = ({name: facility[:name], address: facility[:address], phone: facility[:phone]})
+            facility_locations = Facility.new(facility_locations)
+            return locations 
+        end 
     end
 end
