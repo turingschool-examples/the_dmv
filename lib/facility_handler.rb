6ed94b1015d4752ee5878bafb7f54899b989ca94
Chris Asercion <chris.asercion@gmail.com>
2023-05-23 11:54:35 -0600

    Scraps facility_handler test

--- lib/facility_handler.rb
+++ lib/facility_handler.rb
@@ -1,15 +1,26 @@
+
 class Facility_handler
   def create_facilities(dmv_locations)
     facility_data_sorter(dmv_locations)
-    dmv_locations.map do |location|
+    new_facility = @dmv_key.map do |location|
+    require 'pry'; binding.pry
+      Facility.new(location)
+    end 
   end
-  def facility_data_sorter(dmv_locations)
-    if dmv_locations ==  @or_dmv_office_locations
-      dmv_locations.each do |data|
-        data[:name] = data[:title]
-        data[:phone] = data[:phone_number]
-        data[:address] = data[:location_1][:human_address]
-      end
+  def name_sorter(data)
+    
+  end
+end
+  # def facility_data_sorter(dmv_locations)
+  #   if dmv_locations[1][:website].include?('oregon')
+  #     @dmv_key = dmv_locations.each do |data|
+  #       data[:name] = data[:title]
+  #       @names << data[:name]
+  #       data[:phone] = data[:phone_number]
+  #       @phone << data[:phone]
+  #       # data[:address] = data[:location_1][:human_address]
+  #     end
+  #   end
     # elsif  dmv_locations = mo_dmv_office_locations
     #   dmv_locations.each do |data|
     #     data[:name] = data[:office_name]
@@ -19,8 +30,3 @@ class Facility_handler
     # else 
     #   dmv_locations.each do |data|
     #     data[:address] = data[:address1][:city][:state][:zipcode].to_s.join
-    end
-    
-
-  end
-end
--- spec/facility_handler_spec.rb
+++ spec/facility_handler_spec.rb
@@ -1,11 +1,14 @@
 require 'pry'
 require './lib/facility_handler'
 require './lib/dmv_data_service'
+require './lib/facility'
 
 RSpec.describe Facility_handler do
   it 'can parce data from api sources' do
     facility_array = Facility_handler.new
-    expect(facility_array).to be_a(Array)
+    oregon = DmvDataService.new.or_dmv_office_locations
+    oregon_facility = facility_array.create_facilities(oregon)
+    expect(oregon_facility).to be_a(Array)
   end
   #@or_dmv_office_locations
 
