8421e00a0b2dfd689a3d994efd8b7d1ec205e525
andi lovetto <andilovetto@gmail.com>
2023-05-23 15:11:17 -0600

    feat: Add MO helper methods

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -18,7 +18,7 @@ class FacilityFactory
       # formatted_phone = phone ? phone.insert(3, '-').insert(-5, '-') : nil
       formatted_phone = facility[:public_phone_number].insert(3, '-').insert(-5, '-') unless facility[:public_phone_number] == nil
       facility_details = {}
-      facility_details[:name] = facility[:office_name]
+      facility_details[:name] = facility[:office_name].split.map(&:capitalize).join(" ")
       facility_details[:phone] = formatted_phone
       facility_details[:address] = formatted_ny_address
       Facility.new(facility_details)
@@ -27,13 +27,21 @@ class FacilityFactory
 
   def create_mo_facilities(facilities)
     facilities.map do |facility|
-      formatted_phone = facility[:phone].delete(" ()").insert(3, "-") unless facility[:phone] == nil
-      formatted_mo_address = facility[:address1] + ", " + facility[:city] + ", " + facility[:state] + ", " + facility[:zipcode]
+      #require 'pry'; binding.pry
       facility_details = {}
       facility_details[:name] = facility[:name]
-      facility_details[:phone] = formatted_phone
-      facility_details[:address] = formatted_mo_address
+      facility_details[:phone] = formatted_mo_phone(facility[:phone])
+      facility_details[:address] = formatted_mo_address(facility)
       Facility.new(facility_details)
     end
+  end
+
+  def formatted_mo_address(facility)
+    new_address = facility[:address1] + ", " + facility[:city] + ", " + facility[:state] + ", " + facility[:zipcode]
+    new_address.split.map(&:capitalize).join(' ')
+  end
+
+  def formatted_mo_phone(number)
+    number.delete(" ()").insert(3, "-") unless number == nil
   end
 end
