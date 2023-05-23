fb8794195a62c953a175d22cca76af0ca7a974c2
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-22 19:10:08 -0800

    feat: administer road test method

--- lib/facility.rb
+++ lib/facility.rb
@@ -41,4 +41,10 @@ class Facility
       registrant.license_data[:written] = true
     end
   end
+
+  def administer_road_test(registrant)
+    if @services.include?('Road Test') && registrant.license_data[:written] == true
+      registrant.license_data[:license] = true
+    end
+  end
 end
