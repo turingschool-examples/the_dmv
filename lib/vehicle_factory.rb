a9c7c8b20b3af9f02d3514c5943028898d012560
Artemy Gibson <artemygibson@gmail.com>
2023-05-23 15:28:32 -0700

    Pass single vehicle edge case test

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,7 +1,9 @@
 class VehicleFactory
   def create_vehicles(registrations)
-    registrations.map do |vehicle|
+    registrations = [registrations].flatten.map do |vehicle|
       Vehicle.new({vin: vehicle[:vin_1_10], year: vehicle[:model_year].to_i, make: vehicle[:make], model: vehicle[:model], engine: :ev})
     end
+    return registrations if registrations.length > 1
+    registrations.first if registrations.length == 1
   end
 end
