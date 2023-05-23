5f743aea6a3802d7ee67d578cfca741b390abed4
Wil Fady <wrfady@gmail.com>
2023-05-23 17:33:01 -0400

    Refactor facility creator test into multiple tests for each state

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -13,42 +13,55 @@ RSpec.describe FacilityFactory do
       expect(oregon_facilities[0][:location_1]).to be_a(Hash)
     end
   end
+  
   describe `#create_oregon_facility` do
     it 'can create facility objects from oregon dmv data' do
-      factory = FacilityFactory.new
-      oregon_facilities = DmvDataService.new.or_dmv_office_locations
+      or_dmv_facilities = DmvDataService.new.or_dmv_office_locations
       
-      expect(factory.create_oregon_facility(oregon_facilities)[0]).to be_a(Facility)
-      expect(factory.create_oregon_facility(oregon_facilities)[0].address).to be_a(String)
-      expect(factory.create_oregon_facility(oregon_facilities)).to be_a(Array)
+      expect(or_dmv_facilities).to be_a(Array)
+      expect(or_dmv_facilities.first).to be_a(Hash)
+      or_dmv_facilities.each do |or_dmv|
+        expect(or_dmv[:location_1][:human_address]).to be_a(String)
+      end
+      
+      facility = FacilityFactory.new
+      oregon = facility.facility_creator(or_dmv_facilities)
+      expect(oregon).to be_a(Array)
+      expect(oregon.first).to be_a(Facility)
+    end
+  end
+
+  describe `#create_new_york_facility` do
+    it 'can create facility objects from new york dmv data' do
+      # factory = FacilityFactory.new
+      new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+      
+      expect(new_york_facilities).to be_a(Array)
+      expect(new_york_facilities.first).to be_a(Hash)
+      new_york_facilities.each do |ny_dmv|
+        expect(ny_dmv[:office_name]).to be_a(String)
+      end
+
+      facility = FacilityFactory.new
+      ny = facility.facility_creator(new_york_facilities)
+      expect(ny).to be_a(Array)
+    end
+  end
+
+  describe `#create_missouri_facility` do
+    it 'can create facility objects from oregon dmv data' do
+      # factory = FacilityFactory.new
+      missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+      
+      expect(missouri_facilities).to be_a(Array)
+      expect(missouri_facilities.first).to be_a(Hash)
+      missouri_facilities.each do |mo_dmv|
+        expect(mo_dmv[:name]).to be_a(String)
+      end
+
+      facility = FacilityFactory.new
+      mo = facility.facility_creator(missouri_facilities)
+      expect(mo).to be_a(Array)
     end
   end
 end  
-
-
-
-
-
-
-
-
-
-
-#     it 'can create facility objects from multiple data sources' do
-#       factory = FacilityFactory.new
-#       oregon_facilities = DmvDataService.new.or_dmv_office_locations
-#       new_york_facilities = DmvDataService.new.ny_dmv_office_locations
-#       missouri_facilities = DmvDataService.new.mo_dmv_office_locations
-
-#       expect(factory.create_facility(oregon_facilities)[0]).to be_a(Facility)
-#       # expect(factory.create_facility(new_york_facilities)[0]).to be_a(Facility)
-#       # expect(factory.create_facility(missouri_facilities)[0]).to be_a(Facility)
-
-#     end
-
-#   describe '#facility_name' do
-#     xit 'matches facility object name to dmv name' do
-
-#     end
-#   end
-# end
