020edfef2e2ea21e801c1c4934c6901e709d62ce
Wil Fady <wrfady@gmail.com>
2023-05-23 19:22:28 -0400

    Fix formatting

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -18,50 +18,60 @@ RSpec.describe FacilityFactory do
     it 'can create facility objects from oregon dmv data' do
       or_dmv_facilities = DmvDataService.new.or_dmv_office_locations
       
-      expect(or_dmv_facilities).to be_a(Array)
-      expect(or_dmv_facilities.first).to be_a(Hash)
-      or_dmv_facilities.each do |or_dmv|
-        expect(or_dmv[:location_1][:human_address]).to be_a(String)
-      end
-      
       facility = FacilityFactory.new
       oregon = facility.facility_creator(or_dmv_facilities)
       expect(oregon).to be_a(Array)
       expect(oregon.first).to be_a(Facility)
+
+      expect(or_dmv_facilities).to be_a(Array)
+      expect(or_dmv_facilities.first).to be_a(Hash)
+     
+      or_dmv_facilities.each do |or_dmv|
+        expect(or_dmv[:title]).to be_a(String)
+        expect(or_dmv[:phone_number]).to be_a(String)
+        expect(or_dmv[:location_1]).to have_key(:human_address)
+        expect(or_dmv[:location_1][:human_address]).to be_a(String)
+      end
     end
   end
 
   describe `#create_new_york_facility` do
     it 'can create facility objects from new york dmv data' do
-      # factory = FacilityFactory.new
       new_york_facilities = DmvDataService.new.ny_dmv_office_locations
       
+      facility = FacilityFactory.new
+      new_york = facility.facility_creator(new_york_facilities)
+      expect(new_york).to be_a(Array)
+      expect(new_york.first).to be_a(Facility)
+
       expect(new_york_facilities).to be_a(Array)
       expect(new_york_facilities.first).to be_a(Hash)
+      
       new_york_facilities.each do |ny_dmv|
         expect(ny_dmv[:office_name]).to be_a(String)
+        expect(ny_dmv).to have_key(:street_address_line_1)
+        expect(ny_dmv[:city]).to be_a(String)
       end
-
-      facility = FacilityFactory.new
-      ny = facility.facility_creator(new_york_facilities)
-      expect(ny).to be_a(Array)
     end
   end
 
   describe `#create_missouri_facility` do
-    it 'can create facility objects from oregon dmv data' do
-      # factory = FacilityFactory.new
+    it 'can create facility objects from missouri dmv data' do
       missouri_facilities = DmvDataService.new.mo_dmv_office_locations
       
+      facility = FacilityFactory.new
+      missouri = facility.facility_creator(missouri_facilities)
+      expect(missouri).to be_a(Array)
+      expect(missouri.first).to be_a(Facility)
+
       expect(missouri_facilities).to be_a(Array)
       expect(missouri_facilities.first).to be_a(Hash)
+      
       missouri_facilities.each do |mo_dmv|
         expect(mo_dmv[:name]).to be_a(String)
+        expect(mo_dmv).to have_key(:zipcode)
+        expect(mo_dmv[:address1]).to be_a(String)
       end
-
-      facility = FacilityFactory.new
-      mo = facility.facility_creator(missouri_facilities)
-      expect(mo).to be_a(Array)
     end
   end
 end  
