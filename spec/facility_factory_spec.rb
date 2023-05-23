649058d55993514bc04bbf667a317e1ddd432574
andi lovetto <andilovetto@gmail.com>
2023-05-23 16:02:03 -0600

    test: Refactor for NY helper method tests

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -21,6 +21,24 @@ RSpec.describe FacilityFactory do
     end
   end
 
+  it 'reformats NY phone numbers' do
+    factory = FacilityFactory.new
+    expect(factory.formatted_ny_phone("5857531604")).to eq("585-753-1604")
+    expect(factory.formatted_ny_phone(nil)).to eq(nil)
+  end
+
+  it 'reformats NY addresses' do
+    factory = FacilityFactory.new
+    address_hash = {
+      :street_address_line_1=>"168-46 91ST AVE., 2ND FLR",
+      :city=>"JAMAICA",
+      :state=>"NY",
+      :zip_code=>"11432",
+    }
+    expected = "168-46 91st Ave., 2nd Flr, Jamaica, Ny, 11432"
+    expect(factory.formatted_ny_address(address_hash)).to eq(expected)
+  end
+
   it 'creates MO facility objects' do
     mo_facilities = DmvDataService.new.mo_dmv_office_locations
     factory = FacilityFactory.new
@@ -31,7 +49,7 @@ RSpec.describe FacilityFactory do
     end
   end
 
-  it 'reformats addresses' do
+  it 'reformats MO addresses' do
     factory = FacilityFactory.new
     address_hash = {
       :address1=>"3164 TELEGRAPH ROAD",
@@ -43,7 +61,7 @@ RSpec.describe FacilityFactory do
     expect(factory.formatted_mo_address(address_hash)).to eq(expected)
   end
 
-  it 'reformats phone numbers' do
+  it 'reformats MO phone numbers' do
     factory = FacilityFactory.new
     expect(factory.formatted_mo_phone("(314) 887-1050")).to eq("314-887-1050")
   end
