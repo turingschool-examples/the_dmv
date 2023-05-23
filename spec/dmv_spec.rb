8ddf7affb6cd9fc3d84c07aa5a4f9c916564a0e9
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-23 15:18:44 -0600

    add before each to rspec

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -50,39 +50,40 @@ RSpec.describe Dmv do
     end 
   end 
   describe 'data formatting' do
-    it "can organize data into specified key value pairs" do 
+    before(:each) do 
       or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
       new_york_facilities = DmvDataService.new.ny_dmv_office_locations
       missouri_facilities = DmvDataService.new.mo_dmv_office_locations
       @dmv.add_facility(or_dmv_office_locations)
       @dmv.add_facility(new_york_facilities)
       @dmv.add_facility(missouri_facilities)
+    end 
+      it "can organize data into specified key value pairs" do 
+        expect(@dmv.facilities[0].name).to eq("Albany DMV Office")
+        expect(@dmv.facilities[59].name).to eq("JAMAICA KIOSK")
+        expect(@dmv.facilities[231].name).to eq("OAKVILLE")
 
-      expect(@dmv.facilities[0].name).to eq("Albany DMV Office")
-      expect(@dmv.facilities[59].name).to eq("JAMAICA KIOSK")
-      expect(@dmv.facilities[231].name).to eq("OAKVILLE")
+        expect(@dmv.facilities[0].address).to eq("2242 Santiam Hwy SE")
+        expect(@dmv.facilities[59].address).to eq("168-46 91ST AVE., 2ND FLR")
+        expect(@dmv.facilities[231].address).to eq("3164 TELEGRAPH ROAD")
 
-      expect(@dmv.facilities[0].address).to eq("2242 Santiam Hwy SE")
-      expect(@dmv.facilities[59].address).to eq("168-46 91ST AVE., 2ND FLR")
-      expect(@dmv.facilities[231].address).to eq("3164 TELEGRAPH ROAD")
+        expect(@dmv.facilities[0].phone).to eq("541-967-2014")
+        expect(@dmv.facilities[59].phone).to eq(nil)
+        expect(@dmv.facilities[60].phone).to eq("5857531604")
+        expect(@dmv.facilities[231].phone).to eq("(314) 887-1050")
 
-      expect(@dmv.facilities[0].phone).to eq("541-967-2014")
-      expect(@dmv.facilities[59].phone).to eq(nil)
-      expect(@dmv.facilities[60].phone).to eq("5857531604")
-      expect(@dmv.facilities[231].phone).to eq("(314) 887-1050")
+        expect(@dmv.facilities[0].zip).to eq("97321")
+        expect(@dmv.facilities[59].zip).to eq("11432")
+        expect(@dmv.facilities[231].zip).to eq("63125")
 
-      expect(@dmv.facilities[0].zip).to eq("97321")
-      expect(@dmv.facilities[59].zip).to eq("11432")
-      expect(@dmv.facilities[231].zip).to eq("63125")
+        expect(@dmv.facilities[0].type).to eq("DMV Location")
+        expect(@dmv.facilities[59].type).to eq("DISTRICT OFFICE")
+        expect(@dmv.facilities[231].type).to eq("1MV")
 
-      expect(@dmv.facilities[0].type).to eq("DMV Location")
-      expect(@dmv.facilities[59].type).to eq("DISTRICT OFFICE")
-      expect(@dmv.facilities[231].type).to eq("1MV")
-
-      expect(@dmv.facilities[0].website).to eq("http://www.oregon.gov/ODOT/DMV/pages/offices/albany.aspx")
-      expect(@dmv.facilities[60].website).to eq(nil)
-      expect(@dmv.facilities[231].website).to eq(nil)
-      expect(@dmv.facilities[237].website).to eq("losllc.com")
-    end  
-  end
+        expect(@dmv.facilities[0].website).to eq("http://www.oregon.gov/ODOT/DMV/pages/offices/albany.aspx")
+        expect(@dmv.facilities[60].website).to eq(nil)
+        expect(@dmv.facilities[231].website).to eq(nil)
+        expect(@dmv.facilities[237].website).to eq("losllc.com")
+      end  
+    end
 end
