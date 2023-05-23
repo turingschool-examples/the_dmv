10abafd9f04c269005be8dfa2132473817c5b82e
Zanna Fitch <zannafitch2004@gmail.com>
2023-05-23 17:02:08 -0500

    Tests: Fixed tests, Typo

--- spec/DMVfacility_spec.rb
+++ spec/DMVfacility_spec.rb
@@ -8,11 +8,9 @@ RSpec.describe DMVfacility do
             new_york_facilities = DmvDataService.new.ny_dmv_office_locations
             OR = facilities.create_facility(oregon_facilities)
 
-            expect(OR[:name]).to eq("Albany DMV Office")
-            expect(OR[:address]).to eq("2242 Santiam Hwy SE Albany OR 97321")
-            
-
-
+            expect(OR[0].name).to eq("Albany DMV Office")
+            expect(OR[0].address).to eq("2242 Santiam Hwy SE Albany OR 97321")
+            expect(OR[0].phone).to eq("541-967-2014")
 
         end
     end
