4b937c5f1f7df3d10120ca31cd81cbe0dbf485f7
Anna Wiley <awiley33@gmail.com>
2023-05-23 01:17:24 -0500

    test: Add vehicle factory spec tests

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,13 +1,27 @@
 require './lib/vehicle'
 require './lib/vehicle_factory'
 require './lib/dmv_data_service'
+wa_ev_registrations = DmvDataService.new.wa_ev_registrations
 
 RSpec.describe VehicleFactory do
+
+  let(:wa_dmv_data) do 
+    [{:electric_vehicle_type=>"Battery Electric Vehicle (BEV)", :vin_1_10=>"WMEEJ9AA7E", :dol_vehicle_id=>"349158127", :model_year=>"2014", :make=>"SMART", :model=>"Fortwo Electric Drive", :vehicle_primary_use=>"Passenger", :electric_range=>"58", :odometer_reading=>"0", :odometer_code=>"Odometer reading is not collected at time of renewal", :new_or_used_vehicle=>"Used", :sale_price=>"0", :base_msrp=>"0", :transaction_type=>"Registration Renewal", :transaction_date=>"2021-06-07T00:00:00.000", :transaction_year=>"2021", :county=>"King", :city=>"SEATTLE", :state_of_residence=>"WA", :zip=>"98102", :non_clean_alternative_fuel=>"HB 2778 Eligiblity Requirements not met", :hb_2042_clean_alternative_fuel_vehicle_cafv_eligibility=>"HB 2042 Eligibility Requirements not met", :meets_2019_hb_2042_electric_range_requirement=>true, :meets_2019_hb_2042_sale_date_requirement=>false, :meets_2019_hb_2042_sale_price_value_requirement=>false, :_2019_hb_2042_battery_range_requirement=>"Meets battery range requirement", :_2019_hb_2042_purchase_date_requirement=>"Non-sale, registration transaction", :_2019_hb_2042_sale_price_value_requirement=>"Non-sale, registration transaction", :electric_vehicle_fee_paid=>"Yes", :transportation_electrification_fee_paid=>"Yes", :hybrid_vehicle_electrification_fee_paid=>"No", :census_tract_2020=>"53033007501", :legislative_district=>"43", :electric_utility=>"CITY OF SEATTLE - (WA)|CITY OF TACOMA - (WA)"}]
+  end
 
   it "exists" do
     factory = VehicleFactory.new
 
     expect(factory).to be_an_instance_of(VehicleFactory)
   end
-  
+
+  it "can create vehicles" do
+    factory = VehicleFactory.new
+    vehicle = factory.create_vehicles(wa_dmv_data)
+    expect(vehicle.first.vin).to eq("WMEEJ9AA7E")
+    expect(vehicle.first.year).to eq("2014")
+    expect(vehicle.first.make).to eq("SMART")
+    expect(vehicle.first.model).to eq("Fortwo Electric Drive")
+    expect(vehicle.first.engine).to eq(:ev)
+  end
 end
