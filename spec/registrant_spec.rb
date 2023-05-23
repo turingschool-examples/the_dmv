fd11b6915085dc43efcb91b86aec9882700471d1
Zanna Fitch <zannafitch2004@gmail.com>
2023-05-23 00:49:23 -0500

    Tests: Finished tests for written test method

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -34,6 +34,7 @@ RSpec.describe Registrant do
             expect(@registrant_2.permit?).to eq(true)
         end
     end
+    
     describe '#administer_written_test' do
     it 'written Test' do
         registrant_1 = Registrant.new('Bruce', 18, true )
@@ -52,8 +53,22 @@ RSpec.describe Registrant do
         facility_1.add_service('Written Test')
         expect(facility_1.administer_written_test(registrant_1)).to eq(true)
         expect(registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
-    
-        
+
+        expect(registrant_2.age).to eq(16)
+        expect(registrant_2.permit?).to eq(false)
+        expect(facility_1.administer_written_test(registrant_2)).to eq(false)
+
+        registrant_2.earn_permit
+        expect(facility_1.administer_written_test(registrant_2)).to eq(true)
+        expect(registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+
+        expect(registrant_3.age).to eq(15)
+        expect(registrant_3.permit?).to eq(false)
+        expect(facility_1.administer_written_test(registrant_3)).to eq(false)
+
+        registrant_3.earn_permit
+        expect(facility_1.administer_written_test(registrant_3)).to eq(false)
+        expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
         end
     end
 end
