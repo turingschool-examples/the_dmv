9f30675559aff4df2d7954d63b4eae16f95b0e8b
Derek Chavez <frrenose17@yahoo.com>
2023-05-23 11:48:38 -0600

    test: add assertion to creates facility test

--- spec/facility_creator_spec.rb
+++ spec/facility_creator_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe FacilityCreator do
     state_data = DmvDataService.new.or_dmv_office_locations
     oregon_facility = FacilityCreator.new
     oregon_facility.create_facilities(state_data)
+    facility = oregon_facility.create_facilities(state_data)
     expect(oregon_facility).to be_an_instance_of(FacilityCreator)
+    expect(facility.length).to eq(state_data.length)
   end
 end
