b05e080684dbba9947a5f98b146ecb834f4cfc75
Zanna Fitch <zannafitch2004@gmail.com>
2023-05-23 17:38:03 -0500

    Tests: Add tests for NY office

--- spec/DMVfacility_spec.rb
+++ spec/DMVfacility_spec.rb
@@ -12,6 +12,16 @@ RSpec.describe DMVfacility do
             expect(OR[0].address).to eq("2242 Santiam Hwy SE Albany OR 97321")
             expect(OR[0].phone).to eq("541-967-2014")
 
+            
+            NY = facilities.create_facility(new_york_facilities)
+            expect(NY[0].name).to eq("JAMAICA KIOSK")
+            expect(NY[0].address).to eq("168-46 91ST AVE., 2ND FLR JAMAICA NY 11432")
+            expect(NY[1].phone).to eq("5857531604")
+
+            
         end
     end
 end
+# office_name
+# street_address_line_1
+# public_phone_number
