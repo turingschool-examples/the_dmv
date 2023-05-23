32a827efa67346f129869312b2a1388aec2b1c03
Zanna Fitch <zannafitch2004@gmail.com>
2023-05-23 17:38:36 -0500

    Feat: change method to work for NY

--- lib/DMVfacility.rb
+++ lib/DMVfacility.rb
@@ -10,16 +10,21 @@ class DMVfacility
     end
 
     def name(database)
-        database[:title]
+        database[:title] || database[:office_name] 
     end
     
     def phone(database)
-        database[:phone_number]
+        database[:phone_number] || database[:public_phone_number]
     end
 
     def address(database)
+        if database[:state] == "NY"
+            database.fetch_values(:street_address_line_1, :city , :state, :zip_code).join(" ")
+        elsif database[:location_1][:human_address].include?("OR")
         find_address = eval(database[:location_1][:human_address])
         find_address.fetch_values(:address, :city, :state, :zip).join(" ")
+        else
+        end
     end
 
 
