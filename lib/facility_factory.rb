a5db948887486b6d823de9285e1c78615c5670ba
Anna Wiley <awiley33@gmail.com>
2023-05-23 02:53:25 -0500

    feat: Add a create facility git status
    q

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,3 +1,14 @@
 class FacilityFactory
-  def 
+  def create_facilities(dmv_facilities_data)
+
+  end
 end
+
+
+
+
+# from Facility class:
+# def initialize(facility_details)
+#   @name = facility_details [:name]
+#   @address = facility_details [:address]
+#   @phone = facility_details [:phone]
