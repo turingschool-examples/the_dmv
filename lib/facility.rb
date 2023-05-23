728477378d9fd59f31faec4fe36c96409a7d1a83
Zanna Fitch <zannafitch2004@gmail.com>
2023-05-22 22:25:22 -0500

    feat: register vehicles

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,26 @@
 class Facility
-  attr_reader :name, :address, :phone , :services
+  attr_reader :name, :address, :phone , :services, :registered_vehicles, :collected_fees
 
   def initialize(data)
     @name = data[:name]
     @address = data[:address]
     @phone = data[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(car)
+      if car.engine == :ev
+        car.plate_type = :ev
+      elsif car.antique?
+        car.plate_type = :antique
+      else
+        car.plate_type = :regular
+      end
   end
 end
