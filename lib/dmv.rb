787272814b46b94f79402e3023bd767b6b414a2b
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-23 12:04:01 -0800

    feat: create oregon facilities method

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -14,4 +14,18 @@ class Dmv
       facility.services.include?(service)
     end
   end
+
+  def create_oregon_facilities
+    or_dmv_office_locations = DmvDataService.new.or_dmv_office_locations
+    or_dmv_office_locations.each do |data|
+      facility = Facility.new(
+        name: data[:title],
+        address: parse_oregon_address(data[:location_1][:human_address]),
+        phone: data[:phone_number]
+      )
+      require 'pry'; binding.pry
+      @facilities << facility
+    end
+  end
+
 end
