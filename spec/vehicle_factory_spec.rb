78cfd9bdb7c0767f75dd5934043250fe8a73ad5d
Captainlearyo <clayoleary@gmail.com>
2023-05-23 16:18:49 -0500

    delete new facilities

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 RSpec.describe Registrant do
   
   wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+  new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+  missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+  or_ev_registrations = DmvDataService.new.or_dmv_office_locations
  
 
   describe '#VehicleFactory' do
