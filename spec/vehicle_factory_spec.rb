e0cbfc2df1f2133aa4607ee0527d23d435a3dbc5
Parker Boeing <parker.boeing@gmail.com>
2023-05-23 12:23:12 -0600

    Test: verify functionality of find most popular method

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -19,4 +19,12 @@ RSpec.describe VehicleFactory do
       expect(@factory.created_vehicles).to include(Vehicle)
     end
   end
+
+  describe '#find_most_popular' do
+    it 'can find most popular make and model from an array of created vehicles' do
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+      @factory.create_vehicles(wa_ev_registrations)
+      expect(@factory.find_most_popular).to eq "NISSAN Leaf"
+    end
+  end
 end
