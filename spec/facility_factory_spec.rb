fd7286202893349e279285bb54df50d7b34c51db
Anna Wiley <awiley33@gmail.com>
2023-05-23 02:49:41 -0500

    test: Add different tests for each state dmv, add first object data for comparison examples

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -1,5 +1,5 @@
 require "./spec/spec_helper"
-
+require "./lib/facility_factory"
 
 
 RSpec.describe FacilityFactory do
@@ -15,17 +15,30 @@ RSpec.describe FacilityFactory do
     expect(factory).to be_an_instance_of(FacilityFactory)
   end
 
-  it "can create facilities" do
+  it "can create OR facilities" do
     factory = FacilityFactory.new
     facilities = factory.create_facilities(@oregon_facilities)
 
-    expect(facilities.first.).to eq()
-    expect(facilities.first.).to eq()
-    expect(facilities.first.).to eq()
-    expect(facilities.first.).to eq()
-    expect(facilities.first.).to eq()
-    expect(facilities.first.).to eq()
-    expect(facilities.first.).to eq()
-    expect(facilities.first.).to eq()
+    expect(facilities.first.name).to eq("Albany DMV Office")
+    expect(facilities.first.address).to eq("2242 Santiam Hwy, Albany OR 97321")
+    expect(facilities.first.phone).to eq("541-967-2014")
+  end
+
+  it "can create NY facilities" do
+    factory = FacilityFactory.new
+    facilities = factory.create_facilities(@new_york_facilities)
+
+    expect(facilities.first.name).to eq("Jamaica Kiosk")
+    expect(facilities.first.address).to eq("168-46 91st Ave, 2ND Flr, Jamaica NY 11432")
+    expect(facilities.first.phone).to eq(nil)
+  end
+
+  it "can create MO facilities" do
+    factory = FacilityFactory.new
+    facilities = factory.create_facilities(@missouri_facilities)
+    
+    expect(facilities.first.name).to eq("Oakville")
+    expect(facilities.first.address).to eq("3164 Telegraph Rd, St Louis MO 63125")
+    expect(facilities.first.phone).to eq("314-887-1050")
   end
 end
