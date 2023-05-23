24eb22399f4eeb72a4f16a6be41ca959963efd09
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-23 08:14:52 -0500

    add vehicle factory to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,11 @@ require './lib/facility'
 require './lib/vehicle'
 require './lib/dmv_data_service'
 require './lib/registrant'
+require './lib/vehicle_factory'
+
+
+# :vin_1_10=>
+# :model_year=>
+# :engine=> :ev 
+# :make=> 
+# :model=>
