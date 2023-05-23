0a539d941e6b41d53d5de0a8a9211a91118ca78f
Davis Weimer <davisweimer@gmail.com>
2023-05-23 13:37:40 -0600

    lib: Create design_facility & facility_key_updater methods

--- lib/facility_architect.rb
+++ lib/facility_architect.rb
@@ -1,3 +1,20 @@
 class FacilityArchitect
-  
+
+  def design_facility(facility_array)
+    updated_keys = facility_key_updater(facility_array)
+    updated_keys.map do |attr|
+      Facility.new(attr)
+    end
+  end
+
+  def facility_key_updater(unformatted_facilities)
+    flattened_array = [unformatted_facilities].flatten
+    new_keys = flattened_array.each do |facility|
+      facility[:name] = facility[:title]
+      facility[:phone] = facility[:phone_number]
+      facility[:address] = JSON.parse(facility[:location_1][:human_address])
+    end
+    new_keys
+  end
+
 end
