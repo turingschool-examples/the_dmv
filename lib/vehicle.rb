34d8f00781bb17a3cdc6ac63adc8b8af059486a9
Mike Wood <mikewwood@icloud.com>
2023-05-22 21:51:46 -0400

    refactor: adds comments explaining each  method.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,5 @@
+# Date is a Ruby class that represents a date.
+# It has a method called today that returns today's date.
 require 'date'
 
 class Vehicle
@@ -7,8 +9,11 @@ class Vehicle
               :model,
               :engine,
               :plate_type
+  # :registration_date must be in attr_accessor because it is being
+  # changed in the register_vehicle method in the Facility class.
   attr_accessor :registration_date
 
+  # Creates a new Vehicle object
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
     @year = vehicle_details[:year]
@@ -19,14 +24,20 @@ class Vehicle
     @plate_type = nil
   end
 
+  # Determines if a vehicle is an anqtique.
   def antique?
+    # Date.today.year is a Ruby method that returns the current year.
+    # This checks if the current year minus the year of the vehicle
+    # is greater than 25.
     Date.today.year - @year > 25
   end
 
+  # Determines if a vehicle is an electric vehicle
   def electric_vehicle?
     @engine == :ev
   end
 
+  # Determines the plate type of a vehicle
   def determine_plate_type
     @plate_type = if antique?
                     :antique
