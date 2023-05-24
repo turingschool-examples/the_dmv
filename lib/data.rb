4b1021f63249285e9e2bdeb176ee0e6edbad0bcd
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-05-24 13:12:17 -0400

    edit  create facility method

--- lib/data.rb
+++ lib/data.rb
@@ -13,4 +13,6 @@ class Data
             keys.each do |key|
                 if key.to_s.include?('address') ||  key.to_s.include?('location') 
                         if facility.has_key?(:location_1) ==true 
-            
+                            hash = JSON.parse(facility.dig(:location_1, :human_address))
+                            facility.update({:address => hash.fetch("address")})
+                        else
