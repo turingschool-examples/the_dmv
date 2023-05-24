090cc1416082f9661c7e2725568c173ed1cda2f1
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-05-24 13:10:59 -0400

    edit create facility method in data

--- lib/data.rb
+++ lib/data.rb
@@ -9,4 +9,8 @@ class Data
     
         facility_list.each do|facility|
             keys =  facility.keys
+
+            keys.each do |key|
+                if key.to_s.include?('address') ||  key.to_s.include?('location') 
+                        if facility.has_key?(:location_1) ==true 
             
