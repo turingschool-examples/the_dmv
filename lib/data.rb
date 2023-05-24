75ae61fd5f51052a25399b7820328816407d9425
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-05-24 18:41:47 -0400

    edit typos

--- lib/data.rb
+++ lib/data.rb
@@ -16,11 +16,13 @@ attr_reader :facilities, :vehicles
         facility_list.each do|facility|
             keys =  facility.keys
 
+
+
             keys.each do |key|
                 if key.to_s.include?('address') ||  key.to_s.include?('location') 
                         if facility.has_key?(:location_1) ==true 
                             hash = JSON.parse(facility.dig(:location_1, :human_address))
-                            facility.update({:address => hash.fetch("address")})
+                            facility.update({:address => "#{hash["address"]}, #{hash["city"]}, #{hash["state"]} #{hash["zip"]}"})
                         else
                             facility.update({:address => facility.fetch(key)})
                         end 
