12554bd8a566db31684b7a5a96d3210360e8a8a8
Allan Evans <oneblankpage27@gmail.com>
2023-07-13 16:59:35 -0600

    Add initialize method to regstrant class

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,2 +1,7 @@
 class Registrant
+  def initialize(name, age, permit = nil)
+    @name = name
+    @age = age
+    @permit = permit
+  end
 end
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
+require './lib/registrant'
 
 RSpec.describe Registrant do
-  
+  before(:each) do
+    @registrant_1 = Registrant.new("Bruce", 18, true)
+    @registrant_2 = Registrant.new("Penny", 15)
+  end
+
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@registrant_1).to be_an_instance_of(Registrant)
+      expect(@registrant_2).to be_an_instance_of(Registrant)
+    end
+  end
 end
