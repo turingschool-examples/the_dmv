d3021fb6e97300d8c5b4bea443744c2c6d715462
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-14 09:16:47 -0500

    add registered_vehicles attribute as an empty array, and collected_fees attribute set to 0.

--- lib/facility.rb
+++ lib/facility.rb
@@ -3,13 +3,17 @@ class Facility
   attr_reader :name, 
               :address, 
               :phone, 
-              :services
+              :services,
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(hash_data)
     @name = hash_data[:name]
     @address = hash_data[:address]
     @phone = hash_data[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
