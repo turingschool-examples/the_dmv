d6acfb2e78329971eea56c3e3d9a34ff622d71ef
Reed Hillmar <reedhillmar@gmail.com>
2023-07-14 16:55:15 -0600

    test: add test code for #create_vehicles

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,6 +1,17 @@
 class VehicleFactory
   attr_reader :vehicles
+
   def initialize
     @vehicles = vehicles
+  end
+
+  def create_vehicles
+    #engine = ev
+    #make = :make
+    #model = :model
+    #plate_type = nil
+    #registration_date = nil
+    #vin = :vin_1_10
+    #year = :model_year
   end
 end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -11,4 +11,11 @@ describe VehicleFactory do
       expect(@factory).to be_an_instance_of(VehicleFactory)
     end
   end
+
+  describe "#create_vehicles" do
+    it "can add vehicles from the database" do
+      expect(@factory.create_vehicles(@wa_ev_registrations)).to be_an Array
+      expect(@factory.create_vehicles(@wa_ev_registrations)).to all be_a Vehicle
+    end
+  end
 end
