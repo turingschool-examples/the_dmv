bd5488ebc6512a6f5a66e26dcb60ff6ce00b6946
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-07-13 21:44:53 -0600

    feat: Add details hash to the facility class initialize method

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,15 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(details)
+    @name = details[:name]
+    @address = details[:address]
+    @phone = details[:phone]
     @services = []
+
   end
 
   def add_services(service)
