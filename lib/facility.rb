24deb07543fdf1059d9620bfd96f494edf2c4b5f
Nick Spencer <strangebrew202@gmail.com>
2023-07-14 14:34:10 -0600

    Refactor: move facility test cases into proper file, they were accidentaly placed in dmv_spec

--- lib/facility.rb
+++ lib/facility.rb
@@ -39,4 +39,9 @@ class Facility
     return false if !@services.include?("Written Test") || !registrant.permit? || !(registrant.age >= 16)
     registrant.license_data[:written] = true
   end
+
+  def administer_road_test(registrant)
+    return false if !@services.include?("Road Test") || !registrant.license_data[:written]
+    registrant.license_data[:license] = true
+  end
 end
