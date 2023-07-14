f478b8bddcd495a2597eb681886331f29e143207
Antoine Aube <aaube3@gmail.com>
2023-07-13 19:37:00 -0600

    feat: add permit? method to registrant class

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,9 +1,13 @@
 class Registrant
   attr_reader :name, :age
-  
+
   def initialize(name, age, permit = false)
     @name = name
     @age = age
     @permit = permit
+  end
+
+  def permit? 
+    @permit
   end
 end
