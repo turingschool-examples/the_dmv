ebc4bf50589144e55c0c79f23d5d0f38dcc58d2b
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-13 22:17:04 -0600

    test: add tests for administer_written_test on facility_spec

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -9,6 +9,9 @@ RSpec.describe Facility do
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
     @facility_1.add_service('Vehicle Registration')
+    @registrant_1 = Registrant.new("Bruce", 18, true)
+    @registrant_2 = Registrant.new("Penny", 16)
+    @registrant_3 = Registrant.new("Tucker", 15)
   end
   describe '#initialize' do
     it 'can initialize' do
@@ -117,6 +120,51 @@ RSpec.describe Facility do
 
       @facility_1.collect_registration_fees(@camaro)
       expect(@facility_1.collected_fees).to eq(325)
+    end
+  end
+
+  describe "#administer_written_test" do
+    xit "can only be done by a facility offering Written Test service" do
+      expect(@registrant_1.license_data).to eq({:written => false, :license => false, :renewed => false})
+      expect(@registrant_1.permit?).to be true
+      
+      @facility_1.administer_written_test(@registrant_1)
+      
+      expect(@registrant_1.license_data).to eq({:written => false, :license => false, :renewed => false})
+      
+      @facility_1.add_service("Written Test")
+      @facility_1.administer_written_test(@registrant_1)
+      
+      expect(@registrant_1.license_data).to eq({:written => true, :license => false, :renewed => false})
+    end
+
+    xit "can only be administered to registrants with a permit" do
+      @facility_1.add_service("Written Test")
+
+      expect(@registrant_2.permit?).to be false
+      
+      @facility_1.administer_written_test(@registrant_2)
+      expect(@registrant_2.permit?).to be false
+      
+      @registrant_2.earn_permit
+      @facility_1.administer_written_test(@registrant_2)
+
+      expect(@registrant_2.license_data).to eq({:written => true, :license => false, :renewed => false})
+    end
+    
+    xit "can only be administered to registrants age 16 or older" do
+      @facility_1.add_service("Written Test")
+      @registrant_2.earn_permit
+      @registrant_3.earn_permit
+      
+      expect(@registrant_2.age).to eq(16)
+      expect(@registrant_3.age).to eq(15)
+      
+      @facility_1.administer_written_test(@registrant_2)
+      @facility_1.administer_written_test(@registrant_3)
+      
+      expect(@registrant_2.license_data).to eq({:written => true, :license => false, :renewed => false})
+      expect(@registrant_3.license_data).to eq({:written => false, :license => false, :renewed => false})
     end
   end
 end
