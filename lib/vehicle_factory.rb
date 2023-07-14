c8b5fa8add4691044d4fb0947af1d2dd39181de0
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-14 13:58:19 -0600

    feat: add create_vehicles to vehicle_factory

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,3 +1,21 @@
 class VehicleFactory
-  
+  def initialize
+
+  end
+
+  def create_vehicles(data)
+    vehicles = []
+    #starting with making one to debug easier
+    # need vin, year, make, model, engine (always ev for this)
+    data.each do |api_data|
+      vehicle_details = Hash.new
+      vehicle_details[:vin] = api_data[:vin_1_10]
+      vehicle_details[:year] = api_data[:model_year]
+      vehicle_details[:make] = api_data[:make]
+      vehicle_details[:model] = api_data[:model]
+      vehicle_details[:engine] = :ev
+      vehicles << Vehicle.new(vehicle_details)
+    end
+    vehicles
+  end
 end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe VehicleFactory do
       expect(factory).to be_a(VehicleFactory)
     end
   end
+
+  describe "#create_vehicles" do
+    it "returns a new vehicle from WAs external API data in an array" do
+      factory = VehicleFactory.new
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+
+      expect(factory.create_vehicles(wa_ev_registrations).first.vin).to eq("1G1RC6S55J")
+      expect(factory.create_vehicles(wa_ev_registrations).last.vin).to eq("JN1AZ0CP7B")
+    end
+  end
 end
