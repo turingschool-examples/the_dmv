2d2c791f68508feae884249db7370eede650b6e1
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-07-13 22:43:04 -0600

    test: Test facilities_offering_service method

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -26,6 +26,10 @@ RSpec.describe Dmv do
 
   describe '#facilities_offering_service' do
     it 'can return list of facilities offering a specified Service' do
+      @dmv.add_facility(@facility_1)
+      @dmv.add_facility(@facility_2)
+      @dmv.add_facility(@facility_3)
+      
       @facility_1.add_services('New Drivers License')
       @facility_1.add_services('Renew Drivers License')
       @facility_2.add_services('New Drivers License')
@@ -34,11 +38,7 @@ RSpec.describe Dmv do
       @facility_3.add_services('New Drivers License')
       @facility_3.add_services('Road Test')
 
-      @dmv.add_facility(@facility_1)
-      @dmv.add_facility(@facility_2)
-      @dmv.add_facility(@facility_3)
-
-      expect(@dmv.facilities_offering_service('Road Test').to eq([@facility_2, @facility_3])
+      expect(@dmv.facilities_offering_service('Road Test')).to eq([@facility_2, @facility_3])
     end
   end
 end
