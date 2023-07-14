5cbde5eb3aa55f0f0a477e31aeccb2dc77263ffb
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-07-14 16:13:43 -0600

    test: Test registrant class and initialize method as well as adding registrant.rb to the spec_helper.rb file

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+RSpec.describe Registrant do
+  before(:each) do
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_2 = Registrant.new('Penny', 15 )
+  end
+
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@registrant_1).to be_a(Registrant)
+      expect(@registrant_2).to be_a(Registrant)
+    end
+  end
+end
