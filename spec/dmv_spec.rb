500d91a47bbdc7c6029cf5f9eeffd1670d7f48cb
Nick Spencer <strangebrew202@gmail.com>
2023-07-13 22:18:57 -0600

    Documenting: change working for #register_vehicle test case, #register_vehicle feature complete

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe Dmv do
   end
 
   describe "#register_vehicle" do
-    it "can add vehicles to the @registered_vehicles attribute if it has Vehicle Registration service" do
+    it "Register vehicles and charge fees if Facility has Vehicle Registration service" do
       cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice})
       bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev})
       camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
