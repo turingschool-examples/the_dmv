2d7efa2e0c0ddaa9790902cadfa31e70aedc363d
Robert deLaguna <rjdelaguna@yahoo.com>
2023-07-14 12:47:59 -0700

    add test updating vehicle registration information

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -28,7 +28,6 @@ RSpec.describe Facility do
     before(:each) do
       @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
       @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
-   
       @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
       @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
       @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
@@ -36,20 +35,17 @@ RSpec.describe Facility do
     end
 
     it 'shows vehicles registration, facility registered vehicles, and collected fees have no values to begin' do
-      
       expect(@cruz.registration_date).to eq(nil)
       expect(@facility_1.registered_vehicles).to eq([])
       expect(@facility_1.collected_fees).to eq(0)
     end
-    
-    # facility_1.register_vehicle(cruz)
-    # #=> [#<Vehicle:0x0000000135a48b08...>]
-    
-    # cruz.registration_date
-    # #=> #<Date: 2023-01-12 ((2459957j,0s,0n),+0s,2299161j)>
-    
-    # cruz.plate_type
-    # #=> :regular
+
+    it 'updates vehicle with registration information' do
+      @facility_1.register_vehicle(@cruz)
+
+      expect(@cruz.registration_date).to eq("<Date: 2023-01-12 ((2459957j,0s,0n),+0s,2299161j)>")
+      expect(@cruz.plate_type).to eq(:regular)
+    end
     
     # facility_1.registered_vehicles
     # #=> [#<Vehicle:0x0000000135a48b08...>]
