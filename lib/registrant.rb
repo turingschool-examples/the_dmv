a6d30f5aef71a2b5ed2befcf9d12943057939c04
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-13 20:22:49 -0600

    feat: add license_data to registrant

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,10 +1,16 @@
 class Registrant
   attr_reader :name,
-              :age
+              :age,
+              :license_data
   def initialize(name, age, permit = false)
     @name = name
     @age = age
     @permit = permit
+    @license_data = {
+      written: false,
+      license: false,
+      renewed: false
+    }
   end
 
   def permit?
