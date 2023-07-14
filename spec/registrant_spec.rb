12bf7e21d578b53b32b272590fd02392cfbcbfec
Antoine Aube <aaube3@gmail.com>
2023-07-13 19:39:26 -0600

    test: it has license data

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -13,8 +13,8 @@ RSpec.describe Registrant do
     it "has an age, name and permit" do 
       registrant_1 = Registrant.new('Bruce', 18, true )
       registrant_2 = Registrant.new('Penny', 15 )
-
-
+      
+      
       expect(registrant_1.name).to eq("Bruce")
       expect(registrant_1.age).to eq(18)
       expect(registrant_1.permit?).to eq(true)
@@ -22,5 +22,11 @@ RSpec.describe Registrant do
       expect(registrant_2.age).to eq(15)
       expect(registrant_2.permit?).to eq(false)
     end
+    
+    it "has liscense data" do 
+      registrant_2 = Registrant.new('Penny', 15 )
+
+      expect(license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end 
   end
 end
