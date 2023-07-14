dd688093f4049d41f74b4e0b50dac0c1090868f9
Antoine Aube <aaube3@gmail.com>
2023-07-13 20:22:16 -0600

    feat: registered vehicles instance variable

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,15 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles
 
   def initialize(facility_information)
     @name = facility_information[:name]
     @address = facility_information[:address]
     @phone = facility_information[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
   end
 end
