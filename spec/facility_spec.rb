1ea0efebe2ba8bcb65e9eee0979f3396545d46fa
Antoine Aube <aaube3@gmail.com>
2023-07-13 20:21:37 -0600

    test: #registered vehicles

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
+require './lib/facility'
+require './lib/vehicle'
 
 RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+    cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+    camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+
   end
   describe '#initialize' do
     it 'can initialize' do
@@ -17,10 +25,24 @@ RSpec.describe Facility do
   describe '#add service' do
     it 'can add available services' do
       expect(@facility.services).to eq([])
-      @facility.add_services('New Drivers License')
-      @facility.add_services('Renew Drivers License')
-      @facility.add_services('Vehicle Registration')
+      @facility.add_service('New Drivers License')
+      @facility.add_service('Renew Drivers License')
+      @facility.add_service('Vehicle Registration')
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+    end
+  end
+
+  describe '#add service' do 
+    it 'can add available services' do 
+      @facility_1.add_service("Vehicle Registration")
+
+      expect(@facility_1.services).to eq(["Vehicle Registration"])
+    end
+  end
+
+  describe '#registered_vehicles' do 
+    it "has no registered vehicles" do 
+      expect(@facility_1.registered_vehicles).to eq([])
     end
   end
 end
