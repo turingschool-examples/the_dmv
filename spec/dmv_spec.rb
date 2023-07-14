8b4c5e03f84a2442d82ef80eca7037392a23d3c4
Jimmy Trulock <ttrulock23@gmail.com>
2023-07-14 14:00:49 -0600

    adjust code to pass rspec testing

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -6,10 +6,14 @@ RSpec.describe Dmv do
     @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
     @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
     @facility_3 = Facility.new({name: 'DMV Northwest Branch', address: '3698 W. 44th Avenue Denver CO 80211', phone: '(720) 865-4600'})
+
+  
   end
 
   describe '#initialize' do
     it 'can initialize' do
+        
+
       expect(@dmv).to be_an_instance_of(Dmv)
       expect(@dmv.facilities).to eq([])
     end
