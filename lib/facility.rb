ae5956b99d05c9339abd4c5fdd1d9b47443a275e
Robert deLaguna <rjdelaguna@yahoo.com>
2023-07-14 16:25:46 -0700

    add the renew drivers license method with all tests passing

--- lib/facility.rb
+++ lib/facility.rb
@@ -54,4 +54,11 @@ class Facility
     end
   end
 
+  def renew_drivers_license(registrant)
+    if @services.include?("Renew License")
+      if registrant.license_data[:license] == true
+        registrant.license_data[:renewed] = true
+      end
+    end
+  end
 end
