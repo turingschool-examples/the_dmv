df2b945be7b5ca38c3060b741ae7d78f27727702
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-07-13 19:35:41 -0500

    feat: initialize registrant

---
+++ lib/registrant.rb
@@ -0,0 +1,17 @@
+class Registrant
+  attr_reader :name, :age, :permit, :license_data
+  def initialize(name, age, permit = false)
+    @age = age
+    @license_data = {
+      written: false,
+      license: false,
+      renewed: false,
+    }
+    @name = name
+    @permit = permit
+  end
+  def permit?
+    @permit
+  end
+end
+    
