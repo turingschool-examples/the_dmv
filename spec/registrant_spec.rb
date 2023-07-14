349f124a4aa73f438d6e58555807c0fda16ef721
Antoine Aube <aaube3@gmail.com>
2023-07-13 19:32:11 -0600

    test: has age, name and permit

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -3,11 +3,18 @@ require './lib/registrant'
 RSpec.describe Registrant do 
   describe "#initialize" do 
     it "exists" do
-      registrant_1 = Registrant.new('Bruce', 18, true )
       registrant_2 = Registrant.new('Penny', 15 )
-
+      
       expect(registrant_1).to be_a Registrant
       expect(registrant_2).to be_a Registrant
+    end
+    
+    it "has an age, name and permit" do 
+      registrant_1 = Registrant.new('Bruce', 18, true )
+
+      expect(registrant_1.name).to eq("Bruce")
+      expect(registrant_1.age).to eq(18)
+      expect(registrant_1.permit?).to eq(true)
     end
   end
 end
