372b4bc91e30b3c5e0a109762f521e353682837e
Reed Hillmar <reedhillmar@gmail.com>
2023-07-13 21:33:59 -0600

    test: Add test for #permit? and #earn_permit

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -11,15 +11,22 @@ describe Registrant do
       expect(@registrant_1).to be_an_instance_of Registrant
       expect(@registrant_1.name).to eq("Bruce")
       expect(@registrant_1.age).to eq(18)
-      expect(@registrant_1.permit?).to eq(true)
-      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
-      expect(@registrant_2).to be_an_instance_of Registrant
-      expect(@registrant_2.name).to eq("Penny")
-      expect(@registrant_1.age).to eq(15)
-      expect(@registrant_1.permit?).to eq(false)
       expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
   end
 
+  describe "#permit?" do
+    it "can tell if a registrant is permitted" do
+      expect(@registrant_1.permit?).to be true
+      expect(@registrant_2.permit?).to be false
+    end
+  end
 
+  describe "#earn_permit" do
+    it "can grant a permit to a non-permitted registrant" do
+      @registrant_2.earn_permit
+
+      expect(@registrant_2.permit?).to be true
+    end
+  end
 end
