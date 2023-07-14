5271c06da8ba49b077334b42d65a00d2e7cdffb8
Will Chen <wfchen1990@gmail.com>
2023-07-13 21:40:57 -0600

    Fix test duplication

--- spec/regristrant_spec.rb
+++ spec/regristrant_spec.rb
@@ -25,23 +25,4 @@ RSpec.describe Registrant do
       expect(@registrant_2.permit?).to eq(true)
     end
   end
-
-  # describe "iteration 2.2" do
-  #   before (:each) do
-  #     @registrant_1 = Registrant.new('Bruce', 18, true )
-  #     @registrant_2 = Registrant.new('Penny', 16 )
-  #     @registrant_3 = Registrant.new('Tucker', 15 )
-  #     #is there a quick way to use entered code accross spec files
-  #     @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
-  #     @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
-  #   end
-
-  #   it ".administer_written_test" do
-  #     expect(@registrant_1.license_data).to eq({:license=>false, :renewed=>false, :written=>false})
-  #     expect(@registrant_1.permit?).to eq(true)
-  #     expect(@facility_1.administer_written_test(@registrant_1)).to eq(true)
-  #   end
-
-  end
-
 end
