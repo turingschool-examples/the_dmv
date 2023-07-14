b481546ebd9ac270ba01ca74a5cc63f066e0da07
Antoine Aube <aaube3@gmail.com>
2023-07-13 19:35:55 -0600

    fix: add second registrant to has name/age/permit test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -3,6 +3,7 @@ require './lib/registrant'
 RSpec.describe Registrant do 
   describe "#initialize" do 
     it "exists" do
+      registrant_1 = Registrant.new('Bruce', 18, true )
       registrant_2 = Registrant.new('Penny', 15 )
       
       expect(registrant_1).to be_a Registrant
@@ -11,10 +12,15 @@ RSpec.describe Registrant do
     
     it "has an age, name and permit" do 
       registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 15 )
+
 
       expect(registrant_1.name).to eq("Bruce")
       expect(registrant_1.age).to eq(18)
       expect(registrant_1.permit?).to eq(true)
+      expect(registrant_2.name).to eq("Penny")
+      expect(registrant_2.age).to eq(15)
+      expect(registrant_2.permit?).to eq(false)
     end
   end
 end
