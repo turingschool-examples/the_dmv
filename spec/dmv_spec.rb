e4285b7f9497b8c44d63d067dd5b16c57d72e4ba
Nick Spencer <strangebrew202@gmail.com>
2023-07-14 12:56:51 -0600

    Documentation: rework #Administer_written_test test cases into 3 separate it do tests

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -80,11 +80,12 @@ RSpec.describe Dmv do
 
   describe "Getting a drivers license" do
     describe "#Administer_written_test" do
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 16 )
+      registrant_3 = Registrant.new('Tucker', 15 )
+
       it "sets :written to true if Facility has Written Test service, registrant has permit
       and registrant is 16 or older" do
-        registrant_1 = Registrant.new('Bruce', 18, true )
-        registrant_2 = Registrant.new('Penny', 16 )
-        registrant_3 = Registrant.new('Tucker', 15 )
 
         expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
         expect(registrant_1.permit?).to eq(true)
@@ -95,6 +96,24 @@ RSpec.describe Dmv do
         @facility_1.administer_written_test(registrant_1)
         expect(registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
 
+      end
+
+      it "properly identifies if registrant doesn't have permit" do
+        @facility_1.add_service("Written Test")
+
+        expect(@facility_1.administer_written_test(registrant_2)).to eq(false)
+        registrant_2.earn_permit
+        expect(@facility_1.administer_written_test(registrant_2)).to eq(true)
+        expect(registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+      end
+
+      it "properly identifies if registrant is under 16 years of age" do 
+        @facility_1.add_service("Written Test")
+
+        expect(@facility_1.administer_written_test(registrant_3)).to eq(false)
+        registrant_3.earn_permit
+        expect(@facility_1.administer_written_test(registrant_3)).to eq(false)
+        expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
       end
     end
   end
