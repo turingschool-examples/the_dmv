1085da64054c8ba87d2dc048e0d7cbf37d13447d
Antoine Aube <aaube3@gmail.com>
2023-07-14 19:56:01 -0600

    test: can create new vehicles

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -11,8 +11,11 @@ RSpec.describe VehicleFactory do
 
   describe "new_vehicles" do
     it "can create new vehicles" do 
-      factory_1 = 
-
+      factory_1 = VehicleFactory.new
+      registrations = DmvDataService.new.wa_ev_registrations
+      
+      expect(factory_1.create_vehicles(registrations)).to be_an Array
+      expect(factory_1.create_vehicles(registrations)).to all be_a Vehicle
     end
   end
 end
