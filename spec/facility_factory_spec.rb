1b4ab202f5b8338699bb23b5a98c983cc55acc1a
Antoine Aube <aaube3@gmail.com>
2023-07-15 17:34:37 -0600

    test: can create new facilities

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 RSpec.describe FacilityFactory do 
   before(:each) do
-    @colorado_facilities = DmvDataService.new.co_dmv_office_locations
-    @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
-    @missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+    @colorado_facilities_data = DmvDataService.new.co_dmv_office_locations
+    @new_york_facilities_data = DmvDataService.new.ny_dmv_office_locations
+    @missouri_facilities_data = DmvDataService.new.mo_dmv_office_locations
     @colorado = FacilityFactory.new
     @new_york = FacilityFactory.new
     @missouri = FacilityFactory.new
@@ -14,6 +14,14 @@ RSpec.describe FacilityFactory do
     it "exists"  do 
 
       expect(@colorado).to be_a FacilityFactory
+    end
+  end
+
+  describe "#create_facilities" do
+    it "can create new facilities" do
+       expect(@colorado.colorado_facilities_created).to eq([])
+       @colorado.create_facilities(@colorado_facilities_data)
+       expect(@colorado.colorado_facilities_created).to all be_a Facility
     end
   end
 end
