841ac4366f8cc6646585941a630517388e566b68
Will Chen <wfchen1990@gmail.com>
2023-07-14 20:31:28 -0600

    Finish bare bones iteration 3. need tests, maybe more facility formating

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -19,18 +19,36 @@ class FacilityFactory
   def format_name(data)
     if data[:state] == "CO"
       data[:dmv_office]
+    elsif data[:state] == "NY"
+      data[:office_name]
+      #all caps, do I need DMV in front?
+    elsif data[:state] == "MO"
+      data[:name]
     end
+    
   end
 
   def format_address(data)
     if data[:state] == "CO"
       address_parts = [data[:address_li], data[:address__1], data[:city], data[:state], data[:zip]]
       address_parts.compact.join(" ")
+    elsif data[:state] == "NY"
+      address_parts = [data[:street_address_line_1], data[:street_address_line_2], data[:city], data[:state], data[:zip_code]]
+      address_parts.compact.join(" ")
+      #all caps
+    elsif data[:state] == "MO"
+      address_parts = [data[:address1], data[:city], data[:state], data[:zipcode]]
+      address_parts.compact.join(" ")
     end
   end
 
   def format_phone(data)
     if data[:state] == "CO"
+      data[:phone]
+    elsif data[:state] == "NY"
+      number = data[:public_phone_number]
+      #no () space or -
+    elsif data[:state] == "MO"
       data[:phone]
     end
   end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -8,4 +8,22 @@ RSpec.describe FacilityFactory do
     expect(factory.facilities).to all be_a Facility
     expect(factory.facilities).to be_an Array
   end
+
+  it "NY" do
+    factory = FacilityFactory.new
+    ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
+    factory.create_facilities(ny_dmv_office_locations)
+    expect(factory.facilities).to all be_a Facility
+    expect(factory.facilities).to be_an Array
+  end
+
+  it "MO" do
+    factory = FacilityFactory.new
+    mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
+    factory.create_facilities(mo_dmv_office_locations)
+    # require 'pry';binding.pry
+    expect(factory.facilities).to all be_a Facility
+    expect(factory.facilities).to be_an Array
+  end
+
 end
