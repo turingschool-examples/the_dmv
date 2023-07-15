b88fed44273f2017a60a33fe2d30035d660efe27
Antoine Aube <aaube3@gmail.com>
2023-07-15 17:22:13 -0600

    test: facility factory exists

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -1,1 +1,19 @@
 require 'spec_helper'
+
+RSpec.describe FacilityFactory do 
+  before(:each) do
+    @colorado_facilities = DmvDataService.new.co_dmv_office_locations
+    @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+    @missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+    @colorado = FacilityFactory.new
+    @new_york = FacilityFactory.new
+    @missouri = FacilityFactory.new
+    end
+
+  describe "#initialize" do
+    it "exists"  do 
+
+      expect(@colorado).to be_a FacilityFactory
+    end
+  end
+end
