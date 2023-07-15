8dd4b8096444baffc30834c1f7a86bdbeb6af7a7
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-14 21:00:45 -0600

    Last progress for the night

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
   end
 
   describe '#initialize' do
@@ -36,10 +38,21 @@ RSpec.describe Facility do
 
       # bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
       # camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
-      # facility_2.register_vehicle(bolt)
-      # facility_2.register_vehicle(camaro)
+      # @facility_2.register_vehicle(bolt)
+      # @facility_2.register_vehicle(camaro)
 
       # expect(@facility_2.registered_vehicles).to eq([bolt, camaro])
+    end
+  end
+
+  describe '#collected_fees' do
+    it 'can print total fees collected from registered vehicles in that facility' do
+      expect(@facility.collected_fees).to eq([])
+
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      @facility.register_vehicle(cruz)
+
+      expect(@facility.registered_vehicles).to eq([cruz])
     end
   end
 end
