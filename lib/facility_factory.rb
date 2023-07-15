f8fb346437b8178dc120fdd36f8771c993aeaf85
Antoine Aube <aaube3@gmail.com>
2023-07-15 17:24:36 -0600

    Facility factory class created

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,3 +1,17 @@
 class FacilityFactory
   
+  def initialize
+
+    @co_facilities = []    
+  end
+
+  # def create_facility(data)
+  #   data.each do |dmv_data|
+  #     @co_facilties << Facility.new({
+  #     name: dmv_data[:dmv_office],
+  #     address: ("#{dmv_data[::address_li]} #{} #{} #{} #{}")
+  #     })
+
+  #   end 
+  # end
 end
