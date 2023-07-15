044b87b9278f56830ea2e439c964afe6af7afa9a
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-07-15 11:20:29 -0600

    test: Test earn_permit method

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -26,4 +26,14 @@ RSpec.describe Registrant do
       expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
   end
+
+  describe "#earn_permit" do
+    it "adds permit if not yet earned" do
+      expect(@registrant_2.permit?).to eq(false)
+
+      @registrant_2.earn_permit
+
+      expect(@registrant_2.permit?).to eq(true)
+    end
+  end
 end
