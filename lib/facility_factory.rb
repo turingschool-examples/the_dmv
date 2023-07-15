cf396f1a2214825dab33bc001c73d615e03fb036
Antoine Aube <aaube3@gmail.com>
2023-07-15 17:56:59 -0600

    feat: create facilities method

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,17 +1,19 @@
 class FacilityFactory
-  
+    attr_reader :co_facilities_created
+
   def initialize
 
-    @co_facilities = []    
+    @co_facilities_created = []    
   end
 
-  # def create_facility(data)
-  #   data.each do |dmv_data|
-  #     @co_facilties << Facility.new({
-  #     name: dmv_data[:dmv_office],
-  #     address: ("#{dmv_data[::address_li]} #{} #{} #{} #{}")
-  #     })
-
-  #   end 
-  # end
+  def create_facilities(data)
+    data.each do |dmv_data|
+      @co_facilities_created << Facility.new({
+      name: dmv_data[:dmv_office],
+      address: ("#{dmv_data[:address_li]} #{dmv_data[:address__1]} #{dmv_data[:city]} #{dmv_data[:state]} #{dmv_data[:zip]}"),
+      phone: dmv_data[:phone]
+      })
+    end 
+    @co_facilities_created
+  end
 end
