3412a34e95070fa3391c805f61e6b21cac9a953f
Antoine Aube <aaube3@gmail.com>
2023-07-14 20:05:00 -0600

    feat: #create_vechiles in vehicle factory

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -3,7 +3,16 @@ class VehicleFactory
     @vehicles = []
   end
 
-  def new_vehicles(data)
-
+  def create_vehicles(data)
+    data.each do |dmv_data|
+      @vehicles << Vehicle.new({
+        vin: dmv_data[:vin_1_10],
+        year: dmv_data[:model_year],
+        make: dmv_data[:make],
+        model: dmv_data[:model],
+        engine: :ev
+       })
+    end
+    @vehicles
   end
 end
