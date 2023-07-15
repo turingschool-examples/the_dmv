f0ec867bec61cd480a64485e5d4d6241eeed793d
Lane Bretschneider <lanebret85@gmail.com>
2023-07-14 20:40:05 -0700

    test: add tests for registrant class

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,37 @@
+require './lib/registrant'
+
+RSpec.describe Registrant do
+  describe "#exists" do
+    it "exists" do
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      
+      expect(registrant_1).to be_a (Registrant)
+    end
+  end
+  
+  describe "#has_readable_attributes" do
+    it "has readable attributes" do
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 15 )
+
+      expect(registrant_1.name).to eq("Bruce")
+      expect(registrant_1.age).to eq(18)
+      expect(registrant_1.permit?).to eq(true)
+      expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+
+      expect(registrant_2.name).to eq("Penny")
+      expect(registrant_2.age).to eq(15)
+      expect(registrant_2.permit?).to eq(false)
+      expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+  end
+
+  describe "#earn_permit" do
+    it "can return value of true when registrant earns permit" do
+      registrant_2 = Registrant.new('Penny', 15 )
+      
+      expect(registrant_2.earn_permit).to eq()
+      expect(registrant_2.permit?).to eq(true)
+    end
+  end
+end
