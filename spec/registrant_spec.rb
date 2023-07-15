394f1e1d28fbb7129a1fe6f1c7870948816c2fc4
Cory Powell <coryrpowell@gmail.com>
2023-07-15 15:08:18 -0400

    Add permit and refactor

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -9,7 +9,11 @@ RSpec.describe Registrant do
   describe "#initialize" do
     it "can initialize" do
       expect(@registrant_1).to be_a(Registrant)
+    end
+  end
 
+  describe "#attributes" do
+    it "has readable attributes" do
       expect(@registrant_1.name).to eq('Bruce')
       expect(@registrant_1.age).to eq(18)
       expect(@registrant_1.permit?).to eq(true)
@@ -22,7 +26,12 @@ RSpec.describe Registrant do
       expect(@registrant_2.license_data).to eq({:written=>false, 
       :license=>false, :renewed=>false})
     end
-  
+  end
 
+  describe "#earn_permit" do
+    it "can earn a permit" do
+      expect(@registrant_2.earn_permit).to eq(true)
+      expect(@registrant_2.permit?).to eq(true)
+    end
   end
 end
