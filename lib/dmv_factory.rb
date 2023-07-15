9fd1d42f77e07b7f3e9f215849defcb99ff5c62b
Nick Spencer <strangebrew202@gmail.com>
2023-07-14 21:47:18 -0600

    Add: #scrape_ny_data to dmv_factory and associated tests

--- lib/dmv_factory.rb
+++ lib/dmv_factory.rb
@@ -5,25 +5,58 @@ class DmvFactory
   def create_facilities(data)
     if data[0][:state] == "CO"
       return scrape_co_data(data)
+    elsif data[0][:state] == "NY"
+      return scrape_ny_data(data)
     end
   end
 
-  def build_address(line_1, line_2, city, state, zip)
-    address_arr = [line_1, line_2, city , state, zip]
-    address = address_arr.join(" ")
-  end
+  # build_address is ment to help take the multiple address lines that come in as data
+  # and join them all together into 1 address value. currently it's not saving any lines
+  # though tryiing to make it work for multiple data sources. may be useful to make
+  # code more readable
+  # def build_address(address_info)
+  #   address_arr = [line_1, line_2, city , state, zip]
+  #   address = address_arr.join(" ")
+  # end
 
   def scrape_co_data(data)
     facilities_array = []
     data.each do |facility|
       facility_details = {
         name: facility[:dmv_office],
-        address: build_address(facility[:address_li], facility[:address__1], facility[:city], facility[:state], facility[:zip]), 
+        address: [facility[:address_li], facility[:address__1], facility[:city], facility[:state], facility[:zip]].join(' '), 
         phone: facility[:phone]
       }
       facilities_array << Facility.new(facility_details)
     end
     facilities_array
   end
+
+  def scrape_ny_data(data)
+    facilities_array = []
+    data.each do |facility|
+      facility_details = {
+        name: facility[:office_name],
+        address: [facility[:street_address_line_1], facility[:city], facility[:state], facility[:zip_code]].join(' '), 
+        phone: facility[:phone]
+      }
+      require 'pry';binding.pry
+      facilities_array << Facility.new(facility_details)
+    end
+    facilities_array
+  end
+
+  # def scrape_mo_data(data)
+  #   facilities_array = []
+  #   data.each do |facility|
+  #     facility_details = {
+  #       name: facility[:name],
+  #       address: [facility[:address1], facility[:city], facility[:state], facility[:zipcode].join(' '), 
+  #       phone: facility[:phone]
+  #     }
+  #     facilities_array << Facility.new(facility_details)
+  #   end
+  #   facilities_array
+  # end
 
 end
--- spec/dmv_factory_spec.rb
+++ spec/dmv_factory_spec.rb
@@ -3,9 +3,9 @@ require './lib/dmv_data_service'
 require './lib/facility'
 
 describe DmvFactory do
-  dmv_factory = DmvFactory.new
-  co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
-  describe "#create_facilities" do
+  describe "#create_facilities from co_dmv api" do
+    dmv_factory = DmvFactory.new
+    co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
     it "brings data in as an Array" do
 
       expect(dmv_factory.create_facilities(co_dmv_office_locations)).to be_a Array
@@ -20,6 +20,26 @@ describe DmvFactory do
     it "the returned array is not empty" do
 
       expect(dmv_factory.create_facilities(co_dmv_office_locations).length).to be_a Integer
+    end
+  end
+
+  describe "#create_facilities from ny_dmv api" do
+    dmv_factory = DmvFactory.new
+    ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
+    it "brings data in as an Array" do
+
+      expect(dmv_factory.create_facilities(ny_dmv_office_locations)).to be_a Array
+    end
+
+    it "The returned array is full of only facility objects" do
+      
+      
+      expect(dmv_factory.create_facilities(ny_dmv_office_locations)).to all be_a Facility
+    end
+
+    it "the returned array is not empty" do
+
+      expect(dmv_factory.create_facilities(ny_dmv_office_locations).length).to be_a Integer
     end
   end
 end
