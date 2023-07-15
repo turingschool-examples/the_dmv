5cf8210d53a8e008ca2eefc37a47ac7e569fc4cf
Robert deLaguna <rjdelaguna@yahoo.com>
2023-07-14 18:23:05 -0700

    pass initial tests for vehicle factory

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -2,24 +2,19 @@ require 'spec_helper'
 
 RSpec.describe VehicleFactory do
   before(:each) do
-    factory = VehicleFactory.new
-    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+    @factory = VehicleFactory.new
+    @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
   end
   describe 'initialize' do
     it 'can initialize' do
-      expect(factory).to be_an_instance_of(VehicleFactory)
-      expect(wa_ev_registrations).to be_an_instance_of(DmvDataService)
+      expect(@factory).to be_an_instance_of(VehicleFactory)
+      # require 'pry'; binding.pry
+    end
+  end
+  
+  describe '#create_vehicles' do
+    it 'returns a vehicle from the factory' do
+        expect(@factory.create_vehicles(@wa_ev_registrations)).to be_a(Vehicle)
     end
   end
 end
-
-      
-      
-      # pry(main)> factory.create_vehicles(wa_ev_registrations)
-        #=> [#<Vehicle:0x000000012d3812f0 @engine=:ev, @make="TOYOTA", @model="Prius Plug-in", @plate_type=nil, @registration_date=nil, @vin="JTDKN3DP8D", @year="2013">,
-        #<Vehicle:0x000000012d3812a0 @engine=:ev, @make="TOYOTA", @model="Prius Prime", @plate_type=nil, @registration_date=nil, @vin="JTDKARFP9J", @year="2018">,
-        #<Vehicle:0x000000012d381200 @engine=:ev, @make="NISSAN", @model="Leaf", @plate_type=nil, @registration_date=nil, @vin="1N4AZ1CP0J", @year="2018">,
-        #<Vehicle:0x000000012d381188 @engine=:ev, @make="NISSAN", @model="Leaf", @plate_type=nil, @registration_date=nil, @vin="1N4AZ1CP0J", @year="2018">,
-        #<Vehicle:0x000000012d381138 @engine=:ev, @make="NISSAN", @model="Leaf", @plate_type=nil, @registration_date=nil, @vin="1N4AZ1CP0J", @year="2018">,
-        # ...]
-      
