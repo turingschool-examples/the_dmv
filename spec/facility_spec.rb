0a6d054d20b5912d9fb7973f4bc812a0e5039972
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-14 20:47:16 -0600

    Iteration-2: start, create first test, create 'registered vehicles' test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
   end
+
   describe '#initialize' do
     it 'can initialize' do
       expect(@facility).to be_an_instance_of(Facility)
@@ -21,6 +22,24 @@ RSpec.describe Facility do
       @facility.add_service('Renew Drivers License')
       @facility.add_service('Vehicle Registration')
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+    end
+  end
+
+  describe '#registered vehicles' do
+    it 'can add vehicles to a facility and display them when method is called' do
+      expect(@facility.registered_vehicles).to eq([])
+
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      @facility.register_vehicle(cruz)
+
+      expect(@facility.registered_vehicles).to eq([cruz])
+
+      # bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      # camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+      # facility_2.register_vehicle(bolt)
+      # facility_2.register_vehicle(camaro)
+
+      # expect(@facility_2.registered_vehicles).to eq([bolt, camaro])
     end
   end
 end
