b966a0ae4c7d39545a810aaa765eebe880fa3727
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-14 22:07:59 -0500

    add the earn_permit method to my registrant class

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,11 +1,17 @@
 class Registrant
 attr_reader :name, :age, :permit?, :license_data
 
-  def initialize(age, name, permit = false)
+  def initialize(age, name, permit? = false)
     @age = age
     @license_data = {:written, :license, :renewed}
     @name = name
-    @permit = permit
+    @permit? = permit?
     @tested = false
   end
+
+  def earn_permit
+# Changes permit? to false
+  end
+
+
 end
