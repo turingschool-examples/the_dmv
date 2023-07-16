aa1e9afe704a42e215bb0fdc148a4352d15c2ac6
Antoine Aube <aaube3@gmail.com>
2023-07-16 00:17:38 -0600

    feat: #format_facility_address - NY

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -2,34 +2,32 @@ class FacilityFactory
     attr_reader :facilities_created
 
   def initialize
-
     @facilities_created = []    
   end
 
   def create_facilities(data)
     data.each do |dmv_data|
-      # if dmv_data[:state] == "CO"
       @facilities_created << Facility.new({
       name: format_facility_name(dmv_data),
-      # name: dmv_data[:dmv_office],
       address: format_facility_address(dmv_data),
-      # address: ("#{dmv_data[:address_li]}#{" " + "#{dmv_data[:address__1]}" if dmv_data[:address__1] } #{dmv_data[:city]} #{dmv_data[:state]} #{dmv_data[:zip]}"),
       phone: format_facility_phone(dmv_data)
       })
-      # end 
-      @facilities_created
     end 
   end
 
   def format_facility_name(data)
     if data[:state] == "CO"
       data[:dmv_office]
+    elsif data[:state] == "NY"
+      capitalize_string("#{data[:office_name]} #{data[:office_type]}")
     end
   end
 
   def format_facility_address(data)
     if data[:state] == "CO"
       ("#{data[:address_li]}#{" " + "#{data[:address__1]}" if data[:address__1] } #{data[:city]} #{data[:state]} #{data[:zip]}")  
+    elsif data[:state] == "NY"
+      capitalize_string("#{data[:street_address_line_1]} #{data[:city]} #{data[:state]} #{data[:zip_code]}")
     end
   end
 
@@ -38,6 +36,12 @@ class FacilityFactory
       data[:phone]
     end
   end
+
+  def capitalize_string(string)
+    data_string = string.split.map do |word| 
+      word.include?("NY") ? word : word.capitalize
+    end 
+    data_string.join(" ")
+  end
 end
 
-#could do helper methods for each key -> name, address, and phone
