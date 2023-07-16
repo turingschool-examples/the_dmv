1c023e71c02214462cbc93ec88a84a8cbab6c024
Lane Bretschneider <lanebret85@gmail.com>
2023-07-16 16:58:47 -0700

    refactor: modify code inside .each block of create_vehicles method

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -5,10 +5,19 @@ class VehicleFactory
   def create_vehicles(registrations)
     vehicles = []
     registrations.each do |registration|
-      require 'pry';binding.pry
-      registration[engine] = :ev
-      vehicle = Vehicle.new({registration[:engine], registration[:make], registration[:model]})
-      vehicles << vehicle
+      # require 'pry';binding.pry
+      registration[:engine] = :ev
+      #need to refactor below; throwing syntax error
+      new_vehicle = Vehicle.new({
+        engine: registration[:engine],
+        make: registration[:make],
+        model: registration[:model],
+        plate_type: registration[:plate_type],
+        registration_date: registration[:registration_date],
+        vin: registration[:vin_1_10],
+        year: registration[:model_year]
+      })
+      vehicles << new_vehicle
     end
     vehicles
   end
