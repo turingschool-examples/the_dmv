65fb94ae1a7ffa1bd9d2106a8e150a4c9b6ad9a6
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-16 12:50:52 -0500

    define creat_vehicle method in vehicle facorty class. Yet to test logic in spec file.

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,4 +1,23 @@
 class VehicleFactory
+  attr_reader
 
-  
+  def initialize
+
+  end
+
+  def create_vehicles(registration_location)
+    new_vehicles = []
+    registration_location.each do |vehicle|
+      vehicle_details = ({
+        vin: vehicle[:vin_1_10] , 
+        year: vehicle[:model_year] , 
+        make: vehicle[:make] , 
+        model: vehicle[:model] , 
+        engine: [:ev]
+      })
+      new_vehicle << Vehicle.new(vehicle_details) 
+    end
+    new_vehicles
+    require 'pry'; binding.pry
+  end
 end
