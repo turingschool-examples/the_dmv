f720cc8a593cd0a2c6b7f5113604d47854d36f5e
Cory Powell <coryrpowell@gmail.com>
2023-07-16 12:04:03 -0400

    Begin tests for vehicle_registration

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -3,13 +3,28 @@ require 'spec_helper'
 RSpec.describe Facility do
   before do
     @facility_1 = Facility.new({name: 'DMV Tremont Branch', 
-    address: '2855 Tremont Place Suite 118 Denver CO 80205', 
-    phone: '(720) 865-4600'
-    })
+      address: '2855 Tremont Place Suite 118 Denver CO 80205', 
+      phone: '(720) 865-4600'
+      })
     @facility_2 = Facility.new({name: 'DMV Northeast Branch', 
-    address: '4685 Peoria Street Suite 101 Denver CO 80239', 
-    phone: '(720) 865-4600'
-    })
+      address: '4685 Peoria Street Suite 101 Denver CO 80239', 
+      phone: '(720) 865-4600'
+      })
+    @cruz = Vehicle.new({vin: '123456789abcdefgh',
+      year: 2012,
+      make: 'Chevrolet',
+      model: 'Cruz',
+      engine: :ice} )
+    @bolt = Vehicle.new({vin: '987654321abcdefgh',
+      year: 2019,
+      make: 'Chevrolet',
+      model: 'Bolt', 
+      engine: :ev} )
+    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f',
+      year: 1969,
+      make: 'Chevrolet',
+      model: 'Camaro',
+      engine: :ice} )
   end
 
   describe '#initialize' do
@@ -44,12 +59,17 @@ RSpec.describe Facility do
     end
   end
 
-  describe '#registered_vehicles' do
-    xit 'can register vehicles' do
-      excpect(facility_1.register_vehicle(cruz)).to eq
+  describe '#vehicle_registration' do
+    it 'can register vehicles' do
+      @facility_1.add_service('Vehicle Registration')
+      @facility_1.register_vehicle(@cruz)
+
+      expect(@cruz.registration_date).to eq(nil)
+      expect(@facility_1.registered_vehicles).to eq([])
+      expect(@facility_1.collected_fees).to eq(0)
     
 
-      require 'pry';binding.pry
+      # require 'pry';binding.pry
     end
   end
 end
