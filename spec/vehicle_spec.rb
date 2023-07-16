fdc9aff5a8d295f9b13ed1d4f1cbf934e8881cc0
Nicholas McEnroe <nsmcenro@gmail.com>
2023-07-16 15:45:52 -0400

    Test: Add setting registration date tests

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -5,7 +5,10 @@ RSpec.describe Vehicle do
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+    @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
   end
+
   describe '#initialize' do
     it 'can initialize' do
       expect(@cruz).to be_an_instance_of(Vehicle)
@@ -37,6 +40,8 @@ RSpec.describe Vehicle do
   describe '#registration_date' do
     it 'can add the registration date of a vehicle' do
         expect(@cruz.registration_date).to eq(nil)
+        @facility_1.register_vehicle(@cruz)
+        expect(@cruz.registration_date).not_to eq(nil)
     end
   end
 end
