4eab06813fa187977ffc77cf18afeda6d233844f
Lane Bretschneider <lanebret85@gmail.com>
2023-07-16 12:29:37 -0700

    refctor: modify test formats for initialize and create_vehicles methods

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -4,13 +4,14 @@ RSpec.describe VehicleFactory do
   describe "#initialize" do
     it "can initialize" do
       factory = VehicleFactory.new
-      
+
       expect(factory).to be_a(VehicleFactory)
     end
   end 
 
   describe "#create_vehicles" do
-    xit "can create vehicles" do
+    it "can create vehicles" do
+      factory = VehicleFactory.new
       wa_ev_registrations = DmvDataService.new.wa_ev_registrations
 
       expect(factory.create_vehicles(wa_ev_registrations)).to eq(@wa_ev_registrations)
