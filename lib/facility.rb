c8b4458101f07e5bb560af4e760edea4609a2ac0
Cory Powell <coryrpowell@gmail.com>
2023-07-16 00:24:58 -0400

    Start register_vehicle method

--- lib/facility.rb
+++ lib/facility.rb
@@ -11,7 +11,24 @@ class Facility
     @services = []
   end
 
+  # can probably refactor this in to the initialization
   def add_service(services)
     @services << services
+  end
+
+  def registered_vehicles
+    @registered_vehicles = []
+  end
+
+  def collected_fees
+    @collected_fees = 0
+  end
+
+  def register_vehicle(vehicle)
+    @registered_vehicles << (vehicle)
+    vehicle.registration_date = Date.today
+
+    
+    
   end
 end
