8f4fbaa31277ee453639c3aa0fc2635724f4ceec
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-16 11:02:40 -0500

    Test initialization of Vehicle Factory class.

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -3,5 +3,12 @@ require 'spec_helper'
 RSpec.describe VehicleFactory do 
   before(:each) do 
     @factory = VehicleFactory.new
+    @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
   end
+  describe '#initialize' do 
+    it 'can initialize' do 
+      expect(@factory).to be_a(VehicleFactory)
+    end
+  end
+  
 end
