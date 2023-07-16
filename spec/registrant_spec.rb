2e8d61a051b79f2e916a7106e2372ddd4722ed02
Lane Bretschneider <lanebret85@gmail.com>
2023-07-15 23:55:28 -0700

    test: add test for renew_drivers_license method

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -103,4 +103,35 @@ RSpec.describe Registrant do
       expect(registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
     end
   end
+
+  describe "#renew_drivers_license" do
+    it "can return a value of true when registrant renews their license" do
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 16 )
+      registrant_3 = Registrant.new('Tucker', 15 )
+      
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+      
+      facility_1.add_service('Written Test')
+      registrant_2.earn_permit
+      facility_1.administer_written_test(registrant_1)
+      facility_1.administer_written_test(registrant_2)
+
+      expect(facility_1.renew_drivers_license(registrant_1)).to eq(false)
+      
+      facility_1.add_service('Renew License')
+      facility_1.renew_drivers_license(registrant_1)
+
+      expect(registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
+      
+      facility_1.renew_drivers_license(registrant_3)
+      
+      expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      
+      facility_1.renew_drivers_license(registrant_2)
+      
+      expect(registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
+    end
+  end
 end
