264d5058baf4443fc00b87a95a14b508b28fba77
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-07-16 15:59:08 -0500

    test/refactor: registrant class

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -10,10 +10,12 @@ RSpec.describe Registrant do
       expect(@registrant_1).to be_an_instance_of(Registrant)
       expect(@registrant_1.name).to eq("Bruce")
       expect(@registrant_1.age).to eq(18)
+      expect(@registrant_1.permit?).to eq(true)
       expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
-      expect(@registrant_1).to be_an_instance_of(Registrant)
+      expect(@registrant_2).to be_an_instance_of(Registrant)
       expect(@registrant_2.name).to eq("Penny")
       expect(@registrant_2.age).to eq(15)
+      expect(@registrant_2.permit?).to eq(false)
       expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
   end
@@ -21,12 +23,17 @@ RSpec.describe Registrant do
   describe '#permit?' do
     it 'returns if registrant has permit' do
       expect(@registrant_1.permit?).to eq(true)
+      expect(@registrant_2.permit?).to eq(false)
+      @registrant_2.earn_permit
+
+      expect(@registrant_2.permit?).to eq(true)
     end
   end
 
   describe "#earn_permit" do
     it "Changes permit to true" do
       @registrant_2.earn_permit
+      
       expect(@registrant_2.permit?).to eq(true)
     end
   end
