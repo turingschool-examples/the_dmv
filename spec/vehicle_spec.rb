663ad00a79e72ca8036b07df669416a039398135
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-15 20:08:33 -0600

    Failure in interaction pattern, correct 'registration date' test

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Vehicle do
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+    @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
   end
   describe '#initialize' do
     it 'can initialize' do
@@ -44,9 +45,9 @@ RSpec.describe Vehicle do
 
   describe '#registration_date' do
     it 'can determine registration date of a vehicle' do
-      @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
       @facility.register_vehicle(@cruz)
 
+      expect(@facility.registered_vehicles).to eq([@cruz])
       expect(@cruz.plate_type).to eq(:regular)
       expect(@cruz.registration_date).to eq(Date.today.year)
     end
