97bbd414bde82d36c8f844de6012ff7ae8d2be80
Cory Powell <coryrpowell@gmail.com>
2023-07-15 22:31:35 -0400

    Add plate_type and clean up spec

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -15,17 +15,28 @@ RSpec.describe Facility do
   describe '#initialize' do
     it 'can initialize' do
       expect(@facility_1).to be_a(Facility)
-      
+      expect(@facility_2).to be_a(Facility)
+    end
+  end
+
+  describe '#attributes' do
+    it 'has readable attributes' do
       expect(@facility_1.name).to eq('DMV Tremont Branch')
       expect(@facility_1.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
       expect(@facility_1.phone).to eq('(720) 865-4600')
       expect(@facility_1.services).to eq([])
+
+      expect(@facility_2.name).to eq('DMV Northeast Branch')
+      expect(@facility_2.address).to eq('4685 Peoria Street Suite 101 Denver CO 80239')
+      expect(@facility_2.phone).to eq('(720) 865-4600')
+      expect(@facility_2.services).to eq([])
     end
   end
 
   describe '#add service' do
-    xit 'can add available services' do
-      expect(@facility.services).to eq([])
+    it 'can add available services' do
+      expect(@facility_1.services).to eq([])
+      
       @facility_1.add_service('New Drivers License')
       @facility_1.add_service('Renew Drivers License')
       @facility_1.add_service('Vehicle Registration')
