2ec44d88dfa692204f5beb6ff6639a130e773896
Allan Evans <oneblankpage27@gmail.com>
2023-07-16 19:52:54 -0600

    Test: Add service for new york and missouri service along with tests

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,28 +1,50 @@
 class FacilityFactory
   attr_reader :dmv_list
   def initialize
+    @current_state = ""
     @dmv_list = []
   end
 
   def find_facilities(data)
     #require 'pry';binding.pry
+    check_state(data.first[:state])
     data.each do |dmv|
-      #(vehicle) :ev
-      @dmv_list << Facility.new({
-        name: dmv[:dmv_office],
-         address: format_address(dmv[:address_li], dmv[:address__1]),
-          phone: dmv[:phone]})
+      @dmv_list << Facility.new(
+      if @current_state == "CO"
+        {name: dmv[:dmv_office],
+        address: format_co_address(dmv[:address_li], dmv[:address__1], dmv[:city], dmv[:state], dmv[:zip]),
+        phone: dmv[:phone]}
+      elsif @current_state == "NY"
+        {name: dmv[:office_name],
+        address: format_address(dmv[:street_address_line_1], dmv[:city], dmv[:state], dmv[:zip_code]),
+        phone: dmv[:public_phone_number]}
+      elsif @current_state == "MO"
+        {name: dmv[:name],
+        address: format_address(dmv[:address1], dmv[:city], dmv[:state], dmv[:zipcode]),
+        phone: dmv[:phone]}
+      else
+        #default values to check for
+      end
+    )
     end
         #require 'pry';binding.pry
 
     #puts @vehicle_list.first.model
   end
 
-  def format_address(address1, address2)
+  def check_state(state)
+    @current_state = state
+  end
+
+  def format_co_address(address1, address2, city, state, zip)
     if address2 == nil
-      "#{address1}"
+      "#{address1} #{city} #{state} #{zip}"
     else
-      "#{address1}, #{address2}"
+      "#{address1} #{address2} #{city} #{state} #{zip}"
     end
+  end
+
+  def format_address(address, city, state, zip)
+      "#{address} #{city} #{state} #{zip}"
   end
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -16,4 +16,30 @@ RSpec.describe FacilityFactory do
       expect(factory.dmv_list.length).to be_an(Integer)
     end
   end
+
+  describe '#Sources' do
+    it 'data sources from different states' do
+      factory = FacilityFactory.new
+      expect(factory).to be_an_instance_of(FacilityFactory)
+
+      co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+      new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+      missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+
+      expect(factory.find_facilities(co_dmv_office_locations)).to be_an(Array)
+      expect(factory.dmv_list.first).to be_an_instance_of(Facility) 
+      expect(factory.dmv_list.last).to be_an_instance_of(Facility)
+      expect(factory.dmv_list.length).to be_an(Integer)
+
+      expect(factory.find_facilities(new_york_facilities)).to be_an(Array)
+      expect(factory.dmv_list.first).to be_an_instance_of(Facility) 
+      expect(factory.dmv_list.last).to be_an_instance_of(Facility)
+      expect(factory.dmv_list.length).to be_an(Integer)
+
+      expect(factory.find_facilities(missouri_facilities)).to be_an(Array)
+      expect(factory.dmv_list.first).to be_an_instance_of(Facility) 
+      expect(factory.dmv_list.last).to be_an_instance_of(Facility)
+      expect(factory.dmv_list.length).to be_an(Integer)
+    end
+  end
 end
