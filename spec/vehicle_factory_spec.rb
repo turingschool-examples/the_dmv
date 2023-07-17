8ee6103974453f9d050a14ada25588992588127e
Joop Stark <stark.joop@gmail.com>
2023-07-17 13:18:08 -0600

    test: exists for vehicle factory

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe VehicleFactory do
+  it "exists" do
+    factory = VehicleFactory.new
+    
+    expect(factory).to be_a(VehicleFactory)
+  end
+
   it "can take data from an API and create vehicle objects" do
     wa_ev_registrations = DmvDataService.new.wa_ev_registrations
     factory = VehicleFactory.new
