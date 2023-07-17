c4de13661a9703ed4942dea7e60a4de30dd1d6ca
Allan Evans <oneblankpage27@gmail.com>
2023-07-17 14:54:35 -0600

    Test: Add most popular tests and methods to vehicle factory class and spec.  Updated initializer in vehicle class to take countys.  Tests still run fine

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -5,19 +5,40 @@ class VehicleFactory
   end
 
   def create_vehicles(data)
-    #require 'pry';binding.pry
     data.each do |vehicle|
-      #(vehicle) :ev
       @vehicle_list << Vehicle.new({
         vin: vehicle[:vin_1_10],
         year: vehicle[:model_year],
         make: vehicle[:make],
         model: vehicle[:model],
+        county: vehicle[:county],
         engine: :ev
+        
       })
     end
-        #require 'pry';binding.pry
+  end
 
-    #puts @vehicle_list.first.model
+  def most_popular_registered_make
+    make_count = Hash.new(0)
+    @vehicle_list.each {|car| make_count[car.make] += 1}
+    make_count.sort_by { |make,number| number}.last[0]
+  end
+
+  def most_popular_registered_model
+    model_count = Hash.new(0)
+    @vehicle_list.each {|car| model_count[car.model] += 1}
+    model_count.sort_by { |model,number| number}.last[0]
+  end
+
+  def most_popular_registered_year
+    year_count = Hash.new(0)
+    @vehicle_list.each {|car| year_count[car.year] += 1}
+    year_count.sort_by { |year,number| number}.last[0]
+  end
+
+  def most_popular_registered_county
+    county_count = Hash.new(0)
+    @vehicle_list.each {|car| county_count[car.county] += 1}
+    county_count.sort_by { |county,number| number}.last[0]
   end
 end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -16,4 +16,22 @@ RSpec.describe VehicleFactory do
       expect(factory.vehicle_list.length).to be_an(Integer)
     end
   end
+
+  describe '#popular' do
+    it 'find most popular make, model, year, and county' do
+      factory = VehicleFactory.new
+      expect(factory).to be_an_instance_of(VehicleFactory)
+
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+      expect(factory.create_vehicles(wa_ev_registrations)).to be_an(Array)
+      expect(factory.vehicle_list.first).to be_an_instance_of(Vehicle) 
+      expect(factory.vehicle_list.last).to be_an_instance_of(Vehicle)
+      expect(factory.vehicle_list.length).to be_an(Integer)
+
+      expect(factory.most_popular_registered_make).to eq("TESLA")
+      expect(factory.most_popular_registered_model).to eq("Leaf")
+      expect(factory.most_popular_registered_year).to eq("2018")
+      expect(factory.most_popular_registered_county).to eq("King")
+    end
+  end
 end
