c70e17bd6966b2479fc3930047a982208670e13f
Lane Bretschneider <lanebret85@gmail.com>
2023-07-17 14:28:43 -0700

    refactor: modify initialize and create_facilities methods

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,19 +1,18 @@
 class FacilityFactory
   def initialize
+    @facilities = []
   end
 
   def create_facilities(locations)
-    facilities = []
     locations.each do |location|
       new_facility = Facility.new({
         name: location[:dmv_office],
-        #need to fix:
-        address: location[:address_li + " " + :address__1 + " " + :city + ", " + :state + " " + :zip],
-        phone: location[:phone],
-        services: location[:services_p]
+        address: "#{location[:address_li]} #{location[:address__1]} #{location[:city]}, #{location[:state]} #{location[:zip]}",
+        phone: location[:phone]
       })
-      facilities << new_facility
+      new_facility.add_service(location[:services_p])
+      @facilities << new_facility
     end
-    facilities
+    @facilities
   end
 end
