caabb2da5f5e7f232ab640762331ba83a5bfabae
Nicholas McEnroe <nsmcenro@gmail.com>
2023-07-17 16:41:51 -0400

    Test: Add renew license test that fails when conditions are not met

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -147,10 +147,29 @@ RSpec.describe Facility do
       @facility_1.add_service('Written Test')
       @facility_1.add_service('Road Test')
       @facility_1.add_service('Renew License')
+
       @facility_1.administer_written_test(@registrant_1)
       @facility_1.administer_road_test(@registrant_1)
       @facility_1.renew_drivers_license(@registrant_1)
       expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
+
+      @registrant_2.earn_permit
+      @facility_1.administer_written_test(@registrant_2)
+      @facility_1.administer_road_test(@registrant_2)
+      @facility_1.renew_drivers_license(@registrant_2)
+      expect(@registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
+    end
+
+    it 'does not renew the license when conditions are not met' do
+      @facility_1.add_service('Written Test')
+      @facility_1.add_service('Road Test')
+      @facility_1.add_service('Renew License')
+
+      @registrant_3.earn_permit
+      @facility_1.administer_written_test(@registrant_3)
+      @facility_1.administer_road_test(@registrant_3)
+      @facility_1.renew_drivers_license(@registrant_3)
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
   end
 
