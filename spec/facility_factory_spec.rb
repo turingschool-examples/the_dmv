816d1a71be92e732b1d13224365af0fc1c4ee84f
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-16 20:11:11 -0500

    test initialization of FacilityFactory class, correct spelling line 4, rename object line 10.

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
-RSpec.describe FacilityFactroy do 
-  
+RSpec.describe FacilityFactory do 
+  before(:each) do 
+    @factory = FacilityFactory.new
+  end
+
+  describe '#initialize' do 
+    it 'can initialize' do 
+      expect(@factory).to be_a(FacilityFactory)
+    end
+  end
 end
