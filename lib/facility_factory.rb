8018a7d0033b09a7eeabe675772bfdf96534ec2a
Reed Hillmar <reedhillmar@gmail.com>
2023-07-17 08:33:07 -0600

    refactor: make facility_factory more robust by adding city/state/zip to address

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -10,19 +10,19 @@ class FacilityFactory
       if facility[:state] == "CO"
         @facilities << Facility.new({
           name: name_formatter(facility[:dmv_office]),
-          address: name_formatter(facility[:address_li]),
+          address: name_formatter("#{facility[:address_li]} #{facility[:address__1]}, #{facility[:city]}, #{facility[:state]} #{facility[:zip]}"),
           phone: phone_number_formatter(facility[:phone])
         })
       elsif facility[:state] == "NY"
         @facilities << Facility.new({
           name: name_formatter(facility[:office_name]),
-          address: name_formatter(facility[:street_address_line_1]),
+          address: name_formatter("#{facility[:street_address_line_1]} #{facility[:street_address_line_2]}, #{facility[:city]}, #{facility[:state]} #{facility[:zip_code]}"),
           phone: phone_number_formatter(facility[:public_phone_number])
         })
       elsif facility[:state] == "MO"
         @facilities << Facility.new({
           name: name_formatter(facility[:name]),
-          address: name_formatter(facility[:address1]),
+          address: name_formatter("#{facility[:address1]} #{facility[:address2]}, #{facility[:city]}, #{facility[:state]} #{facility[:zipcode]}"),
           phone: phone_number_formatter(facility[:phone])
         })
       end
@@ -31,7 +31,7 @@ class FacilityFactory
   end
 
   def phone_number_formatter(string)
-    if string != nil
+    if !string.nil?
       string.delete("^0-9").insert(6, "-").insert(3, ") ").prepend("(")
     else
       "Not listed"
@@ -43,11 +43,17 @@ class FacilityFactory
       string_array = string.downcase.split.map do |word|
         if word == "dmv"
           word.upcase
+        elsif word == "co"
+          word.upcase
+        elsif word == "ny"
+          word.upcase
+        elsif word == "mo"
+          word.upcase
         else
           word.capitalize
         end
       end
-      string_array.join(" ")
+      string_array.join(" ").sub(" ,", ",")
     else
       "Not listed"
     end
