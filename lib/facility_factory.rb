009a886b81c8f74ce8b66a97378ea48ad67c6a09
Joop Stark <stark.joop@gmail.com>
2023-07-16 22:56:09 -0600

    test: researching how to test

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,6 +1,5 @@
 require './spec/spec_helper'
 
-
 class FacilityFactory
   attr_reader :state_factilities
   def initialize
@@ -14,10 +13,22 @@ class FacilityFactory
   end
 end
 
-co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+# co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+# colorado = FacilityFactory.new
+# colorado.store_facitlities(co_dmv_office_locations)
+# colorado.state_factilities.each { |facility| p facility.phone}
+# p co_dmv_office_locations.count
 
-colorado = FacilityFactory.new
+new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+p new_york_facilities[1]
+p new_york_facilities[2]
+new_york = FacilityFactory.new
+new_york.store_facitlities(new_york_facilities)
 
-colorado.store_facitlities(co_dmv_office_locations)
+new_york.state_factilities.each {|x| p x.address}
+# p new_york.state_factilities.each { |facility| p facility}
 
-p colorado.state_factilities[0]
+# missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+# missouri = FacilityFactory.new
+# missouri.store_facitlities(missouri_facilities)
+# missouri.state_factilities.each { |facility| p facility.address}
