ff20f10611a06f92f885679068179d51e69002c4
Lane Bretschneider <lanebret85@gmail.com>
2023-07-17 16:00:13 -0700

    test: add test for create_facilities_ny

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe FacilityFactory do
     end
   end
 
-  describe "#create_facilities" do
-    it "can create facilities" do
+  describe "#create_facilities_co" do
+    xit "can create facilities for Colorado DMV locations" do
       factory = FacilityFactory.new
       co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
       factory.create_facilities(co_dmv_office_locations)
@@ -19,7 +19,12 @@ RSpec.describe FacilityFactory do
       expect(factory.create_facilities(co_dmv_office_locations)[0].name).to eq("DMV Tremont Branch")
       expect(factory.create_facilities(co_dmv_office_locations)[0].address).to eq("2855 Tremont Place Suite 118 Denver, CO 80205")
       expect(factory.create_facilities(co_dmv_office_locations)[0].phone).to eq("(720) 865-4600")
+    end
+  end
 
+  describe "#create_facilities_ny" do
+    it "can create facilities for New York DMV locations" do
+      factory = FacilityFactory.new
       ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
       factory.create_facilities(ny_dmv_office_locations)
 
