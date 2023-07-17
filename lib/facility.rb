dd625c39a48718d39595d25cff3b84bf5b975d72
Will Weston <Williamweston81@gmail.com>
2023-07-17 15:17:54 -0500

    Weston(1) Fix admin_written_test to include else false return # This passes         expect(@facility_1.administer_written_test(@registrant_2)).to be false test

--- lib/facility.rb
+++ lib/facility.rb
@@ -69,7 +69,11 @@ class Facility
   end
 
   def administer_written_test(registrant)
-    registrant.permit? && registrant.age >= 16
+    if registrant.permit? && registrant.age >= 16
     registrant.license_data[:written] = true
+      true
+    else 
+      false
+    end
   end
 end
