0245a0ca8317855bf801c13a266ea3855c5542df
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-07-17 15:07:48 -0600

    feat: Add register_vehicle method to facility class and add plate_type instance variable method to vehicle class

--- lib/facility.rb
+++ lib/facility.rb
@@ -2,16 +2,39 @@ class Facility
   attr_reader :name, 
               :address, 
               :phone, 
-              :services
+              :services,
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(details)
     @name = details[:name]
     @address = details[:address]
     @phone = details[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    if services.include?("Vehicle Registration")
+      vehicle.registration_date = Date.today
+
+    @registered_vehicles << vehicle
+      if vehicle.antique?
+        vehicle.plate_type = :antique
+        @collected_fees += 25
+      elsif vehicle.electric_vehicle?
+        vehicle.plate_type = :ev
+        @collected_fees += 200
+      else 
+        vehicle.plate_type = :regular
+        @collected_fees += 100
+      end
+      vehicle
+    end
   end
 end
