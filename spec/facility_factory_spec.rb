b49be95ece64211085e4a77ea7becfdba8b08f53
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-17 10:30:18 -0600

    test: add CO data test for facility factory class

---
+++ spec/facility_factory_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+require "./lib/facility_factory"
+
+RSpec.describe FacilityFactory do
+  describe "#initialize" do
+    it "exists" do
+      factory = FacilityFactory.new
+
+      expect(factory).to be_a(FacilityFactory)
+    end
+  end
+
+  describe "create_facilities" do
+    it "creates facilities with CO external data" do
+      factory = FacilityFactory.new
+
+      colorado_facilities = DmvDataService.new.co_dmv_office_locations
+      
+      expect(factory.create_facilities).to be_a(Array)
+    end
+  end
+end
