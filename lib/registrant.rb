aac7803b9513084259cc9401513b37306baebebd
Nurudeen Sulaimon <nerodean@Neros-MacBook-Pro.local>
2023-07-17 01:10:42 -0400

    Added Class Method Permit?

---
+++ lib/registrant.rb
@@ -0,0 +1,13 @@
+class Registrant
+   attr_reader :name, :age, :permit, :license_data
+    def initialize(name,age, permit=false)
+       @name=name 
+       @age=age 
+       @license_data={:written=>false, :license=>false, :renewed=>false}
+       @permit=permit 
+    end 
+
+    def permit?
+      true 
+    end 
+  end 
