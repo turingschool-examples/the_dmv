726cb139b6fcfdd94fa8efdd206c5a46f71ac274
Joop Stark <stark.joop@gmail.com>
2023-07-17 15:49:54 -0600

    test: use test
    ''

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -198,7 +198,7 @@ RSpec.describe Facility do
     end
   end
 
-  describe "helper tests" do
+  describe "use tests" do
     it "ht1 #issue_plate_fees can issue new place and add to collected fees" do
       @facility_1.issue_plate_fees(@cruz)
       @facility_1.issue_plate_fees(@bolt)
@@ -210,4 +210,21 @@ RSpec.describe Facility do
       expect(@camaro.plate_type).to eq(:antique)
     end
   end
+
+  describe "API use tests" do
+    let (:colorado_datapoint) {{:the_geom=>{:type=>"Point", :coordinates=>[-104.97443112500002, 39.75525297420336]}, :dmv_id=>"1", :dmv_office=>"DMV Tremont Branch", :address_li=>"2855 Tremont Place", :address__1=>"Suite 118", :city=>"Denver", :state=>"CO", :zip=>"80205", :phone=>"(720) 865-4600", :hours=>"Mon, Tue, Thur, Fri  8:00 a.m.- 4:30 p.m. / Wed 8:30 a.m.-4:30 p.m.", :services_p=>"vehicle titles, registration, renewals;  VIN inspections", :parking_no=>"parking available in the lot at the back of the bldg (Glenarm Street)", :photo=>"images/Tremont.jpg", :address_id=>"175164", :":@computed_region_nku6_53ud"=>"1444"}}
+    let (:new_york_datapoint) {{:the_geom=>{:type=>"Point", :coordinates=>[-104.97443112500002, 39.75525297420336]}, :dmv_id=>"1", :dmv_office=>"DMV Tremont Branch", :address_li=>"2855 Tremont Place", :address__1=>"Suite 118", :city=>"Denver", :state=>"CO", :zip=>"80205", :phone=>"(720) 865-4600", :hours=>"Mon, Tue, Thur, Fri  8:00 a.m.- 4:30 p.m. / Wed 8:30 a.m.-4:30 p.m.", :services_p=>"vehicle titles, registration, renewals;  VIN inspections", :parking_no=>"parking available in the lot at the back of the bldg (Glenarm Street)", :photo=>"images/Tremont.jpg", :address_id=>"175164", :":@computed_region_nku6_53ud"=>"1444"}}
+    let (:missouri_datapoint) {{:the_geom=>{:type=>"Point", :coordinates=>[-104.97443112500002, 39.75525297420336]}, :dmv_id=>"1", :dmv_office=>"DMV Tremont Branch", :address_li=>"2855 Tremont Place", :address__1=>"Suite 118", :city=>"Denver", :state=>"CO", :zip=>"80205", :phone=>"(720) 865-4600", :hours=>"Mon, Tue, Thur, Fri  8:00 a.m.- 4:30 p.m. / Wed 8:30 a.m.-4:30 p.m.", :services_p=>"vehicle titles, registration, renewals;  VIN inspections", :parking_no=>"parking available in the lot at the back of the bldg (Glenarm Street)", :photo=>"images/Tremont.jpg", :address_id=>"175164", :":@computed_region_nku6_53ud"=>"1444"}}
+    
+    it "#name_finder can finds names in colorado" do
+      expect(@facility_1.name_finder(colorado_datapoint)).to eq("DMV Tremont Branch")
+    end
+
+
+    
+  end
+
+
+
+
 end
