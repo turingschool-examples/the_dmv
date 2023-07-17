858765b55845af8114b377aa99c34edbf849c8a4
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-17 08:48:20 -0500

    refractor logic in vehicle and facility factory methods.

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -3,11 +3,11 @@ class FacilityFactory
   def create_facilities(facility_location)
     new_facilities = []
     facility_location.each do |facility|
-      facility_details = {
+      facility_details = ({
         name: facility[:name],
         address: facility[:address],
         phone: facility[:phone]
-      }
+      })
       new_facilities << Facility.new(facility_details)
     end
     new_facilities
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 RSpec.describe VehicleFactory do 
   before(:each) do 
-    @factory = VehicleFactory.new
+    @vehicle_factory = VehicleFactory.new
     @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
   end
 
   describe '#initialize' do 
     it 'can initialize' do 
-      expect(@factory).to be_a(VehicleFactory)
+      expect(@vehicle_factory).to be_a(VehicleFactory)
     end
   end
   
   describe '#create vehicles' do 
     xit 'can create vehicle objects' do 
-      new_vehicles = @factory.create_vehicles(@wa_ev_registrations)
+      new_vehicles = @vehicle_factory.create_vehicles(@wa_ev_registrations)
       expect(new_vehicles).to be_a(Vehicle)
     end
   end
