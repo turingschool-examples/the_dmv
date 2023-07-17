cc9322f478185bbf5434ba5b8255e87eadbfc20a
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-16 20:14:54 -0500

    add new york and missouri facility object to test create_facilities method to be written.

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -3,11 +3,19 @@ require 'spec_helper'
 RSpec.describe FacilityFactory do 
   before(:each) do 
     @factory = FacilityFactory.new
+    @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+    @missouri_facilities = DmvDataService.new.mo_dmv_office_locations
   end
 
   describe '#initialize' do 
     it 'can initialize' do 
       expect(@factory).to be_a(FacilityFactory)
+    end
+  end
+
+  describe '#create_facilities' do 
+    it 'can create facility objects' do 
+      
     end
   end
 end
