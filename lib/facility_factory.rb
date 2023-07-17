4dd499f78d97db101e1864e48e53ca0fa648daa8
Joop Stark <stark.joop@gmail.com>
2023-07-17 14:08:38 -0600

    refactor: removed extra lines

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,5 +1,3 @@
-require './spec/spec_helper'
-
 class FacilityFactory
   attr_reader :state_factilities
   def initialize
@@ -12,23 +10,3 @@ class FacilityFactory
   }
   end
 end
-
-# co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
-# colorado = FacilityFactory.new
-# colorado.store_facitlities(co_dmv_office_locations)
-# colorado.state_factilities.each { |facility| p facility.phone}
-# p co_dmv_office_locations.count
-
-new_york_facilities = DmvDataService.new.ny_dmv_office_locations
-p new_york_facilities[1]
-p new_york_facilities[2]
-new_york = FacilityFactory.new
-new_york.store_facitlities(new_york_facilities)
-
-new_york.state_factilities.each {|x| p x.address}
-# p new_york.state_factilities.each { |facility| p facility}
-
-# missouri_facilities = DmvDataService.new.mo_dmv_office_locations
-# missouri = FacilityFactory.new
-# missouri.store_facitlities(missouri_facilities)
-# missouri.state_factilities.each { |facility| p facility.address}
