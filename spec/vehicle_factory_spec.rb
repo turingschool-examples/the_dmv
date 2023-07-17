8a04a111eab8c6ee69d9fb142436c900f6d552cf
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-07-17 16:03:16 -0500

    test: updated TDD Vehicle Factory

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -12,6 +12,12 @@ RSpec.describe VehicleFactory do
       @factory.create_vehicles(@wa_ev_registrations)
       expect(@factory.create_vehicles(@wa_ev_registrations)).to be_an Array
       expect(@factory.create_vehicles(@wa_ev_registrations)).to all be_a Vehicle
+      expect(@factory.create_vehicles(@wa_ev_registrations).first.engine).to eq(:ev)
+      expect(@factory.create_vehicles(@wa_ev_registrations).first.model).to eq("Volt")
+      expect(@factory.create_vehicles(@wa_ev_registrations).first.plate_type).to eq(nil)
+      expect(@factory.create_vehicles(@wa_ev_registrations).first.registration_date).to eq(nil)
+      expect(@factory.create_vehicles(@wa_ev_registrations).first.vin).to eq("1G1RC6S55J")
+      expect(@factory.create_vehicles(@wa_ev_registrations).first.year).to eq("2018")
     end
   end
 end
