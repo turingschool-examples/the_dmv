9c5fab40b3299c9de42dc1d8789dae3c8946e69e
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-17 21:51:53 -0600

    test: add test for create_facilities in facility_factory

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe FacilityFactory do
       expect(ff).to be_a FacilityFactory
     end
   end
+
+  describe "#create_facilities" do
+    it "creates an array of facilities using API data from CO" do
+      ff = FacilityFactory.new
+
+      co_dmv_offices = DmvDataService.new.co_dmv_office_locations
+
+      expect(ff.create_facilities(co_dmv_offices)).to not be_empty
+      expect(ff.create_facilities(co_dmv_offices).first).to be_a Facility
+    end
+  end
 end
