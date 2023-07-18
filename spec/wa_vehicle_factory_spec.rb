eb4f130f22d997e386e9293a61838d5d8664e1cd
Will Weston <Williamweston81@gmail.com>
2023-07-17 19:31:04 -0500

    Weston(1) Factory pulls data Created create_vehicles test to pull wa_ev_registrations -- pass?

--- spec/wa_vehicle_factory_spec.rb
+++ spec/wa_vehicle_factory_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Registrant do
       expect(@factory).to be_an_instance_of(VehicleFactory)
     end
   end
+
+  describe '#wa_ev_registrations' do
+    it 'shows pulled data from wa_ev_registrations' do
+      expect(@wa_ev_registrations = DmvDataService.new.wa_ev_registrations).to include()
+    end
+  end
+
+  describe '#create WA vehicles' do
+    it 'creates WA vehicles' do
+      expect(@factory.create_vehicles(@wa_ev_registrations)).to include()
+    end
+  end
 end
