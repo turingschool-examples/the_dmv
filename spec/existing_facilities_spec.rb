6e87551648a4ee1e51fbb240897ba92bfbec8ea9
Nicholas McEnroe <nsmcenro@gmail.com>
2023-07-18 14:43:41 -0400

    Test: Add aditional test info for Missouri database

--- spec/existing_facilities_spec.rb
+++ spec/existing_facilities_spec.rb
@@ -12,11 +12,13 @@ RSpec.describe ExistingFacility do
     it "can initialize other facilities" do
         existing_facility_2 = ExistingFacility.new
         new_york_facilities = DmvDataService.new.ny_dmv_office_locations
-        puts existing_facility_2.read_information(new_york_facilities)
+        existing_facility_2.read_information(new_york_facilities)
+        #multiple instances exist of a single address, overriding some data, didnt have time to refactor that out
 
-        # existing_facility_3 = ExistingFacility.new
-        # missouri_facilities = DmvDataService.new.co_dmv_office_locations
-        # existing_facility.read_information(missouri_facilities)
+        existing_facility_3 = ExistingFacility.new
+        missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+        existing_facility_3.read_information(missouri_facilities)
+        #Note: I decided against adding the "services" of Missouri DMVs into the array.  Their "services" are better accessibility features, not actual services the facility can do on a registrant.
     end
 
 
