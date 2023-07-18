457563a53173a969d401cd728880e6fcb97ba6b5
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-07-18 15:27:49 -0600

    test: Test initialize and create_vehicles methods in vehiclefactory spec test

---
+++ spec/vehicle_factory_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+RSpec.describe VehicleFactory do
+  before(:each) do
+    @factory = VehicleFactory.new
+  end
+
+  describe "#initialize" do
+    it 'initializes' do
+      expect(@factory).to be_a(VehicleFactory)
+    end
+  end
+  
+  describe '#create_vehicles' do
+    it 'creates vehicles with the assigned attributes' do
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+
+      @factory.create_vehicles(wa_ev_registrations)
+
+      expect(@factory.vehicles).to be_a Array
+require 'pry';binding.pry
+    end
+  end
+end
