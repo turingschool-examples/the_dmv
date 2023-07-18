db934244a0b98be2862086e8479dcdb10bb419ff
Nicholas McEnroe <nsmcenro@gmail.com>
2023-07-18 17:49:14 -0400

    Fix: fix all scope issues in facility_spec file

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -76,111 +76,139 @@ RSpec.describe Facility do
     end
   end
 
-#   describe '#register_vehicle' do
-#     it "adds a vehicle to the facility's registered vehicles" do
-#       @facility_1.add_service('Vehicle Registration')
-#       @facility_1.register_vehicle(@cruz)
-#       expect(@facility_1.registered_vehicles).not_to eq([])
-#       @facility_1.register_vehicle(@camaro)
-#       expect(@facility_1.registered_vehicles).to eq([@cruz,@camaro])
-#       expect(@facility_1.register_vehicle(@bolt)).to eq([@cruz,@camaro,@bolt])
-#     end
+  describe '#register_vehicle' do
+    it "adds a vehicle to the facility's registered vehicles" do
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+      facility_1.add_service('Vehicle Registration')
+      facility_1.register_vehicle(cruz)
+      expect(facility_1.registered_vehicles).not_to eq([])
+      facility_1.register_vehicle(camaro)
+      expect(facility_1.registered_vehicles).to eq([cruz,camaro])
+      expect(facility_1.register_vehicle(bolt)).to eq([cruz,camaro,bolt])
+    end
 
-#     it "does not register a vehicle when the service is not available" do
-#       expect(@facility_2.register_vehicle(@bolt)).to eq(nil)
-#       expect(@facility_2.registered_vehicles).to eq([])
-#       expect(@facility_2.collected_fees).to eq(0)
-#     end
-#   end
+    it "does not register a vehicle when the service is not available" do
+      facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      expect(facility_2.register_vehicle(bolt)).to eq(nil)
+      expect(facility_2.registered_vehicles).to eq([])
+      expect(facility_2.collected_fees).to eq(0)
+    end
+  end
 
-#   describe '#administer_written_test' do
-#     it "returns the current_data when service has not been activated" do
-#       expect(@facility_1.administer_written_test(@registrant_1)).to eq(false)
-#     end
+  describe '#administer_written_test' do
+    it "returns the current_data when service has not been activated" do
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      expect(facility_1.administer_written_test(registrant_1)).to eq(false)
+    end
 
-#     it "returns true when using the method if service is activated" do
-#       expect(@facility_1.administer_written_test(@registrant_1)).to eq(false)
-#       expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
-#       @facility_1.add_service('Written Test')
-#       @facility_1.administer_written_test(@registrant_1)
-#       expect(@registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
-#     end
+    it "returns true when using the method if service is activated" do
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      expect(facility_1.administer_written_test(registrant_1)).to eq(false)
+      expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      facility_1.add_service('Written Test')
+      facility_1.administer_written_test(registrant_1)
+      expect(registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+    end
 
-#     it "only gives a written test when all conditions are met" do
-#       expect(@facility_1.administer_written_test(@registrant_2)).to eq(false)
-#       @facility_1.add_service('Written Test')
-#       @registrant_2.earn_permit
-#       @facility_1.administer_written_test(@registrant_2)
-#       expect(@registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
-#     end
+    it "only gives a written test when all conditions are met" do
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      registrant_2 = Registrant.new('Penny', 16 )
+      expect(facility_1.administer_written_test(registrant_2)).to eq(false)
+      facility_1.add_service('Written Test')
+      registrant_2.earn_permit
+      facility_1.administer_written_test(registrant_2)
+      expect(registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+    end
 
-#     it "returns another true statement when all conditions are met" do
-#       expect(@facility_1.administer_written_test(@registrant_3)).to eq(false)
-#       @facility_1.add_service('Written Test')
-#       @registrant_3.earn_permit
-#       @facility_1.administer_written_test(@registrant_3)
-#       expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
-#     end
-#   end
+    it "returns another true statement when all conditions are met" do
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      registrant_3 = Registrant.new('Tucker', 15 )
+      expect(facility_1.administer_written_test(registrant_3)).to eq(false)
+      facility_1.add_service('Written Test')
+      registrant_3.earn_permit
+      facility_1.administer_written_test(registrant_3)
+      expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+  end
 
-#   describe '#administer_written_test' do
-#     it "returns a false when all conditions are not met" do
-#       @facility_1.add_service('Road Test')
-#       expect(@facility_1.administer_road_test(@registrant_3)).to eq(false)
-#       @registrant_3.earn_permit
-#       @facility_1.administer_road_test(@registrant_3)
-#       expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
-#     end
+  describe '#administer_written_test' do
+    it "returns a false when all conditions are not met" do
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      registrant_3 = Registrant.new('Tucker', 15 )
+      facility_1.add_service('Road Test')
+      expect(@facility_1.administer_road_test(registrant_3)).to eq(false)
+      registrant_3.earn_permit
+      facility_1.administer_road_test(@registrant_3)
+      expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
 
-#     it "returns a true when all conditions are met" do
-#       expect(@facility_1.administer_road_test(@registrant_1)).to eq(false)
-#       @facility_1.add_service('Written Test')
-#       @facility_1.add_service('Road Test')
-#       @facility_1.administer_written_test(@registrant_1)
-#       @facility_1.administer_road_test(@registrant_1)
-#       expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
-#     end
+    it "returns a true when all conditions are met" do
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      expect(facility_1.administer_road_test(registrant_1)).to eq(false)
+      facility_1.add_service('Written Test')
+      facility_1.add_service('Road Test')
+      facility_1.administer_written_test(registrant_1)
+      facility_1.administer_road_test(registrant_1)
+      expect(registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
+    end
 
-#     it "returns a true when all conditions are originally met" do
-#       @facility_1.add_service('Written Test')
-#       @facility_1.add_service('Road Test')
-#       @registrant_2.earn_permit
-#       @facility_1.administer_written_test(@registrant_2)
-#       @facility_1.administer_road_test(@registrant_2)
-#       @facility_1.administer_road_test(@registrant_2)
-#       expect(@registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
-#     end
-#   end
+    it "returns a true when all conditions are originally met" do
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      registrant_2 = Registrant.new('Penny', 16 )
+      facility_1.add_service('Written Test')
+      facility_1.add_service('Road Test')
+      registrant_2.earn_permit
+      facility_1.administer_written_test(registrant_2)
+      facility_1.administer_road_test(registrant_2)
+      facility_1.administer_road_test(registrant_2)
+      expect(registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
+    end
+  end
 
-#   describe '#renew_drivers_license' do
-#     it 'renews the license only when conditions are met' do
-#       expect(@facility_1.renew_drivers_license(@registrant_1)).to be false
-#       @facility_1.add_service('Written Test')
-#       @facility_1.add_service('Road Test')
-#       @facility_1.add_service('Renew License')
+  describe '#renew_drivers_license' do
+    it 'renews the license only when conditions are met' do
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 16 )
+      
+      expect(facility_1.renew_drivers_license(registrant_1)).to be false
+      facility_1.add_service('Written Test')
+      facility_1.add_service('Road Test')
+      facility_1.add_service('Renew License')
 
-#       @facility_1.administer_written_test(@registrant_1)
-#       @facility_1.administer_road_test(@registrant_1)
-#       @facility_1.renew_drivers_license(@registrant_1)
-#       expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
+      facility_1.administer_written_test(registrant_1)
+      facility_1.administer_road_test(registrant_1)
+      facility_1.renew_drivers_license(registrant_1)
+      expect(registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
 
-#       @registrant_2.earn_permit
-#       @facility_1.administer_written_test(@registrant_2)
-#       @facility_1.administer_road_test(@registrant_2)
-#       @facility_1.renew_drivers_license(@registrant_2)
-#       expect(@registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
-#     end
+      registrant_2.earn_permit
+      facility_1.administer_written_test(registrant_2)
+      facility_1.administer_road_test(registrant_2)
+      facility_1.renew_drivers_license(registrant_2)
+      expect(registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
+    end
 
-#     it 'does not renew the license when conditions are not met' do
-#       @facility_1.add_service('Written Test')
-#       @facility_1.add_service('Road Test')
-#       @facility_1.add_service('Renew License')
+    it 'does not renew the license when conditions are not met' do
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      registrant_3 = Registrant.new('Tucker', 15 )
 
-#       @registrant_3.earn_permit
-#       @facility_1.administer_written_test(@registrant_3)
-#       @facility_1.administer_road_test(@registrant_3)
-#       @facility_1.renew_drivers_license(@registrant_3)
-#       expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
-#     end
-#   end
+      facility_1.add_service('Written Test')
+      facility_1.add_service('Road Test')
+      facility_1.add_service('Renew License')
+
+      registrant_3.earn_permit
+      facility_1.administer_written_test(registrant_3)
+      facility_1.administer_road_test(registrant_3)
+      facility_1.renew_drivers_license(registrant_3)
+      expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+  end
 end
