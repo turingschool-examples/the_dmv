33bf3dce152e6c768737c5ca35a10559c3b27e16
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-18 14:28:27 -0600

    Error, restart fresh

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -2,17 +2,4 @@ require "./lib/vehicle"
 
 class VehicleFactory
 
-  def create_vehicle(data)
-    vin = data[:vin]
-    year = data[:year]
-    make = data[:make]
-    model = data[:model]
-    engine = :ev
-  end
 end
-
-  def create_vehicles(vehicle_data)
-    vehicle_data.map do |data|
-      create_vehicle(vehicle_data)
-    end
-  end
