a0a693edb0d83c1fa8298a0e9907ca2e74496baa
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-18 16:51:22 -0600

    Make it as far as possible into Interation-3, stopped at creation of CO office locations

--- lib/dmv_data_service.rb
+++ lib/dmv_data_service.rb
@@ -23,45 +23,26 @@ class DmvDataService
     @mo_dmv_office_locations ||= load_data('https://data.mo.gov/resource/835g-7keg.json')
   end
 
-  # def create_facility_objects(data)
-  #   facilities = []
-
-  #   data.map do |facility|
-  #     facility_details = {
-  #       the_geom: data[:the_geom],
-  #       dmv_id: data[:dmv_id],
-  #       dmv_office: data[:dmv_office],
-  #       address_li: data[:address_li],
-  #       address__1: data[:address__1],
-  #       city: data[:city],
-  #       state: data[:state],
-  #       zip: data[:zip],
-  #       phone: data[:phone],
-  #       hours: data[:hours],
-  #       services_p: data[:services_p],
-  #       photo: data[:photo],
-  #       address_id: data[:address_id]
-        
-  #   }
-  #   end
-  # end
   def create_facility_objects(data)
     facilities = []
 
     data.map do |facility|
       facility_details = {
-
-        name: facility[:name],
-        address: facility[:address],
-        city: facility[:city],
-        state: facility[:state],
-        zip: facility[:zip],
-        phone: facility[:phone],
-        hours: facility[:hours],
-        services: facility[:services]
-
+        the_geom: data[:the_geom],
+        dmv_id: data[:dmv_id],
+        dmv_office: data[:dmv_office],
+        address_li: data[:address_li],
+        address__1: data[:address__1],
+        city: data[:city],
+        state: data[:state],
+        zip: data[:zip],
+        phone: data[:phone],
+        hours: data[:hours],
+        services_p: data[:services_p],
+        photo: data[:photo],
+        address_id: data[:address_id]
+        
     }
-    DmvDataService.new(facility_details)
     end
   end
 end
