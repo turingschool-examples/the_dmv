2695b6bc01caafc34f993487e899bbb1e4f029e3
Hayden McSorley <haymcsorley@gmail.com>
2023-07-18 17:19:47 -0500

    Add methods to facility class

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,43 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles, :collected_fees
 
-  def initialize(name, address, phone)
+  def initialize(name:, address:, phone:, collected_fees: 0)
     @name = name
     @address = address
     @phone = phone
     @services = []
+    @registered_vehicles = []
+    @collected_fees = collected_fees
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    return nil if services.include?("vehicle_registration") == false
+    vehicle.registration_date = Date.today
+    @registered_vehicles << vehicle
+    @collected_fees += add_fee(vehicle)
+  end
+
+  def add_fee(vehicle)
+    if vehicle.antique?
+      25
+    elsif vehicle.electric_vehicle?
+      200
+    else 
+      100
+    end
+  end
+
+  def plate_type(vehicle)
+    if vehicle.antique?
+      :antique
+    elsif vehicle.electric_vehicle?
+      :ev
+    else
+      :regular  
+    end
   end
 end
