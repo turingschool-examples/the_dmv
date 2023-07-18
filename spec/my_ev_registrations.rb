f3ee8216462f7e97df9a564d562d23f68fbfb392
Reed Hillmar <reedhillmar@gmail.com>
2023-07-18 13:58:03 -0600

    delete obsolete file

--- spec/my_ev_registrations.rb
+++
@@ -1,5 +0,0 @@
-[{"vin_1_10":"fakevin","model_year":"2020","make":"Chevrolet","model":"Bolt"},
-{"vin_1_10":"fakevin","model_year":"2021","make":"Chevrolet","model":"Bolt"},
-{"vin_1_10":"fakevin","model_year":"2021","make":"Chevrolet","model":"Bolt"},
-{"vin_1_10":"fakevin","model_year":"2020","make":"Tesla","model":"Model Y"},
-{"vin_1_10":"fakevin","model_year":"2021","make":"Tesla","model":"Model Y"}]
