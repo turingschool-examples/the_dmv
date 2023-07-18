60fb4989826c0542731f9d310d1203865474519f
Charles Ren <cren04@syr.edu>
2023-07-17 23:37:45 -0700

    feat: update format methods for NY/MO, and subsequent tests

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -12,16 +12,37 @@ RSpec.describe FacilityFactory do
     end
 
     describe '#create_facilities' do
-      it 'can create facilities' do
+      it 'can create co_facilities' do
         co_facilities = DmvDataService.new.co_dmv_office_locations
         @facility.create_facilities(co_facilities)
 
-        #require 'pry';binding.pry
         expect(@facility.facility_list).to all be_a Facility
         expect(@facility.facility_list).to be_an Array
-        expect(@facility.facility_list.first.name).to eq(["DMV Tremont Branch"])
+        expect(@facility.facility_list.first.name).to eq("DMV Tremont Branch")
         expect(@facility.facility_list.first.address).to eq("2855 Tremont Place, Suite 118, Denver, CO, 80205")
-        expect(@facility.facility_list.first.phone).to eq(["(720) 865-4600"])
+        expect(@facility.facility_list.first.phone).to eq("(720) 865-4600")
+      end
+
+      it 'can create new_york_facilities' do
+        ny_facilities = DmvDataService.new.ny_dmv_office_locations
+        @facility.create_facilities(ny_facilities)
+
+        expect(@facility.facility_list).to all be_a Facility
+        expect(@facility.facility_list).to be_an Array
+        expect(@facility.facility_list.first.name).to eq("DISTRICT OFFICE")
+        expect(@facility.facility_list.first.address).to eq("168-46 91ST AVE., 2ND FLR")
+        expect(@facility.facility_list.first.phone).to eq("7189666155")
+      end
+
+      it 'can create missouri_facilities' do
+        mo_facilities = DmvDataService.new.mo_dmv_office_locations
+        @facility.create_facilities(mo_facilities)
+
+        expect(@facility.facility_list).to all be_a Facility
+        expect(@facility.facility_list).to be_an Array
+        expect(@facility.facility_list.first.name).to eq("STODDARD COUNTY LICENSE OFFICE")
+        expect(@facility.facility_list.first.address).to eq("119 VINE ST")
+        expect(@facility.facility_list.first.phone).to eq("(573) 624-8808")
       end
     end
 end
