19c6d0a47352e5407d653fabe9ccc82da79d417b
Cory Powell <coryrpowell@gmail.com>
2023-07-18 19:07:03 -0400

    Add data from WA tests

--- spec/vehicle_factory.rb
+++ spec/vehicle_factory.rb
@@ -1,13 +1,27 @@
+require 'spec_helper'
 require './lib/vehicle'
 require './lib/vehicle_factory'
 require './lib/dmv_data_service'
 
-RSpec.describe VehicleFactory
+RSpec.describe VehicleFactory do
   before do
     @factory = VehicleFactory.new
   end
 
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@factory).to be_a(VehicleFactory)
+    end
+  end
 
-  #this needs to go somewhere in here
-  wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+  describe '#dmv data' do
+    it 'can recieve data from WA' do
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+      result = @factory.create_vehicles(wa_ev_registrations)
+
+      expect(result).to be_a(Array)
+      expect(result[0]).to be_a(Vehicle)
+      expect(result[0].year).to be_a("2018")
+    end
+  end
 end
