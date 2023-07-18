4d10d8b6f0ba17d1654c96a94cd3c2c87c27900b
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-18 14:36:37 -0600

    Use outside resources to assist with spec test

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,1 +1,26 @@
 require 'spec_helper'
+
+RSpec.describe VehicleFactory do
+  describe "#create vehicles" do
+    it "creates vehicles based off data" do
+      dmv_data_service = instance_double("DmvDataService")
+      allow(dmv_data_service).to receive(:wa_ev_registrations).and_return([
+      {:electric_vehicle_type=>"Plug-in Hybrid Electric Vehicle (PHEV)",
+      :vin_1_10=>"JTDKN3DP8D",
+      :dol_vehicle_id=>"229686908",
+      :model_year=>"2013",
+      :make=>"TOYOTA",
+      :model=>"Prius Plug-in",
+      },
+      {:electric_vehicle_type=>"Plug-in Hybrid Electric Vehicle (PHEV)",
+      :vin_1_10=>"1G1RD6E47D",
+      :dol_vehicle_id=>"289314742",
+      :model_year=>"2013",
+      :make=>"CHEVROLET",
+      :model=>"Volt",
+      }
+
+      ])
+    end
+  end
+end
