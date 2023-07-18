af9893cd028d4b3add1f6d95a1d525e1ab168c83
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-17 20:52:24 -0600

    test: update vehicle_factory test to handle non-specific data

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -14,8 +14,12 @@ RSpec.describe VehicleFactory do
       factory = VehicleFactory.new
       wa_ev_registrations = DmvDataService.new.wa_ev_registrations
 
+      # this works in this specific case, but is not consistent as APIs can change
       expect(factory.create_vehicles(wa_ev_registrations).first.vin).to eq("1G1RC6S55J")
       expect(factory.create_vehicles(wa_ev_registrations).last.vin).to eq("JN1AZ0CP7B")
+
+      expect(factory.create_vehicles(wa_ev_registrations)).to_not be_empty
+      expect(factory.create_vehicles(wa_ev_registrations).first).to be_a Vehicle
     end
   end
 end
