1bb188b479e3e7697b78c990eb81ea6a38d7e803
Nicholas McEnroe <nsmcenro@gmail.com>
2023-07-18 17:54:55 -0400

    Fix: Fix scope issues in registrant_spec

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -5,14 +5,6 @@ require './lib/vehicle'
 RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
-    @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
-    @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
-    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
-    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
-    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
-    @registrant_1 = Registrant.new('Bruce', 18, true )
-    @registrant_2 = Registrant.new('Penny', 16 )
-    @registrant_3 = Registrant.new('Tucker', 15 )
   end
 
   describe '#initialize' do
@@ -143,9 +135,9 @@ RSpec.describe Facility do
       facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
       registrant_3 = Registrant.new('Tucker', 15 )
       facility_1.add_service('Road Test')
-      expect(@facility_1.administer_road_test(registrant_3)).to eq(false)
+      expect(facility_1.administer_road_test(registrant_3)).to eq(false)
       registrant_3.earn_permit
-      facility_1.administer_road_test(@registrant_3)
+      facility_1.administer_road_test(registrant_3)
       expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
 
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -3,54 +3,63 @@ require './lib/registrant'
 require './lib/facility'
 
 RSpec.describe Registrant do
-    before(:each) do
-        @registrant_1 = Registrant.new('Bruce', 18, true )
-        @registrant_2 = Registrant.new('Penny', 15 )
-        @registrant_3 = Registrant.new('Tucker', 15 )
-        @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
-        @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
-    end
-    
     describe '#initialize' do
         it 'can initialize' do
-            expect(@registrant_1).to be_a(Registrant)
-            expect(@registrant_2).to be_a(Registrant)
+            registrant_1 = Registrant.new('Bruce', 18, true )
+            registrant_2 = Registrant.new('Penny', 15 )
+
+            expect(registrant_1).to be_a(Registrant)
+            expect(registrant_2).to be_a(Registrant)
         end
     end
 
     describe '#name' do
         it 'can return a name' do
-            expect(@registrant_1.name).to eq("Bruce")
-            expect(@registrant_2.name).to eq("Penny")
+            registrant_1 = Registrant.new('Bruce', 18, true )
+            registrant_2 = Registrant.new('Penny', 15 )
+        
+            expect(registrant_1.name).to eq("Bruce")
+            expect(registrant_2.name).to eq("Penny")
         end
     end
 
     describe '#age' do
         it 'can return an age' do
-            expect(@registrant_1.age).to eq(18)
-            expect(@registrant_2.age).to eq(15)
+            registrant_1 = Registrant.new('Bruce', 18, true )
+            registrant_2 = Registrant.new('Penny', 15 )
+        
+            expect(registrant_1.age).to eq(18)
+            expect(registrant_2.age).to eq(15)
         end
     end
 
     describe '#permit?' do
         it 'can return a boolean if registrant has a permit' do
-            expect(@registrant_1.permit?).to eq(true)
-            expect(@registrant_2.permit?).to eq(false)
+            registrant_1 = Registrant.new('Bruce', 18, true )
+            registrant_2 = Registrant.new('Penny', 15 )
+       
+            expect(registrant_1.permit?).to eq(true)
+            expect(registrant_2.permit?).to eq(false)
         end
     end
 
     describe '#license_data' do 
         it 'can return the correct info in a hash' do
-            expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
-            expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+            registrant_1 = Registrant.new('Bruce', 18, true )
+            registrant_2 = Registrant.new('Penny', 15 )
+       
+            expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+            expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
         end
     end
 
     describe '#earn_permit' do
         it 'can allow a registrant to earn a permit' do
-            expect(@registrant_2.permit?).to eq(false)
-            @registrant_2.earn_permit
-            expect(@registrant_2.permit?).to eq(true)
+            registrant_2 = Registrant.new('Penny', 15 )
+    
+            expect(registrant_2.permit?).to eq(false)
+            registrant_2.earn_permit
+            expect(registrant_2.permit?).to eq(true)
         end
     end
 end
