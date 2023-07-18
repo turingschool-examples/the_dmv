aa466248044fdcf16ff313514fb8e429c24ac19e
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-18 13:13:10 -0500

    add a dmv_facilities_data method to return data about dmv's

--- lib/dmv_facility.rb
+++ lib/dmv_facility.rb
@@ -3,8 +3,12 @@ class DmvFacility
   end
 end
 
-def create_dmv_facility(facilities)
-  dmvs = []
-  facilities.each do |facility|
+def create_dmv_facility
   end
+end
+
+dmv_facilities_data = DmvDataService.new.co_dmv_office_locations
+
+dmv_facilities = dmv_facilities_data.map do |data|
+  DmvFacility.new(data)
 end
