fbcdba7f1856940c79744085015297795dcd27c7
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-07-18 17:54:14 -0600

    feat: Add an elsif statment to be able to add MO locations

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -7,12 +7,33 @@ class FacilityFactory
     facilities =[]
 
     api_database.each do |location|
-      new_facility = Facility.new({
-        name: location[:dmv_office],
-        address: "#{location[:address_li]}, #{location[:address__1]}, #{location[:city]}, #{location[:state]}, #{location[:zip]} ",
-        phone: location[:phone]
+      if location[:state] == "CO"
+        new_facility = Facility.new({
+          name: location[:dmv_office],
+          address: "#{location[:address_li]}, #{location[:address__1]}, #{location[:city]}, #{location[:state]}, #{location[:zip]} ",
+          phone: location[:phone]
+        })
+      facilities << new_facility
+
+      elsif location[:state] == "NY"
+        new_facility = Facility.new({
+        name: location[:office_name],
+        address: "#{location[:street_address_line_1]}, #{location[:city]}, #{location[:state]}, #{location[:zip_code]} ",
+        phone: location[:public_phone_number] ||= '0000000000'
       })
       facilities << new_facility
+
+      elsif location[:state] == "MO"
+        new_facility = Facility.new({
+        name: location[:name],
+        address: "#{location[:address1]}, #{location[:city]}, #{location[:state]}, #{location[:zipcode]} ",
+        phone: location[:public_phone_number] ||= '0000000000'
+      })
+    facilities << new_facility
+
+
+    end
+
     end
     facilities
   end
