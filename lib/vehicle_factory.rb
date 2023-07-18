5ed7487307e69dfa13540bda673a4dc1c0f471f7
Charles Ren <cren04@syr.edu>
2023-07-17 22:05:25 -0700

    Finish Test Iteration 2

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,13 +1,20 @@
-require './lib/vehicle'
+class VehicleFactory
+    attr_accessor :vehicle_list
 
-class VehicleFactory do
-    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
-
-    def initialize()
-
+    def initialize
+        @vehicle_list = []
     end
 
-    def create_vehicles(state_registration)
-
+    def create_vehicles(registrations)
+      registrations.each do |vehicle|
+        details = {
+            vin: vehicle[:vin_1_10],
+            year: vehicle[:model_year],
+            make: vehicle[:make],
+            model: vehicle[:model],
+            engine: :ev
+        }
+        @vehicle_list << Vehicle.new(details)
+      end
     end
 end
