2c5bfc12ea9ef83ec301cea82d42edfb8529f45f
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-17 21:44:25 -0500

    clean up the facility spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
-require 'pry'
-require './lib/dmv'
-require './lib/facility'
-require './lib/vehicle'
-require './lib/dmv_data_service'
+require "pry"
+require "./lib/dmv"
+require "./lib/facility"
+require "./lib/vehicle"
+require "./lib/dmv_data_service"
+requrie "./lib/registrant"
