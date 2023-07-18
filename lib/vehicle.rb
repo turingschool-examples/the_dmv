b6bfb52250093edaebe0e969e280c7fbcbdd620e
Robert deLaguna <rjdelaguna@yahoo.com>
2023-07-18 13:15:29 -0700

    be the final commit

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -13,7 +13,7 @@ class Vehicle
     @vin = vehicle_details[:vin_1_10] || vehicle_details[:vin]
     @year = vehicle_details[:model_year] || vehicle_details[:year]
     @make = vehicle_details[:make]
-    @model = vehicle_details[:model]
+    @model = vehicle_details[:model] || vehicle_details[:body_type]
     @engine = vehicle_details[:engine] || :ev
     @registration_date = nil
     @plate_type = nil
