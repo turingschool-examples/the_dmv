b5e55cda38935d4bdad49bcfb48df68e89142941
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-17 22:02:48 -0500

    create the administer_written_test method

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -72,78 +72,78 @@ RSpec.describe Facility do
   end
 
   it "updates written test" do
-    expect(registrant_1.license_data).to eq({written: false, license: false, renewed: false})
-    expect(registrant_1.permit?).to eq(true)
+    expect(@registrant_1.license_data).to eq({written: false, license: false, renewed: false})
+    expect(@registrant_1.permit?).to eq(true)
 
-    expect(facility_1.administer_written_test(registrant_1)).to eq(false)
+    expect(@facility_1.administer_written_test(@registrant_1)).to eq(false)
 
-    expect(registrant_1.license_data).to eq({written: false, license: false, renewed: false})
+    expect(@registrant_1.license_data).to eq({written: false, license: false, renewed: false})
 
-    expect(facility_1.add_service("Written Test")).to eq(["Written Test"])
-    expect(facility_1.administer_written_test(registrant_1)).to eq(true)
+    expect(@facility_1.add_service("Written Test")).to eq(["Written Test"])
+    expect(@facility_1.administer_written_test(@registrant_1)).to eq(true)
 
-    expect(registrant_1.license_data).to eq({written: true, license: false, renewed: false})
+    expect(@registrant_1.license_data).to eq({written: true, license: false, renewed: false})
 
-    expect(registrant_2.age).to eq(16)
-    expect(registrant_2.permit?).to eq(false)
+    expect(@registrant_2.age).to eq(16)
+    expect(@registrant_2.permit?).to eq(false)
 
-    expect(facility_1.administer_written_test(registrant_2)).to eq(false)
+    expect(@facility_1.administer_written_test(@registrant_2)).to eq(false)
 
-    registrant_2.earn_permit
+    @registrant_2.earn_permit
 
-    expect(facility_1.administer_written_test(registrant_2)).to eq(true)
+    expect(@facility_1.administer_written_test(@registrant_2)).to eq(true)
 
-    expect(registrant_2.license_data).to eq({written: true, license: false, renewed: false})
+    expect(@registrant_2.license_data).to eq({written: true, license: false, renewed: false})
 
-    expect(registrant_3.age).to eq(15)
-    expect(registrant_3.permit?).to eq(false)
+    expect(@registrant_3.age).to eq(15)
+    expect(@registrant_3.permit?).to eq(false)
 
-    expect(facility_1.administer_written_test(registrant_3)).to eq(false)
+    expect(@facility_1.administer_written_test(@registrant_3)).to eq(false)
 
-    registrant_3.earn_permit
+    @registrant_3.earn_permit
 
-    expect(facility_1.administer_written_test(registrant_3)).to eq(false)
+    expect(@facility_1.administer_written_test(@registrant_3)).to eq(false)
 
-    expect(registrant_3.license_data).to eq({written: false, license: false, renewed: false})
+    expect(@registrant_3.license_data).to eq({written: false, license: false, renewed: false})
   end
 
-  xit "updates road test" do
-    expect(facility_1.administer_road_test(registrant_3)).to eq(false)
+  it "updates road test" do
+    expect(@facility_1.administer_road_test(@registrant_3)).to eq(false)
 
-    registrant_3.earn_permit
+    @registrant_3.earn_permit
 
-    expect(facility_1.administer_road_test(registrant_3)).to eq(false)
+    expect(@facility_1.administer_road_test(@registrant_3)).to eq(false)
 
-    expect(registrant_3.license_data).to eq({written: false, license: false, renewed: false})
+    expect(@registrant_3.license_data).to eq({written: false, license: false, renewed: false})
 
-    expect(facility_1.administer_road_test(registrant_1)).to eq(false)
+    expect(@facility_1.administer_road_test(@registrant_1)).to eq(false)
 
-    expect(facility_1.add_service("Road Test")).to eq(["Written Test", "Road Test"])
+    expect(@facility_1.add_service("Road Test")).to eq(["Written Test", "Road Test"])
 
-    expect(facility_1.administer_road_test(registrant_1)).to eq(true)
+    expect(@facility_1.administer_road_test(@registrant_1)).to eq(true)
 
-    expect(registrant_1.license_data).to eq({written: true, license: true, renewed: false})
+    expect(@registrant_1.license_data).to eq({written: true, license: true, renewed: false})
 
-    expect(facility_1.administer_road_test(registrant_2)).to eq(true)
+    expect(@facility_1.administer_road_test(@registrant_2)).to eq(true)
 
-    expect(registrant_2.license_data).to eq({written: true, license: true, renewed: false})
+    expect(@registrant_2.license_data).to eq({written: true, license: true, renewed: false})
   end
 
   xit "updates renew test" do
-    expect(facility_1.renew_drivers_license(registrant_1)).to eq(false)
+    expect(@facility_1.renew_drivers_license(@registrant_1)).to eq(false)
 
-    expect(facility_1.add_service("Renew License")).to eq(["Written Test", "Road Test", "Renew License"])
+    expect(@facility_1.add_service("Renew License")).to eq(["Written Test", "Road Test", "Renew License"])
 
-    expect(facility_1.renew_drivers_license(registrant_1)).to eq(true)
+    expect(@facility_1.renew_drivers_license(@registrant_1)).to eq(true)
 
-    expect(registrant_1.license_data).to eq({written: true, license: true, renewed: true})
+    expect(@registrant_1.license_data).to eq({written: true, license: true, renewed: true})
 
-    expect(facility_1.renew_drivers_license(registrant_3)).to eq(false)
+    expect(@facility_1.renew_drivers_license(@registrant_3)).to eq(false)
 
-    expect(registrant_3.license_data).to eq({written: false, license: false, renewed: false})
+    expect(@registrant_3.license_data).to eq({written: false, license: false, renewed: false})
 
-    expect(facility_1.renew_drivers_license(registrant_2)).to eq(true)
+    expect(@facility_1.renew_drivers_license(@registrant_2)).to eq(true)
 
-    expect(registrant_2.license_data).to eq({written: true, license: true, renewed: true})
+    expect(@registrant_2.license_data).to eq({written: true, license: true, renewed: true})
   end
 end
