c2f9889fd5666902ff0baf42fbbdb8ff95e33e3c
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-18 16:10:13 -0500

    add testing for co_dmv_office_locations

--- lib/dmv_factory.rb
+++ lib/dmv_factory.rb
@@ -2,21 +2,13 @@ class DmvFactory
   def initialize
   end
 
-  def self.create_facilities(dmv_facilities_data)
+  def create_facilities(dmv_facilities_data)
     dmv_facilities = []
     dmv_facilities_data.map do |data|
       new_facility = Facility.new({
-        dmv_id: data[:dmv_id],
-        dmv_office: data[:dmv_office],
-        address: "#{data[:address_li]}, #{data[:address__1]}",
-        city: data[:city],
-        state: data[:state],
-        zip: data[:zip],
-        phone: data[:phone],
-        hours: data[:hours],
-        services: data[:services_p],
-        parking: data[:parking_no],
-        photo: data[:photo]
+        name: data[:dmv_office],
+        address: "#{data[:address_li]}, #{data[:address__1]}, #{data[:city]}, #{data[:state]}, #{data[:zip]}",
+        phone: data[:phone]
       })
       dmv_facilities << new_facility
     end
--- spec/dmv_factory_spec.rb
+++ spec/dmv_factory_spec.rb
@@ -3,13 +3,24 @@ require "spec_helper"
 RSpec.describe "#dmv_factory" do
   it "creates a dmv factory" do
     factory = DmvFactory.new
-    expect(factory).to be_an_instance_of(Dmvfactory)
+    expect(factory).to be_an_instance_of(DmvFactory)
   end
 
   it "#creates a new facility" do
     factory = DmvFactory.new
+
     co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
 
     factory.create_facilities(co_dmv_office_locations)
+
+    expect(factory.create_facilities(co_dmv_office_locations)[0].name).to eq("DMV Tremont Branch")
+    expect(factory.create_facilities(co_dmv_office_locations)[0].address).to eq("2855 Tremont Place, Suite 118, Denver, CO, 80205")
+    expect(factory.create_facilities(co_dmv_office_locations)[0].phone).to eq("(720) 865-4600")
+  end
+
+  it "creates a new_york_facility" do
+  end
+
+  it "creates a missouri_facility" do
   end
 end
