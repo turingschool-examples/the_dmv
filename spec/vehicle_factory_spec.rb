c1049d01bedb492aea83a9ccb18c0a0bfaefab1f
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-18 14:40:28 -0600

    Continue to use outside resources to help with spec test

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -21,6 +21,17 @@ RSpec.describe VehicleFactory do
       }
 
       ])
+
+      factory = VehicleFactory.new
+
+      vehicles = factory.create_vehicles(dmv_data_service.wa_ev_registrations
+      
+      expect(vehicles[0]).to be_an_instance_of(Vehicle)
+      expect(vehicles[0].make).to eq("TOYOTA")
+      expect(vehicles[0].model).to eq("Prius Plug-in")
+      expect(vehicles[0].vin).to eq(JTDKN3DP8D)
+      expect(vehicles[0].year).to eq(2013)
+      expect(vehicles[0].engine).to eq(:ev)
     end
   end
 end
