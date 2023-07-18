cc869ba39fe1a916be0568226ec9d12b71e7a6d8
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-07-17 20:16:30 -0600

    test: Add the rest of the vehicle registrations tests in the facility rspec file

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -46,6 +46,7 @@ RSpec.describe Facility do
       facility_1.register_vehicle(cruz)
 
       expect(facility_1.registered_vehicles).to be_a(Array)
+    end
   end
   
   describe "#register_vehicle" do
@@ -66,11 +67,30 @@ RSpec.describe Facility do
 
       facility_1.register_vehicle(cruz)
 
-      expect(facility_1.registered_vehicles).to be_a(Array)
       expect(cruz.registration_date).to eq(Date.today)
       expect(cruz.plate_type).to eq(:regular)
       expect(facility_1.registered_vehicles).to be_a(Array)
-require 'pry';binding.pry
+      expect(facility_1.collected_fees).to eq(100)
+      
+      facility_1.register_vehicle(camaro)
+
+      expect(camaro.registration_date).to eq(Date.today)
+      expect(camaro.plate_type).to eq(:antique)
+
+      facility_1.register_vehicle(bolt)
+
+      expect(bolt.registration_date).to eq(Date.today)
+      expect(bolt.plate_type).to eq(:ev)
+      expect(facility_1.registered_vehicles).to be_a(Array)
+      expect(facility_1.collected_fees).to eq(325)
+      
+      expect(facility_2.registered_vehicles).to eq([])
+      expect(facility_2.services).to eq([])
+      
+      facility_2.register_vehicle(bolt)
+
+      expect(facility_2.registered_vehicles).to eq([])
+      expect(facility_2.collected_fees).to eq(0)
     end
   end
 end
