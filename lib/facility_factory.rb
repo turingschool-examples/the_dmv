b7d2436b523f9588b6238a7e79a97c0236cb4527
Lane Bretschneider <lanebret85@gmail.com>
2023-07-17 19:08:58 -0700

    feat: add valid_co_address and valid_ny_address methods, and refactor create_facilities method

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -2,20 +2,6 @@ class FacilityFactory
   def initialize
   end
 
-  # def create_facilities(locations)
-  #   facilities = []
-  #   locations.each do |location|
-  #     new_facility = Facility.new({
-  #       name: location[:dmv_office],
-  #       address: "#{location[:address_li]} #{location[:address__1]} #{location[:city]}, #{location[:state]} #{location[:zip]}",
-  #       phone: location[:phone]
-  #     })
-  #     facilities << new_facility
-  #   end
-  #   facilities
-  # end
-# end
-
   def create_facilities(locations)
     facilities = []
     
