35e62dca49cbdc1c5d318ed10da7ce21dae53b17
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-18 18:07:48 -0500

    finish iteration 3

--- lib/dmv_factory.rb
+++ lib/dmv_factory.rb
@@ -5,19 +5,19 @@ class DmvFactory
   def create_facilities(dmv_facilities_data)
     dmv_facilities = []
     dmv_facilities_data.map do |data|
-      if data[:state] = "CO"
+      if data[:state] == "CO"
         new_facility = Facility.new({
           name: data[:dmv_office],
           address: "#{data[:address_li]}, #{data[:address__1]}, #{data[:city]}, #{data[:state]}, #{data[:zip]}",
           phone: data[:phone]
         })
-      elsif data[:state] = "NY"
+      elsif data[:state] == "NY"
         new_facility = Facility.new({
           name: data[:office_name],
           address: "#{data[:street_address_line_1]}, #{data[:city]}, #{data[:state]}, #{data[:zip_code]}",
           phone: data[:public_phone_number]
         })
-      elsif data[:state] = "MO"
+      elsif data[:state] == "MO"
         new_facility = Facility.new({
           name: data[:name],
           address: "#{data[:address1]}, #{data[:city]}, #{data[:state]}, #{data[:zipcode]}",
