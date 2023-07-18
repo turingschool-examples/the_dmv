3d4d8ca2ba41d8e895741b85568db4fda6b1b043
Lane Bretschneider <lanebret85@gmail.com>
2023-07-18 16:18:12 -0700

    feat: add code to phone_filter method to account for nil case, and apply filters to create_facilities method

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe FacilityFactory do
       factory.create_facilities(co_dmv_office_locations)
 
       expect(factory.create_facilities(co_dmv_office_locations)[0]).to be_a(Facility)
-      expect(factory.create_facilities(co_dmv_office_locations)[0].name).to eq("DMV Tremont Branch")
+      expect(factory.create_facilities(co_dmv_office_locations)[0].name).to eq("Dmv Tremont Branch")
       expect(factory.create_facilities(co_dmv_office_locations)[0].address).to eq("2855 Tremont Place Suite 118 Denver, CO 80205")
       expect(factory.create_facilities(co_dmv_office_locations)[0].phone).to eq("(720) 865-4600")
     end
@@ -29,9 +29,9 @@ RSpec.describe FacilityFactory do
       factory.create_facilities(ny_dmv_office_locations)
 
       expect(factory.create_facilities(ny_dmv_office_locations)[0]).to be_a(Facility)
-      expect(factory.create_facilities(ny_dmv_office_locations)[0].name).to eq("JAMAICA")
-      expect(factory.create_facilities(ny_dmv_office_locations)[0].address).to eq("168-46 91ST AVE., 2ND FLR JAMAICA, NY 11432")
-      expect(factory.create_facilities(ny_dmv_office_locations)[0].phone).to eq("7189666155")
+      expect(factory.create_facilities(ny_dmv_office_locations)[0].name).to eq("Jamaica")
+      expect(factory.create_facilities(ny_dmv_office_locations)[0].address).to eq("168-46 91st Ave., 2nd Flr Jamaica, NY 11432")
+      expect(factory.create_facilities(ny_dmv_office_locations)[0].phone).to eq("(718) 966-6155")
     end
   end
 
@@ -42,8 +42,8 @@ RSpec.describe FacilityFactory do
       factory.create_facilities(mo_dmv_office_locations)
 
       expect(factory.create_facilities(mo_dmv_office_locations)[0]).to be_a(Facility)
-      expect(factory.create_facilities(mo_dmv_office_locations)[0].name).to eq("DEXTER")
-      expect(factory.create_facilities(mo_dmv_office_locations)[0].address).to eq("119 VINE ST DEXTER, MO 63841")
+      expect(factory.create_facilities(mo_dmv_office_locations)[0].name).to eq("Dexter")
+      expect(factory.create_facilities(mo_dmv_office_locations)[0].address).to eq("119 Vine St Dexter, MO 63841")
       expect(factory.create_facilities(mo_dmv_office_locations)[0].phone).to eq("(573) 624-8808")
     end
   end
