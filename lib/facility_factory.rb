6b5daecded03b46148807617e6807d25ab4a39b4
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-18 12:44:12 -0600

    feat and test: add and test fucntionality to facility factory class for NY data

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,19 +1,29 @@
 class FacilityFactory
   attr_reader :facilities
-  
+
   def initialize
     @facilities = []
   end
 
   def create_facilities(data)
     @facilities = data.map do |facility_data|
-      Facility.new(
-        {
-          name: facility_data[:dmv_office],
-          address: "#{facility_data[:address_li]} #{facility_data[:address__1]} #{facility_data[:city]} #{facility_data[:state]} #{facility_data[:zip]}",
-          phone: facility_data[:phone]
-        }
-      )
+      if facility_data[:state] == "CO"
+        Facility.new(
+          {
+            name: facility_data[:dmv_office],
+            address: "#{facility_data[:address_li]} #{facility_data[:address__1]} #{facility_data[:city]} #{facility_data[:state]} #{facility_data[:zip]}",
+            phone: facility_data[:phone]
+          }
+        )
+      elsif facility_data[:state] == "NY"
+        Facility.new(
+          {
+            name: "#{facility_data[:office_name]} #{facility_data[:office_type]}",
+            address: "#{facility_data[:street_address_line_1]} #{facility_data[:city]} #{facility_data[:state]} #{facility_data[:zip_code]}",
+            phone: facility_data[:public_phone_number]
+          }
+        )
+      end
     end
   end
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -19,5 +19,17 @@ RSpec.describe FacilityFactory do
       expect(factory.create_facilities(colorado_facilities)).to be_a(Array)
       expect(factory.facilities.first.name).to eq("DMV Tremont Branch")
     end
+
+    it "creates facilities with NY data" do
+      factory = FacilityFactory.new
+
+      new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+      require 'pry';binding.pry
+
+      factory.create_facilities(new_york_facilities)
+      expect(factory.facilities).to be_a(Array)
+      expect(factory.facilities.first.phone).to eq("7189666155")
+      expect(factory.facilities[1].address).to eq("200 OLD COUNTRY ROAD RIVERHEAD NY 11901")
+    end
   end
 end
