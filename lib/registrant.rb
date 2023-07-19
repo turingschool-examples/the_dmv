0082919461ee879f9ee0b7c9382843c9eacf771b
Victor Sanchez <vsanantone@gmail.com>
2023-07-18 19:08:20 -0500

    fix ruby standards of indentation/formating.

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,22 +1,22 @@
 class Registrant
-    attr_reader :name,
-                :age,
-                :permit,
-                :license_data
+	attr_reader :name,
+              :age,
+              :permit,
+              :license_data
 
-    def initialize(name, age, permit = false)
-        @name = name
-        @age = age
-        @permit = permit
-        @license_data = {:written=>false, :license=>false, :renewed=>false}
+  def initialize(name, age, permit = false)
+    @name = name
+    @age = age
+    @permit = permit
+    @license_data = {:written=>false, :license=>false, :renewed=>false}
 
-    end
+  end
 
-    def permit?
-        @permit
-    end
+  def permit?
+      @permit
+  end
 
-    def earn_permit
-        @permit = true
-    end
+  def earn_permit
+      @permit = true
+  end
 end
--- lib/vehicle_factory_class.rb
+++ lib/vehicle_factory_class.rb
@@ -1,16 +1,16 @@
 class VehicleFactory
    
-    def create_vehicles(registration)
-        registration.map do |vehicle_hash|
-        car_hash = {
-            vin: vehicle_hash[:vin_1_10],
-            model: vehicle_hash[:model],
-            make: vehicle_hash[:make],
-            engine: :ev,
-            year: vehicle_hash[:model_year].to_i
+  def create_vehicles(registration)
+    registration.map do |vehicle_hash|
+      car_hash = {
+        vin: vehicle_hash[:vin_1_10],
+        model: vehicle_hash[:model],
+        make: vehicle_hash[:make],
+        engine: :ev,
+        year: vehicle_hash[:model_year].to_i
         }
-        Vehicle.new(car_hash)
-        end
-    end
+      Vehicle.new(car_hash)
+  	end
+	end
 end
 
