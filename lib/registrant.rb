1f564bb866b0f90c8a283295c7b3bf1218e8d8d5
Taylor Pubins <tpubz@icloud.com>
2023-08-31 11:44:01 -0700

    Add Registrant class and specs, necessary for managing registrants and their attributes

---
+++ lib/registrant.rb
@@ -0,0 +1,8 @@
+class Registrant
+  def initialize(name, age, permit = false)
+    @name = name
+    @age = age
+    @permit = permit
+    @license_data = {:written => false, :license => false, :renewed => false}
+  end
+end
