f7eaadb1d5c00ccaf6fab61d797b0e11cbe2fbbf
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-08-31 14:46:57 -0600

    Debug: Pass the first facility test

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,10 +1,10 @@
 class Facility
   attr_reader :name, :address, :phone, :services
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(facility_data)
+    @name = facility_data[:name]
+    @address = facility_data[:address]
+    @phone = facility_data[:phone]
     @services = []
   end
 
