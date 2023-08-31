8a0c5d47509692912c79a84afe17e9b737352be4
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-08-31 15:44:01 -0600

    Add all tests for iteration 1 registrant class

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -24,13 +24,34 @@ RSpec.describe Registrant do
   describe "#permit?" do
 
     it "has a permit if defined in #initialize" do
+    
       expect(@registrant_1.permit?).to be_truthy
     end
 
     it "does not have a permit by default" do
+
       expect(@registrant_2.permit?).to be_falsy
     end
   end
 
+  describe "#license_data" do
 
+    it "stores license data values as false by default" do
+
+      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+  end
+
+  describe "#earn_permit" do
+    it "changes permit to true when earn_permit is called" do
+
+      expect(@registrant_2.permit?).to eq(false)
+
+      @registrant_2.earn_permit
+
+      expect(@registrant_2.permit?).to eq(true)
+    end
+
+  end
 end
