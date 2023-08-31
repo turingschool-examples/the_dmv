710c41d49b56a2a02903a8ed29e0c0fcd75040a1
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-08-31 15:37:36 -0600

    Add tests for registrant initialize and permit methods

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+RSpec.describe Registrant do
+  before(:each) do
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_2 = Registrant.new('Penny', 15 )
+  end
+
+  describe "#initialize" do
+
+    it "has a name" do
+      expect(@registrant_1.name).to eq("Bruce")
+      expect(@registrant_2.name).to eq("Penny")
+
+    end
+
+    it "has an age" do
+      expect(@registrant_1.age).to eq(18)
+      expect(@registrant_2.age).to eq(15)
+    end
+
+  end
+
+  describe "#permit?" do
+
+    it "has a permit if defined in #initialize" do
+      expect(@registrant_1.permit?).to be_truthy
+    end
+
+    it "does not have a permit by default" do
+      expect(@registrant_2.permit?).to be_falsy
+    end
+  end
+
+
+end
