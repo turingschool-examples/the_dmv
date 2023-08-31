74b8ff9aa6824fcc1912a6d7731addd0d1609de3
Martin Chavez <martinchavez1203@gmail.com>
2023-08-31 15:50:44 -0600

    add second test and almost third

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe Registrant do
             expect(registrant.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
         end   
     end 
+
+    it 'registrant without a permit' do 
+        registrant = Registrant.new('Penny',15)
+        expect(registrant.name).to eq('Penny')
+        expect(registrant.age).to eq(15)
+        expect(registrant.permit?).to be(false)
+        expect(registrant.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end 
+
+    describe 'earn permit' do 
+        it 'chnages the permit status to false'do 
+         registrant = Registrant.new('Penny',15)
+         expect(registrant.permit?).to be(false)
+         
+
+
 end 
+
