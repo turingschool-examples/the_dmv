c9aaef2c519fa77282b0677d8ef4fd1c42fc383a
Scott DeVoss <scottdevoss@yahoo.com>
2023-08-31 16:16:54 -0600

    feat: made tweaks to tests and methods at the end of the first block in iteration 2

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -23,4 +23,28 @@ RSpec.describe Facility do
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
   end
+
+  describe '#register vehicle' do
+    it 'can register a vehicle' do
+      expect(@facility.registered_vehicles).to eq([])
+
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      @facility.register_vehicle(cruz)
+      expect(@facility.registered_vehicles).to_not be nil
+    end
+  end
+
+  describe '#collected fees' do
+    it 'can collect fees after registering' do
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      @facility.register_vehicle(cruz)
+      expect(@facility.collected_fees).to eq(100)
+
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+      @facility.register_vehicle(bolt)
+      @facility.register_vehicle(camaro)
+      expect(@facility.collected_fees).to eq(325)
+    end
+  end
 end
