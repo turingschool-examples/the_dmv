baeff9ebad90d91991a249a1f186eb1087bd0bcc
Kam Kennedy <kameronk013@gmail.com>
2023-08-31 16:36:19 -0600

    create registrant spec

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+RSpec.describe Registrant do
+  before(:each) do 
+    registrant_1 = Registrant.new('Bruce', 18, true )
+    registrant_2 = Registrant.new('Penny', 15 )
+  end
