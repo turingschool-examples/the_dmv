741d63ad2b3ee158a8102e17a7094c8f5d32800c
Kam Kennedy <kameronk013@gmail.com>
2023-08-31 16:42:02 -0600

    add one test to registrant spec

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 RSpec.describe Registrant do
-  before(:each) do 
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
+  before(:each) do #how does scope this work, where does the end go
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_2 = Registrant.new('Penny', 15 )
   end
+
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@registrant_1).to be_an_instance_of(Registrant)
+      expect(@registrant_2).to be_an_instance_of(Registrant) 
+    end
+  end
+end
