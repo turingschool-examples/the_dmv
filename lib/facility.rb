d844a3267c8312303523cd54f20e1c92cd046650
Kevin Zolman <kevin.zolman@gmail.com>
2023-08-31 15:31:29 -0600

    Add #register_vehicle functionality

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,38 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services, 
+              :registered_vehicles, 
+              :collected_fees
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(facility_details)
+    @name = facility_details[:name]
+    @address = facility_details[:address]
+    @phone = facility_details[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    vehicle.registration_date = Date.today
+    if vehicle.engine == :ev
+      vehicle.plate_type = :ev
+      @registered_vehicles << vehicle
+      @collected_fees += 200
+    elsif vehicle.registration_date.year - vehicle.year < 25
+      vehicle.plate_type = :regular
+      @registered_vehicles << vehicle
+      @collected_fees += 100
+    else
+      vehicle.plate_type = :antique
+      @registered_vehicles << vehicle
+      @collected_fees += 25
+    end
   end
 end
