b155f90a4a1f5161d8c29ebf6275b5d17702ad36
Justin Lee <justinslee201100@yahoo.com>
2023-08-31 17:22:12 -0600

    Add tests and code for registrant

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -16,15 +16,31 @@ RSpec.describe 'Registrant' do
   describe 'Name' do
     it 'checks registrants name' do
       expect(@registrant_1.name).to eq('Bruce')
+      expect(@registrant_2.name).to eq('Penny')
     end
   end
 
   describe 'Age' do
     it 'Checks registrants age' do
       expect(@registrant_1.age).to eq(18)
+      expect(@registrant_2.age).to eq(15)
     end
   end
-  registrant_1.age
-  registrant_1.permit?
-  registrant_1.license_data
+
+  describe 'Permit?' do
+    it 'checks if registrant has a permit' do
+      expect(@registrant_1.permit?).to be(true)
+      expect(@registrant_2.permit?).to be(false)
+
+      @registrant_2.earn_permit
+      expect(@registrant_2.permit?).to be(true)
+    end
+  end
+
+  describe 'License data' do
+    it 'checks license data' do
+      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+  end
 end
