980768073c83975cf980e2f7c48ba07d68b89567
Nathan Trautenberg <Natester123@mac.com>
2023-09-01 14:35:50 -0600

    begin tests for getting a driver's license for registrant 1

---
+++ spec/drivers_license_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.describe Facility do
+  before(:each) do
+    @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_2 = Registrant.new('Penny', 16 )
+    @registrant_3 = Registrant.new('Tucker', 15 )
+    @facility_1.add_service('Vehicle Registration')
+  end
+  
+  it 'can obtain license data' do
+    expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+   end
+
+  end
