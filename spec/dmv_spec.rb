8ae522cac1f23c0ac9020c0b00bb7eb717b6e9b2
Joseph Lee <jhjlee702@gmail.com>
2023-08-31 21:04:24 -0600

    Initial commit

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -15,29 +15,29 @@ RSpec.describe Dmv do
     end
   end
 
-  describe '#add facilities' do
-    it 'can add available facilities' do
-      expect(@dmv.facilities).to eq([])
-      @dmv.add_facility(@facility_1)
-      expect(@dmv.facilities).to eq([@facility_1])
-    end
-  end
+  # describe '#add facilities' do
+  #   it 'can add available facilities' do
+  #     expect(@dmv.facilities).to eq([])
+  #     @dmv.add_facility(@facility_1)
+  #     expect(@dmv.facilities).to eq([@facility_1])
+  #   end
+  # end
 
-  describe '#facilities_offering_service' do
-    it 'can return list of facilities offering a specified Service' do
-      @facility_1.add_service('New Drivers License')
-      @facility_1.add_service('Renew Drivers License')
-      @facility_2.add_service('New Drivers License')
-      @facility_2.add_service('Road Test')
-      @facility_2.add_service('Written Test')
-      @facility_3.add_service('New Drivers License')
-      @facility_3.add_service('Road Test')
+  # describe '#facilities_offering_service' do
+  #   it 'can return list of facilities offering a specified Service' do
+  #     @facility_1.add_service('New Drivers License')
+  #     @facility_1.add_service('Renew Drivers License')
+  #     @facility_2.add_service('New Drivers License')
+  #     @facility_2.add_service('Road Test')
+  #     @facility_2.add_service('Written Test')
+  #     @facility_3.add_service('New Drivers License')
+  #     @facility_3.add_service('Road Test')
 
-      @dmv.add_facility(@facility_1)
-      @dmv.add_facility(@facility_2)
-      @dmv.add_facility(@facility_3)
+  #     @dmv.add_facility(@facility_1)
+  #     @dmv.add_facility(@facility_2)
+  #     @dmv.add_facility(@facility_3)
 
-      expect(@dmv.facilities_offering_service('Road Test')).to eq([@facility_2, @facility_3])
-    end
-  end
+  #     expect(@dmv.facilities_offering_service('Road Test')).to eq([@facility_2, @facility_3])
+  #   end
+  # end
 end
