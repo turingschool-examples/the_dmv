20daa77d5125bfb6cf74471ee95b077c8bec3ffd
Kevin Zolman <kevin.zolman@gmail.com>
2023-09-01 17:08:23 -0600

    Modify #create_facilities, now allows for other datasets

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -5,14 +5,28 @@ class FacilityFactory
 
   def create_facilities(locations)
     facilities = locations.map do |location|
-      name = location[:dmv_office]
-      address = "#{location[:address_li]} #{location[:address__1]} #{location[:city]} #{location[:state]} #{location[:zip]}"
-      phone = location[:phone]
-      # service descriptions do not match method terms, normalize this
-      # just do services manually with in-house method
-      # services = location[:services_p].split
+      if location[:dmv_office] != nil
+        name = location[:dmv_office]
+        address = "#{location[:address_li]} #{location[:address__1]} #{location[:city]} 
+        #{location[:state]} #{location[:zip]}"
+        phone = location[:phone]
+      elsif location[:office_name] != nil
+        name = location[:office_name]
+        address = "#{location[:street_address_line_1]} #{location[:street_address_line_2]} 
+        #{location[:city]} #{location[:state]} #{location[:zip_code]}"
+        # normalize this phone number
+        phone = location[:public_phone_number]
+      else
+        name = location[:name]
+        address = "#{location[:address1]} #{location[:city]} #{location[:state]} #{:zipcode}"
+        phone = location[:phone]
+      end
 
       Facility.new(name: name, address: address, phone: phone)
     end
   end
 end
+
+# service descriptions do not match method terms, normalize this
+      # just do services manually with in-house method
+      # services = location[:services_p].split
