add7166b4a020f886694acbec21b3e1af071cbb3
Nathan Trautenberg <Natester123@mac.com>
2023-09-01 15:22:14 -0600

    administer written test for registrant 3

--- spec/drivers_license_spec.rb
+++ spec/drivers_license_spec.rb
@@ -36,4 +36,15 @@ RSpec.describe Facility do
     expect(@facility_1.administer_written_test(@registrant_2)).to eq(true)
     expect(@registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
   end
+
+  it 'can administer a written test for registrant 2' do
+    @facility_1.add_service('Written Test')
+    #require 'pry';binding.pry
+    expect(@registrant_3.age).to eq(15)
+    expect(@registrant_3.permit?).to eq(false)
+    expect(@facility_1.administer_written_test(@registrant_2)).to eq(false)
+    @registrant_3.earn_permit
+    expect(@facility_1.administer_written_test(@registrant_2)).to eq(false)
+    expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+  end
 end
