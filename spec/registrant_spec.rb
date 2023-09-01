ad64e99068a0d2df8a6092a45dbe8771fa4a516e
Sooyung Kim <kimx1279@gmail.com>
2023-08-31 19:26:37 -0600

    Update registrant spec with third test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -21,6 +21,14 @@ RSpect.describe Registrant do
       expect(registrant_1.permit?).to be false
       expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end 
+    
+    it 'earns permit and updates permit status' do
+      registrant_2 = Registrant.new('Penny', 15 )
+
+      registrant_2.earn_permit
+
+      expect(registrant_2.permit?).to be true
+    end
   end
 
 end
