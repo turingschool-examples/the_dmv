e5237ecf95b6d79a00f6109f0e3a7839ec81736c
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-09-01 07:56:12 -0600

    Write tests for administering written test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -83,6 +83,35 @@ RSpec.describe Facility do
       end
     end
 
+    describe '#license_data' do
+      before(:each) do
+        @registrant_1 = Registrant.new('Bruce', 18, true )
+        @registrant_2 = Registrant.new('Penny', 16 )
+        @registrant_3 = Registrant.new('Tucker', 15 )
+        @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+        @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+
+      end
+
+      it 'does not administer written test if service not provided' do
+
+        expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+        expect(@registrant_1.permit?).to be_truthy
+
+        @facility_1.administer_written_test(@registrant_1)
+
+        expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+
+      end
+
+      it 'administers written test if service provided' do
+
+        @facility_1.add_service('Written Test')
+        @facility_1.administer_written_test(@registrant_1)
+
+        expect(registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+
+      end
 
   end
 
