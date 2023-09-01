0a541cf14a7b6cbd3823347b34ab0b1b5b89c97e
Joseph Lee <jhjlee702@gmail.com>
2023-09-01 12:03:11 -0600

    check for registered_vehicles

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,21 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(facility_details)
+    @name = facility_details[:name]
+    @address = facility_details[:address]
+    @phone = facility_details[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
   end
 end
+
+#services:
+  # register a vehicle
+  # administer a written test
+  # administer a road test
+  # renew a driver's license
