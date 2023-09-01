632a3e845f3ba2da4c5f52668dc2ed94fbba9af5
Kevin Zolman <kevin.zolman@gmail.com>
2023-09-01 07:52:44 -0600

    Modify #create_vehicles test

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -3,11 +3,13 @@ require 'spec_helper'
 RSpec.describe VehicleFactory do
   before(:each) do
     @factory = VehicleFactory.new
+    @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
   end
 
   describe '#create_vehicles' do
     it 'will create Vehicle objects using a registration dataset' do
-      expect(@factory.create_vehicles(wa_ev_registrations)[0]).to be_an_instance_of(Vehicle)
+      expect(@factory.create_vehicles(@wa_ev_registrations)[0]).to be_an_instance_of(Vehicle)
+      expect(@factory.create_vehicles(@wa_ev_registrations)[0].engine).to eq(:ev) 
     end
   end
 end
