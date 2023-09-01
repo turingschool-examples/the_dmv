9a8c8fd3252903bfa42edda03d7096bc53a9e33d
Miranda Delaney <delaney.miranda1@gmail.com>
2023-09-01 13:48:51 -0400

    feat: Add tests for each registrant

---
+++ lib/registrant.rb
@@ -0,0 +1,17 @@
+class Registrant
+  attr_reader :name, :age, :permit
+  def initialize(name, age, permit = false)
+    @name = name
+    @age = age
+    @permit = permit
+
+  end
+
+  def permit?
+    @permit
+  end
+
+  def earn_permit
+    @permit = true
+  end
+end
---
+++ spec/registrant_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require './lib/registrant'
+
+RSpec.describe Registrant do
+  it 'initializes' do
+    registrant_1 = Registrant.new('Bruce', 18, true )
+    registrant_2 = Registrant.new('Penny', 15 )
+
+    expect(registrant_1).to be_a(Registrant)
+    expect(registrant_2).to be_a(Registrant)
+    expect(registrant_1.name).to eq('Bruce')
+    expect(registrant_1.age).to eq(18)
+    expect(registrant_1.permit?).to be true
+    expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    expect(registrant_2.name).to eq('Penny')
+    expect(registrant_2.age).to eq(15)
+    expect(registrant_2.permit?).to be false
+    expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    
+  end
+end
