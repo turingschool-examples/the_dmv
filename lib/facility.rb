61013a93149647aa624d6568933ba9208723319f
Billy Burch <billyburch40@gmail.com>
2023-08-31 22:24:45 -0600

    add: keys and values for facility_details

--- lib/facility.rb
+++ lib/facility.rb
@@ -2,17 +2,18 @@ class Facility
   attr_reader :name, :address, :phone, :services
 
   def initialize(facility_details)
-    @name = name
-    @address = address
-    @phone = phone
+    @name = facility_details[:name]
+    @address = facility_details[:address]
+    @phone = facility_details[:phone]
     @services = []
+    # @vin = vehicle_details[:vin]
+    # @year = vehicle_details[:year]
+    # @make = vehicle_details[:make]
+    # @model = vehicle_details[:model]
+    # @engine = vehicle_details[:engine]
   end
 
   def add_service(service)
     @services << service
   end
-
-  # Here we go trying things
-
-
 end
