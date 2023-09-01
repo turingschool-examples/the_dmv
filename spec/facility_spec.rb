1a5df604f4e117273c1240eb945c2527a804335d
Joseph Lee <jhjlee702@gmail.com>
2023-09-01 16:50:56 -0600

    clear clear rspec spec/facility_spec.rb [H[2J
    exit
    git add spec/facility_spec.rb!
    '
    ';

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -92,6 +92,15 @@ end
       #require the facility to have the service you're performing
     end
   end
+
+  describe '#check F1 registered vehicles again' do
+    it 'checks the registered vehicles after all three are added' do
+      @facility_1.register_vehicle(@cruz)
+      @facility_1.register_vehicle(@camaro)
+      @facility_1.register_vehicle(@bolt)
+      expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro, @bolt])
+    end
+  end
 end
 
 #use today and year methods???
