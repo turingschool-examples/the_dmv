aa07ce7afa5409b64466499b96156bb12e87a5f9
Eric Belongea <eric.belongea@gmail.com>
2023-08-31 18:11:08 -0600

    Preps facility for registering

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,13 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :collected_fees, :registered_vehicles
 
   def initialize(hash)
     @name = hash[:name]
     @address = hash[:address]
     @phone = hash[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
