186ab9ce62d64c347227a0df73bc42b5d4abe6e4
Billy Burch <billyburch40@gmail.com>
2023-08-31 22:36:09 -0600

    save progress for 8.31.23

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe Dmv do
   before(:each) do
     @dmv = Dmv.new
-    # @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
     @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
     @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
     @facility_3 = Facility.new({name: 'DMV Northwest Branch', address: '3698 W. 44th Avenue Denver CO 80211', phone: '(720) 865-4600'})
