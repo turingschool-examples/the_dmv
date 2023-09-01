6f9d2c945a6be0920cb481cf28868fd61d41421f
Justin Lee <justinslee201100@yahoo.com>
2023-08-31 21:22:46 -0600

    Refactor facility tests

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe Facility do
   before(:each) do
     @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
-    @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+    
 
   end
   describe '#initialize' do
@@ -26,26 +26,54 @@ RSpec.describe Facility do
   end
 
     describe '#Register Vehicle' do
-    it 'starts with no vehicles' do
-      expect(@facility_1.registered_vehicles).to eq([])
-      expect(@cruz.registration_date).to eq(nil)
-      expect(@cruz.plate_type).to eq(:regular)
-      expect(@cruz.registration_date).to eq(Date)
-      expect(@facility_1.registered_vehicles).to eq([@cruz])
+      it 'starts with no vehicles' do
+        expect(@facility_1.registered_vehicles).to eq([])
+      end
 
+      it 'Register a cruz' do  
+        expect(@cruz.registration_date).to eq(nil)
+        expect(@cruz.plate_type).to eq(:regular)
+        @facility_1.register_vehicle(@cruz)
+        expect(@cruz.registration_date).to eq(Date)
+        expect(@facility_1.registered_vehicles).to eq([@cruz])
+      end
 
-    end
+      it 'Register camaro' do
+        @facility_1.register_vehicle(@cruz)
+        @facility_1.register_vehicle(@camaro)
+        expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro])
+        expect(@camaro.registration_date).to eq(Date)
+        expect(@camaro.plate_type).to eq(:antique)
+        
+      end
 
-      it 'collects a fee when service is used' do
-        expect(@facility_1.collected_fees).to eq(0)
-        @facility_1.register_vehicle(cruz)
-        expect(@facility_1.collected_fees).to eq(100)
+      it 'Register bolt' do
+        @facility_1.register_vehicle(@cruz)
+        @facility_1.register_vehicle(@camaro)
+        @facility_1.register_vehicle(@bolt)
+        expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro, @bolt])
+        expect(@bolt.registration_date).to eq(Date)
+        expect(@bolt.plate_type).to eq(:ev)
+      end
+
+        it 'collects a fee' do
+          expect(@facility_1.collected_fees).to eq(0)
+          @facility_1.register_vehicle(@cruz)
+          expect(@facility_1.collected_fees).to eq(100)
+          @facility_1.register_vehicle(@camaro)
+          expect(@facility_1.collected_fees).to eq(125)
+          @facility_1.register_vehicle(@bolt)
+          expect(@facility_1.collected_fees).to eq(325)
+        end
+
+        it 'Check facility_2' do
+          facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+          expect(facility_2.registered_vehicles).to eq([])
+          expect(facility_2.services).to eq([])
+          expect(facility_2.register_vehicle(bolt)).to eq(nil)
+          expect(facility_2.registered_vehicles).to eq([])
+          expect(facility_2.collected_fees).to eq(0)
+        end
       end
 end
-facility_1.registered_vehicles
-facility_1.collected_fees
-facility_1.register_vehicle(cruz)
-cruz.registration_date
-cruz.plate_type
-facility_1.registered_vehicles
-facility_1.collected_fees
+
