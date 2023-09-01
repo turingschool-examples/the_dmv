b899f57fa2ea6b017c2cd41f36bfe6f552d1050e
Sooyung Kim <kimx1279@gmail.com>
2023-08-31 19:21:08 -0600

    Update registrant spec with second test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -3,19 +3,24 @@ require 'pry'
 
 RSpect.describe Registrant do
   describe '#initalize' do
-    it 'exist' do
+    it 'has name, age, permit and license status for registrant 1' do
       registrant_1 = Registrant.new('Bruce', 18, true )
-      registrant_2 = Registrant.new('Penny', 15 )
-
+      
       expect(registrant_1.name).to eq('Bruce')
       expect(registrant_1.age).to eq(18)
       expect(registrant_1.permit?).to be true
       expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
-      
-      
-      
-
     end
+    
+    
+    it 'has name, age, permit and license status for registrant 2' do
+      registrant_2 = Registrant.new('Penny', 15 )
+      
+      expect(registrant_1.name).to eq('Penny')
+      expect(registrant_1.age).to eq(15)
+      expect(registrant_1.permit?).to be false
+      expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end 
   end
 
 end
