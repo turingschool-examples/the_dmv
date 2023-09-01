022e26d728afd31a05cee200e39747763bfc6da4
Sooyung Kim <kimx1279@gmail.com>
2023-08-31 19:16:04 -0600

    Create registrant spec

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,21 @@
+require './lib/registrant'
+require 'pry'
+
+RSpect.describe Registrant do
+  describe '#initalize' do
+    it 'exist' do
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 15 )
+
+      expect(registrant_1.name).to eq('Bruce')
+      expect(registrant_1.age).to eq(18)
+      expect(registrant_1.permit?).to be true
+      expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      
+      
+      
+
+    end
+  end
+
+end
