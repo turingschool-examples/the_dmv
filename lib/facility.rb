85d6fdb86b1f37a264222ff78750522a6854ab32
Sooyung Kim <kimx1279@gmail.com>
2023-09-01 12:35:54 -0600

    Add methods and create test

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,22 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services, 
+              :registered_vehicles, 
+              :collected_fees
   
   def initialize(facility_data)
-      @name = facility_data[:name] 
-      @address = facility_data[:address] 
-      @phone = facility_data[:phone] 
+    @name = facility_data[:name] 
+    @address = facility_data[:address] 
+    @phone = facility_data[:phone] 
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
   end
+
 end
