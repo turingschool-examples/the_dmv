a4732c46833a430d499b7eee41a12c0d55022926
Miranda Delaney <delaney.miranda1@gmail.com>
2023-09-01 13:16:45 -0400

    feat: Add registration date as an instance variable

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,7 +1,7 @@
 require 'date'
 
 class Vehicle
-  attr_reader :vin, :year, :make, :model, :engine
+  attr_reader :vin, :year, :make, :model, :engine, :registration_date
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -9,6 +9,7 @@ class Vehicle
     @make = vehicle_details[:make]
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
+    @registration_date = vehicle_details[:registration_date]
   end
 
   def antique?
@@ -18,4 +19,5 @@ class Vehicle
   def electric_vehicle?
     @engine == :ev
   end
+
 end
