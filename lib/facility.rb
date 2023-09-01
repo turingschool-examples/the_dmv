bca692627022c33904689143c50b2e0661bfd493
Sooyung Kim <kimx1279@gmail.com>
2023-09-01 17:44:22 -0600

    Update register vehicle, collected fees method and test

--- lib/facility.rb
+++ lib/facility.rb
@@ -19,17 +19,27 @@ class Facility
 
   def collected_fees
     @collected_fees = 0
+    @registered_vehicles.map do |vehicle|
+      if vehicle.plate_type == :antique
+        @collected_fees += 25
+      elsif vehicle.plate_type == :ev
+        @collected_fees += 200
+      else vehicle.plate_type == :regular
+        @collected_fees += 100
+      end
+    end
+    @collected_fees.to_i
   end
 
-  def register_vehicle(car_type)
-    @registered_vehicles << car_type
-    car_type.registration_date = Date.today
-      if car_type.antique? == true
-        car_type.plate_type = :antique
-      elsif car_type.engine == :ev
-        car_type.plate_type = :ev
+  def register_vehicle(car)
+    @registered_vehicles << car
+    car.registration_date = Date.today
+      if car.antique? == true
+        car.plate_type = :antique
+      elsif car.engine == :ev
+        car.plate_type = :ev
       else
-        car_type.plate_type = :regular
+        car.plate_type = :regular
       end
   end
 end
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -72,6 +72,30 @@ RSpec.describe Facility do
     end
   end
 
+  describe '#collect fees' do
+    it 'collect fees' do
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+
+      facility_1.register_vehicle(cruz)
+
+      expect(facility_1.collected_fees).to eq(100)
+    end
+  end
+
+  describe '#register vehicle' do
+    it 'adds additional vehicle' do
+    facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+
+    facility_1.register_vehicle(cruz)
+    facility_1.register_vehicle(camaro)
+
+    expect(camaro.registration_date).to eq(Date.today)
+    expect(camaro.plate_type).to eq(:antique)
+    end
+  end
   # facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
   #     facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
   #     cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
