9878d97fe4a2cdf9aad18f7c64738407f6692b17
Heather McNeil <hd_mcneil@yahoo.com>
2023-08-31 18:51:20 -0700

    Add more initalize testing

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -12,7 +12,15 @@ describe Registrant do
   describe '#initialize' do
     it 'can initialize' do
       expect(@registrant).to be_an_instance_of(Registrant)
- 
+      expect(@registrant_1.name).to eq("Bruce")
+      expect(@registrant_1.age).to eq(18)
+      expect(@registrant_1.permit?).to be(true)
+      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@registrant_2.name).to eq("Penny")
+      expect(registrant_2.age).to eq(15)
+      expect(registrant_2.permit?).to eq(false)
+      expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      
   end
 end
 
