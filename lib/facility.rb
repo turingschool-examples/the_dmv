1c50734b64ca051692741dc9bcb72e161e200386
Joseph Lee <jhjlee702@gmail.com>
2023-09-01 16:34:08 -0600

    write register_vehicle

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,5 +1,6 @@
 class Facility
   attr_reader :name, :address, :phone, :services, :registered_vehicles
+  attr_accessor :collected_fees
 
   def initialize(facility_details)
     @name = facility_details[:name]
@@ -7,10 +8,35 @@ class Facility
     @phone = facility_details[:phone]
     @services = []
     @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
+  end
+
+  #make sure I test this and write a test
+  def set_plate_type(car)
+    if car.antique? == true
+      :antique
+    elsif car.electric_vehicle? == true
+      :ev
+    else 
+      :regular
+    end
+  end
+
+  def register_vehicle(car)
+    @registered_vehicles << car
+    car.registration_date = Date.today
+    car.plate_type = set_plate_type(car)
+    if car.antique?
+      @collected_fees += 25
+    elsif car.electric_vehicle?
+      @collected_fees += 325
+    else 
+      @collected_fees += 100
+    end
   end
 end
 
