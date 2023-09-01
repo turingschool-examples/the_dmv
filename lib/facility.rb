f0bb658e9871a942f4e45ab5343228a52b058582
Sam Tran <samuelttran@gmail.com>
2023-09-01 12:01:35 -0600

    Change initialize method to accept single hash argument

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,10 +1,10 @@
 class Facility
   attr_reader :name, :address, :phone, :services
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(facility_details)
+    @name = facility_details[:name]
+    @address = facility_details[:address]
+    @phone = facility_details[:phone]
     @services = []
   end
 
