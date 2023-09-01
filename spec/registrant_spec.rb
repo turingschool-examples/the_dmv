29cbfa7848e06df0b5c5be15d97c67883d7a71ac
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-09-01 12:59:23 -0600

    Minor update to Gemfile

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,6 +1,6 @@
 require './lib/registrant'
 
-RSpec describe Registrant do
+RSpec.describe Registrant do
   describe '#initialize' do
     it 'can initialize' do
       registrant_1 = Registrant.new('Bruce', 18, true )
@@ -11,34 +11,35 @@ RSpec describe Registrant do
     end
 
     it 'has a name' do
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 15 )
+      
+      expect(registrant_1.name).to eq('Bruce')
+      expect(registrant_2.name).to eq('Penny')
+    end
+    
+    it 'has an age' do
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 15 )
 
-    expect(registrant_1.name).to eq('Bruce')
-    expect(registrant_2.name).to eq('Penny')
+      expect(registrant_1.age).to eq(18)
+      expect(registrant_2.age).to eq(15)
+    end
+    
+    it 'has a permit' do
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 15 )
+
+      expect(registrant_1.permit).to eq(true)
+      expect(registrant_2.permit).to eq(false)
+    end
+    
+    it 'has license data' do
+      registrant_1 = Registrant.new('Bruce', 18, true )
+      registrant_2 = Registrant.new('Penny', 15 )
+
+      expect(registrant_1.license_data).to be_an_instance_of(Hash)
+      expect(registrant_2.license_data).to be_an_instance_of(Hash)
+    end
   end
-  it 'has an age' do
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
-
-    expect(registrant_1.age).to eq(18)
-    expect(registrant_2.age).to eq(15)
-    end
-  it 'has a permit' do
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
-
-    expect(registrant_1.permit).to eq(true)
-    expect(registrant_2.permit).to eq(false)
-    end
-  it 'has license data' do
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
-
-    expect(registrant_1.license_data).to be_an_instance_of(Hash)
-    expect(registrant_2.license_data).to be_an_instance_of(Hash)
-    end
-  end
-
-
 end
