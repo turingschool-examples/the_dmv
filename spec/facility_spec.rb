bdf8a2ef9bd0af5e3a4482e3105094624b0c568b
Joseph Lee <jhjlee702@gmail.com>
2023-09-01 12:36:55 -0600

    Check cruz registration

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -5,6 +5,9 @@ require './lib/vehicle'
 RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
   end
 
   describe '#initialize' do
@@ -27,6 +30,12 @@ RSpec.describe Facility do
     end
   end
 
+describe '#check cruz.registration_date' do
+  it 'checks registration date of cruz' do
+    expect(@cruz.registration_date).to eq(nil)
+  end
+end
+
   describe '#check F1 vehicles' do
     it 'checks registered_vehicles for facility_1' do
       expect(@facility.registered_vehicles).to eq([])
@@ -38,4 +47,13 @@ RSpec.describe Facility do
       expect(@facility.collected_fees).to eq(0)
     end
   end
+
+  # describe '#register cruz and check info' do
+  #   it 'registers cruz and checks info' do
+  #     @facility.register_vehicle(@cruz)
+      
+  #   end
+  # end
 end
+
+#use today and year methods???
