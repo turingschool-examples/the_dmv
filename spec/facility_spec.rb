b60b2a074fe4b2d334ba3b3d81812aa6a2d21a12
Joseph Lee <jhjlee702@gmail.com>
2023-09-01 16:33:45 -0600

    register cruz and check facility details

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -4,7 +4,7 @@ require './lib/vehicle'
 
 RSpec.describe Facility do
   before(:each) do
-    @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
@@ -12,21 +12,21 @@ RSpec.describe Facility do
 
   describe '#initialize' do
     it 'can initialize' do
-      expect(@facility).to be_an_instance_of(Facility)
-      expect(@facility.name).to eq('DMV Tremont Branch')
-      expect(@facility.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
-      expect(@facility.phone).to eq('(720) 865-4600')
-      expect(@facility.services).to eq([])
+      expect(@facility_1).to be_an_instance_of(Facility)
+      expect(@facility_1.name).to eq('DMV Tremont Branch')
+      expect(@facility_1.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
+      expect(@facility_1.phone).to eq('(720) 865-4600')
+      expect(@facility_1.services).to eq([])
     end
   end
 
   describe '#add service' do
     it 'can add available services' do
-      expect(@facility.services).to eq([])
-      @facility.add_service('New Drivers License')
-      @facility.add_service('Renew Drivers License')
-      @facility.add_service('Vehicle Registration')
-      expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+      expect(@facility_1.services).to eq([])
+      @facility_1.add_service('New Drivers License')
+      @facility_1.add_service('Renew Drivers License')
+      @facility_1.add_service('Vehicle Registration')
+      expect(@facility_1.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
   end
 
@@ -38,22 +38,40 @@ end
 
   describe '#check F1 vehicles' do
     it 'checks registered_vehicles for facility_1' do
-      expect(@facility.registered_vehicles).to eq([])
+      expect(@facility_1.registered_vehicles).to eq([])
     end
   end
 
   describe '#check F1 collected fees' do
     it 'checks collected fees for facility_1' do
-      expect(@facility.collected_fees).to eq(0)
+      expect(@facility_1.collected_fees).to eq(0)
     end
   end
 
-  # describe '#register cruz and check info' do
-  #   it 'registers cruz and checks info' do
-  #     @facility.register_vehicle(@cruz)
-      
-  #   end
-  # end
+  describe '#register cruz and check info' do
+    it 'registers cruz and checks info' do
+      expect(@cruz).to be_an_instance_of(Vehicle)
+      @facility_1.register_vehicle(@cruz)
+      expect(@cruz.registration_date).to eq(Date.today)
+      expect(@cruz.plate_type).to eq(:regular)
+      #require the facility to have the service you're performing
+    end
+  end
+
+  describe '#F1 registered vehicles' do
+    it 'checks registered vehicles in facility_1' do
+      @facility_1.register_vehicle(@cruz)
+      expect(@facility_1.registered_vehicles).to eq([@cruz])
+      #require the facility to have the service you're performing
+    end
+  end
+
+  describe '#F1 collected fees' do
+    it 'checks collected fees in facility_1' do
+      @facility_1.register_vehicle(@cruz)
+      expect(@facility_1.collected_fees).to eq(100)
+    end
+  end
 end
 
 #use today and year methods???
