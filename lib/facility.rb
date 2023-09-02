8d98bef2e2f10121d54b518a74c4408198ece570
Martin Chavez <martinchavez1203@gmail.com>
2023-09-02 15:16:02 -0600

    update class

--- lib/facility.rb
+++ lib/facility.rb
@@ -12,21 +12,23 @@ class Facility
   def add_service(service)
     @services << service
   end
-  def register_vehicle(vehicle)
-    vehicle.registration_date = Date.today
-    @registered_vehicles << vehicle
 
-    if vehicle.antique? 
-      vehicle.plate_type = :antique
-      @collected_fees += 25
-    elsif vehicle.electric_vehicle? == true 
-      vehicle.plate_type = :ev
-      @collected_fees += 200
-    else   
-      vehicle.plate_type = :regular
-      @collected_fees += 100 
+  def register_vehicle(vehicle)
+    if services.include?("Vehicle Registration")
+      vehicle.registration_date = Date.today
+      @registered_vehicles << vehicle
+
+      if vehicle.antique? 
+        vehicle.plate_type = :antique
+        @collected_fees += 25
+      elsif vehicle.electric_vehicle? == true 
+        vehicle.plate_type = :ev
+        @collected_fees += 200
+      else   
+        vehicle.plate_type = :regular
+        @collected_fees += 100 
+      end 
+      registered_vehicles
     end 
-    registered_vehicles
   end 
-  
 end
