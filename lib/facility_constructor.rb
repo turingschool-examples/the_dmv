c380ab197ff59e8440152596cca66aea67e3c0fd
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-09-02 00:29:32 -0600

    Pass all tests for facility constructor

--- lib/facility_constructor.rb
+++ lib/facility_constructor.rb
@@ -4,6 +4,7 @@ class FacilityConstructor
     new_facilities = []
     api_hash_array.each do |facility_data|
       facility = Facility.new(parse(facility_data))
+      facility.services = add_services(facility_data)
       new_facilities << facility
     end
     new_facilities
@@ -13,5 +14,15 @@ class FacilityConstructor
     api_hash[:name] = api_hash[:dmv_office]
     api_hash[:address] = "#{api_hash[:address_li]} #{api_hash[:address__1]} #{api_hash[:city]} #{api_hash[:state]} #{api_hash[:zip]}"
     api_hash
+  end
+
+  def add_services(api_hash)
+    services = api_hash[:services_p]
+    facility_services = []
+    facility_services << 'Vehicle Registration' if services.include?('registration')
+    facility_services << 'Written Test' if services.include?('wtest')
+    facility_services << 'Road Test'  if services.include?('rtest')
+    facility_services << 'Renew License' if services.include?('renewals')
+    facility_services
   end
 end
