5e8bf73cd404b3217e90a4a39f9802fcfb055dd7
Martin Chavez <martinchavez1203@gmail.com>
2023-09-02 13:07:58 -0600

    add register for cruz

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,32 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
-
+  attr_reader :name, :address, :phone, :services, :registered_vehicles, :collected_fees
   def initialize(info)
     @name = info[:name]
     @address = info[:address]
     @phone = info[:phone]
     @services = []
+    @registered_vehicles =[]
+    @collected_fees = 0 
   end
 
   def add_service(service)
     @services << service
   end
+  def register_vehicle(vehicle)
+    vehicle.registration_date = Date.today
+    @registered_vehicles << vehicle
+
+    if vehicle.antique? 
+      vehicle.plate_type = :antique
+      @collected_fees += 25
+    elsif vehicle.electric_vehicle? == true 
+      vehicle.plate_type = :ev
+      @collected_fees += 200
+    else   
+      vehicle.plate_type = :regular
+      @collected_fees += 100 
+    end 
+    registered_vehicles
+  end 
+  
 end
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -2,25 +2,98 @@ require 'spec_helper'
 
 RSpec.describe Facility do
   before(:each) do
-    @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+    # @registrant_1 = Registrant.new('Bruce', 18, true)
+    # @registrant_2 = Registrant.new('Penny', 15 )
+    
   end
   describe '#initialize' do
-    it 'can initialize' do
-      expect(@facility).to be_an_instance_of(Facility)
-      expect(@facility.name).to eq('DMV Tremont Branch')
-      expect(@facility.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
-      expect(@facility.phone).to eq('(720) 865-4600')
-      expect(@facility.services).to eq([])
+    it 'can initialize' do    
+      expect(@facility_1).to be_an_instance_of(Facility)
+      expect(@facility_1.name).to eq('DMV Tremont Branch')
+      expect(@facility_1.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
+      expect(@facility_1.phone).to eq('(720) 865-4600')
+      expect(@facility_1.services).to eq([])
+      expect(@facility_1.registered_vehicles).to eq([])
+      expect(@facility_1.collected_fees).to eq(0)
     end
   end
 
   describe '#add service' do
     it 'can add available services' do
-      expect(@facility.services).to eq([])
-      @facility.add_service('New Drivers License')
-      @facility.add_service('Renew Drivers License')
-      @facility.add_service('Vehicle Registration')
-      expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+      expect(@facility_1.services).to eq([])
+      @facility_1.add_service('New Drivers License')
+      @facility_1.add_service('Renew Drivers License')
+      @facility_1.add_service('Vehicle Registration')
+      expect(@facility_1.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
   end
-end
+
+
+ describe 'register_vehicle' do 
+    it 'sets the registration date if the service is available ' do 
+      expect(@cruz.registration_date).to eq(nil) 
+      #require 'pry'; binding.pry
+      @facility_1.add_service('vehicle Registraction')
+      expect(@facility_1.registered_vehicles).to eq([])
+      @facility_1.register_vehicle(@cruz)
+
+   
+      
+      expect(@cruz.registration_date).to eq(Date.today)
+      expect(@cruz.plate_type).to eq(:regular)
+      expect(@facility_1.registered_vehicles).to eq([@cruz])
+      expect(@facility_1.collected_fees).to eq(100)
+    end 
+    
+    it '' do 
+      
+    end 
+
+  end 
+
+
+
+end  
+
+
+
+
+
+
+
+
+#       facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+#       facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+#       cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+#       bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+#       camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+#       expect(facility_1.add_service('Vehicle Registration')).to eq(["Vehicle Registration"])
+#       expect(cruz.registration_date).to eq(nil) ;
+#       expect(facility_1.registered_vehicles).to eq([])
+#       expect(facility_1.collected_fees).to eq(0)
+#       expect(facility_1.register_vehicle(cruz)).to eq([cruz])
+#       expect(cruz.registration_date).to eq(Date.today)
+#       expect(cruz.plate_type).to eq(:regular)
+#       expect(facility_1.registered_vehicles).to eq([cruz])
+#       expect(facility_1.collected_fees).to eq(100)
+#       expect(facility_1.register_vehicle(camaro)).to eq([cruz,camaro])
+#       expect(camaro.registration_date).to eq(Date.today)
+#       expect(camaro.plate_type).to eq(:antique)
+#       expect(facility_1.register_vehicle(bolt)).to eq([cruz,camaro,bolt])
+#       expect(bolt.registration_date).to eq(Date.today)
+#       expect(bolt.plate_type).to eq(:ev)
+#       expect(facility_1.registered_vehicles).to eq([cruz,camaro,bolt])
+#       expect(facility_1.collected_fees).to eq(325)
+#       expect(facility_2.registered_vehicles).to eq([])
+#       expect(facility_2.services).to eq([])
+#       expect(facility_2.register_vehicle(bolt)).to eq(nil)
+#       expect(facility_2.registered_vehicles).to eq([])
+#       expect(facility_2.collected_fees).to eq(0)
+#     end 
+#   end 
+# end 
