ab8515372fcc9c2d275003d3e7775622a5675cc3
Blaine Kennedy <blainekennedy@gmail.com>
2023-09-01 21:00:18 -0500

    feat: add registrant class

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,8 +1,15 @@
 class Registrant
-  def initialize
-    @name
-    @age 
-    @permit = false
+  attr_accessor :name, :age, :permit, :license_data
+  def initialize(name, age, permit = false)
+    @name = name
+    @age = age
+    @permit
+    @license_data = {written: false,
+    license: false,
+  renewed: false}
+  end
+  def permit
+    @permit
   end
   
 end
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
+require './lib/registrant'
 
 RSpec.describe Vehicle do
   before(:each) do
     @registrant_1 = Registrant.new('Bruce', 18, true )
     @registrant_2 = Registrant.new('Penny', 15 )
   end
-  def '#initialize'do
+  describe '#initialize' do
     it 'will initialize' do
-      expect(registrant_1.name).to eq('Bruce')
-      expect(registrant_1.age).to eq(18)
-      expect(registrant_1.permit?).to eq(true)
+      expect(@registrant_1.name).to eq('Bruce')
+      expect(@registrant_1.age).to eq(18)
+    end
   end
+  
 end
 
