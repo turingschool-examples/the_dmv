913ddd17bc64652f41eb8f8ffda49e6cc9324aee
Xander Hendry <vhendry.coding@gmail.com>
2023-09-01 21:36:34 -0400

    Add register vehicle helper method and tests

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,5 +1,9 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name,
+              :address,
+              :phone, 
+              :services,
+              :registered_vehicles
 
   def initialize(facility_details)
     @name = facility_details[:name]
@@ -13,22 +17,27 @@ class Facility
     @services << service
   end
   
+  # write test for helper method
   def register_vehicle_helper
-    if @registered_vehicles.nil?
+    if @registered_vehicles.nil? && @services.include?('Vehicle Registration')
       @registered_vehicles = []
     end
   end
-
+  # write test for helper method
   def collected_fees_helper
     if @collected_fees.nil?
       @collected_fees = 0
     end
   end
 
-  def registered_vehicles
-    register_vehicle_helper
-    @registered_vehicles
-  end
+  # def registered_vehicles
+  #   if @services.includes?('Vehicle Registration')
+  #     register_vehicle_helper
+  #     @registered_vehicles
+  #   else
+  #     "This service is not offered at this location"
+  #   end
+  # end
 
   def collected_fees
     collected_fees_helper
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -8,59 +8,76 @@ RSpec.describe Facility do
 
   describe '#initialize' do
     it 'can initialize' do
-      expect(@facility).to be_an_instance_of(Facility)
-      expect(@facility.name).to eq('DMV Tremont Branch')
-      expect(@facility.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
-      expect(@facility.phone).to eq('(720) 865-4600')
-      expect(@facility.services).to eq([])
+      expect(@facility_1).to be_an_instance_of(Facility)
+      expect(@facility_1.name).to eq('DMV Tremont Branch')
+      expect(@facility_1.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
+      expect(@facility_1.phone).to eq('(720) 865-4600')
+      expect(@facility_1.services).to eq([])
     end
   end
 
   describe '#add service' do
     it 'can add available services' do
-      expect(@facility.services).to eq([])
-      @facility.add_service('New Drivers License')
-      @facility.add_service('Renew Drivers License')
-      @facility.add_service('Vehicle Registration')
-      expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'] )
+      expect(@facility_1.services).to eq([])
+      @facility_1.add_service('New Drivers License')
+      @facility_1.add_service('Renew Drivers License')
+      @facility_1.add_service('Vehicle Registration')
+      expect(@facility_1.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'] )
     end
   end
 
-  describe '#register_vehicle' do
+  describe '#register vehicle helper' do
     before(:each) do 
       @facility_1.add_service('Vehicle Registration')
-      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
-      bolt = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ev} )
-      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+      @facility_1.register_vehicle_helper 
+      @facility_2.register_vehicle_helper
     end
-    
-    it 'keeps an array for registered Vehicles' do
-      expect(facility.registered_vehicles).to eq([])
-    end 
-
-    it 'keep track of collected_fees' do
-      expect(facility.registered_vehicles).to eq(0)
+    it 'creates an array to store registered vehicles' do
+      expect(@facility_1.registered_vehicles).to eq([])
     end
 
-    it 'will add registered Vehicles to the array' do
-      @facility_1.register_vehicle(cruz)
-      expect(facility.registered_vehicles).to eq([cruz])
-      @facility_1.register_vehicle(bolt)
-      expect(facility.registered_vehicles).to eq([cruz, bolt])
+    it 'will only add an array if the facility offers vehicle registration' do
+      expect(@facility_2.registered_vehicles).to eq(nil)
     end
+  end
 
-    it 'will give the Vehicle a registration date' do
-      @facility_1.register_vehicle(cruz)
-      expect(cruz.registration_date).to be_an_instance_of(Date)
+  xdescribe '#register vehicle' do
+    context 'before vehicles have been registered' do
+      before(:each) do 
+        @facility_1.add_service('Vehicle Registration')
+      end
+      it 'has an array to store registered vehicles' do
+        expect
+      end   
     end
-
-    it 'assigns plates by type' do
-      @facility_1.register_vehicle(cruz)
-      @facility_1.register_vehicle(bolt)
-      expect(cruz.plate_type).to eq(:ice)
-      expect(bolt.plate_type).to eq(:ev)
-    end
-
+  end
       
       
 end
+    # it 'keeps an array for registered Vehicles' do
+    #   expect(facility.registered_vehicles).to eq([])
+    # end 
+
+    # it 'keep track of collected_fees' do
+    #   expect(facility.registered_vehicles).to eq(0)
+    # end
+
+    # it 'will add registered Vehicles to the array' do
+    #   @facility_1.register_vehicle(cruz)
+    #   expect(facility.registered_vehicles).to eq([cruz])
+    #   @facility_1.register_vehicle(bolt)
+    #   expect(facility.registered_vehicles).to eq([cruz, bolt])
+    # end
+
+    # it 'will give the Vehicle a registration date' do
+    #   @facility_1.register_vehicle(cruz)
+    #   expect(cruz.registration_date).to be_an_instance_of(Date)
+    # end
+
+    # it 'assigns plates by type' do
+    #   @facility_1.register_vehicle(cruz)
+    #   @facility_1.register_vehicle(bolt)
+    #   @facility_
+    #   expect(cruz.plate_type).to eq(:regular)
+    #   expect(bolt.plate_type).to eq(:ev)
+    # end
