a8ef08efa98bbaa7b6ec4f8811ae99bb5c6c3ef0
Justin Lee <justinslee201100@yahoo.com>
2023-09-02 14:36:02 -0600

    Add touchup to methods

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,5 +1,10 @@
 class Facility
-  attr_reader :name, :address, :phone, :services, :registered_vehicles
+  attr_reader :name,
+              :address,
+              :phone,
+              :services,
+              :registered_vehicles,
+              :collected_fees
   
 
   def initialize(facility_details)
@@ -8,6 +13,7 @@ class Facility
     @phone = facility_details[:phone]
     @services = []
     @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
@@ -15,18 +21,18 @@ class Facility
   end
   
   def register_vehicle(vehicle)
-    # require 'pry'; binding.pry
-    if !services.include?("Vehicle Registration")
-      return nil
-      if services.include?("Vehicle Registration")
-        vehicle.registration_date = Date.today
-        @registered_vehicles << vehicle
-        if vehicle.antique?
-          @plate_type = :antique
-        elsif vehicle.electric_vehicle? == true
-          @plate_type = :ev
-        else @plate_type = :regular
-        end
+    if services.include?("Vehicle Registration")
+      vehicle.registration_date = Date.today
+      @registered_vehicles << vehicle
+      
+      if vehicle.antique?
+        vehicle.plate_type = :antique
+        @collected_fees += 25
+      elsif vehicle.electric_vehicle? == true
+        vehicle.plate_type = :ev
+        @collected_fees += 200
+      else vehicle.plate_type = :regular
+        @collected_fees += 100
       end
     end
   end
