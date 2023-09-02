ba7408433663aed92f3a882a9ae24ced157d5290
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-09-02 10:58:14 -0600

    Write test for making a facility with different API data

--- spec/facility_constructor_spec.rb
+++ spec/facility_constructor_spec.rb
@@ -20,10 +20,23 @@ RSpec.describe FacilityConstructor do
       end
     end
 
-    it 'creates facilities with all data' do
+    it 'creates facilities with all data intact' do
       creation_log = @constructor.create_facilities(@co_dmv_offices)
       expect(creation_log).to be_an_instance_of(Array)
       creation_log.each do |facility|
+        expect(facility.name).not_to eq(nil)
+        expect(facility.address).not_to eq(nil)
+        expect(facility.phone).not_to eq(nil)
+        expect(facility.services).not_to eq([])
+      end
+    end
+
+    it 'can create facilities with a different data set' do
+      ny_dmv_offices = DmvDataService.new.ny_dmv_office_locations
+      creation_log = @constructor.create_facilities(ny_dmv_offices)
+      expect(creation_log).to be_an_instance_of(Array)
+      creation_log.each do |facility|
+        expect(facility).to be_an_instance_of(Facility)
         expect(facility.name).not_to eq(nil)
         expect(facility.address).not_to eq(nil)
         expect(facility.phone).not_to eq(nil)
