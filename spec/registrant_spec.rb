9dbb007331c17a0eb0779b6fa66eb82883cde7e1
Kam Kennedy <kameronk013@gmail.com>
2023-09-02 09:00:15 -0600

    update registrant spec

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 RSpec.describe Registrant do
   before(:each) do #how does scope this work, where does the end go
     @registrant_1 = Registrant.new('Bruce', 18, true )
-    @registrant_2 = Registrant.new('Penny', 15 )
+    @registrant_2 = Registrant.new('Penny', 16 ) #was age 15 before, changed following interaction pattern
+    @registrant_3 = Registrant.new('Tucker', 15 )
+    @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
   end
 
   describe '#initialize' do
@@ -28,6 +31,25 @@ RSpec.describe Registrant do
     it '#earn_permit changes permit status to true' do
       @registrant_2.earn_permit
       expect(@registrant_2.permit).to eq(true)
+    end
+  end
+
+  describe '#administer written test' do
+    it '#accesses registrant license data' do
+      expect(registrant_1.instance_variable_get(:@license_data).length).to eq(3)
+      expect(registrant_1.permit?).to eq(false)
+    end
+
+    it '#does not administer without offering' do
+      expect(facility_1.administer_written_test(registrant_1)).to eq(false)
+      facility_1.add_service('Written Test')
+      expect(facility_1.administer_written_test(registrant_1)).to eq(true)
+    end
+
+    it '#changes hash to true after administering' do
+      facility_1.add_service('Written Test')
+      facility_1.administer_written_test(registrant_1)
+      expect(registrant_1.license_data(:license)).to eq(true)
     end
   end
 end
