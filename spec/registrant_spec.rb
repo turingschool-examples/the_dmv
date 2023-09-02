ec392cf5469ca09dde450202df4617ffa2daec66
Billy Burch <billyburch40@gmail.com>
2023-09-02 13:07:57 -0600

    add: test for 2 arguments and return false

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -3,14 +3,22 @@ require './lib/registrant'
 require 'pry'
 
 RSpec.describe Registrant do
-  it 'exists' do
-    expect(Registrant.new).to be_an_instance_of(Registrant) 
+
+  describe '#initialize' do
+    it 'creates new registrant' do
+      registrant_1 = Registrant.new('Bruce', 18, true)
+      expect(registrant_1).to be_a Registrant
+    end
+  end
+  
+  describe '#initialize2' do
+    it 'creates registrant no permit' do
+      registrant_2 = Registrant.new('Penny', 15 )
+      expect(registrant_2).to be_a Registrant
+      expect(registrant_2.permit).to eq(false)
+    end
   end
 
-  xit 'creates new registrant' do
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    expect(registrant_1).to be_a(Registrant)
-  end
 end
 
   
