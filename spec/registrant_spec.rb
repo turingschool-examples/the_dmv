8603b949d59afac8dcd3d97ca89e1380cd6a04b8
Blaine Kennedy <blainekennedy@gmail.com>
2023-09-01 20:28:56 -0500

    test: add #initialize test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -5,3 +5,11 @@ RSpec.describe Vehicle do
     @registrant_1 = Registrant.new('Bruce', 18, true )
     @registrant_2 = Registrant.new('Penny', 15 )
   end
+  def '#initialize'do
+    it 'will initialize' do
+      expect(registrant_1.name).to eq('Bruce')
+      expect(registrant_1.age).to eq(18)
+      expect(registrant_1.permit?).to eq(true))
+  end
+end
+
