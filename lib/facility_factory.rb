b1e2fc31f5ff3c226720c7403536ace71103a11f
Kevin Zolman <kevin.zolman@gmail.com>
2023-09-02 12:30:50 -0600

    Add comments to facility_factory and associated tests

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -5,17 +5,20 @@ class FacilityFactory
 
   def create_facilities(locations)
     facilities = locations.map do |location|
+      # CO
       if location[:dmv_office] != nil
         name = location[:dmv_office]
         address = "#{location[:address_li]} #{location[:address__1]} #{location[:city]} 
         #{location[:state]} #{location[:zip]}"
         phone = location[:phone]
+      # NY
       elsif location[:office_name] != nil
         name = location[:office_name]
         address = "#{location[:street_address_line_1]} #{location[:street_address_line_2]} 
         #{location[:city]} #{location[:state]} #{location[:zip_code]}"
-        # normalize this phone number
+        # normalize this phone number (5555555555)
         phone = location[:public_phone_number]
+      # MO
       else
         name = location[:name]
         address = "#{location[:address1]} #{location[:city]} #{location[:state]} #{:zipcode}"
@@ -26,7 +29,3 @@ class FacilityFactory
     end
   end
 end
-
-# service descriptions do not match method terms, normalize this
-      # just do services manually with in-house method
-      # services = location[:services_p].split
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -8,6 +8,21 @@ RSpec.describe FacilityFactory do
     @mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
   end
 
+  # describe '#normalize_phone' do
+  #   it 'will normalize a phone number to the output (555) 555-5555' do
+  #     number = 1234567890
+      
+  #     expect(normalize_phone(number)).to eq("(123) 456-7890")
+  #   end
+
+  #   # if this method is used, may have to make all other phone numbers into strings
+  #   it 'will output the same number if fed an already normalized number' do
+  #     number = "(123) 456-7890"
+
+  #     expect(normalize_phone(number)).to eq("(123) 456-7890")
+  #   end
+  # end
+
   describe '#create_facilities' do
     it 'will create Facility objects using a location dataset' do
       @factory.create_facilities(@co_dmv_office_locations).each do |location|
