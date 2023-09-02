72ef84af24b6abcf83692d865971f63c66e21851
Billy Burch <billyburch40@gmail.com>
2023-09-02 13:18:21 -0600

    add: attribute spec test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -18,10 +18,12 @@ RSpec.describe Registrant do
       expect(registrant_2.permit).to eq(false)
     end
   end
-
+  describe 'attributes' do
+    it 'returns registrant attributes' do
+      registrant_1 = Registrant.new('Bruce', 18, true)
+      expect(registrant_1.name).to eq("Bruce") 
+      expect(registrant_1.age).to eq(18) 
+      expect(registrant_1.permit).to eq(true) 
+    end
+  end
 end
-
-  
-
-
-# registrant_2 = Registrant.new('Penny', 15 )
