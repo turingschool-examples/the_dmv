b0562e625176ad0cf78d888bb067af43213ef0cc
Sam Tran <samuelttran@gmail.com>
2023-09-01 20:21:09 -0600

    Add default states for registered_vehicles and collected_fees

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,13 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles, :collected_fees
 
   def initialize(facility_details)
     @name = facility_details[:name]
     @address = facility_details[:address]
     @phone = facility_details[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
