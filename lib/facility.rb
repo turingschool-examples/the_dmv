64394067d6409ea7689942b160f77ff15ecc80bc
Billy Burch <billyburch40@gmail.com>
2023-09-02 16:23:23 -0600

    delete comments

--- lib/facility.rb
+++ lib/facility.rb
@@ -6,13 +6,6 @@ class Facility
     @address = facility_details[:address]
     @phone = facility_details[:phone]
     @services = []
-
-    # Below copy and paste helped with syntax comparison
-    # @vin = vehicle_details[:vin]
-    # @year = vehicle_details[:year]
-    # @make = vehicle_details[:make]
-    # @model = vehicle_details[:model]
-    # @engine = vehicle_details[:engine]
   end
 
   def add_service(service)
