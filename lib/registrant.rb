82f6c964b5c17a1e0d92b72b1e320ec407479d74
Billy Burch <billyburch40@gmail.com>
2023-09-02 13:06:46 -0600

    add: attr_reader to pass test initialize2

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,7 +1,8 @@
 class Registrant
+  attr_reader :name, :age, :permit
   def initialize(name, age, permit = false)
     @name = name
     @age = age
-    @permit = false
+    @permit = permit
   end
 end
