27bffd26134149dec4880e829e4d12a9d3d0e827
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-09-01 22:20:51 -0600

    Satisfy all factory test conditions for creating proper vehicle instances from API data

--- lib/factory.rb
+++ lib/factory.rb
@@ -1,12 +1,20 @@
 class Factory
 
-  def create_vehicles(wa_ev_registrations)
+  def create_vehicles(api_hash_array)
     new_vehicles = []
-    wa_ev_registrations.each do |vehicle_data|
-      vehicle = Vehicle.new(vehicle_data)
+    api_hash_array.each do |vehicle_data|
+      vehicle = Vehicle.new(parse(vehicle_data))
       new_vehicles << vehicle
     end
     new_vehicles
   end
+
+  def parse(api_hash)
+    api_hash[:year] = api_hash[:model_year]
+    api_hash[:engine] = :ev
+    api_hash[:vin] = api_hash[:vin_1_10]
+    api_hash
+  end
+
 
 end
