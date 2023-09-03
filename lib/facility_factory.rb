35c8c85ad215d11aa48be4862b419f4cacb2ac9a
Scott DeVoss <scottdevoss@yahoo.com>
2023-09-03 15:23:45 -0600

    feat: added to my create_facilities method to be able to access multiple data sets

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,10 +1,56 @@
+# class FacilityFactory
+#   def create_facilities(data)
+#     data.map do |facility_data|
+#       facility = Facility.new({name: facility_data[:dmv_office],
+#         address: facility_data[:address_li],
+#         phone: facility_data[:phone]})
+#     end
+#   end
+
+  # facility_data[:address__l], facility_data[:city], facility_data[:state], facility_data[:zip]
+
+  # def get_co_address(facility_data)
+  #   facility_data[:address_li]
+  #   facility_data[:address_li]
+  # end
+
+  
+# end
+
+
+#facility needs to have name, address, phone
+  #co 
+      #name: facility_data[:dmv_office]
+      #address: facility_data[:address_li]
+      #phone: facility_data[:phone]
+
+  #ny 
+      #name: facility_data[:office_name]
+      #address: facility_data[:]
+
+
+
 class FacilityFactory
   def create_facilities(data)
-    data.map do |facility_data|
+    if data == DmvDataService.new.co_dmv_office_locations
+      data.map do |facility_data|
       facility = Facility.new({name: facility_data[:dmv_office],
         address: facility_data[:address_li],
-        phone: facility_data[:phone],
-        services: facility_data[:services_p]})
-    end
+        phone: facility_data[:phone]})
+      end 
+    elsif data == DmvDataService.new.ny_dmv_office_locations
+      data.map do |facility_data|
+      facility = Facility.new({name: facility_data[:office_name],
+        address: facility_data[:street_address_line_1],
+        phone: facility_data[:public_phone_number]})
+      end 
+    elsif data == DmvDataService.new.mo_dmv_office_locations
+      data.map do |facility_data|
+      facility = Facility.new({name: facility_data[:name],
+        address: facility_data[:address1],
+        phone: facility_data[:phone]})
+      end 
+    end 
+    
   end
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -12,7 +12,10 @@ RSpec.describe FacilityFactory do
     it 'can create a facility from provided data' do
       factory = FacilityFactory.new
       co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
-      expect(factory.create_facilities(co_dmv_office_locations)).to_not be nil
+      result = factory.create_facilities(co_dmv_office_locations)
+      expect(result.class).to eq(Array)
+      expect(result.first.class).to eq(Facility)
+      # require 'pry'; binding.pry
       #did not know how to write what to expect for the output.
     end
   end
@@ -23,10 +26,35 @@ RSpec.describe FacilityFactory do
       co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
       new_york_facilities = DmvDataService.new.ny_dmv_office_locations
       missouri_facilities = DmvDataService.new.mo_dmv_office_locations
-      expect(factory.create_facilities(co_dmv_office_locations)).to_not be nil
-      expect(factory.create_facilities(new_york_facilities)).to_not be nil
-      expect(factory.create_facilities(missouri_facilities)).to_not be nil
+      co_result = factory.create_facilities(co_dmv_office_locations)
+      ny_result = factory.create_facilities(new_york_facilities)
+      mo_result = factory.create_facilities(missouri_facilities)
+      expect(co_result.class).to eq(Array)
+      expect(co_result.first.class).to eq(Facility)
+      expect(ny_result.class).to eq(Array)
+      expect(ny_result.first.class).to eq(Facility)
+      expect(mo_result.class).to eq(Array)
+      expect(mo_result.first.class).to eq(Facility)
+
+      # expect(factory.create_facilities(new_york_facilities)).to_not be nil
+      # expect(factory.create_facilities(missouri_facilities)).to_not be nil
       #did not know how to write what to expect for the output.
     end
   end
+
+
+  # describe '#create facilities' do
+  #   it 'can create a facility from multiple provided datasets' do
+  #     co_dmv = FacilityFactory.new
+  #     ny_dmv = FacilityFactory.new
+  #     mo_dmv = FacilityFactory.new
+  #     co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+  #     new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+  #     missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+  #     expect(co_dmv.create_facilities(co_dmv_office_locations)).to_not be nil
+  #     expect(ny_dmv.create_facilities(new_york_facilities)).to_not be nil
+  #     expect(mo_dmv.create_facilities(missouri_facilities)).to_not be nil
+  #     #did not know how to write what to expect for the output.
+  #   end
+  # end
 end 
