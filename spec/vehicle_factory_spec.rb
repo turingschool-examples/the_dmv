bcca21bfc13447843a3aadb72a956642ffe2fd1a
Martin Chavez <martinchavez1203@gmail.com>
2023-09-03 16:28:47 -0600

    add VehicleFactory class

---
+++ spec/vehicle_factory_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+RSpec.describe VehicleFactory do 
+  before(:each) do 
+    @factory = VehicleFactory.new
+    @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+  end
+
+  describe "create_vehicles" do 
+    it "will create vehicle  objects with registration " do 
+    @factory.create_vehicles(wa_ev_registrations).each do |registration|
+      expect(@registration).to be_an_instance_of(vehicle)
+    end 
+    
+    
