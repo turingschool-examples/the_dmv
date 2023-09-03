796abe17555acbfa071a466ef60a0a365fa08ce7
Sooyung Kim <kimx1279@gmail.com>
2023-09-03 13:50:58 -0600

    Update to add create vehicle method and test

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,6 +1,16 @@
 class VehicleFactory
   def initalize
   end
-
   
+  def create_vehicle(registrations)
+    @create_vehicle = []
+    registrations.map do |registration|
+      # registration.transform_keys(:vin_1_10 vin:)
+      #  :model_year year:)
+      # binding.pry
+      # binding.pry
+      vehicle = Vehicle.new(registration)
+      @create_vehicle << vehicle
+    end
+  end
 end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -3,17 +3,18 @@ require 'spec_helper'
 RSpec.describe VehicleFactory do
   before(:each) do
     @factory = VehicleFactory.new
+   @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
   end
-
+  
   describe '#initialize' do
     it 'initialize' do
       expect(@factory).to be_an_instance_of(VehicleFactory)
     end
   end
-
+  
   describe '#create_vehicle' do
-    it 'creates vehicles' do
-      
+    it 'creates vehicles up to 1000' do
+      expect(@factory.create_vehicle(@wa_ev_registrations).length).to eq(1000)
     end
   end
 end
