21fd5cdb1ecb42f15bd7c8f71caa2bc2de9c9027
Miranda Delaney <delaney.miranda1@gmail.com>
2023-09-03 16:28:07 -0400

    fix: Changes unneccessary fixes

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,10 +1,15 @@
 class Registrant
-  attr_reader :name, :age, :permit, :license_data
+  attr_reader :name, 
+              :age, 
+              :permit, 
+              :license_data
   def initialize(name, age, permit = false)
     @name = name
     @age = age
     @permit = permit
-    @license_data = {written: false, license: false, renewed: false}
+    @license_data = {written: false,
+        license: false,
+        renewed: false}
   end
 
   def permit?
