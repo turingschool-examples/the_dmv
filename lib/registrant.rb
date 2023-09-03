89cae4045f744fd864894997cffa6c7bd526020c
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2023-09-03 12:43:29 -0400

    Added Test for earn_permit and permit? Added earn permit and permit? methods

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -13,6 +13,6 @@ attr_reader :name,
   end
 
   def permit?
-    @permit=true
+    @permit
   end
 end 
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -7,16 +7,18 @@ RSpec.describe Registrant do
   @registrant_2 = Registrant.new('Penny', 15 )
  end 
 
-  xit "has an attributes name" do 
+  it "has an attributes name" do 
     expect(@registrant_1.name).to eq("Bruce")
     expect(@registrant_1.age).to eq(18)
     expect(@registrant_1.permit?).to eq(true)
-    
+  end 
+
+  it "has attributes for registrant2" do
     expect(@registrant_2.name).to eq("Penny")
     expect(@registrant_2.age).to eq(15)
     expect(@registrant_2.permit?).to eq(false)
-    license_data {:written=>false, :license=>false, renewed=>false}
-    expect(@registrant_2.license_data).to eq(license_data)
+    # license_data= {:written=>false, :license=>false, renewed=>false}
+    # expect(@registrant_2.license_data).to eq(license_data)
     @registrant_2.earn_permit
     expect(@registrant_2.permit?).to eq(true)
   end 
