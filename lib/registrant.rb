fc72af735f28f12b8694a9e89cd77165719d3d97
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2023-09-03 12:29:37 -0400

    Added Test for Attribute names Added class attrubutes for Registrant Class

---
+++ lib/registrant.rb
@@ -0,0 +1,18 @@
+class Registrant
+attr_reader :name,
+            :age,
+            :permit
+  def initialize(name,age,permit=false)
+    @name = name 
+    @age = age 
+    @permit=permit
+  end 
+  
+  def earn_permit
+    @permit = true
+  end
+
+  def permit?
+    @permit=true
+  end
+end 
