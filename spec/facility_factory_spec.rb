e87a575cdccf0cee4be1fcbf1b7cc4e650c336f3
Scott DeVoss <scottdevoss@yahoo.com>
2023-09-03 08:43:13 -0600

    added a test for multiple provided datasets

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -16,4 +16,17 @@ RSpec.describe FacilityFactory do
       #did not know how to write what to expect for the output.
     end
   end
+
+  describe '#create facilities' do
+    it 'can create a facility from multiple provided datasets' do
+      factory = FacilityFactory.new
+      co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+      new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+      missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+      expect(factory.create_facilities(co_dmv_office_locations)).to_not be nil
+      expect(factory.create_facilities(new_york_facilities)).to_not be nil
+      expect(factory.create_facilities(missouri_facilities)).to_not be nil
+      #did not know how to write what to expect for the output.
+    end
+  end
 end 
