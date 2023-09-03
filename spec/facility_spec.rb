2466fc0e2a992f61612a0354d26195f1097bc5d0
Joseph Lee <jhjlee702@gmail.com>
2023-09-03 13:52:22 -0600

    reg_3 cannot get a permit

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -171,5 +171,13 @@ end
       expect(@facility_1.administer_written_test(@registrant_2)).to be true
       expect(@registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
     end
+
+    it 'checks reg_2 information and tries to administer a written test' do
+      expect(@registrant_3.age).to eq(15)
+      expect(@registrant_3.permit?).to be false
+      @facility_1.add_service('Written Test')
+      expect(@facility_1.administer_written_test(@registrant_3)).to be false
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
   end
 end
