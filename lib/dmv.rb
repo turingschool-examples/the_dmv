0a31edae8d73c449ebe7ecce9ad16f576b45c605
Sooyung Kim <kimx1279@gmail.com>
2023-09-04 10:29:51 -0600

    Add create facility helper method

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -1,5 +1,5 @@
 class Dmv
-  attr_reader :facilities
+  attr_reader :facilities, :list_of_offices
   def initialize
     @facilities = []
   end
@@ -13,4 +13,32 @@ class Dmv
       facility.services.include?(service)
     end
   end
+
+  def create_facility_helper(offices)
+    @list_of_offices = []
+    offices.map do |office|
+      address_data = office[:address_li], office[:address__1], office[:city], office[:state], office[:zip]
+      full_address = address_data.join(" ")
+      facility_details = { name: office[:dmv_office], address: full_address, phone: office[:phone]}
+      @list_of_offices << facility_details
+    end
+    @list_of_offices
+  end
+
+
+
+
+
+  # def create_facility(facilities)
+  #   new_facility = facilities.map do |facility|
+  #     if facility[:state] == 'CO'
+  #       facility_details = { name: facility[:dmv_office],
+  #         address: facility[:address_li, :address__1, :city ,:state ,:zip], phone: facility[:phone] }
+  #     end
+  #     facility = Facility.new(facility_details)
+  #   end
+
+  # end
 end
+
+# facility_details = {name: facility[:dvm_office], address: facility[:addres_li + :address__1 + :city + :state + :zip], phone: facility[:phone]}
