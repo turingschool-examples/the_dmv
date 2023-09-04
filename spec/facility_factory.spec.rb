6c20bd6380ae4e9c1d890794fc6045d15b96422e
Joseph Lee <jhjlee702@gmail.com>
2023-09-04 14:51:40 -0600

    Create NY facilities

--- spec/facility_factory.spec.rb
+++ spec/facility_factory.spec.rb
@@ -12,4 +12,16 @@ RSpec.describe Facility_Factory do
     end
     end
   end
+
+  describe '#NY_DMV facility data' do
+    it 'creates "NY facilities' do
+      facility = Facility_Factory.new
+      NY_facilities = DmvDataService.new.ny_dmv_office_locations
+
+    expect(facility.create_office_locations(NY_facilities)[0]).to be_an_instance_of (Facility)
+    facility.create_office_locations(NY_facilities).each do |data|
+      expect(data).to be_instance_of (Facility)
+    end
+    end
+  end
 end
