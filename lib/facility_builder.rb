acd2fdf0877fbbb765d5d03faa1813dc92a9b582
Xander Hendry <vhendry.coding@gmail.com>
2023-09-04 19:16:41 -0400

    Add facility_builder passing tests for all data sets

--- lib/facility_builder.rb
+++ lib/facility_builder.rb
@@ -1,24 +1,50 @@
 class FacilityBuilder
   attr_reader :facility_list
               :facility_details
-  def build_facility_helper(data)
-    @facility_list = data.map do |facility|
-      facility = facility.slice(:dmv_office, :address_li, :address_1, :city, :state, :zip, :phone)
-      full_address = []
-      filter = [:address_li, :address__1, :city, :state, :zip]
-      @facility_details = facility.map do |key, value|
-        if key == :dmv_office
-          key = :name, value = value
-        elsif filter.include?(key)
-          full_address << value
-          key = :address, value = full_address.join(' ')
-        else 
-          key = key, value = value
-        end
-      end.to_h
+  # def co_facility_helper(data)
+  #   @facility_list = data.map do |facility|
+  #     facility = facility.slice(:dmv_office, :address_li, :address_1, :city, :state, :zip, :phone)
+  #     full_address = []
+  #     filter = [:address_li, :address__1, :city, :state, :zip]
+  #     @facility_details = facility.map do |key, value|
+  #       if key == :dmv_office
+  #         key = :name, value = value
+  #       elsif filter.include?(key)
+  #         full_address << value
+  #         key = :address, value = full_address.join(' ')
+  #       else 
+  #         key = key, value = value
+  #       end
+  #     end.to_h
+  #   end
+  #   @facility_list
+  # end
+
+def build_facility_helper(data)
+  # binding.pry
+  @facility_list = data.map do |facility|
+    # binding.pry
+    if facility[:state] == 'CO'
+      full_address = [facility[:address_li], facility[:address__1], facility[:city], facility[:state], facility[:zip]]
+      # binding.pry
+      facility_details = { name: facility[:dmv_office], address: full_address.join(' '), phone: facility[:phone]}
+      # binding.pry
+    elsif facility[:state] == 'NY'
+      full_address = [facility[:street_address_line_1], facility[:city], facility[:state], facility[:zip_code]]
+      # binding.pry
+      facility_details = { name: facility[:office_name], address: full_address.join(' '), phone: facility[:public_phone_number]}
+      # binding.pry
+    elsif facility[:state] == 'MO'
+      full_address = [facility[:address1], facility[:city], facility[:state], facility[:zipcode]]
+      # binding.pry
+      facility_details = {name: facility[:name], address: full_address.join(' '), phone: facility[:phone]}
+      # binding.pry
+    else
+      'No format prepared'
     end
-    @facility_list
   end
+
+end
 
   def build_facility(data)
     self.build_facility_helper(data)
--- spec/facility_builder_spec.rb
+++ spec/facility_builder_spec.rb
@@ -15,31 +15,59 @@ RSpec.describe FacilityBuilder do
       @builder = FacilityBuilder.new
     end
     it 'extracts needed elements from the given data set' do
-      @facility_data = @builder.build_facility_helper(@data)
-      expect(@facility_data).to be_an_instance_of(Array)
-      expect(@facility_data.length).to eq(@data.length)
+      facility_data = @builder.build_facility_helper(@data)
+      expect(facility_data).to be_an_instance_of(Array)
+      expect(facility_data.length).to eq(@data.length)
     end
 
     it 'formats data to be used by Facility class' do
-      @facility_data = @builder.build_facility_helper(@data)
-      expect(@facility_data.first).to eq({:name=>"DMV Tremont Branch", :address=>"2855 Tremont Place Denver CO 80205", :phone=>"(720) 865-4600"})
+      facility_data = @builder.build_facility_helper(@data)
+      expect(facility_data.first).to eq({:address=>"2855 Tremont Place Suite 118 Denver CO 80205", :name=>"DMV Tremont Branch", :phone=>"(720) 865-4600"})
+    end
+
+    it 'will work with a data set from NY' do
+      new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+      facility_data = @builder.build_facility_helper(new_york_facilities)
+      expect(facility_data).to be_an_instance_of(Array)
+      expect(facility_data.length).to eq(new_york_facilities.length)
+      expect(facility_data.first).to eq({:name=>"EVANS", :address=>"6853 ERIE RD DERBY NY 14006", :phone=>"7168587450"})
+    end
+
+    it 'will work with a data set from MO' do
+      missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+      facility_data = @builder.build_facility_helper(missouri_facilities)
+      expect(facility_data).to be_an_instance_of(Array)
+      expect(facility_data.length).to eq(missouri_facilities.length)
+      expect(facility_data.first).to eq({:name=>"OAKVILLE", :address=>"3164 TELEGRAPH ROAD ST LOUIS MO 63125", :phone=>"(314) 887-1050"})
     end
   end
 
-  describe '@build facility' do
+  describe '#build facility' do
     before(:each) do
       @data = DmvDataService.new.co_dmv_office_locations
       @builder = FacilityBuilder.new
     end
     it 'Will create an array of facilities using a provided data set' do
-      @facilities = @builder.build_facility(@data)
-      expect(@facilities).to be_an_instance_of(Array)
-      expect(@facilities.length).to eq(@data.length)
+      facilities = @builder.build_facility(@data)
+      expect(facilities).to be_an_instance_of(Array)
+      expect(facilities.length).to eq(@data.length)
     end
     
     it 'Will create an instance of Facility class for each element from given data set' do
-      @facilities = @builder.build_facility(@data)
-      expect(@facilities.first).to be_an_instance_of(Facility)
+      facilities = @builder.build_facility(@data)
+      expect(facilities.first).to be_an_instance_of(Facility)
+    end
+
+    it 'Will work with other data sets' do
+      new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+      facilities = @builder.build_facility(new_york_facilities)
+      expect(facilities).to be_an_instance_of(Array)
+      expect(facilities.length).to eq(new_york_facilities.length)
+      missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+      facilities = @builder.build_facility(missouri_facilities)
+      expect(facilities).to be_an_instance_of(Array)
+      expect(facilities.length).to eq(missouri_facilities.length)
     end
   end
+
 end
