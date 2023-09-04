30397ff9128456282f17b099adc857a8aa606589
Kam Kennedy <kameronk013@gmail.com>
2023-09-04 13:24:37 -0600

    add ny funcitonality

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -19,4 +19,19 @@ def create_co_facilities
   facilities
 end
 
+def create_ny_facilities
+  facility_data = DmvDataService.new.ny_dmv_office_locations
+  facilities = []
+  facility_data.each do |facility|
+    facility_data = {
+      name:facility[:office_name],
+      address:"#{facility[:street_address_line_1]}, #{facility[:city]}, #{facility[:state]}, #{facility[:zip_code]}",
+      phone:facility[:public_phone_number],
+      services:[],
+      collected_fees:0,
+      registered_vehicles:[]}
+    facilities << Facility.new(facility_data)
+  end
+  facilities
+end
 end
