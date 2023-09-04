4466a44fb975a332adf7759e76f5b849bd4ff8db
Sooyung Kim <kimx1279@gmail.com>
2023-09-04 10:11:33 -0600

    Update to make create facility helper test

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -6,19 +6,17 @@ RSpec.describe Dmv do
     @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
     @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
     @facility_3 = Facility.new({name: 'DMV Northwest Branch', address: '3698 W. 44th Avenue Denver CO 80211', phone: '(720) 865-4600'})
-    @facility = Facility.new
     @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
-    @ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
-    @mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
-  end
 
+  end
+  
   describe '#initialize' do
     it 'can initialize' do
       expect(@dmv).to be_an_instance_of(Dmv)
       expect(@dmv.facilities).to eq([])
     end
   end
-
+  
   describe '#add facilities' do
     it 'can add available facilities' do
       expect(@dmv.facilities).to eq([])
@@ -26,7 +24,7 @@ RSpec.describe Dmv do
       expect(@dmv.facilities).to eq([@facility_1])
     end
   end
-
+  
   describe '#facilities_offering_service' do
     it 'can return list of facilities offering a specified Service' do
       @facility_1.add_service('New Drivers License')
@@ -36,18 +34,28 @@ RSpec.describe Dmv do
       @facility_2.add_service('Written Test')
       @facility_3.add_service('New Drivers License')
       @facility_3.add_service('Road Test')
-
+      
       @dmv.add_facility(@facility_1)
       @dmv.add_facility(@facility_2)
       @dmv.add_facility(@facility_3)
-
+      
       expect(@dmv.facilities_offering_service('Road Test')).to eq([@facility_2, @facility_3])
+    end
+  end
+  
+  describe '#create_facility_helper' do
+    it 'converts co facility information from API correct format' do
+      co_facility_info = @dmv.create_facility_helper(@co_dmv_office_locations)
+      expect(@dmv.create_facility_helper(@co_dmv_office_locations).class).to eq(Array)
+      expect(co_facility_info.first).to eq({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
     end
   end
 
   describe '#create_facility' do
-    it 'converts co facility information from API' do
-      co_facility_info = @facility.create_facility(@co_dmv_office_locations)
+    xit 'converts co facility information from API' do
+      
+      co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+      co_facility_info = @dmv.create_facility(co_dmv_office_locations)
       expect(co_facility_info.count).to eq(5)
       expect(co_facility_info.class).to eq(Array)
       expect(co_facility_info.first.class).to eq(Facility)
@@ -55,4 +63,8 @@ RSpec.describe Dmv do
       expect(co_facility_info.first.phone).to eq("(720) 865-4600")
     end
   end
+  
+  # ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
+  # mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
+  
 end
