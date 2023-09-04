43a78d3c8e8454b29e6e6b217ce4640c604a44d7
Miranda Delaney <delaney.miranda1@gmail.com>
2023-09-04 18:29:39 -0400

    fix: Add fixed create vehicles method and test

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -3,15 +3,10 @@ class VehicleFactory
     @factory_vehicles = []
   end
 
-  def create_vehicles(vehicle_data)
-    vehicle_data.each do |data|
-      
-      vin = data[:vin_1_10]
-      make = data[:make]
-      model = data[:model]
-      year = data[:model_year]
-      engine = :ev
-      vehicle = Vehicle.new(vin: vin, make: make, model: model, year: year, engine: engine)
+  def create_vehicles(api_data)
+    api_data.each do |data|
+      vehicle_data = { vin: data[:vin_1_10], make: data[:make], model: data[:model], year: data[:model_year], engine: 'ev' }
+      vehicle = Vehicle.new(vehicle_data)
       @factory_vehicles << vehicle
     end
     @factory_vehicles
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 RSpec.describe VehicleFactory do
   before(:each) do
     @factory = VehicleFactory.new
-    DmvDataService.new.wa_ev_registrations
-
+    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
   end
 
   describe '#initialize' do
@@ -14,13 +13,21 @@ RSpec.describe VehicleFactory do
   end
 
   describe '#create_vehicles' do
-    it 'creates Vehicle objects with attributes' do
-      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
-      vehicle_data = { vin_1_10: ['JTDKN3DP8D'], make: ['TOYOTA'], 
-        model: ['Prius Plug-in'], model_year: ['2013'], engine: ['ev'] }
-      factory_vehicles = @factory.create_vehicles(wa_ev_registrations)
-      expect(factory_vehicles.length).to eq(wa_ev_registrations.length)
-     
+    it 'creates Vehicle objects with given attributes' do
+      # wa_ev_registrations = [ { :vin_1_10 => 'JTDKN3DP8D', :make => 'TOYOTA', 
+      #   :model => 'Prius Plug-in', :model_year => '2013', :engine => 'ev' }, 
+      #   { :vin_1_10 => '1G1RD6E47D', :make => 'CHEVROLET', :model => 'Volt', 
+      #   :model_year => '2013', :engine => 'ev' } ]
+        wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+        vehicles = @factory.create_vehicles(wa_ev_registrations)
+        vehicles.each do |car|
+          require 'pry'; binding.pry
+          expect(car).to be_a(Vehicle)
+          expect(car.vin).to_not eq(nil)
+          
+        end
+      
+      
     end
   end
 end
