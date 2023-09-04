885008ecd8183a7263e83d004e1bcc47a7e65565
Joseph Lee <jhjlee702@gmail.com>
2023-09-03 19:57:41 -0600

    create WA vehicles from external data source

---
+++ lib/vehicle_factory.rb
@@ -0,0 +1,16 @@
+class VehicleFactory
+
+  def initialize
+  end
+
+  def create_vehicles(registration)
+    vehicles = registration.map do |registration|
+      vin = registration[:vin_1_10]
+      year = registration[:model_year]
+      make = registration[:make]
+      model = registration[:model]
+
+      vehicle = Vehicle.new(vin: vin, year: year, make: make, model: model, engine: :ev)
+    end
+  end
+end
