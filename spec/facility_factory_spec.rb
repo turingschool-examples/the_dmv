f6feb5ed58e5f7366584304ac738a69543a1fd68
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-09-04 15:08:18 -0600

    Clean up facility factory spec file

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -16,7 +16,6 @@ RSpec.describe do
       factory = FacilityFactory.new
       co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
       factory.create_facilities(co_dmv_office_locations)
-      # require "pry"; binding.pry
 
       expect(factory.facilities.count).to be(5)
     end
@@ -25,7 +24,7 @@ RSpec.describe do
       factory = FacilityFactory.new
       co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
       factory.create_facilities(co_dmv_office_locations)
-      #require "pry"; binding.pry
+      
       expect(factory.facilities[0].services.count).to eq(4)
       expect(factory.facilities[1].services.count).to eq(4)
       expect(factory.facilities[2].services.count).to eq(4)
@@ -36,7 +35,6 @@ RSpec.describe do
       factory = FacilityFactory.new
       ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
       factory.create_facilities(ny_dmv_office_locations)
-      #require "pry"; binding.pry
 
       expect(factory.facilities.count).to eq(172)
     end
@@ -45,9 +43,10 @@ RSpec.describe do
       factory = FacilityFactory.new
       mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
       factory.create_facilities(mo_dmv_office_locations)
-      #require "pry"; binding.pry
 
       expect(factory.facilities.count).to eq(178)
     end
+
   end
+  
 end
