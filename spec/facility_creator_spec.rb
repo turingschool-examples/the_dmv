6e7f7f0e3c3392b56234658c4cb82c9056fd4258
Anthea Yur <acyur6@gmail.com>
2023-09-04 01:07:00 -0500

    Completed RSPec, now refactoring NYC number

--- spec/facility_creator_spec.rb
+++ spec/facility_creator_spec.rb
@@ -2,16 +2,54 @@ require 'spec_helper'
 require './lib/vehicle'
 require './lib/vehicle_factory'
 require './lib/dmv_data_service'
+require './lib/facility_creator'
 
-creator = FacilityCreator.new
-co_dmv_registrations = [] 
-co_dmv_registrations << DmvDataService.new.co_dmv_office_locations[0]
-co_dmv_registrations << DmvDataService.new.co_dmv_office_locations[1]
-co_dmv_registrations << DmvDataService.new.co_dmv_office_locations[2]
-check_results = creator.create_facility(co_dmv_registrations)
+RSpec.describe FacilityCreator do
 
-creator = FacilityCreator.new
-co_dmv_registrations = DmvDataService.new.co_dmv_office_locations
-co_dmv_registrations << DmvDataService.new.co_dmv_office_locations[1]
-co_dmv_registrations << DmvDataService.new.co_dmv_office_locations[2]
-check_results << creator.create_facility(co_dmv_registrations)
+  before(:each) do
+    @creator = FacilityCreator.new
+    @co_dmv_registrations = [] 
+    @co_dmv_registrations << DmvDataService.new.co_dmv_office_locations[0]
+    @co_dmv_registrations << DmvDataService.new.co_dmv_office_locations[1]
+    @co_dmv_registrations << DmvDataService.new.co_dmv_office_locations[2]
+    @check_results = @creator.create_facility(@co_dmv_registrations)
+    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+  end 
+
+  it 'can take Colorado information and return new facilities per Facility class' do
+    expect(@check_results.class).to eq(Array)
+    expect(@check_results[0].class).to be(Facility)
+  end
+
+  it 'should return initial values of empty arrays and 0 collected fees per each facility' do
+    expect(@check_results[0].collected_fees).to eq(0)
+    expect(@check_results[0].registered_vehicles).to eq([])
+  end
+
+  it 'can add a service and register a vehicle, changing the collected fees' do
+    @check_results[0].add_service("Vehicle Registration")
+    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    @check_results[0].register_vehicle(@cruz)
+    expect(@check_results[0].collected_fees).to eq(100)
+    expect(@check_results[0].registered_vehicles.count).to be(1)
+  end
+
+  it 'can accept information from New York submissions' do
+    new_york_facilities = []
+    new_york_facilities << DmvDataService.new.ny_dmv_office_locations[0]
+    @check_results << @creator.create_facility(new_york_facilities)
+    expect(DmvDataService.new.ny_dmv_office_locations[0].class).to be(Hash)
+    expect(@check_results[3].to_s).to include("NY")
+    expect(@check_results[3].class).to be(Array)
+  end
+
+  it 'can accept information from Missouri submissions' do
+    missouri_facilities = []
+    missouri_facilities <<  DmvDataService.new.mo_dmv_office_locations[0]
+    @check_results << @creator.create_facility(missouri_facilities) 
+    expect(DmvDataService.new.mo_dmv_office_locations[0].class).to be(Hash)
+    expect(@check_results[3].to_s).to include("MO")
+    expect(@check_results[3].class).to be(Array)
+  end
+
+end
