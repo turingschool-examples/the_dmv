e3e3d66290b4dd5b104aeb7407888a7c6f23c81f
Joseph Lee <jhjlee702@gmail.com>
2023-09-04 15:30:43 -0600

    clean up

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -7,13 +7,12 @@ RSpec.describe VehicleFactory do
     @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
   end
 
-  describe '#create_vehicles' do
+  describe '#create_WA_EV_vehicles' do
     it 'will create Vehicle objects using a registration dataset' do
       @factory.create_vehicles(@wa_ev_registrations).each do |registration|
         expect(registration).to be_an_instance_of(Vehicle)
+        expect(registration.engine).to eq(:ev)
       end
     end
-
-    #what tests do I write here?
   end
 end
