b6fb7230d514f084dcddfc9283a992bb28e4b66a
Miranda Delaney <delaney.miranda1@gmail.com>
2023-09-04 22:49:24 -0400

    feat: Add tests to create objects from multiple data sources

--- spec/facility_office_spec.rb
+++ spec/facility_office_spec.rb
@@ -3,19 +3,34 @@ require 'spec_helper'
 
 RSpec.describe FacilityOffice do
   before(:each) do
-    @office = FacilityOffice.new(@co_dmv_office_locations)
+    @co_office = FacilityOffice.new(@co_dmv_office_locations)
+    @ny_office = FacilityOffice.new(@ny_dmv_office_locations)
+    @mo_office = FacilityOffice.new(@mo_dmv_office_locations)
     @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+    @ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
+    @mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
   end
 
   describe '#create_offices' do
-    it 'creates Facility objects with given attributes' do
-  
-     @office_list = @office.create_offices(@co_dmv_office_locations)
+    it 'creates Facility objects with given attributes from Colorado data source' do
+      @office_list = @office.create_offices(@co_dmv_office_locations)
       
       expect(@office_list.length).to eq(@co_dmv_office_locations.length)
       expect(@office_list.first).to be_a(FacilityOffice)
     end
+
+    it 'creates Facility objects with given attributes from New York data source' do
+      @office_list = @office.create_offices(@ny_dmv_office_locations)
+
+      expect(@office_list.length).to eq(@ny_dmv_office_locations.length)
+      expect(@office_list.first).to be_a(FacilityOffice)
+    end
+
+    it 'creates Facility objects with given attributes from Missouri data source' do
+      @office_list = @office.create_offices(@mo_dmv_office_locations)
+
+      expect(@office_list.length).to eq(@mo_dmv_office_locations.length)
+      expect(@office_list.first).to be_a(FacilityOffice)
+    end
   end
 end
-
-# vin: data[:vin_1_10], make: data[:make], model: data[:model], year: data[:model_year], engine: 'ev'
