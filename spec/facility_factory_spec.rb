df8b9f7392dc5651ad1228ccfbc342e73e212448
Martin Chavez <martinchavez1203@gmail.com>
2023-09-05 14:41:03 -0600

    Add for co

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe FacilityFactory do
     it "will create vehicle objects with registration " do 
      facility = FacilityFactory.new
      colorado_facilities = DmvDataService.new.co_dmv_office_locations
+     expect(facility.create_facility(colorado_facilities)[0]).to be_an_instance_of Facility
+     expect(facility.create_facility(colorado_facilities)[3]).to be_an_instance_of Facility
 
       
       facility.create_facility(colorado_facilities).each do |data|
-       
       expect(data).to be_an_instance_of(Facility)
       end
     end
