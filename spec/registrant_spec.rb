60df710332b792e8ebed08534a9f3e471118eae3
Eric Belongea <eric.belongea@gmail.com>
2023-09-05 12:44:56 -0600

    Updates registrant spec

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,43 +1,36 @@
 require 'spec_helper'
 
 RSpec.describe Registrant do
+  before(:each) do
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_2 = Registrant.new('Penny', 15 )
+  end
   describe '#registrant' do
     it 'exists' do
-      registrant_1 = Registrant.new('Bruce', 18, true )
-      registrant_2 = Registrant.new('Penny', 15 )
-
-      expect(registrant_1).to be_an_instance_of(Registrant)
+      expect(@registrant_1).to be_an_instance_of(Registrant)
     end
 
     it 'registrant_1 has attribute data' do
-      registrant_1 = Registrant.new('Bruce', 18, true )
-      registrant_2 = Registrant.new('Penny', 15 )
-  
-      expect(registrant_1.name).to eq('Bruce')
-      expect(registrant_1.age).to eq(18)
-      expect(registrant_1.permit?).to be true
-      expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@registrant_1.name).to eq('Bruce')
+      expect(@registrant_1.age).to eq(18)
+      expect(@registrant_1.permit?).to be true
+      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
   
     it 'registrant_2 has attribute data' do
-      registrant_1 = Registrant.new('Bruce', 18, true )
-      registrant_2 = Registrant.new('Penny', 15 )
-  
-      expect(registrant_2.name).to eq('Penny')
-      expect(registrant_2.age).to eq(15)
-      expect(registrant_2.permit?).to be false
-      expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@registrant_2.name).to eq('Penny')
+      expect(@registrant_2.age).to eq(15)
+      expect(@registrant_2.permit?).to be false
+      expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
 
     it 'can earn a permit' do
-      registrant_2 = Registrant.new('Penny', 15 )
-  
-      expect(registrant_2.permit?).to be false
-      expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@registrant_2.permit?).to be false
+      expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
       
-      registrant_2.earn_permit
+      @registrant_2.earn_permit
 
-      expect(registrant_2.permit?).to be true
+      expect(@registrant_2.permit?).to be true
     end
   end
 end
