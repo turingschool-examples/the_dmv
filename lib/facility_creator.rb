56c4d2dc9cb756d6a72f90980c17e29034fd91fe
Billy Burch <billyburch40@gmail.com>
2023-09-05 16:04:14 -0600

    add: ny facility method

--- lib/facility_creator.rb
+++ lib/facility_creator.rb
@@ -5,8 +5,8 @@ class FacilityCreator
     @new_facilities = []
   end
 
-  def create_facilities(remote_data)
-       remote_data.each do |data|
+  def create_co_facilities(co_remote_data)
+    co_remote_data.each do |data|
       facility = Facility.new({
         name:data[:dmv_office],
         address:"#{data[:address_li]}, #{data[:address__1]}, #{data[:city]}, #{data[:state]}, #{data[:zip]}",
@@ -18,5 +18,33 @@ class FacilityCreator
     end
     @new_facilities
   end
+
+  def create_ny_facilities(ny_remote_data)
+    ny_remote_data.each do |data|
+      facility = Facility.new({
+        name:data[:office_name],
+        address:"#{data[:street_address_line_1]}, #{data[:city]}, #{data[:state]}, #{data[:zip_code]}",
+        phone:data[:public_phone_number],
+        services:[],
+        collected_fees:0,
+        registered_vehicles:[]})
+      @new_facilities << facility
+    end
+    @new_facilities
+  end
+
+  # def create_mo_facilities(mo_remote_data)
+  #   mo_remote_data.each do |data|
+  #     facility = Facility.new({
+  #       name:data[:office_name],
+  #       address:"#{data[:street_address_line_1]}, #{data[:city]}, #{data[:state]}, #{data[:zip_code]}",
+  #       phone:data[:public_phone_number],
+  #       services:[],
+  #       collected_fees:0,
+  #       registered_vehicles:[]})
+  #     @new_facilities << facility
+  #   end
+  #   @new_facilities
+  # end
 end
 
