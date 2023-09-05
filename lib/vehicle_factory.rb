8c2f2abf8b11c527a8aaaa7542883cf89357468c
Heather McNeil <hd_mcneil@yahoo.com>
2023-09-05 11:20:17 -0700

    Debug create_vehicles method

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,9 +1,11 @@
 class VehicleFactory
   attr_reader :factory_vehicles,
-              :vehicle_data           
+              :vehicle_data,
+              :api_data
 
   def initialize(engine = :ev)
     @factory_vehicles = []
+    @api_data = DmvDataService.new.wa_ev_registrations
     @vehicle_data = {
       vin: nil,
       make: nil,
@@ -15,7 +17,13 @@ class VehicleFactory
 
   def create_vehicles(api_data)
     api_data.each do |data|
-      vehicle = Vehicle.new(data)
+      vehicle = Vehicle.new(
+        vin: data[:vin_1_10],
+        make: data[:make],
+        model: data[:model],
+        year: data[:model_year],
+        engine: @vehicle_data[:engine]
+      )
       @factory_vehicles << vehicle
     end
   end
