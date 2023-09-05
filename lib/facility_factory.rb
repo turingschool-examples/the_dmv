6939aef5d4ff28da7aacaacaea34141a305da8d0
Justin Lee <justinslee201100@yahoo.com>
2023-09-05 12:11:15 -0600

    Fix facility factory

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -5,12 +5,27 @@ class FacilityFactory
 
   def create_facility(api_data)
     facilities = api_data.map do |data|
-    Facility.new(
-    name: data[:dmv_office], 
-    address: "#{data[:address_li]} #{data[:address__l]}
-              #{data[:city]} #{data[:state]} #{data[:zip]}",
-    phone: data[:phone] 
-    )
+      if data[:dmv_office] != nil
+      Facility.new(
+      name: data[:dmv_office], 
+      address: "#{data[:address_li]} #{data[:address__l]} #{data[:city]} #{data[:state]} #{data[:zip]}",
+      phone: data[:phone] 
+      )
+      # NY
+      elsif data[:office_name] != nil
+        Facility.new(
+      name: data[:office_name], 
+      address: "#{data[:street_address_line_1]} #{data[:street_addrass_line_2]} #{data[:city]} #{data[:state]} #{data[:zip]}",
+      phone: data[:public_phone_number] 
+      )
+      # MO
+      else data[:address1] != nil
+        Facility.new(
+      name: data[:name], 
+      address: "#{data[:address1]} #{data[:city]} #{data[:state]} #{data[:zip]}",
+      phone: data[:phone] 
+      )
+      end
     end
   end
 end
