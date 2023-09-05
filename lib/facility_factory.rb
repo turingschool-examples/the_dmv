ddee148ce62c055f31f4c9d04b3d4ab93d7b27b0
Joseph Lee <jhjlee702@gmail.com>
2023-09-05 13:07:28 -0600

    create facility for each state

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -4,12 +4,28 @@ class Facility_Factory
   end
 
   def create_office_locations(locations)
-    locations = locations.map do |facility|
-      Facility.new(
-      name: facility[:dmv_office],
-      address: "#{facility[:address_li]} #{facility[:address_l]} #{facility[:city]} #{facility[:state]} #{facility[:zip]}",
-      phone: facility[:phone]
-      )
+    facilities = locations.map do |facility|
+      #CO
+      if facility[:dmv_office] != nil
+        Facility.new(
+          name: facility[:dmv_office],
+          address: "#{facility[:address_li]} #{facility[:address_l]} #{facility[:city]} #{facility[:state]} #{facility[:zip]}",
+          phone: facility[:phone]
+        )
+        #NY
+      elsif facility[:office_name] != nil
+        Facility.new(
+          name: facility[:office_name],
+          address: "#{facility[:street_address_line_1]} #{facility[:street_address_line_2]} #{facility[:city]} #{facility[:state]} #{facility[:zip_code]}",
+          phone: facility[:public_phone_number]
+        )
+      else facility[:name] != nil
+        Facility.new(
+          name: facility[:name],
+          address: "#{facility[:address1]} #{facility[:city]} #{facility[:state]} #{facility[:zipcode]}",
+          phone: facility[:phone]
+        )
+      end
     end
   end
 end
