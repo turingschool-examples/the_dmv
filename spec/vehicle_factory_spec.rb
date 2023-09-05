819681f4f539ad569d83507ee2a07f9978a9b778
Heather McNeil <hd_mcneil@yahoo.com>
2023-09-05 11:21:35 -0700

    Correct vehicle_factory test given debugging changes to create_vehicle method

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -18,21 +18,19 @@ RSpec.describe VehicleFactory do
   it 'creates vehicles' do
     @factory.create_vehicles(@api_data)
 
-    @factory.create_vehicles(api_data)
-
     vehicles = @factory.factory_vehicles
-      expect(vehicles.size).to eq(2)
+      expect(vehicles.size).to eq(1000)
 
       vehicle_1 = vehicles[0]
-      expect(vehicle_1.vin).to eq('12345')
-      expect(vehicle_1.make).to eq('Toyota')
-      expect(vehicle_1.model).to eq('Prius Plug-in')
-      expect(vehicle_1.year).to eq(2013)
+      expect(vehicle_1.vin).to eq('1N4AZ0CPXF')
+      expect(vehicle_1.make).to eq('NISSAN')
+      expect(vehicle_1.model).to eq('Leaf')
+      expect(vehicle_1.year).to eq("2015")
 
       vehicle_2 = vehicles[1]
-      expect(vehicle_2.vin).to eq('67890')
-      expect(vehicle_2.make).to eq('Nissan')
-      expect(vehicle_2.model).to eq('Leaf')
-      expect(vehicle_2.year).to eq(2018)
+      expect(vehicle_2.vin).to eq('5YJ3E1EB6K')
+      expect(vehicle_2.make).to eq('TESLA')
+      expect(vehicle_2.model).to eq('Model 3')
+      expect(vehicle_2.year).to eq("2019")  
   end
 end
