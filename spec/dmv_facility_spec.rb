2ec6d500cb4a9ad1d2ed56d363709f975e024385
Heather McNeil <hd_mcneil@yahoo.com>
2023-09-05 16:49:09 -0700

    Add testing for multiple state data sets

--- spec/dmv_facility_spec.rb
+++ spec/dmv_facility_spec.rb
@@ -3,16 +3,16 @@ require './lib/dmv_facility'
 
 RSpec.describe DmvFacility do
   before :each do
-    @dmv = DmvFacility.new
+    @dmv = DmvFacility.new(:co)
     @dmv_data_service = DmvDataService.new
     @dmv_api_data = @dmv_data_service.co_dmv_office_locations
   end
 
   it 'is instance object of DmvFacility' do
-    expect(@dmv = DmvFacility.new).to be_an_instance_of(DmvFacility)
+    expect(@dmv = DmvFacility.new(:co)).to be_an_instance_of(DmvFacility)
   end
 
-  it 'creates dmv locations in CO' do
+  it 'creates dmv locations for CO' do
     @dmv.create_dmv_facility
     dmvs = @dmv.dmv_office_locations
     expect(dmvs.size).to eq(5)
@@ -48,5 +48,33 @@ RSpec.describe DmvFacility do
     expect(dmv_4.phone).to eq("(720) 865-4600")
     expect(dmv_5.services).to eq("vehicle titles, registration, renewals;  VIN inspections")
   end
+end
+
+RSpec.describe DmvFacility do
+  before :each do
+    @co_dmv = DmvFacility.new(:co)
+    @ny_dmv = DmvFacility.new(:ny)
+    @mo_dmv = DmvFacility.new(:mo)
+  end
+
+  it 'is an instance object of DmvFacility' do
+    expect(@co_dmv = DmvFacility.new(:co)).to be_an_instance_of(DmvFacility)
+    expect(@ny_dmv = DmvFacility.new(:ny)).to be_an_instance_of(DmvFacility)
+    expect(@mo_dmv = DmvFacility.new(:mo)).to be_an_instance_of(DmvFacility)
+  end
+
+  it 'can pull data for New York state' do
+    @ny_dmv.create_dmv_facility
+    ny_dmvs = @ny_dmv.dmv_office_locations
+    expect(ny_dmvs.size).to eq(5)
+
+    ny_1 = ny_dmvs[0]
+    ny_2 = ny_dmvs[1]
+    ny_3 = ny_dmvs[2]
+    ny_4 = ny_dmvs[3]
+    ny_5 = ny_dmvs[4]
+
+  end
+
 
 end
