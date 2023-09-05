61a931d4e782299467b6fd7905a7a051f075b61b
Blaine Kennedy <blainekennedy@gmail.com>
2023-09-04 21:49:49 -0500

    test: add test and refactor to accept multiple sataes

--- spec/facility_location_spec.rb
+++ spec/facility_location_spec.rb
@@ -6,14 +6,25 @@ require'./lib/facility_location'
 
 RSpec.describe FacilityLocation do
   before(:each) do
-    @office = FacilityLocation.new(@co_dmv_office_locations)
+    @co_office = FacilityLocation.new(@co_dmv_office_locations)
+    @ny_office = FacilityLocation.new(@ny_dmv_office_locations)
+    @mo_office = FacilityLocation.new(@mo_dmv_office_locations)
     @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+    @ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
+    @mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
   end
   describe '#create_offices' do
     it 'creates Facility Location objects from API data' do
-      @office_list = @office.create_offices(@co_dmv_office_locations)
+      @office_list = @co_office.create_offices(@co_dmv_office_locations)
       
       expect(@office_list.length).to eq(@co_dmv_office_locations.length)
+      expect(@office_list.first).to be_a(FacilityLocation)
+      
+    end
+     it 'creates Facility Location objects from API data from multiple cities' do
+      @office_list = @ny_office.create_offices(@ny_dmv_office_locations)
+      
+      expect(@office_list.length).to eq(@ny_dmv_office_locations.length)
       expect(@office_list.first).to be_a(FacilityLocation)
       
     end
