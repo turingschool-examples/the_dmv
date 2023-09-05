e4dc4eb9f329ea1f354766bca648f86a398dce42
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-09-05 14:16:39 -0600

    Pass vehicle factory test with ny data set

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -10,11 +10,32 @@ class Factory
   end
 
   def parse(api_hash)
+    if api_hash[:state_of_residence] == "WA"
+      wa_parse(api_hash)
+    else
+      ny_parse(api_hash)
+    end
+  end
+
+  def wa_parse(api_hash)
     api_hash[:year] = api_hash[:model_year]
     api_hash[:engine] = :ev
     api_hash[:vin] = api_hash[:vin_1_10]
     api_hash
   end
+
+  def ny_parse(api_hash)
+    api_hash[:year] = api_hash[:model_year]
+    if api_hash[:fuel_type] != nil
+      api_hash[:engine] = api_hash[:fuel_type]
+    else
+      api_hash[:engine] = "NONE"
+    end
+    api_hash[:vin] = api_hash[:vin]
+    api_hash[:model] = api_hash[:body_type]
+    api_hash
+  end
+
 
 
 end
