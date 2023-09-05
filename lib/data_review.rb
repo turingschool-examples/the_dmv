65b1affe6639583cc193376ae9b72cf927ff6c14
Anthea Yur <acyur6@gmail.com>
2023-09-05 14:23:21 -0500

    Included new count by model year and most ppopular by make

--- lib/data_review.rb
+++ lib/data_review.rb
@@ -19,11 +19,28 @@ class DataReview
   end
 
   def find_popular_by_model
-    
+    models = {}
+    facility.registered_vehicles.each do |vehicle|
+      if models.keys.include?(vehicle.model)
+        models[vehicle.model]+=1
+      else
+        models.merge!({vehicle.model => 1})
+      end
+    end
+    models = models.sort_by{|key, value| -value}.to_h
+    models.first[0].capitalize + " is the most popular make at the #{@facility.name}"
   end
 
-  def registered_vehicle_count_by_model
-    #Similar approach ising hashes
+  def registered_vehicle_count_by_model_year(year)
+    model_years = {}
+    facility.registered_vehicles.each do |vehicle|
+      if model_years.keys.include?(vehicle.year)
+        model_years[vehicle.year]+=1
+      else
+        model_years.merge!({vehicle.year => 1})
+      end
+    end
+    model_years[year.to_s].to_s + " registered vehicle per the model year of #{year.to_s}"
   end
 end
 
