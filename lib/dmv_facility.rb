ff50630ab612584c75038f9f0e6b5383d8557dfd
Heather McNeil <hd_mcneil@yahoo.com>
2023-09-05 16:48:19 -0700

    Add multiple state data sets

--- lib/dmv_facility.rb
+++ lib/dmv_facility.rb
@@ -1,3 +1,9 @@
+DATA_SOURCES = {
+  co: { name:}
+  ny: { }
+  mo: { }
+}
+
 class DmvLocation
   attr_accessor :dmv_office, 
                 :address, 
@@ -25,11 +31,22 @@ end
 class DmvFacility
   attr_reader :dmv_api_data,
               :dmv_office_locations
+              
 
-  def initialize
+  def initialize(data_source)
     @dmv_office_locations = []
     @dmv_data_service = DmvDataService.new
-    @dmv_api_data = @dmv_data_service.co_dmv_office_locations
+
+    case data_source
+    when :co
+      @dmv_api_data = @dmv_data_service.co_dmv_office_locations
+    when :ny
+      @dmv_api_data = @dmv_data_service.ny_dmv_office_locations
+    when :mo
+      @dmv_api_data = @dmv_data_service.mo_dmv_office_locations
+    else
+      return "Invalid data source"
+    end
   end
 
   def create_dmv_facility
