ca3a697804bc2ad4ff18c1d601091fa483d2ded1
Heather McNeil <hd_mcneil@yahoo.com>
2023-09-05 14:26:25 -0700

    Add create_dmv_method in DMVFacility class and refactor

--- lib/dmv_facility.rb
+++ lib/dmv_facility.rb
@@ -1,24 +1,41 @@
+class DmvLocation
+  attr_accessor :dmv_office, 
+                :address, 
+                :address_suite, 
+                :city, 
+                :state, 
+                :zip, 
+                :phone, 
+                :hours, 
+                :services
+
+  def initialize(data)
+    @dmv_office = data[:dmv_office]
+    @address = data[:address_li]
+    @address_suite = data[:address__1]
+    @city = data[:city]
+    @state = data[:state]
+    @zip = data[:zip]
+    @phone = data[:phone]
+    @hours = data[:hours]
+    @services = data[:services_p]
+  end
+end
+
 class DmvFacility
-  attr_reader :dmv_api_data
+  attr_reader :dmv_api_data,
+              :dmv_office_locations
 
   def initialize
     @dmv_office_locations = []
-    @dmv_api_data = DmvDataService.new.co_dmv_office_locations
-    @dmv_location_data = {
-      dmv_office: nil,
-      address: nil,
-      address_suite: nil,
-      city: nil,
-      state: nil,
-      zip: nil,
-      phone: nil,
-      hours: nil,
-      services: nil
-    }
+    @dmv_data_service = DmvDataService.new
+    @dmv_api_data = @dmv_data_service.co_dmv_office_locations
   end
 
-  def create_dmv_facility(dmv_api_data)
+  def create_dmv_facility
     dmv_api_data.each do |data|
-
-
+      dmv_location = DmvLocation.new(data)
+      @dmv_office_locations << dmv_location
+    end
+  end
 end
