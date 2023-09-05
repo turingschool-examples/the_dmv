0d8cde406bd3eddc2a8aa1f7c526ae30b6b4d175
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-09-05 13:54:45 -0600

    Small formatting changes

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Registrant do
     it 'can initialize' do
       registrant_1 = Registrant.new('Bruce', 18, true )
       registrant_2 = Registrant.new('Penny', 15 )
-      
       expect(registrant_1).to be_an_instance_of(Registrant)
       expect(registrant_2).to be_an_instance_of(Registrant)
     end
@@ -14,7 +13,6 @@ RSpec.describe Registrant do
     it 'has a name' do
       registrant_1 = Registrant.new('Bruce', 18, true )
       registrant_2 = Registrant.new('Penny', 15 )
-      
       expect(registrant_1.name).to eq('Bruce')
       expect(registrant_2.name).to eq('Penny')
     end
@@ -22,7 +20,6 @@ RSpec.describe Registrant do
     it 'has an age' do
       registrant_1 = Registrant.new('Bruce', 18, true )
       registrant_2 = Registrant.new('Penny', 15 )
-
       expect(registrant_1.age).to eq(18)
       expect(registrant_2.age).to eq(15)
     end
@@ -40,13 +37,12 @@ RSpec.describe Registrant do
       expect(registrant_1.license_data).to be_an_instance_of(Hash)
       expect(registrant_2.license_data).to be_an_instance_of(Hash)
     end
+
     it 'can earn a permit' do
-      
       registrant_2 = Registrant.new('Penny', 15 )
       expect(registrant_2.permit).to eq(false)
       registrant_2.earn_permit
       expect(registrant_2.permit).to eq(true)
-
     end
   end
 end
