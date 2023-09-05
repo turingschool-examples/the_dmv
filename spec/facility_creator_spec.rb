aaf6d91c65f2eb660e900abbd0c90ddc339d1130
Billy Burch <billyburch40@gmail.com>
2023-09-05 16:04:48 -0600

    add: ny facility test

--- spec/facility_creator_spec.rb
+++ spec/facility_creator_spec.rb
@@ -15,12 +15,31 @@ RSpec.describe FacilityCreator do
       expect(co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations).to be_a(Array)
     end
 
-    it 'returns facilities' do
+    it 'returns CO facilities' do
       creator = FacilityCreator.new
       co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
-      new_facilities = creator.create_facilities(co_dmv_office_locations)
+      new_facilities = creator.create_co_facilities(co_dmv_office_locations)
       expect(new_facilities).to be_a(Array)
       expect(new_facilities).to all be_a(Facility)
     end
+  end
+
+  describe '#multiple data sources' do
+    it 'returns NY facilities' do
+      creator = FacilityCreator.new
+      new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+      new_facilities = creator.create_ny_facilities(new_york_facilities)
+      expect(new_facilities).to be_a(Array)
+      expect(new_facilities).to all be_a(Facility)
+    end
+    
+    # it 'returns MO facilities' do
+    #   creator = FacilityCreator.new
+    #   missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+    #   new_facilities = creator.create_mo_facilities(missouri_facilities)
+    #   pry
+    #   expect(new_facilities).to be_a(Array)
+    #   expect(new_facilities).to all be_a(Facility)
+    # end
   end
 end
