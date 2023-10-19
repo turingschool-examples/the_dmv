947a13e8dfaff16be9598d0fe995841fc2d2aff5
Meg Roth <megroth2@gmail.com>
2023-10-19 17:28:06 -0600

    Add shell for vehicle factory spec

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -3,11 +3,13 @@ require 'spec_helper'
 RSpec.describe VehicleFactory do
 
   it 'exists' do
+    factory = VehicleFactory.new
 
+    expect(factory).to be_instance_of(VehicleFactory)
   end
 
   it 'creates vehicles' do
-    
+
   end
 
 end
