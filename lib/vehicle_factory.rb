284d5e2e89ac739f333cbdeb8fc1a22cfdebc174
Meg Roth <megroth2@gmail.com>
2023-10-19 17:30:21 -0600

    Add shell for vehicle factory class

---
+++ lib/vehicle_factory.rb
@@ -0,0 +1,17 @@
+require '/lib/dmv'
+require '/lib/facility'
+require '/lib/vehicle'
+require '/lib/dmv_data_service'
+require '/lib/registrant'
+
+Class VehicleFactory
+
+  def initialize
+
+  end
+
+  def create_vehicles
+  
+  end
+
+end
