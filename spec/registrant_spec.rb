558c8ab19a96c3646a17afa75d8c6237f99fbdf1
Martin Chavez <martinchavez1203@gmail.com>
2023-10-18 22:52:04 -0600

    show the name and age of a Registrant

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -11,4 +11,11 @@ RSpec.describe Registrant do
       expect(@registrant_1).to be_a(Registrant)
     end
   end
+
+  describe '#name & age' do
+    it 'returns registrants name & age' do
+      expect(@registrant_1.name).to eq("Bruce")
+      expect(@registrant_1.age).to eq(18)
+    end
+  end
 end
