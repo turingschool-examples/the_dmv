e2f761a05e27e10f7ca76874fb313700f8ada895
Sam Puttman <daputter2@gmail.com>
2023-10-19 18:05:10 -0400

    finish iteration 1 tests

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -2,16 +2,20 @@ class Registrant
   attr_reader :age, :license_data, :name, :permit
   
   def initialize(name, age, permit = false, 
-    license_data = { 
-    written: false, 
-    license: false, 
-    renewed: false 
-  })
+    license_data = { written: false, license: false, renewed: false })
 
     @age = age
     @name = name
     @permit = permit
     @license_data = license_data
+  end
 
+  def permit?
+    return @permit
+  end
+
+  def earn_permit
+    @permit = true
+  return @permit
   end
 end
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -18,10 +18,33 @@ RSpec.describe Registrant do
   it "can contain age, name, and permit data" do
     registrant_1 = Registrant.new('Bruce', 18, true)
     registrant_2 = Registrant.new('Penny', 15)
-    binding.pry
+
     expect(registrant_1.age).to eq(18)
     expect(registrant_2.name).to eq('Penny') 
     expect(registrant_2.permit).to eq(false)
   end
 
+  it "can obtain license data" do
+    registrant_1 = Registrant.new('Bruce', 18, true)
+    registrant_2 = Registrant.new('Penny', 15)
+
+    expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+  end
+
+  it "can check for permit" do
+    registrant_1 = Registrant.new('Bruce', 18, true)
+    registrant_2 = Registrant.new('Penny', 15)
+
+    expect(registrant_1.permit?).to eq(true)
+    expect(registrant_2.permit?).to eq(false)
+  end
+
+  it "can assign earned permit" do
+    registrant_1 = Registrant.new('Bruce', 18, true)
+    registrant_2 = Registrant.new('Penny', 15)
+    registrant_2.earn_permit
+
+    expect(registrant_1.permit?).to eq(true)
+    expect(registrant_2.permit?).to eq(true)
+  end
 end
