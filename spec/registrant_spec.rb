f04deca86c8443d2e9a7a37a68bc426ee7d43b61
Martin Chavez <martinchavez1203@gmail.com>
2023-10-18 23:35:40 -0600

    add test for the #earn_permit method

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -13,22 +13,38 @@ RSpec.describe Registrant do
   end
 
   describe '#name & age' do
-    it 'returns registrants name & age' do
+    it 'returns registrant_1 name & age' do
       expect(@registrant_1.name).to eq("Bruce")
       expect(@registrant_1.age).to eq(18)
     end
   end
 
-  describe '#Permit?' do
+  describe '#Permit? for Registrant 1' do
     it 'tells us if the registrant_1 has a permit' do
       expect(@registrant_1.permit?).to eq(true)
 
       expect(@registrant_1.license_data).to eq({:written=>false, 
       :license=>false, :renewed=>false})
     end
+  end
 
-    it 'tells us if registrant_2 has a permit' do
-      expect().to eq()
+  describe '#name & age' do
+    it 'returns registrant_2 name & age' do
+      expect(@registrant_2.name).to eq("Penny")
+      expect(@registrant_2.age).to eq(15)
     end
   end
+
+  describe '#Permit? & #earn_permit' do
+    it 'tells us if the registrant_2 has a permit' do
+      expect(@registrant_2.permit?).to eq(false)
+
+      expect(@registrant_1.license_data).to eq({:written=>false, 
+      :license=>false, :renewed=>false})
+
+      @registrant_2.earn_permit
+      expect(@registrant_2.permit?).to eq(true)
+    end
+  end
+
 end
