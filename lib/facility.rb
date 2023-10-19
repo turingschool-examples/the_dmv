1e587f82b0ed8f92d3ec81d19c71bba8c010ccf5
Quin Nordmark <quinnordmark@gmail.com>
2023-10-19 13:37:37 -0700

    Facility new attribute: registered_vehicles

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,14 @@
+require './lib/vehicle'
+
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles
 
   def initialize(name, address, phone)
     @name = name
     @address = address
     @phone = phone
     @services = []
+    @registered_vehicles = []
   end
 
   def add_services(service)
