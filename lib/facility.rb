e4f3cc80d5697fb43637529fd643547719c69fd8
Yain Porter <yainporter@gmail.com>
2023-10-20 10:45:24 -0700

    feat: Create a completely tested #administer_road_test for Facility that checks if the service is offered at the Facility, and makes sure that the Registrant took the written test

--- lib/facility.rb
+++ lib/facility.rb
@@ -32,4 +32,13 @@ class Facility
     end
   end
 
+  def administer_road_test(registrant)
+    if @services.include?("Road Test") && registrant.license_data[:written] == true
+      registrant.license_data[:license] = true
+      true
+    else
+      false
+    end
+  end
+
 end
