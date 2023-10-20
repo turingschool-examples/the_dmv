9b7d5f1e9fb1547dea0e9fc17808602125fb82db
Yain Porter <yainporter@gmail.com>
2023-10-20 13:01:05 -0700

    fix: Create the correct facility for mo_offices in spec and add conditional statements to #create_facilities to check for keys

--- lib/facility_locations.rb
+++ lib/facility_locations.rb
@@ -10,9 +10,15 @@ class FacilityLocations
       old_name = facility.keys.select {|key| key.name.include?("name")}.join.to_sym
       old_address = facility.keys.select {|key| key.name.include?("address")}.join.to_sym
       old_phone = facility.keys.select {|key| key.name.include?("phone")}.join.to_sym
-      facility[:address] = facility.delete old_address
-      facility[:name] = facility.delete old_name
-      facility[:phone] = facility.delete old_phone
+      if facility.has_key?(:address) == false
+        facility[:address] = facility.delete old_address
+      end
+      if facility.has_key?(:name) == false
+        facility[:name] = facility.delete old_name
+      end
+      if facility.has_key?(:phone) == false
+        facility[:phone] = facility.delete old_phone
+      end
       facility = Facility.new(facility)
       @facilities << facility
     end
