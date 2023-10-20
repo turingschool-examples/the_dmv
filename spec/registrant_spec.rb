e1ea645a3466f4c5f4336bad09c215996ce5f3f8
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-10-19 18:14:01 -0600

    Add tests to change permit value and to access license_data

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -22,6 +22,11 @@ RSpec.describe Registrant do
     expect(registrant_1.age).to eq(18)
   end
 
+  it "has license data" do
+    registrant_1 = Registrant.new("Bruce",18, true)
+    expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+  end
+
   it "has a permit value of true or false" do
     registrant_1 = Registrant.new("Bruce",18, true)
     expect(registrant_1.permit).to eq(true)
@@ -29,7 +34,10 @@ RSpec.describe Registrant do
     expect(registrant_2.permit).to eq(false)
   end
     
-    
+  it "has a method to change permit value" do
+    registrant_2 = Registrant.new('Penny', 15)
+    expect(registrant_2.earn_permit).to eq(true)
+  end
 
 
 
