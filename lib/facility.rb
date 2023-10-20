210ea9f6113c968916135777a5026a8341e356c1
Stephen Nash <stephen.aa.nash@gmail.com>
2023-10-20 15:13:53 -0400

    fix: Remove all bugs from intial code

--- lib/facility.rb
+++ lib/facility.rb
@@ -2,9 +2,9 @@ class Facility
   attr_reader :name, :address, :phone, :services
 
   def initialize(name_address_phone)
-    @name = name
-    @address = address
-    @phone = phone
+    @name = name_address_phone[:name]
+    @address = name_address_phone[:address]
+    @phone = name_address_phone[:phone]
     @services = []
   end
 
