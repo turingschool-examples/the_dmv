efa80e4501b925ccb18d1a04ffee62af1de014d7
Martin Chavez <martinchavez1203@gmail.com>
2023-10-21 17:55:52 -0600

    read new_york_facilities data

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -5,9 +5,17 @@ class FacilityFactory
   # require 'pry'; binding.pry
   def create_facility(api_data)
     facilities = api_data.map do |data|
-      name = data[:dmv_office]
-      address = "#{data[:address_li]} #{data[:address__1]} #{data[:city]} #{data[:state]} #{data[:zip]}"
-      phone = data[:phone]
+      #co
+      if data[:dmv_office] != nil
+        name = data[:dmv_office]
+        address = "#{data[:address_li]} #{data[:address__1]} #{data[:city]} #{data[:state]} #{data[:zip]}"
+        phone = data[:phone]
+      elsif data[:office_name] != nil
+        name = data[:office_name]
+        address = "#{data[:street_address_line_1]}"
+        address += "#{data[:street_address_line_2]} #{data[:city]} #{data[:state]} #{data[:zip_code]}"
+        phone = data[:public_phone_number]
+      end
       facility = Facility.new(name: name, address: address, phone: phone)
     end
     # require 'pry'; binding.pry
