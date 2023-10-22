49aa08c1c02cc4bb11d9fec4c86df51eea120b34
Logan Finnegan <logantfinnegan@protonmail.com>
2023-10-22 18:16:51 -0500

    cleaned up notes

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -18,26 +18,3 @@ class Registrant
       @permit = true
     end
 end
-
-
-
-
-
-  # class Registrant
-# def initialize(name, age, premit)
-# end
-
-#Creating Registrant class so our vistors can use services.
-# class Registrant
-    # attr_accessor :name, 
-    #               :age, 
-    #               :permit, 
-    #               :license_data
-  
-    # def initialize(name, age, permit = false)
-    # @age = age
-    # @name = name
-    # @permit = permit
-    # @license_data = {:written=>false, :license=>false, :renewed=>false}
-    # end
-#   end
