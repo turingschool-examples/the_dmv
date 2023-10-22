192694635c38cf000458607e99a217c60a9b44f1
Logan Finnegan <logantfinnegan@protonmail.com>
2023-10-22 13:22:17 -0500

    add #permit? test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -25,16 +25,23 @@ RSpec.describe Registrant do
     end
 
     it "#name" do
-    registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_1 = Registrant.new('Bruce', 18, true )
 
-    expect(registrant_1.name).to eq("Bruce")
+        expect(registrant_1.name).to eq("Bruce")
     end
 
     it "#age" do
-    registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_1 = Registrant.new('Bruce', 18, true )
 
-    expect(registrant_1.age).to eq(18)
+        expect(registrant_1.age).to eq(18)
     end
+
+    it "#permit?" do
+        registrant_1 = Registrant.new('Bruce', 18, true )
+
+        expect(registrant_1.permit?).to eq(true)
+    end
+
 end
 
 
