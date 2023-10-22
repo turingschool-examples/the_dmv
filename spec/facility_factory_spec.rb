2c759ade661fcc50ddc6d563be5493be7f4e5da6
Martin Chavez <martinchavez1203@gmail.com>
2023-10-21 18:15:24 -0600

    read Missouri_facilities data & tests for it

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -8,30 +8,36 @@ RSpec.describe FacilityFactory do
     @missouri_facilities = DmvDataService.new.mo_dmv_office_locations
   end
   describe '#Facility Factory' do
-  it 'shows us Colorado data' do
-    expect(@factory).to be_a(FacilityFactory)
-    facilities = @factory.create_facility(@Colorado)
-    expect(facilities).to be_a(Array)
-    expect(@Colorado[0][:dmv_office]).to be_a(String)
-    expect(facilities[0]).to be_a(Facility)
-    expect(@Colorado[0][:address_li]).to be_a(String)
-    expect(@Colorado[0][:address__1]).to be_a(String)
-    expect(@Colorado[0][:city]).to be_a(String)
-  end
-  
-  it 'shows us New York data' do
+    it 'shows us Colorado data' do
+      expect(@factory).to be_a(FacilityFactory)
+      facilities = @factory.create_facility(@Colorado)
+      expect(facilities).to be_a(Array)
+      expect(@Colorado[0][:dmv_office]).to be_a(String)
+      expect(facilities[0]).to be_a(Facility)
+      expect(@Colorado[0][:address_li]).to be_a(String)
+      expect(@Colorado[0][:address__1]).to be_a(String)
+      expect(@Colorado[0][:city]).to be_a(String)
+    end
     
-    expect(@factory).to be_a(FacilityFactory)
-    facilities = @factory.create_facility(@new_york_facilities)
-    expect(@new_york_facilities[0][:office_name]).to be_a(String)
-    expect(facilities[0]).to be_a(Facility)
-    expect(@new_york_facilities[0][:street_address_line_1]).to be_a(String)
-    expect(@new_york_facilities[0][:city]).to be_a(String)
-    expect(@new_york_facilities[0][:state]).to be_a(String)
-    
+    it 'shows us New York data' do
+      expect(@factory).to be_a(FacilityFactory)
+      facilities = @factory.create_facility(@new_york_facilities)
+      expect(@new_york_facilities[0][:office_name]).to be_a(String)
+      expect(facilities[0]).to be_a(Facility)
+      expect(@new_york_facilities[0][:street_address_line_1]).to be_a(String)
+      expect(@new_york_facilities[0][:city]).to be_a(String)
+      expect(@new_york_facilities[0][:state]).to be_a(String)
+    end
 
-    # require 'pry'; binding.pry
-      # expect().to eq()
+    it 'shows Missouri data' do
+      expect(@factory).to be_a(FacilityFactory)
+      facilities = @factory.create_facility(@missouri_facilities)
+      expect(@missouri_facilities[0][:name]).to be_a(String)
+      expect(facilities[0]).to be_a(Facility)
+      expect(@missouri_facilities[0][:address1]).to be_a(String)
+      expect(@missouri_facilities[0][:city]).to be_a(String)
+      expect(@missouri_facilities[0][:state]).to be_a(String)
+      # require 'pry'; binding.pry
     end
   end
 end
