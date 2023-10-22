f9457d40b3de507ee807d7659b1703a9a88437de
Stephen Nash <stephen.aa.nash@gmail.com>
2023-10-22 15:25:38 -0400

    Improve formatting for readability

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -8,36 +8,31 @@ RSpec.describe Dmv do
     @facility_3 = Facility.new({name: 'DMV Northwest Branch', address: '3698 W. 44th Avenue Denver CO 80211', phone: '(720) 865-4600'})
   end
 
-  describe '#initialize' do
-    it 'can initialize' do
-      expect(@dmv).to be_an_instance_of(Dmv)
-      expect(@dmv.facilities).to eq([])
-    end
+  it 'can initialize' do
+    expect(@dmv).to be_an_instance_of(Dmv)
+    expect(@dmv.facilities).to eq([])
   end
 
-  describe '#add facilities' do
-    it 'can add available facilities' do
-      expect(@dmv.facilities).to eq([])
-      @dmv.add_facility(@facility_1)
-      expect(@dmv.facilities).to eq([@facility_1])
-    end
+  it 'can add available facilities' do
+    expect(@dmv.facilities).to eq([])
+    @dmv.add_facility(@facility_1)
+    expect(@dmv.facilities).to eq([@facility_1])
+  end
+  
+  it 'can return list of facilities offering a specified Service' do
+    @facility_1.add_service('New Drivers License')
+    @facility_1.add_service('Renew Drivers License')
+    @facility_2.add_service('New Drivers License')
+    @facility_2.add_service('Road Test')
+    @facility_2.add_service('Written Test')
+    @facility_3.add_service('New Drivers License')
+    @facility_3.add_service('Road Test')
+
+    @dmv.add_facility(@facility_1)
+    @dmv.add_facility(@facility_2)
+    @dmv.add_facility(@facility_3)
+
+    expect(@dmv.facilities_offering_service('Road Test')).to eq([@facility_2, @facility_3])
   end
 
-  describe '#facilities_offering_service' do
-    it 'can return list of facilities offering a specified Service' do
-      @facility_1.add_service('New Drivers License')
-      @facility_1.add_service('Renew Drivers License')
-      @facility_2.add_service('New Drivers License')
-      @facility_2.add_service('Road Test')
-      @facility_2.add_service('Written Test')
-      @facility_3.add_service('New Drivers License')
-      @facility_3.add_service('Road Test')
-
-      @dmv.add_facility(@facility_1)
-      @dmv.add_facility(@facility_2)
-      @dmv.add_facility(@facility_3)
-
-      expect(@dmv.facilities_offering_service('Road Test')).to eq([@facility_2, @facility_3])
-    end
-  end
 end
