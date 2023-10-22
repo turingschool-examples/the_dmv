7a3313a33c4c087aebf772db775b668921fb38bb
Logan Finnegan <logantfinnegan@protonmail.com>
2023-10-22 17:57:21 -0500

    Add #earn_permit test

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -7,10 +7,14 @@ class Registrant
         @name = name
         @age = age
         @permit = permit
-        @license_data = {:written=>false, :license=>false, :renewed=>false}
-
-      
+        @license_data = {:written=>false, :license=>false, :renewed=>false}      
     end
+
+    def permit?
+        @permit
+    end
+
+    # def earn_permit
 end
 
 
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -31,12 +31,12 @@ RSpec.describe Registrant do
         expect(registrant_2.age).to eq(15)
     end
 
-    it "#permit" do
+    it "#permit?" do
         registrant_1 = Registrant.new('Bruce', 18, true )
         registrant_2 = Registrant.new('Penny', 15 )
 
-        expect(registrant_1.permit).to eq(true)
-        expect(registrant_2.permit).to eq(false)
+        expect(registrant_1.permit?).to eq(true)
+        expect(registrant_2.permit?).to eq(false)
     end
 
     it "#license_data" do
@@ -46,6 +46,17 @@ RSpec.describe Registrant do
         expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
         expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
+
+    it '#earn_permit' do
+        registrant_2 = Registrant.new('Penny', 15 )
+        
+        expect(registrant_2.permit?).to eq(false)
+
+        registrant_2.earn_permit
+
+        expect(registrant_2.permit?).to eq(true)
+    end
+
 
 
 end
