d22a1027675422ebe5c1d1f62166a8fb9e9b4ceb
Logan Finnegan <logantfinnegan@protonmail.com>
2023-10-22 13:05:48 -0500

    add R1 & R2 exists test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -15,7 +15,17 @@ RSpec.describe Registrant do
 
         expect(registrant_1).to be_a(Registrant)
     end
+
+    it "exists R1 % R2" do
+        registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 15 )
+        
+        expect(registrant_1).to be_an_instance_of(Registrant)
+        expect(registrant_2).to be_an_instance_of(Registrant)
+    end
 end
+
+
 
 
 
