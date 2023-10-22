b4eb2fc4037785b8597d1e84579b33d8a7a31283
Logan Finnegan <logantfinnegan@protonmail.com>
2023-10-22 13:53:17 -0500

    Add r2 to #age test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -34,8 +34,10 @@ RSpec.describe Registrant do
 
     it "#age" do
         registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 15 )
 
         expect(registrant_1.age).to eq(18)
+        expect(registrant_2.age).to eq(15)
     end
 
     it "#permit" do
