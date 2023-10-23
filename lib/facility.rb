429977f9d38ac704ff4e0c81108ee24d5ed98c00
Dana Howell <howelld115@gmail.com>
2023-10-22 20:01:08 -0600

    Register vehicle with corresponding spec

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,5 +1,5 @@
 class Facility
-  attr_reader :name, :address, :phone, :services, :facility
+  attr_accessor :name, :address, :phone, :services, :facility, :registered_vehicles, :collected_fees
 
   def initialize(facility)
     @name = facility[:name]
@@ -7,9 +7,27 @@ class Facility
     @phone = facility[:phone]
     @services = []
     @facility = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
   end
+  
+  def register_vehicle(vehicle)
+    registered_vehicles << vehicle
+    if vehicle.antique?
+      @collected_fees += 25
+    elsif vehicle.electric_vehicle?
+      @collected_fees += 200
+    else 
+      @collected_fees += 100
+    end
+  end
+# After they collect the fees they still need to tell the vehicle "here's the registration"
+  #** I know registration and type of registration.   
+
+
+
 end
