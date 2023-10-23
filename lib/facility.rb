c73469906159e01ec396b857407228b33326ef66
Logan Finnegan <logantfinnegan@protonmail.com>
2023-10-23 14:51:05 -0500

    Add #register_vehicle test

--- lib/facility.rb
+++ lib/facility.rb
@@ -3,17 +3,27 @@ class Facility
               :address, 
               :phone, 
               :services, 
-              :registerd_vehicles
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(data)
     @name = data[:name]
     @address = data[:address]
     @phone = data[:phone]
     @services = []
-    @registerd_vehicles = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def collected_fees
+    @collected_fees
+  end
+
+  def register_vehicle(vehicle)
+    
   end
 end
