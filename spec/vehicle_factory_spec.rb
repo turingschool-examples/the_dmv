acd3594ed12600c4f0fab475ab142ca89e2b6623
Stephen Nash <stephen.aa.nash@gmail.com>
2023-10-23 16:59:35 -0400

    test: Complete rspec for iteration2-reading_from_external_data_sets

---
+++ spec/vehicle_factory_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+RSpec.describe VehicleFactory do
+
+  it 'exists' do
+    factory = VehicleFactory.new
+
+    expect(VehicleFactory.new).to be_an_instance_of(VehicleFactory)
+  end
+
+  it 'also exists' do
+    factory = VehicleFactory.new
+    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+    
+    expect(wa_ev_registrations = DmvDataService.new.wa_ev_registrations).to be_an_instance_of(Array)
+  end
+
+  it 'takes the registered vehicles and arrays it into vehicles' do
+    factory = VehicleFactory.new
+    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+
+    #require 'pry'; binding.pry
+    expect(factory.create_vehicles(wa_ev_registrations)).to be_an_instance_of(Array)
+  end
+
+end
