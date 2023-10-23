3430646694e84e8f9c1d4453bda122d3ebded029
Yain Porter <yainporter@gmail.com>
2023-10-22 19:53:47 -0700

    refactor: Create methods for the @hours, @phone, @address, and @name attributes in FacilityLocations so there is better organization and readability

--- lib/facility_locations.rb
+++ lib/facility_locations.rb
@@ -7,10 +7,56 @@ class FacilityLocations
 
   def create_facilities(dmv_data)
       dmv_data.each do |facility| 
+      hours(dmv_data)
+      phone(dmv_data)
+      address(dmv_data)
+      name(dmv_data)
+      holidays_closed(dmv_data)
+      facility = Facility.new(facility)
+      @facilities << facility
+    end
+  end
+
+  def holidays_closed(dmv_data)
+    dmv_data.each do |facility| 
+      old_holidaysclosed = facility.keys.select {|key| key.name.include?("holidaysclosed")}[0] 
+      # require 'pry'; binding.pry if facility.values.include?("117A NORTH MAIN",) || facility.values.include?("101 WEST 3RD STREET") || facility.values.include?("222")
+      if facility.has_key?(:holidays_closed) == false
+      facility[:holidays_closed] = facility.delete old_holidaysclosed
+      end
+      # require 'pry'; binding.pry if facility.keys.include?(:daysopen) && facility[:holidays_closed] == nil
+    end
+  end
+
+  def name(dmv_data)
+    dmv_data.each do |facility| 
       old_name = facility.keys.select {|key| key.name.include?("name") || key.name.include?("office")}[0]
+      if facility.has_key?(:name) == false
+        facility[:name] = facility.delete old_name
+      end
+    end
+  end
+
+  def address(dmv_data)
+    dmv_data.each do |facility|  
       old_address = facility.keys.select {|key| key.name.include?("address")}[0]
+      if facility.has_key?(:address) == false
+        facility[:address] = facility.delete old_address
+      end
+    end
+  end
+
+  def phone(dmv_data)
+    dmv_data.each do |facility| 
       old_phone = facility.keys.select {|key| key.name.include?("phone")}[0]
-      # Check for hours of facilities
+      if facility.has_key?(:phone) == false
+        facility[:phone] = facility.delete old_phone  
+      end
+    end
+  end
+
+  def hours(dmv_data)
+    dmv_data.each do |facility| 
       if facility.keys.select {|key| key.name.include?("hour")}.count > 1
       # Create an array of hashes
         hash_of_hours = facility.select {|key,value| key.name.include?("hour")}
@@ -38,30 +84,6 @@ class FacilityLocations
         end
         facility[:hours] = notice
       end
-      if facility.has_key?(:address) == false
-        facility[:address] = facility.delete old_address
-      end
-      if facility.has_key?(:name) == false
-        facility[:name] = facility.delete old_name
-      end
-      if facility.has_key?(:phone) == false
-        facility[:phone] = facility.delete old_phone  
-      end
-      require 'pry'; binding.pry if facility[:hours] == nil
-      holidays_closed(dmv_data)
-      facility = Facility.new(facility)
-      @facilities << facility
-    end
-  end
-
-  def holidays_closed(dmv_data)
-    dmv_data.each do |facility| 
-      old_holidaysclosed = facility.keys.select {|key| key.name.include?("holidaysclosed")}[0] 
-      # require 'pry'; binding.pry if facility.values.include?("117A NORTH MAIN",) || facility.values.include?("101 WEST 3RD STREET") || facility.values.include?("222")
-      if facility.has_key?(:holidays_closed) == false
-      facility[:holidays_closed] = facility.delete old_holidaysclosed
-      end
-      # require 'pry'; binding.pry if facility.keys.include?(:daysopen) && facility[:holidays_closed] == nil
     end
   end
 end
