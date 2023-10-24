97e99bd7c62f069d2cc3fb7ef53a3b08b7e58678
Dylan Perry <dylanbperry@gmail.com>
2023-10-24 16:16:27 -0700

    Calculate most popular make and model

---
+++ analytics.rb
@@ -0,0 +1,41 @@
+require "./lib/facility.rb"
+require "./lib/facility_list.rb"
+require "./lib/vehicle.rb"
+require "./lib/vehicle_factory.rb"
+require "./lib/dmv_data_service.rb"
+
+# EV Registrations:
+
+    # Find the most popular make/model registered
+
+    factory = VehicleFactory.new
+    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+    factory.create_vehicles(wa_ev_registrations)
+
+    vehicle_makes = []
+    vehicle_models = []
+    factory.vehicles.each do |vehicle|
+        vehicle_makes << vehicle.make
+        vehicle_models << vehicle.model
+    end
+
+    puts vehicle_makes.tally.sort_by(&:last).reverse.first
+    ## TESLA is the most popular make
+
+    puts vehicle_models.tally.sort_by(&:last).reverse.first
+    ## Leaf is the most popular model
+
+    # Count of registered vehicle for a model year
+
+
+
+    # County with most registered vehicles
+
+
+
+# Facilities:
+    # Add in a few more helpful attributes.
+    # What are the daily hours for each facility?
+    # For the states that provide (Missouri), which holidays are they closed?
+# Vehicles:
+    # Add in a second data source for vehicles and create some Vehicle objects from NY State Registration Data: https://data.ny.gov/resource/w4pv-hbkt.json
