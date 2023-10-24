5cd725c3ebc2ab2680d77e882d786f84bc07a563
Dylan Perry <dylanbperry@gmail.com>
2023-10-23 20:06:27 -0700

    Expand methods and tests with checks for whether facility offers respective services

--- lib/facility.rb
+++ lib/facility.rb
@@ -15,40 +15,56 @@ class Facility
   end
 
   def register_vehicle(vehicle)
-    @registered_vehicles << vehicle
-    if vehicle.antique?
-      @collected_fees += 25
-      vehicle.plate_type = :antique
-    elsif vehicle.electric_vehicle?
-      @collected_fees += 200
-      vehicle.plate_type = :ev
+    if @services.include?("Vehicle Registration")
+      @registered_vehicles << vehicle
+      if vehicle.antique?
+        @collected_fees += 25
+        vehicle.plate_type = :antique
+      elsif vehicle.electric_vehicle?
+        @collected_fees += 200
+        vehicle.plate_type = :ev
+      else
+        @collected_fees += 100
+        vehicle.plate_type = :regular
+      end
     else
-      @collected_fees += 100
-      vehicle.plate_type = :regular
+      "Error: #{name} does not offer Vehicle Registration services."
     end
   end
 
   def administer_written_test(registrant)
-    if !registrant.permit? || registrant.age < 16
-      "Error: Written tests can only be administered to registrants with a permit, and who are at least 16 years of age."
+    if @services.include?("New Drivers License")
+      if !registrant.permit? || registrant.age < 16
+        "Error: Written tests can only be administered to registrants with a permit, and who are at least 16 years of age."
+      else
+        registrant.license_data[:written] = true
+      end
     else
-      registrant.license_data[:written] = true
+      "Error: #{@name} does not offer Written Test services."
     end
   end
 
   def administer_road_test(registrant)
-    if !registrant.license_data[:written]
-      "Error: Road tests can only be administered to registrants who have passed a written test."
+    if @services.include?("New Drivers License")
+      if !registrant.license_data[:written]
+        "Error: Road tests can only be administered to registrants who have passed a written test."
+      else
+        registrant.license_data[:license] = true
+      end
     else
-      registrant.license_data[:license] = true
+      "Error: #{@name} does not offer Road Test services."
     end
   end
 
   def renew_drivers_license(registrant)
-    if !registrant.license_data[:license]
-      "Error: Registrant does not have a license to renew."
+    if @services.include?("Renew Drivers License")
+      if !registrant.license_data[:license]
+        "Error: Registrant does not have a license to renew."
+      else
+        registrant.license_data[:renewed] = true
+      end
     else
-      registrant.license_data[:renewed] = true
+      "Error: #{@name} does not offer License Renewal services."
     end
   end
 end
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -29,6 +29,7 @@ RSpec.describe Facility do
 
   describe "#register_vehicle" do
     it "adds a vehicle to the registered_vehicles array" do
+      @facility.add_service('Vehicle Registration')
       @facility.register_vehicle(@cruz)
       @facility.register_vehicle(@bolt)
 
@@ -36,6 +37,7 @@ RSpec.describe Facility do
     end
 
     it "charges fees and assigns plate types based on vehicle type" do
+      @facility.add_service('Vehicle Registration')
       @facility.register_vehicle(@cruz)
 
       expect(@facility.collected_fees).to eq(100)
@@ -52,12 +54,17 @@ RSpec.describe Facility do
       expect(@bolt.plate_type).to eq(:ev)
       expect(@camaro.plate_type).to eq(:antique)
     end
+
+    it "errors out if facility does not offer vehicle registration" do
+      expect(@facility.register_vehicle(@cruz)).to eq("Error: DMV Tremont Branch does not offer Vehicle Registration services.")
+    end
   end
 
   describe "#administer_written_test" do
     it "errors out if registrant is younger than 16, or if registrant lacks a permit" do
       registrant_1 = Registrant.new("Bruce", 18, false)
       registrant_2 = Registrant.new("Penny", 15, true)
+      @facility.add_service('New Drivers License')
 
       expect(@facility.administer_written_test(registrant_1)).to eq("Error: Written tests can only be administered to registrants with a permit, and who are at least 16 years of age.")
       expect(@facility.administer_written_test(registrant_2)).to eq("Error: Written tests can only be administered to registrants with a permit, and who are at least 16 years of age.")
@@ -65,9 +72,16 @@ RSpec.describe Facility do
 
     it "administers written test if registrant is 16 or older, and has a permit" do
       registrant = Registrant.new("Chrysanthemum", 16, true)
+      @facility.add_service("New Drivers License")
       @facility.administer_written_test(registrant)
 
       expect(registrant.license_data[:written]).to eq true
+    end
+
+    it "errors out if facility does not offer written tests" do
+      registrant = Registrant.new("Chrysanthemum", 16, true)
+
+      expect(@facility.administer_written_test(registrant)).to eq("Error: DMV Tremont Branch does not offer Written Test services.")
     end
   end
 
@@ -75,6 +89,7 @@ RSpec.describe Facility do
     it "errors out if a registrant has not passed a written test" do
       registrant_1 = Registrant.new("Bruce", 18, true)
       registrant_2 = Registrant.new("Penny", 15)
+      @facility.add_service("New Drivers License")
 
       expect(@facility.administer_road_test(registrant_1)
       ).to eq("Error: Road tests can only be administered to registrants who have passed a written test.")
@@ -84,22 +99,32 @@ RSpec.describe Facility do
 
     it "administers a road test if registrant passed a written test" do
       registrant_1 = Registrant.new("Bruce", 18, true)
+      @facility.add_service("New Drivers License")
       @facility.administer_written_test(registrant_1)
       @facility.administer_road_test(registrant_1)
 
       expect(registrant_1.license_data[:license]).to eq true
+    end
+
+    it "errors out if facility does not offer road tests" do
+      registrant_1 = Registrant.new("Bruce", 18, true)
+
+      expect(@facility.administer_road_test(registrant_1)).to eq("Error: DMV Tremont Branch does not offer Road Test services.")
     end
   end
 
   describe "#renew_drivers_license" do
     it "errors out if the registrant does not have a pre-existing license" do
       registrant_1 = Registrant.new("Bruce", 18, true)
+      @facility.add_service('Renew Drivers License')
 
       expect(@facility.renew_drivers_license(registrant_1)).to eq("Error: Registrant does not have a license to renew.")
     end
   
     it "renews an existing drivers license" do
       registrant_1 = Registrant.new("Bruce", 18, true)
+      @facility.add_service("New Drivers License")
+      @facility.add_service('Renew Drivers License')
       @facility.administer_written_test(registrant_1)
       @facility.administer_road_test(registrant_1)
 
@@ -108,6 +133,12 @@ RSpec.describe Facility do
       @facility.renew_drivers_license(registrant_1)
 
       expect(registrant_1.license_data[:renewed]).to eq true
+    end
+
+    it "errors out if facility does not offer license renewals" do
+      registrant_1 = Registrant.new("Bruce", 18, true)
+
+      expect(@facility.renew_drivers_license(registrant_1)).to eq("Error: DMV Tremont Branch does not offer License Renewal services.")
     end
   end
 end
