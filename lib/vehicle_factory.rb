1a212e143ada2a846b59016f9fde83d2db829e53
Dylan Perry <dylanbperry@gmail.com>
2023-10-24 12:09:19 -0700

    Add method and tests for create_vehicles

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,6 +1,19 @@
 class VehicleFactory
+    attr_reader :vehicles
 
     def initialize
-        
+        @vehicles = []
+    end
+
+    def create_vehicles(vehicle_list)
+        vehicle_list.each do |vehicle_api|
+            vehicle_ours = Vehicle.new(vehicle_api)
+            vehicle_ours.vin = vehicle_api[:vin_1_10]
+            vehicle_ours.year = vehicle_api[:model_year]
+            if vehicle_api[:electric_range]
+                vehicle_ours.engine = :ev
+            end
+            @vehicles << vehicle_ours
+        end
     end
 end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -10,4 +10,16 @@ describe VehicleFactory do
         expect(factory).to be_a VehicleFactory
     end
 
+    it "creates vehicles with API data" do
+        factory = VehicleFactory.new
+        wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+        factory.create_vehicles(wa_ev_registrations)
+
+        expect(factory.vehicles.first).to be_a(Vehicle)
+        expect(factory.vehicles.first.vin).not_to be_nil
+        expect(factory.vehicles.first.year).not_to be_nil
+        expect(factory.vehicles.first.make).not_to be_nil
+        expect(factory.vehicles.first.model).not_to be_nil
+        expect(factory.vehicles.first.engine).not_to be_nil
+    end
 end
