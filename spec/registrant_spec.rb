0ef59755b4c67f851de7c932e18ffcb6cdea9a22
Mel Langhoff <mlanghoff@uwalumni.com>
2023-10-24 17:02:19 -0600

    Registrant class

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Registrant do
 
     it 'can initialize' do
     @registrant_1 = Registrant.new('Bruce', 18, true)
-    @registrant_2 = Registrant.new('Penny', 15, false)
+    @registrant_2 = Registrant.new('Penny', 15)
      
     expect(@registrant_1).to be_an_instance_of(Registrant)
     expect(@registrant_2).to be_an_instance_of(Registrant)
@@ -13,7 +13,7 @@ RSpec.describe Registrant do
 
     it '#permit?' do
     @registrant_1 = Registrant.new('Bruce', 18, true)
-    @registrant_2 = Registrant.new('Penny', 15, false)
+    @registrant_2 = Registrant.new('Penny', 15)
 
     expect(@registrant_1.permit).to eq true
     expect(@registrant_2.permit).to eq false
