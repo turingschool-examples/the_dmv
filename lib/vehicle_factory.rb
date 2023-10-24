a1d6a28c23b8a0e37d8ab3ba47c2de1e1a2f7c5a
Sam Puttman <daputter2@gmail.com>
2023-10-24 12:43:50 -0400

    add framework for most popular make & model

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,12 +1,11 @@
 class VehicleFactory
-  attr_accessor :vehicle_instances
+  attr_reader :vehicle_instances
 
   def initialize
     @vehicle_instances = []
   end
 
-  def create_electric_vehicles(state_vehicle_registrations)
-    #for each hash, a new vehicle needs to be created (and stored into an array?)
+  def create_vehicles(state_vehicle_registrations)
     @vehicle_instances = state_vehicle_registrations.map do |vehicle|
       (Vehicle.new(
         vin: vehicle[:vin_1_10],
@@ -18,5 +17,42 @@ class VehicleFactory
     end
   end
 
+  def most_popular_make
+    make_count = {}
+
+    @vehicle_instances.each do |vehicle|
+      make_count[vehicle.make] =+ 1
+    end
+    most_popular_make = find_most_popular(make_count)
+
+    return most_popular_make
+  end
+
+  def most_popular_model
+    model_count = {}
+
+    @vehicle_instances.each do |vehicle|
+      model_count[vehicle.model] =+ 1
+    end
+    most_popular_model = find_most_popular(model_count)
+    return most_popular_model
+  end
+
+
+
+  #generic "find most popular in a hash" fetcher (Thanks StackOverflow)
+  def find_most_popular(count_hash)
+    most_popular = nil
+    max_count = 0
+
+    count_hash.each do |item, count|
+      if count > max_count
+        most_popular = item
+        max_count = count
+      end
+    end
+    return most_popular
+  end
 end
+
 
