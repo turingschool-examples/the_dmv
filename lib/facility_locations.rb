c13fc6c946360397b39ff8c49a637f8a4260ac5c
Yain Porter <yainporter@gmail.com>
2023-10-24 12:23:03 -0700

    refactor: Break down #hours by creating 3 different helper methods to make it more readable

--- lib/facility_locations.rb
+++ lib/facility_locations.rb
@@ -57,33 +57,72 @@ class FacilityLocations
 
   def hours(dmv_data)
     dmv_data.each do |facility| 
-      if facility.keys.select {|key| key.name.include?("hour")}.count > 1
-      # Create an array of hashes
-        hash_of_hours = facility.select {|key,value| key.name.include?("hour")}
-        facility.map do |key,value|
-          if key.name.include?("hour")
-            facility.delete(key)
-          end
-        end
-        facility[:hours] = hash_of_hours
-      else
-        old_hours = facility.keys.select {|key| key.name.include?("hour")|| key.name.include?("daysopen")}[0]
-      end
+      # if facility.keys.select {|key| key.name.include?("hour")}.count > 1
+      # # Create an array of hashes
+      #   hash_of_hours = facility.select {|key,value| key.name.include?("hour")}
+      #   facility.map do |key,value|
+      #     if key.name.include?("hour")
+      #       facility.delete(key)
+      #     end
+      #   end
+      #   facility[:hours] = hash_of_hours
+      # else
+      #   old_hours = facility.keys.select {|key| key.name.include?("hour")|| key.name.include?("daysopen")}[0]
+      # end
       # Make a new hash for @hours
-      if facility.has_key?(:hours) == false
-        old_hours = facility.keys.select {|key| key.name.include?("hour")|| key.name.include?("daysopen")}[0]
-        facility[:hours] = facility.delete old_hours 
-      end
+      check_for_multiple_hours(facility)
+      # if facility.has_key?(:hours) == false
+      #   old_hours = facility.keys.select {|key| key.name.include?("hour")|| key.name.include?("daysopen")}[0]
+      #   facility[:hours] = facility.delete old_hours 
+      # end
+      hours_of_facility(facility)
       # Add in text for closed offices
-      notice = nil
-      if facility[:hours] == nil
-        facility.select do |key, value|
-          if value != nil && (value.include?("CLOSED UNTIL") || value.include?("closed until"))
-          notice = value
-          end
-        end
-        facility[:hours] = notice
-      end
+      @notice = nil
+      notice_for_closed_hours(facility)
+      # if facility[:hours] == nil
+      #   facility.select do |key, value|
+      #     if value != nil && (value.include?("CLOSED UNTIL") || value.include?("closed until"))
+      #     notice = value
+      #     end
+      #   end
+      #   facility[:hours] = notice
+      # end
     end
   end
+  
+# Helper method for finding closed offices
+  def notice_for_closed_hours(facility)
+    if facility[:hours] == nil
+      facility.select do |key, value|
+        if value != nil && (value.include?("CLOSED UNTIL") || value.include?("closed until"))
+        @notice = value
+        end
+      end
+      facility[:hours] = @notice
+    end
+  end
+
+# Helper method for checking if there are multiple keys for hours
+  def check_for_multiple_hours(facility)
+    if facility.keys.select {|key| key.name.include?("hour")}.count > 1
+    # Create an array of hashes
+      hash_of_hours = facility.select {|key,value| key.name.include?("hour")}
+      facility.map do |key,value|
+        if key.name.include?("hour")
+          facility.delete(key)
+        end
+      end
+      facility[:hours] = hash_of_hours
+    else
+      old_hours = facility.keys.select {|key| key.name.include?("hour")|| key.name.include?("daysopen")}[0]
+    end
+  end
+# Helper method to find the :hours of a facility
+  def hours_of_facility(facility)
+    if facility.has_key?(:hours) == false
+      old_hours = facility.keys.select {|key| key.name.include?("hour")|| key.name.include?("daysopen")}[0]
+      facility[:hours] = facility.delete old_hours 
+    end
+  end
+
 end
