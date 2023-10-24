5b8682708b56b3267d6aaf75108ee19a3c662e47
Dylan Perry <dylanbperry@gmail.com>
2023-10-24 16:54:34 -0700

    Uncomment erroneously commented out sections

--- analytics.rb
+++ analytics.rb
@@ -4,58 +4,58 @@ require "./lib/vehicle.rb"
 require "./lib/vehicle_factory.rb"
 require "./lib/dmv_data_service.rb"
 
-# # EV Registrations:
+# EV Registrations:
 
-#     factory = VehicleFactory.new
-#     wa_ev_registrations = DmvDataService.new.wa_ev_registrations
-#     factory.create_vehicles(wa_ev_registrations)
+    factory = VehicleFactory.new
+    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+    factory.create_vehicles(wa_ev_registrations)
 
-#     # Find the most popular make/model registered
+    # Find the most popular make/model registered
 
-#     vehicle_makes = []
-#     vehicle_models = []
-#     factory.vehicles.each do |vehicle|
-#         vehicle_makes << vehicle.make
-#         vehicle_models << vehicle.model
-#     end
+    vehicle_makes = []
+    vehicle_models = []
+    factory.vehicles.each do |vehicle|
+        vehicle_makes << vehicle.make
+        vehicle_models << vehicle.model
+    end
 
-#     puts vehicle_makes.tally.sort_by(&:last).reverse.first
-#     ## TESLA is the most popular make
+    puts vehicle_makes.tally.sort_by(&:last).reverse.first
+    ## TESLA is the most popular make
 
-#     puts vehicle_models.tally.sort_by(&:last).reverse.first
-#     ## Leaf is the most popular model
+    puts vehicle_models.tally.sort_by(&:last).reverse.first
+    ## Leaf is the most popular model
 
-#     # Count of registered vehicle for a model year
+    # Count of registered vehicle for a model year
 
-#     registered_years = []
-#     factory.vehicles.each do |vehicle|
-#         registered_years << vehicle.year
-#     end
+    registered_years = []
+    factory.vehicles.each do |vehicle|
+        registered_years << vehicle.year
+    end
 
-#     puts registered_years.tally.sort
-#     ## Displays number of vehicles for each model year
+    puts registered_years.tally.sort
+    ## Displays number of vehicles for each model year
 
-#     # County with most registered vehicles
+    # County with most registered vehicles
 
-#     registration_counties = []
-#     wa_ev_registrations.each do |registration|
-#         registration_counties << registration[:county]
-#     end
+    registration_counties = []
+    wa_ev_registrations.each do |registration|
+        registration_counties << registration[:county]
+    end
 
-#     puts registration_counties.tally.sort_by(&:last).reverse.first
-#     ## King County has the most vehicle registrations
+    puts registration_counties.tally.sort_by(&:last).reverse.first
+    ## King County has the most vehicle registrations
 
-# # Facilities:
-#     # Add in a few more helpful attributes.
-#     # What are the daily hours for each facility?
+# Facilities:
+    # Add in a few more helpful attributes.
+    # What are the daily hours for each facility?
 
-#     ## Added functionality to existing classes/tests
+    ## Added functionality to existing classes/tests
 
-#     # For the states that provide (Missouri), which holidays are they closed?
+    # For the states that provide (Missouri), which holidays are they closed?
 
-#     ## Added functionality to existing classes/tests
-# # Vehicles:
-#     # Add in a second data source for vehicles and create some Vehicle objects from NY State Registration Data: https://data.ny.gov/resource/w4pv-hbkt.json
+    ## Added functionality to existing classes/tests
+# Vehicles:
+    # Add in a second data source for vehicles and create some Vehicle objects from NY State Registration Data: https://data.ny.gov/resource/w4pv-hbkt.json
 
     factory_ny = VehicleFactory.new
     ny_vehicle_data = DmvDataService.new.ny_vehicle_data
