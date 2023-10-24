70c3e2ce806aa21968e6c3a316a1ee8dcaec721e
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-10-24 15:15:24 -0600

    Check registrant class and test files for final submission

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,11 +1,13 @@
 class Registrant
 
-  attr_accessor :name, :age, :permit, :license_data
+  attr_accessor :name, :age, 
+                :permit, :license_data
 
   def initialize(name, age, permit = false)
     @name = name
     @age = age
-    @license_data = {written: false, license: false, renewed: false}
+    @license_data = {written: false, 
+    license: false, renewed: false}
     @permit = permit  
   end
 
