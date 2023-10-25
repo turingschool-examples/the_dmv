c3277a7bad4ec68371936082c6f96009baa0bba2
Barry Allahyar <barrya@gmail.com>
2023-10-24 21:54:40 -0500

    test: add name, age, permit? tests

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -8,9 +8,26 @@ RSpec.describe Registrant do
     expect(registrant_1).to be_a Registrant
     expect(registrant_2).to be_a Registrant
   end
-end
 
-  describe 
+
+  it 'returns registrant_1 name' do
+    registrant_1 = Registrant.new('Bruce', 18, true )
+
+    expect(registrant_1.name).to eq('Bruce')
+  end
+
+  it 'returns registrant_1 age' do
+    registrant_1 = Registrant.new('Bruce', 18, true )
+    
+    expect(registrant_1.age).to eq(18)
+  end
+
+  it 'returns registrant_1 permit' do
+    registrant_1 = Registrant.new('Bruce', 18, true )
+    
+    expect(registrant_1.permit?).to eq(true)
+  end
+end
 
 
       
