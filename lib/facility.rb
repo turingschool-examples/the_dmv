0f8be64573a71d3280bcbd14acbdb45ce33ddefc
Quin Nordmark <quinnordmark@gmail.com>
2023-10-25 08:36:21 -0700

    Facility fix initialize inputs and add_service misspell

--- lib/facility.rb
+++ lib/facility.rb
@@ -3,16 +3,16 @@ require './lib/vehicle'
 class Facility
   attr_reader :name, :address, :phone, :services, :registered_vehicles, :collected_fees
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(facility_details)
+    @name = facility_details[:name]
+    @address = facility_details[:address]
+    @phone = facility_details[:phone]
     @services = []
     @registered_vehicles = []
     @collected_fees = 0
   end
 
-  def add_services(service)
-    @services << service
+  def add_service(service)
+    @services.append(service)
   end
 end
