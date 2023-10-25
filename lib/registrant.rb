fcc6280e1ceeb67c898da8c319ca9fe972058238
Quin Nordmark <quinnordmark@gmail.com>
2023-10-25 12:34:44 -0700

    registrant initial commit

---
+++ lib/registrant.rb
@@ -0,0 +1,18 @@
+#require './lib/vehicle'
+
+class Registrant
+  attr_reader :name, :address, :phone, :services, :registered_vehicles, :collected_fees
+
+  def initialize(facility_details)
+    @name = facility_details[:name]
+    @address = facility_details[:address]
+    @phone = facility_details[:phone]
+    @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
+  end
+
+  def add_service(service)
+    @services.append(service)
+  end
+end
