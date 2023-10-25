5138654cc0c546fbdce2af1366f540b034f5f5f9
Barry Allahyar <barrya@gmail.com>
2023-10-24 21:02:37 -0500

    Test: Add second registrant Penny to spec file

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -3,11 +3,14 @@ require 'spec_helper'
 RSpec.describe Registrant do 
   it "can initialize" do
     registrant_1 = Registrant.new('Bruce', 18, true )
+    registrant_2 = Registrant.new('Penny', 15 )
 
-    expect(registrant_1).to be_a(Registrant)
+    expect(registrant_1).to be_a Registrant
+    expect(registrant_2).to be_a Registrant
   end
 end
 
+  describe 
 
 
       
