6b591988608733db64f8b44c46fe870b2242958d
Hoa Dam <hoangocdam@gmail.com>
2023-12-06 14:57:55 -0800

    change the name of var facility to facility_1

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,28 @@
-class Facility
-  attr_reader :name, :address, :phone, :services
+require './lib/vehicle'
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+class Facility
+  attr_reader :name, :address, :phone, :services, :collected_fees
+
+  def initialize(input)
+    @name = input[:name]
+    @address = input[:address]
+    @phone = input[:phone]
     @services = []
+    @collected_fees = 0
+    @plate_type = {}
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
+  end
+
+  def collect_fees(vehicle)
+    if vehicle.plate_type == :ev
+      @collected_fees  += 200
+    elsif vehicle.plate_type == :antique
+      @collected_fees += 25
+    else
+      @collected_fees += 100
+    end
   end
 end
