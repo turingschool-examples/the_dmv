08a74976a8148cea33bcb72136389bcbd6afee43
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-07 15:09:27 -0800

    feat: Add register_vehicle and registered_vehicles methods

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,23 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services, 
+              :registered_vehicles
 
   def initialize(info)
     @name = info[:name]
     @address = info[:address]
     @phone = info[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    @registered_vehicles << vehicle
   end
 end
