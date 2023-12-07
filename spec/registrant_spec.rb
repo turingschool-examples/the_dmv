3873c42a9b95f660265356d69ed870aaf59a4898
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-07 14:31:35 -0800

    test: Add tests for earn_permit

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -47,4 +47,22 @@ RSpec.describe Registrant do
             expect(registrant_2.license_data).to eq({written: false, license: false, renewed: false})
         end
     end
+
+    describe '#earn_permit' do
+        it 'can earn its permit' do
+            registrant_2 = Registrant.new('Penny', 15)
+
+            registrant_2.earn_permit
+
+            expect(registrant_2.permit?).to eq(true)
+        end
+
+        it 'does not affect someone with a permit already' do
+            registrant_1 = Registrant.new('Bruce', 18, true)
+            
+            registrant_1.earn_permit
+
+            expect(registrant_2.permit?).to eq(true)
+        end
+    end
 end
