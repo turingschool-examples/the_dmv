0f3477b6dd37a377fc22caea626475f86db652f0
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2023-12-07 15:49:48 -0700

    first registrant test

---
+++ lib/registrant.rb
@@ -0,0 +1,10 @@
+class Registrant
+    
+    def initialize(name, age , permit = false)
+        @name = name
+        @age = age
+        @permit = permit
+        @license_data={:written => false, :license => false, :renewed => false}
+
+    end
+end
