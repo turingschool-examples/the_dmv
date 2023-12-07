22cf67b3fb08dad579c9a2c8e4b3f8ffb7dc94ae
Hoa Dam <hoangocdam@gmail.com>
2023-12-06 22:16:39 -0800

    test: administer road test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe Facility do
   let(:cruz) { Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice}) }
   let(:bolt) { Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev}) }
   let(:camaro) { Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice}) }
+  let(:registrant_1) { Registrant.new('Bruce', 18, true) }
+  let(:registrant_2) { Registrant.new('Penny', 16) }
+  let(:registrant_3) { Registrant.new('Tucker', 15) }
 
   describe '#initialize' do
     it 'can initialize' do
@@ -52,6 +55,48 @@ RSpec.describe Facility do
 
       expect(facility_2.register_vehicle(bolt)).to eq (nil)
       expect(facility_2.registered_vehicles).to eq ([])
+    end
+  end
+
+  describe '#administer_written_test' do
+    it "administers a written test for a registrant at least 16 years old and has a permit, then updates license data" do
+      facility_1.add_service('Written Test')
+
+      expect(facility_1.administer_written_test(registrant_1)).to eq (true)
+      expect(registrant_1.license_data[:written]).to eq (true)
+    end
+
+    it "does not administers a written test for a registrant who does not have a permit" do
+      facility_1.add_service('Written Test')
+
+      expect(registrant_2.license_data[:written]).to eq (false)
+      expect(facility_1.administer_written_test(registrant_2)).to eq (false)
+    end
+
+    it "does not administer a written test for a registrant under 16 years old" do
+      facility_1.add_service('Written Test')
+
+      expect(registrant_3.license_data[:written]).to eq (false)
+      expect(facility_1.administer_written_test(registrant_3)).to eq (false)
+    end
+  end
+
+  describe '#administer_road_test' do
+    it "administer road test for a registrant who passed the written test" do
+      facility_1.add_service('Written Test')
+      facility_1.add_service('Road Test')
+      facility_1.administer_written_test(registrant_1)
+
+      expect(registrant_1.license_data[:license]).to eq (false)
+      expect(facility_1.administer_road_test(registrant_1)).to eq (true)
+      expect(registrant_1.license_data[:license]).to eq (true)
+    end
+
+    it "does not administer a road test to a registrant who haven't passed the written test" do
+      facility_1.add_service('Road Test')
+
+      expect(facility_1.administer_road_test(registrant_3)).to eq (false)
+      expect(registrant_3.license_data[:license]).to eq (false)
     end
   end
 end
