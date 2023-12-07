f4ec8ebb2226aff404c7ea86e484d679e50d1352
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-07 14:25:57 -0800

    test: Add test for license data

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -37,4 +37,14 @@ RSpec.describe Registrant do
             expect(registrant_1.permit?).to eq(true)
         end
     end
-    en
+
+    describe '#license_data' do
+        it 'starts with default values for license data' do
+            registrant_1 = Registrant.new('Bruce', 18, true)
+            registrant_2 = Registrant.new('Penny', 15)
+
+            expect(registrant_1.license_data).to eq({written: false, license: false, renewed: false})
+            expect(registrant_2.license_data).to eq({written: false, license: false, renewed: false})
+        end
+    end
+end
