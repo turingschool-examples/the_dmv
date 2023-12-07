183112196961c1c04cbb3340512af7c34f59dced
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-07 18:14:35 -0500

    Feat: initialize method and attribute reader for Registrant class

---
+++ lib/registrant.rb
@@ -0,0 +1,10 @@
+class Registrant
+   attr_reader :name,
+               :age,
+               :permit
+
+   def initialize(name, age, permit = false)
+      @name = name
+      @age = age
+      @permit = permit
+end
