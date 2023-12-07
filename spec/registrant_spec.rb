61c74a8deb6defee6afe23e7847faf8e62af9731
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-07 14:20:23 -0800

    test: Add tests for initialize and permit?

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,40 @@
+require './lib/registrant.rb'
+
+RSpec.describe Registrant do
+    describe '#initialize' do
+        it 'exists' do
+            registrant_1 = Registrant.new('Bruce', 18, true)
+            
+            expect(registrant_1).to be_a(Registrant)
+        end
+
+        it 'has a name' do
+            registrant_1 = Registrant.new('Bruce', 18, true)
+
+            expect(registrant_1.name).to eq('Bruce')
+        end
+
+        it 'can have a different name' do
+            registrant_1 = Registrant.new('Bruce', 18, true)
+            registrant_2 = Registrant.new('Penny', 15)
+
+            expect(registrant_2.name).to eq('Penny')
+        end
+    end
+
+    describe '#permit?' do
+        it 'has a default value of false for permit' do
+            registrant_1 = Registrant.new('Bruce', 18, true)
+            registrant_2 = Registrant.new('Penny', 15)
+
+            expect(registrant_2.permit?).to eq(false)
+        end
+
+        it 'can ask if it has a permit' do
+            registrant_1 = Registrant.new('Bruce', 18, true)
+            registrant_2 = Registrant.new('Penny', 15)
+
+            expect(registrant_1.permit?).to eq(true)
+        end
+    end
+    en
