16a48525a8d59464360fea318d66b3548f2b74af
Mel Langhoff <mlanghoff@uwalumni.com>
2023-12-07 15:43:58 -0700

    Add #permit?, #earn_permit

---
+++ lib/registrant.rb
@@ -0,0 +1,29 @@
+class Registrant
+    attr_reader :age,
+                :license_data,
+                :name,
+                :permit
+
+    def initialize(name, age, permit = false)
+        @name = name
+        @age = age
+        @license_data = {
+            written: false,
+            license: false,
+            renewed: false
+            }
+        @permit = permit
+               
+    end
+
+    def permit?
+        @permit == @permit
+    end
+
+    def earn_permit
+        if @permit == false
+            @permit != false
+        end
+    end
+
+end
