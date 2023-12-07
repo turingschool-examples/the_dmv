44414daf2ce6d3f74e16f100d69aed58f9147115
Hoa Dam <hoangocdam@gmail.com>
2023-12-06 21:14:01 -0800

    feat: administer written test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe Facility do
   let(:cruz) { Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice}) }
   let(:bolt) { Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev}) }
   let(:camaro) { Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice}) }
+  let(:registrant_1) { Registrant.new('Bruce', 18, true) }
+  let(:registrant_2) { Registrant.new('Penny', 15) }
+  let(:registrant_3) { Registrant.new('Tucker', 15) }
 
   describe '#initialize' do
     it 'can initialize' do
@@ -54,4 +57,29 @@ RSpec.describe Facility do
       expect(facility_2.registered_vehicles).to eq ([])
     end
   end
+
+  describe '#administer_written_test' do
+    it "administers a written test for a registrant at least 16 years old and has a permit, then updates license data" do
+      facility_1.add_service('Written Test')
+
+      expect(facility_1.administer_written_test(registrant_1)).to eq (true)
+      expect(registrant_1.license_data[:written]).to eq (true)
+    end
+
+    it "does not administers a written test for a registrant who does not have a permit" do
+      facility_1.add_service('Written Test')
+
+      expect(registrant_2.license_data[:written]).to eq (false)
+      expect(facility_1.administer_written_test(registrant_2)).to eq (false)
+    end
+
+    it "does not administer a written test for a registrant under 16 years old" do
+      facility_1.add_service('Written Test')
+
+      expect(registrant_3.license_data[:written]).to eq (false)
+      expect(facility_1.administer_written_test(registrant_3)).to eq (false)
+    end
+  end
+
+
 end
