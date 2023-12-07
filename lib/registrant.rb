7597f8e5afb50c166325fb5286cd72127c66cce5
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-07 14:28:33 -0800

    feat: Add default value for license data and accessor method

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,11 +1,16 @@
 class Registrant
 
     attr_reader :name, :age, :permit
+    attr_accessor :license_data
 
     def initialize(name, age, permit = false)
         @name = name
         @age = age
         @permit = permit
+        @license_data = {written: false,
+        license: false,
+        renewed: false
+        }
     end
 
     def permit?
