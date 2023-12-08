7c84d3edd88574f6ba9a76550446ac6867dc7752
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-07 21:09:13 -0800

    test: Add tests for create_vehicles method

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe VehicleFactory do
             expect(factory).to be_a(VehicleFactory)
         end
     end
+
+    describe '#create_vehicles' do
+        it 'can create vehicles from external data' do
+            factory = VehicleFactory.new
+            wa_ev_registrations = DmvDataService.new.(wa_ev_registrations)
+            
+            expect(factory.create_vehicles(wa_ev_registrations).first).to be_a(Hash)
+            expect(factory.create_vehicles(wa_ev_registrations).last).to be_a(Hash)
+            expect(factory.create_vehicles(wa_ev_registrations)).to be_a(Array)
+        end
+    end
 end
