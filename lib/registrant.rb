1d0408c3db241308723188c88ff2ab7bd386d71e
Jessica Kohl <kohl.jess45@gmail.com>
2023-12-08 00:18:09 -0600

    Feat: earn and state permit status

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,10 +1,19 @@
 class Registrant
     attr_reader :name,
-                :age
+                :age,
+                :permit
     
     def initialize(name, age, permit = false)
         @name = name
         @age = age
+        @permit = permit
+    end
+
+    def permit?
         @permit
+    end
+
+    def earn_permit
+        @permit = true
     end
 end
