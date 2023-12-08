78b49c8ed80bf63c1293d6336d49c94be86c3a61
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-07 20:38:30 -0800

    feat: Move all attributes in vehicle class to attr_accessor

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,14 +1,14 @@
 require 'date'
 
 class Vehicle
-  attr_reader :vin,
-              :year,
-              :make,
-              :model,
-              :engine
 
   attr_accessor :registration_date,
-                :plate_type
+                :plate_type,
+                :vin,
+                :year,
+                :make,
+                :model,
+                :engine
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
