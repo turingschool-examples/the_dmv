2cbda6a90cac46d46ca6c9c9ccf56383d6e951d2
Joey Reyes <joeyreyes105@gmail.com>
2023-12-07 22:30:28 -0800

    Add register fee method

--- lib/facility.rb
+++ lib/facility.rb
@@ -7,6 +7,7 @@ class Facility
     @phone = hash[:phone]
     @services = []
     @registered_vehicles = []
+    @collected_fees = []
   end
 
   def add_service(service)
@@ -15,8 +16,22 @@ class Facility
 
   def register_vehicle(vehicle)
     vehicle.registration_date = Date.today
-    vehicle.plate_type
+    vehicle.plate_type(vehicle)
     @registered_vehicles << vehicle 
   end 
+
+  def register_fee(vehicle)
+    plate_type = vehicle.plate_type
+
+    case plate_type 
+    when :antique 
+      @collected_fees += 25
+    when :ev 
+      @collected_fees += 200 
+    else 
+      @collected_fees += 100
+    end 
+  end
+
 
 end
