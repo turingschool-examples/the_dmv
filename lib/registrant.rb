6eb7809114859e18c2d1d998fb7b7588b464e13d
Odell McFarland IV <odell.mac4@gmail.com>
2023-12-07 19:31:37 -0800

    Feat: create initialize method for registrant

---
+++ lib/registrant.rb
@@ -0,0 +1,15 @@
+class Registrant
+
+    attr_reader :name, :age, :license_data
+
+    def initialize(name , age , permit = false)
+        @name = name
+        @age = age
+        @permit = permit
+        @license_data = {:written=>false, :license=>false, :renewed=>false}
+    end
+
+    def permit?
+        @permit = true
+    end
+end
