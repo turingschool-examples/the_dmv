40b62a83560b42a3c767a87fbd083ab1c16d5905
Kevin Gonzalez <kevinglz619@gmail.com>
2023-12-07 17:14:37 -0800

    create registrant class file

---
+++ lib/registrant.rb
@@ -0,0 +1,21 @@
+class Registrant
+    attr_reader :name :age, :permit
+    def initialize(name, age, permit)
+        @name = name
+        @age = age
+        @permit = false
+        
+
+
+
+
+
+def license_data={:
+written=>false, 
+:license=>false, 
+:renewed=>false
+}
+
+def permit?
+    @permit
+end
