4ec63250bfa533671bec8adfb7c417e654cba396
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-08 13:09:07 -0500

    Test: include more scenarios to test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -60,7 +60,7 @@ RSpec.describe Facility do
       expect(@facility_1.collected_fees).to eq 100
 
       @facility_1.register_vehicle(@camaro)
-
+      expect(@facility_1.collected_fees).to eq 125
     end
   end
 
