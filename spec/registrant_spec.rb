6dd445895b4f0f6fa50b43a5b788fa9674c3f95a
Selena Hawamdeh <selenahawamdeh@gmail.com>
2023-12-07 21:39:17 -0700

    Make test2 pass and modify test1

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -3,7 +3,6 @@ require './lib/registrant'
 
 describe Registrant do
   before(:each) do
-    @registrant = Registrant.new("John", 29)
     @registrant_1 = Registrant.new("Bruce", 18, true )
     @registrant_2 = Registrant.new("Penny", 15 )
 
@@ -12,9 +11,16 @@ describe Registrant do
   describe '#initialize' do
     it 'can initialize' do
 
-      expect(@registrant).to be_an_instance_of(Registrant)
+      expect(@registrant_1).to be_an_instance_of(Registrant)
+      expect(@registrant_2).to be_an_instance_of(Registrant)
     end
   end
 
-  # describe '#'
+  describe '#name' do
+    it 'can read the name' do
+
+      expect(@registrant_1.name).to eq("Bruce")
+      expect(@registrant_2.name).to eq("Penny")
+    end
+  end
 end
