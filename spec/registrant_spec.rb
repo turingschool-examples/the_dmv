2c4fe69ea1e97e381eec06b4388997b00d65541c
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-07 17:44:45 -0700

    test: Refactor evaluates if the registrant has permit, and after earning the permit upgrades attribute status test and create the returns license process data test for Registrant class

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -14,10 +14,19 @@ RSpec.describe Registrant do
         expect(registrant_1.permit).to eq(true)
     end
 
-    it "evaluates if the registrant has permit" do
+    it "evaluates if the registrant has permit, and after earning the permit upgrades attribute status" do
         registrant_1 = Registrant.new('Bruce', 18, true )
         registrant_2 = Registrant.new('Penny', 15 )
 
         expect(registrant_2.permit?).to eq (false)
+
+        registrant_2.earn_permit
+        expect(registrant_2.permit?).to eq (true)
+    end
+
+    it "returns license process data" do
+        registrant_2 = Registrant.new('Penny', 15 )
+
+        expect(registrant_2.license_data).to eq ({written: false, license: false, renewed: false})
     end
 end
