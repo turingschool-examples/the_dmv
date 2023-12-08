1e1a5604a97eb94a9a25a3b5b1849469ca18f8da
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2023-12-08 06:40:11 -0500

    Fix failing test

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,15 @@
 class Facility
   attr_reader :name, :address, :phone, :services
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(hash)
+    @name = hash[:name]
+    # require 'pry' ; binding.pry
+    @address = hash[:address]
+    @phone = hash[:phone]
     @services = []
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
   end
 end
