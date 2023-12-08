4bd1f0b77b9a43e8aec90b670716a61b94b99e81
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-08 11:27:29 -0700

    test: Test that facility can execute the services available, change attributes for registrant and facility and guarantee requirements to administer a road test are met

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -90,6 +90,7 @@ RSpec.describe Facility do
       expect(@facility_1.administer_written_test(@registrant_2)).to eq(true)
       expect(@registrant_2.license_data).to eq ({written: true, license: false, renewed: false})
 
+      #Guarantees requirements to administer written test are met
       expect(@registrant_3.age).to eq (15)
       expect(@registrant_3.permit?).to eq (false)
       expect(@facility_1.administer_written_test(@registrant_3)).to eq(false)
@@ -97,6 +98,18 @@ RSpec.describe Facility do
       @registrant_3.earn_permit
       expect(@facility_1.administer_written_test(@registrant_3)).to eq(false)
       expect(@registrant_3.license_data).to eq ({written: false, license: false, renewed: false})
+
+      #Guarantees requirements to administer road test are met
+      expect(@facility_1.administer_road_test(@registrant_3)).to eq (false)
+
+      @registrant_3.earn_permit
+      expect(@facility_1.administer_road_test(@registrant_3)).to eq (false)
+      expect(@registrant_3.license_data).to eq ({written: false, license: false, renewed: false})
+      expect(@facility_1.administer_road_test(@registrant_1)).to eq (false)
+
+      @facility_1.add_service('Road Test')
+      expect(@facility_1.administer_road_test(@registrant_1)).to eq (true)
+      expect(@registrant_1.license_data).to eq ({written: true, license: true, renewed: false})
     end
   end
 end
