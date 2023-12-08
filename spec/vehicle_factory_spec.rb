c1033156d4a87cae008d1b27e6aa648b986eb38c
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-07 21:12:21 -0800

    feat: Add create vehicles method

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -12,10 +12,10 @@ RSpec.describe VehicleFactory do
     describe '#create_vehicles' do
         it 'can create vehicles from external data' do
             factory = VehicleFactory.new
-            wa_ev_registrations = DmvDataService.new.(wa_ev_registrations)
+            wa_ev_registrations = DmvDataService.new.wa_ev_registrations
             
-            expect(factory.create_vehicles(wa_ev_registrations).first).to be_a(Hash)
-            expect(factory.create_vehicles(wa_ev_registrations).last).to be_a(Hash)
+            expect(factory.create_vehicles(wa_ev_registrations).first).to be_a(Vehicle)
+            expect(factory.create_vehicles(wa_ev_registrations).last).to be_a(Vehicle)
             expect(factory.create_vehicles(wa_ev_registrations)).to be_a(Array)
         end
     end
