f63207835ba09e0101afe3eb1f7b3b79cb5d19b1
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-07 19:17:08 -0700

    test: Test that facility can only do services available

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -55,6 +55,10 @@ RSpec.describe Facility do
       expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro, @bolt])
       expect(@facility_1.collected_fees).to eq(325)
 
+      expect(@facility_2.services).to eq([])
+      expect(@facility_2.register_vehicle(@bolt)).to eq (nil)
+      expect(@facility_2.registered_vehicles).to eq([])
+      expect(@facility_2.collected_fees).to eq(0)
     end
   end
 
