1471093114dd343655f0a2e7934e9291a72a4176
lance butler <lancej2004@gmail.com>
2023-12-08 15:12:23 -0700

    registrant class created

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -2,11 +2,27 @@ require 'spec_helper'
 require './lib/registrant'
 
 RSpec.describe Registrant do
-    
+  
     describe 'intialize' do 
-        it 'creates a new registrant with permit set to false' do
-            registrant = Registrant.new("Bruce" , 18)
-            expect(registrant.permit).to be_falsey
+        it 'exsist' do
+            registrant = Registrant.new("Bruce", 18)
+            expect(registrant).to be_an_instance_of(Registrant)
+            expect(registrant.age).to eq(18)
+            expect(registrant.name).to eq("Bruce")
+            expect(registrant.permit?).to be false
+        end
+        
+        it 'creates a new registrant with permit already acquired' do
+            registrant = Registrant.new("Bruce" , 18, true)
+            expect(registrant.permit?).to be true
+        end
+    end
+
+    describe 'earn_permit' do
+        it 'changes permit return from false to true' do
+            registrant = Registrant.new("Bruce", 18)
+            registrant.earn_permit
+            expect(registrant.permit?).to be true
         end
     end
 end
