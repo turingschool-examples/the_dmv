1afc393f3539736c4d3a3991558135dfac6d20ad
Odell McFarland IV <odell.mac4@gmail.com>
2023-12-07 18:25:02 -0800

    Debug: replace map method with find_all method to return name of facilities

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -1,5 +1,7 @@
 class Dmv
 
+  attr_reader :facilities
+
   def initialize
     @facilities = []
   end
@@ -7,9 +9,9 @@ class Dmv
   def add_facility(facility)
     @facilities << facility
   end
-
+  
   def facilities_offering_service(service)
-    @facilities.find do |facility|
+    @facilities.find_all do |facility|
       facility.services.include?(service)
     end
   end
