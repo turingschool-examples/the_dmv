cdf1ecaa1889f99a0c40c5b55f555aafd5301681
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-09 10:30:37 -0700

    feat: create the create_vehicles method for the VehicleFactory class

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -5,9 +5,18 @@ class VehicleFactory
         @vehicles = []
     end
 
-    def create_vehicles(data)
-        data.find_all do |vehicle|
-            vehicle
+    def create_vehicles(registrations)
+        registrations.each do |registration|
+            attributes = {}
+            attributes[:vin] = registration[:vin_1_10]
+            attributes[:year] = registration[:model_year]
+            attributes[:make] = registration[:make]
+            attributes[:model] = registration[:model]
+            attributes[:engine] = :ev
+            vehicle = Vehicle.new(attributes)
+            @vehicles << vehicle
         end
+        @vehicles
     end
+
 end
