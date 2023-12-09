f28ef7be7f8ec4c4eb00a5fcf7ae583417866e18
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-08 20:48:00 -0800

    test: Add tests for vehicle models, most popular model, and model counter helper methods

--- spec/analyzer_spec.rb
+++ spec/analyzer_spec.rb
@@ -39,4 +39,46 @@ RSpec.describe Analyzer do
             expect(analyzer.most_popular_vehicle(vehicles)).to eq([])
         end
     end
+
+    describe '#vehicle_models(vehicles)' do
+        it 'will list all models of vehicles analyzed' do
+            cruz_1 = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice})
+            cruz_2 = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice})
+            camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
+            bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev})
+            analyzer = Analyzer.new
+
+            vehicles = [cruz_1, cruz_2, camaro, bolt]
+
+            expect(analyzer.vehicle_models(vehicles)).to eq(["Cruz", "Cruz", "Camaro", "Bolt"])
+        end
+    end
+
+    describe '#model_counter' do
+        it 'can count how many of each model of vehicle there are and return a hash with the model as a key and count as value' do
+            cruz_1 = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice})
+            cruz_2 = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice})
+            camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
+            bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev})
+            analyzer = Analyzer.new
+
+            vehicles = [cruz_1, cruz_2, camaro, bolt]
+
+            expect(analyzer.model_counter(vehicles)).to eq({"Cruz"=> 2, "Camaro"=> 1, "Bolt"=>1})
+        end
+    end
+
+    describe '#most_popular_model(vehicles)' do
+        it 'returns a string with the model of the most popular vehicle' do
+            cruz_1 = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice})
+            cruz_2 = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice})
+            camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
+            bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev})
+            analyzer = Analyzer.new
+
+            vehicles = [cruz_1, cruz_2, camaro, bolt]
+
+            expect(analyzer.most_popular_model(vehicles)).to eq("Cruz")
+        end
+    end
 end
