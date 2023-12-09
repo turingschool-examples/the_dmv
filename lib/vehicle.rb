47bad175fa0dfba0f74568d22cb47aa35afcc12a
Odell McFarland IV <odell.mac4@gmail.com>
2023-12-08 23:51:48 -0800

    Set instantiated variables for reg date and plate type

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,12 +1,15 @@
 require 'date'
 
 class Vehicle
-  attr_reader :vin,
+  attr_reader 
+  
+attr_accessor :registration_date,
+              :vin, 
               :year,
               :make,
               :model,
               :engine,
-              :registration_date
+              :plate_type
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -14,7 +17,8 @@ class Vehicle
     @make = vehicle_details[:make]
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
-    @registration_date = vehicle_details[:registration_date]
+    @registration_date = nil
+    @plate_type = nil
   end
 
   def antique?
@@ -28,3 +32,5 @@ end
 
 
 # First error: The test was expecting a registration_date method, but there was not method created in the Vehicle class. the return value needed to be nil so I called registration date detail from the vehicle objects. The value equals nil since registration date key doesn't exit in vehicle details hash.
+
+#hash with plate type as key and registration cost as value
