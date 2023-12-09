0b76595924630ad6a068c02318e652c95d25524f
Kevin Gonzalez <kevinglz619@gmail.com>
2023-12-09 13:40:57 -0800

    add admister methods,written and road

--- lib/facility.rb
+++ lib/facility.rb
@@ -16,18 +16,47 @@ class Facility
   end
   
  
-  def register_vehicle(car)
+    def register_vehicle(vehicle)
+      
+      if @services.include?("Vehicle Registration")
+      registered_vehicles << vehicle 
+
+        if vehicle.antique?
+          @collected_fees += 25
+          vehicle.plate_type = :antique
+        elsif vehicle.engine == :ev
+          @collected_fees += 200
+          vehicle.plate_type = :ev
+        else
+          @collected_fees += 100 
+          vehicle.plate_type = :regular
+        end
     
-    registered_vehicles << car if 
-    @services.include?("Vehicle Registration")
+        if vehicle.registration_date = Date.today.year
+        @registered_vehicle
+        else 
+          nil
+        end
+      end
+   end
 
+  def administer_written_test(registrant)
+    if @services.include?("Written Test") && registrant.permit? && registrant.age >= 16 
+      registrant.license_data[:written] = true
+    end
+  end
   
-  end 
+  def administer_road_test(registrant)
+    if @services.include?("Road Test")  && registrant.license_data[:written]
+      registrant.license_data[:license] = true
+    end
+  end
 
+  def renew_drivers_license(registrant)
+    if @services.include?("Renew License")  && registrant.license_data[:license]
+      registrant.license_data[:renewed] = true
+    end
 
-
-
-
-
-
+  end
 end
+  
