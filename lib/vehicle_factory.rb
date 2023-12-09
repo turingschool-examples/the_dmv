37ce91aef7bc0db7f5c880f5913e69949db1898a
Joey Reyes <joeyreyes105@gmail.com>
2023-12-09 12:22:11 -0800

    Add create vehicle method

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,8 +1,21 @@
 class VehicleFactory 
-    attr_reader 
+    attr_reader :created_vehicles
     attr_accessor 
 
     def initialize
         @created_vehicles = []
     end 
+end 
+
+def create_vehicles(registrations) 
+    registrations.each do |vehicle_hash| 
+        vehicle_info = {}
+        vehicle_info[:engine] = :ev 
+        vehicle_info[:vin] = vehicle_info[:vin_1_10]
+        vehicle_info[:year] = vehicle_info[:model_year]
+        vehicle_info[:make] = vehicle_info[:make]
+        vehicle_info[:model] = vehicle_info[:model]
+        @created_vehicles << Vehicle.new(vehicle_info)
+    end 
+    @created_vehicles
 end 
