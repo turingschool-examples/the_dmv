3c6593b8c9846494a88393cc8d6f87cb4abc4f1a
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-09 10:52:47 -0500

    Test: Create first piece of initialize test

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,1 +1,13 @@
 require 'spec_helper'
+
+RSpec.describe VehicleFactory do
+   before(:each) do
+      @factory = VehicleFactory.new
+   end
+   describe '#initialize' do
+      it 'can initialize' do
+         expect(@factory).to be_an_instance_of VehicleFactory
+      end
+   end
+
+end
