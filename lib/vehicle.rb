5c988f5d51efdf63507f3c8e9cdaa7dcf6d1a1d1
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-09 15:04:38 -0500

    Refactor: create methods for vehicle class and remove attribute accessor

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,9 +5,9 @@ class Vehicle
               :year,
               :make,
               :model,
-              :engine
-  attr_accessor :registration_date,
-                :plate_type
+              :engine,
+              :registration_date,
+              :plate_type
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -25,5 +25,19 @@ class Vehicle
 
   def electric_vehicle?
     @engine == :ev
+  end
+
+  def change_plate_type
+    if antique?
+      @plate_type = :antique
+    elsif electric_vehicle?
+      @plate_type = :ev
+    else
+      @plate_type = :regular
+    end
+  end
+
+  def register
+    @registration_date = Date.today
   end
 end
