155d961b1774bdcee756e8794edb8da04cd0df94
Kevin Gonzalez <kevinglz619@gmail.com>
2023-12-09 13:42:10 -0800

    changes to plate method

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -7,12 +7,17 @@ class Vehicle
               :model,
               :engine
 
+    attr_accessor :plate_type,
+                  :registration_date
+
   def initialize(vehicle_details) 
     @vin = vehicle_details[:vin]
     @year = vehicle_details[:year]
     @make = vehicle_details[:make]
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
+    @plate_type = nil
+    @registration_date = nil
   end
 
   def antique?
@@ -23,25 +28,7 @@ class Vehicle
     @engine == :ev
   end
 
-  def registration_date
-    Date.today.year
-    
-  end
-
-  def plate_type(
-    :regular,
-    :antique,
-    :ev)
   
-  if @engine == ice && registration_date = date.today.year > 25
-    :antique
-    #collected_fees 25
-  elsif @engine == ice && registration_date = date.today.year < 25
-    :regular
-    #collected_fees 100
-  else @engine == ev
-    :ev
-    #collected_fees 200
-  end
 
+  
 end
