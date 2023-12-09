16ce8a8ac44c175b007772741e28039f1e7f19e4
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-09 10:30:01 -0700

    test: Creates vehicles collection describe with:creates a collection with no nil or empty values, and assigns the attributes correctly tests

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -3,12 +3,13 @@ require "./lib/vehicle_factory"
 require "./lib/dmv_data_service"
 
 RSpec.describe VehicleFactory do
-    before(:each) do
-        @factory = VehicleFactory.new
-    end
+before(:each) do
+    @factory = VehicleFactory.new
+    @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+end
 
     describe "#Initialize" do
-        it "exists"do
+        it "exists" do
             expect(@factory).to be_an_instance_of (VehicleFactory)
         end
 
@@ -17,8 +18,19 @@ RSpec.describe VehicleFactory do
         end
     end
     describe "#Creates a vehicles collection" do
-        it "stores the attributes correctly" do
-            
+        it "creates a collection with no nil or empty values" do
+            expect(@factory.create_vehicles(@wa_ev_registrations)).to be_an_instance_of (Array)
+            expect(@factory.create_vehicles(@wa_ev_registrations).empty?).to eq(false)
+            expect(@factory.create_vehicles(@wa_ev_registrations).include?(nil)).to eq(false)
+            expect(@factory.vehicles.first).to eq(@factory.create_vehicles(@wa_ev_registrations).first)
+        end
+        it "assigns the attributes correctly" do
+            expect(@factory.create_vehicles(@wa_ev_registrations).first).to be_an_instance_of (Vehicle)
+            expect(@factory.create_vehicles(@wa_ev_registrations).first.vin).to eq (@wa_ev_registrations.first[:vin_1_10])
+            expect(@factory.create_vehicles(@wa_ev_registrations).first.year).to eq (@wa_ev_registrations.first[:model_year])
+            expect(@factory.create_vehicles(@wa_ev_registrations).first.make).to eq (@wa_ev_registrations.first[:make])
+            expect(@factory.create_vehicles(@wa_ev_registrations).first.model).to eq (@wa_ev_registrations.first[:model])
+            expect(@factory.create_vehicles(@wa_ev_registrations).first.engine).to eq (:ev)
         end
     end
 
