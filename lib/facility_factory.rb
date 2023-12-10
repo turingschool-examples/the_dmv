334474f6d371574815f721cce70cefc777eb63f6
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-09 21:12:47 -0700

    fix: Fix create for CO locations describe with: creates a collection with no nil or empty values and assigns the attributes correctly tests

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -13,16 +13,19 @@ class FacilityFactory
                 details[:address] = (location[:address_li].to_s)+" "+(location[:address__1].to_s)+" "+location[:city]+" "+location[:state]+" "+location[:zip]
                 details[:phone] = location[:phone]
                 facility = Facility.new(details)
-                facility.add_service(location[:services])
+                facility.add_service(location[:services_p])
             elsif location[:state] == "NY"
                 details[:name] = location[:office_name]
                 details[:address] = (location[:street_address_line_1].to_s)+" "+(location[:street_address_line_2].to_s)+" "+location[:city]+" "+location[:state]+" "+location[:zip_code]
                 details[:phone] = location[:public_phone_number]
                 facility = Facility.new(details)
-            #elsif location[:state] == "MO"
-
+            elsif location[:state] == "MO"
+                details[:name] = location[:name]
+                details[:address] = (location[:address1].to_s)+" "+location[:city]+" "+location[:state]+" "+location[:zipcode]
+                details[:phone] = location[:phone]
+                facility = Facility.new(details)
+                facility.add_service(location[:additional_license_office_info])
             end
-            
             @facilities << facility
         end
     end
