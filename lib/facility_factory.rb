da4ec3ab998270103f3f3f816e39bc7c20c7cbb9
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-09 20:44:48 -0700

    feat: Add NY syntax to the create_facilities method for the FacilityFactory class

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -8,12 +8,21 @@ class FacilityFactory
     def create_facilities(locations)
         locations.each do |location|
          details = {}
-            details[:name] = location[:dmv_office]
-            #require 'pry'; binding.pry
-            details[:address] = (location[:address_li].to_s)+" "+location[:address__1]+" "+location[:city]+" "+location[:zip]
-            details[:phone] = location[:phone]
-            facility = Facility.new(details)
-            facility.add_service(location[:services])
+            if location[:state] == "CO"
+                details[:name] = location[:dmv_office]
+                details[:address] = (location[:address_li].to_s)+" "+(location[:address__1].to_s)+" "+location[:city]+" "+location[:state]+" "+location[:zip]
+                details[:phone] = location[:phone]
+                facility = Facility.new(details)
+                facility.add_service(location[:services])
+            elsif location[:state] == "NY"
+                details[:name] = location[:office_name]
+                details[:address] = (location[:street_address_line_1].to_s)+" "+(location[:street_address_line_2].to_s)+" "+location[:city]+" "+location[:state]+" "+location[:zip_code]
+                details[:phone] = location[:public_phone_number]
+                facility = Facility.new(details)
+            #elsif location[:state] == "MO"
+
+            end
+            
             @facilities << facility
         end
     end
