166df607be85bc360cf2e5c6abb561e4d6627bd1
Odell McFarland IV <odell.mac4@gmail.com>
2023-12-10 12:26:28 -0800

    Add license data as an attribute

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,16 +1,18 @@
 class Registrant
 
-    attr_reader :name, :age, :license_data
+    attr_reader :name, :age
+
+    attr_accessor :license_data
 
     def initialize(name , age , permit = false)
         @name = name
         @age = age
         @permit = permit
-        @license_data = {:written=>false, :license=>false, :renewed=>false}
+        @license_data = {written: false, license: false, renewed: false}
     end
 
     def permit?
-        @permit == true
+        @permit
     end
 
     def earn_permit
