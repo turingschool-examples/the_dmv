42ccbf80316cdc6d13dde37cd9f179d9907098c9
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-09 20:22:41 -0700

    feat: createthe create_facilities method for the FacilityFactory class

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -4,4 +4,17 @@ class FacilityFactory
     def initialize
         @facilities = []
     end
+
+    def create_facilities(locations)
+        locations.each do |location|
+         details = {}
+            details[:name] = location[:dmv_office]
+            #require 'pry'; binding.pry
+            details[:address] = (location[:address_li].to_s)+" "+location[:address__1]+" "+location[:city]+" "+location[:zip]
+            details[:phone] = location[:phone]
+            facility = Facility.new(details)
+            facility.add_service(location[:services])
+            @facilities << facility
+        end
+    end
 end
