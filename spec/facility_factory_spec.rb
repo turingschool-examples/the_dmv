45f17a8cdd57013200b3210bfd3101ff0265dfbb
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-09 20:44:19 -0700

    test: Creates factories collection for NY locations describe with: creates a collection with no nil or empty values and assigns the attributes correctly tests

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe FacilityFactory do
     before(:each) do
         @factory = FacilityFactory.new
         @co_dmv_locations = DmvDataService.new.co_dmv_office_locations
+        @ny_dmv_locations = DmvDataService.new.ny_dmv_office_locations
+        @mo_dmv_locations = DmvDataService.new.mo_dmv_office_locations
     end
     describe "#Initialize" do
         it "exists" do
@@ -17,7 +19,7 @@ RSpec.describe FacilityFactory do
             expect(@factory.facilities).to eq ([])
         end
     end
-    describe"#creates factories collection" do
+    describe"#creates factories collection for CO Locations" do
     it "creates a collection with no nil or empty values" do
         expect(@factory.create_facilities(@co_dmv_locations)).to be_an_instance_of (Array)
         expect(@factory.create_facilities(@co_dmv_locations).empty?).to eq(false)
@@ -27,9 +29,25 @@ RSpec.describe FacilityFactory do
     it "assigns the attributes correctly" do
         expect(@factory.create_facilities(@co_dmv_locations).first).to be_an_instance_of (Facility)
         expect(@factory.create_facilities(@co_dmv_locations).first.name).to eq (@co_dmv_locations.first[:dmv_office])
-        expect(@factory.create_facilities(@co_dmv_locations).first.address).to eq ((@co_dmv_locations.first[:address_li])+" "+(@co_dmv_locations.first[:address__1])+" "+(@co_dmv_locations.first[:city])+" "+(@co_dmv_locations.first[:state])+" "+(@co_dmv_locations.first[:zip]))
+        expect(@factory.create_facilities(@co_dmv_locations).first.address).to eq ((@co_dmv_locations.first[:address_li].to_s)+" "+(@co_dmv_locations.first[:address__1].to_s)+" "+(@co_dmv_locations.first[:city])+" "+(@co_dmv_locations.first[:state])+" "+(@co_dmv_locations.first[:zip]))
         expect(@factory.create_facilities(@co_dmv_locations).first.phone).to eq (@co_dmv_locations.first[:phone])
         expect(@factory.create_facilities(@co_dmv_locations).first.services).to eq (@co_dmv_locations.first[:services])
     end
-end
+    end
+
+    describe"#creates factories collection for NY Locations" do
+    it "creates a collection with no nil or empty values" do
+        expect(@factory.create_facilities(@ny_dmv_locations)).to be_an_instance_of (Array)
+        expect(@factory.create_facilities(@ny_dmv_locations).empty?).to eq(false)
+        expect(@factory.create_facilities(@ny_dmv_locations).include?(nil)).to eq(false)
+        expect(@factory.facilities.first).to eq(@factory.create_facilities(@ny_dmv_locations).first)
+    end
+    it "assigns the attributes correctly" do
+        expect(@factory.create_facilities(@ny_dmv_locations).first).to be_an_instance_of (Facility)
+        expect(@factory.create_facilities(@ny_dmv_locations).first.name).to eq (@ny_dmv_locations.first[:office_name])
+        expect(@factory.create_facilities(@ny_dmv_locations).first.address).to eq ((@ny_dmv_locations.first[:street_address_line_1].to_s)+" "+(@ny_dmv_locations.first[:street_address_line_2].to_s)+" "+(@ny_dmv_locations.first[:city])+" "+(@ny_dmv_locations.first[:state])+" "+(@ny_dmv_locations.first[:zip_code]))
+        expect(@factory.create_facilities(@ny_dmv_locations).first.phone).to eq (@ny_dmv_locations.first[:phone])
+        expect(@factory.create_facilities(@ny_dmv_locations).first.services).to eq (@ny_dmv_locations.first[:services])
+    end
+    end
 end
