18bc751d35c6d4315ee2555c5149b6139717bd9c
Joey Reyes <joeyreyes105@gmail.com>
2023-12-09 16:54:49 -0800

    Initialize facility factory class

---
+++ spec/facility_factory_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+RSpec.describe FacilityFactory do 
+    describe '#initialize' do 
+        it 'initializes' do 
+           facility_factory =  FacilityFactory.new 
+           
+           expect(facility_factory).to be_an_instance_of(FacilityFactory)
+        end 
+    end 
+
+    describe '#create_co_facility' do 
+        it 'is an object of Facility' do 
+            facility_factory =  FacilityFactory.new 
+            colorado_facilities = DmvDataService.new.co_dmv_office_locations
+
+            expect(facility_factory.create_co_facility(colorado_facilities)).to be_an_instance_of(Facility)
+        end 
+    end 
+end 
