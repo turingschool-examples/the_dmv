0ed2ae32f4f1c497ddd098894fe38ef6bc54b38a
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2023-12-10 07:20:24 -0500

    Add a new attr

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -6,7 +6,9 @@ class Vehicle
               :make,
               :model,
               :engine,
-              :registration_date
+              :registration_date,
+              :plate_type
+  attr_writer :registration_date, :plate_type
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -15,6 +17,7 @@ class Vehicle
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
     @registration_date = vehicle_details[:registration_date]
+    @plate_type = nil
   end
 
   def antique?
