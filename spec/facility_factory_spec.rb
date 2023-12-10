0dd34bfd759839cde8f3228cca4189c59798c871
Joey Reyes <joeyreyes105@gmail.com>
2023-12-09 19:25:01 -0800

    Add test to see if it can access data

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -15,6 +15,18 @@ RSpec.describe FacilityFactory do
             colorado_facilities = DmvDataService.new.co_dmv_office_locations
 
             expect(facility_factory.create_co_facilities(colorado_facilities).first).to be_an_instance_of(Facility)
+        end
+        
+        it 'can access the data' do 
+            facility_factory =  FacilityFactory.new 
+            colorado_facilities = DmvDataService.new.co_dmv_office_locations
+
+            expect(facility_factory.create_co_facilities(colorado_facilities).first.name).not_to be(nil)
+            expect(facility_factory.create_co_facilities(colorado_facilities).first.address).not_to be(nil)
+            expect(facility_factory.create_co_facilities(colorado_facilities).first.registered_vehicles).to eq([])
+            expect(facility_factory.create_co_facilities(colorado_facilities).first.collected_fees).to eq(0)
+            expect(facility_factory.create_co_facilities(colorado_facilities).first.services).not_to be(nil)
+            expect(facility_factory.create_co_facilities(colorado_facilities).first.phone).not_to be(nil)
         end 
     end 
 end 
