2780ff30fa34cbbc9bed5019249885da53301351
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2023-12-10 16:36:43 -0600

    Feat: Register car and collect fees

--- lib/facility.rb
+++ lib/facility.rb
@@ -2,16 +2,35 @@ class Facility
   attr_reader :name, 
               :address, 
               :phone, 
-              :services
+              :services,
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(facility_info)
     @name = facility_info[:name]
     @address = facility_info[:address]
     @phone = facility_info[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
+  end
+  
+  def register_vehicle(vehicle)
+    if @services.include?("Vehicle Registration")
+      @registered_vehicles << vehicle
+      vehicle.registration_date = Date.today
+      
+      if vehicle.antique?
+        @collected_fees += 25
+      elsif vehicle.electric_vehicle?
+        @collected_fees += 200
+      else
+        @collected_fees += 100
+      end
+    end
   end
 end
