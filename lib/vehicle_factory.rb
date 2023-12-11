defb99a918b4a2fab01ef78fdf03583ebbec6e27
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2023-12-11 15:44:02 -0700

    fetch vehicle data complete

---
+++ lib/vehicle_factory.rb
@@ -0,0 +1,20 @@
+class VehicleFactory
+    def initialize
+    end
+
+
+
+
+    def create_vehicles(vehicle_details)
+        vehicles_created = []
+        vehicle_details.each do |vehicle|
+            vehicle[:engine] = "ev"
+            vehicles_created << Vehicle.new(vehicle)
+        end
+
+        vehicles_created
+    end
+
+
+end
+
---
+++ spec/vehicle_factory_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require './lib/vehicle'
+require './lib/vehicle_factory'
+require './lib/dmv_data_service'
+
+RSpec.describe VehicleFactory do
+    it 'can initialize' do
+        expect(factory = VehicleFactory.new).to be_an_instance_of(VehicleFactory)
+        wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+        vehicles = factory.create_vehicles(wa_ev_registrations)
+        expect(vehicles.length).to be_a(Integer)
+        expect(vehicles[0].engine).to eq("ev")
+        
+    end
+end
