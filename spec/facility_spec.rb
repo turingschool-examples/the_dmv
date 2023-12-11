40a746f21c1869db3c996094409386d127ce7846
Selena Hawamdeh <selenahawamdeh@gmail.com>
2023-12-10 17:25:37 -0700

    Pass RDL test for registrsnt 3

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -126,5 +126,16 @@ RSpec.describe Facility do
 
       expect(@facility_1.renew_drivers_license(@registrant_1)).to eq(true)
     end
+
+    it 'returns if registrant can renew drivers license' do
+      @registrant_3.earn_permit
+      @facility_1.add_service('Written Test')
+      @facility_1.add_service('Road Test')
+      @facility_1.add_service('Renew License')
+      @facility_1.administer_written_test(@registrant_3)
+      @facility_1.administer_road_test(@registrant_3)
+
+      expect(@facility_1.renew_drivers_license(@registrant_3)).to eq(true)
+    end
   end
 end
