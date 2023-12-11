e985ad0ced912162cfd94f4d2279a287811ea733
Selena Hawamdeh <selenahawamdeh@gmail.com>
2023-12-10 20:41:49 -0700

    Pass creat vehicles test Chevrolet

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -20,7 +20,21 @@ describe VehicleFactory do
       expect(vehicles.first.year).to eq("2013")
       expect(vehicles.first.model).to eq("Prius Plug-in")
     end
-  end
 
-  
+
+    it 'creates an array of vehicles from Chevrolet data' do
+      factory = VehicleFactory.new
+      chevrolet_data = [
+        { electric_vehicle_type: "Plug-in Hybrid Electric Vehicle (PHEV)", vin_1_10: "1G1RD6E47D", model_year: "2013", make: "CHEVROLET", model: "Volt" }
+      ]
+
+      vehicles = factory.create_vehicles(chevrolet_data)
+
+      expect(vehicles.first).to be_a(Vehicle)
+      expect(vehicles.first.make).to eq("CHEVROLET")
+      expect(vehicles.first.vin).to eq("1G1RD6E47D")
+      expect(vehicles.first.year).to eq("2013")
+      expect(vehicles.first.model).to eq("Volt")
+    end
+  end
 end
