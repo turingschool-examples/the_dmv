dbcba5149a59be2baad105f2fff73c20efad79fc
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2023-12-11 05:41:34 -0500

    Write a new test for facilities in new york

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -10,7 +10,22 @@ RSpec.describe FacilityFactory do
         factory = FacilityFactory.new
         co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
         expect(factory.create_facilitys(co_dmv_office_locations)).to be_an Array
-
-
+        expect(factory.create_facilitys(co_dmv_office_locations)[0]).to be_an_instance_of Facility 
+        expect(factory.create_facilitys(co_dmv_office_locations)[0].name).to eq("DMV Tremont Branch")
+        expect(factory.create_facilitys(co_dmv_office_locations)[0].address).to eq("2855 Tremont Place")
+        expect(factory.create_facilitys(co_dmv_office_locations)[0].phone).to eq("(720) 865-4600")
+        
     end 
+
+    it 'can a have a new state ny' do
+        factory = FacilityFactory.new
+        new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+        expect(factory.create_facilitys(new_york_facilities)).to be_an Array
+        expect(factory.create_facilitys(new_york_facilities)[0]).to be_an_instance_of Facility 
+        expect(factory.create_facilitys(new_york_facilities)[0].name).to eq("IRONDEQUOIT")
+        expect(factory.create_facilitys(new_york_facilities)[0].address).to eq("545 TITUS AVENUE")
+        expect(factory.create_facilitys(new_york_facilities)[0].phone).to eq("5857531604")
+    end
+
+
 end 
