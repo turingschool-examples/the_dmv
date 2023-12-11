a30ea4dc8c43ca824c55d70635ac0093959695ee
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2023-12-11 05:57:25 -0500

    Add new information for the other states that i used

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -6,12 +6,10 @@ class FacilityFactory
     def create_facilitys(data)
         data.map do |office_data|
             facility_attributes = {}
-            facility_attributes[:name] = office_data[:dmv_office] || office_data[:office_name]
-            facility_attributes[:address] = office_data[:address_li] || office_data[:street_address_line_1]
-            facility_attributes[:phone] = office_data[:phone] || office_data[:public_phone_number]
-
+            facility_attributes[:name] = office_data[:dmv_office] || office_data[:office_name] || office_data[:name]
+            facility_attributes[:address] = office_data[:address_li] || office_data[:street_address_line_1] || office_data[:address1]
+            facility_attributes[:phone] = office_data[:phone] || office_data[:public_phone_number] || office_data[:phone]
             Facility.new(facility_attributes)
         end
-
     end
 end
