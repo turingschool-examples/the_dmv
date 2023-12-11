3a7c1c155b4d50614783c23c5e1cf7ddf422fa91
Aurora Sanderson <AuroraTuring@gmail.com>
2023-12-10 17:01:20 -0700

    Initialize and complete tests for #earn_permit.

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,51 +1,38 @@
-require './lib/registrant'
+require 'spec_helper'
 
 RSpec.describe Registrant do 
-  before[:each] do 
+  before(:each) do 
     @registrant_1 = Registrant.new('Bruce', 18, true)
     @registrant_2 = Registrant.new('Penny', 15)
   end
 
-  describe '#initialize' do
-    it 'can initialize' do 
-      expect(registrant).to be_an_instance_of(Registrant) 
+  # describe "#initialize" do
+    it "exists" do 
+      expect(@registrant_1).to be_an_instance_of(Registrant) 
     end
-  end
+
+    it "has attributes" do
+      expect(@registrant_1.name).to eq("Bruce")
+      expect(@registrant_1.age).to eq(18)
+      expect(@registrant_1.permit?).to eq(true)
+      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@registrant_2.name).to eq("Penny")
+      expect(@registrant_2.age).to eq(15)
+      expect(@registrant_2.permit?).to eq(false)
+      expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+
+      @registrant_2.earn_permit
+      
+      expect(@registrant_2.permit?).to eq(true)
+    end
 end
 
 
+# registrant_1 = Registrant.new('Bruce', 18, true )
+# #=> #<Registrant:0x000000015c10bed8 @age=18, @license_data={:written=>false, :license=>false, :renewed=>false}, @name="Bruce", @permit=true>
 
-registrant_1 = Registrant.new('Bruce', 18, true )
-#=> #<Registrant:0x000000015c10bed8 @age=18, @license_data={:written=>false, :license=>false, :renewed=>false}, @name="Bruce", @permit=true>
+# registrant_2 = Registrant.new('Penny', 15 )
+# #=> #<Registrant:0x000000015c0778c8 @age=15, @license_data={:written=>false, :license=>false, :renewed=>false}, @name="Penny", @permit=false>
 
-registrant_2 = Registrant.new('Penny', 15 )
-#=> #<Registrant:0x000000015c0778c8 @age=15, @license_data={:written=>false, :license=>false, :renewed=>false}, @name="Penny", @permit=false>
 
-registrant_1.name
-#=> "Bruce"
 
-registrant_1.age
-#=> 18
-
-registrant_1.permit?
-#=> true
-
-registrant_1.license_data
-#=> {:written=>false, :license=>false, :renewed=>false}
-
-registrant_2.name
-#=> "Penny"
-
-registrant_2.age
-#=> 15
-
-registrant_2.permit?
-#=> false
-
-registrant_2.license_data
-#=> {:written=>false, :license=>false, :renewed=>false}
-
-registrant_2.earn_permit
-
-registrant_2.permit?
-#=> true
