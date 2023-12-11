1202410449e359469fe9862de6fcee5cd30ce05f
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-11 10:45:46 -0500

    Refactor: add more tests

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -14,38 +14,55 @@ RSpec.describe FacilityFactory do
       it 'creates Facility objects with correct attributes for Colorado data' do
          colorado = DmvDataService.new.co_dmv_office_locations
          expect(@factory.create_facilities(colorado)).to be_an Array  
-         
-         facilities = @factory.create_facilities(colorado)
-   
-         expect(facilities).to all(be_a(Facility))
-         expect(facilities.first.name).not_to eq(nil)
-         expect(facilities.first.address).not_to eq(nil)
-         expect(facilities.first.phone).not_to eq(nil)
       end
 
       it 'creates Facility objects with correct attributes for New York data' do
          ny_facilities = DmvDataService.new.ny_dmv_office_locations
-         factory = FacilityFactory.new
-   
-         facilities = factory.create_facilities(ny_facilities)
-   
-         expect(facilities).to all(be_a(Facility))
-         expect(facilities.first.name).not_to eq(nil)
-         expect(facilities.first.address).not_to eq(nil)
-         expect(facilities.first.phone).not_to eq(nil)
+         expect(@factory.create_facilities(ny_facilities)).to be_an Array
       end
 
       it 'creates Facility objects with correct attributes for Misouri data' do
          missouri_facilities = DmvDataService.new.mo_dmv_office_locations
-         factory = FacilityFactory.new
+         expect(@factory.create_facilities(missouri_facilities)).to be_an Array
+      end
+   end
    
-         facilities = factory.create_facilities(missouri_facilities)
-   
+   describe '#create_co_facility' do 
+      it 'creates Facility objects with correct attributes for Colorado data' do
+         colorado = DmvDataService.new.co_dmv_office_locations
+                  
+         facilities = @factory.create_facilities(colorado)
+
          expect(facilities).to all(be_a(Facility))
          expect(facilities.first.name).not_to eq(nil)
          expect(facilities.first.address).not_to eq(nil)
          expect(facilities.first.phone).not_to eq(nil)
       end
-   end   
+   end
 
+   describe '#create_ny_facility' do 
+      it 'creates Facility objects with correct attributes for New York data' do
+         ny_facilities = DmvDataService.new.ny_dmv_office_locations
+
+         facilities = @factory.create_facilities(ny_facilities)
+
+         expect(facilities).to all(be_a(Facility))
+         expect(facilities.first.name).not_to eq(nil)
+         expect(facilities.first.address).not_to eq(nil)
+         expect(facilities.first.phone).not_to eq(nil)
+      end
+   end
+
+   describe '#create_mo_facility' do 
+      it 'creates Facility objects with correct attributes for Misouri data' do
+         missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+
+         facilities = @factory.create_facilities(missouri_facilities)
+
+         expect(facilities).to all(be_a(Facility))
+         expect(facilities.first.name).not_to eq(nil)
+         expect(facilities.first.address).not_to eq(nil)
+         expect(facilities.first.phone).not_to eq(nil)
+      end
+   end
 end
