7686c5d8c37a117d40fba7ae38eb1bcc2a12fc87
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2023-12-12 09:36:34 -0700

    added test

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe VehicleFactory do
         wa_ev_registrations = DmvDataService.new.wa_ev_registrations
         vehicles = factory.create_vehicles(wa_ev_registrations)
         expect(vehicles.length).to be_a(Integer)
-        expect(vehicles[0].engine).to eq("ev")
+        expect(vehicles[5].engine).to eq("ev")
         expect(vehicles.first).to be_an_instance_of(Vehicle)
         
     end
