233a095e5dd20bcc9e09756a97ae1ab800214fd9
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-11 20:22:32 -0500

    Feat: Add create_vehicle details to use NY dataset

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,15 +1,40 @@
 class VehicleFactory
 
-   def create_vehicles(data)
-      data.map do |vehicle_data|
-         vehicle_details = {
-            vin: vehicle_data[:vin_1_10],
-            year: vehicle_data[:model_year],
-            make: vehicle_data[:make],
-            model: vehicle_data[:model],
-            engine: :ev,
-         }
-         Vehicle.new(vehicle_details)
-      end
+   def create_vehicles(registration_data)
+      registration_data.map { |vehicle_data| create_vehicle(vehicle_data) }.compact
+   end
+   
+   def create_vehicle(vehicle_data)
+      state = vehicle_data[:state] || vehicle_data[:state_of_residence]
+      
+      if state == "NY"
+         vehicle_details = create_ny_vehicle(vehicle_data)
+      elsif state == "WA"
+         vehicle_details = create_wa_vehicle(vehicle_data)
+      else
+         "Unsupported state: #{state}"
+      end                     
+      vehicle_details.nil? ? nil : Vehicle.new(vehicle_details)
+   end
+   
+   def create_ny_vehicle(data)
+      return nil if data[:record_type] != "VEH"
+      {
+        vin_number: data[:vin],
+        year: data[:model_year],
+        make: data[:make],
+        model: data[:body_type],
+        engine: data[:fuel_type].to_sym
+      }
+    end
+   
+   def create_wa_vehicle(data)
+      {
+         vin_number: data[:vin_1_10],
+         year: data[:model_year],
+         make: data[:make],
+         model: data[:model],
+         engine: :ev
+      }
    end
 end
