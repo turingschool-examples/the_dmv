397d0abbd9dcf5c2a9660c078c9086f89403f0dc
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-11 17:52:20 -0700

    test: all created vehicles are instances of vehicles

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -12,26 +12,20 @@ end
         it "exists" do
             expect(@factory).to be_an_instance_of (VehicleFactory)
         end
-
-        it "starts with no vehicles" do
-            expect(@factory.vehicles).to eq ([])
-        end
     end
     describe "#Creates a vehicles collection" do
         it "creates a collection with no nil or empty values" do
             expect(@factory.create_vehicles(@wa_ev_registrations)).to be_an_instance_of (Array)
             expect(@factory.create_vehicles(@wa_ev_registrations).empty?).to eq(false)
             expect(@factory.create_vehicles(@wa_ev_registrations).include?(nil)).to eq(false)
-            expect(@factory.vehicles.first).to eq(@factory.create_vehicles(@wa_ev_registrations).first)
+            expect(@factory.create_vehicles(@wa_ev_registrations)).to all(be_an_instance_of Vehicle)
         end
         it "assigns the attributes correctly" do
-            expect(@factory.create_vehicles(@wa_ev_registrations).first).to be_an_instance_of (Vehicle)
             expect(@factory.create_vehicles(@wa_ev_registrations).first.vin).to eq (@wa_ev_registrations.first[:vin_1_10])
             expect(@factory.create_vehicles(@wa_ev_registrations).first.year).to eq (@wa_ev_registrations.first[:model_year])
             expect(@factory.create_vehicles(@wa_ev_registrations).first.make).to eq (@wa_ev_registrations.first[:make])
             expect(@factory.create_vehicles(@wa_ev_registrations).first.model).to eq (@wa_ev_registrations.first[:model])
             expect(@factory.create_vehicles(@wa_ev_registrations).first.engine).to eq (:ev)
-            expect(@factory.create_vehicles(@wa_ev_registrations).last).to be_an_instance_of (Vehicle)
             expect(@factory.create_vehicles(@wa_ev_registrations).last.vin).to eq (@wa_ev_registrations.last[:vin_1_10])
             expect(@factory.create_vehicles(@wa_ev_registrations).last.year).to eq (@wa_ev_registrations.last[:model_year])
             expect(@factory.create_vehicles(@wa_ev_registrations).last.make).to eq (@wa_ev_registrations.last[:make])
