903b6eb19cb386247d7f51a3112ee0bf719ac4af
Kevin Gonzalez <kevinglz619@gmail.com>
2023-12-12 13:16:50 -0800

    add mo test, copied ny and co test and make changes if need be

--- spec/facility_creator_spec.rb
+++ spec/facility_creator_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Facility_creator do
       expect(facility_creator).to be_an_instance_of(Facility_creator)
     end 
   end
-
+#create facilities for each, use co as template.
   describe '#create_co_facilities' do 
     it 'is a CO instance of Facility' do 
       facility_creator = Facility_creator.new 
@@ -48,5 +48,24 @@ RSpec.describe Facility_creator do
       expect(facility_creator.create_ny_facilities(new_york_facilities).first.collected_fees).to eq(0)
     end 
   end 
-  #at this point its just copy paste the tests before with the state updated.
+
+  describe '#create_mo_facilities' do 
+    it 'is an object of Facility' do 
+      facility_creator = Facility_creator.new 
+      missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+      
+      expect(facility_creator.create_mo_facilities(missouri_facilities).first).to be_an_instance_of(Facility)
+    end
+
+    it 'can access the facility data' do 
+      facility_creator = Facility_creator.new 
+      missouri_facilities = DmvDataService.new.mo_dmv_office_locations
+
+      expect(facility_creator.create_mo_facilities(missouri_facilities).first.name).not_to be(nil)
+      expect(facility_creator.create_mo_facilities(missouri_facilities).first.address).not_to be(nil)
+      expect(facility_creator.create_mo_facilities(missouri_facilities).first.phone).not_to be(nil)
+      expect(facility_creator.create_mo_facilities(missouri_facilities).first.registered_vehicles).to eq([])
+      expect(facility_creator.create_mo_facilities(missouri_facilities).first.collected_fees).to eq(0)
+    end      
+  end 
 end
