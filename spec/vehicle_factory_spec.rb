72d9cdeb8dd61cff4b3ae8ced00fb82bb2b0ad19
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2023-12-12 05:55:30 -0500

    Create a test for my helper method in my vehicle factory class

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -11,7 +11,29 @@ RSpec.describe VehicleFactory do
     it 'can create vehicles' do
         factory = VehicleFactory.new
         wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+
         expect(factory.create_vehicles(wa_ev_registrations)).to be_an Array
         expect(factory.create_vehicles(wa_ev_registrations)[0]).to be_an_instance_of Vehicle
+    end
+
+    it 'should be able to construct vehicle attributes' do
+        factory = VehicleFactory.new
+        vehicle_data = {
+            :make=> "Tesla",
+            :model=> "model S",
+            :vin_1_10=> "5256635263",
+            :model_year=> "2020"
+        }
+
+        expected_hash = {
+            :make=> "Tesla",
+            :model=> "model S",
+            :vin=> "5256635263",
+            :year=> "2020",
+            :engine=> :ev
+        }
+
+        expect(factory.construct_vehicle_attr_helper(vehicle_data)).to eq(expected_hash)
+
     end
 end
