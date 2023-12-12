71080e4538670531c46d23d1942429ae80ca6b66
Hoa Dam <hoangocdam@gmail.com>
2023-12-11 22:26:44 -0800

    final commit

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -3,7 +3,12 @@ require './lib/vehicle'
 class VehicleFactory
   def create_vehicles(data)
     data.map do |vehicle_data|
-      Vehicle.new(:vin=>vehicle_data[:vin_1_10], :year=>vehicle_data[:model_year], :make=>vehicle_data[:make], :model=>vehicle_data[:model], :engine=>:ev )
+      Vehicle.new(
+        :vin=>vehicle_data[:vin_1_10],
+        :year=>vehicle_data[:model_year],
+        :make=>vehicle_data[:make],
+        :model=>vehicle_data[:model],
+        :engine=>:ev )
     end
   end
 end
