1f52b93ac08fdea43f74e80b890c6b2971100800
Kevin Gonzalez <kevinglz619@gmail.com>
2023-12-11 17:23:58 -0800

    add new york version of the test

--- spec/facility_creator_spec.rb
+++ spec/facility_creator_spec.rb
@@ -1,35 +1,52 @@
 require 'spec_helper'
 
-
 RSpec.describe Facility_creator do 
-    describe '#initialize' do 
-        it 'initializes' do 
-           facility_creator =  Facility_creator.new 
-           
-           expect(facility_creator).to be_an_instance_of(Facility_creator)
-        end 
+  describe '#initialize' do 
+    it 'initializes' do 
+      facility_creator = Facility_creator.new 
+      expect(facility_creator).to be_an_instance_of(Facility_creator)
     end 
-    #checking if its a new instance of facility creator class
+  end
 
-    describe '#create_co_facilities' do 
+  describe '#create_co_facilities' do 
     it 'is a CO instance of Facility' do 
-        facility_creator =  Facility_creator.new 
-        colorado_facilities = DmvDataService.new.co_dmv_office_locations
+      facility_creator = Facility_creator.new 
+      colorado_facilities = DmvDataService.new.co_dmv_office_locations
 
-        expect(facility_creator.create_co_facilities(colorado_facilities).first).to be_an_instance_of(Facility)
-        end
-    end  
-    #checking that create CO facilities works when given CO data.
+      expect(facility_creator.create_co_facilities(colorado_facilities).first).to be_an_instance_of(Facility)
+    end
+
     it 'can access the data' do 
-        facility_creator =  Facility_creator.new 
-        colorado_facilities = DmvDataService.new.co_dmv_office_locations
+      facility_creator = Facility_creator.new 
+      colorado_facilities = DmvDataService.new.co_dmv_office_locations
 
-        expect(facility_creator.create_co_facilities(colorado_facilities).first.name).not_to be(nil)
-        expect(facility_creator.create_co_facilities(colorado_facilities).first.address).not_to be(nil)
-        expect(facility_creator.create_co_facilities(colorado_facilities).first.registered_vehicles).to eq([])
-        expect(facility_creator.create_co_facilities(colorado_facilities).first.collected_fees).to eq(0)
-        expect(facility_creator.create_co_facilities(colorado_facilities).first.services).not_to be(nil)
-        expect(facility_creator.create_co_facilities(colorado_facilities).first.phone).not_to be(nil)
+      expect(facility_creator.create_co_facilities(colorado_facilities).first.name).not_to be(nil)
+      expect(facility_creator.create_co_facilities(colorado_facilities).first.address).not_to be(nil)
+      expect(facility_creator.create_co_facilities(colorado_facilities).first.registered_vehicles).to eq([])
+      expect(facility_creator.create_co_facilities(colorado_facilities).first.collected_fees).to eq(0)
+      expect(facility_creator.create_co_facilities(colorado_facilities).first.services).not_to be(nil)
+      expect(facility_creator.create_co_facilities(colorado_facilities).first.phone).not_to be(nil)
     end 
-end 
-#checking that create_co_facilities facilities have a value(or just not nil)
+  end
+
+  describe '#create_ny_facilities' do 
+    it 'is an object of Facility' do 
+      facility_creator = Facility_creator.new 
+      new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+
+      expect(facility_creator.create_ny_facilities(new_york_facilities).first).to be_an_instance_of(Facility)
+    end
+
+    it 'can access the data' do 
+      facility_creator = Facility_creator.new 
+      new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+
+      expect(facility_creator.create_ny_facilities(new_york_facilities).first.name).not_to be(nil)
+      expect(facility_creator.create_ny_facilities(new_york_facilities).first.address).not_to be(nil)
+      expect(facility_creator.create_ny_facilities(new_york_facilities).first.phone).not_to be(nil)
+      expect(facility_creator.create_ny_facilities(new_york_facilities).first.registered_vehicles).to eq([])
+      expect(facility_creator.create_ny_facilities(new_york_facilities).first.collected_fees).to eq(0)
+    end 
+  end 
+  #at this point its just copy paste the tests before with the state updated.
+end
