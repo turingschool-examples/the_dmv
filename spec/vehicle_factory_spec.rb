05899d2c31f9c115d135d7ec86fa2fb2747007a5
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2023-12-12 15:41:48 -0600

    Refactor: before each code moved into test

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper'
 
 RSpec.describe VehicleFactory do
-    before (:each) do
-        @factory = VehicleFactory.new
-        @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
-    end
-
     it 'returns a vehicle object' do
-        expect(@factory.create_vehicles(@wa_ev_registrations)).to all(be_an_instance_of(Vehicle))
+        factory = VehicleFactory.new
+        wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+        
+        expect(factory.create_vehicles(wa_ev_registrations)).to all(be_an_instance_of(Vehicle))
     end
 end
