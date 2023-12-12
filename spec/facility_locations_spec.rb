ae5cdd4fbce5143e70263c7189cb58b806909a4b
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2023-12-11 21:08:50 -0600

    Test: MO and NY facility objects

--- spec/facility_locations_spec.rb
+++ spec/facility_locations_spec.rb
@@ -10,5 +10,7 @@ RSpec.describe FacilityLocations do
 
     it 'returns facility objects' do
         expect(@dmv.co_dmv_offices(@co_dmv_office_locations)).to all(be_an_instance_of(Facility))
+        expect(@dmv.ny_dmv_offices(@ny_dmv_office_locations)).to all(be_an_instance_of(Facility))
+        expect(@dmv.mo_dmv_offices(@mo_dmv_office_locations)).to all(be_an_instance_of(Facility))
     end
 end
