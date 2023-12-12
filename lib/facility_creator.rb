53941d33c9b620442818f1fdec82a5e0f4744faa
Kevin Gonzalez <kevinglz619@gmail.com>
2023-12-11 17:24:36 -0800

    add new york method to facility creator class file

--- lib/facility_creator.rb
+++ lib/facility_creator.rb
@@ -1,9 +1,8 @@
 class Facility_creator
     def create_co_facilities(dmv_data)
         dmv_data.map do |facility_data|
-            Facility.new(
-            #have to put it as an object here
-            {
+            Facility.new({
+                #have to put it as an object here
             name: facility_data[:dmv_office],
             address: {
                 street: facility_data[:address_li],
@@ -20,5 +19,24 @@ class Facility_creator
             }) 
         end 
     end
-end
+
         # we are able to take dmv data and change it so the facility class can use
+        def create_ny_facilities(dmv_data)
+            dmv_data.map do |facility_data|
+                Facility.new({
+                name: facility_data[:office_name],
+                address: {
+                    street: facility_data[:street_address_1],
+                    city: facility_data[:city],
+                    state: facility_data[:state],
+                    zip_code: facility_data[:zip_code]
+                },
+                phone: facility_data[:public_phone_number],
+                registered_vehicles: [],
+                collected_fees: 0 
+                }) 
+            end 
+        end 
+        #replicate tests but account for key changes off gov website
+        
+end
