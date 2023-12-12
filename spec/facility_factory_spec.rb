9541c9c221becc1b77a8c5285b274231818be00f
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-12 05:52:45 -0700

    feat: Add daily hours attribute tests for CO, NY and MO locations, and observed_holidays for MO locations

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -25,9 +25,11 @@ RSpec.describe FacilityFactory do
         expect(@factory.create_facilities(@co_dmv_locations).first.name).to eq (@co_dmv_locations.first[:dmv_office])
         expect(@factory.create_facilities(@co_dmv_locations).first.address).to eq ((@co_dmv_locations.first[:address_li].to_s)+" "+(@co_dmv_locations.first[:address__1].to_s)+" "+(@co_dmv_locations.first[:city])+" "+(@co_dmv_locations.first[:state])+" "+(@co_dmv_locations.first[:zip]))
         expect(@factory.create_facilities(@co_dmv_locations).first.phone).to eq (@co_dmv_locations.first[:phone])
+        expect(@factory.create_facilities(@co_dmv_locations).first.daily_hours).to eq (@co_dmv_locations.first[:hours])
         expect(@factory.create_facilities(@co_dmv_locations).last.name).to eq (@co_dmv_locations.last[:dmv_office])
         expect(@factory.create_facilities(@co_dmv_locations).last.address).to eq ((@co_dmv_locations.last[:address_li].to_s)+" "+(@co_dmv_locations.last[:address__1].to_s)+" "+(@co_dmv_locations.last[:city])+" "+(@co_dmv_locations.last[:state])+" "+(@co_dmv_locations.last[:zip]))
         expect(@factory.create_facilities(@co_dmv_locations).last.phone).to eq (@co_dmv_locations.last[:phone])
+        expect(@factory.create_facilities(@co_dmv_locations).last.daily_hours).to eq (@co_dmv_locations.last[:hours])
     end
     end
 
@@ -44,6 +46,7 @@ RSpec.describe FacilityFactory do
         expect(@factory.create_facilities(@ny_dmv_locations).last.name).to eq (@ny_dmv_locations.last[:office_name])
         expect(@factory.create_facilities(@ny_dmv_locations).last.address).to eq ((@ny_dmv_locations.last[:street_address_line_1].to_s)+" "+(@ny_dmv_locations.last[:street_address_line_2].to_s)+" "+(@ny_dmv_locations.last[:city])+" "+(@ny_dmv_locations.last[:state])+" "+(@ny_dmv_locations.last[:zip_code]))
         expect(@factory.create_facilities(@ny_dmv_locations).last.phone).to eq (@ny_dmv_locations.last[:public_phone_number])
+        expect(@factory.create_facilities(@ny_dmv_locations).last.daily_hours).to eq (@ny_dmv_locations.last[:monday_beginning_hours]+" to "+@ny_dmv_locations.last[:monday_ending_hours]+", "+@ny_dmv_locations.last[:tuesday_beginning_hours]+" to "+@ny_dmv_locations.last[:tuesday_ending_hours]+",  "+@ny_dmv_locations.last[:wednesday_beginning_hours]+" to"+@ny_dmv_locations.last[:wednesday_ending_hours]+", "+@ny_dmv_locations.last[:thursday_beginning_hours]+" to"+@ny_dmv_locations.last[:thursday_ending_hours]+", "+@ny_dmv_locations.last[:friday_beginning_hours]+" to"+@ny_dmv_locations.last[:friday_ending_hours])
     end
     end
     describe"#creates factories collection for MO Locations" do
@@ -56,9 +59,13 @@ RSpec.describe FacilityFactory do
         expect(@factory.create_facilities(@mo_dmv_locations).first.name).to eq (@mo_dmv_locations.first[:name])
         expect(@factory.create_facilities(@mo_dmv_locations).first.address).to eq ((@mo_dmv_locations.first[:address1].to_s)+" "+(@mo_dmv_locations.first[:city])+" "+(@mo_dmv_locations.first[:state])+" "+(@mo_dmv_locations.first[:zipcode]))
         expect(@factory.create_facilities(@mo_dmv_locations).first.phone).to eq (@mo_dmv_locations.first[:phone])
+        expect(@factory.create_facilities(@mo_dmv_locations).first.daily_hours).to eq (@mo_dmv_locations.first[:daysopen])
+        expect(@factory.create_facilities(@mo_dmv_locations).first.observed_holidays).to eq (@mo_dmv_locations.first[:holidaysclosed])
         expect(@factory.create_facilities(@mo_dmv_locations).last.name).to eq (@mo_dmv_locations.last[:name])
         expect(@factory.create_facilities(@mo_dmv_locations).last.address).to eq ((@mo_dmv_locations.last[:address1].to_s)+" "+(@mo_dmv_locations.last[:city])+" "+(@mo_dmv_locations.last[:state])+" "+(@mo_dmv_locations.last[:zipcode]))
         expect(@factory.create_facilities(@mo_dmv_locations).last.phone).to eq (@mo_dmv_locations.last[:phone])
+        expect(@factory.create_facilities(@mo_dmv_locations).last.daily_hours).to eq (@mo_dmv_locations.last[:daysopen])
+        expect(@factory.create_facilities(@mo_dmv_locations).last.observed_holidays).to eq (@mo_dmv_locations.last[:holidaysclosed])
     end
     end
 end
