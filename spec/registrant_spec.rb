dd9154320918e7d098527b2d6af1ba1317e00683
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-12 09:22:48 -0500

    Refactor: include more scenarios in the tests

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -59,7 +59,6 @@ RSpec.describe Registrant do
          expect(@bruce.license_data).to eq ({:written=>true, :license=>false, :renewed=>false})
 
          @penny.earn_permit
-         expect(@penny.permit?).to eq true
          expect(facility_1.administer_written_test(@penny)).to eq true
          expect(@penny.license_data).to eq ({:written=>true, :license=>false, :renewed=>false})
       end
@@ -73,6 +72,11 @@ RSpec.describe Registrant do
          facility_1.add_service('Road Test')
          expect(facility_1.administer_road_test(@bruce)).to eq true
          expect(@bruce.license_data).to eq ({:written=>true, :license=>true, :renewed=>false})
+
+         @penny.earn_permit
+         expect(facility_1.administer_written_test(@penny)).to eq true
+         expect(facility_1.administer_road_test(@penny)).to eq true
+         expect(@penny.license_data).to eq ({:written=>true, :license=>true, :renewed=>false})
       end
 
       it 'can get their drivers license renewed' do
@@ -82,12 +86,20 @@ RSpec.describe Registrant do
          expect(@bruce.license_data).to eq ({:written=>true, :license=>false, :renewed=>false})
          
          facility_1.add_service('Road Test')
-         expect(facility_1.administer_road_test(@bruce)).to eq true
+         facility_1.administer_road_test(@bruce)
          expect(@bruce.license_data).to eq ({:written=>true, :license=>true, :renewed=>false})
+
+         @penny.earn_permit
+         facility_1.administer_written_test(@penny)
+         facility_1.administer_road_test(@penny)
+         expect(@penny.license_data).to eq ({:written=>true, :license=>true, :renewed=>false})
 
          facility_1.add_service('Renew License')
          expect(facility_1.renew_drivers_license(@bruce)).to eq true
          expect(@bruce.license_data).to eq ({:written=>true, :license=>true, :renewed=>true})
+
+         expect(facility_1.renew_drivers_license(@penny)).to eq true
+         expect(@penny.license_data).to eq ({:written=>true, :license=>true, :renewed=>true})
       end
    end
 end
