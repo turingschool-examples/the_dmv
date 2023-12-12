149a9fb72e7c24255a6c329c65559c57ec53da52
Hoa Dam <hoangocdam@gmail.com>
2023-12-11 21:45:20 -0800

    feat:return the daily hours for NY data

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -19,5 +19,29 @@ class FacilityFactory
     end
   end
 
+  def daily_hours_ny(facility_data)
+    schedule= {
+                :M=>"#{facility_data[:monday_beginning_hours]}-#{facility_data[:monday_ending_hours]}",
+                :T=>"#{facility_data[:tuesday_beginning_hours]}-#{facility_data[:tuesday_ending_hours]}",
+                :W=>"#{facility_data[:wednesday_beginning_hours]}-#{facility_data[:wednesday_ending_hours]}",
+                :Th=>"#{facility_data[:thursday_beginning_hours]}-#{facility_data[:thursday_ending_hours]}",
+                :F=>"#{facility_data[:friday_beginning_hours]}-#{facility_data[:friday_ending_hours]}"
+              }
+
+    if facility_data.key?(:saturday_beginning_hours) && facility_data.key?(:saturday_ending_hours)
+      schedule[:SAT] = "#{facility_data[:saturday_beginning_hours]}-#{facility_data[:saturday_ending_hours]}"
+    end
+
+    if facility_data[:saturday_beginning_hours] = "CLOSED" && facility_data[:saturday_ending_hours] == "CLOSED"
+      schedule[:SAT] = "CLOSED"
+    end
+
+    grouped_schedule = Hash.new([])
+    schedule.each do |day, hour|
+      grouped_schedule[hour] = grouped_schedule[hour] + [day]
+    end
+    grouped_schedule.map { |hours, days| "#{days.join(', ')}: #{hours}"}
+  end
+
 
 end
