c20b9dd197225af2bee3afac06b2f3cdf18114f0
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2023-12-11 18:06:40 -0600

    Feat: create vehicles array

---
+++ lib/vehiclefactory.rb
@@ -0,0 +1,13 @@
+
+require_relative 'dmv_data_service'
+class VehicleFactory
+    attr_reader :created_vehicles
+
+    def initialize
+        @created_vehicles = []
+    end
+
+    def create_vehicles(registration)
+        #code
+    end
+end
