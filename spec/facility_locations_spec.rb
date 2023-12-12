fe4197b90c138605324e47c7d4a5e951a9a5e2f8
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2023-12-11 21:03:28 -0600

    Test: fix facility objects

--- spec/facility_locations_spec.rb
+++ spec/facility_locations_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe FacilityLocations do
+    before(:each) do
+        @dmv = FacilityLocations.new
+        @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+        @ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
+        @mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
+    end
+
     it 'returns facility objects' do
-        expect(DmvDataService.new.co_dmv_office_locations).to all(be_an_instance_of(Facility))
+        expect(@dmv.co_dmv_offices(@co_dmv_office_locations)).to all(be_an_instance_of(Facility))
     end
 end
