ac4fcf608260237955518c9c0ac6ba65ba5127b8
NeilTheSeal <neil.hendren@gmail.com>
2024-02-08 16:50:41 -0700

    fix: Errors from last commit's added tests

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,12 +1,15 @@
 # Documentation for class Facility
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles,
+              :collected_fees
 
   def initialize(info)
     @name = info[:name]
     @address = info[:address]
     @phone = info[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
