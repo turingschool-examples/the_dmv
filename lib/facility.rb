36d7fdaae4cf4d405d4c7535763014074ec879d9
Grant Davis <grantdavis303@gmail.com>
2024-02-08 16:18:22 -0700

    Correct instance variables in facility file

--- lib/facility.rb
+++ lib/facility.rb
@@ -2,14 +2,14 @@ class Facility
 
   attr_reader :name, :address, :phone, :services
 
-  def initialize(information)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(facility_info)
+    @name = facility_info[:name] 
+    @address = facility_info[:address]
+    @phone = facility_info[:phone]
     @services = []
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
   end
 
