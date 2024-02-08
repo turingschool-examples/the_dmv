0e7a96e89128affea4ba7a7e6febeb1907ac7e63
Grant Davis <grantdavis303@gmail.com>
2024-02-08 16:31:48 -0700

    Spec File Formatting

--- spec/dmv_data_service_spec.rb
+++ spec/dmv_data_service_spec.rb
@@ -1,45 +1,60 @@
 require 'spec_helper'
 
 RSpec.describe DmvDataService do
+
   before(:each) do
     @dds = DmvDataService.new
   end
+
   describe '#initialize' do
+
     it 'can initialize' do
       expect(@dds).to be_an_instance_of(DmvDataService)
     end
+
   end
 
   describe '#load_data' do
+
     it 'can load data from a given source' do
       source = 'https://data.colorado.gov/resource/dsw3-mrn4.json'
       data_response = @dds.load_data(source)
       expect(data_response).to be_an_instance_of(Array)
       expect(data_response.size).to be_an(Integer)
     end
+
   end
 
   describe '#wa_ev_registrations' do
+
     it 'can load washington ev registration data' do
       expect(@dds.wa_ev_registrations.size).to be_an(Integer)
     end
+
   end
 
   describe '#co_dmv_office_locations' do
+
     it 'can load colorado dmv office locations' do
       expect(@dds.co_dmv_office_locations.size).to be_an(Integer)
     end
+
   end
 
   describe '#ny_dmv_office_locations' do
+  
     it 'can load new york dmv office locations' do
       expect(@dds.ny_dmv_office_locations.size).to be_an(Integer)
     end
+
   end
 
   describe '#mo_dmv_office_locations' do
+
     it 'can load missouri dmv office locations' do
       expect(@dds.mo_dmv_office_locations.size).to be_an(Integer)
     end
+
   end
+  
 end
--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -1,6 +1,11 @@
-require 'spec_helper'
+require 'spec_helper.rb'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
 
 RSpec.describe Dmv do
+
   before(:each) do
     @dmv = Dmv.new
     @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
@@ -9,21 +14,26 @@ RSpec.describe Dmv do
   end
 
   describe '#initialize' do
+  
     it 'can initialize' do
       expect(@dmv).to be_an_instance_of(Dmv)
       expect(@dmv.facilities).to eq([])
     end
+
   end
 
   describe '#add facilities' do
+
     it 'can add available facilities' do
       expect(@dmv.facilities).to eq([])
       @dmv.add_facility(@facility_1)
       expect(@dmv.facilities).to eq([@facility_1])
     end
+
   end
 
   describe '#facilities_offering_service' do
+
     it 'can return list of facilities offering a specified Service' do
       @facility_1.add_service('New Drivers License')
       @facility_1.add_service('Renew Drivers License')
@@ -39,5 +49,7 @@ RSpec.describe Dmv do
 
       expect(@dmv.facilities_offering_service('Road Test')).to eq([@facility_2, @facility_3])
     end
+    
   end
+
 end
