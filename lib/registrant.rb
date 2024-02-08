a451154b52efaf0270d93c715918b5457cfb82ed
Billy Wallace <wallace.billy1@gmail.com>
2024-02-08 15:43:19 -0700

    Create test for initializing Registrant class and build initialize method

---
+++ lib/registrant.rb
@@ -0,0 +1,12 @@
+class Registrant
+    attr_reader :name, 
+                :age, 
+                :permit, 
+                :license_data
+
+    def initialize(name, age, permit = false)
+        @name = name
+        @age = age
+        @permit = permit
+    end
+end
