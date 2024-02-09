b1af36e2ce1895e7ab1c2c024c9a487fd9d4f43a
Jared Hobson <jaredmhobson@gmail.com>
2024-02-08 23:34:24 -0800

    Add test for vehicles attr_reader method

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe VehicleFactory do
+  let (:factory) { VehicleFactory.new }
+  let (:wa_ev_registrations) { DmvDataService.new.wa_ev_registrations }
   it 'exists' do
-    factory = VehicleFactory.new
-    wa_ev_registrations = DmvDataService.new.wa_ev_registrations
-
     expect(factory).to be_an_instance_of(VehicleFactory)
+  end
+
+  it 'has no vehicles by default' do
+    expect(factory.vehicles).to be_empty
   end
 end
