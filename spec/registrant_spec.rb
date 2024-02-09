b416ce4fd6340f6886277a307457c93189332ff1
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-09 15:06:40 -0700

    Creates a registrant and the ability to earn a permit

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -9,6 +9,33 @@ RSpec.describe Registrant do
   describe '#initialize' do
     it 'can initialize' do
       expect(@registrant_1).to be_a(Registrant)
+      expect(@registrant_2).to be_a(Registrant)
+    end
+
+    it 'has attributes' do
+      expect(@registrant_1.name).to eq("Bruce")
+      expect(@registrant_1.age).to eq(18)
+      expect(@registrant_1.permit).to eq(true)
+      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@registrant_2.name).to eq("Penny")
+      expect(@registrant_2.age).to eq(15)
+      expect(@registrant_2.permit).to eq(false)
+      expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+  end
+
+  describe '#permit?' do
+    it 'has a permit or not' do
+      expect(@registrant_1.permit?).to eq(true)
+      expect(@registrant_2.permit?).to eq(false)
+    end
+  end
+
+  describe '#earn_permit' do
+    it 'earns a permit' do
+      @registrant_2.earn_permit
+
+      expect(@registrant_2.permit?).to eq(true)
     end
   end
 
