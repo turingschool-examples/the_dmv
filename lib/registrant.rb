40c784ea6109214bb4e305398a2c7b45cc447bca
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-08 20:21:48 -0700

    Add registrant class

---
+++ lib/registrant.rb
@@ -0,0 +1,19 @@
+class Registrant
+  attr_reader :license_data
+              :written
+              :renewed
+
+  def initialize(name, age, permit = false)
+    @name = name 
+    @age = age
+    @permit = permit 
+  end
+
+  def 
+  end
+
+  
+  
+
+
+end
