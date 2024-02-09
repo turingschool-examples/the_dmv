6b116b3a1543b58e0cd822b347738fbff78ec9c5
Grant Davis <grantdavis303@gmail.com>
2024-02-08 17:42:06 -0700

    Initialize the Registrant class

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,3 +1,16 @@
 class Registrant
 
+    attr_reader :name, :age, :permit, :license_data
+
+    def initialize(name, age, permit = false, license_data = {})
+        @name = name
+        @age = age
+        @permit = permit
+        @license_data = {
+            :written => false,
+            :license => false,
+            :renewed => false
+        }
+    end
+
 end
