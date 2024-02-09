c23752fdfad8250655acec4a88c6b90dd484e947
Billy Wallace <wallace.billy1@gmail.com>
2024-02-09 13:16:37 -0700

    Create register_vehicle method

--- lib/facility.rb
+++ lib/facility.rb
@@ -3,7 +3,8 @@ class Facility
               :address, 
               :phone, 
               :services,
-              :collected_fees
+              :collected_fees,
+              :registered_vehicles
 
   def initialize(info)
     @name = info[:name]
@@ -11,9 +12,14 @@ class Facility
     @phone = info[:phone]
     @services = []
     @collected_fees = 0
+    @registered_vehicles = []
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle) 
+    @registered_vehicles << vehicle
   end
 end
