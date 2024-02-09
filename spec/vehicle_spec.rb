9b049c558ec8f646cf7340ca0fcc1da5c424e615
Jared Hobson <jaredmhobson@gmail.com>
2024-02-08 16:50:34 -0800

    Update instantiations for vehicle spec

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -1,36 +1,36 @@
 require 'spec_helper'
 
 RSpec.describe Vehicle do
-  before(:each) do
-    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
-    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
-    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
-  end
+  let (:facility_1) { Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'}) }
+  let (:facility_2) { Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'}) }
+  let (:cruz) { Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice}) }
+  let (:bolt) { Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev}) }
+  let (:camaro) { Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice}) }
   describe '#initialize' do
     it 'can initialize' do
-      expect(@cruz).to be_an_instance_of(Vehicle)
-      expect(@cruz.vin).to eq('123456789abcdefgh')
-      expect(@cruz.year).to eq(2012)
-      expect(@cruz.make).to eq('Chevrolet')
-      expect(@cruz.model).to eq('Cruz')
-      expect(@cruz.engine).to eq(:ice)
-      expect(@cruz.registration_date).to eq(nil)
+      expect(cruz).to be_an_instance_of(Vehicle)
+      expect(cruz.vin).to eq('123456789abcdefgh')
+      expect(cruz.year).to eq(2012)
+      expect(cruz.make).to eq('Chevrolet')
+      expect(cruz.model).to eq('Cruz')
+      expect(cruz.engine).to eq(:ice)
+      expect(cruz.registration_date).to eq(nil)
     end
   end
 
   describe '#antique?' do
     it 'can determine if a vehicle is an antique' do
-      expect(@cruz.antique?).to eq(false)
-      expect(@bolt.antique?).to eq(false)
-      expect(@camaro.antique?).to eq(true)
+      expect(cruz.antique?).to eq(false)
+      expect(bolt.antique?).to eq(false)
+      expect(camaro.antique?).to eq(true)
     end
   end
 
   describe '#electric_vehicle?' do
     it 'can determine if a vehicle is an ev' do
-      expect(@cruz.electric_vehicle?).to eq(false)
-      expect(@bolt.electric_vehicle?).to eq(true)
-      expect(@camaro.electric_vehicle?).to eq(false)
+      expect(cruz.electric_vehicle?).to eq(false)
+      expect(bolt.electric_vehicle?).to eq(true)
+      expect(camaro.electric_vehicle?).to eq(false)
     end
   end
 end
