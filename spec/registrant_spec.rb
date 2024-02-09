39e8b8f0fedbd3fdaeb596dcf319277870851772
Grant Davis <grantdavis303@gmail.com>
2024-02-08 17:37:07 -0700

    Test for if registrants have a permit or not

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -30,4 +30,13 @@ RSpec.describe Registrant do
         expect(registrant_2.age).to eq (15)
     end
 
+
+    it 'registrants have a permit' do
+        registrant_1 = Registrant.new('Bruce', 18, true )
+        registrant_2 = Registrant.new('Penny', 15 )
+
+        expect(registrant_1.permit?).to eq (true)
+        expect(registrant_2.permit?).to eq (false)
+    end
+
 end
