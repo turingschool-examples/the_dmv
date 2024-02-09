dfd1230a3fb98ea66b6575480a947e6a504edfb1
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-09 16:14:57 -0700

    Add instance variables

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,18 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name,
+              :address, 
+              :phone, 
+              :services,
+              :registard_vehicles,
+              :collected_fees
 
     def initialize(info)
     @name = info[:name]
     @address = info[:address]
     @phone = info[:phone]
     @services = []
+    @registard_vehicles =[]
+    @collected_fees = 0
   end
 
   def add_service(service)
