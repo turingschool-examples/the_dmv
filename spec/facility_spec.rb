08ecb15fd0e2f88ce890e719efa30675bb526198
Billy Wallace <wallace.billy1@gmail.com>
2024-02-09 13:30:08 -0700

    Add test for collecting registration fees based on vehicle

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -65,4 +65,26 @@ RSpec.describe Facility do
     end
   end 
 
+  describe '#register vehicle' do
+    it 'collects a fee based on the vehicle' do
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+      @facility.add_service('Vehicle Registration')
+      @facility.register_vehicle(cruz)
+
+      expect(@facility.collected_fees).to eq 100
+
+      @facility.register_vehicle(bolt)
+
+      expect(@facility.collected_fees).to eq 300
+
+      @facility.register_vehicle(camaro)
+      
+      expect(@facility.collected_fees).to eq 325
+
+
+    end
+  end 
+
 end
