63658ef7558564748defe0ae12b72ee8a09089b1
Jared Hobson <jaredmhobson@gmail.com>
2024-02-08 23:38:01 -0800

    Add create vehicles method for vehicle factory class

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -4,4 +4,10 @@ class VehicleFactory
   def initialize
     @vehicles = []
   end
+
+  def create_vehicles(data_service)
+    @vehicles = data_service.map do |vehicle_info|
+      Vehicle.new(vehicle_info)
+    end
+  end
 end
