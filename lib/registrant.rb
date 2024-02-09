8b5e6de23ba586a31aa695e2d7d580532e894465
Steddman Bell <steddfast@gmail.com>
2024-02-08 16:34:46 -0800

    Created Registrant class and completed refactoring codebase

---
+++ lib/registrant.rb
@@ -0,0 +1,9 @@
+class Registrant
+    attr_reader
+
+    def initialize(name, age, permit=false)
+        @name = name
+        @age = age
+        @license_data = hash[written:, license:, renewed:]
+    end
+end
