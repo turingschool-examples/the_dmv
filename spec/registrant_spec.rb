519c0f229d8e1a9a3ccc39a022483dbe16be017b
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-09 14:14:22 -0700

    Fix permit within the initialize

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,15 @@
+require './lib/registrant'
+
+RSpec.describe Registrant do
+  before(:each) do
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_2 = Registrant.new('Penny', 15 )
+  end
+
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@registrant_1).to be_a(Registrant)
+    end
+  end
+
+end
