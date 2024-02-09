36cc77c32c6c6cd3e6d6f0ea39926bcf5e0f8454
NeilTheSeal <neil.hendren@gmail.com>
2024-02-09 13:52:30 -0700

    Add config options to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+# typed: ignore
+
 require "pry"
 require "./lib/dmv"
 require "./lib/facility"
@@ -6,3 +8,7 @@ require "./lib/dmv_data_service"
 require "./lib/registrant"
 require "./lib/vehicle_factory"
 require "date"
+require "rspec"
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
