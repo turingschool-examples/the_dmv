0baaf4421ab3bf164a1dd8b5b81a9e99536b3e35
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-09 17:04:57 -0700

    Condense #initialize

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -10,9 +10,7 @@ RSpec.describe Registrant do
     it 'can initialize' do
       expect(@registrant_1).to be_a(Registrant)
       expect(@registrant_2).to be_a(Registrant)
-    end
 
-    it 'has attributes' do
       expect(@registrant_1.name).to eq("Bruce")
       expect(@registrant_1.age).to eq(18)
       expect(@registrant_1.permit).to eq(true)
