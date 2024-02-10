ee7a9958052f3f6d52a9ec4b4c6bd6919fb8f6bd
Jared Hobson <jaredmhobson@gmail.com>
2024-02-09 21:07:01 -0800

    Add test for new york facilities to facility factory spec

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -13,20 +13,27 @@ RSpec.describe FacilityFactory do
     expect(factory.facilities).to be_empty
   end
 
-  it 'can create facilities' do
-    expect(factory.facilities).to be_empty
+  describe 'creating facilities' do
+    it 'can create facilities from Colorado' do
+      expect(factory.facilities).to be_empty
 
-    factory.create_facility(colorado_facilities)
+      factory.create_facility(colorado_facilities)
 
-    expect(factory.facilities[0]).to be_an_instance_of(Facility)
-    expect(factory.facilities[0].address).not_to be(nil)
-    expect(factory.facilities[0].name).not_to be(nil)
-    expect(factory.facilities[0].phone).not_to be(nil)
+      expect(factory.facilities[0]).to be_an_instance_of(Facility)
+      expect(factory.facilities[0].address).not_to be(nil)
+      expect(factory.facilities[0].name).not_to be(nil)
+      expect(factory.facilities[0].phone).not_to be(nil)
+    end
+
+    it 'can create facilities from New York' do
+      expect(factory.facilities).to be_empty
+
+      factory.create_facility(new_york_facilities)
+
+      expect(factory.facilities[0]).to be_an_instance_of(Facility)
+      expect(factory.facilities[0].address).not_to be(nil)
+      expect(factory.facilities[0].name).not_to be(nil)
+      expect(factory.facilities[0].phone).not_to be(nil)
+    end
   end
-
-  # it 'can create facilities from different data sources' do
-  #   expect(factory.facilities).to be_empty
-
-  #   factory.create_facility(new_york_facilities)
-  # end
 end
