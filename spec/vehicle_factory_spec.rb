3b3fa4f8242766c3c61347866d89c199be9021b4
Jared Hobson <jaredmhobson@gmail.com>
2024-02-09 17:38:14 -0800

    Update create vehicle test to accurately look at vehicles attribute

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe VehicleFactory do
   it 'can parse registration data from JSON to create vehicles and change their engine to ev if it is nil' do
     vehicles = factory.create_vehicles(wa_ev_registrations)
 
-    expect(vehicles[5].engine).to eq(:ev)
+    expect(factory.vehicles[0].engine).to eq(:ev)
 
-    expect(vehicles[0]).to be_an_instance_of(Vehicle)
+    expect(factory.vehicles[0]).to be_an_instance_of(Vehicle)
   end
 end
