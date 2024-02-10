193f1f70de84b606d7a3824cd2052e925d38c9d3
Jared Hobson <jaredmhobson@gmail.com>
2024-02-09 17:40:29 -0800

    Update create vehicles method to correctly push vehicles into vehicles attribute for vehicle factory class

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -6,11 +6,11 @@ class VehicleFactory
   end
 
   def create_vehicles(data_service)
-    @vehicles = data_service.map do |vehicle_info|
+    data_service.each do |vehicle_info|
       vehicle_info[:engine] = :ev if vehicle_info[:engine] == nil
       vehicle_info[:year] = vehicle_info[:model_year]
       vehicle_info[:vin] = vehicle_info[:vin_1_10]
-      Vehicle.new(vehicle_info)
+      @vehicles << Vehicle.new(vehicle_info)
     end
   end
 end
