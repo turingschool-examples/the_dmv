da900f0743b055060bd8328f1c88e8b0f1c56d3c
NeilTheSeal <neil.hendren@gmail.com>
2024-02-10 15:08:00 -0700

    Add first specs for vehicle stat functionality

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -4,7 +4,7 @@ require "rspec"
 require "./spec/spec_helper"
 require "./lib/vehicle_factory"
 
-RSpec.describe VehicleFactory do
+RSpec.describe VehicleFactory do # rubocop:disable Metrics/BlockLength
   before(:each) do
     @vehicle_factory = VehicleFactory.new
     @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
@@ -31,5 +31,24 @@ RSpec.describe VehicleFactory do
       @vehicle_factory.create_vehicles(@wa_ev_registrations, :ev)
       expect(@vehicle_factory.vehicles[0]).to be_an_instance_of(Vehicle)
     end
+  end
+
+  describe "#vehicle registration data" do
+    it "initializes with nil empty data set" do
+      @vehicle_factory.vehicle_data.each_value do |value|
+        expect(value).to eq(nil)
+      end
+    end
+    it "counts the number of vehicles for each make" do
+      @vehicle_factory.create_vehicles(@wa_ev_registrations, :ev)
+      makes_and_models_data = @vehicle_factory.retrieve_vehicle_makes_and_models
+      total_make_count = 0
+      makes_and_models_data.each_value do |make_data|
+        total_make_count += make_data[:total]
+      end
+      expect(total_make_count).to eq(@vehicle_factory.vehicles.count)
+    end
+    # it "counts the number of vehicles for each model" do
+    # end
   end
 end
