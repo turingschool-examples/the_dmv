757992a6fa4bc6e5c57b679c0858e8a02ecb2568
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-09 17:42:25 -0700

    add #register_vehicle(vehicle)

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,20 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles, :collected_fees
 
   def initialize(attributes)
     @name = attributes[:name]
     @address = attributes[:address]
     @phone = attributes[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    @registered_vehicles << vehicle
   end
 end
