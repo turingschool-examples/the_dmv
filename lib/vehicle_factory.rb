cdc1197f9429eaf1ecbb676ae579dc6def9e3f6a
Steddman Bell <steddfast@gmail.com>
2024-02-10 23:23:35 -0800

    Updated vehicle factory spec file

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -5,10 +5,11 @@ class VehicleFactory
         @vehicles = []
     end
 
-    def create_vehicles(data_service)
-        @vehicles = data_service.map do |vehicle_info|
-            vehicle_info[:eng] = :ev if vehicle_info[:engine] == nil
-            vehicle_info[:year] = vehicle_info[:model_year]
-            vehicle_info[:vin] = vehicle_info[1-10]
-        end
-    end
+    # def create_vehicles(data_service)
+    #     @vehicles = data_service.map do |vehicle_info|
+    #         vehicle_info[:eng] = :ev if vehicle_info[:engine] == nil
+    #         vehicle_info[:make] = vehicle_info[:make]
+    #         vehicle_info[:year] = vehicle_info[:model_year]
+    #         vehicle_info[:vin] = vehicle_info[1-10]
+    #     end
+    # end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,4 +1,18 @@
-require './lib/vehicle_data'
+require './lib/vehicle'
+require './lib/vehicle_factory'
 require './dmv_data_service'
 
+RSpec.describe VehicleFactory do
+    before(:each) do
+        @factory = VehicleFactory.new
+        @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
 
+    end
+
+    describe '#initialize'do
+        it 'exists'do
+        expect(@factory).to be_an_instance_of(VehicleFactory)
+        expect(@wa_ev_registrations).to be_an_instance_of(DmvDataService)
+        end
+    end
+end
