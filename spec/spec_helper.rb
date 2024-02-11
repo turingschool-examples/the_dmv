e840fa3d7e539bfae787cfb9eae35386ed2f1016
Grant Davis <grantdavis303@gmail.com>
2024-02-10 17:01:17 -0700

    Added Vehicle Factory to Spec Helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require './lib/dmv_data_service.rb'
 require './lib/facility.rb'
 require './lib/registrant.rb'
 require './lib/vehicle.rb'
+require './lib/vehicle_factory.rb'
 
 RSpec.configure do |config|
     config.formatter = :documentation
