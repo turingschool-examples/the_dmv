0006e37a5350ba558b19d8f0a93800e1395f2217
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-11 01:22:16 -0700

    Add without administering the written test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -7,7 +7,9 @@ RSpec.describe Facility do
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
-  
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_2 = Registrant.new('Penny', 15 )
+    @registrant_3 = Registrant.new('Tucker', 15 )
   end
   describe '#initialize' do
     it 'can initialize' do
@@ -95,6 +97,27 @@ RSpec.describe Facility do
       expect(@facility_2.collected_fees).to eq(0)
       
     end
+  end
+
+  
+  describe '#Written test' do
+    it 'Registrent 1 without adding service written test' do
+      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(@registrant_1.permit?).to eq(true)
+      expect(@facility.administer_written_test(@registrant_1)).to eq(false)
+      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+
+    it'is administering the written test' do
+      @facility.add_service('Written Test')
+      expect(@facility.administer_written_test(@registrant_1)).to eq(true)
+      expect(@registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+      
+
+    end
+
+    
+
   end
 end     
 
