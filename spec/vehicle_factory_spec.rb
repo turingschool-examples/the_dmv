0d2f684d3babeae270706fbb88eba5c9d6058c12
Jared Hobson <jaredmhobson@gmail.com>
2024-02-11 12:40:24 -0800

    Add count_vehicle_makes test to vehicle_factory_spec

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -7,15 +7,35 @@ RSpec.describe VehicleFactory do
     expect(factory).to be_an_instance_of(VehicleFactory)
   end
 
-  it 'has no vehicles by default' do
-    expect(factory.vehicles).to be_empty
+  describe 'storing vehicles' do
+    it 'has no vehicles by default' do
+      expect(factory.vehicles).to be_empty
+    end
+
+    it 'can have vehicles' do
+      factory.create_vehicles(wa_ev_registrations)
+
+      expect(factory.vehicles).not_to be_empty
+    end
   end
 
-  it 'can parse registration data from JSON to create vehicles and change their engine to ev if it is nil' do
-    vehicles = factory.create_vehicles(wa_ev_registrations)
+  describe '#create_vehicles' do
+    it 'can parse registration data from JSON to create vehicles and change their engine to ev if it is nil' do
+      factory.create_vehicles(wa_ev_registrations)
 
-    expect(factory.vehicles[0].engine).to eq(:ev)
+      expect(factory.vehicles[0].engine).to eq(:ev)
 
-    expect(factory.vehicles[0]).to be_an_instance_of(Vehicle)
+      expect(factory.vehicles[0]).to be_an_instance_of(Vehicle)
+    end
+  end
+
+  describe '#count_vehicle_makes' do
+    it ' can count how many of each make there is' do
+      factory.create_vehicles(wa_ev_registrations)
+
+      make_counts = factory.count_vehicle_makes
+
+      expect(make_counts[:TESLA]).to be_an_instance_of(Integer)
+    end
   end
 end
