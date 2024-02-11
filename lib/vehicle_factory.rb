9d45f4e67a01490a07a254fe536a71fa85f3aad4
Steddman Bell <steddfast@gmail.com>
2024-02-11 00:56:00 -0800

    Creates save point for Feb 10

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -5,11 +5,16 @@ class VehicleFactory
         @vehicles = []
     end
 
-    # def create_vehicles(data_service)
-    #     @vehicles = data_service.map do |vehicle_info|
-    #         vehicle_info[:eng] = :ev if vehicle_info[:engine] == nil
-    #         vehicle_info[:make] = vehicle_info[:make]
-    #         vehicle_info[:year] = vehicle_info[:model_year]
-    #         vehicle_info[:vin] = vehicle_info[1-10]
-    #     end
-    # end
+    def create_vehicles(data_service)
+        @vehicles = data_service.map do |vehicle_info|
+        vehicle_info[:engine] = :ev if vehicle_info[:engine] == nil
+        vehicle_info[:make] = vehicle_info[:make]
+        vehicle_info[:model] = vehicle_info[:model]
+        vehicle_info[:plate_type] = nil
+        vehicle_info[:registration_date] = nil
+        vehicle_info[:vin] = vehicle_info[:vin_1_10]
+        vehicle_info[:year] = vehicle_info[:model_year]
+        
+        end
+    end
+end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,18 +1,22 @@
 require './lib/vehicle'
 require './lib/vehicle_factory'
-require './dmv_data_service'
+require './lib/dmv_data_service'
 
 RSpec.describe VehicleFactory do
     before(:each) do
         @factory = VehicleFactory.new
         @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
-
     end
 
-    describe '#initialize'do
-        it 'exists'do
+    describe '#initialize' do
+        it 'exists' do
         expect(@factory).to be_an_instance_of(VehicleFactory)
-        expect(@wa_ev_registrations).to be_an_instance_of(DmvDataService)
+        end
+    end
+
+    describe "#create_vehicle" do
+        it 'creates vehicle' do
+        expect(@factory.create_vehicles(@wa_ev_registrations)).to eq(VehicleFactory.vehicles)
         end
     end
 end
