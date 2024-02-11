8fd2f6e6bb85be325fda03151f1464f07e8fe3d5
Steddman Bell <steddfast@gmail.com>
2024-02-10 22:08:07 -0800

    Creates initialize test for vehicle factory spec

---
+++ lib/vehicle_factory.rb
@@ -0,0 +1,14 @@
+class VehicleFactory
+    attr_reader :vehicles
+
+    def initialize
+        @vehicles = []
+    end
+
+    def create_vehicles(data_service)
+        @vehicles = data_service.map do |vehicle_info|
+            vehicle_info[:eng] = :ev if vehicle_info[:engine] == nil
+            vehicle_info[:year] = vehicle_info[:model_year]
+            vehicle_info[:vin] = vehicle_info[1-10]
+        end
+    end
