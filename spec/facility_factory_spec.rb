211795ceca02b48c8dc4128bc0b3c8b5d4bec849
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-12 23:15:01 -0700

    Add RSpec block and before each

---
+++ spec/facility_factory_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+RSpec.describe FacilityFactory do 
+  before(:each) do 
+    @factory = FacilityFactory.new
+    @co_dmv_office_locations = FacilityFactory.new.co_dmv_office_locations
+  end
+
+ 
+end
