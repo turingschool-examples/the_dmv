725c60fafdefbf200388729667b60e53058eafd8
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-12 20:28:03 -0700

    Add different vin, year, and engine spelling ways

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,16 +1,16 @@
 require 'date'
 
 class Vehicle
-  attr_reader :vin, :year,:make,:model,:engine
+  attr_reader :vin, :year, :make, :model, :engine
 attr_accessor :registration_date, :plate_type
              
 
   def initialize(vehicle_details)
-    @vin = vehicle_details[:vin]
-    @year = vehicle_details[:year]
+    @vin = vehicle_details[:vin] || vehicle_details[:vin_1_10]
+    @year = vehicle_details[:year] || vehicle_details[:model_year]
     @make = vehicle_details[:make]
     @model = vehicle_details[:model]
-    @engine = vehicle_details[:engine]
+    @engine = vehicle_details[:engine] || vehicle_details[:electric_vehicle_type]
     @registration_date = nil
     @plate_type = nil
    
