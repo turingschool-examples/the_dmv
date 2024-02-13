d00895e4bfc4e719b6ee1392a3a46382bf685026
Jared Hobson <jaredmhobson@gmail.com>
2024-02-13 10:56:36 -0800

    Update tests to be more clear in facility_spec

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -8,20 +8,23 @@ RSpec.describe Facility do
     let (:camaro) { Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice}) }
     let(:registrant_1) { Registrant.new('Bruce', 18, true ) }
     let(:registrant_2) { Registrant.new('Penny', 16) }
-    let(:registrant_3) { Registrant.new('Tucker', 15) }  
+    let(:registrant_3) { Registrant.new('Tucker', 15) }
+
   describe '#initialize' do
     it 'can initialize' do
       expect(facility_1).to be_an_instance_of(Facility)
       expect(facility_1.name).to eq('DMV Tremont Branch')
       expect(facility_1.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
       expect(facility_1.phone).to eq('(720) 865-4600')
+      expect(facility_1.hours).to eq(nil)
+      expect(facility_1.holidays_closed).to eq(nil)
       expect(facility_1.services).to eq([])
       expect(facility_1.registered_vehicles).to eq([])
       expect(facility_1.collected_fees).to eq(0)
     end
   end
 
-  describe 'services' do
+  describe '#services' do
     it 'can add available services' do
       expect(facility_1.services).to eq([])
       facility_1.add_service('New Drivers License')
@@ -29,7 +32,9 @@ RSpec.describe Facility do
       facility_1.add_service('Vehicle Registration')
       expect(facility_1.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
+  end
 
+  describe '#has_service?' do
     it 'can check if it has a service' do
       expect(facility_1.services).to be_empty
 
@@ -40,68 +45,75 @@ RSpec.describe Facility do
 
       expect(facility_1.has_service?('Written Test')).to be true
     end
+  end
 
-    it 'can administer written tests if it has that service and registrant has permit and is 16 or older or it returns false' do
+  describe '#administer_written_test' do
+    it 'cannot administer written tests if it does not have the service' do
       expect(registrant_1.license_data[:written]).to be false
       expect(registrant_1.permit?).to be true
       expect(registrant_1.age).to eq(18)
 
       expect(facility_1.services).to be_empty
 
-      # to show it returns false if facility doesn't have the service and registrant is eligible
       expect(facility_1.administer_written_test(registrant_1)).to be false
-
       expect(registrant_1.license_data[:written]).to be false
+    end
 
+    it 'can administer written tests if it has the service, the registrant is above 16 and has permit' do
       facility_1.add_service('Written Test')
-      expect(facility_1.services).to eq(['Written Test'])
 
-      # to show it returns true if facility does have the service and registrant is eligible to take written
       expect(facility_1.administer_written_test(registrant_1)).to be true
-
-      # to show it returns false if facility does have the service but the registrant isn't eligible to take written
-      expect(facility_1.administer_written_test(registrant_2)).to be false
-
-
       expect(registrant_1.license_data[:written]).to be true
     end
 
-    it 'can administer road tests if it has that service and registrant passed written or it returns false' do
-      expect(registrant_1.license_data[:license]).to be false
-      expect(registrant_1.license_data[:written]).to be false
+    it 'cannot administer written tests if it has the service but the registrant does not meet the requirements' do
+      expect(registrant_2.license_data[:written]).to be false
+      expect(registrant_2.permit?).to be false
+      expect(registrant_2.age).to eq(16)
 
       facility_1.add_service('Written Test')
 
-      expect(facility_1.services).to eq(['Written Test'])
+      expect(facility_1.administer_written_test(registrant_2)).to be false
+      expect(registrant_2.license_data[:written]).to be false
+    end
+  end
 
-      # to show it returns false if facility doesn't have the service but registrant is eligible to take road test
-      expect(facility_1.administer_road_test(registrant_1)).to be false
-
+  describe '#administer_road_test' do
+    it 'cannot administer road test if registrant meets the requirements but it does not have the service' do
       expect(registrant_1.license_data[:license]).to be false
 
+      facility_1.add_service('Written Test')
+      facility_1.administer_written_test(registrant_1)
+
+      expect(facility_1.services).to eq(['Written Test'])
+
+      expect(facility_1.administer_road_test(registrant_1)).to be false
+    end
+
+    it 'can administer road test if it has the service and registrant has permit and written' do
+      facility_1.add_service('Written Test')
+      facility_1.add_service('Road Test')
+      facility_1.administer_written_test(registrant_1)
+      expect(facility_1.services).to eq(['Written Test', 'Road Test'])
+
+      expect(facility_1.administer_road_test(registrant_1)).to be true
+      expect(registrant_1.license_data[:license]).to be true
+    end
+
+    it 'cannot administer road test if it has the service but registrant does not meet the requirements' do
+      facility_1.add_service('Written Test')
       facility_1.add_service('Road Test')
       expect(facility_1.services).to eq(['Written Test', 'Road Test'])
 
-      facility_1.administer_written_test(registrant_1)
+      facility_1.administer_road_test(registrant_1)
 
-      expect(registrant_1.license_data[:written]).to be true
-
-      # to show it returns true if facility does have the service and registrant is eligible to take road test
-      expect(facility_1.administer_road_test(registrant_1)).to be true
-
-      expect(registrant_1.license_data[:license]).to be true
-
-
-      expect(registrant_2.written?).to be false
-
-      # to show it returns false if facility does have the service but the registrant isn't eligible to take road test
-      expect(facility_1.administer_road_test(registrant_2)).to be false
-    end
-
-    it 'can renew licenses if it has that service and registrant has license or it returns false' do
+      expect(facility_1.administer_road_test(registrant_1)).to be false
       expect(registrant_1.license_data[:license]).to be false
-      expect(registrant_1.license_data[:renewed]).to be false
+    end
+  end
 
+  describe '#renew_drivers_license' do
+    it 'cannot renew license if registrant meets the requirements but it does not have the service' do
       facility_1.add_service('Written Test')
       facility_1.add_service('Road Test')
       expect(facility_1.services).to eq(['Written Test', 'Road Test'])
@@ -109,37 +121,44 @@ RSpec.describe Facility do
       facility_1.administer_written_test(registrant_1)
       facility_1.administer_road_test(registrant_1)
 
-      expect(registrant_1.license_data[:license]).to be true
-
-      # to show it returns false if facility doesn't have the service but registrant is eligible to renew license
       expect(facility_1.renew_drivers_license(registrant_1)).to be false
-
       expect(registrant_1.license_data[:renewed]).to be false
+    end
 
+    it 'can renew license if it has the service and registrant has their license' do
+      facility_1.add_service('Written Test')
+      facility_1.add_service('Road Test')
       facility_1.add_service('Renew License')
-
       expect(facility_1.services).to eq(['Written Test', 'Road Test', 'Renew License'])
 
-      # to show it returns true if facility does have the service and registrant is eligible to renew license
+      facility_1.administer_written_test(registrant_1)
+      facility_1.administer_road_test(registrant_1)
+
       expect(facility_1.renew_drivers_license(registrant_1)).to be true
-
       expect(registrant_1.license_data[:renewed]).to be true
+    end
 
-      registrant_2.earn_permit
-      facility_1.administer_written_test(registrant_2)
+    it 'cannot renew license if it has the service but registrant does not meet the requirements' do
+      facility_1.add_service('Written Test')
+      facility_1.add_service('Road Test')
+      facility_1.add_service('Renew License')
+      expect(facility_1.services).to eq(['Written Test', 'Road Test', 'Renew License'])
 
-      expect(registrant_2.license_data[:license]).to be false
+      facility_1.administer_written_test(registrant_1)
 
-      # to show it returns false if facility does have the service but the registrant isn't eligible to renew license
-      expect(facility_1.renew_drivers_license(registrant_2)).to be false
-
-      expect(registrant_2.license_data[:renewed]).to be false
-
+      expect(facility_1.renew_drivers_license(registrant_1)).to be false
+      expect(registrant_1.license_data[:renewed]).to be false
     end
   end
 
-  describe 'registering vehicles' do
+  describe '#register_vehicle' do
     it 'can register vehicles' do
+      facility_1.register_vehicle(cruz)
+
+      expect(cruz.registered?).to be true
+    end
+
+    it 'will add the registered vehicle to registered_vehicles' do
       facility_1.register_vehicle(cruz)
 
       expect(facility_1.registered_vehicles).to eq([cruz])
@@ -167,6 +186,31 @@ RSpec.describe Facility do
       expect(camaro.plate_type).to eq(:antique)
     end
 
+    it 'cannot register a vehicle that is already registered' do
+      expect(facility_1.registered_vehicles).to be_empty
+      expect(facility_2.registered_vehicles).to be_empty
+
+      facility_1.register_vehicle(cruz)
+
+      expect(facility_1.registered_vehicles).to eq([cruz])
+      expect(facility_2.registered_vehicles).to be_empty
+
+      # to show it can't be registered to facility 1 twice
+      facility_1.register_vehicle(cruz)
+
+      expect(facility_1.registered_vehicles).to eq([cruz])
+
+      facility_2.register_vehicle(cruz)
+
+      expect(facility_2.registered_vehicles).to be_empty
+
+      facility_2.register_vehicle(bolt)
+
+      expect(facility_2.registered_vehicles).to eq([bolt])
+    end
+  end
+
+  describe '#add_collected_fees' do
     it 'can add to collected fees' do
       expect(facility_1.collected_fees).to eq(0)
 
@@ -197,29 +241,6 @@ RSpec.describe Facility do
       facility_1.register_vehicle(camaro)
 
       expect(facility_1.collected_fees).to eq(325)
-    end
-
-    it 'cannot register a vehicle that is already registered' do
-      expect(facility_1.registered_vehicles).to be_empty
-      expect(facility_2.registered_vehicles).to be_empty
-
-      facility_1.register_vehicle(cruz)
-
-      expect(facility_1.registered_vehicles).to eq([cruz])
-      expect(facility_2.registered_vehicles).to be_empty
-
-      # to show it can't be registered to facility 1 twice
-      facility_1.register_vehicle(cruz)
-
-      expect(facility_1.registered_vehicles).to eq([cruz])
-
-      facility_2.register_vehicle(cruz)
-
-      expect(facility_2.registered_vehicles).to be_empty
-
-      facility_2.register_vehicle(bolt)
-
-      expect(facility_2.registered_vehicles).to eq([bolt])
     end
   end
 end
