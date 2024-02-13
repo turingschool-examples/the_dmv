20fa18d1df4876e8d245ebdb4d21afd4d03e354e
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-13 11:31:17 -0700

    Set default value for plate_type

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -2,17 +2,15 @@ class VehicleFactory
 
   def create_vehicles(registrations)
     registrations.map do |registration|
-      vehicle_info = {engine: :ev}
+      vehicle_info = {engine: :ev, plate_type: :ev}
       vehicle_info[:vin] = registration[:vin_1_10]
-      vehicle_info[:year] = registrations.first[:model_year]
+      vehicle_info[:year] = registration[:model_year].to_i
       vehicle_info[:make] = registration[:make]
       vehicle_info[:model] = registration[:model]
-      vehicle_info[:plate_type] = registration.first[:plate_type]
+# require 'pry'; binding.pry
       vehicle_info[:registration_date] = registration[:transaction_date]
 
       Vehicle.new(vehicle_info)
     end
-
   end
-
 end
