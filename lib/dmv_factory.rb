6383327ef73a959b55cf2ed4a0e7bf92b3baa8aa
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-13 15:13:09 -0700

    Add the ability to create facilities in new york and missouri as well as colorado

--- lib/dmv_factory.rb
+++ lib/dmv_factory.rb
@@ -1,7 +1,6 @@
 class DmvFactory
 
-
-  def create_facilities(buildings)
+  def create_colorado_facilities(buildings)
     dmv_buildings = []
     buildings.map do |building|
       facility_info = {}
@@ -14,4 +13,29 @@ class DmvFactory
     end
     dmv_buildings
   end
+
+  def create_new_york_facilities(buildings)
+    dmv_buildings = []
+    buildings.map do |building|
+      facility_info = {}
+      facility_info[:address] = building[:street_address_line_1]
+      facility_info[:zip] = building[:zip_code]
+      facility_info[:phone] = building[:public_phone_number]
+      dmv_buildings << facility_info
+    end
+    dmv_buildings
+  end
+
+  def create_missouri_facilities(buildings)
+    dmv_buildings = []
+    buildings.map do |building|
+      facility_info = {}
+      facility_info[:address] = building[:address1]
+      facility_info[:zip] = building[:zipcode]
+      facility_info[:phone] = building[:phone]
+      dmv_buildings << facility_info
+    end
+    dmv_buildings
+  end
+
 end
