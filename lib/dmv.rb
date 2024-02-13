6b043915c2e9979b1c431de1480d879d01c37ff3
Billy Wallace <wallace.billy1@gmail.com>
2024-02-12 17:10:56 -0700

    Add to #format_address method to account for MO data

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -20,13 +20,15 @@ class Dmv
       dmv_site[:address_li] + " " + dmv_site[:address__1].to_s + " " + dmv_site[:city] + " " + dmv_site[:state] + " " + dmv_site[:zip]
     elsif dmv_site.has_key?(:street_address_line_1)
       dmv_site[:street_address_line_1] + " " + dmv_site[:city] + " " + dmv_site[:state] + " " + dmv_site[:zip_code]
+    elsif dmv_site.has_key?(:address1)
+      dmv_site[:address1] + " " + dmv_site[:city] + " " + dmv_site[:state] + " " + dmv_site[:zipcode]
     end
   end
 
   def create_facilities(dmv_location_data)
     dmv_location_data.map do |dmv_site|
       dmv_facility_details = {
-          name: dmv_site[:dmv_office] || dmv_site[:office_name], 
+          name: dmv_site[:dmv_office] || dmv_site[:office_name] || dmv_site[:name], 
           address: format_address(dmv_site),
           phone: dmv_site[:phone] || dmv_site[:public_phone_number]
         }
@@ -35,27 +37,3 @@ class Dmv
     end
   end
 end
-
-
-# ny_api_data:
-# {:office_name=>"IRONDEQUOIT",
-#   :office_type=>"COUNTY OFFICE",
-#   :public_phone_number=>"5857531604",
-#   :street_address_line_1=>"545 TITUS AVENUE",
-#   :city=>"ROCHESTER",
-#   :state=>"NY",
-#   :zip_code=>"14617",
-#   :monday_beginning_hours=>"8:30 AM",
-#   :monday_ending_hours=>"4:30 PM",
-#   :tuesday_beginning_hours=>"8:30 AM",
-#   :tuesday_ending_hours=>"4:30 PM",
-#   :wednesday_beginning_hours=>"8:30 AM",
-#   :wednesday_ending_hours=>"6:30 PM",
-#   :thursday_beginning_hours=>"8:30 AM",
-#   :thursday_ending_hours=>"4:30 PM",
-#   :friday_beginning_hours=>"8:30 AM",
-#   :friday_ending_hours=>"4:30 PM",
-#   :georeference=>{:type=>"Point", :coordinates=>[-77.601698077, 43.20982583]},
-#   :":@computed_region_yamh_8v7k"=>"545",
-#   :":@computed_region_wbg7_3whc"=>"1676",
-#   :":@computed_region_kjdx_g34t"=>"2093"},
