c66a85bd26f75ef2f625f839ec172eb25ec24382
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-12 17:39:03 -0700

    Complete #renew_drivers_license

--- lib/facility.rb
+++ lib/facility.rb
@@ -33,7 +33,7 @@ class Facility
   end
 
   def administer_written_test(registrant)
-    if registrant.age >= 16 && registrant.permit? && @services.include?("Written Test")
+    if @services.include?("Written Test") && registrant.age >= 16 && registrant.permit?
       registrant.license_data[:written] = true
     else
       false
@@ -41,8 +41,16 @@ class Facility
   end
 
   def administer_road_test(registrant)
-    if registrant.license_data[:written] == true && @services.include?("Road Test")
+    if @services.include?("Road Test") && registrant.license_data[:written] == true
       registrant.license_data[:license] = true
+    else
+      false
+    end
+  end
+
+  def renew_drivers_license(registrant)
+    if @services.include?("Renew License") && registrant.license_data[:license] == true
+      registrant.license_data[:renewed] = true
     else
       false
     end
