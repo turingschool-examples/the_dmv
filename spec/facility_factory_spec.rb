c5f7adbf41ef86ff06e0eb91cdb7f606e1d5f63a
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-13 15:03:47 -0700

    Add New york facilities

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -4,13 +4,30 @@ RSpec.describe FacilityFactory do
   before(:each) do 
     @factory = FacilityFactory.new
     @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+    @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
   end
 
-  describe'#Facility factory'
-    it 'shows where the dmv offices are' do 
+  describe'#Facility factory' do 
+    it 'shows where the dmv offices are in colorado' do 
       expect(@factory).to be_a(FacilityFactory)
       facilities = @factory.create_facilities(@co_dmv_office_locations)
       expect(facilities).to be_a(Array)
-      require 'pry'; binding.pry
+      
+
+      # facilities.each do |facility|
+      #   expect(facility.address).to_not be nil
+      #   require 'pry'; binding.pry
+      #   expect(facility.name).to_not be nil
+      #   expect(facility.phone).to_not be nil
+      # end
     end
+
+    it 'shows where the dmv offices are in New york' do 
+      expect(@factory).to be_a(FacilityFactory)
+      facilities = @factory.create_facilities(@new_york_facilities)
+      expect(facilities).to be_a(Array)
+    end
+
+    
+  end
 end
