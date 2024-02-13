2a6479e790e388929ac618b8e9a36498998f0dfe
Steddman Bell <steddfast@gmail.com>
2024-02-13 10:48:32 -0800

    Creates helper method for pulling api into specific locations

--- lib/facility.rb
+++ lib/facility.rb
@@ -6,16 +6,29 @@ class Facility
               :registered_vehicles,
               :collected_fees
 
-  def initialize(hash_data)
-    hash_data.each do |dmv|
-    dmv[:name] = dmv[:office_name]
-    dmv[:address] = dmv[:address]
-    dmv[:phone] = dmv[:public_phone_number]
+  def initialize
+    @dmv_name = dmv_name
+    @dmv_address = dmv_address
+    @dmv_phone = dmv_phone
+    require 'pry'; binding.pry
     @services = []
     @registered_vehicles = []
     @collected_fees = 0
     @fees = {antique: 25, ev: 200, regular: 100}
-    end
+    
+  end
+
+  #helper method
+  def create_facilities_api_data(hash_data)
+      @dmv_name = hash_data.map do |dmv|
+      dmv[:name] = dmv[:office_name] || dmv[:dmv_office]
+      end
+      @dmv_address = hash_data.map do |dmv|
+      dmv[:address] = (dmv[:street_address_line_1] || dmv[:address_li])
+      end
+      @dmv_phone = hash_data.map do |dmv|
+      dmv[:phone] = dmv[:public_phone_number] || dmv[:phone]
+      end
   end
 
   def add_service(service)
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 
 RSpec.describe Facility do
   before(:each) do
-    
-    @new_york_facilities = Facility.new(DmvDataService.new.ny_dmv_office_locations)
-    @facility_co = Facility.new(DmvDataService.new.co_dmv_office_locations)
-    @facility_mo = Facility.new(DmvDataService.new.mo_dmv_office_locations)
+    @new_york_facilities = Facility.new.create_facilities_api_data(DmvDataService.new.ny_dmv_office_locations)
+    @facility_co = Facility.new.create_facilities_api_data(DmvDataService.new.co_dmv_office_locations)
+    @facility_mo = Facility.new.create_facilities_api_data(DmvDataService.new.mo_dmv_office_locations)
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
@@ -13,41 +12,41 @@ RSpec.describe Facility do
   describe '#initialize' do
     it 'can initialize' do
       expect(@new_york_facilities).to be_an_instance_of(Facility)
-      expect(@@new_york_facilities.name).to eq()
-      expect(@@new_york_facilities.address).to eq()
-      expect(@@new_york_facilities.phone).to eq()
-      expect(@@new_york_facilities.services).to eq([])
+      expect(@new_york_facilities.dmv_name).to eq(Facility[:office_name])
+      expect(@new_york_facilities.dmv_address).to eq(Facility[:street_address_line_1])
+      expect(@new_york_facilities.dmv_phone).to eq(Facility[:public_phone_number])
+      expect(@new_york_facilities.services).to eq([])
     end
   end
 
   describe '#add service' do
-    it 'can add available services' do
-      expect(@@new_york_facilities.services).to eq([])
-      @@new_york_facilities.add_service('New Drivers License')
-      @@new_york_facilities.add_service('Renew Drivers License')
-      @@new_york_facilities.add_service('Vehicle Registration')
-      expect(@@new_york_facilities.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+    xit 'can add available services' do
+      expect(@new_york_facilities.services).to eq([])
+      @new_york_facilities.add_service('New Drivers License')
+      @new_york_facilities.add_service('Renew Drivers License')
+      @new_york_facilities.add_service('Vehicle Registration')
+      expect(@new_york_facilities.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
   end
 
   describe '#register vehicle' do
-    it 'contains registery of vehicles' do
-      expect(@@new_york_facilities.registered_vehicles).to eq([])
+    xit 'contains registery of vehicles' do
+      expect(@new_york_facilities.registered_vehicles).to eq([])
     end
 
-    it 'collects fees' do
-      expect(@@new_york_facilities.collected_fees).to eq(0)
+    xit 'collects fees' do
+      expect(@new_york_facilities.collected_fees).to eq(0)
     end
 
-    it 'can register vehicles' do
+    xit 'can register vehicles' do
       day = Date.today
      
-      @@new_york_facilities.add_service('Vehicle Registration')
-      expect(@@new_york_facilities.register_vehicle(@cruz)).to eq([@cruz])
+      @new_york_facilities.add_service('Vehicle Registration')
+      expect(@new_york_facilities.register_vehicle(@cruz)).to eq([@cruz])
       expect(@cruz.registration_date).to eq(day)
       expect(@cruz.plate_type).to eq(:regular)
-      expect(@@new_york_facilities.registered_vehicles).to eq([@cruz])
-      expect(@@new_york_facilities.collected_fees).to eq(100)
+      expect(@new_york_facilities.registered_vehicles).to eq([@cruz])
+      expect(@new_york_facilities.collected_fees).to eq(100)
     end
 
   end
