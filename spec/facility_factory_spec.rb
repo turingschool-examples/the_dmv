5acea19d98947e426658e9fd9c129dac60fd080e
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-13 11:42:34 -0700

    check if it works

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -3,9 +3,14 @@ require 'spec_helper'
 RSpec.describe FacilityFactory do 
   before(:each) do 
     @factory = FacilityFactory.new
-    @co_dmv_office_locations = FacilityFactory.new.co_dmv_office_locations
+    @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
   end
 
   describe'#Facility factory'
-    it 
+    it 'shows where the dmv offices are' do 
+      expect(@factory).to be_a(FacilityFactory)
+      facilities = @factory.create_facilities(@co_dmv_office_locations)
+      expect(facilities).to be_a(Array)
+      require 'pry'; binding.pry
+    end
 end
