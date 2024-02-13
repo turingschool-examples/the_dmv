ee7aa4852d79065621095debb37f0be13eecd154
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-13 13:50:18 -0700

    Added details to expect statements and reassigned create_vehicles(THIS)

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,17 +1,18 @@
 require './spec/spec_helper'
 
 RSpec.describe VehicleFactory do
-  it "can create vehicles from registrations" do
+  it "can create vehicles from washington registrations" do
     factory = VehicleFactory.new
     wa_ev_registrations = DmvDataService.new.wa_ev_registrations
     all_vehicles = factory.create_vehicles(wa_ev_registrations)
-    single_vehicle = all_vehicles.first
+    wa_vehicle = all_vehicles.first
 
-    expect(single_vehicle.vin).to eq("5YJSA1H2XF")
-    expect(single_vehicle.year).to eq(2015)
-    expect(single_vehicle.make).to eq("TESLA")
-    expect(single_vehicle.model).to eq("Model S")
-    expect(single_vehicle.plate_type).to eq(:ev)
-    expect(single_vehicle.registration_date).to eq("2020-05-27T00:00:00.000")
+    expect(wa_vehicle.vin).to eq("5YJSA1H2XF")
+    expect(wa_vehicle.year).to eq(2015)
+    expect(wa_vehicle.make).to eq("TESLA")
+    expect(wa_vehicle.model).to eq("Model S")
+    expect(wa_vehicle.plate_type).to eq(:ev)
+    expect(wa_vehicle.registration_date).to eq("2020-05-27T00:00:00.000")
   end
+
 end
