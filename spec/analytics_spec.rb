6cd318b018fa4a53a06a494f17080ad1b48eccf4
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-13 18:06:27 -0700

    Add TDD for #most_popular_car

--- spec/analytics_spec.rb
+++ spec/analytics_spec.rb
@@ -1,1 +1,22 @@
 require 'spec_helper'
+
+RSpec.describe Analytics.new do
+
+  before(:each) do
+    @analytics = Analytics.new
+  end
+
+  describe '#initialize' do
+  it 'can initialize' do
+    expect(@analytics).to be_an_instance_of(Analytics)
+  end
+end
+
+  describe "#most_popular_car" do
+    it "finds the most popular car make and model" do
+      expect(@analytics.most_popular_car).to eq("TESLA Model 3")
+    end
+  end
+
+
+end
