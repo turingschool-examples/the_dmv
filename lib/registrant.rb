95478e395d8afeb7d113a9c339863c96c2886d48
Chee Lee <cheeleertr@gmail.com>
2024-03-28 17:52:19 -0500

    Feature registrant class with some tests

---
+++ lib/registrant.rb
@@ -0,0 +1,15 @@
+class Registrant
+    attr_reader :name,
+                :age,
+                :permit
+
+    def initialize(name, age, permit = false)
+        @name = name
+        @age = age
+        @permit = permit
+    end
+
+    def permit?
+        @permit
+    end
+end
---
+++ spec/registrant_spec.rb
@@ -0,0 +1,47 @@
+require "spec_helper"
+
+RSpec.describe Registrant do
+    before(:each) do
+        @registrant_1 = Registrant.new('Bruce', 18, true )
+        @registrant_2 = Registrant.new('Penny', 15 )
+    end
+
+    it "is a class of Registrant" do
+        expect(@registrant_1).to be_an_instance_of(Registrant)
+    end
+
+    it "checks if registrant has a name" do
+        expect(@registrant_1.name).to eq('Bruce')
+    end
+
+    it "checks if registrant has an age" do
+        expect(@registrant_1.age).to eq(18)
+    end
+
+    it "checks if registrant has a permit" do
+        expect(@registrant_1.permit?).to eq(true)
+    end
+
+    xit "" do
+        expect().to eq()
+    end
+    
+    xit "" do
+        expect().to eq()
+    end
+    xit "" do
+        expect().to eq()
+    end
+    
+    xit "" do
+        expect().to eq()
+    end
+
+    xit "" do
+        expect().to eq()
+    end
+    
+    xit "" do
+        expect().to eq()
+    end
+end
