177661c33be2f426284226912da3ded54312101d
Noah Durbin <noahdurbin@gmail.com>
2024-03-28 15:47:45 -0600

    fix: fixed the facilities offered method

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -1,5 +1,5 @@
 class Dmv
-    attr_reader :facilities,
+    attr_reader :facilities
 
   def initialize
     @facilities = []
@@ -10,7 +10,7 @@ class Dmv
   end
 
   def facilities_offering_service(service)
-    @facilities.find do |facility|
+    @facilities.find_all do |facility|
       facility.services.include?(service)
     end
   end
