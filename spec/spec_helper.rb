4494655bb4f4afd190fa14253f0c18ff2352e05b
Zach Bergman <bzach634@gmail.com>
2024-03-28 16:49:29 -0600

    refactor: Add Registrant class file and RSpec formatter to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,8 @@ require './lib/dmv'
 require './lib/facility'
 require './lib/vehicle'
 require './lib/dmv_data_service'
+require './lib/registrant'
+
+RSpec.configure do |config|
+    config.formatter = :documentation
+end
