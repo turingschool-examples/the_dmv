118100a5cd5d24b649990726cb15b6e7a94453f5
Noah Durbin <noahdurbin@gmail.com>
2024-03-28 18:42:22 -0600

    feat: wrote a test for license data and added license data

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -2,12 +2,14 @@ class Registrant
 
     attr_reader :name,
                 :age,
-                :permit
+                :permit,
+                :license_data
 
-    def initialize(name, age, permit)
+    def initialize(name, age, permit = false)
         @name = name
         @age = age
         @permit = permit
+        @license_data = {:written=>false, :license=>false, :renewed=>false}
     end
 
     def permit?
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 RSpec.describe Registrant do
     before (:each) do
         @registrant_1 = Registrant.new('Bruce', 18, true)
+        @registrant_2 = Registrant.new('Penny', 15 )
     end
 
     it 'can initialize' do
@@ -10,5 +11,13 @@ RSpec.describe Registrant do
             expect(@registrant_1.name).to eq('Bruce')
             expect(@registrant_1.age).to be(18)
             expect(@registrant_1.permit?).to eq(true)
+    end
+
+    it "initializes without a permit" do
+       expect(@registrant_2.permit?).to eq(false)
+    end
+
+    it "starts without any license data" do
+       expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
 end
