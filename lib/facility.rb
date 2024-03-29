b7c437c7e044448d4653dd2515b3c3b4c1310da6
Zach Bergman <bzach634@gmail.com>
2024-03-28 21:13:32 -0600

    feat: Add #collect_fees helper into #register_vehicle

--- lib/facility.rb
+++ lib/facility.rb
@@ -22,6 +22,7 @@ class Facility
   def register_vehicle(vehicle)
     if @services.include?('Vehicle Registration')
       @registered_vehicles << vehicle
+      collect_fees(vehicle)
     end
   end
 
