f3208b95ab649706c714a486f7c3644954ba42be
Lance Nelson <lancen04@gmail.com>
2024-03-29 18:07:30 -0400

    created and structured registrant spec file

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,27 @@
+require 'spec helper'
+
+RSpec.describe Registrant do
+    before(:each) do
+        @registrant = Registrant.new
+        @registrant_1 = Registrant.new('Bruce', 18, true)
+        @registrant_2 = Registrant.new('Penny', 15 )
+    end
+end
+
+describe '#initialize' do
+    it 'can initialize' do
+      expect(@registrant).to be_an_instance_of(Registrant)
+      expect(@registrant.name).to eq(name)
+      expect(@registrant.age).to eq(age)
+      expect(@registrant.permit).to be false
+      expect(@registrant.license_date).to eq({:written => false, :license => false, :renewed => false})
+    end)
+    end
+  end
+
+# describe 'earn permit' do
+#     it 'can earn a permit' do
+#         expect(@registrant).
+
+#     end
+# end
