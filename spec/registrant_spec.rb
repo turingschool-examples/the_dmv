cc4dfe9486d0d998900b12c25430f7038d6bda93
Noah Durbin <noahdurbin@gmail.com>
2024-03-28 18:14:48 -0600

    feat: wrote test for initialize

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -5,9 +5,10 @@ RSpec.describe Registrant do
         @registrant_1 = Registrant.new('Bruce', 18, true)
     end
 
-    describe 'initialize' do
-        it 'can initialize' do
-            expect(@registrant_1).to be(Registrant)
-        end
+    it 'can initialize' do
+            expect(@registrant_1).to be_a(Registrant)
+            expect(@registrant_1.name).to eq('Bruce')
+            expect(@registrant_1.age).to be(18)
+            expect(@registrant_1.permit).to be(true)
     end
 end
