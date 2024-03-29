83a06155ae115a1a70bbe01f9afca7c0d08b40f9
Zach Bergman <bzach634@gmail.com>
2024-03-28 19:55:55 -0600

    feat: Add #register_vehicle - store vehicle instances in array

--- lib/facility.rb
+++ lib/facility.rb
@@ -2,16 +2,24 @@ class Facility
   attr_reader :name, 
               :address, 
               :phone,
-              :services
+              :services,
+              :registered_vehicles
 
   def initialize(facility_details)
     @name = facility_details[:name]
     @address = facility_details[:address]
     @phone = facility_details[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    if @services.include?('Vehicle Registration')
+      @registered_vehicles << vehicle
+    end
   end
 end
