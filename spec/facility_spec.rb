8b798af9a2f777bb041a7ebbe3dda2db74878bbe
Garrett Bowman <garrettbowman248@gmail.com>
2024-03-28 19:56:30 -0500

    Test: Added new test to facility to further development

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -23,4 +23,41 @@ RSpec.describe Facility do
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
   end
+
+  describe 'registered_vehicles' do
+    it 'can list registered vehicles' do
+      expect(@facility.registered_vehicles).to eq([])
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      @facility.register_vehicle(cruz)
+      expect(@facility.registered_vehicles).to eq([cruz, bolt])
+    end
+  end
+
+  describe 'register_vehicle' do
+    it 'can register a vehicle' do
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      expect(@facility.register_vehicle(cruz)).to eq([cruz])
+    end
+  end
+
+  describe 'registration_fee' do
+    it 'can calculate registration fee' do
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      @facility.register_vehicle(cruz)
+      expect(@facility.registration_fee).to eq(100)
+    end
+
+    it 'can change vehicle plate type' do
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      @facility.register_vehicle(cruz)
+      expect(@facility.plate_type).to eq('regular')
+    end
+
+    it 'updates collected fees' do
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      @facility.register_vehicle(cruz)
+      expect(@facility.collected_fees).to eq(100)
+    end
+  end
 end
