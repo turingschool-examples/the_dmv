56b60088ff298f6bc8dfe7bb701c1dcb1806fe97
James McHugh <jdmchugh111@gmail.com>
2024-03-28 21:02:06 -0400

    Wrote Registrant class spec test

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+RSpec describe Registrant do
+    before (:each) do
+        @registrant_1 = Registrant.new("Bruce", 18, true)
+        @registrant_2 = Registrant.new("Penny", 15)
+    end
+
+    it "can initialize" do
+        expect(@registrant_1).to be_an_instance_of(Registrant)
+        expect(@registrant_1.name).to eq("Bruce")
+        expect(@registrant_1.age).to eq(18)
+        expect(@registrant_1.permit?).to eq(true)
+        exepect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+
+    it "defaults to false if no value provided for permit" do
+        expect(@registrant_2.permit?).to eq(false)
+    end
+    
+    it "allows registrants to earn their permit" do
+        @registrant_2.earn_permit
+        expect(@registrant_2.earn_permit).to eq(true)
+    end
+end
