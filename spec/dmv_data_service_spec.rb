091577e2402cb407b152a7181ab4866bb1228951
Jan Lehigh <janlehigh461437@gmail.com>
2024-03-29 13:25:44 -0600

    Add registrant_spec.rb file and adds RSpec.configure do |config| method to all spec files

--- spec/dmv_data_service_spec.rb
+++ spec/dmv_data_service_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
+RSpec.configure do |config|
+      config.formatter = :documentation
+end
+
 RSpec.describe DmvDataService do
   before(:each) do
     @dds = DmvDataService.new
