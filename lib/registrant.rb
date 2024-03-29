358fb0a17b6ff386e8c555c8c01dc6382aea7125
James McHugh <jdmchugh111@gmail.com>
2024-03-28 21:51:33 -0400

    Defined Registrant class and wrote additional test

---
+++ lib/registrant.rb
@@ -0,0 +1,20 @@
+class Registrant
+    attr_reader :name,
+                :age,
+                :license_data
+
+    def initialize(name, age, permit = false)
+        @name = name
+        @age = age
+        @permit = permit
+        @license_data = {:written=>false, :license=>false, :renewed=>false}
+    end
+
+    def permit?
+        @permit == true
+    end
+    
+    def earn_permit
+        @permit = true
+    end
+end
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
+require './lib/registrant'
 
-RSpec describe Registrant do
+RSpec.describe Registrant do
     before (:each) do
         @registrant_1 = Registrant.new("Bruce", 18, true)
         @registrant_2 = Registrant.new("Penny", 15)
@@ -11,7 +12,11 @@ RSpec describe Registrant do
         expect(@registrant_1.name).to eq("Bruce")
         expect(@registrant_1.age).to eq(18)
         expect(@registrant_1.permit?).to eq(true)
-        exepect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+        expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+
+    it "returns true if true provided in permit parameter" do
+        expect(@registrant_1.permit?).to eq(true)
     end
 
     it "defaults to false if no value provided for permit" do
