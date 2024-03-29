860c9cd088104a8d0eefb1fd1e07594f8b760fe2
Austin Kenny <austinkennywork@gmail.com>
2024-03-28 20:36:03 -0400

    Debugged Facility Spec

---
+++ lib/registrant.rb
@@ -0,0 +1,12 @@
+class Registrant
+    attr_reader :name, :age, :permit
+
+    def initialize
+        @name = name
+        @age = age
+        @permit = false
+        @license_data = {}
+    end
+
+
+end
---
+++ spec/registrant_spec.rb
@@ -0,0 +1,12 @@
+require "./lib/registrant"
+
+RSpec.describe Registrant do
+    it "exists" do
+        registrant_1 = Registrant.new("Bruce", 18, true )
+
+        expect(registrant_1).to be_instance_of(Registrant)
+    end
+
+    
+
+end
