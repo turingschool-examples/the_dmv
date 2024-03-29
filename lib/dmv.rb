82125d1faeec76f33fdfc2c210a82ddbcf7ee1dd
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-03-28 21:30:04 -0600

    fix: create facilities instance variable and attr_reader

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -1,16 +1,18 @@
 class Dmv
 
+  attr_reader :facilities
+
   def initialize
     @facilities = []
   end
 
-#   def add_facility(facility)
-#     @facilities << facility
-#   end
+  def add_facility(facility)
+    @facilities << facility
+  end
 
-#   def facilities_offering_service(service)
-#     @facilities.find do |facility|
-#       facility.services.include?(service)
-#     end
-#   end
+  # def facilities_offering_service(service)
+  #   @facilities.find do |facility|
+  #     facility.services.include?(service)
+  #   end
+  # end
 end
--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -3,20 +3,20 @@ require 'spec_helper'
 RSpec.describe Dmv do
   before(:each) do
     @dmv = Dmv.new
-    # @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
-    # @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
-    # @facility_3 = Facility.new({name: 'DMV Northwest Branch', address: '3698 W. 44th Avenue Denver CO 80211', phone: '(720) 865-4600'})
+    @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+    @facility_3 = Facility.new({name: 'DMV Northwest Branch', address: '3698 W. 44th Avenue Denver CO 80211', phone: '(720) 865-4600'})
   end
 
   describe '#initialize' do
     it 'can initialize' do
       expect(@dmv).to be_an_instance_of(Dmv)
-      # expect(@dmv.facilities).to eq([])
+      expect(@dmv.facilities).to eq([])
     end
   end
 
   describe '#add facilities' do
-    xit 'can add available facilities' do
+    it 'can add available facilities' do
       expect(@dmv.facilities).to eq([])
       @dmv.add_facility(@facility_1)
       expect(@dmv.facilities).to eq([@facility_1])
