9ee47d5d4f7ee8a4737d3135e27bee53a701c579
Chee Lee <cheeleertr@gmail.com>
2024-03-29 20:36:13 -0500

    Create VehicleFactory classfiles

---
+++ lib/vehicle_factory.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+class VehicleFactory
+    attr_reader
+
+    def initialize
+        
+    end
+end
+# puts wa_ev_registrations
---
+++ spec/vehicle_factory_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+RSpec.describe VehicleFactory do
+  describe 'initialize' do
+    vehicle_factory = VehicleFactory.new
+  end
+
+
+end
