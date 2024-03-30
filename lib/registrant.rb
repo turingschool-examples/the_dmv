7e73be4398ae45cf76b1ccbf8479b443b4c3a92b
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-03-30 15:08:34 -0600

    feature: build and test initialize

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,14 +1,23 @@
 class Registrant
-
+  
   attr_reader :name,
               :age,
               :permit,
               :license_data,
 
   def initialize(name, age, permit = nil)
-       @name = name
-       @age = age
-       @permit = permit
-       @license_data
+    @name = name
+    @age = age
+    @permit = permit
+    @license_data = {
+      written: false,
+      license: false,
+      renewed: false
+    }
+  end
+
+  # def earn_permit
+  #   permit? = true
+  # end
 
 end
