a685f40e088eb81af89cdf827654b8434037a437
Chee Lee <cheeleertr@gmail.com>
2024-03-29 22:26:50 -0500

    Add semi-working code to filter data

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+# require 'spec_helper'
 
 class VehicleFactory
     attr_reader
@@ -6,5 +6,18 @@ class VehicleFactory
     def initialize
         
     end
+
+    def create_vehicles(registrations)
+        registrations.map do |info|
+        info[:name]
+        end
+    end
 end
-# puts wa_ev_registrations
+# wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+
+# if [key] == name || key == make || key == model
+
+
+# will have to use map instead of doing it on index 0
+vehicle_info = wa_ev_registrations[0].select {|key, value| 
+    key == :make || key == :model || key == :model_year || key == :vin_1_10}
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe VehicleFactory do
     vehicle_factory = VehicleFactory.new
   end
 
+  describe 'create_vehicles' do
+    it "can create cars from registrations"
 
+
+    expect(vehicles).to eq([car])
+  end
+#take info
+vehicle_info_array = sort info to only vin, year, make, model, engine
+vehicle_info_array.map |vehicle_info| Car.new(vehicle_info)
 end
