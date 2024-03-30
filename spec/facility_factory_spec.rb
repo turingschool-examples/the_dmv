f530d5d256c7e03cc3e1cfbb4e89b1904bccbba5
Zach Bergman <bzach634@gmail.com>
2024-03-30 12:31:27 -0600

    test: Add tests for #create_facilities - NY DMV

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -4,11 +4,16 @@ RSpec.describe FacilityFactory do
     before(:each) do
         @factory = FacilityFactory.new
         @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+        @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
     end
 
     describe 'loads data' do
         it 'loads CO DMV data' do
             expect(@co_dmv_office_locations).to be_an_instance_of(Array)
+        end
+
+        it 'loads NY DMV data' do
+            expect(@new_york_facilities).to be_an_instance_of(Array)
         end
     end
 
@@ -19,30 +24,60 @@ RSpec.describe FacilityFactory do
     end
 
     describe '#create_facilities' do
-        it 'can use data to create new CO DMV Facility instances' do
-            facilities = @factory.create_facilities(@co_dmv_office_locations)
-            expect(facilities).to be_an_instance_of(Array)
-            expect(facilities.size).to be_an(Integer)
-            expect(facilities.first).to be_an_instance_of(Facility)
-            expect(facilities.last).to be_an_instance_of(Facility)
+        describe 'creating CO facilities' do
+            it 'can use data to create new CO DMV Facility instances' do
+                facilities = @factory.create_facilities(@co_dmv_office_locations)
+                expect(facilities).to be_an_instance_of(Array)
+                expect(facilities.size).to be_an(Integer)
+                expect(facilities.first).to be_an_instance_of(Facility)
+                expect(facilities.last).to be_an_instance_of(Facility)
+            end
+
+            it 'creates proper name for the CO DMV Facility instances' do
+                facilities = @factory.create_facilities(@co_dmv_office_locations)
+                expect(facilities.first.name).not_to eq(nil)
+                expect(facilities.first.name).to be_a(String)
+            end
+
+            it 'creates proper address for the CO DMV Facility instances' do
+                facilities = @factory.create_facilities(@co_dmv_office_locations)
+                expect(facilities.first.address).not_to eq(nil)
+                expect(facilities.first.address).to be_a(String)
+            end
+
+            it 'creates proper phone number for the CO DMV Facility instances' do
+                facilities = @factory.create_facilities(@co_dmv_office_locations)
+                expect(facilities.first.phone).not_to eq(nil)
+                expect(facilities.first.phone).to be_a(String)
+            end
         end
 
-        it 'creates proper name for the CO DMV Facility instances' do
-            facilities = @factory.create_facilities(@co_dmv_office_locations)
-            expect(facilities.first.name).not_to eq(nil)
-            expect(facilities.first.name).to be_a(String)
-        end
+        describe 'creating NY facilities' do
+            it 'can use data to create new NY DMV Facility instances' do
+                facilities = @factory.create_facilities(@new_york_facilities)
+                expect(facilities).to be_an_instance_of(Array)
+                expect(facilities.size).to be_an(Integer)
+                expect(facilities.first).to be_an_instance_of(Facility)
+                expect(facilities.last).to be_an_instance_of(Facility)
+            end
 
-        it 'creates proper address for the CO DMV Facility instances' do
-            facilities = @factory.create_facilities(@co_dmv_office_locations)
-            expect(facilities.first.address).not_to eq(nil)
-            expect(facilities.first.address).to be_a(String)
-        end
+            it 'creates proper name for the NY DMV Facility instances' do
+                facilities = @factory.create_facilities(@new_york_facilities)
+                expect(facilities.first.name).not_to eq(nil)
+                expect(facilities.first.name).to be_a(String)
+            end
 
-        it 'creates proper phone number for the CO DMV Facility instances' do
-            facilities = @factory.create_facilities(@co_dmv_office_locations)
-            expect(facilities.first.phone).not_to eq(nil)
-            expect(facilities.first.phone).to be_a(String)
+            it 'creates proper address for the NY DMV Facility instances' do
+                facilities = @factory.create_facilities(@new_york_facilities)
+                expect(facilities.first.address).not_to eq(nil)
+                expect(facilities.first.address).to be_a(String)
+            end
+
+            it 'creates proper phone number for the NY DMV Facility instances' do
+                facilities = @factory.create_facilities(@new_york_facilities)
+                expect(facilities.first.phone).not_to eq(nil)
+                expect(facilities.first.phone).to be_a(String)
+            end
         end
     end
 
