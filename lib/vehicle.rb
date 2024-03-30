ca72423173bc2c6bfebf23fde0864270157e5d6e
Zach Bergman <bzach634@gmail.com>
2024-03-30 10:02:53 -0600

    refactor: Change plate_type instance variable value

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -16,7 +16,7 @@ class Vehicle
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
     @registration_date = vehicle_details[:registration_date]
-    @plate_type = nil
+    @plate_type = vehicle_details[:plate_type]
   end
 
   def antique?
