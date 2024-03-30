c0bd569f1aec227111eae5bb9ab41b88737a2ff0
Noah Durbin <noahdurbin@gmail.com>
2024-03-29 21:03:36 -0600

    test: wrote a test for vehicle factory

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,1 +1,14 @@
 require 'spec_helper'
+
+describe VehicleFactory do
+    it 'is a vehicle factory' do
+        factory = VehicleFactory.new
+        expect(factory).to be_a(VehicleFactory)
+    end
+
+    it 'creates vehicle objects using api data' do
+        factory = VehicleFactory.new
+        wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+        factory.create_vehicles(wa_ev_registrations)
+    end
+end
