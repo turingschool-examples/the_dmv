5f781cdc3c7ad85b2626c87315862f071042a79b
Zach Bergman <bzach634@gmail.com>
2024-03-30 09:00:35 -0600

    test: Add #initialize test

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe VehicleFactory do
     it 'loads data for WA EV registrations' do
         expect(@wa_ev_registrations).to be_an_instance_of(Array)
     end
+
+    describe '#initialize' do
+        it 'exists' do
+            expect(@factory).to be_an_instance_of(VehicleFactory)
+        end
+    end
 end
