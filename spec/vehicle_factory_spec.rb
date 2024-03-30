4beabc888c953a91060a46fa9d2a19c8a3385c60
Zach Bergman <bzach634@gmail.com>
2024-03-30 08:47:37 -0600

    feat: Add files and test setup

---
+++ spec/vehicle_factory_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+RSpec.describe VehicleFactory do
+    before(:each) do
+        @factory = VehicleFactory.new
+        @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+    end
+end
