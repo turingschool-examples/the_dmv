20ad4711c0264c5d36c462f44789b3508b7e76e2
Zach Bergman <bzach634@gmail.com>
2024-03-30 11:35:49 -0600

    refactor: Fix mistake in tests

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -21,28 +21,28 @@ RSpec.describe FacilityFactory do
     describe '#create_facilities' do
         it 'can use data to create new CO DMV Facility instances' do
             facilities = @factory.create_facilities(@co_dmv_office_locations)
-            expect(@factory.facilities).to be_an_instance_of(Array)
-            expect(@factory.facilities.size).to be_an(Integer)
-            expect(@factory.facilities.first).to be_an_instance_of(Facility)
-            expect(@factory.facilities.last).to be_an_instance_of(Facility)
+            expect(facilities).to be_an_instance_of(Array)
+            expect(facilities.size).to be_an(Integer)
+            expect(facilities.first).to be_an_instance_of(Facility)
+            expect(facilities.last).to be_an_instance_of(Facility)
         end
 
         it 'creates proper name for the CO DMV Facility instances' do
             facilities = @factory.create_facilities(@co_dmv_office_locations)
-            expect(@factory.facilities.first.name).not_to eq(nil)
-            expect(@factory.facilities.first.name).to be_a(String)
+            expect(facilities.first.name).not_to eq(nil)
+            expect(facilities.first.name).to be_a(String)
         end
 
         it 'creates proper address for the CO DMV Facility instances' do
             facilities = @factory.create_facilities(@co_dmv_office_locations)
-            expect(@factory.facilities.first.address).not_to eq(nil)
-            expect(@factory.facilities.first.address).to be_a(String)
+            expect(facilities.first.address).not_to eq(nil)
+            expect(facilities.first.address).to be_a(String)
         end
 
         it 'creates proper phone number for the CO DMV Facility instances' do
             facilities = @factory.create_facilities(@co_dmv_office_locations)
-            expect(@factory.facilities.first.phone).not_to eq(nil)
-            expect(@factory.facilities.first.phone).to be_a(String)
+            expect(facilities.first.phone).not_to eq(nil)
+            expect(facilities.first.phone).to be_a(String)
         end
     end
 end
