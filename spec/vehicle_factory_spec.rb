45e312e50d067545d5ba5d19e924f80a6c442062
Zach Bergman <bzach634@gmail.com>
2024-03-30 09:06:42 -0600

    test: Add test for #create_vehicles

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -15,4 +15,13 @@ RSpec.describe VehicleFactory do
             expect(@factory).to be_an_instance_of(VehicleFactory)
         end
     end
+
+    describe '#create_vehicles' do
+        it 'can use data to create new instances of Vehicle object' do
+            expect(@factory.create_vehicles(@wa_ev_registrations)).to be_an_instance_of(Array)
+            expect(@factory.create_vehicles(@wa_ev_registrations).size).to be_an(Integer)
+            expect(@factory.create_vehicles(@wa_ev_registrations).first).to be_an_instance_of(Vehicle)
+            expect(@factory.create_vehicles(@wa_ev_registrations).last).to be_an_instance_of(Vehicle)
+        end
+    end
 end
