07dda2e6915298561feac0200e45b2d81f047ee5
Clyde Autin <clyde.autin@gmail.com>
2024-03-30 16:18:09 -0700

    Added test for registrant registration details

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe Registrant do
     before(:each) do
         @registrant_1 = Registrant.new('Bruce', 18, true)
         @registrant_2 = Registrant.new('Penny', 15)
+        @registrant_3 = Registrant.new('Tucker', 15 )
     end
 
     describe '#initialize' do
@@ -29,11 +30,15 @@ RSpec.describe Registrant do
         it 'shows if someone has a permit' do
             expect(@registrant_1.permit).to eq(true)
         end
-    end
 
-    describe '#permit' do
         it 'shows if someone does not have a permit' do
             expect(@registrant_2.permit).to eq(false)
+        end
+    end
+
+    describe '#license_data' do
+        it 'shows registration details for a registrant' do
+            expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
         end
     end
 
