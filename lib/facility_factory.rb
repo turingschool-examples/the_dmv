843e97f956f6fe2a2e59e2674096d8ff11721800
Zach Bergman <bzach634@gmail.com>
2024-03-30 11:37:54 -0600

    feat: Add #create_facilities

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,3 +1,13 @@
 class FacilityFactory
-    
+
+    def create_facilities(facility_data)
+        facility_data.map do |facility|
+            facility_details = {
+                name: facility[:dmv_office],
+                address: facility[:address_li],
+                phone: facility[:phone]
+            }
+            Facility.new(facility_details)
+        end
+    end
 end
