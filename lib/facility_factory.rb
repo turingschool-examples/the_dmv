effb9bf0f52ba8ddf619cf5080a255647f9487ee
Zach Bergman <bzach634@gmail.com>
2024-03-30 12:50:48 -0600

    feat: Add logic to #create_facilites, create NY DMV facilities

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -2,17 +2,31 @@ class FacilityFactory
 
     def create_facilities(facility_data)
         facility_data.map do |facility|
-            facility_details = {
-                name: facility[:dmv_office],
-                address: format_co_address(facility[:address_li], facility[:address__1], 
-                facility[:city], facility[:state], facility[:zip]),
-                phone: facility[:phone]
-            }
-            Facility.new(facility_details)
+            if facility[:state] == 'CO'
+                facility_details = {
+                    name: facility[:dmv_office],
+                    address: format_co_address(facility[:address_li], facility[:address__1], 
+                    facility[:city], facility[:state], facility[:zip]),
+                    phone: facility[:phone]
+                }
+                Facility.new(facility_details)
+            elsif facility[:state] == 'NY'
+                facility_details = {
+                    name: facility[:office_name],
+                    address: format_ny_address(facility[:street_address_line_1], 
+                    facility[:city], facility[:state], facility[:zip_code]),
+                    phone: facility[:public_phone_number]
+                }
+                Facility.new(facility_details)
+            end
         end
     end
 
     def format_co_address(address_1, address_2, city, state, zip)
         "#{address_1} #{address_2} #{city} #{state} #{zip}"
+    end
+
+    def format_ny_address(address, city, state, zip)
+        "#{address} #{city} #{state} #{zip}"
     end
 end
