d049a91ee8afc551416f1a80ffca6eb48ac533b1
Zach Bergman <bzach634@gmail.com>
2024-03-30 11:08:51 -0600

    test: Add test for loading CO DMV data

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe FacilityFactory do
         @factory = FacilityFactory.new
         @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
     end
+    
+    describe 'loads data' do
+        it 'loads CO DMV data' do
+            expect(@co_dmv_office_locations).to be_an_instance_of(Array)
+        end
+    end
 end
