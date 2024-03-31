52e6b8939a0071ee51649e1976c1604989ea9c25
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-03-30 20:52:06 -0600

    test: ready to test, not getting errors

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe Facility do
 
   before(:each) do
     @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
-    @facility_2 Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+    @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
   end
   
   describe '#initialize' do
-    it 'can initialize' do
+    xit 'can initialize' do
       expect(@facility).to be_an_instance_of(Facility)
       expect(@facility.name).to eq('DMV Tremont Branch')
       expect(@facility.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
@@ -25,7 +25,7 @@ RSpec.describe Facility do
   end
 
   describe '#add service' do
-    it 'can add available services' do
+    xit 'can add available services' do
       expect(@facility.services).to eq([])
       @facility.add_service('New Drivers License')
       @facility.add_service('Renew Drivers License')
@@ -33,126 +33,126 @@ RSpec.describe Facility do
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
   end
-
-  #NOTE: A facility must offer a service in order to perform it. 
-  #Just because the DMV allows facilities to perform certain services, does not mean that every facility provides every service.
   
   describe '#vehicle registration empty until vehicles added' do
-    it 'can add service without adding vehicles' do
+    xit 'can add service without adding vehicles' do
      @facility_1.add_service('Vehicle Registration')
      expect(@facility.services).to eq(["Vehicle Registration"])
      expect(@cruz.registration_date).to eq(nil)
      expect(@facility_1.registered_vehicles).to eq([])
      expect(@facility_1.collected_fees).to eq(0)
     end 
+  end
 
   describe '#register regular vehicle' do
-    it 'can register a regular vehicle' do
+    xit 'can register a regular vehicle' do
       @facility_1.register_vehicle(@cruz)
       expect(@facility_1.registered_vehicles).to eq([@cruz])
     end
 
-    it 'can log a registration date' do
+    xit 'can log a registration date' do
       @facility_1.register_vehicle(@cruz)
       expect(@cruz.registration_date).to eq Date
     end
 
-    it 'can log a plate type' do
+    xit 'can log a plate type' do
       @facility_1.register_vehicle(@cruz)
       expect(@cruz.plate_type).to eq[:regular] #look up if this should be parentheses
     end 
 
-    it 'can collect fees on regular vehicle' do
+    xit 'can collect fees on regular vehicle' do
       @facility_1.register_vehicle(@cruz)
         expect(@facility_1.collected_fees).to eq (100)
     end
   end
 
   describe '#register antique vehicle' do
-    it 'can register an antique vehicle' do
+    xit 'can register an antique vehicle' do
       @facility_1.register_vehicle(@camaro)
       expect(@facility_1.registered_vehicles).to eq([@camaro])
     end
 
-    it 'can log a registration date' do
+    xit 'can log a registration date' do
       @facility_1.register_vehicle(@camaro)
       expect(@camaro.registration_date).to eq Date
     end
 
-    it 'can log a plate type' do
+    xit 'can log a plate type' do
       @facility_1.register_vehicle(@camaro)
       expect(@camaro.plate_type).to eq[:antique] #look up if this should be parentheses
     end 
 
-    it 'can collect fees on an antique vehicle' do
+    xit 'can collect fees on an antique vehicle' do
       @facility_1.register_vehicle(@camaro)
         expect(@facility_1.collected_fees).to eq (25)
     end
   end
 
   describe '#register EV vehicle' do
-    it 'can register an EV' do
+    xit 'can register an EV' do
       @facility_1.register_vehicle(@bolt)
       expect(@facility_1.registered_vehicles).to eq(@bolt)
     end
 
-    it 'can log a registration date' do
+    xit 'can log a registration date' do
       @facility_1.register_vehicle(@bolt)
-      expect(@bolt).registration_date).to eq Date
+      expect(@bolt.registration_date).to eq Date
     end
 
-    it 'can log a plate type' do
+    xit 'can log a plate type' do
       @facility_1.register_vehicle(@bolt)
       expect(@bolt.plate_type).to eq[:ev] #look up if this should be parentheses
     end 
 
-    it 'can collect fees on an EV' do
+    xit 'can collect fees on an EV' do
       @facility_1.register_vehicle(@bolt)
         expect(@facility_1.collected_fees).to eq (200)
     end
   end
 
   describe '#check registered vehicles' do
-    it 'can collect fees on an EV' do
+    xit 'can collect fees on an EV' do
       @facility_1.register_vehicle(@cruz)
       @facility_1.register_vehicle(@camaro)
       @facility_1.register_vehicle(@bolt)
       expect(@facility_1.collected_fees).to eq (325)
+    end
   end
 
   describe '#check another facility' do
-    it 'has no registrations if registration services unavailable' do
+    xit 'has no registrations if registration services unavailable' do
       expect(@facility_2.registered_vehicles).to eq([])
       expect(@facility_2.services).to eq([])
       @facility_2.register_vehicle(@bolt)
       expect(@facility_2.registered_vehicles).to eq([])
       expect(@facility_1.collected_fees).to eq(0)
     end
+  end
 
 end
 
-      #if
-        #antique $25 fee
-        #EV $200 fee
-        #everything else $100 fee
+          #if
+            #antique $25 fee
+            #EV $200 fee
+            #everything else $100 fee
 
-    #it can record a plate_type et to :regular, :antique, or :ev upon successful registration
-      #if __ 
+        #it can record a plate_type et to :regular, :antique, or :ev upon successful registration
+          #if __ 
 
-  #decribe Administer a written test
-    #it can administer to only some
-      #permit true
-      #age >= 16 
+      #decribe Administer a written test
+        #it can administer to only some
+          #permit true
+          #age >= 16 
 
-  #describe Administer a road test
-    #it can administer to only some
-      #passed written test
+      #describe Administer a road test
+        #it can administer to only some
+          #passed written test
 
-    #it can give license if road test passed
+        #it can give license if road test passed
 
-  #describe renew licence
-    #it can renew license
-      #if road test == pass && earn license
+      #describe renew licence
+        #it can renew license
+          #if road test == pass && earn license
 
 
 
