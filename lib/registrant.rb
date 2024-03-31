7c09cda7ac4146a133888dea44e412d04918e401
Jan Lehigh <janlehigh461437@gmail.com>
2024-03-31 08:08:25 -0600

    Add refactored earn_permit method and initialize method

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -2,11 +2,11 @@
 class Registrant 
     attr_reader :name, :age, :permit, :license_data
 
-    def initialize(registrant_details)
-        @name = registrant_details[:name]
-        @age = registrant_details[:age]
-        @permit = registrant_details.fetch(:permit, false) # uses fetch method to retrieve value from hash based on given key,
-        @license_data = {written: false, license: false, renewed: false} # then specifies default value to return if key does not exist in hash
+    def initialize(name, age, permit = false)
+        @name = name
+        @age = age
+        @permit = permit 
+        @license_data = {written: false, license: false, renewed: false} 
 
     end
 
@@ -15,8 +15,7 @@ class Registrant
     end
 
     def earn_permit
-        @permit = true if @permit != true || @permit = true 
-        # checks if permit is not equal to true, if it is, then it changes it to true
+        @permit = true 
     end
 
 
