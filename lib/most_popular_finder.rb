f8076281e6a284e54fb40abf915e728312a9469c
Zach Bergman <bzach634@gmail.com>
2024-03-31 08:53:05 -0600

    feat: Add #most_popular_make_registered

--- lib/most_popular_finder.rb
+++ lib/most_popular_finder.rb
@@ -1,7 +1,15 @@
 class MostPopularFinder
     attr_reader :factory
-    
+
     def initialize
         @factory = VehicleFactory.new
+    end
+
+    def most_popular_make_registered(vehicle_registrations)
+        vehicle_make_counter = Hash.new(0)
+        @factory.create_vehicles(vehicle_registrations).each do |vehicle|
+            vehicle_make_counter[vehicle.make] += 1
+        end
+        vehicle_make_counter.first[0]
     end
 end
