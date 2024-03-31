5ea76734c51001f48b91aa85076ab299a238872f
Chee Lee <cheeleertr@gmail.com>
2024-03-30 20:57:13 -0500

    Update vehicle_factory, retrieve, filter, update methods being made w/tests

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,23 +1,80 @@
 # require 'spec_helper'
 
 class VehicleFactory
-    attr_reader
+  attr_reader :filtered_data,
+              :created_cars,
+              :retrieved_data
+              :@updated_data
 
-    def initialize
-        
+  def initialize
+    # @filter_keys = [:vin_1_10, :model_year, :make, :model, :engine]
+    @filtered_data = nil
+    @created_cars = []
+#should dat be stored like this? Mainly for testing purposes but how to test w/o it?
+    @retrieved_data = nil
+    @updated_data = nil
+  end
+
+
+  def add_ev_engine(vehicle_info)
+    vehicle_info.map do |vehicle_data|
+      added_ev = vehicle_data[:engine] = :ev
+      added_ev
+# is there a better way to write this? implicit return is the added pair
     end
-
-    def create_vehicles(registrations)
-        registrations.map do |info|
-        info[:name]
+  end
+        #  @filter_keys.include?(key)
+        # is it best to update keys to match or change original keys to match data?
+  def filter_data(registrations)
+      retrieved_data = registrations.map do |hash|
+        hash.select do |key, value|
+          key == :make || key == :model || key == :model_year || key == :vin_1_10
         end
+      end
+      #still an array of hashes with vehicles info
+
+      updated_data = retrieved_data.map do |vehicle_data|
+        vehicle_data.map do |key, value|
+          if key == :model_year
+            key = :year
+            {key => value}
+          elsif key == :vin_1_10 
+            key = :vin
+            {key => value}
+          else
+            {key => value}
+          end
+          #turns to array of hashes separate hash w/one key
+        end
+      end
+      combined_as_hash = {}
+      # .reduce({}, :merge)
+      @filtered_data = updated_data
+      # @filtered_data = add_ev_engine(updated_data)
+      
+      # a.map do |key, value| if key == :model_year then key = :year elsif key == :vin_1_10 then key = :vin else key end
+  end
+
+  def create_vehicles(registrations)
+    filter_data(registrations).map do |data|
+      data.map do |car_data|
+        new_cars = Vehicle.new(data)
+        @created_cars << new_cars
+      end
     end
+  end
+
 end
-# wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+
+
+
+  # wa_ev_registrations = DmvDataService.new.wa_ev_registrations
 
 # if [key] == name || key == make || key == model
 
 
 # will have to use map instead of doing it on index 0
-vehicle_info = wa_ev_registrations[0].select {|key, value| 
-    key == :make || key == :model || key == :model_year || key == :vin_1_10}
+# vehicle_info = wa_ev_registrations[0].select {|key, value| 
+#     key == :make || key == :model || key == :model_year || key == :vin_1_10}
+
+
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -5,13 +5,46 @@ RSpec.describe VehicleFactory do
     vehicle_factory = VehicleFactory.new
   end
 
-  describe 'create_vehicles' do
-    it "can create cars from registrations"
+  # describe "retrieve_data" do
+  #   it "can retrieve data" do
+  #     vehicle_factory = VehicleFactory.new
+  #     wa_ev_registrations = DmvDataService.new.wa_ev_registrations
 
+  #     expect(vehicle_factory.retrieve_data(wa_ev_registrations)).to eq(vehicle_factory.retrieved_data)
+  #   end
+  # end
 
-    expect(vehicles).to eq([car])
+  describe "update data" do
+    it "can update data" do
+      vehicle_factory = VehicleFactory.new
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+      data = vehicle_factory.retrieve_data(wa_ev_registrations)
+
+      expect(vehicle_factory.update_data(data)).to eq(vehicle_factory.updated_data)
+    end
   end
-#take info
-vehicle_info_array = sort info to only vin, year, make, model, engine
-vehicle_info_array.map |vehicle_info| Car.new(vehicle_info)
+
+  describe 'filter data' do
+    it "can filter data" do
+      vehicle_factory = VehicleFactory.new
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+# binding.pry
+      expect(vehicle_factory.filter_data(wa_ev_registrations)).to eq(vehicle_factory.filtered_data)
+      # binding.pry
+    end
+  end
+
+  describe 'create_vehicles' do
+    it "can create cars from registrations" do
+      vehicle_factory = VehicleFactory.new
+      wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+      binding.pry
+      vehicle_factory.filter_data(wa_ev_registrations)
+            # binding.pry
+            #need to know what to test equivalency
+            #error when running, data matches in binding pry
+      expect(vehicle_factory.create_vehicles(wa_ev_registrations)).to eq(vehicle_factory.create_vehicles(wa_ev_registrations))
+        # vehicle_factory.created_cars)
+    end
+  end
 end
