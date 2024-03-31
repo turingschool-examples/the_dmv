deb4f0fb19eed8e1506a9e467007a8b52e45509d
Jan Lehigh <janlehigh461437@gmail.com>
2024-03-30 18:42:39 -0600

    Fixes initialize method in Registrant class to pass rspec test

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -5,8 +5,8 @@ class Registrant
     def initialize(registrant_details)
         @name = registrant_details[:name]
         @age = registrant_details[:age]
-        @permit = registrant_details[:permit]
-        @license_data = {written: false, license: false, renewed: false}
+        @permit = registrant_details.fetch(:permit, false) # uses fetch method to retrieve value from hash based on given key,
+        @license_data = {written: false, license: false, renewed: false} # then specifies default value to return if key does not exist in hash
 
     end
 
