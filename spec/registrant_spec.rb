fbab7c369e5bfcdd362cbfaa4a30840cf35f1d17
Cory Bretsch <corybretsch@gmail.com>
2024-03-31 13:12:56 -0500

    add registrant class

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+RSpec.configure do |config|
+    config.formatter = :documentation
+end
+
+RSpec.describe Registrant do
+    before(:each) do
+        @registrant_1 = Registrant.new('Bruce', 18, true)
+        @registrant_2 = Registrant.new('Penny', 15)
+    end
+
+    describe "#can initialize" do
+        it "it can initialize" do
+            expect(@registrant_1).to be_an_instance_of(Registrant)
+            expect(@registrant_1.age).to eq(18)
+            expect(@registrant_1.name).to eq("Bruce")
+            expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+        end
+    end
+
+    describe "#can earn permit" do
+        it "can has a permit value" do
+            expect(@registrant_1.permit?).to eq(true)
+            expect(@registrant_2.permit?).to eq(false)  
+        end
+        
+        it "can earn permit" do
+            expect(@registrant_2.earn_permit).to eq(true)
+        end
+    end
+end
