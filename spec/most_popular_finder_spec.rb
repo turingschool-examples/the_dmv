f0c25b0a60ec09ffe11afa47f6c9b93afffc23ae
Zach Bergman <bzach634@gmail.com>
2024-03-31 06:44:12 -0600

    test: Add #initialize test and add class file to spec_helper

--- spec/most_popular_finder_spec.rb
+++ spec/most_popular_finder_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe MostPopularFinder do
         @wa_ev_registrations = DmvDataService.new.wa_ev_registrations
         @ny_vehicle_registrations = DmvDataService.new.ny_vehicle_registrations
     end
+
+    describe '#initialize' do
+        it 'exists' do
+            expect(@most_popular_finder).to be_an_instance_of(MostPopularFinder)
+        end
+    end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ require './lib/dmv_data_service'
 require './lib/registrant'
 require './lib/vehicle_factory'
 require './lib/facility_factory'
+require './lib/most_popular_finder'
 
 RSpec.configure do |config|
     config.formatter = :documentation
