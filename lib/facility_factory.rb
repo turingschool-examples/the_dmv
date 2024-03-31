fbf23765c7cc4339ae08ae4ea8fbb7d27cb8b5f8
Chee Lee <cheeleertr@gmail.com>
2024-03-31 15:31:34 -0500

    Refactor FacilityFactory to create_factories, removed helper methods, services is buggy

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -7,43 +7,47 @@ class FacilityFactory
   def create_facilities(registrations)
     registrations.map do |hash|
       hash[:name] = hash.delete :dmv_office
-      # hash[:address] = hash.delete ***:concatonate
-      hash[:services] = hash.delete :services_p.map do |service|
-        clean_services_data(service)
+      hash[:address] = "#{hash[:address_li]}" + "#{hash[:address_1]}" + " #{hash[:city]}" + " #{hash[:state]}" + " #{hash[:zip]}"
+      hash[:services] = "Vehicle Registration" if hash[:services_p].include?("registration")
+
+      # hash[:services] = hash.delete hash[:services_p].map do |service|
+      #   clean_services_data(service)
         # (".sub according services")
-      end
+      
       Facility.new(hash)
     end
   end
+end
 
 
-  def clean_services_data(service_string)
-    services_array = service_string.gsub("registration", "Vehicle Registration").split(", ")
+  # def clean_services_data(service_string)
+  #   services_array = service_string.gsub("registration", "Vehicle Registration").split(", ")
     
-    if services_array.include?("Vehicle Registration")
-      "Vehicle Registration"
-    else
-      nil
-    end
-  end
+  #   if services_array.include?("Vehicle Registration")
+  #     "Vehicle Registration"
+  #   else
+  #     nil
+  #   end
+  # end
 
-  def clean_address_data(array)
+  # def clean_address_data(array)
   # :address_li=>"2855 Tremont Place",
   # :address__1=>"Suite 118",
   # :city=>"Denver",
   # :state=>"CO",
   # :zip=>"80205",
 
+  # "#{a[:address_li]}" + "#{a[:address_1]}" + " #{a[:city]}" + " #{a[:state]}" + " #{a[:zip]}"
+  # "vehicle titles, registration, renewals;  VIN inspections"
+  # a[:services_p].gsub("registration", "Vehicle Registration")
+  # "vehicle titles, Vehicle Registration, renewals;  VIN inspections"
+#   end
+# end
+
   # colorado_facilities = DmvDataService.new.co_dmv_office_locations
 
   # a = colorado_facilities[0]
   # binding.pry
-  # a[:services_p]
-  # "vehicle titles, registration, renewals;  VIN inspections"
-  # a[:services_p].gsub("registration", "Vehicle Registration")
-  # "vehicle titles, Vehicle Registration, renewals;  VIN inspections"
-  end
-end
 # build
   #class reference **********
 #   class Facility
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -9,25 +9,22 @@ RSpec.describe FacilityFactory do
     end
   end
 
-  describe 'clean_service_data' do
-    it "can clean service data" do
-      facility_factory = FacilityFactory.new
-      colorado_facilities = DmvDataService.new.co_dmv_office_locations
-      # binding.pry
-      services_string = colorado_facilities[0][:services_p]
-# binding.pry
-      expect(facility_factory.clean_services_data(services_string)).to eq("Vehicle Registration")
-
-    end
-  end
   describe 'create_facilities' do
-    xit "can create facilities from facility data" do
+    it "can create facilities from facility data" do
       facility_factory = FacilityFactory.new
       colorado_facilities = DmvDataService.new.co_dmv_office_locations
-# binding.pry
-      #need more robust testing for actual vehicle data
-      #maybe use vehicle example checking attr filtered correctly
+      facility_data = [{:the_geom=>{:type=>"Point", :coordinates=>[-105.04992605145736, 39.655588304878336]}, :dmv_id=>"4", :dmv_office=>"DMV Southwest Branch", :address_li=>"3100 S. Sheridan Blvd.", :location=>"Bear Valley Shopping Center", :city=>"Denver", :state=>"CO", :zip=>"80227", :phone=>"(720) 865-4600", :hours=>"Mon, Tue, Thur, Fri  8:00 a.m.- 4:30 p.m. / Wed 8:30 a.m.-4:30 p.m.", :services_p=>"vehicle titles, registration, renewals;  VIN inspections", :parking_no=>"parking in front of the building", :photo=>"images/Sheridan.jpg", :address_id=>"175532", :":@computed_region_nku6_53ud"=>"1444"}]
+      facility_1 = facility_factory.create_facilities(facility_data)
+
+      expect(facility_1[0].name).to eq("DMV Southwest Branch")
+      expect(facility_1[0].address).to eq("3100 S. Sheridan Blvd. Denver CO 80227")
+      expect(facility_1[0].phone).to eq("(720) 865-4600")
+      expect(facility_1[0].services).to eq(["Vehicle Registration"])
+      expect(facility_1[0].registered_vehicles).to eq([])
+      expect(facility_1[0].collected_fees).to eq(0)
+      expect(facility_1[0]).to be_an_instance_of(Facility)
       expect(facility_factory.create_facilities(colorado_facilities)).to all(be_a(Facility))
     end
   end
 end
+
