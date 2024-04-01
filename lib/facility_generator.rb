a6382e00e002c22ff574d78966bbfaad5dc0ed88
James McHugh <jdmchugh111@gmail.com>
2024-04-01 01:11:34 -0400

    Added a couple more tests in facility_generator_spec

--- lib/facility_generator.rb
+++ lib/facility_generator.rb
@@ -5,16 +5,16 @@ class FacilityGenerator
         locations_data.map do |branch|
             if branch[:the_geom] != nil
                 Facility.new({name: branch[:dmv_office], 
-                        address: "#{branch[:address_li]} #{branch[:address_1]} #{branch[:city]} #{branch[:state]} #{branch[:zip]}", 
-                        phone: branch[:phone]})
+                            address: "#{branch[:address_li]} #{branch[:address_1]} #{branch[:city]} #{branch[:state]} #{branch[:zip]}", 
+                            phone: branch[:phone]})
             elsif branch[:georeference] != nil
                 Facility.new({name: "#{branch[:office_name]} #{branch[:office_type]}", 
-                address: "#{branch[:street_address_line_1]} #{branch[:city]} #{branch[:state]} #{branch[:zip_code]}", 
-                phone: branch[:public_phone_number]})
+                            address: "#{branch[:street_address_line_1]} #{branch[:city]} #{branch[:state]} #{branch[:zip_code]}", 
+                            phone: branch[:public_phone_number]})
             else
                 Facility.new({name: branch[:name], 
-                address: "#{branch[:address1]} #{branch[:city]} #{branch[:state]} #{branch[:zipcode]}", 
-                phone: branch[:phone]})
+                            address: "#{branch[:address1]} #{branch[:city]} #{branch[:state]} #{branch[:zipcode]}", 
+                            phone: branch[:phone]})
             end
         end
     end
--- spec/facility_generator_spec.rb
+++ spec/facility_generator_spec.rb
@@ -5,8 +5,12 @@ RSpec.describe FacilityGenerator do
     it "can generate an array of Facility objects from data input" do
         generator = FacilityGenerator.new
         co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+        ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
+        mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
 
         expect(generator.create_facilities(co_dmv_office_locations)[0]).to be_instance_of(Facility)
+        expect(generator.create_facilities(ny_dmv_office_locations)[0]).to be_instance_of(Facility)
+        expect(generator.create_facilities(mo_dmv_office_locations)[0]).to be_instance_of(Facility)
     end
 
     it "won't return nil for key values" do
