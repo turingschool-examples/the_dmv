8e363e70f252c5b36d72ca59c1677350486ce7b8
Austin Kenny <austinkennywork@gmail.com>
2024-03-31 21:38:19 -0400

    It 1 and the start of 2

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -36,4 +36,19 @@ RSpec.describe Registrant do
         expect(registrant_1.permit?).to eq(true)
         expect(registrant_2.permit?).to eq(false)
     end
+
+    it "has liscense data" do
+        registrant_1 = Registrant.new("Bruce", 18, true)
+        registrant_2 = Registrant.new("Penny", 15)
+
+        expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+        expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+
+    it "earn permit" do
+        registrant_1 = Registrant.new("Bruce", 18, true)
+        registrant_2 = Registrant.new("Penny", 15)
+
+        expect(registrant_2.earn_permit).to eq(true)
+    end
 end
