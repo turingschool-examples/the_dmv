96117e929314fc89a0c0b791c628c1b2e683d2b5
James McHugh <jdmchugh111@gmail.com>
2024-03-31 23:15:39 -0400

    Created and Tested VehicleFactory

---
+++ lib/vehicle_factory.rb
@@ -0,0 +1,13 @@
+require 'vehicle'
+
+class VehicleFactory
+    def create_vehicles(registration_data)
+        registration_data.map do |vehicle|
+            Vehicle.new({vin: vehicle[:vin_1_10], 
+                        year: vehicle[:model_year], 
+                        make: vehicle[:make], 
+                        model: vehicle[:model], 
+                        engine: :ev} )
+        end
+    end
+end
