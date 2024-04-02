713d4ef0520780dd6862a7f46076019a4d444c90
Lance Nelson <lancen04@gmail.com>
2024-04-01 21:07:30 -0400

    added register vehicle method along with a corresponding set of tests

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,42 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles, :collected_fees
 
   def initialize(args)
     @name = args[:name]
     @address = args[:address]
     @phone = args[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
   end
+  
+  def register_vehicle(vehicle)
+    @registered_vehicles << vehicle
+    vehicle.registration_date == Date.today
+     if vehicle.antique? == true
+      @collected_fees += 25
+      vehicle.plate_type = :antique
+    
+     elsif vehicle.electric_vehicle? == true
+      @collected_fees += 200
+      vehicle.plate_type = :ev
+      
+     else
+      @collected_fees += 100
+      vehicle.plate_type = :regular
+     end
+
+  
+
+    # elsif @vehicle.engine = :ev
+    #   @collected_fees += 200
+    # elsif @vehicle.engine = :ice
+    #   @collected_fees += 100
+    #end 
+  end
+
 end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -6,7 +6,8 @@ class Vehicle
               :make,
               :model,
               :engine,
-              :registration_date
+              :registration_date,
+              :plate_type
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -14,6 +15,7 @@ class Vehicle
     @make = vehicle_details[:make]
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
+    @plate_type = vehicle_details[:plate_type]
     @registration_date = nil
     
   end
