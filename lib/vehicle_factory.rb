aca4707c550211b4442cde9599506f01427de149
Clyde Autin <clyde.autin@gmail.com>
2024-04-01 18:13:34 -0700

    Can now take vehicle from DMV API and process into our vehicle factory

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -6,6 +6,17 @@ class VehicleFactory
         # @name = name
     end
 
-    
+    def create_vehicle(api)
+        api.map do |car| 
+            vehicle_details = {
+            vin: car[:vin_1_10],
+            year: car[:model_year],
+            make: car[:make],
+            model: car[:model],
+            engine: :ev
+            }
+            Vehicle.new(vehicle_details)
+        end
+    end
 
 end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Facility do
 
         expect(factory).to be_an_instance_of(VehicleFactory)
     end
+
+    it 'can take a vehicle from an API and format it for our needs' do
+        factory = VehicleFactory.new
+        wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+
+        expect(factory.create_vehicle(wa_ev_registrations)).to be_an_instance_of(Array)
+    binding.pry
+    end
 end
