514013c1e3888004e25b8a3883c7ac39d516c2e7
Clyde Autin <clyde.autin@gmail.com>
2024-04-02 11:00:42 -0700

    Start of method to find popular washingtion evs not operational yet

--- lib/fun_facts.rb
+++ lib/fun_facts.rb
@@ -1,3 +1,17 @@
 class FunFacts
-    
+
+    def initialize
+    end
+
+    def most_popular_make(api)
+        make_counts = api.map do |car|
+            make_counts.tally
+        end        
+        most_popular_make = make_counts.max_by do |make, count|
+            most_popular_make.count
+        end
+        most_popular_make = most_popular_make.first
+        most_popular_make
+    end
+
 end
--- spec/fun_facts_spec.rb
+++ spec/fun_facts_spec.rb
@@ -4,3 +4,21 @@ require 'pry'
 RSpec.configure do |config|
     config.formatter = :documentation
 end
+
+RSpec.describe FunFacts do
+    it 'exists' do
+        fun_facts = FunFacts.new
+
+        expect(fun_facts).to be_an_instance_of(FunFacts)
+    end
+
+    it 'can find the most popular ev make of vehicle registered in WA' do
+        washington_fun_facts = FunFacts.new
+        wa_dmvs = DmvDataService.new.wa_ev_registrations
+
+        wa_fun_facts.most_popular_make(wa_dmvs)
+
+        expect(washington_fun_facts).to eq("TESLA")
+        binding.pry
+    end
+end
