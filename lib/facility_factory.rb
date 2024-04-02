0029c72fcacd8f15b9ab1edf45f45adf14cc2c97
Clyde Autin <clyde.autin@gmail.com>
2024-04-02 10:21:20 -0700

    Iteration 3 now complete, can take data from DMV NY, MO and CO Apis to create a facility in our format

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -3,11 +3,35 @@ class FacilityFactory
     def initialize
     end
 
-    def create_facility(api)
+    def create_co_facility(api)
         api.map do |dmv|
             facility_details = {
                 name: dmv[:dmv_office],
                 address: dmv[:address_li],
+                phone: dmv[:phone],
+                services: []
+            }
+            Facility.new(facility_details)
+        end
+    end
+
+    def create_ny_facility(api)
+        api.map do |dmv|
+            facility_details = {
+                name: dmv[:office_name],
+                address: dmv[:street_address_line_1],
+                phone: dmv[:public_phone_number],
+                services: []
+            }
+            Facility.new(facility_details)
+        end
+    end
+
+    def create_mo_facility(api)
+        api.map do |dmv|
+            facility_details = {
+                name: dmv[:number],
+                address: dmv[:address1],
                 phone: dmv[:phone],
                 services: []
             }
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -13,10 +13,25 @@ RSpec.describe FacilityFactory do
         expect(factory).to be_an_instance_of(FacilityFactory)
     end
 
-    it 'can take a DMV from an API and format it for our needs' do
+    it 'can take a Colorado DMV info from an API and format it for our needs' do
         factory = FacilityFactory.new
         co_dmvs = DmvDataService.new.co_dmv_office_locations
 
-        expect(factory.create_facility(co_dmvs)).to be_an_instance_of(Array)
+        expect(factory.create_co_facility(co_dmvs)).to be_an_instance_of(Array)
+    end
+
+    it 'can take a New York DMV info from an API and format it for our needs' do
+        factory = FacilityFactory.new
+        ny_dmvs = DmvDataService.new.ny_dmv_office_locations
+
+        expect(factory.create_ny_facility(ny_dmvs)).to be_an_instance_of(Array)
+    end
+
+    it 'can take a Missouri DMV info from an API and format it for our needs' do
+        factory = FacilityFactory.new
+        mo_dmvs = DmvDataService.new.mo_dmv_office_locations
+
+        expect(factory.create_mo_facility(mo_dmvs)).to be_an_instance_of(Array)
+        binding.pry
     end
 end
