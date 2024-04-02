c1573d340afad846fff7a90ecde387264aecf60f
Noah Durbin <noahdurbin@gmail.com>
2024-04-01 22:04:07 -0600

    fix:refactored vehicle tests

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 RSpec.describe Vehicle do
-  before(:each) do
+  before do
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
   end
+
   describe '#initialize' do
     it 'can initialize' do
       expect(@cruz).to be_an_instance_of(Vehicle)
@@ -32,5 +33,25 @@ RSpec.describe Vehicle do
       expect(@bolt.electric_vehicle?).to eq(true)
       expect(@camaro.electric_vehicle?).to eq(false)
     end
+  end
+
+  describe 'assign_plate' do
+      it 'assigns an electric plate for an ev type' do
+          expect(@bolt.assign_plate).to eq(:ev)
+      end
+
+      it 'assigns antique plates for vehicles 25+ years old' do
+          expect(@camaro.assign_plate).to eq(:antique)
+      end
+
+      it 'assigns regular for other vehicles' do
+          expect(@cruz.assign_plate).to eq(:regular)
+      end
+  end
+
+  describe 'assign registration date' do
+      it 'assigns current date' do
+          expect(@camaro.assign_registration_date).to eq(Date.today)
+      end
   end
 end
