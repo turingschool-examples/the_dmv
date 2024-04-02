63d2730f2ff2a01376b41c99a0e842e505aad19f
Austin Kenny <austinkennywork@gmail.com>
2024-04-01 22:50:52 -0400

    added registration parts to vehicle pt2

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -14,6 +14,7 @@ class Vehicle
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
     @registration_date = nil
+    @plate_type = nil
   end
 
   def antique?
@@ -25,11 +26,22 @@ class Vehicle
   end
 
   def registration_date
-
+    @registration_date = Date.today
+    return @registration_date
   end
-  
+
   def register(facility)
     @registration_date = Date.today
     facility.add_service("Vehicle Registration")
   end
 end
+
+  def plate_type
+    if vehicle.antique?
+      vehicle.set_plate(:antique)
+    elsif vehicle.electric_vehicle?
+      vehicle.set_plate(:ev)
+    else
+      vehicle.set_plate(:regular)
+    end
+  end
