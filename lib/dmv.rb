b82724cc845fb7f347b3355970ccdb49cc94e82c
Candice Cirbo <thecirbos@icloud.com>
2024-05-16 17:35:09 -0600

    fix: The debugging portion of Iteration 1 is done

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -1,4 +1,5 @@
 class Dmv
+   attr_reader :facilities
 
   def initialize
     @facilities = []
@@ -9,8 +10,9 @@ class Dmv
   end
 
   def facilities_offering_service(service)
-    @facilities.find do |facility|
+    @facilities.select do |facility|
       facility.services.include?(service)
     end
+
   end
 end
