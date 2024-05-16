7ea7c87942308c6d4b5e4be476848fc37b05f33f
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-05-16 17:44:22 -0600

    Debugging complete

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,27 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
-
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  attr_reader :name, 
+              :phone, 
+              :address, 
+              :services
+              
+  def initialize(facility_info)
+    @name = facility_info[:name]
+    @phone = facility_info[:phone]
+    @address = facility_info[:address]
     @services = []
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
   end
 end
+
+
+# @facility_2
+  # "New Driver's License"
+  # "Road Test"
+  # "Written Test"
+
+# @facility_3
+  # "New Drivers License"
+  # "Road Test"
