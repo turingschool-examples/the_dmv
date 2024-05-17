27f19ef87a6829ed7b96cb540cd58342d4d2d40e
Karl Fallenius <smilodon.p@gmail.com>
2024-05-17 14:20:28 -0600

    debugging existing code complete

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -1,7 +1,7 @@
 class Dmv
 
+  attr_reader :facilities
   def initialize
-    
     @facilities = []
   end
 
@@ -10,8 +10,7 @@ class Dmv
   end
 
   def facilities_offering_service(service)
-    @facilities.find do |facility|
-      facility.services.include?(service)
-    end
+    @facilities.find_all {|facility| (facility.services.include?(service))}
+    
   end
 end
