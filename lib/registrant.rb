96df0f91fe3476811f55a9df57e01f245b5e28a2
James-Cochran <jameslikesfruit@gmail.com>
2024-05-17 15:46:37 -0400

    wip

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,4 +1,6 @@
 class Registrant
+    attr_reader :facilities
+                :
 
 def initialize (name, age, permit)
     @name = name
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,1 +1,12 @@
-require '.lib/registrant'
+require 'spec_helper'
+
+RSpec.describe Registrant do
+    before (:each) do
+        @registrant_1 = Registrant.new ("Bruce", 18, true)
+        @registrant_2 = Registrant.new ("Penny", 15)
+    end
+
+    describe #initialize do
+    it 'exists' do
+        expect(@registrant_1).to be_an_instance_of(Registrant)
+    end
