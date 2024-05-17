83ba62cf2d40a97adaf9d55e0f49010d739d801e
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-05-17 10:38:13 -0600

    Add IP2 steps to Facility class

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -28,4 +28,91 @@ RSpec.describe Facility do
     end
   end
 end
-binding.pry
+
+# pry(main)> require './lib/facility'
+# #=> true
+
+# pry(main)> require './lib/vehicle'
+# #=> true
+
+# pry(main)> facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+# #=> #<Facility:0x000000010e5ad5e8 @address="2855 Tremont Place Suite 118 Denver CO 80205", @name="DMV Tremont Branch", @phone="(720) 865-4600", @services=[]>
+
+# pry(main)> facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+# #=> #<Facility:0x000000010e5ad480 @address="4685 Peoria Street Suite 101 Denver CO 80239", @name="DMV Northeast Branch", @phone="(720) 865-4600", @services=[]>
+
+# pry(main)> cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+# #=> #<Vehicle:0x0000000135a48b08 @engine=:ice, @make="Chevrolet", @model="Cruz", @plate_type=nil, @registration_date=nil, @vin="123456789abcdefgh", @year=2012>
+
+# pry(main)> bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+# #=> #<Vehicle:0x0000000125832180 @engine=:ev, @make="Chevrolet", @model="Bolt", @plate_type=nil, @registration_date=nil, @vin="987654321abcdefgh", @year=2019>
+
+# pry(main)> camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+# #=> #<Vehicle:0x0000000135adb610 @engine=:ice, @make="Chevrolet", @model="Camaro", @plate_type=nil, @registration_date=nil, @vin="1a2b3c4d5e6f", @year=1969>
+
+# pry(main)> facility_1.add_service('Vehicle Registration')
+# #=> ["Vehicle Registration"]
+
+# pry(main)> cruz.registration_date
+# #=> nil
+
+# pry(main)> facility_1.registered_vehicles
+# #=> []
+
+# pry(main)> facility_1.collected_fees
+# #=> 0
+
+# pry(main)> facility_1.register_vehicle(cruz)
+# #=> [#<Vehicle:0x0000000135a48b08...>]
+
+# pry(main)> cruz.registration_date
+# #=> #<Date: 2023-01-12 ((2459957j,0s,0n),+0s,2299161j)>
+
+# pry(main)> cruz.plate_type
+# #=> :regular
+
+# pry(main)> facility_1.registered_vehicles
+# #=> [#<Vehicle:0x0000000135a48b08...>]
+
+# pry(main)> facility_1.collected_fees
+# #=> 100
+
+# pry(main)> facility_1.register_vehicle(camaro)
+# #=> [#<Vehicle:0x0000000135a48b08...>, #<Vehicle:0x0000000135adb610...>]
+
+# pry(main)> camaro.registration_date
+# #=> #<Date: 2023-01-12 ((2459957j,0s,0n),+0s,2299161j)>
+
+# pry(main)> camaro.plate_type
+# #=> :antique
+
+# pry(main)> facility_1.register_vehicle(bolt)
+# #=> [#<Vehicle:0x0000000135a48b08...>, #<Vehicle:0x0000000135adb610...>, #<Vehicle:0x0000000125832180...>]
+
+# pry(main)> bolt.registration_date
+# #=> #<Date: 2023-01-12 ((2459957j,0s,0n),+0s,2299161j)>
+
+# pry(main)> bolt.plate_type
+# #=> :ev
+
+# pry(main)> facility_1.registered_vehicles
+# #=> [#<Vehicle:0x0000000135a48b08...>, #<Vehicle:0x0000000135adb610...>, #<Vehicle:0x0000000125832180...>]
+
+# pry(main)> facility_1.collected_fees
+# #=> 325
+
+# pry(main)> facility_2.registered_vehicles
+# #=> []
+
+# pry(main)> facility_2.services
+# #=> []
+
+# pry(main)> facility_2.register_vehicle(bolt)
+# #=> nil
+
+# pry(main)> facility_2.registered_vehicles
+# #=> []
+
+# pry(main)> facility_2.collected_fees
+# #=> 0
+
