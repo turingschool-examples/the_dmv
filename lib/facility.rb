fc08adf39e3366084937cae7f7c3fbd2cac2b75f
Kaelin Salazar <kaelinpsalazar@gmail.com>
2024-05-17 07:54:35 -0600

    debugging complete

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,12 @@
 class Facility
   attr_reader :name, :address, :phone, :services
-
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(facilities_info)
+    @name = facilities_info[:name]
+    @address = facilities_info[:address]
+    @phone = facilities_info[:phone]
     @services = []
   end
-
-  def add_services(service)
+  def add_service(service)
     @services << service
   end
 end
