fd97345d96a55595569f318715924231d7fce098
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-05-17 09:00:21 -0600

    Show Registrant class passing tests

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -7,11 +7,11 @@ end
 RSpec.describe Registrant do
   before(:each) do
 
-    registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_1 = Registrant.new('Bruce', 18, true )
     # pry(main)> registrant_1 = Registrant.new('Bruce', 18, true )
     # #=> #<Registrant:0x000000015c10bed8 @age=18, @license_data={:written=>false, :license=>false, :renewed=>false}, @name="Bruce", @permit=true>
     
-    registrant_2 = Registrant.new('Penny', 15 )
+    @registrant_2 = Registrant.new('Penny', 15 )
     # pry(main)> registrant_2 = Registrant.new('Penny', 15 )
     # #=> #<Registrant:0x000000015c0778c8 @age=15, @license_data={:written=>false, :license=>false, :renewed=>false}, @name="Penny", @permit=false>
     
@@ -37,29 +37,28 @@ RSpec.describe Registrant do
     # pry(main)> registrant_1.age
     # #=> 18
     
-    except(@registrant_2.age).to eq(15)
+    expect(@registrant_2.age).to eq(15)
     # pry(main)> registrant_2.age
     # #=> 15
   end
 
   it "can have a permit (#permit?)" do
-    expect(@registrant.permit?).to be(true)
+    expect(@registrant_1.permit?).to be(true)
     # pry(main)> registrant_1.permit?
     # #=> true
 
-    
     expect(@registrant_2.permit?).to be(false)  # I think #permit? defaults to false >> check IP for age requirements, etc.
     # pry(main)> registrant_2.permit?
     # #=> false
   end
 
-  it "can earn a permit (#earn_permit?)" do   # FROM IP: "There is no age limit to when someone can earn their permit."
-    expect(@registrant_2.earn_permit?).to be(false)
+  it "can earn a permit (#earn_permit)" do   # FROM IP: "There is no age limit to when someone can earn their permit."
+    expect(@registrant_2.permit?).to be(false)
     
     @registrant_2.earn_permit
     # pry(main)> registrant_2.earn_permit
     
-    expect(@registrant_2.earn_permit?).to be(true)
+    expect(@registrant_2.permit?).to be(true)
     # pry(main)> registrant_2.permit?
     # #=> true
   end
