11b6d7dbf22f65fe2213ec4b834d93d3d6861466
Evan Davis <evandavis427@gmail.com>
2024-05-17 05:44:53 -0600

    Fixes to Facility class now passes all tests

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,15 @@
 class Facility
   attr_reader :name, :address, :phone, :services
-
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
-    @services = []
+# changed to accept single argument as hash named facility_info
+# added .fetch to use for default service value
+  def initialize(facility_info)
+    @name = facility_info[:name]
+    @address = facility_info[:address]
+    @phone = facility_info[:phone]
+    @services = facility_info.fetch(:services, [])
   end
-
-  def add_services(service)
+# add_services changed to add_service to pass test
+  def add_service(service)
     @services << service
   end
 end
