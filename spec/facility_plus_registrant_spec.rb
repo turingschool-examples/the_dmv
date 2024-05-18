33ad50d8432d5ce8f04e2d7ec893adfe08261633
Evan Davis <evandavis427@gmail.com>
2024-05-18 12:49:49 -0600

    Add tests to new file for both facility and registrant classes

---
+++ spec/facility_plus_registrant_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require './lib/registrant'
+require './lib/facility'
+
+RSpec.describe Facility do
+  before(:each) do
+    @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+  end
+
+  describe '#initialize' do
+    it 'creates a facility' do
+      expect(@facility.name).to eq('DMV Tremont Branch')
+    end
+  end
+
+  describe '#administer_written_test' do
+    it 'returns true and updates license_data when permit is earned' do
+      registrant = Registrant.new('Penny', 16)
+      @facility.add_service('Written Test')
+      expect(@facility.administer_written_test(registrant)).to eq(true)
+      expect(registrant.license_data[:written]).to eq(true)
+    end
+  end
+end
