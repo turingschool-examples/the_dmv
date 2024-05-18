efb8b364bbdd266a6eae68fefeb4fd5ac808d230
Candice Cirbo <thecirbos@icloud.com>
2024-05-17 21:29:21 -0600

    refactor: added pseudo code to class

--- lib/facility.rb
+++ lib/facility.rb
@@ -12,3 +12,23 @@ class Facility
     @services << service
   end
 end
+
+def facilities_offering_service(service)
+  @facilities.select do |facility|
+    facility.services.include?(service)
+  end
+end
+
+
+# Register a vehicle
+# Vehicles have the following rules:
+# Vehicles 25 years old or older are considered antique and cost $25 
+# to register / The vehicle.rb has a test for this and it is working for this
+# facility class
+
+# Electric Vehicles (EV) cost $200 to register / The vehicle.rb has a test for this
+# and it is available to this facility class
+
+# All other vehicles cost $100 to register
+# A vehicle’s plate_type should be set to :regular, :antique, or :ev 
+# upon successful registration
