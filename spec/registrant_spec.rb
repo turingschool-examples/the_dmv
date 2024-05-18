72e5fc724465987da5e58cb67b3aadb61d26b0a3
Karl Fallenius <smilodon.p@gmail.com>
2024-05-17 21:01:23 -0600

    registrant code & rspec completed

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,19 +1,29 @@
 require 'spec_helper'
 
 RSpec.describe Registrant do
-  # before(:each) do
-    @registrant = Registrant.new#(name:, age:, permit:, license_data:)
-    registrant_1 = Registrant.new('Bruce', 18, true )
-    registrant_2 = Registrant.new('Penny', 15 )
+  before(:each) do
+    @registrant = Registrant.new(@name, @age, @permit = false)
+    registrant_1 = Registrant.new('Bruce', 18, true)
+    registrant_2 = Registrant.new('Penny', 15)
   end
 
   describe '#initialize' do
     it 'can initialize' do
       expect(@registrant).to be_an_instance_of(Registrant)
-      expect(permit?).to eq(false)
-      
+      expect(@permit).to be false
     end
   end
 
+  describe 'license data' do
+    it 'has no license data by default' do
+      expect(@registrant.license_data).to eq({:written => false, :license => false, :renewed => false})
+    end
+  end
+
+  describe 'gets a permit' do
+    it 'earns a permit' do
+      expect(@registrant.earn_permit).to be true
+    end
+  end
 
 end
