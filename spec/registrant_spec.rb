900a1766b13ff9f76e586c08678730b383379ebc
Evan Davis <evandavis427@gmail.com>
2024-05-18 07:52:16 -0600

    Add a missing end to registrant_spec

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -8,21 +8,22 @@ RSpec.describe Registrant do
 
     describe '#initialize' do
      it 'makes a registrant with a permit' do
-        expect(@registrant_1.name).to eq('Bruce')
-        expect(@registrant_1.age).to eq(18)
-        expect(@registrant_1.permit?).to eq(true)
-        expect(@registrant_1.license_data).to eq({:written => false, :license => false, :renewed => false})
-     end
+            expect(@registrant_1.name).to eq('Bruce')
+            expect(@registrant_1.age).to eq(18)
+            expect(@registrant_1.permit?).to eq(true)
+            expect(@registrant_1.license_data).to eq({:written => false, :license => false, :renewed => false})
+        end
 
-    it 'makes a registrant with no permit' do
-        expect(@registrant_2.name).to eq('Penny')
-        expect(@registrant_2.age).to eq(15)
-        expect(@registrant_2.permit?).to eq(false)
-        expect(@registrant_2.license_data).to eq({:written => false, :license => false, :renewed => false})
+        it 'makes a registrant with no permit' do
+            expect(@registrant_2.name).to eq('Penny')
+            expect(@registrant_2.age).to eq(15)
+            expect(@registrant_2.permit?).to eq(false)
+            expect(@registrant_2.license_data).to eq({:written => false, :license => false, :renewed => false})
+        end
     end
 
     describe '#earn_permit' do
-     it 'updates permit to true' do
+        it 'updates permit to true' do
         expect(@registrant_2.permit?).to eq(false)
         @registrant_2.earn_permit
         expect(@registrant_2.permit?).to eq(true)
