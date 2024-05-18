8bdfea355f02250b414436bdd2a90c368651c2a8
Candice Cirbo <thecirbos@icloud.com>
2024-05-17 21:41:53 -0600

    refactor: added to require files, new registrant, new test

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,5 +1,7 @@
-require './lib/registrant'
 require 'spec_helper'
+require './lib/registrant'
+require './lib/facility'
+
 
 RSpec.configure do |config|
   config.formatter = :documentation
@@ -9,6 +11,7 @@ RSpec.describe Registrant do
     before (:each) do
       @registrant_1 = Registrant.new('Bruce', 18, true)  
       @registrant_2 = Registrant.new('Penny', 15)
+      @registrant_3 = Registrant.new('Tucker', 15)
     end
 
     describe '#initialize' do
@@ -19,8 +22,11 @@ RSpec.describe Registrant do
             expect(@registrant_1.age).to eq(18)
             expect(@registrant_1.permit).to eq(true)
             expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
-            expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+            expect(@registrant_2.name).to eq('Penny')
+            expect(@registrant_2.age).to eq(15)
             expect(@registrant_2.permit).to eq(false)
+            expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+            
         end
     end
 
@@ -37,6 +43,13 @@ RSpec.describe Registrant do
             @registrant_2.earn_permit 
             expect(@registrant_2.permit?).to eq(true)
         end
+    end
+
+    describe '#written test' do
+        it 'administer a written test' do
+
+        end
+
     end
 end
 
