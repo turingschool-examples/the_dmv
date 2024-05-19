d6d4195e83f3e0ac8765199af1795f0fb5f94ccb
Evan Davis <evandavis427@gmail.com>
2024-05-19 08:37:51 -0600

    Add administer_written_test method to facility class, passes test

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,3 +1,5 @@
+require 'registrant'
+
 class Facility
   attr_reader :name, :address, :phone, :services
 # changed to accept single argument as hash named facility_info
@@ -11,5 +13,14 @@ class Facility
 # add_services changed to add_service to pass test
   def add_service(service)
     @services << service
+  end
+  # administer_written_test method to update license
+  def administer_written_test(registrant)
+    if @services.include?('Written Test') 
+    registrant.license_data[:written] = true
+    return true
+    else 
+    return false
+    end
   end
 end
