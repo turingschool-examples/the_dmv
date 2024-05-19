904e1a04acfb3a5d747d2b82b5edb7d12431e7b4
Evan Davis <evandavis427@gmail.com>
2024-05-19 09:59:51 -0600

    Add methods to facility class for road test and renewing license

--- lib/facility.rb
+++ lib/facility.rb
@@ -10,17 +10,35 @@ class Facility
     @phone = facility_info[:phone]
     @services = facility_info.fetch(:services, [])
   end
-# add_services changed to add_service to pass test
+# add_services changed to add_service 
   def add_service(service)
     @services << service
   end
-  # administer_written_test method to update license
+  # administer_written_test method to update license_data
   def administer_written_test(registrant)
     if @services.include?('Written Test') 
     registrant.license_data[:written] = true
     return true
     else 
     return false
+    end
+  end
+# administer_road_test method to update license_data
+  def administer_road_test(registrant)
+    if @services.include?('Road Test')
+      registrant.license_data[:license] = true
+      return true
+    else
+      return false
+    end
+  end
+# renew_drivers_license method to update license_data
+  def renew_drivers_license(registrant)
+    if @services.include?('Renew License')
+      registrant.license_data[:renewed] = true
+      return true
+    else
+      return false
     end
   end
 end
