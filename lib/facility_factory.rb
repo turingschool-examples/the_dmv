df0ddf79875c59c7e4b8687aced0bb29ddbc66ba
Cory Bretsch <corybretsch@gmail.com>
2024-05-19 21:26:46 -0500

    class: FacilitFactory exists

---
+++ lib/facility_factory.rb
@@ -0,0 +1,7 @@
+class FacilityFactory
+
+
+
+
+  
+end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe FacilityFactory do
   before(:each) do
     @factory = FacilityFactory.new
   end
+
+  describe "#initialize" do
+    it "exists" do
+      expect(@factory).to be_a FacilityFactory
+    end
+  end
+
+
+
+
 end
