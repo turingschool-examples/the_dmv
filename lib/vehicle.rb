6ac70af98b0a87a0e54cdc1a60a75a828948f99e
Evan Davis <evandavis427@gmail.com>
2024-05-20 03:31:50 -0600

    Test now passes vehicle creation test

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,25 +1,22 @@
 require 'date'
-
 class Vehicle
+  
   # added the rest of the keys to attr_reader
-  attr_reader :vin,
-              :year,
-              :make,
-              :model,
-              :engine,
-              :registration_date
+  attr_reader :vin, :year, :make, :model, :engine
+  attr_accessor :registration_date, :plate_type
 
-  def initialize(vehicle_details)
-    @vin = vehicle_details[:vin]
-    @year = vehicle_details[:year]
-    @make = vehicle_details[:make]
-    @model = vehicle_details[:model]
-    @engine = vehicle_details[:engine]
-    @registration_date = vehicle_details[:registration_date]
+  def initialize(vehicle_data)
+    @vin = vehicle_data[:vin]
+    @year = vehicle_data[:year]
+    @make = vehicle_data[:make]
+    @model = vehicle_data[:model]
+    @engine = vehicle_data[:engine]
+    @registration_date = nil
+    @plate_type = nil
   end
 
   def antique?
-    Date.today.year - @year > 25
+    Date.today.year - @year > 25 
   end
 
   def electric_vehicle?
