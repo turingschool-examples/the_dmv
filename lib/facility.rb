09e0833badec125f919ae05b2bba6e4c92d8e8ba
Evan Davis <evandavis427@gmail.com>
2024-05-20 03:33:40 -0600

    facility class updated to include methods for registration and updating permit and license data

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,7 +1,8 @@
 require 'registrant'
-
+require 'date'
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles, :collected_fees
+
 # changed to accept single argument as hash named facility_info
 # added .fetch to use for default service value
   def initialize(facility_info)
@@ -9,6 +10,8 @@ class Facility
     @address = facility_info[:address]
     @phone = facility_info[:phone]
     @services = facility_info.fetch(:services, [])
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 # add_services changed to add_service 
   def add_service(service)
@@ -39,6 +42,26 @@ class Facility
       return true
     else
       return false
+    end
+  end
+  # register_vehicle method to register vehicles
+  # determines fee collection based on plat_type
+  def register_vehicle(vehicle)
+    if @services.include?('Vehicle Registration')
+      vehicle.registration_date = Date.today
+  
+      if vehicle.engine == :ev
+        vehicle.plate_type = :ev
+        @collected_fees += 325
+      elsif vehicle.year < 1990
+        vehicle.plate_type = :antique
+        @collected_fees += 200
+      else
+        vehicle.plate_type = :regular
+        @collected_fees += 100
+      end
+      
+      @registered_vehicles << vehicle
     end
   end
 end
