b00aa0a810d972f8b369792fab1b97fb7cf08cd6
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-05-20 22:38:37 -0600

    facility info 1

--- lib/facility.rb
+++ lib/facility.rb
@@ -12,3 +12,99 @@ class Facility
     @services << service
   end
 end
+
+
+require './lib/facility' = true
+
+require './lib/vehicle' = true
+
+facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+@address = "2855 Tremont Place Suite 118 Denver CO 80205", 
+@name = "DMV Tremont Branch", 
+@phone = "(720) 865-4600", 
+@services = []
+
+facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+@address = "4685 Peoria Street Suite 101 Denver CO 80239", 
+@name = "DMV Northeast Branch", 
+@phone = "(720) 865-4600", 
+@services = []
+
+cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+@engine = :ice, 
+@make = "Chevrolet", 
+@model = "Cruz", 
+@plate_type = nil, 
+@registration_date = nil, 
+@vin = "123456789abcdefgh", 
+@year = 2012
+
+bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+@engine = :ev, 
+@make = "Chevrolet", 
+@model = "Bolt", 
+@plate_type = nil, 
+@registration_date = nil, 
+@vin = "987654321abcdefgh", 
+@year = 2019
+
+camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+@engine = :ice, 
+@make = "Chevrolet", 
+@model = "Camaro", 
+@plate_type = nil, 
+@registration_date = nil, 
+@vin = "1a2b3c4d5e6f", 
+@year = 1969
+
+facility_1.add_service('Vehicle Registration') = ["Vehicle Registration"]
+
+cruz.registration_date = nil
+
+facility_1.registered_vehicles = []
+
+facility_1.collected_fees = 0
+
+facility_1.register_vehicle(cruz)
+#=> [#<Vehicle:0x0000000135a48b08...>]
+
+cruz.registration_date
+#=> #<Date: 2023-01-12 ((2459957j,0s,0n),+0s,2299161j)>
+
+cruz.plate_type = :regular
+
+facility_1.registered_vehicles
+#=> [#<Vehicle:0x0000000135a48b08...>]
+
+facility_1.collected_fees = 100
+
+facility_1.register_vehicle(camaro)
+#=> [#<Vehicle:0x0000000135a48b08...>, #<Vehicle:0x0000000135adb610...>]
+
+camaro.registration_date
+#=> #<Date: 2023-01-12 ((2459957j,0s,0n),+0s,2299161j)>
+
+camaro.plate_type = :antique
+
+facility_1.register_vehicle(bolt)
+#=> [#<Vehicle:0x0000000135a48b08...>, #<Vehicle:0x0000000135adb610...>, #<Vehicle:0x0000000125832180...>]
+
+bolt.registration_date
+#=> #<Date: 2023-01-12 ((2459957j,0s,0n),+0s,2299161j)>
+
+bolt.plate_type = :ev
+
+facility_1.registered_vehicles
+#=> [#<Vehicle:0x0000000135a48b08...>, #<Vehicle:0x0000000135adb610...>, #<Vehicle:0x0000000125832180...>]
+
+facility_1.collected_fees = 325
+
+facility_2.registered_vehicles = []
+
+facility_2.services = []
+
+facility_2.register_vehicle(bolt) = nil
+
+facility_2.registered_vehicles = []
+
+facility_2.collected_fees = 0
---
+++ lib/registrant.rb
@@ -0,0 +1,24 @@
+require './lib/registrant'
+
+registrant_1 = Registrant.new('Bruce', 18, true )
+registrant_2 = Registrant.new('Penny', 15 )
+
+# @age=18, 
+# @license_data={:written=>false, :license=>false, :renewed=>false}, 
+# @name="Bruce", @permit=true
+# @age=15, 
+# @license_data={:written=>false, :license=>false, :renewed=>false}, 
+# @name="Penny", 
+# @permit=false
+
+registrant_1.name = "Bruce"
+registrant_1.age = 18
+registrant_1.permit? = true
+registrant_1.license_data = {:written=>false, :license=>false, :renewed=>false}
+
+registrant_2.name = "Penny"
+registrant_2.age = 15
+registrant_2.permit? = false
+registrant_2.license_data = {:written=>false, :license=>false, :renewed=>false}
+registrant_2.earn_permit = false
+registrant_2.permit? = true
