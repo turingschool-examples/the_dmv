5a98298ed4de273ec82d8ce9231bd6d287752214
Candice Cirbo <thecirbos@icloud.com>
2024-05-20 21:09:33 -0600

    feat: working on adding tests

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -15,9 +15,20 @@ RSpec.describe VehicleFactory do
     end 
 
     it 'can create bulk vehicles' do
-        # expect(@factory.create_vehicles(@wa_ev_registrations).length).to eq(1000)
+        expect(@factory.create_vehicles(@wa_ev_registrations).length).to eq(1000)
         expect(@factory.create_vehicles(@wa_ev_registrations)).to be_an_instance_of(Array)
         expect(@factory.create_vehicles(@wa_ev_registrations)[0]).to be_an_instance_of(Vehicle)
+      
+        expect(@factory.create_vehicles(@wa_ev_registrations)[0].vin).to eq("1N4AZ1BV0R")
+        expect(@factory.create_vehicles(@wa_ev_registrations)[0].year).to eq("2024")
+        expect(@factory.create_vehicles(@wa_ev_registrations)[0].make).to eq("NISSAN")
+        expect(@factory.create_vehicles(@wa_ev_registrations)[0].model).to eq("Leaf")
+      
+        expect(@factory.create_vehicles(@wa_ev_registrations)[250].vin).to eq("5YJYGAEE3M")
+        expect(@factory.create_vehicles(@wa_ev_registrations)[250].year).to eq("2021")
+        expect(@factory.create_vehicles(@wa_ev_registrations)[250].make).to eq("TESLA")
+        expect(@factory.create_vehicles(@wa_ev_registrations)[250].model).to eq("Model Y")
+      
     end
 
 
