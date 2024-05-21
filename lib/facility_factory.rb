6dfffabb633bac17fc62a6ccca0eaa17ddc15677
Candice Cirbo <thecirbos@icloud.com>
2024-05-20 21:44:42 -0600

    feat: added tests and fixed methods

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -11,23 +11,18 @@ class FacilityFactory
                 facility_information[:address] = "#{facility[:address_li]} #{facility[:address_1]} #{facility[:city]} #{facility[:state]} #{facility[:zip]}"
                 facility_information[:phone] = facility[:phone]
                 facility_information[:services] = facility[:services_p]
+
             elsif facility[:office_name]
-
-
+                facility_information[:name] = facility[:office_name]   
+                facility_information[:address] = "#{facility[:street_address_line_1]} #{facility[:street_address_line_2]} #{facility[:city]} #{facility[:state]} #{facility[:zip_code]}"
+                facility_information[:phone] = facility[:public_phone_number]
+               
             elsif facility[:name]
                 facility_information[:name] = facility[:name]   
                 facility_information[:address] = "#{facility[:address1]} #{facility[:city]} #{facility[:state]} #{facility[:zipcode]}"
                 facility_information[:phone] = facility[:phone]
-               
-binding.pry
-
             end     
-                    
-           Facility.new(facility_information)
+        Facility.new(facility_information)
         end 
-        
-
-                        
     end
-    
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -11,23 +11,37 @@ RSpec.configure do |config|
   RSpec.describe FacilityFactory do
     before (:each) do
         @colorado_facilities = DmvDataService.new.co_dmv_office_locations
-        @ffactory = FacilityFactory.new
+        @ffactory = FacilityFactory.new     
         @ny_facilities = DmvDataService.new.ny_dmv_office_locations
         @mo_facilities = DmvDataService.new.mo_dmv_office_locations
     end  
   
-  describe 'colorado facilities' do
-    it 'can create bulk facilities' do
-        # expect(@ffactory.create_facility(@colorado_facilities).length).to eq(5)
-        # # expect(@ffactory.create_facility(@colorado_facilities.address)).to eq(address)
-        # # expect(@ffactory.create_facilities(@colorado_facilities)).to be_an_instance_of(Array)
-        # expect(@ffactory.create_facility(@colorado_facilities)[0]).to be_an_instance_of(Facility)
-        # expect(@ffactory.create_facility(@colorado_facilities)[0].name).to eq("DMV Tremont Branch")
-        expect(@ffactory.create_facility(@colorado_facilities)[0].address).to eq("2855 Tremont Place  Denver CO 80205")
-        # expect(@ffactory.create_facility(@colorado_facilities)[3].address).to eq("3100 S. Sheridan Blvd.  Denver CO 80227")
+    describe '#create colorado facilities' do
+        it 'can create bulk facilities' do
+            expect(@ffactory.create_facility(@colorado_facilities).length).to eq(5)
+            expect(@ffactory.create_facility(@colorado_facilities)[0]).to be_an_instance_of(Facility)
+            expect(@ffactory.create_facility(@colorado_facilities)[0].name).to eq("DMV Tremont Branch")
+            expect(@ffactory.create_facility(@colorado_facilities)[0].address).to eq("2855 Tremont Place  Denver CO 80205")
+            expect(@ffactory.create_facility(@colorado_facilities)[3].address).to eq("3100 S. Sheridan Blvd.  Denver CO 80227")
+        end 
+    end
 
-        # expect(@ffactory.create_facility(@ny_facilities)).to eq("")
-       expect(@ffactory.create_facility(@mo_facilities)).to eq("")
+    describe '#create new york facilities' do
+        it 'can create bulk facilities' do
+            expect(@ffactory.create_facility(@ny_facilities).length).to eq(169)
+            expect(@ffactory.create_facility(@ny_facilities)[0]).to be_an_instance_of(Facility)
+            expect(@ffactory.create_facility(@ny_facilities)[0].name).to eq("IRONDEQUOIT")
+            expect(@ffactory.create_facility(@ny_facilities)[0].address).to eq("545 TITUS AVENUE  ROCHESTER NY 14617")
         end
-    end 
+    end
+
+    describe '#create missouri facilities' do
+        it 'can create bulk facilities' do
+            expect(@ffactory.create_facility(@mo_facilities).length).to eq(178)
+            expect(@ffactory.create_facility(@mo_facilities)[0]).to be_an_instance_of(Facility)
+            expect(@ffactory.create_facility(@mo_facilities)[0].name).to eq("NEVADA")
+            expect(@ffactory.create_facility(@mo_facilities)[0].address).to eq("409 E AUSTIN BLVD NEVADA MO 64772")
+        end
+    end
+    
 end
