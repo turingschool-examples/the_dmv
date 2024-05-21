af3ec08787970e54b88035c7f9d1b4a60323525d
Candice Cirbo <thecirbos@icloud.com>
2024-05-20 21:05:24 -0600

    feat: added creat facility method, changes

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,16 +1,33 @@
-# class VehicleFactory 
+class FacilityFactory 
+    def initialize
+        
+    end
 
-#     def create_vehicles(vehicle_raw_data)
-#         vehicle_raw_data.map do |vehicle| 
-#             Vehicle.new(
-#                 {
-#                     vin: vehicle[:vin_1_10], 
-#                     year: vehicle[:model_year], 
-#                     make: vehicle[:make], 
-#                     model: vehicle[:model], 
-#                     engine: :ev
-#                 }
-#             )
-#         end           
-#     end
-# end
+    def create_facility(facility_raw_data)
+        facility_raw_data.map do |facility| 
+            facility_information = {}
+            if  facility[:dmv_office]
+                facility_information[:name] = facility[:dmv_office]   
+                facility_information[:address] = "#{facility[:address_li]} #{facility[:address_1]} #{facility[:city]} #{facility[:state]} #{facility[:zip]}"
+                facility_information[:phone] = facility[:phone]
+                facility_information[:services] = facility[:services_p]
+            elsif facility[:office_name]
+
+
+            elsif facility[:name]
+                facility_information[:name] = facility[:name]   
+                facility_information[:address] = "#{facility[:address1]} #{facility[:city]} #{facility[:state]} #{facility[:zipcode]}"
+                facility_information[:phone] = facility[:phone]
+               
+binding.pry
+
+            end     
+                    
+           Facility.new(facility_information)
+        end 
+        
+
+                        
+    end
+    
+end
