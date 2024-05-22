92921c4b250628bcaddaae3e55bb960260429b9d
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-05-21 20:01:54 -0600

    uncommenting in facilities

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -3,13 +3,14 @@ require './lib/registrant'
 registrant_1 = Registrant.new('Bruce', 18, true )
 registrant_2 = Registrant.new('Penny', 15 )
 
-# @age=18, 
-# @license_data={:written=>false, :license=>false, :renewed=>false}, 
-# @name="Bruce", @permit=true
-# @age=15, 
-# @license_data={:written=>false, :license=>false, :renewed=>false}, 
-# @name="Penny", 
-# @permit=false
+@age = 18, 
+@license_data = {:written=>false, :license=>false, :renewed=>false}, 
+@name = "Bruce", 
+@permit = true,
+@age = 15, 
+@license_data = {:written=>false, :license=>false, :renewed=>false}, 
+@name = "Penny", 
+@permit = false
 
 registrant_1.name = "Bruce"
 registrant_1.age = 18
